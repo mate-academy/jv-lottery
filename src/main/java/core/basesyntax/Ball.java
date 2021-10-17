1420667d4f9dd33cb9116d5d8fea5c29830b4b41
Oleg Shiriaev <mystidreamer@gmail.com>
2021-10-17 23:28:28 +0300

    lottery ball program with random number and color was created

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,27 +1,11 @@
 package core.basesyntax;
 
 public class Ball {
-    private String color;
-    private int number;
+    public String color;
+    public int number;
 
     public Ball(String color, int number) {
         this.color = color;
-        this.number = number;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
         this.number = number;
     }
 
