3df4f70d135c8ab2b94da5063e4d16f2546f040d
Oleg Shiriaev <mystidreamer@gmail.com>
2021-10-17 23:37:35 +0300

    checkstyle mistaces was fixed

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,11 +1,27 @@
 package core.basesyntax;
 
 public class Ball {
-    public String color;
-    public int number;
+    private String color;
+    private int number;
 
     public Ball(String color, int number) {
         this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
         this.number = number;
     }
 
