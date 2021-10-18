a321cf29fe3b6516ed2c037c959cb2be37e80e2c
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2021-10-18 15:21:18 +0300

    Created a simple lottery that will return balls with a random color and random number.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        String[] colors = new String[] {"White", "Black", "Silver", "Gold", "Bronze"};
+        int count = random.nextInt(colors.length);
+        return colors[count];
     }
 }
