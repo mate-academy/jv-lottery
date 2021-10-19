9b2a3f4f3e1a52ede12ae9eda8668ac5f3d4d655
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-10-20 01:44:43 +0300

    three objects of class Ball was created in class main()

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball firstBall = lottery.getRandomBall();
+        System.out.println(firstBall.toString());
+        Ball secondBall = lottery.getRandomBall();
+        System.out.println(secondBall.toString());
+        Ball thirdBall = lottery.getRandomBall();
+        System.out.println(thirdBall.toString());
     }
 }
