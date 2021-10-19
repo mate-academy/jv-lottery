a2d3e9162a4264122e8d6596c360403ef0cc0952
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-10-20 01:35:22 +0300

    class Ball was created with two fields and one override method

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+public class Ball {
+    private final String color;
+    private final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball color = " + getColor() + ", ball number = " + getNumber();
+    }
+}
