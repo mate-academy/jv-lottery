1a683794696a8a50bf180bd2f1b26a453a111482
Andriy-dz <andriydzdz@gmail.com>
2021-10-19 15:53:21 +0300

    homework solving

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery ball = new Lottery();
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Ball №" + i + ": " + ball.getRandomBall().toString());
+        }
     }
 }
