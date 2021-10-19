e5e425a46101c09ce59de90d8ed696fe1952265e
Sergii Svystun <sergiisvystun@gmail.com>
2021-10-19 16:52:15 +0300

    changed name of Lottery object, method getRandomBall is shorter

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
-        Lottery randomLotteryBall = new Lottery();
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            System.out.println(randomLotteryBall.getRandomBall());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
