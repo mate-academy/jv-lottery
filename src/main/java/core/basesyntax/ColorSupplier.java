559ebf8c8ae2981788663dff3155ac5ff56b748f
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-10-19 18:51:02 +0300

    fixed fields and lines

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     public String getRandomColor() {
         Random random = new Random();
         int index = random.nextInt(Colors.values().length);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int MAX_NUMBER = 100;
+    private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
