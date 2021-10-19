e8812b6df09b80305576a9c997651bd06e35b834
Roman Polulikh <polilikh.r@gmail.com>
2021-10-19 15:11:19 +0300

    fixed reviewer's remarks

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 1; i <= 3; i++) {
-            Ball currentBall = new Lottery().getRandomBall();
+            Ball currentBall = lottery.getRandomBall();
             System.out.println(currentBall.toString());
         }
     }
