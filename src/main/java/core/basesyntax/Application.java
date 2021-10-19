992b13f240a2b1959700af7fa1c8ad7bb0b1fa19
Volodimir <vova.stepanenko2020@gmail.com>
2021-10-19 13:36:14 +0300

    fix task

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class Application {
 
     public static void main(java.lang.String[] args) {
-        // create three balls using class Lottery and print information about them in console
 
         Ball ball1 = Lottery.getRandomBall();
         Ball ball2 = Lottery.getRandomBall();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    private static final int MAX_NUM = 100;
+
     public static Ball getRandomBall() {
         Random random = new Random();
-        final int MAX_NUM = 100;
         int randomNum = random.nextInt(MAX_NUM);
         return new Ball(randomNum, ColorSupplier.getRandomColor());
     }
