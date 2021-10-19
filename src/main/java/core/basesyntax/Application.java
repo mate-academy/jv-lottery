66b7b0267387965a48416b9532fd542d890241ce
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-10-19 14:14:35 +0300

    jv - lottery, fixed x3

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        final int NUMBER_OF_BALLS = 3;
-        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        final int Number_OfBalls = 3;
+        Ball[] balls = new Ball[Number_OfBalls];
         for (Ball ball : balls) {
             Lottery lottery = new Lottery();
             ball = lottery.getRandomBall();
