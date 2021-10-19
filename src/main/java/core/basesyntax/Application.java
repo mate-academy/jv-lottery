5d1832dd9b2e59f16a445533b859c6afcd8faba0
Olha Tankevych <freakystyley85@gmail.com>
2021-10-19 14:53:09 +0300

    added classes lottery and methods

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Ball[] ball = new Ball[3];
         Lottery lottery = new Lottery();
         for (int i = 0; i < ball.length; i++) {
