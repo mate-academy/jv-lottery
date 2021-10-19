2d29d971756a0577c8f8e7d6f48df437b24238d1
farfardesu <savavlad003@gmail.com>
2021-10-19 17:47:19 +0300

    Class Ball was added with color and number fields. Also method toString was overriden. Enum Colors was created. Method getRandomColor() in class ColorSupplier was implemented. Lottery class was added with getRandomBall() method.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball[] balls = {new Lottery().getRandomBall(),
+                        new Lottery().getRandomBall(),
+                        new Lottery().getRandomBall()};
+
+        for (Ball ball : balls) {
+            System.out.println(ball.toString());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    YELLOW,
+    BLACK,
+    ORANGE,
+    GREEN,
+    WHITE,
+    BLUE,
+    ROSE
+}
