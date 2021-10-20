a9afa17a040ecd9c43f293b7778a58e964563cc2
Olha Tankevych <freakystyley85@gmail.com>
2021-10-20 08:49:07 +0300

    rename index and update getRandomBall

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,6 @@ public class Lottery {
     public Ball getRandomBall() {
         ColorSupplier color = new ColorSupplier();
         int number = new Random().nextInt(100);
-        Ball ball = new Ball(number, color.getRandomColor());
-        return ball;
+        return new Ball(number, color.getRandomColor());
     }
 }
