7e7cb99f06a12592e2bf3707d26ff39bb59be3f6
Kate Deshchinska <katerinka9636@gmail.com>
2021-10-20 20:17:00 +0300

    The lottery for ball was created

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        return new Ball(ColorSupplier.getRandomColor(),new Random().nextInt(100));
+    }
+}
