a1a64cf5b629460e1a4a98dee6bdfda158d8b11b
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-10-20 13:32:32 +0300

    changes after review were made

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-
     private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             balls[i] = lottery.getRandomBall();
         }
         for (Ball ball : balls) {
