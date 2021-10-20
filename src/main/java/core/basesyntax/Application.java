a8240debfbf2d6d89c01efa31f2c7a034b215413
Vladyslav Chyver <lione2009lione@gmail.com>
2021-10-20 13:05:59 +0300

    enum Color, class Lottery , Ball fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball[] balls = new Ball[3];
+        for (int counter = 0; counter < balls.length; counter++) {
+            balls[counter] = new Lottery().getRandomBall();
+            System.out.println(balls[counter]);
+        }
     }
 }
