a6778dcfba74dbd5db5101d98eb22ff1bba5967a
Volodimir <vova.stepanenko2020@gmail.com>
2021-10-20 09:49:18 +0300

    delete empty line

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     public Color getRandomColor() {
         int randomNum = new Random().nextInt(Color.values().length);
         return Color.values()[randomNum];
