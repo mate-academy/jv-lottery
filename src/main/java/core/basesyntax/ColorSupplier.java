790ad1e3a6d61c584aecd13d542fe1a5f954be62
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-10-20 11:33:25 +0300

    Rename enam

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         Random random = new Random();
-        int index = random.nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].toString();
     }
 }
