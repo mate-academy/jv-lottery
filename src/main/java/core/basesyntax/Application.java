6c99d13b2a76d9596fb0c1cb10b715cb29489a18
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-10-20 12:00:10 +0300

    object lottery and other magic were added

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+
+    private static int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
         }
         for (Ball ball : balls) {
             System.out.println(ball.toString());
