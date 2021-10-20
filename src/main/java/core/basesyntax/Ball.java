a7bd2d9329f0ce03f0644a29931cf8f38cbb7747
Anatolii Huryk <tolik.mario96@gmail.com>
2021-10-20 13:49:19 +0300

    lottery

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -11,9 +11,10 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball {"
+                + "color = '" + color
+                + '\''
+                + ", number = " + number
+                + '}';
     }
 }
