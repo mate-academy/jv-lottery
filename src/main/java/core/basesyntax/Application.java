8a914ff1e178d0a8c1dd717056a0bab2644588bd
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-10-20 09:01:38 +0300

    massive balls was implemeted and foreach loop was implemented

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball firstBall = lottery.getRandomBall();
-        System.out.println(firstBall.toString());
-        Ball secondBall = lottery.getRandomBall();
-        System.out.println(secondBall.toString());
-        Ball thirdBall = lottery.getRandomBall();
-        System.out.println(thirdBall.toString());
+        Ball[] balls = {lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
+        for (Ball ball : balls) {
+            System.out.println(ball.toString());
+        }
     }
 }
