5246f506634e4d7daa5824d4215d563d1df5778a
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-10-20 08:30:20 +0300

    jv - lottery, fixed x5

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        Colors color = Colors.values()[index];
+        return color.toString();
     }
 }
