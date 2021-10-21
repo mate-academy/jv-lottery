294b77c5f69dfec67bedcde53b41f3663d9ef4e4
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-10-21 12:12:26 +0300

    Ball creation removed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball = new Ball();
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            ball = lottery.getRandomBall();
+            Ball ball = lottery.getRandomBall();
             System.out.println(ball.toString());
         }
     }
