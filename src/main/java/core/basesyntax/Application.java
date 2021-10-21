9abcf472c86605edccc044a87fcedecf8bb42d95
Kate Deshchinska <katerinka9636@gmail.com>
2021-10-21 13:29:22 +0300

    move constant to Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        Ball[] balls = new Ball[Ball.getNumberOfBalls()];
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
         Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,17 +1,12 @@
 package core.basesyntax;
 
 public class Ball {
-    private static final int NUMBER_OF_BALLS = 3;
     private String color;
     private int number;
 
     Ball(String color, int number) {
         this.color = color;
         this.number = number;
-    }
-
-    public static int getNumberOfBalls() {
-        return NUMBER_OF_BALLS;
     }
 
     public String toString() {
