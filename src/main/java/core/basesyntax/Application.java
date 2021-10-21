7844f6da15a820fac28fade16b4396b644d95119
Svitlana <wsv3ta@gmail.com>
2021-10-21 16:57:06 +0300

    extra comment куÑ removed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
         Ball ball1 = lottery.getRandomBall();
         Ball ball2 = lottery.getRandomBall();
