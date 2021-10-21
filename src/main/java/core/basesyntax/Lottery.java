fbc86424df83ecda56885261e983f6b482fe821d
Dany Nesterov <nesterov1200997@gmail.com>
2021-10-21 09:29:34 +0300

    Required changes are fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Lottery {
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
-        Ball ball = new Ball(supplier.getRandomColor(), new Random().nextInt(100));
-        return ball;
+        return new Ball(supplier.getRandomColor(), new Random().nextInt(100));
     }
 }
