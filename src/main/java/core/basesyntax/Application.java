b94f67f888c48bffeebcb1862c78971c6f84400e
Kate Deshchinska <katerinka9636@gmail.com>
2021-10-21 13:11:52 +0300

    private modifier was added, lottery was created

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-
-        Ball[] balls = new Ball[Ball.NUMBER_OF_BALLS];
+        Ball[] balls = new Ball[Ball.getNumberOfBalls()];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
         }
     }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,13 +1,17 @@
 package core.basesyntax;
 
 public class Ball {
-    static final int NUMBER_OF_BALLS = 3;
+    private static final int NUMBER_OF_BALLS = 3;
     private String color;
     private int number;
 
     Ball(String color, int number) {
         this.color = color;
         this.number = number;
+    }
+
+    public static int getNumberOfBalls() {
+        return NUMBER_OF_BALLS;
     }
 
     public String toString() {
