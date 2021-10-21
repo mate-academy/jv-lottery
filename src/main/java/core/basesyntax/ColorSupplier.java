cf4a11fd6198ef5c48b6c35bac7bdc7099efeab3
Yurii Khomiak <ykhomiak@st.swps.edu.pl>
2021-10-21 13:12:47 +0200

    Checkstyle fixed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor(){
-    int randomColor = new Random().nextInt(Color.values().length);
+    public String getRandomColor() {
+        int randomColor = new Random().nextInt(Color.values().length);
         return String.valueOf(Color.values()[randomColor]);
     }
 }
