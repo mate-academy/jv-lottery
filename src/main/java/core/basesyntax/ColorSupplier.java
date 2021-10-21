b6b31fca803acb47a1a3a14a13624a9a5bdae2f9
Yurii Khomiak <ykhomiak@st.swps.edu.pl>
2021-10-21 14:13:52 +0200

    Objects were named as requested

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int randomColor = new Random().nextInt(Color.values().length);
-        return String.valueOf(Color.values()[randomColor]);
+        int randomColorIndex  = new Random().nextInt(Color.values().length);
+        return String.valueOf(Color.values()[randomColorIndex ]);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        ColorSupplier color = new ColorSupplier();
-        return new Ball(color.getRandomColor(), new Random().nextInt(100));
+        ColorSupplier colorSupplier  = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(100));
     }
 }
