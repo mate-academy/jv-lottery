ddd6bc9d209c5b8b65eb6b3e105f4635133e082a
Anatolii Huryk <tolik.mario96@gmail.com>
2021-10-21 10:02:53 +0300

    lottery3

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+
+    static final int constant = 3;
+
     public static void main(String[] args) {
-        int constant = 3;
         Lottery lottery = new Lottery();
         for (int i = 0; i < constant; i++) {
             System.out.println(lottery.getRandomBall());
