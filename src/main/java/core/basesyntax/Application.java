8f2dfd3892dd9f8c89b958433abc920738741dc3
Dima Barshay <barshayerum8@gmail.com>
2021-10-21 14:21:56 +0300

    optimized balls creation

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball [] balls = new Ball[]{
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall()
-        };
+        Lottery lottery = new Lottery();
+        Ball [] balls = new Ball[3];
 
         for (Ball ball : balls) {
+            ball = lottery.getRandomBall();
             System.out.println(ball);
         }
     }
