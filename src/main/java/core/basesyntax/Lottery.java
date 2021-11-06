b011ec8544693ada9d30a04d1fa4626c06236947
Stanislav <stassonich@icloud.com>
2021-11-06 19:10:56 +0200

    added 3 colors for random numbers of balls

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
+
         ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(101));
     }
