ac426848356ed9debff9c072b2a5770f6193cba0
Stanislav <stassonich@icloud.com>
2021-11-08 20:46:51 +0200

    Внес изменения по трем классам.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(100));
