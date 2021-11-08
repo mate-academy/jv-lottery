c290628d44d1851687bb3ce11b66169c8e65b9a2
Stanislav <stassonich@icloud.com>
2021-11-08 20:37:27 +0200

    Внес изменения по трем классам.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,7 +6,7 @@ public class ColorSupplier {
     private Random random = new Random();
 
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+   private Random random = new Random();
+   ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(100));
     }
 }
