29fffaefd32242f479047dc56fca521d2368f6c0
Serhii Petrenko <Poshtasp76@gmail.com>
2021-11-09 20:55:50 +0200

    ColorSupplier.java: - Random variable moved to class. Lottery.java: - Random and ColorSupplier variables moved to class.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public Color getRandomColor() {
-        return Color.values()[(new Random()).nextInt(Color.values().length)];
+    private Random random = new Random();
+
+    public String getRandomColor() {
+        return String.valueOf(Color.values()[random.nextInt(Color.values().length)]);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        Random random = new Random();
-        return new Ball((new ColorSupplier()).getRandomColor().toString(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
     }
 }
