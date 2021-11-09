3465b8b0853db833a452c544fbd6a9cf137c183e
dmitriy-tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-11-09 17:54:11 +0100

    added Solution

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    public Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(100);
+        return new Ball(color, number);
+
+    }
+}
