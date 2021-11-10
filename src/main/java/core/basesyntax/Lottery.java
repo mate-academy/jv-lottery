f96f2e92e526afe821b441e84df69e6be18b28f9
dmitriy-tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-11-10 20:29:13 +0100

    fixed Build Errored

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
-    String color = colorSupplier.getRandomColor();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    private String color = colorSupplier.getRandomColor();
 
     public Ball getRandomBall() {
         int number = new Random().nextInt(100);
