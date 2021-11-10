f0cd6e39ca3ed329edaeb432e68734c4b8af3206
dmitriy-tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-11-10 20:32:22 +0100

    delete line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
-
     private String color = colorSupplier.getRandomColor();
 
     public Ball getRandomBall() {
