7c9da1548fd1989223bb1c63170ed34703a77993
dmitriy-tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-11-10 20:15:46 +0100

    fixed Solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-
         Lottery lottery = new Lottery();
         for (int k = 0; k < 3; k++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    ColorSupplier colorSupplier = new ColorSupplier();
+    String color = colorSupplier.getRandomColor();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String color = colorSupplier.getRandomColor();
         int number = new Random().nextInt(100);
         return new Ball(color, number);
-
     }
 }
