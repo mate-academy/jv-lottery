3399ac48657d712bea1a680f425019abb18a45e7
dmitriy-tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-11-11 19:37:14 +0100

    move the variable random and colorSupplier to the class level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int index = new Random().nextInt(Colors.values().length);
+    private final Random random = new Random();
 
     public String getRandomColor() {
+        int index = random.nextInt(Colors.values().length);
         return "" + Colors.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private String color = colorSupplier.getRandomColor();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        int number = new Random().nextInt(100);
+        int number = random.nextInt(100);
+        String color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
 }
