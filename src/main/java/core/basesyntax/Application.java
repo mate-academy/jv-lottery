4e7442f6409e80c63557c79e74a51218ed969e54
Evgen Lanovenko <ev.lanovenko@gmail.com>
2021-11-13 23:45:22 +0200

    Created 3 classes and method getRandomBall()

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery1 = new Lottery();
+        Lottery lottery2 = new Lottery();
+        Lottery lottery3 = new Lottery();
+
+        System.out.println(lottery1.getRandomBall());
+        System.out.println(lottery2.getRandomBall());
+        System.out.println(lottery3.getRandomBall());
         // create three balls using class Lottery and print information about them in console
     }
 }
