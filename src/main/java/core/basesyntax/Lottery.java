a9c06ed42165d5eed31770f4c74e31ddc62d63b7
Oleg <oleg21871@gmail.com>
2021-11-14 15:17:14 +0200

    class Ball, Lottery and enum Colors was added. Application and ColorSupplier was updated

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private int maxPossibleNumberOnBall = 100;
+
+    public Lottery() {
+
+    }
+
+    public Lottery(int maxPossibleNumberOnBall) {
+        this.maxPossibleNumberOnBall = maxPossibleNumberOnBall;
+    }
+
+    public Ball getRandomBall() {
+        Random random = new Random();
+        return new Ball(new ColorSupplier().getRandomColor(), random.nextInt(maxPossibleNumberOnBall) +1);
+    }
+}
