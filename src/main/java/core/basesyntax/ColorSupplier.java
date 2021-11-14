eecc5c98401e0107413a2f280e82e16bbce5b8c0
Pavlo Heorh <georg_pasha@ukr.net>
2021-11-15 00:53:43 +0200

    Task has been complete! So I`m waiting for your advices and remarks!

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final Random random = new Random();
     public String getRandomColor() {
-        return null;
+
+        return String.valueOf(Colors.values()[random.nextInt(Colors.values().length)]);
     }
 }
