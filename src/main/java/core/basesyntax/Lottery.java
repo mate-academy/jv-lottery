7fcb4ca1e23386a9028bc45f6e12aaa112cfe4b4
Evgen Lanovenko <ev.lanovenko@gmail.com>
2021-11-15 17:27:33 +0200

    Added field random with constructor

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.util.Random;
 
 public class Lottery {
+    private Random random = new Random();
+
     public Ball getRandomBall() {
+        int index = random.nextInt(100);
         ColorSupplier supplier = new ColorSupplier();
         Color color = supplier.getRandomColor();
-        Ball ball = new Ball(color, RandomIndex.getRandomIndex(100));
+        Ball ball = new Ball(color, index);
         return ball;
     }
 }
