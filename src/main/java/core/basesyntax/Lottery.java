c056185ab8ff4e83ab63706177f1695a1b49d371
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2021-11-15 16:51:44 +0200

    Added object random in class ColorSupplier. Delete inheritance in Lottey class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,10 +2,12 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball newBall = new Ball();
-        newBall.setColor(getRandomColor());
+        newBall.setColor(colorSupplier.getRandomColor());
         newBall.setNumber(new Random().nextInt(101));
         return newBall;
     }
