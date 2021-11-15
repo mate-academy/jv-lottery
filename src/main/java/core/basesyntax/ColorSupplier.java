6c4492c76a4911e7d033ff463ec2222fc0a48c1d
Evgen Lanovenko <ev.lanovenko@gmail.com>
2021-11-15 14:45:26 +0200

    Added class RandomIndex with method getRandomIndex

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
-import java.util.Random;
 
 public class ColorSupplier {
     public Color getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        return Color.values()[index];
+        return Color.values()[RandomIndex.getRandomIndex(Color.values().length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,13 +1,11 @@
 package core.basesyntax;
 
-import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        int index = new Random().nextInt(100);
         ColorSupplier supplier = new ColorSupplier();
         Color color = supplier.getRandomColor();
-        Ball ball = new Ball(color, index);
+        Ball ball = new Ball(color, RandomIndex.getRandomIndex(100));
         return ball;
     }
 }
