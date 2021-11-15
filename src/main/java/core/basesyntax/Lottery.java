f6c60b0b6f0c6af09fe888ba35854dac7374c378
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2021-11-15 18:02:06 +0200

    Added object random in class Lottery.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball newBall = new Ball();
         newBall.setColor(colorSupplier.getRandomColor());
-        newBall.setNumber(new Random().nextInt(101));
+        newBall.setNumber(random.nextInt(101));
         return newBall;
     }
 }
