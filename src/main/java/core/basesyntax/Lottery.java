d739f84b724bb871479e12f1e5c564ea0ba79e4d
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2021-11-16 12:46:18 +0200

    Improving the code

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private final Random random = new Random();
+    private final ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
         int value = random.nextInt(100);
-        Ball ball = new Ball(value,ColorSupplier.getRandomColor());
+        Ball ball = new Ball(value,color.getRandomColor());
         return ball;
     }
 }
