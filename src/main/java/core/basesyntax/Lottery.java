9b24feb6c9ef1d664510b45fb9a05a33f07efb34
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-18 11:04:12 +0200

    Lottery.random and colorSupplier UpDate

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    Random random = new Random();
+    ColorSupplier colorSupplier = new ColorSupplier();
+
     public String getRandomBall() {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
         Ball luckyBall = new Ball();
         luckyBall.setColor(colorSupplier.getRandomColor());
         luckyBall.setNumber(random.nextInt(100));
