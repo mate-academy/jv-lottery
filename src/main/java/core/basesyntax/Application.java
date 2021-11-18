82a576dc1331769a3cceb3c7fc6f1e77fecb912c
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2021-11-18 20:14:33 +0200

    OOP Lottery vers1

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-
-        ColorSupplier colorSupplier = new ColorSupplier();
-        System.out.println("ColorRanomOfEnum:" + colorSupplier.getRandomColorList());
         Lottery lottery = new Lottery();
         for (int i = 1; i < 4; i++) {
             System.out.println("NumberBall " + i + " :" + lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     public String getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         ColorList color = colorSupplier.getRandomColorList();
