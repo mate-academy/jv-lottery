ca0d53b068bfee2ca4e0b7b796f8de4bdc4c9a9a
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-11-18 11:18:15 +0200

    Lottery.new Random and new ColorSupplier UpDate

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
-
     public String getRandomBall() {
         Ball luckyBall = new Ball();
-        luckyBall.setColor(colorSupplier.getRandomColor());
-        luckyBall.setNumber(random.nextInt(100));
+        luckyBall.setColor(new ColorSupplier().getRandomColor());
+        luckyBall.setNumber(new Random().nextInt(100));
         return luckyBall.toString();
     }
 }
