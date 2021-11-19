6422c79ec262b4105cd10ec811a94a527a871275
Ganna Shkarupa <sexykatz@gmail.com>
2021-11-19 18:46:07 +0200

    creation of array was fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball [] balls = {null, null, null};
+        Ball [] balls = new Ball[3];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
