522a88715f9df5df6339fcbb0d571c2d35de58cd
oleg <9213195@gmail.com>
2021-11-19 19:04:01 +0200

    fixed ColorSupplier, the index variable has become private

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    int index = new Random().nextInt(Colors.values().length);
+    private int index = new Random().nextInt(Colors.values().length);
 
     public String getRandomColor() {
         return Colors.values()[index].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    public  Ball getRandomBall() {
+
+    public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(100));
