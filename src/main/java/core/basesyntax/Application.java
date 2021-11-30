38a67f16a4dadfd5e0c38b50584f4b470578ed68
Oleh B <oleg.busygin@gmail.com>
2021-11-30 13:24:50 +0200

    checkstyle errors fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         int createBallsNum = 3;
-        for (int i = 0; i < createBallsNum; i++){
+        for (int i = 0; i < createBallsNum; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
+
     public String getRandomColor() {
         int index = random.nextInt(Colors.values().length);
         return Colors.values()[index].toString();
