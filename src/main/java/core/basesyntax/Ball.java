483867272b4fdb399bddb965ff448183ef870354
Arina <crowlynx4@gmail.com>
2021-12-01 14:23:21 +0200

    lottery hm is completed, BUILD SUCCESS

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,32 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "The ball has color " + getColor() + " and number " + getNumber() + ".";
+    }
+}
