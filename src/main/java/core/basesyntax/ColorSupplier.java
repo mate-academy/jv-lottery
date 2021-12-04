6a4bd96b75b78f9f1378aa14e81e2e12579918b4
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-04 16:44:04 +0200

    implemented method String getRandomColor() in class ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+
+    public ColorSupplier() {
+
+    }
+
     public String getRandomColor() {
-        return null;
+
+        int colors = 0;
+        for (Color color : Color.values()) {
+            colors++;
+        }
+
+        Random random = new Random();
+
+        int colorNumber = random.nextInt(colors - 1);
+
+        colors = 0;
+
+        String resultColor = "";
+
+        for (Color color : Color.values()) {
+            if (colors == colorNumber) {
+                resultColor = color.toString();
+            }
+            colors++;
+        }
+
+        return resultColor;
     }
 }
