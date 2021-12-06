23471d1b26dd13f2d39cba75ccd167599a241d3c
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-06 21:40:43 +0200

    variables random, colorSupplier transformed to class Lottery fields, corrected bound of nexInt call

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
 
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
 
         Ball ball = new Ball();
-        Random random = new Random();
-        ball.setNumber(random.nextInt(100));
-        ColorSupplier colorSupplier = new ColorSupplier();
+        ball.setNumber(random.nextInt(101));
         ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
