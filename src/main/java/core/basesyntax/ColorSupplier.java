c0a37938a1b79df35ff6210f9d82f90a7e158025
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-07 22:08:42 +0200

    method declarations separated from previous statements with empty lines

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
         Color[] colors = Color.values();
-        Random random = new Random();
         String resultColor = colors[random.nextInt(colors.length)].toString();
         return resultColor;
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setNumber(random.nextInt(101));
