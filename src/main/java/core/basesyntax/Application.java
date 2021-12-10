4851cab19d344251a36ee10ae77f9926b98b7b60
Natalia Kurhanska <kurhnatali@gmail.com>
2021-12-11 00:33:16 +0200

    created classes Ball, Lottery, enum Colors imlementation method in ColorSupplier created 3balls inApplication

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery[] balls1 = {new Lottery(), new Lottery(), new Lottery()};
+        for (Lottery pr : balls1) {
+            System.out.println(pr.getRandomBall());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    BLUE,
+    YELLOW,
+    BLACK,
+    GREEN,
+    PURPLE,
+    PINK,
+    GREY
+}
+
+;
