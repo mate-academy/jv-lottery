23a9989735504c5e23392217dff6bbb735a2f8c0
Vitaliy Zahrenko <vzagorenko@gmail.com>
2021-12-11 21:54:44 +0200

    changed lotterydraw

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Lottery ltr = new Lottery();
+        Lottery lotteryDraw = new Lottery();
         Ball[] balls = new Ball[3];
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = ltr.getRandomBall();
+            balls[i] = lotteryDraw.getRandomBall();
             System.out.println(balls[i].toString());
         }
 
