e7c18257eccbee53802b1ce45aed14f0021496a5
Roman Kobryn <romankobryn.work@gmail.com>
2021-12-13 21:35:53 +0200

    fixed checkstyles errors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private Random random = new Random();
+    public void setRandom(int valur) {
+
+    }
 
     public String getRandomColor() {
         int value = random.nextInt(Colors.values().length);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
 
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         Ball ball = new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
         return ball;
     }
