6051f76915549380d22036a10a085dcb8c910ec1
Roman Kobryn <romankobryn.work@gmail.com>
2021-12-13 21:40:51 +0200

    fixed checkstyle error

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,10 +4,15 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery ball1 = new Lottery();
+
         Lottery ball2 = new Lottery();
+
         Lottery ball3 = new Lottery();
+
         System.out.println(ball1.getRandomBall());
+
         System.out.println(ball2.getRandomBall());
+
         System.out.println(ball3.getRandomBall());
     }
 }
