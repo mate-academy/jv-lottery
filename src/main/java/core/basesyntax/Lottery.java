91515017e2adb203aaf5eb4ea8dbaa97acac554a
vladsysa <vladsysa@mail.ru>
2021-12-14 10:21:05 +0100

    Changes after mentor 03

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private int randomNumber = random.nextInt(100);
-    private String randomColor = colorSupplier.getRandomColor();
 
     public Ball getRandomBall() {
+        int randomNumber = random.nextInt(100);
+        String randomColor = colorSupplier.getRandomColor();
         Ball ball = new Ball();
         ball.setNumber(randomNumber);
         ball.setColor(randomColor);
