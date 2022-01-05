0e59bc4237fbdedbcb9bfa19547b57fcd21d1595
Anton Hut <tonnyhut@icloud.com>
2022-01-05 22:26:13 +0200

    implementation a loop which use class Lottery for creating three balls and print all of him

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+
+        for (int i = 0; i < 3; i++) {
+            Lottery lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
