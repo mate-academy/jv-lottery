2621f5e6b49455137d816bf3adb3b5fa4f4695f6
Anton Hut <tonnyhut@icloud.com>
2022-01-06 12:35:06 +0200

    now I understand why you wrote to move only variable random ))) p.s hope that understood )

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
 
     private Random random = new Random();
-    private int value = random.nextInt(100);
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ColorSupplier supplier = new ColorSupplier();
+        int value = random.nextInt(100);
         ball.setColor(supplier.getRandomColor());
         ball.setNumber(value);
         return ball;
