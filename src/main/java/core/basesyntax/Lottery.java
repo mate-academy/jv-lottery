db28ea903eb7208dcd5800981279fb3929ab609a
Anton Hut <tonnyhut@icloud.com>
2022-01-06 12:18:31 +0200

    brought the variable value to the class level and change access modifier

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall() {
-        Random random = new Random();
-        int value = random.nextInt(100);
 
+    private Random random = new Random();
+    private int value = random.nextInt(100);
+
+    public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(ColorSupplier.getRandomColor());
+        ColorSupplier supplier = new ColorSupplier();
+        ball.setColor(supplier.getRandomColor());
         ball.setNumber(value);
         return ball;
     }
