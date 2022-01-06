68d4e464e29b83afe7e482a25cd377de661c193e
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-01-06 17:33:52 +0100

    hw-2-jv-lottery-finalCreate

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private Random random = new Random();
-    private ColorSupplier colours = new ColorSupplier();
+    private ColorSupplier colors = new ColorSupplier();
 
     public Random getRandom() {
         return random;
@@ -14,13 +14,17 @@ public class Lottery {
         this.random = random;
     }
 
-    public void setColours(ColorSupplier colours) {
-        this.colours = colours;
+    public void setColors(ColorSupplier colors) {
+        this.colors = colors;
+    }
+
+    public ColorSupplier getColors() {
+        return colors;
     }
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(colours.getRandomColor().toString());
+        ball.setColor(colors.getRandomColor().toString());
         ball.setNumber(random.nextInt(100));
         return ball;
     }
