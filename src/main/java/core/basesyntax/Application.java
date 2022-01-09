d624f0a1b8ac7d18dfd69715f50771d36117a78d
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-01-09 23:38:34 +0200

    - added three new different balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,11 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery ballFirst = new Lottery();
+        Lottery ballSecond = new Lottery();
+        Lottery ballThird = new Lottery();
+        System.out.println(ballFirst.getRandomBall());
+        System.out.println(ballSecond.getRandomBall());
+        System.out.println(ballThird.getRandomBall());
     }
 }
