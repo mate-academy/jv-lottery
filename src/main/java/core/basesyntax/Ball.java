0beba2bbe6d8751ee23df27ed279cd883b36dea8
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-11 00:27:09 +0200

    I'm working...

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class Ball {
+    private Color color;
+    private int number;
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Color {
+    WHITE,
+    BLACK,
+    YELLOW,
+    GREEN,
+    RED,
+    BLUE,
+    BROWN,
+    GRAY,
+    PURPLE,
+    ORANGE
+}
