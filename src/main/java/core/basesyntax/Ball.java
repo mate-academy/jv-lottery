8f0735c5162ca5a1584cc99ecef147455f264c18
Sergii Turchyn <turchyn13@gmail.com>
2022-01-10 19:07:01 +0200

    added @Overrided method .toString

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -11,6 +11,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball number: " + number + ", Ball color: " + color;
+        return "Ball number: " + number + ", color: " + color;
     }
 }
