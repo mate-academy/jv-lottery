970faf331131a26c2044f2cd0a9a7996e3354919
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-01-11 18:10:27 +0200

    - added constructor

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,10 +1,13 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Ball {
     private String color;
     private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
 
     @Override
     public String toString() {
