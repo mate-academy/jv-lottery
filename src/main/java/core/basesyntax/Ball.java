9deab521051623bc357133753b3916735e4c6fe1
daniel <daniya522@gmail.com>
2022-01-12 17:25:13 +0200

    - Create Ball class with parameters colors and number; - Create ENUM with colors; - implemented String getRandomColor () method in ColorSupplier class; - Create Lottery class with getRandomBall() method; - Created a loop and walked over each ball.

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball color is '" + color + '\''
+                + ", number is " + number;
+    }
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery extends ColorSupplier {
+    public Ball getRandomBall() {
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+
+        Ball ball = new Ball();
+        ball.setNumber(random.nextInt(100));
+        ball.setColor(colorSupplier.getRandomColor());
+        return ball;
+    }
+}
