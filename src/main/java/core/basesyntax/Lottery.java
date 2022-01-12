aedfc1b1004b24712c10cf4cab71db66c4401353
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-01-12 11:20:10 +0200

    - corrected lotery - added condition (for)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(100));
     }
 }
