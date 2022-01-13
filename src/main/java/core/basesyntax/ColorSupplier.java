52691b824e895e58815dcfdaccc2c341d8b1a1c6
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-13 21:34:54 +0200

    Of course some comments

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
+
 import java.security.SecureRandom;
-import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int randomInt = new SecureRandom().nextInt(9 - 0);
-          Color ballcolor = Color.values()[randomInt];
-          return ballcolor.toString();
+        int randomInt = new SecureRandom().nextInt(9);
+        Color ballcolor = Color.values()[randomInt];
+        return ballcolor.toString();
     }
-public ColorSupplier() {}
+
+    public ColorSupplier() {}
 
 }
