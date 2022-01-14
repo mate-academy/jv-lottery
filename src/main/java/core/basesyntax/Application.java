c20d99126ae40cea5990aa1270605b60c9750149
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-14 19:19:51 +0200

    Now I made adjustments and fixed everything. May be...

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,8 @@ public class Application {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             Lottery randomBall = new Lottery();
-            String message = "preffix " + randomBall.getRandomBall();
-            System.out.println(randomBall.getRandomBall().toString());
+            String message = "" + randomBall.getRandomBall();
+            System.out.println(message);
         }
 
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.security.SecureRandom;
 
 public class Lottery {
+    private ColorSupplier getBallColor = new ColorSupplier();
+
     public Ball getRandomBall() {
         int currentNumber = new SecureRandom().nextInt(100 - 1) + 1;
-        ColorSupplier ballColor = new ColorSupplier();
-        String currentColor = ballColor.getRandomColor();
+        String currentColor = getBallColor.getRandomColor();
         Ball ball = new Ball(currentColor, currentNumber);
         return ball;
     }
