8da5702822febd8e01b7184e25ef0c8bbf805e8f
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-16 18:03:27 +0200

    created class Ball, created enum Color

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,36 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball color is [" + color + "] and number is [" + number + "]";
+    }
+}
+enum Color {
+    WHITE,
+    BLACK,
+    BLUE,
+    PURPLE,
+    GREEN,
+    PINK,
+    RED
+}
