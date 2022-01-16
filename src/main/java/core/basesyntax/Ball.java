f43898c39edf3f77428d7451e283556989b281fa
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-16 18:11:54 +0200

    updated class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,22 +1,11 @@
 package core.basesyntax;
 
 public class Ball {
-    private String color;
-    private int number;
+    private final String color;
+    private final int number;
 
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
+    public Ball(String color, int number) {
         this.color = color;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
         this.number = number;
     }
 
