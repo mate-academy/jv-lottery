5f08c8386371eef74657656596c7c14ba284e568
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-01-17 11:05:04 +0200

    added fixes after review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     public Ball getRandomBall() {
         return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
     }
