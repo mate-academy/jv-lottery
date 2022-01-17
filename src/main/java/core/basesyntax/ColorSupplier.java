cd2fcf307f61b88851e9f1825e52903e37c3ab07
Oleksandr Fedchik <fed4iksasha@gmail.com>
2022-01-17 22:06:41 +0300

    transferred Random variable at class level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
         Color[] color = Color.values();
-        Random generator = new Random();
-        return String.valueOf(color[generator.nextInt(color.length)]);
+        return String.valueOf(color[random.nextInt(color.length)]);
     }
 }
