feba1994c33939ebc2b6f82dd4d8092567d94566
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-01-17 11:15:07 +0200

    Comment remowed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        // create three balls using class Lottery and print information about them in console
         for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
         }
