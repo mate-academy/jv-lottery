8fe7531dacc5af8d1733382d81ed4c06e87fcbe2
Dima Semeniuk <semeniuk.dima@ukr.net>
2022-01-17 11:12:30 +0200

    removed redundant empty line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery lottery = new Lottery();
+       Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
         }
