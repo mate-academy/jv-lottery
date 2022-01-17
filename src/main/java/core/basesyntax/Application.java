1470078dda30c562d736e30fc44bae45d89159f2
Sasha Bachynskyi <allfred12@gmail.com>
2022-01-17 11:52:14 +0200

    changed psfi number_of_balls name, created one object newLottery outside the loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int NUMBER_OF_USERS = 3;
+    private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
-        Ball[] balls = new Ball[NUMBER_OF_USERS];
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        Lottery newLottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = newLottery.getRandomBall();
             System.out.println(balls[i].toString());
         }
     }
