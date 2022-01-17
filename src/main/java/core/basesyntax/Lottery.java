5bbb700c175645b3ee612034a5ab3ae0f42d7663
sltnsrh <lifenjoy8888@gmail.com>
2022-01-17 20:28:40 +0200

    hw solution

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public String getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100)).toString();
+    }
+}
