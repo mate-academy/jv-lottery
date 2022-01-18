5793871b203abb6a4d1a0b15a0e9ce39cd8f0c68
lugburz.hr <lugburz.hr@gmail.com>
2022-01-18 12:11:45 +0200

    Using loop for creating and printing random balls.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball firstBall = lottery.getRandomBall();
-        Ball secondBall = lottery.getRandomBall();
-        Ball thirdBall = lottery.getRandomBall();
-        System.out.println(firstBall);
-        System.out.println(secondBall);
-        System.out.println(thirdBall);
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < 3; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println(balls[i]);
+        }
     }
 }
