cf6e51e488c61fc421f0f4ee6250f451c0c05626
Yurii Tomashevskyi <niteris92@gmail.com>
2022-01-18 19:11:16 +0200

    added solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery1 = new Lottery();
+        System.out.println("Ball number 1:" + lottery1.getRandomBall().toString());
+        Lottery lottery2 = new Lottery();
+        System.out.println("Ball number 2:" + lottery2.getRandomBall().toString());
+        Lottery lottery3 = new Lottery();
+        System.out.println("Ball number 3:" + lottery3.getRandomBall().toString());
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        Color[] values = Color.values();
+        return String.valueOf(values[random.nextInt(values.length)]);
     }
 }
