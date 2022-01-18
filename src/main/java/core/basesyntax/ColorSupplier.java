2a59874b4c23543a00091bf7393c716da715481d
sviat <svyatoslav.grinik@gmail.com>
2022-01-18 17:46:51 +0200

    removed all redundant empty lines

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     protected String getRandomColor() {
         int randomIndex = new Random().nextInt(Color.values().length);
-
         return Color.values()[randomIndex].toString();
     }
 }
