48caf83c6fe406e924ea8ce9748da1949f8cd8cb
Liubov Chernohor <liubovchernohor@gmail.com>
2022-01-18 16:03:55 +0200

    main method was changed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Ball ball = new Lottery().getRandomBall();
+            Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
     }
