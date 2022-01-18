f18162f8c9b42569f988f945d4ab39cd4efa04d5
Liubov Chernohor <liubovchernohor@gmail.com>
2022-01-18 13:44:56 +0200

    Two classes with methods were added, enam was created

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball {colour is "
+                + color
+                + ", number is "
+                + number + "}";
+    }
+}
