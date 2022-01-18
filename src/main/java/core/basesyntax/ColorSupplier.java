a779285b84b04828c069338ad545623a7f9d1f2c
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-01-18 18:59:38 +0200

    Rename and redo. Correct some Maven's comments.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 
 public class ColorSupplier {
     private SecureRandom secureRandom = new SecureRandom();
+
     public String getRandomColor() {
         int randomInt = secureRandom.nextInt(9);
         Color ballcolor = Color.values()[randomInt];
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
+    
     public Ball getRandomBall() {
         int currentNumber = new SecureRandom().nextInt(100 - 1) + 1;
         String currentColor = colorSupplier.getRandomColor();
