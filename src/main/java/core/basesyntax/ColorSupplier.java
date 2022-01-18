28cafd3585ce843daba98e138ee00b345c1157bc
sviat <svyatoslav.grinik@gmail.com>
2022-01-18 17:37:29 +0200

    classes Ball, Color, Lottery was added. Methods of all classes was implemented. Hw done.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    protected String getRandomColor() {
+        int randomIndex = new Random().nextInt(Color.values().length);
+
+        return Color.values()[randomIndex].toString();
     }
 }
