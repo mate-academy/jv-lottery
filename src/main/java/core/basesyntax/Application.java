64eb692c137d1a308ac99e0a5d8857a038a50071
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-19 12:10:01 +0200

    added for loop to create and print balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball firstBall = lottery.getRandomBall();
-        Ball secondBall = lottery.getRandomBall();
-        Ball thirdBall = lottery.getRandomBall();
-        System.out.println("1st " + firstBall.toString());
-        System.out.println("2nd " + secondBall.toString());
-        System.out.println("3rd " + thirdBall.toString());
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println((i + 1) + "). " + balls[i].toString());
+        }
     }
 }
