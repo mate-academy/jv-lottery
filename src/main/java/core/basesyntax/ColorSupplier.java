b85ac82bbc98601b5b400512514e42b051548c39
Ivan Pavluchuk <java.python10@gmail.com>
2022-01-19 17:47:50 +0200

    re-added solution 03

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,6 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        return Colors.values() [new Random().nextInt(5)].toString();
+        return Colors.values() [new Random().nextInt(Colors.values().length)].toString();
     }
 }
