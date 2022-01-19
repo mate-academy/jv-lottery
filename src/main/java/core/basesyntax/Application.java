92831fec6304cdf833f25f651458354043f6c5a6
Roman <dudanecr@gmail.com>
2022-01-19 13:28:15 +0200

    homevork Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery letDoLottery = new Lottery();
-        Ball [] randomBalls = new Ball[3];
+        Ball[] randomBalls = new Ball[3];
         for (int i = 0; i < randomBalls.length; i++) {
             randomBalls[i] = letDoLottery.getRandomBall();
             System.out.println(randomBalls[i]);
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-
         int indexColor = new Random().nextInt(Color.values().length);
         return Color.values()[indexColor].toString();
-
     }
 }
