7d4f8193613a70a19bf131d6cbabc814da75645e
Vadym <vadya829@gmail.com>
2022-01-19 13:56:13 +0100

    Added method in Lottery class which is creating Balls with random parameters + 2 classes(Lottery, Ball) + 1 Enum(Color); + declaration of 3 random Balls added to main method along with printing them; + all static methods have been changed to non-static   (along with their initialization); + changed variable type color in Ball from Color to String; + changed return type in ColorSupplier.getRandomColor from Color to String; + moved creation of the Lottery outside the loop.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery l = new Lottery();
         Ball[] balls = new Ball[3];
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = l.getRandomBall();
             System.out.println(balls[i].toString());
         }
     }
