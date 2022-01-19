a53c054a16a44f7e9c9fc9491676121234028ec0
anuytamysko <a_misko@ukr.net>
2022-01-19 17:48:11 +0200

    lottery outside the loop, deleted local.ROOT

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Ball[] lotteryBalls = new Ball[3];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < lotteryBalls.length; i++) {
-            lotteryBalls[i] = new Lottery().getRandomBall();
+            lotteryBalls[i] = lottery.getRandomBall();
             System.out.println(lotteryBalls[i].toString());
         }
     }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
 
-import java.util.Locale;
 import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
-        return String.valueOf(Colors.values()[index]).toLowerCase(Locale.ROOT);
+        return String.valueOf(Colors.values()[index]).toLowerCase();
     }
 }
