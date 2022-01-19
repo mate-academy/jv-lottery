c36afbae6050e3747e875de55065cdd26d4d3efa
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-19 11:39:08 +0200

    fixed codestyle

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,15 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
         Ball firstBall = lottery.getRandomBall();
         Ball secondBall = lottery.getRandomBall();
         Ball thirdBall = lottery.getRandomBall();
-
         System.out.println("1st " + firstBall.toString());
         System.out.println("2nd " + secondBall.toString());
         System.out.println("3rd " + thirdBall.toString());
-
     }
 }
