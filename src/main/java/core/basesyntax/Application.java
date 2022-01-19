dd4c26bf7bbe9d7e517bccf067a955fb42c2ead3
anuytamysko <a_misko@ukr.net>
2022-01-19 17:16:13 +0200

    implement lottery app

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball[] lotteryBalls = new Ball[3];
+        for (int i = 0; i < lotteryBalls.length; i++) {
+            lotteryBalls[i] = new Lottery().getRandomBall();
+            System.out.println(lotteryBalls[i].toString());
+        }
     }
 }
