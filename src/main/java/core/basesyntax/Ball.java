720e089dd5720453ebe5f0186038f1d5c643c65c
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-01-27 21:31:10 +0200

    Add consructor to Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,11 +4,13 @@ public class Ball {
     private String color;
     private int number;
 
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball{" + "color='" + color + '\'' + ", number=" + number + '}';
     }
 }
