70a738563b3b65454a9cd0f5014238ddc6613a10
Dmytro Chernenko <mitresko12@gmail.com>
2022-01-29 12:58:49 +0200

    changed color type field From Color to String in Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class Ball {
-    private Color color;
+    private String color;
     private int number;
 
-    public Ball(Color color, int number) {
+    public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
 
-    public Color getColor() {
+    public String getColor() {
         return color;
     }
 
@@ -17,7 +17,7 @@ public class Ball {
         return number;
     }
 
-    public void setColor(Color color) {
+    public void setColor(String color) {
         this.color = color;
     }
 
