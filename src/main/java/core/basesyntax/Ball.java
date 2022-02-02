012e37fcbcf2b91f928d6f59a79a7da72177ba07
Serhii Buria <sergeiburya@gmail.com>
2022-02-02 09:54:47 +0200

    added class ball, lottery, enum color

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(){
+    }
+
+    public Ball(String colorBall, int numberBall) {
+        this.color = colorBall;
+        this.number = numberBall;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    public Ball getRandomBall() {
+    Ball randomBall = new Ball();
+    randomBall.toString();
+    return randomBall;
+    }
+}
