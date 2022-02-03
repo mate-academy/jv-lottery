4ba8d9927c30d8e54ec547e89cbc3a01d2ab0a24
dmytro.sylenok <ulinmob@gmail.com>
2022-02-03 15:33:06 +0200

    more syntax

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier color = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier color = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
