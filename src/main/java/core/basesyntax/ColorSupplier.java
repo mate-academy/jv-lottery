25b5ce4f0f874f29281ddfc98dde29217357a65f
dmytro.sylenok <ulinmob@gmail.com>
2022-02-03 15:20:22 +0200

    fixing syntax.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random rand = new Random();
+    private Random rand = new Random();
+
     public String getRandomColor() {
         int random = rand.nextInt(Colors.values().length);
         return Colors.values()[random].toString();
