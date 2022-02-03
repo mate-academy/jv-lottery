54f1e32c47cd29acc87c2e17f42faa96b1522099
dmytro.sylenok <ulinmob@gmail.com>
2022-02-03 15:30:31 +0200

    more fixing

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random rand = new Random();
+    private Random random = new Random();
 
     public String getRandomColor() {
-        int random = rand.nextInt(Colors.values().length);
-        return Colors.values()[random].toString();
+        int number = random.nextInt(Colors.values().length);
+        return Colors.values()[number].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    ColorSupplier color = new ColorSupplier();
+    Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setColor(color.getRandomColor());
+        ball.setNumber(random.nextInt(100));
         return ball;
     }
 }
