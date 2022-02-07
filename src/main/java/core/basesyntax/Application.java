289286843700c5b34d3d887591172e24ddc7504a
vlad <vladdzyadevich@gmail.com>
2022-02-07 12:56:53 +0200

    Refactor code by comment

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball = lottery.getRandomBall();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        System.out.println(ball);
-        System.out.println(ball1);
-        System.out.println(ball2);
+        Ball ball = new Ball();
+        Ball ball1 = new Ball();
+        Ball ball2 = new Ball();
+        Ball[] balls = new Ball[]{ball,ball1, ball2};
+        for(int i = 0; i < 3; i++){
+            System.out.println(balls[i] = lottery.getRandomBall());
+        }
     }
 }
