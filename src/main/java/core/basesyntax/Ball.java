8452510f7d9874280a15f5f6944592d7b6c177cc
Nataliia Urazaeva <natasha.uraza@gmail.com>
2022-02-15 17:44:44 +0200

    jc-fixed-after-review

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,16 @@ public class Ball {
     Color color;
     int number;
 
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
     @Override
     public String toString() {
         return "Ball{" +
                 "color=" + color +
                 ", number=" + number +
                 '}';
-    }
-
-    public Ball(Color color, int number) {
-        this.color = color;
-        this.number = number;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,6 @@ public class Lottery {
     public Ball getRandomBall() {
         Color color = new ColorSupplier().getRandomColor();
         int number  = new Random().nextInt(100);
-        Ball ball = new Ball(color, number);
-        return ball;
+        return new Ball(color, number);
     }
 }
