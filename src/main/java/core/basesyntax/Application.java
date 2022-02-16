59cc9ba18fe4f45d73013839455c3c370194dce3
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-02-16 17:54:05 +0200

    Create loop for make a Balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,11 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        System.out.println(ball1.toString());
-        Ball ball2 = lottery.getRandomBall();
-        System.out.println(ball2.toString());
-        Ball ball3 = lottery.getRandomBall();
-        System.out.println(ball3.toString());
+        for (int i=1; i < 4; i++){
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball.toString());
+        }
     }
 }
