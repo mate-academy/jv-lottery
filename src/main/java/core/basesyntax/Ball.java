f76922b835a53961c39f07052e290e3b64220698
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-02-16 09:09:06 +0200

    change class Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Ball extends ColorSupplier {
 private int number;
-private Colors color;
+private String color;
 
     public int getNumber() {
         return number;
@@ -12,11 +12,11 @@ private Colors color;
         this.number = number;
     }
 
-    public Colors getColor() {
+    public String getColor() {
         return color;
     }
 
-    public void setColor(Colors color) {
+    public void setColor(String color) {
         this.color = color;
     }
 
@@ -25,7 +25,7 @@ private Colors color;
         StringBuilder builder = new StringBuilder();
         builder.append("Number: ")
                 .append(number)
-                .append("color: ")
+                .append(" color: ")
                 .append(color);
         return builder.toString();
     }
