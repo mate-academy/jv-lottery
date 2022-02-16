6c5fe773c37bafb6b6910b95c0ba5c288aa2c0f8
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-02-16 09:00:20 +0200

    Create method getRandomcolor()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,9 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public abstract class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Colors[] colorsR = Colors.values();
+        Random mRnd = new Random();
+        return colorsR[mRnd.nextInt(colorsR.length)].toString();
     }
+
     public abstract String toString();
 }
 
