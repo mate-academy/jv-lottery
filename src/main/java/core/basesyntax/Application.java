acc1611ed39590c1867db7bf307cae9ff52099ee
VladimirShtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-02-17 17:11:35 +0200

    lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,6 +3,6 @@ package core.basesyntax;
 public class Application { public static void main(String[] args) {
     for (int i = 0; i < 3; i++) {
         System.out.println(new Lottery().getRandomBall().toString());
-    }
+     }
 }
 }
