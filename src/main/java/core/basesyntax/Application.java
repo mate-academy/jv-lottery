c27431a67697116051fa8e96f2d43855a7bbf73a
Volodymyr Shtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-02-17 17:26:08 +0200

    Delete Application.java

--- src/main/java/core/basesyntax/Application.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Application { public static void main(String[] args) {
-    for (int i = 0; i < 3; i++) {
-        System.out.println(new Lottery().getRandomBall().toString());
-     }
-}
-}
