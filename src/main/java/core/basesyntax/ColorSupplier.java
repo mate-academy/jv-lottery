bf9ffb93fbe37687078363a8b951288d1f52656b
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-02-17 21:45:21 +0200

    1-st attempt in solving jv-lottery task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public Colors getRandomColor() {
+        int pickColor = new Random().nextInt(Colors.values().length);
+        return Colors.values()[pickColor];
     }
 }
+
+
