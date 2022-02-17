949dae2f3e0ad417ae658f3736981e68fb94f6f3
VladimirShtukar <99186204+VladimirShtukar@users.noreply.github.com>
2022-02-17 21:40:31 +0200

    lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
     public String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
         return String.valueOf(Colors.values()[index]);
