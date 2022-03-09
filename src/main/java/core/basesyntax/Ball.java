a156cb3c0556c9d589a6dfa4992bddcb7034e48d
Oleksandr Domnich <dmnichz@gmail.com>
2022-03-09 12:57:47 +0200

    added fixes after review.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,17 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Ball {
-    private String color;
-    private int number;
+    private final String color;
+    private final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
 
     @Override
     public String toString() {
-        return "Ball #" + new Random().nextInt(100)
-                + ", color: " + new ColorSupplier().getRandomColor();
+        return "Ball #" + number
+                + ", color: " + color;
     }
 }
