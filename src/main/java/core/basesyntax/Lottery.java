3b8ef48f01c52e4af68446de810b9eb106d504ef
Iryna Hrynda <hryndairyna1@gmail.com>
2022-03-10 13:40:04 +0200

    did refactoring the code according to the reviewer's comments

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(),
-                new Random().nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(),
+                random.nextInt(100));
     }
 }
