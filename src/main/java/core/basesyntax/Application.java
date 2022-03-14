829c45b85d676b60dfc808951c361844789c84dc
AndrewLinskii <tolstuy1996@gmail.com>
2022-03-14 20:48:59 +0200

    Solution to HmLottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,16 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+
+        Ball[] balls = new Ball[]{
+                lottery.getRandomBall(),
+                lottery.getRandomBall(),
+                lottery.getRandomBall()
+        };
+
+        for (Ball ball : balls) {
+            System.out.println(ball.toString());
+        }
     }
 }
