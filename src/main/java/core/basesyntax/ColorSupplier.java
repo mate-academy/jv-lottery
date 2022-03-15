c5234e426459c3f1a0a648af81f3c81c2535f21c
Dima Pilipenko <dimadenver3@gmail.com>
2022-03-15 12:20:50 +0200

    added solution in ColorSuplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return String.valueOf(Colors.values()[index]);
+        //int index = new Random().nextInt(Colors.values().length);
+        //return Colors.values()[index].name();
+        return Colors.values()[new Random().nextInt(Colors.values().length)].name();
     }
 }
