60a7d8e8abde5ac34c67a2f19cac8b6e1024ff78
AndrewLinskii <tolstuy1996@gmail.com>
2022-03-17 17:50:21 +0200

    change to right constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int bound = 100;
+    private static final int BOUND = 100;
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = new Random().nextInt(bound);
+        int number = new Random().nextInt(BOUND);
         return new Ball(color, number);
     }
 }
