216c18133e3563f8b10b05756ffc2411c3a203f4
DanyloTiazhev <finckreg@gmail.com>
2022-03-17 19:20:54 +0200

    getRandomBall method implemented and tested

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        String ball1 = "What is our first ball? " + lottery.getRandomBall();
+        String ball2 = "What is our second ball? " + lottery.getRandomBall();
+        String ball3 = "What is our third ball? " + lottery.getRandomBall();
+
+        System.out.println(ball1);
+        System.out.println(ball2);
+        System.out.println(ball3);
         // create three balls using class Lottery and print information about them in console
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public String getRandomBall() {
+        ColorSupplier color = new ColorSupplier();
+        color.getRandomColor();
+        return "It`s a " + color.getRandomColor() + " ball number " + new Random().nextInt(100) + "!";
+    }
+}
