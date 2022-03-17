58cf64601d8b7bece8aeaf4bac5622419aff811b
Lev Ber <levberezov01@gmail.com>
2022-03-17 09:28:53 +0300

    added fixes: , Lottery create constant static final MAXIMUM_POSSIBLE_NUMBER, Application call method in a loop 3 times

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery lottery1 = new Lottery();
-        Lottery lottery2 = new Lottery();
-        Lottery lottery3 = new Lottery();
-        Lottery [] lotteries = {lottery1,lottery2,lottery3};
-        for (Lottery lottery:lotteries) {
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
         }
-
     }
 }
+
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAXIMUM_POSSIBLE_NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
         Ball ball = new Ball();
         ball.setColor(color);
-        int randomNumber = new Random().nextInt(100);
+        int randomNumber = new Random().nextInt(MAXIMUM_POSSIBLE_NUMBER);
         ball.setNumber(randomNumber);
         return ball;
     }
