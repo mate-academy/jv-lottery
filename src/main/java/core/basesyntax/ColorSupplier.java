28bdbd61a3c8d0eaff6c31b29f2da7e1b2465abd
Roma Yarishkin <romarijkov@gmail.com>
2022-03-18 13:18:20 +0200

    Added fixes after review

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
 
-    public String getRandomColors() {
-        return Colors.values()[random.nextInt(Colors.values().length)].toString();
+    public String getRandomColor() {
+        return Color.values()[random.nextInt(Color.values().length)].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int BOUND = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColors(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BOUND));
     }
 }
