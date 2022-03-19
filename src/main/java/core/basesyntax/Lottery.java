f6b7af0569b36f2574ea4dc42562d8e588cde529
Dmytro Koliada <dmitrikstar@gmail.com>
2022-03-19 18:43:12 +0200

    optoptimized 1 line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -8,7 +8,6 @@ public class Lottery {
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
         Random random = new Random();
-        Ball ball = new Ball(supplier.getRandomColor(), random.nextInt(UPPER_BOUND));
-        return ball;
+        return new Ball(supplier.getRandomColor(), random.nextInt(UPPER_BOUND));
     }
 }
