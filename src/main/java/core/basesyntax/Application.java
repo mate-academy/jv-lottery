649607552473d769091c221de001a5daf496a04b
yuriikozar <yuriikozar@gmail.com>
2022-03-19 16:57:26 +0200

    Commit 2 Objects "ball2" and "ball2" added v2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,5 +5,9 @@ public class Application {
         // create three balls using class Lottery and print information about them in console
         Ball ball1 = new Lottery().getRandomBall();
         System.out.println(ball1);
+        Ball ball2 = new Lottery().getRandomBall();
+        System.out.println(ball2);
+        Ball ball3 = new Lottery().getRandomBall();
+        System.out.println(ball3);
     }
 }
