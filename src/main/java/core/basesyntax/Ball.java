34d1a8df2c0ec64afd87372b17b55246bcbe9135
l_osyash <los.nazar24@gmail.com>
2022-03-21 16:33:03 +0200

    Class Ball and Enum Color was created.

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Ball {
+    private Color color;
+    private int number;
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                + "color=" + color
+                + ", number=" + number
+                + '}';
+    }
+}
