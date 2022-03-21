27cf87b629f9535cc5d322c0881882565bb18ec5
Lyudmila D <dunda94lyudmila@gmail.com>
2022-03-21 15:48:26 +0200

    checked with mvn clean package

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery ball1 = new Lottery();
-        for (int i = 1; i <=3; i++) {
+        for (int i = 1; i <= 3; i++) {
             System.out.println(ball1.getRandomBall());
         }
     }
