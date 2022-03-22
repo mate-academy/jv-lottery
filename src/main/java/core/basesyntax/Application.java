3ba3910910872f634c9ab58d75b2c90ebf4f24f2
DanyloTiazhev <finckreg@gmail.com>
2022-03-22 12:07:24 +0200

    commented code and redundant methods deleted

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,11 +6,9 @@ public class Application {
         Ball[] lotteryResult = new Ball[3];
         for (int i = 0; i < lotteryResult.length; i++) {
             lotteryResult[i] = lottery.getRandomBall();
-            System.out.println(lotteryResult[i].toString());
+            System.out.println(lotteryResult[i]);
 
         }
         // create three balls using class Lottery and print information about them in console
-        /* System.out.println("It`s " + lotteryResult[i].getColor() + " ball number "
-                + lotteryResult[i].getNumber() + "!"); */
     }
 }
