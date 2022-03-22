021d7a6d2d84bfe3c12b3af6408ab0c776e43339
Nesstor Fedak <nesstortor@gmail.com>
2022-03-22 14:44:59 +0200

    created all needed classes and methods, third try:)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int LIMIT = 100;
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(LIMIT));
     }
 }
