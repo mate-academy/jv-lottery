9fef066f728358ef0094873162a8c90fef74d271
Sergey Pshenichnyh <serg.pshenichnyh@gmail.com>
2022-03-22 23:23:03 +0200

    Method getRandomBall() return instance of Ball class with random number and random color.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,7 +4,13 @@ public class Ball {
     private String color;
     private int number;
 
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
     public String toString() {
-        return toString();
+        return "Ball{" + "color = " + color + ' ' + ", number = " + number + '}';
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends ColorSupplier {
-    public int getRandomBall() {
-        java.util.Random random = new Random();
-        int value = random.nextInt(100);
-        return value;
+    public Ball getRandomBall() {
+        Random random = new Random();
+        Ball ball = new Ball(getRandomColor(), random.nextInt(100));
+        return ball;
     }
 }
