27926def977c50ab965745a155decf9072254fba
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-03-23 20:48:48 +0200

    fixed code style again

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,8 @@ public class Application {
     public static void main(String[] args) {
         final int COUNT = 5;
 
-       for (int i = 0; i < COUNT; i++) {
-           System.out.println(new Lottery().getRandomBall());
-       }
+        for (int i = 0; i < COUNT; i++) {
+            System.out.println(new Lottery().getRandomBall());
+        }
     }
 }
