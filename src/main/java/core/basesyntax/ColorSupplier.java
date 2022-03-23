63ef728b49b932f4f12098a89792694685ea98ea
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-03-23 19:05:20 +0200

    added fix of mistake second time

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     public static String getRandomColor() {
         Random random = new Random();
-        int number = random.nextInt(Colors.values().length);
-        return Colors.values()[number].toString();
+        int number = random.nextInt(Color.values().length);
+        return Color.values()[number].toString();
     }
 }
