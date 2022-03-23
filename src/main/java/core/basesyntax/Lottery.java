15944faa4b0aff27311c1d49dbab0c6ccae5a39b
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-03-23 18:23:35 +0200

    added fix of mistake "random"

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int BOUND = 100;
+
     public Ball getRandomBall() {
         Random random = new Random();
-        return new Ball(ColorSupplier.getRandomColor(), random.nextInt(100));
+        return new Ball(ColorSupplier.getRandomColor(), random.nextInt(BOUND));
     }
 }
