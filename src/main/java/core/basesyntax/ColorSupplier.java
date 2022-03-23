81cda80414677723dfa03cecc3f36aac05f5cdf2
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-03-23 15:10:05 +0200

    implement getRandomColor() in ColorSupplier fix Main method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,27 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Color color;
     private Random random = new Random();
 
     public Color getRandomColor() {
-        switch (Color.values()[random.nextInt(Color.values().length)]) {
-            case RED:
-                color = Color.RED;
-                break;
-            case GREEN:
-                color = Color.GREEN;
-                break;
-            case BLUE:
-                color = Color.BLUE;
-                break;
-            case YELLOW:
-                color = Color.YELLOW;
-                break;
-            default:
-                return null;
-        }
-        return color;
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index];
     }
 }
 
