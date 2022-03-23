626866a00fda4f8b4e6854bd1bf091d969d7646b
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-03-23 20:26:14 +0200

    Finaly

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,22 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        // create three balls using class Lottery
+        // and print information about them in console
+
+        Ball testball = new Ball();
+        testball.setColor(ColorSupplier.getRandomColor());
+        testball.setNumber(Lotery.getRandomBall());
+        System.out.println(testball.toString());
+
+        Ball testball1 = new Ball();
+        testball1.setColor(ColorSupplier.getRandomColor());
+        testball1.setNumber(Lotery.getRandomBall());
+        System.out.println(testball1.toString());
+
+        Ball testball2 = new Ball();
+        testball2.setColor(ColorSupplier.getRandomColor());
+        testball2.setNumber(Lotery.getRandomBall());
+        System.out.println(testball2.toString());
     }
 }
