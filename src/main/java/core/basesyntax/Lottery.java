64016a03148d74b83c99c393848906584643664c
Roman Kachmaryk <roman.kv98@gmail.com>
2022-03-23 20:22:39 +0200

    fixed some errors (forgot to run mvn clean package before)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random random = new Random();
-    ColorSupplier supplier = new ColorSupplier();
+    private Random random = new Random();
+    private ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(supplier.getRandomColor(), random.nextInt(100));
