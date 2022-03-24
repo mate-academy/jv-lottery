bbecc97600c2d44d0eec576d452c8e9e02a4e01d
Artur <zamelatskov@gmail.com>
2022-03-24 10:19:19 +0200

    change for checklist

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Ball[] allBalls = new Ball[3];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < allBalls.length; i++) {
-            Lottery lottery = new Lottery();
             System.out.println(lottery.getRandomBall());
         }
     }
