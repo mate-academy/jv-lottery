53e497d88bb76f25633af1849275d6d2030bce74
Oleksander <ghjwtyrj25@gmail.com>
2022-03-24 15:58:09 +0200

    added method that returns random enum from Colors

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+      ColorSupplier test1 = new ColorSupplier();
+        System.out.println(test1.getRandomColor());
     }
 }
