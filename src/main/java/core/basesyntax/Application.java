81cd1c6674469ca81514629b1d537cb4d77e317a
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-03-24 02:20:09 +0200

    generating three random parameters for objects

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        int count = 3;
+        while (count != 0) {
+            System.out.println(Lottery.getRandomBall());
+            --count;
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball() {
+
+    }
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                +
+                "color='" + color + '\''
+                +
+                ", number=" + number
+                +
+                '}';
+    }
+}
