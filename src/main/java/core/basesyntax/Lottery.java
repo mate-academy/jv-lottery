27f0185ed3eaa822782b1addb43c9f61e4f94f87
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-03-24 09:54:57 +0200

    added "pivate" to "final int BOUND = 100;"

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int BOUND = 100;
+    private static final int BOUND = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
