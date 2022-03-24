1d28b3b076120465652c70191bb222a93b57e1ba
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-03-24 18:13:01 +0200

    added private static finel int ATTEMPTS

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int TRY = 100;
     public Ball getRandomBall() {
         return new Ball(new ColorSupplier().getRandomColor(),
-                new Random().nextInt(100));
+                new Random().nextInt(TRY));
     }
 }
