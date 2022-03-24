d14eb3b5675298ef47c1e844df90b63f960a422a
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-03-24 16:37:18 +0200

    code violations were fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public  class Lottery {
+public class Lottery {
     private static final int BOUND = 100;
+
     public String getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
         return String.valueOf(new Ball(colorSupplier.getRandomColor(), random.nextInt(BOUND)));
-
     }
 }
