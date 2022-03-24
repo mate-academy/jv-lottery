50fbe1a8fdbebf90feae9e8b55d8307bc92163d3
Ihor <ibakay92@gmail.com>
2022-03-24 15:09:33 +0200

    Homework Solution fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    private static final int RANGE = 100;
+
     public Ball getRandomBall() {
-        int value = new Random().nextInt(100);
+        int value = new Random().nextInt(RANGE);
         return new Ball(new ColorSupplier().getRandomColor(), value);
     }
 }
