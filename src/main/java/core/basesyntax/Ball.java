0af1661c85ebb92a9b1ed8c2c1346c5e08c16ce4
Oleksander <ghjwtyrj25@gmail.com>
2022-03-24 15:37:08 +0200

    added fields in class Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,4 +1,14 @@
 package core.basesyntax;
 
 public class Ball {
+    int number;
+    Colors color;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "number=" + number +
+                ", color=" + color +
+                '}';
+    }
 }
