efca4de3833ab6655e6c89aabbca08425a72d828
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-03-24 09:34:21 +0200

    Add empty line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int BOUND = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(BOUND));
