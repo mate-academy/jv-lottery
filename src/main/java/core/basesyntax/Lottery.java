72835a68cb184a5afa4b6b6f2507fa698588d2ac
Artur <zamelatskov@gmail.com>
2022-03-24 18:34:30 +0200

    adde public static final int MAX_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
-        int number = random.nextInt(100);
+        int number = random.nextInt(MAX_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), number);
     }
 
