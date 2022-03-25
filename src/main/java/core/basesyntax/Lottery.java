b803325e3ba94f5eecaa3414aa1a266ef9a7af80
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-03-25 08:56:21 +0200

    mvn clean package

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
+
 import java.util.Random;
 
 public class Lottery {
+
     private static final int TRY = 100;
+
     public Ball getRandomBall() {
         return new Ball(new ColorSupplier().getRandomColor(),
                 new Random().nextInt(TRY));
