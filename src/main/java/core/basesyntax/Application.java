c9f6554548aff900f76b79a073664a860b635ae6
Roman Omeliukh <romik3513@gmail.com>
2022-03-25 17:07:54 +0200

    home task jv-lottery update

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery ball1 = new Lottery();
-        Lottery ball2 = new Lottery();
-        Lottery ball3 = new Lottery();
-        System.out.println(ball1.getRandomBall());
-        System.out.println(ball2.getRandomBall());
-        System.out.println(ball3.getRandomBall());
+        Lottery lottery1 = new Lottery();
+        Lottery lottery2 = new Lottery();
+        Lottery lottery3 = new Lottery();
+        System.out.println(lottery1.getRandomBall());
+        System.out.println(lottery2.getRandomBall());
+        System.out.println(lottery3.getRandomBall());
     }
 }
 
