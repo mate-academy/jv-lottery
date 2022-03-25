c5534fb0fa317bfedbd1ed443917d3ac5886cd6f
Sergey Pshenichnyh <serg.pshenichnyh@gmail.com>
2022-03-25 23:24:50 +0200

    I should use instance of ColorSupplier class inside Lottery class.  I did it. Please check.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,18 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-<<<<<<< HEAD
-public class Lottkb, ,ery {
+public class Lottery {
     public Ball getRandomBall() {
         Random random = new Random();
         ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
-=======
-public class Lottery extends ColorSupplier {
-    public Ball getRandomBall() {
-        Random random = new Random();
-        Ball ball = new Ball(getRandomColor(), random.nextInt(100));
-        return ball;
->>>>>>> 9fef066f728358ef0094873162a8c90fef74d271
+
     }
 }
