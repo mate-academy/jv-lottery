c473f312bb91484513898f76300668058f1cbb01
Mark Shapka <mark.shapka1@gmail.com>
2022-03-28 15:46:31 +0300

    fixed checkstyle failure

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] argsgit) {
         Ball[] balls = new Ball[3];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = new Lottery().getRandomBall();
         }
-        //Ball[] balls = {new Lottery().getRandomBall(), new Lottery().getRandomBall(), new Lottery().getRandomBall()};
         for (Ball ball: balls) {
             System.out.println(ball);
         }
