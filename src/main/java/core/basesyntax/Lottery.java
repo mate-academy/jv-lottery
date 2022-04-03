d4ac69f595c49776f0e94e05dac9750f95ec467b
dippp <Dipppp84@gmail.com>
2022-04-03 17:50:14 +0300

    fix mistakes 2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static Random random = new Random();
     private static ColorSupplier colorSupplier = new ColorSupplier();
-    private final static int NUMBER = 101;
+    private static final int NUMBER = 101;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER));
