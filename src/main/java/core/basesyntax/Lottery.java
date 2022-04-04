a122ab5f11b4c2de2df21278078f267e61797e6d
Maksym <Maksym_Personal@ip-192-168-0-72.eu-central-1.compute.internal>
2022-04-04 20:26:51 +0300

    4st attempt. Methods access modifiers fixed.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static Random random = new Random();
+    private static final int UPPER_LIMIT = 101;
+    private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private final int upperLimit = 101;
 
     public Ball getRandomBall() {
-        Ball ball = new Ball(colorSupplier.getRandomColor(), random.nextInt(upperLimit));
+        Ball ball = new Ball(colorSupplier.getRandomColor(), random.nextInt(UPPER_LIMIT));
         return ball;
     }
 }
