6df54f9f0b7800ab610fc17c6286d97afa7d6e7f
Artem Bordichuk <artyombordichuk@gmail.com>
2022-04-04 15:46:49 +0300

    1. Added setter for instance randomColor

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public class ColorSupplier {
     private Random randomColor = new Random();
 
+    public void setRandomColor(Random randomColor) {
+        this.randomColor = randomColor;
+    }
+
     public String getRandomColor() {
         int index = randomColor.nextInt(Color.values().length);
         return Color.values()[index].name();
