04bc0a6729d3f4c7db8fda6d6d5941ae00e26089
Artem Bordichuk <97861801+artem-bordichuk@users.noreply.github.com>
2022-04-04 18:29:33 +0300

    Update src/main/java/core/basesyntax/Application.java
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[3];
-        Lottery randomBall = new Lottery();
+        Lottery lottery = new Lottery();
 
         for (int i = 0; i < 3; i++) {
             balls[i] = randomBall.getRandomBall();
