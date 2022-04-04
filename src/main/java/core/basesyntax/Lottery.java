1913f0f26fd4b1047f62a193a8a3f16a768edf9c
dippp <Dipppp84@gmail.com>
2022-04-04 16:34:10 +0300

    added SingletonRandom and corrected mistakes fix 1

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,10 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
-    private ColorSupplier colorSupplier = new ColorSupplier();
     private static final int MAX_BALL_NUMBER = 101;
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), SingletonRandom.getInstance().nextInt(MAX_BALL_NUMBER));
+        Random random = SingletonRandom.getInstance();
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
     }
 }
