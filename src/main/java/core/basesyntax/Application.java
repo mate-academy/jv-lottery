9a2e6a2261c522ef285bf12c199bbcd59b23da3c
Artem Bordichuk <artyombordichuk@gmail.com>
2022-04-06 12:44:49 +0300

    Code was fixed   inasmuch to your suggestions. About getters and setters I leaved a comment on GitHub.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] ball = new Ball[3];
+        Ball[] balls = new Ball[3];
         Lottery lottery = new Lottery();
 
         for (int i = 0; i < 3; i++) {
-            ball[i] = lottery.getRandomBall();
-            System.out.println(ball[i]);
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i]);
         }
 
 
