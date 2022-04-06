e897463b598c0916fa2de3db9b0fd4346abe6ef7
Alex <alexoriobel@gmail.com>
2022-04-06 18:54:10 +0300

    Fixed auto-review

--- src/main/java/core/basesyntax/lotteryball/Lottery.java
+++ src/main/java/core/basesyntax/lotteryball/Lottery.java
@@ -1,5 +1,4 @@
 package core.basesyntax.lotteryball;
-
 import java.util.Random;
 
 public class Lottery {
@@ -12,5 +11,13 @@ public class Lottery {
         ball.setBallColor(colorSupplier.getRandomColor());
         ball.setBallNumber(random.nextInt(100));
         return ball;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+    public void setColorSupplier(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
     }
 }
