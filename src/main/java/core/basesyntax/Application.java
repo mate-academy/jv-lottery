ba41ae1c47a2fedaec2a5546102daf4cca4bcf4a
Artem Bordichuk <artyombordichuk@gmail.com>
2022-04-06 12:23:37 +0300

    Code was fixed   inasmuch to your suggestions. About getters and setters I leaved a comment on GitHub.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,15 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
+        Ball[] ball = new Ball[3];
         Lottery lottery = new Lottery();
 
-        Ball first = lottery.getRandomBall();
-        Ball second = lottery.getRandomBall();
-        Ball third = lottery.getRandomBall();
+        for (int i = 0; i < 3; i++) {
+            ball[i] = lottery.getRandomBall();
+            System.out.println(ball[i]);
+        }
 
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,14 +7,6 @@ public class Lottery {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
-    public void setColor(ColorSupplier randomColor) {
-        this.colorSupplier = randomColor;
-    }
-
-    public ColorSupplier getColor() {
-        return colorSupplier;
-    }
-
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
         int number = random.nextInt(BOUND);
