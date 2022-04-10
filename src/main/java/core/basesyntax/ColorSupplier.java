d413c658e647b8cf1036441776c49b9215a5f676
Fokin <Fokin1@ukr.net>
2022-04-10 15:59:56 +0200

    Rename in the ColorSupplier final random to RANDOM

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    static final Random random = new Random();
+    static final Random RANDOM = new Random();
 
     public String getRandomColor() {
-        int randomIndex = random.nextInt(Color.values().length);
+        int randomIndex = RANDOM.nextInt(Color.values().length);
         return Color.values()[randomIndex].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Lottery {
 
     public Ball getRandomBall() {
-        int num = ColorSupplier.random.nextInt(100);
+        int num = ColorSupplier.RANDOM.nextInt(100);
         String color = new ColorSupplier().getRandomColor();
 
         return new Ball(color,num);
