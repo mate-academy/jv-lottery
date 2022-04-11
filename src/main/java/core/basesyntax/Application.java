4fd9b7c666c8a6e02dd9310da287f4ecd183154b
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-04-11 14:13:01 +0300

    5th try: constant BALLS_COUNT moved to the beginning of this class;

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_COUNT = 3;
+
     public static void main(String[] args) {
-        final int countOfBalls = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < countOfBalls; i++) {
+        for (int i = 0; i < BALLS_COUNT; i++) {
             System.out.println(lottery.getRandomBall().toString());
         }
     }
