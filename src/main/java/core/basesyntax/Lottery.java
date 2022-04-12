d90796e881d175f15fd28cc92a36021bc91450fb
Oleksandr Petrunovskyi <o.petrunovskyi@gmail.com>
2022-04-12 17:11:29 +0300

    completed task for verification

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         String color = new ColorSupplier().getRandomColor();
         int number = new Random().nextInt(100);
-        Ball ball = new Ball ();
-        ball.color = color;
-        ball.number = number;
+        Ball ball = new Ball();
+        ball.setColor(color);
+        ball.setNumber(number);
         return ball;
     }
 }
