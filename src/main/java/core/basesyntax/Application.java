01aaa8ffab3b47583f6562b1b8ce3b963526b993
Dmitro Fokin <55239321+IDMFokin@users.noreply.github.com>
2022-04-12 09:27:33 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int BALLS = 3;
+        static final int BALLS = 3;
 
         Ball [] balls = new Ball[BALLS];
         for (int i = 0; i < balls.length; i++) {
