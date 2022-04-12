d267eb03637fcb7ac05064693a90c9befba13f58
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-04-12 11:59:20 +0300

    + private fields + setters

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -7,11 +7,12 @@ public class Ball {
     public void setColor(String color) {
         this.color = color;
     }
+
     public void setNumber(int number) {
         this.number = number;
     }
+
     public String toString() {
         return "The winning ball has " + color + " color and lucky number is " + number;
     }
-
 }
