90c65f1d3e774497e10c1689606764593c7fd09c
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-04-12 15:58:11 +0300

    + moved to the method + for loop

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Color[] colors = Color.values();
     private final Random random = new Random();
 
     public Color getRandomColor() {
+        Color[] colors = Color.values();
         return colors[random.nextInt(colors.length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_RANDOM_NUMBER = 101;
-    private final Ball ball = new Ball();
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
+        final int MaxRandomNumber = 101;
+        Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor().toString());
-        ball.setNumber(random.nextInt(MAX_RANDOM_NUMBER));
+        ball.setNumber(random.nextInt(MaxRandomNumber));
         return ball;
     }
 }
