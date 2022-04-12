77800db5068c8fe7e4a531a05df5e477ac4eba0f
Fokin <Fokin1@ukr.net>
2022-04-12 20:22:18 +0200

    remove empty lines in the all classes. Class Application create constant BALLS. Class Lottery create constant BOUND

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int BALLS = 3;
-
         Ball [] balls = new Ball[BALLS];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BOUND = 100;
     private final Random random = new Random();
-    private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int BOUND = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         int number = random.nextInt(BOUND);
