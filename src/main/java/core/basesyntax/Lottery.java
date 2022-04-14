10da67df714507b0bc1af1cd63bd9f6ee1dceee9
Misha Varenycia <mihvaren@gmail.com>
2022-04-14 14:14:18 +0300

    fixed naming and global field issues

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 import java.util.Random;
 
-class Lottery extends Ball {
+class Lottery {
     private static final int count = 100;
     private static final Random random = new Random();
     private static final ColorSupplier supplier = new ColorSupplier();
