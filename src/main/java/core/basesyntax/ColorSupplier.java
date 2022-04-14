ee151c22638b4fbde84b0c13a5163be6b96eeb09
Misha Varenycia <mihvaren@gmail.com>
2022-04-14 14:16:27 +0300

    fixed naming and global field issues

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private static final Random random = new Random();
+
     public Color getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index];
