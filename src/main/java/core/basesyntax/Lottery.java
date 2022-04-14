f8f8a36a90dd859b2e3cdf6044553546d6ad44e2
Misha Varenycia <mihvaren@gmail.com>
2022-04-14 14:58:45 +0300

    fixed RANDOM CONST and class type

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 import java.util.Random;
 
-class Lottery {
-    private static final int count = 100;
+public class Lottery {
+    private static final int MAX_RANDOM_NUMBER = 100;
     private static final Random random = new Random();
     private static final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(count));
+        ball.setNumber(random.nextInt(MAX_RANDOM_NUMBER));
         ball.setColor(String.valueOf(supplier.getRandomColor()));
         return ball;
     }
