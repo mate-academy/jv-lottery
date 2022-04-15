683b4320dc0ead73bc1ce4429d9e09e96d75ca63
gdp <gdp@zigzag.ints.net>
2022-04-15 10:48:30 +0300

    My solution is added
    - add psvm method to class Application;
    - add enum Color with list of colors;
    - add static method getRandomColor in class ColorSupplier;
    - add class Ball;
    - add static method getRandomBall to class Lottery.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball[] balls = new Ball[3];
+
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = Lottery.getRandomBall();
+            System.out.printf("%d. %s\n", i + 1, balls[i].toString());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        return new Ball(ColorSupplier.getRandomColor(),
+                new Random().nextInt(100));
+    }
+}
