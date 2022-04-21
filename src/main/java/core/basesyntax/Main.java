953ac3304c82b38d030cf29adad9dbf9ee63df77
Natalya Shcheglova <uralgnome@gmail.com>
2022-04-21 16:08:37 +0200

    Checkstyle errors were fixed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             Lottery lottery = new Lottery();
-            Ball randomBall =lottery.getRandomBall();
+            Ball randomBall = lottery.getRandomBall();
             System.out.println("randomBall_" + i + " " + randomBall.toString());
         }
     }
