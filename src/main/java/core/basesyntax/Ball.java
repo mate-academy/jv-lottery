9b3e676c76835ff422b8125956eaf07467c7354e
Vadym Datsiuk <lutskvadym@gmail.com>
2022-04-23 11:55:40 +0300

    lab2 test1

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Ball {
+    int number = 0;
+    String color = "";
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "number=" + number +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
