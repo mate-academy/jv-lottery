2620e85b962d3591424e8ac7abec71bd31ce0619
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-25 10:11:54 +0300

    Instance of the class Lottery in Main class moved out of the loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,8 @@ public class Application {
     public static void main(String[] args) {
         final int numberOfBalls = 3;
         Ball[] balls = new Ball[numberOfBalls];
+        Lottery randomBall = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            Lottery randomBall = new Lottery();
             balls[i] = randomBall.getRandomBall();
             System.out.println(balls[i]);
         }
