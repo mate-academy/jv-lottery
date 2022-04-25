73628c9824fe80e3e962742b750cc5ce60c1e2f1
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-25 10:07:54 +0300

    Created final int numberOfBalls in main class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
+        final int numberOfBalls = 3;
+        Ball[] balls = new Ball[numberOfBalls];
         for (int i = 0; i < balls.length; i++) {
             Lottery randomBall = new Lottery();
             balls[i] = randomBall.getRandomBall();
