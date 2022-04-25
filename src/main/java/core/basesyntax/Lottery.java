7d5f4f90232aa36ae2a312ee2616b71b4c06e2c9
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-25 11:40:34 +0300

    Created final int maxBound in Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     public static ColorSupplier supplier = new ColorSupplier();
     public static Random random = new Random();
+    final int maxBound = 100;
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(supplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(maxBound));
         return ball;
     }
 }
