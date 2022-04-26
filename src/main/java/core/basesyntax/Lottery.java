3b464330572e1ad4b29020dc8fedb89a8dbbf7c1
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-26 12:41:30 +0300

    Changed the name of variable in Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static ColorSupplier supplier = new ColorSupplier();
     private static Random random = new Random();
-    private final int maxBound = 100;
+    private final int MAX_BOUND = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(supplier.getRandomColor());
-        ball.setNumber(random.nextInt(maxBound));
+        ball.setNumber(random.nextInt(MAX_BOUND));
         return ball;
     }
 }
