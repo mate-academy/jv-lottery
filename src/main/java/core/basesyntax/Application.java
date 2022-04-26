132140715d8b30f6f30ab40432753f942dc532b6
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2022-04-26 18:41:10 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
         Lottery lottery = new Lottery();
-        for(int i = 0; i < balls.length; i++) {
+        for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
         }
