3022ea0e765152c071f72a9341a47dae68768239
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-27 12:35:19 +0300

    Changed status of some variables from static to final

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static Random random = new Random();
+    private final Random random = new Random();
 
     public String getRandomColor() {
         int randomColorNumber = random.nextInt(Color.values().length);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static ColorSupplier supplier = new ColorSupplier();
-    private static Random random = new Random();
+    private final ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
     private final int MAX_BOUND = 100;
 
     public Ball getRandomBall() {
