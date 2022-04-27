7ce8c9bbc32826546c5554810e8e7deadaf5bf55
Natalya Shcheglova <uralgnome@gmail.com>
2022-04-27 14:59:36 +0200

    Empty line (Main.java: 4) was deleted Constant MAX_I = 3 was created (Main.java: 4)

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 public class Main {
+    private static final int MAX_I = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < MAX_I; i++) {
             Ball randomBall = lottery.getRandomBall();
             System.out.println(randomBall);
         }
