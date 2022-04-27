a97e08db38dd045ed75c454ca24c1cbcb20f11a7
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-27 13:45:25 +0300

    Fixed names of the variables

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+        private static final int BALLS_NUMBER = 3;
     public static void main(String[] args) {
-        final int numberOfBalls = 3;
-        Ball[] balls = new Ball[numberOfBalls];
-        Lottery randomBall = new Lottery();
+        Ball[] balls = new Ball[BALLS_NUMBER];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = randomBall.getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
     }
