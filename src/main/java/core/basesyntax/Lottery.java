5bd2a6e412fa3782692ebd03f872e8b3ea23c1bd
Vadym Datsiuk <lutskvadym@gmail.com>
2022-04-28 00:38:12 +0300

    final, checked lab2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,7 @@ public class Lottery {
     public Ball getRandomBall() {
         ColorSupplier cc = new ColorSupplier();
         int nn = new Random().nextInt(100);
-        Ball randBall = new Ball(nn, cc.getRandomColor());
-        return randBall;
+        Ball randomBall = new Ball(nn, cc.getRandomColor());
+        return randomBall;
     }
 }
