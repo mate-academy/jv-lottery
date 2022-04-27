0021496864ea81b485dacb0c9d1ec898c7c247bd
Maksim Tsvetkov <tsm_2001@ukr.net>
2022-04-27 22:13:38 +0300

    classes and methods added

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        for (int i = 1; i < 4; i++) {
+            System.out.println(new Lottery().getRandomBall().toString());
+        }
     }
 }
