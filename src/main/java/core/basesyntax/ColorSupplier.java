6d3372371acff820d93fe5650fcaef5f2356e0c4
Sasha Vradiy <alexvradii@gmail.com>
2022-04-27 12:09:00 +0300

    Solving the second task Lottery.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         int randomColor = new Random().nextInt(Colors.values().length);
-        return String.valueOf(Colors.values()[randomColor]);
+        return (Colors.values()[randomColor]).name();
     }
 }
 
