1def83b8af17cc8097d9d8042fce78afa456c659
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-30 01:53:06 +0300

    randomInt in ColorSupplier now private

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static Random randomInt = new Random();
+    private static final Random randomInt = new Random();
 
     public String getRandomColor() {
         return Color.values()[randomInt.nextInt(Color.values().length)].toString();
