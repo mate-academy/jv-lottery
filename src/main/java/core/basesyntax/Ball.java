fc3eab844b3eb9a5ade4a89134d86bdfdbe2ee4e
Ivan Yuriv <yuriv1024@gmail.com>
2022-05-02 20:30:21 +0300

    some warnings from "mvn clean package" was changed.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
+    private String color;
+    private int number;
 
     public void setColor(String color) {
         this.color = color;
@@ -14,9 +14,9 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball{"
+                + "color='" + color + '\''
+                + ", number=" + number
+                + '}';
     }
 }
