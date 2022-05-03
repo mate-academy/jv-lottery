9c0d72fa588b7ea1a740b187f18e7c5b8a175f5a
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-03 22:31:42 +0300

    changed...

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-    public static final int ATTEMPTS = 3;
+    public static final int NUMBER_OF_ATTEMPTS = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < ATTEMPTS; i++) {
+        for (int i = 0; i < NUMBER_OF_ATTEMPTS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
