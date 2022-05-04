ad27a02be344bca277044248d65fe28197d83c70
Benatti <ssunica0@gmail.com>
2022-05-05 01:23:43 +0300

    fix syntax

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,19 +1,38 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
+    private String color;
+    private int number;
 
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball{"
+                + "color='"
+                + color
+                + '\''
+                + ", number="
+                + number
+                + '}';
     }
 }
