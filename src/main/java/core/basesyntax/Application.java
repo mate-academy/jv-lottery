5501ad85fa9581e222660172a35decb4833369b3
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-05-07 23:33:23 +0300

    third set of fixes lottery-solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-    static final int REPETITION_NUMBER = 3;
+    private static final int BALLS_NUMBER = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 1; i <= REPETITION_NUMBER; i++) {
+        for (int i = 1; i <= BALLS_NUMBER; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int UPPER_BOUND = 100;
+    private static final int UPPER_BOUND = 100;
+    private final Ball randomBall = new Ball();
     private final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Ball randomBall = new Ball();
         randomBall.setNumber(new Random().nextInt(UPPER_BOUND));
         randomBall.setColor(supplier.getRandomColor());
         return randomBall;
