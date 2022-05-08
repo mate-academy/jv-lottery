6a15bfc615f705d4c86abd0f574e132dd464c5bf
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-05-08 17:33:05 +0300

    fixed random constantly invocation

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int UPPER_BOUND = 100;
     private final Ball randomBall = new Ball();
+    private final Random random = new Random();
     private final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        randomBall.setNumber(new Random().nextInt(UPPER_BOUND));
+        randomBall.setNumber(random.nextInt(UPPER_BOUND));
         randomBall.setColor(supplier.getRandomColor());
         return randomBall;
     }
