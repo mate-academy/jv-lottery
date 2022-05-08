b3dff191f08f4d32e2d788a325e57efcf40fdade
Maksym Bilan <golosei@gmail.com>
2022-05-08 19:41:25 +0300

    1st commit jv lottery

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "The ball of " + color + "  color " + " under number " + number;
+    }
+}
