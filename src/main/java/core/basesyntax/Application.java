4d311f59631b989c1b0fef5eb50516b919fdee4e
Oleksii Mospan <alexey.mospan@gmail.com>
2022-05-11 03:06:02 +0300

    in Lottery.java removed static, added LOTTERY_DRAW public constant. Deleted Lottery constructor

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[lottery.getLotteryDraw()];
+        Ball[] balls = new Ball[Lottery.LOTTERY_DRAW];
 
-        for (int i = 0; i < lottery.getLotteryDraw(); i++) {
+        for (int i = 0; i < Lottery.LOTTERY_DRAW; i++) {
             balls[i] = lottery.getRandomBall();
         }
 
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,23 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final Random randomInt = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
     private static final int MAX_RANDOM_NUMBER = 100;
-    private final int lotteryDraw;
-
-    public Lottery() {
-        this.lotteryDraw = 3;
-    }
-
-    public int getLotteryDraw() {
-        return lotteryDraw;
-    }
+    public static final int LOTTERY_DRAW = 3;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(randomInt.nextInt(MAX_RANDOM_NUMBER));
+        ball.setNumber(random.nextInt(MAX_RANDOM_NUMBER));
         return ball;
     }
 }
