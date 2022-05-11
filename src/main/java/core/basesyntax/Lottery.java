25f19cf5a22a9c3d2b59f6dd8bb4465100aef660
Dima Povkh <dimapovkh@gmail.com>
2022-05-11 18:38:40 +0200

    hw-2-lottery-fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_RANGE = 100;
+    private final Random random = new Random();
+    private final ColorSupplier supplier = new ColorSupplier();
+    
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+       // Random random = new Random();
+       // ColorSupplier supplier = new ColorSupplier();
+        ball.setColor(supplier.getRandomColor());
+        ball.setNumber(random.nextInt(MAX_RANGE));
         return ball;
     }
 }
