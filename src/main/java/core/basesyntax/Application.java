747a98abb4203562a31730cf09a24329d1d7ff60
Oleksii Mospan <alexey.mospan@gmail.com>
2022-05-11 23:31:00 +0300

    Move BALLS_NUMBER to Application Some minor fixes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+
+    private static final int BALLS_NUMBER = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[Lottery.LOTTERY_DRAW];
-
-        for (int i = 0; i < Lottery.LOTTERY_DRAW; i++) {
+        Ball[] balls = new Ball[BALLS_NUMBER];
+        for (int i = 0; i < BALLS_NUMBER; i++) {
             balls[i] = lottery.getRandomBall();
         }
-
         for (Ball ball : balls) {
             System.out.println(ball);
         }
