625a58f2c98316c5c0996c22d6ba1e50d96462e4
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-16 21:56:06 +0300

    Remove redundant empty line, use method name() to get the name of the enum element, make max value constant, used constructor public Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-        Lottery lottery = new Lottery();
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Lottery lottery = new Lottery(random, colorSupplier);
         Ball [] balls = new Ball[3];
         for (Ball ball : balls) {
             ball = lottery.getRandomBall();
             System.out.println(ball.toString());
         }
-
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_NUMBER = 100;
+    private final Random random;
+    private final ColorSupplier colorSupplier;
+
+    public Lottery(Random random, ColorSupplier colorSupplier) {
+        this.random = random;
+        this.colorSupplier = colorSupplier;
+    }
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
-        int number = random.nextInt(100);
+        int number = random.nextInt(MAX_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), number);
     }
 }
