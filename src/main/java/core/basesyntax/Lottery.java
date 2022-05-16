2a2cf7cc3129ac83cd08a719250e7d8a29252812
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-05-16 12:19:53 +0300

    Corrected remarks

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int BALL_NUMBER = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(BALL_NUMBER));
         return ball;
     }
 }
