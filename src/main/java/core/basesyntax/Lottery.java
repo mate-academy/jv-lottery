224950d530a08a5ff65a2288663c9c4858e6442d
Andrey Chumak <chumakandrey7@gmail.com>
2022-05-16 15:21:20 +0300

    1. Used array and loop in Application class to create "balls" 2. Used "foreach" to print info about balls in Application class 3. Created constructor in Lottery class 4. Created constant MAX_NUMBER_OF_BALL in Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER_OF_BALL = 100;
-    private final Random random ;
+    private final Random random;
     private final ColorSupplier colorSupplier;
+
+    public Lottery(Random random, ColorSupplier colorSupplier) {
+        this.random = random;
+        this.colorSupplier = colorSupplier;
+    }
 
     public Ball getRandomBall() {
         int number = random.nextInt(MAX_NUMBER_OF_BALL);
         String color = colorSupplier.getRandomColor();
         return new Ball(color, number);
-    }
-
-    public Lottery(Random random, ColorSupplier colorSupplier) {
-        this.random = random;
-        this.colorSupplier = colorSupplier;
     }
 }
