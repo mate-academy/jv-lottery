1efd66872b72f46b82aad07bade578d450799397
gdp <gdp@zigzag.ints.net>
2022-05-16 12:42:57 +0300

    Fix problems after review (2)
    - in method getRandomColor() in class ColorSupplier remove empty line;
    - in class Lottery:
    -- add field ColorSupplier supplier
    -- add field Random random
    -- add initialisation of those fields to constructor.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
-
         return Color.values()[index].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,15 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_RANDOM_BALLS = 100;
+    private final ColorSupplier supplier;
+    private final Random random;
+
+    public Lottery() {
+        supplier = new ColorSupplier();
+        random = new Random();
+    }
 
     public Ball getRandomBall() {
-        ColorSupplier supplier = new ColorSupplier();
-        int number = new Random().nextInt(MAX_RANDOM_BALLS);
-
-        return new Ball(supplier.getRandomColor(), number);
+        return new Ball(supplier.getRandomColor(), random.nextInt(MAX_RANDOM_BALLS));
     }
 }
