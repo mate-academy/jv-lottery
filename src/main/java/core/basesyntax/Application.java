830bd401eb08116cce9f4ac29afaf88c71bfd627
Andrey Chumak <chumakandrey7@gmail.com>
2022-05-16 13:45:47 +0300

    Replaced static methods in "Lottery" class.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+
         // create three balls using class Lottery and print information about them in console
-        Ball firstBall = Lottery.getRandomBall();
-        Ball secondBall = Lottery.getRandomBall();
-        Ball thirdBall = Lottery.getRandomBall();
+        Ball firstBall = lottery.getRandomBall();
+        Ball secondBall = lottery.getRandomBall();
+        Ball thirdBall = lottery.getRandomBall();
 
         System.out.println(firstBall);
         System.out.println(secondBall);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        int number = new Random().nextInt(100);
-        String color = ColorSupplier.getRandomColor();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        int number = random.nextInt(100);
+        String color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
 }
