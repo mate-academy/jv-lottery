1a3a4e4a395d8c8e6783c64286d474ffd1b49507
Igor <ZIM20093@gmail.com>
2022-05-16 12:41:09 +0300

    fixed checkstyle

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Lottery {
-    private final ColorSupplier colorSupplier ;
+    private final ColorSupplier colorSupplier;
 
     public Lottery(ColorSupplier colorSupplier) {
         this.colorSupplier = colorSupplier;
