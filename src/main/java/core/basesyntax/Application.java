17a60dd18c1c3542e6fc4d59cae190193bcff621
gdp <gdp@zigzag.ints.net>
2022-05-16 12:10:33 +0300

    Fix problems after review
    - method getRandomBall() in class Lottery make no static method;
    - add var MAX_RANDOM_BALLS with numbers of ball in getting random value.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[3];
+        Lottery lottery = new Lottery();
 
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = Lottery.getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.printf("%d. %s\n", i + 1, balls[i].toString());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(),
-                new Random().nextInt(100));
+    private static final int MAX_RANDOM_BALLS = 100;
+
+    public Ball getRandomBall() {
+        ColorSupplier supplier = new ColorSupplier();
+        int number = new Random().nextInt(MAX_RANDOM_BALLS);
+
+        return new Ball(supplier.getRandomColor(), number);
     }
 }
