4c998e4b30a308cca84e7494040f7225fb5fa6bd
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2022-05-16 20:10:51 +0300

    HW jv-lottery fixes after code review

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -16,5 +16,4 @@ public class Ball {
                 + ", color='" + color + '\''
                 + '}';
     }
-
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier randomColor = new ColorSupplier();
-        String color = randomColor.getRandomColor();
-        int number = new Random().nextInt(100);
-        return new Ball(color, number);
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
 }
