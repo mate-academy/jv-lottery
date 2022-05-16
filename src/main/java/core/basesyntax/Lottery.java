ece3ad0c675a6385cc49578a7bc18141084f0145
Mykola Dubich <nikolay19888@hotmail.com>
2022-05-16 12:46:07 +0300

    Fixed Lottery class, added constructor

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     static final int MAX_BALL_NUMBER = 100;
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Random random = new Random();
+    private ColorSupplier colorSupplier;
+    private Random random;
+
+    public Lottery() {
+        this.colorSupplier = new ColorSupplier();
+        this.random = new Random();
+    }
 
     public Ball getRandomBall() {
         Ball ball = new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
