e2c62b8d5af3025a01b41e5b44aa6a90aa8ac572
Ivan Yuriv <yuriv1024@gmail.com>
2022-05-16 13:22:59 +0300

    Constant field for random method was added. Constructor Ball was added to create it in getRandomBall method. Also was added constructor Lottery and method getRandomBall was changed.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER_LIMIT = 100;
+    private final Random random;
+    private final ColorSupplier colorSupplier;
+
+    public Lottery(Random random, ColorSupplier colorSupplier) {
+        this.random = random;
+        this.colorSupplier = colorSupplier;
+    }
+
     public Ball getRandomBall() {
-        int value = new Random().nextInt(100);
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Ball ball = new Ball();
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(value);
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER_LIMIT));
     }
 }
