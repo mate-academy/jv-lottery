f61bd5a4a92ded1dce8c9de3c5eac482a88bba1a
Anton Karpov <antonkarpov547@gmail.com>
2022-05-17 11:06:25 +0200

    Checkstyle fixed, loop added, comment removed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    private final Random random;
+    private final ColorSupplier colorSupplier;
+
+    public Lottery(Random random, ColorSupplier colorSupplier) {
+        this.random = random;
+        this.colorSupplier = colorSupplier;
+    }
+    public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(ColorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(100));
         return ball;
     }
 }
