b598c648d5605060c2c5e94f8276dbcb4837f7a6
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-17 16:01:02 +0300

    Static methods were changed.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,12 +1,19 @@
 package core.basesyntax;
 
+import java.awt.*;
 import java.util.Random;
 
 public class Lottery {
     static final int MAX_BALL_NUMBER = 100;
+    private static Random random = new Random();
+    private final ColorSupplier colorSupplier;
+
+    public Lottery(Random random, ColorSupplier colorSupplier) {
+        this.random = random;
+        this.colorSupplier = colorSupplier;
+    }
 
     public static Ball getRandomBall() {
-        Random random = new Random();
         int number = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(number, ColorSupplier.getRandomColor());
     }
