96e4bef1649cd46d4b9cd9ed67ca6360022daf0f
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-17 14:18:52 +0300

    All methods and classes were created. Lottery is working.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = Lottery.getRandomBall();
-            System.out.println(balls[i]);
+        for (int i = 0; i < 3; i++) {
+            Ball balls = Lottery.getRandomBall();
+            System.out.println(balls);
         }
     }
 }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,12 @@
 package core.basesyntax;
 
 public class Ball {
-
     private int number;
     private String color;
 
     public Ball(int number, String color) {
         this.number = number;
         this.color = color;
-
     }
 
     @Override
