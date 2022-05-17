5579aab899d71c0871c668344f760c7eaf2c420d
TymurTsvietkov <tymur.tsvietkov@gmail.com>
2022-05-17 15:01:08 +0300

    changes done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,9 +5,12 @@ import java.util.Random;
 public class Application {
     public static void main(String[] args) {
         Random random = new Random();
-        ColorSupplier color = new ColorSupplier();
-        Ball[] balls = {Lottery.getRandomBall(random, color),
-                Lottery.getRandomBall(random, color), Lottery.getRandomBall(random, color)};
+        ColorSupplier color = new ColorSupplier(random);
+        Lottery lottery = new Lottery(random, color);
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < 3; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
         for (Ball ball: balls) {
             System.out.println(ball);
         }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int NUMBER = 100;
+    private static final int NUMBER = 100;
+    private final Random random;
+    private final ColorSupplier color;
 
-    public static Ball getRandomBall(Random random, ColorSupplier color) {
+    public Lottery(Random random, ColorSupplier color) {
+        this.random = random;
+        this.color = color;
+    }
+
+    public Ball getRandomBall() {
         return new Ball(color.getRandomColor(), random.nextInt(NUMBER));
     }
 }
