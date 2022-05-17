8f942c6c336ae7e3c7f5395adb7936e14663fc64
Ilya <zimmboco@gmail.com>
2022-05-17 13:24:16 +0300

    hw-2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,7 +6,11 @@ public class ColorSupplier {
     private final Random random = new Random();
     int index = random.nextInt(Color.values().length);
 
+    public int getIndex() {
+        return index;
+    }
+
     public String getRandomColor() {
-        return Color.values()[index].toString();
+        return Color.values()[getIndex()].toString();
     }
 }
