512cf1b840e9ce8ba595bf23f0a7f1b1819e2424
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-17 12:45:29 +0300

    used for instead massive boll, used constructor public colorsuplier

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,12 +5,10 @@ import java.util.Random;
 public class Application {
     public static void main(String[] args) {
         Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
+        ColorSupplier colorSupplier = new ColorSupplier(random);
         Lottery lottery = new Lottery(random, colorSupplier);
-        Ball [] balls = new Ball[3];
-        for (Ball ball : balls) {
-            ball = lottery.getRandomBall();
-            System.out.println(ball.toString());
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(lottery.getRandomBall().toString());
         }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random;
+
+    public ColorSupplier(Random random) {
+        this.random = random;
+    }
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].name();
+        return Colors.values()[random.nextInt(Colors.values().length)].name();
     }
 }
