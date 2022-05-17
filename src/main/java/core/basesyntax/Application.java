bc04089add9ace12532bf28ece100d62005d8053
Anton Karpov <antonkarpov547@gmail.com>
2022-05-17 08:44:31 +0200

    Ball lottery implemented

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,12 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Ball b1 = Lottery.getRandomBall();
+        Ball b2 = Lottery.getRandomBall();
+        Ball b3 = Lottery.getRandomBall();
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Colors[] colors = Colors.values();
+        return colors[new Random().nextInt(Colors.values().length)].toString();
     }
 }
