11a0ac066529625c8156d2d6432d949fa1dd71af
nazarbaklanov <nazarbaklanov@gmail.com>
2022-05-17 12:04:02 +0300

    corrected after 2-th review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,10 +6,10 @@ public class Application {
     private static final int STEPS = 3;
 
     public static void main(String[] args) {
-        Lottery lottery = new Lottery(new Random(), new ColorSupplier(new Random()));
+        Random random = new Random();
+        Lottery lottery = new Lottery(random, new ColorSupplier(random));
         for (int i = 0; i < STEPS; i++) {
             System.out.println(lottery.getRandomBall().toString());
-
         }
     }
 }
