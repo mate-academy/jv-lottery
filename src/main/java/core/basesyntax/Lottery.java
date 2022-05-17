50359f58723081233d1b2413ac29ea9025c56760
Ilya <zimmboco@gmail.com>
2022-05-17 12:03:21 +0300

    hw-2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public Random random = new Random();
+
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
         String color = supplier.getRandomColor();
-        int number = new Random().nextInt(100);
+        int number = random.nextInt(100);
         return new Ball(color, number);
     }
 }
