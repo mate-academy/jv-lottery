df1e1242032f5e623df0b263c235c0d2be30e9b2
Yaroslav Lastivka <yaroslav0108@gmail.com>
2022-05-18 18:10:46 +0300

    method getrandomball were implemented

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+
+    }
+}
