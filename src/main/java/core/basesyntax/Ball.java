cf0b7850793c9ec2733a0b3f442e99ffb7647fb6
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-05-18 14:01:56 +0300

    Ball class with color and number fields was created. toString() method of Ball class was overrided

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    @Override
+    public String toString() {
+        return "Ball with number: " + number + " and color: " + color;
+    }
+}
