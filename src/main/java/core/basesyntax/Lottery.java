1008712e703aa85e5933a307008cf9f2f39d1eed
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-05-18 18:34:47 +0300

    Lottery class modified

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private Ball randomBall = new Ball();
-    private ColorSupplier randomColor = new ColorSupplier();
+    private ColorSupplier random = new ColorSupplier();
 
     public Ball getRandomBall() {
-        randomBall.setColor(randomColor.getRandomColor());
+        Ball randomBall = new Ball();
+        randomBall.setColor(random.getRandomColor());
         randomBall.setNumber(new Random().nextInt(randomBall.MAX_NUMBER));
         return randomBall;
     }
