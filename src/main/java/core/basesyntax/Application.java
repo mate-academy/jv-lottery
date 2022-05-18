c3b8cae05f018caaf2083ca4cf33ee2658b3b5be
olyaklipa <olyaklipa@gmail.com>
2022-05-19 01:31:19 +0300

    Ball, Lottery classes and Color enum are created; ColorSupplier class was modified (getRandomColor method was implemented)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        Ball firstBall = lottery.getRandomBall();
+        Ball secondBall = lottery.getRandomBall();
+        Ball thirdBall = lottery.getRandomBall();
+        System.out.println("First ball: " + firstBall);
+        System.out.println("Second ball: " + secondBall);
+        System.out.println("Third ball: " + thirdBall);
         // create three balls using class Lottery and print information about them in console
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        int colorCount = Color.values().length;
+        int index = new Random().nextInt(colorCount);
+        Color color = Color.values()[index];
+        return color.toString();
     }
 }
