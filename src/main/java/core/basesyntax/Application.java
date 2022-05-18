53a537e6fb2842917bba3b46b0895525aa4a5c3e
Illya Svyshcho <iill44iikk@gmail.com>
2022-05-18 17:24:32 +0300

    changed new 3

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-        Lottery lot = new Lottery();
-        Ball ball1 = lot.getRandomBall();
-        Ball ball2 = lot.getRandomBall();
-        Ball ball3 = lot.getRandomBall();
-        Ball[] balls = new Ball[]{ball1, ball2, ball3};
-        for (Ball ball: balls) {
-            System.out.println(ball);
+        Lottery lot = new Lottery(new Random(), new ColorSupplier());
+        for (int i = 0;i < 3;i++) {
+            System.out.println(lot.getRandomBall());
         }
     }
 }
