2c69a05837bbe67192f210e3f24331e0ca209517
Illia Khomych <illia.homych@knu.ua>
2022-05-19 15:54:08 +0300

    Update src/main/java/core/basesyntax/ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Random getColour;
+    private final Random random;
 
     public ColorSupplier(Random random) {
-        getColour = random;
+        this.random = random;
     }
 
     public String getRandomColor() {
-        return AvailableColour.values()[getColour.nextInt(AvailableColour.values().length)]
+        return AvailableColour.values()[random.nextInt(AvailableColour.values().length)]
             .name();
     }
 }
