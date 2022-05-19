ecb820b53abd9d3aab1f506790ba36861faaba69
Nata <natserbo@gmail.com>
2022-05-19 13:02:06 +0300

    enum was aded and filled

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    SILVER,
+    GRAY,
+    WHITE,
+    ORANGE,
+    RED,
+    PURPLE,
+    GREEN,
+    OLIVE,
+    YELLOW,
+    BLUE
+}
