47a67f5cebdc2c3914731d3dba9fa666bfd385e9
Dima Podakin <calombo.podakin@ukr.net>
2022-05-19 16:21:39 +0300

    fixed name enum and added variable random robot-rout

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static int Value = 100;
+    private static final int VALUE = 100;
     private ColorSupplier colorSupplier;
     private Random random;
 
@@ -13,6 +13,6 @@ public class Lottery {
     }
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(Value));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(VALUE));
     }
 }
