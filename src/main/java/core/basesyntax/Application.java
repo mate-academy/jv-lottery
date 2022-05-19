b9096095fb68a8885778cbaa15861241b2ca3a78
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-05-19 16:50:11 +0300

    Added a constant, removed from the method static, fixed check style mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,14 +4,11 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         Ball[] balls = new Ball[3];
-
         for (int i = 0; i < 3; i++) {
             balls[i] = lottery.getRandomBall();
         }
-
         for (Ball ball : balls) {
             System.out.println(ball.toString());
         }
-
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
-import static core.basesyntax.ColorSupplier.getRandomColor;
-
 import java.util.Random;
 
 public class Lottery {
+    static final int RANDOM_SIZE = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        String color = getRandomColor();
-        int number = new Random().nextInt(100);
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(RANDOM_SIZE);
         return new Ball(number, color);
     }
 }
