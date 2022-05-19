d9e8820529647afa35ff580a30bfda104c811587
Nata <natserbo@gmail.com>
2022-05-19 13:39:49 +0300

    main realisation, color corrections in Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery ball1 = new Lottery();
+        System.out.println(ball1.getRandomBall());
+        Lottery ball2 = new Lottery();
+        System.out.println(ball2.getRandomBall());
+        Lottery ball3 = new Lottery();
+        System.out.println(ball3.getRandomBall());
         // create three balls using class Lottery and print information about them in console
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     public String getRandomBall(){
         Ball ball = new Ball();
-        ball.setColor(getRandomBall());
+        ColorSupplier color = new ColorSupplier();
+        ball.setColor(color.getRandomColor());
         Random random = new Random();
         ball.setNumber(random.nextInt(100));
-        return "Ball colour: " + ball.getColor() + "number: " + ball.getNumber();
+        return "Ball colour: " + ball.getColor() + ", number: " + ball.getNumber();
     }
 }
