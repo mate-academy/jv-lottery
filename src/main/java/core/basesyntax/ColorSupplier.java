83b5b68a34c44f23d0f3c7778ea46e650dfb6a50
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-05-19 16:17:02 +0300

    Colors enum renamed in Color

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -10,6 +10,6 @@ public class ColorSupplier {
     }
 
     public String getRandomColor() {
-        return Colors.values()[random.nextInt(Colors.values().length)].toString();
+        return Color.values()[random.nextInt(Color.values().length)].toString();
     }
 }
