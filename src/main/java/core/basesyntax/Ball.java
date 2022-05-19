592068e88b743509c3a5e4ac37da1c480bd8628d
Illia Khomych <illia.homych@knu.ua>
2022-05-19 10:01:46 +0300

    Homework task implemented

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Ball {
+
+    private final String colour;
+    private final int printedNumber;
+
+    public Ball(String colour, int number) {
+        this.colour = colour;
+        printedNumber = number;
+    }
+
+    @Override
+    public String toString() {
+        return "The colour of the Ball is " + colour + ".\n"
+                + "Number on the Ball is" + printedNumber;
+    }
+}
