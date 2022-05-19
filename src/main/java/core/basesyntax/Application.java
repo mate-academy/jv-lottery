38e6be5b71fde3246a90b9037e3f51727bde4529
Denys <drtymkivstain@gmail.com>
2022-05-19 13:41:54 +0300

    implemented constructor in ColorSupplier class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Application {
     public static void main(String[] args) {
         Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
+        ColorSupplier colorSupplier = new ColorSupplier(random);
         Lottery lottery = new Lottery(random, colorSupplier);
         Ball ball;
         for (int i = 0; i < 3; i++) {
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random;
+
+    public ColorSupplier(Random random) {
+        this.random = random;
+    }
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
+        int index = random.nextInt(Colors.values().length);
         return String.valueOf(Colors.values()[index]);
     }
 }
