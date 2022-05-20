515dc02acc6628906d8c84858985f2fa0cfeaa2b
Sasha Vradiy <alexvradii@gmail.com>
2022-05-20 10:57:05 +0300

    All comments are corrected.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier randomColor = new ColorSupplier();
     private final Random random = new Random();
-    private static final int NUMBERS_LIMIT = 100;
+    private final int NUMBERS_LIMIT = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
