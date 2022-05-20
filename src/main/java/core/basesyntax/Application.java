5bfa83016315bc42d4ca9a80754b2a2696c098fd
s.filyotkin <serheifil@gmail.com>
2022-05-20 03:18:56 +0300

    psvm optimization

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,9 +6,10 @@ public class Application {
     private static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        Lottery lottery = new Lottery();
 
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
         }
 
         System.out.println(Arrays.toString(balls));
