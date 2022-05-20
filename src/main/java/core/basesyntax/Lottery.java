07eeefdfa2b38ff86af09a002fbbb7ec2eedff49
Nata <natserbo@gmail.com>
2022-05-20 16:54:23 +0300

    create constant ballMaxNumber

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int ballMaxNumber = 100;
     private ColorSupplier color = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(color.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(ballMaxNumber));
         return ball;
     }
 }
