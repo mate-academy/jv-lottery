888f9e53187c8ca7ee3014c04730d3ca6d52b56a
Sasha Vradiy <alexvradii@gmail.com>
2022-05-20 12:36:58 +0300

    Changed name of enum class and name of constant.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBERS_LIMIT = 100;
     private final ColorSupplier randomColor = new ColorSupplier();
     private final Random random = new Random();
-    private static final int LIMIT = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColors(randomColor.getRandomColor());
-        ball.setNumbers(random.nextInt(LIMIT));
+        ball.setNumbers(random.nextInt(NUMBERS_LIMIT));
         return ball;
     }
 }
