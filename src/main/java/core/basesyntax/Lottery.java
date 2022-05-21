456ff6c3f9bf5c5aadf6000761e5a92d388ec645
DOM <kasinets.ulia@gmail.com>
2022-05-21 14:21:49 +0300

    done

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,11 +2,13 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery{
+public class Lottery {
     ColorSupplier supplier = new ColorSupplier();
     Random random = new Random();
+
     public Ball getRandomBall() {
-        return new Ball(supplier.getRandomColor(), random.nextInt(100));
+        Ball ball = new Ball(supplier.getRandomColor(), random.nextInt(100));
+        return ball;
     }
- }
+}
 
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,16 +1,14 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-    Ball[] balls = new Ball[3];
-    for(int i = 0; i < balls.length; i++) {
-        Ball ball = lottery.getRandomBall();
-        balls[i] = ball;
-        System.out.println(i + 1);
-        System.out.println(balls[i]);
-    }
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < balls.length; i++) {
+            Ball ball = lottery.getRandomBall();
+            balls[i] = ball;
+            System.out.println(i + 1);
+            System.out.println(balls[i]);
+        }
     }
 }
