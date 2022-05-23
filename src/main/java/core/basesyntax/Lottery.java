e2e35396eec7f1ede18967065524a0664fadb3a7
DOM <kasinets.ulia@gmail.com>
2022-05-23 19:20:18 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier supplier = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier supplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball(supplier.getRandomColor(), random.nextInt(100));
