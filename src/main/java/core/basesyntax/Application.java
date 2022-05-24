e20aa30478ee7734907076a7bd97ca4592cadc8d
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 11:18:49 +0300

    Created final lottery balls and added static final int BALLS_NUMBER = 3ц without array

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,6 @@ public class Application {
     static final int BALLS_NUMBER = 3;
 
     public static void main(String[] args) {
-        Ball[] balls = new Ball[BALLS_NUMBER];
         for (int i = 0; i < BALLS_NUMBER; i++) {
             final Lottery ball = new Lottery(new ColorSupplier(new Random()), new Random());
             System.out.println(ball.getRandomBall());
