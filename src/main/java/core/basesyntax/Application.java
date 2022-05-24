91f648a8ed052c1655d2361c6b3f7b5004517876
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 10:54:02 +0300

    Created final lottery balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Application {
 
     public static void main(String[] args) {
-        Lottery ball1 = new Lottery(new ColorSupplier(new Random()), new Random());
+        final Lottery ball1 = new Lottery(new ColorSupplier(new Random()), new Random());
         System.out.println(ball1.getRandomBall());
-        Lottery ball2 = new Lottery(new ColorSupplier(new Random()), new Random());
+        final Lottery ball2 = new Lottery(new ColorSupplier(new Random()), new Random());
         System.out.println(ball2.getRandomBall());
-        Lottery ball3 = new Lottery(new ColorSupplier(new Random()), new Random());
+        final Lottery ball3 = new Lottery(new ColorSupplier(new Random()), new Random());
         System.out.println(ball3.getRandomBall());
     }
 }
