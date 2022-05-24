c267a0cadea033ca7f6620d3a7b137b928d047b6
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 08:23:47 +0300

    created private static final int BALLS_NUMBER

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Application {
+    private static final int BALLS_NUMBER = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery(new ColorSupplier(new Random()), new Random());
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < 3; i++) {
+        Ball[] balls = new Ball[BALLS_NUMBER];
+        for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
