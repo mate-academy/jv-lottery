1a4b2968ad9053c2481f7bf011c899c1552498b3
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 10:25:36 +0300

    Created three balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Application {
-    private static final int BALLS_NUMBER = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery(new ColorSupplier(new Random()), new Random());
-        for (int i = 1; i <= BALLS_NUMBER; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println(ball);
-        }
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
+        System.out.println(ball1);
+        System.out.println(ball2);
+        System.out.println(ball3);
     }
 }
