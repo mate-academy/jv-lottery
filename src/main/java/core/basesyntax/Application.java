d5ce2c6d842028c85d1931b605b2dff3407eac8f
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 13:14:43 +0300

    created array

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,17 @@
 package core.basesyntax;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Application {
     static final int BALLS_NUMBER = 3;
 
     public static void main(String[] args) {
+        final Lottery ball = new Lottery(new ColorSupplier(new Random()), new Random());
+        Ball[] balls = new Ball[BALLS_NUMBER];
         for (int i = 0; i < BALLS_NUMBER; i++) {
-            final Lottery ball = new Lottery(new ColorSupplier(new Random()), new Random());
-            System.out.println(ball.getRandomBall());
+            balls[i] = ball.getRandomBall();
         }
+        System.out.println(Arrays.toString(balls));
     }
 }
