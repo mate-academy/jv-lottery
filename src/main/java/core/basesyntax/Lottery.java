9f97e1d4c70e161e5ccde686b510dc5dee07cd61
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 14:48:02 +0300

    changed in Lottery static final

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUM = 100;
     private final ColorSupplier colorSupplier;
     private final Random random;
 
@@ -12,9 +13,9 @@ public class Lottery {
     }
 
     public Ball getRandomBall() {
-        final int MaX_NuM = 100;
+
         String color;
-        int number = random.nextInt(MaX_NuM);
+        int number = random.nextInt(MAX_NUM);
         color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
