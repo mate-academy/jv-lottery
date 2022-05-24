45c4b0761633fa88abd3f96fbbdc08d3ec2cb928
DOM <kasinets.ulia@gmail.com>
2022-05-24 13:57:38 +0300

    added a constant

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
+        final int cons = 3;
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
+        Ball[] balls = new Ball[cons];
         for (int i = 0; i < balls.length; i++) {
             Ball ball = lottery.getRandomBall();
             balls[i] = ball;
-            System.out.println(i + 1);
             System.out.println(balls[i]);
         }
     }
