12461a68adde9a884416cecb82747a39e0da542d
Denis Kravziuk <deniskravziuk@gmail.com>
2022-05-24 18:08:01 +0300

    Jv-lottery task complete. New classes were created, methods were implemented.

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public enum Color {
+    BLACK("black"), YELLOW("yellow"), RED("red"), GREEN("green"), WHITE("white"), ORANGE("orange");
+    private String color;
+
+    Color(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int BOUND = 100;
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final Random random = new Random();
+
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BOUND));
+    }
+}
