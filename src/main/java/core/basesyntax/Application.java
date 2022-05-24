a1e1030900d230e31ca9a14bd133acff716f469b
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-05-24 08:49:45 +0300

    use BALLS_NUMBER

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -8,7 +8,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery(new ColorSupplier(new Random()), new Random());
         Ball[] balls = new Ball[BALLS_NUMBER];
-        for (int i = 0; i < balls.length; i++) {
+        for (int i = 0; i < BALLS_NUMBER; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
