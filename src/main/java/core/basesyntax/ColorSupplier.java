e3155a3da82fc1a6d93348e53f4f23eb40a81330
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-05-25 11:29:28 +0300

    made a global Random field instead of new Random()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        int randomIndex = new Random().nextInt(Color.values().length);
+        int randomIndex = random.nextInt(Color.values().length);
         return Color.values()[randomIndex].toString();
     }
 }
