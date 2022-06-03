e4c3bd1bf308a6c5800be1f9d5d321a755593cb9
Aleksandr Karman <karmanaleksandr@gmail.com>
2022-06-03 16:10:33 +0300

    Created lottery app

---
+++ src/main/java/core/basesyntax/ColorOfBalls.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+
+public enum ColorOfBalls {
+    RED,
+    BLACK
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(ColorOfBalls.values().length);
+        return ColorOfBalls.values()[index].toString();
     }
 }
