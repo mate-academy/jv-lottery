863cc2849b026fd31381b2e0430debae6160cdb4
Aleksandr Karman <karmanaleksandr@gmail.com>
2022-06-03 17:59:55 +0300

    Created lottery app (fixed) Ide asked to make color and number to final and i created 3 lottery's object

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
-            System.out.println(lottery.getRandomBall());
-        }
+        Lottery lottery1 = new Lottery();
+        Lottery lottery2 = new Lottery();
+        System.out.println(lottery.getRandomBall());
+        System.out.println(lottery1.getRandomBall());
+        System.out.println(lottery2.getRandomBall());
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final String color = new ColorSupplier().getRandomColor();
+    private final int number = new Random().nextInt(100);
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(color, number);
     }
 }
