e13e6a47d77aa1be71291254d1a2709ddac05484
Aleksandr Karman <karmanaleksandr@gmail.com>
2022-06-06 16:59:31 +0300

    Created lottery app fixed(Random random - belongs to class, index - belongs to method) +checkstyle

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-     private final Random random = new Random();
-     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         int nubmerOfBalls = 100;
