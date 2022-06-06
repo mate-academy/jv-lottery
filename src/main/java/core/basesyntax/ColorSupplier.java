c226f6d1e0af5abbc7865a33a3e71daa133f62cf
Aleksandr Karman <karmanaleksandr@gmail.com>
2022-06-06 16:27:43 +0300

    Created lottery app fixed(Random random - belongs to class, index - belongs to method) Ide asked to make color and number to final and i created 3 lottery's object

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int index = new Random().nextInt(Color.values().length);
+    private final Random random = new Random();
 
     public String getRandomColor() {
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].toString();
     }
 }
