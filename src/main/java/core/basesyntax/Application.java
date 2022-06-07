cd79b2b3f2d93d56b50e704f162d4e6441ada3e9
Aleksandr Karman <karmanaleksandr@gmail.com>
2022-06-07 09:46:05 +0300

    Created lottery app fixed(one instance of Lottery, correct name of constant)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Lottery lottery1 = new Lottery();
-        Lottery lottery2 = new Lottery();
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery1.getRandomBall());
-        System.out.println(lottery2.getRandomBall());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int NUBMER_OF_BALS = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(NUBMER_OF_BALS);
+        int number = random.nextInt(MAX_RANDOM_NUMBER);
         return new Ball(color, number);
     }
 }
