caa6a0c1c791deb5bbce6d6ed272a2d9ed0d6fbc
SergiiUstinchenko <s.ustinchenko@gmail.com>
2022-06-09 18:40:29 +0300

    Third commit: fixed after review

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class ColorSupplier {
     public static String getRandomColor() {
         int index = Lottery.getRandom().nextInt(Color.values().length);
