3902372e24b8fa4129cb1f89b04bb82e26ed2770
Ivan Aldokhin <105324572+vanoald2601@users.noreply.github.com>
2022-06-11 00:58:52 +0300

    Add 4 Classes and edit Application

---
+++ src/main/java/core/basesyntax/DifferentColors.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public enum DifferentColors {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE,
+    PINK,
+    WHITE,
+    GREY,
+    BROWN,
+    BLACK,
+    KHAKI,
+    TEAL,
+    INDIGO,
+    MAGENTA,
+    VIOLET,
+    SALMON,
+    CRIMSON,
+    LAVENDER,
+    MAROON,
+    PLUM
+}
