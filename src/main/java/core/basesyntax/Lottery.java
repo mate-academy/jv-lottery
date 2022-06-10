154c6940a083b6cd6933fa0019f1d4de2f377229
Vasyl Piznak <piznak3145@gmail.com>
2022-06-10 11:40:41 +0300

    ColorSupplier.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_RANDOM_NUMBER = 100;
-    private static final ColorSupplier RANDOM_COLOR = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setNumber(random.nextInt(MAX_RANDOM_NUMBER));
-        ball.setColor(RANDOM_COLOR.getRandomColor());
+        ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
 }
