578f983f749f0fdf51ecec010d35ffc94c1c7319
Ivan Aldokhin <105324572+vanoald2601@users.noreply.github.com>
2022-06-11 01:51:02 +0300

    Update. Add loop in Application, constructor in Ball (del setters) Add 4 Classes and edit Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i < 4; i++) {
             System.out.println(new Lottery().getRandomBall());
         }
     }
