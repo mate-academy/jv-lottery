d786f6f67acdd79c19284b3f52e9111172b2b9ce
Ivan Aldokhin <105324572+vanoald2601@users.noreply.github.com>
2022-06-13 23:17:32 +0300

    Add  1 private static Random instance. Add 1 public static random method. Add also static final int MAXIMUM_NUMBER_OF_BALL = 100; private ColorSupplier instance in Class Lottery; And create a new instance of Lottery and print it 3 times.
    Update.
    Add loop in Application, constructor in Ball (del setters)
    Add 4 Classes and edit Application

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,12 +1,9 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class ColorSupplier {
-    private Random random = new Random();
 
     public String getRandomColor() {
-        int index = random.nextInt(Color.values().length);
+        int index = Lottery.randomNumber(Color.values().length);
         return Color.values()[index].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,16 @@ import java.util.Random;
 
 public class Lottery {
     static final int MAXIMUM_NUMBER_OF_BALL = 100;
-    private Random random = new Random();
+    private static Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
+    public static int randomNumber(int number) {
+        int randomnumber = random.nextInt(number);
+        return randomnumber;
+    }
+
     public Ball getRandomBall() {
-        int number = random.nextInt(MAXIMUM_NUMBER_OF_BALL);
+        int number = Lottery.randomNumber(MAXIMUM_NUMBER_OF_BALL);
         String color = colorSupplier.getRandomColor();
         return new Ball(number,color);
     }
