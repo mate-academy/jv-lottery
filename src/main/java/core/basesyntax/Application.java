88c8f2bd546234e1707857e9825609b448c60d60
Voinkov Roman <gredisroman@gmail.com>
2022-06-20 17:53:50 +0200

    loterry/Gredis/one

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Application {
     public static void main(String[] args) {
         Random random = new Random();
-        ColorSupplier COLO = new ColorSupplier();
+        ColorSupplier colorSupplier = new ColorSupplier();
         for (int i = 0; i < 3; i++) {
             int value = random.nextInt(10);
-            System.out.print(COLO.getRandomColor() + " ");
+            System.out.print(colorSupplier.getRandomColor() + " ");
             System.out.println(value);
         }
     }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,22 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
+    private String color;
+    private int number;
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 
     @Override
     public String toString() {
         return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
+                "color='" + getColor() + '\'' +
+                ", number=" + getNumber() +
                 '}';
     }
 }
