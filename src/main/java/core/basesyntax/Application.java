346c08ad119fd986c164db7ad66d63cafcd43439
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-06-20 18:30:22 +0300

    Create classes and write solution of task

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Ball[] randomBalls = new Ball[3];
+        for (int i = 0; i < randomBalls.length; i++) {
+            randomBalls[i] = new Ball();
+        }
+        for (int i = 0; i < randomBalls.length; i++) {
+            randomBalls[i] = randomBalls[i].getRandomBall();
+        }
+        for (int i = 0; i < randomBalls.length; i++) {
+            System.out.println(randomBalls.toString());
+        }
     }
 }
