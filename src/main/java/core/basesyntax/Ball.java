4992f754e0b658886c602e70944858b48fd71540
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-06-20 18:24:24 +0300

    Create classes and write solution of task

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+import java.util.Random;
+public class Ball extends Lottery {
+    private String color;
+    private int number;
+
+    public Ball() { }
+
+    public Ball(int number, String color){
+        this.color = color;
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "number: " + getNumber() + ", color: " + getColor();
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+public enum Colors {
+    BLUE,
+    RED,
+    GREEN,
+    YELLOW,
+    BLACK,
+    WHITE,
+    GRAY,
+    BROWN,
+    ORANGE,
+    VIOLET
+}
