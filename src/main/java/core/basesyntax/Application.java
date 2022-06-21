90ae5e0da7454f8bb35512a3593e2ab444431324
Oleksandr <dovy_al@ukr.net>
2022-06-21 13:18:16 +0300

    fix maven error

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery lottery = new  Lottery();
+        Lottery lottery = new Lottery();
         int ball = 3;
         while (ball > 0) {
             System.out.println(lottery.getRandomBall());
-            ball --;
+            ball--;
         }
     }
 }
