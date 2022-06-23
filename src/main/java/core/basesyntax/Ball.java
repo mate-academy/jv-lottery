f96fc6e845f6c016d0c505e4dbdb898e7e6ec14e
Valerii Shalaiev <val-sh@ukr.net>
2022-06-23 21:08:02 +0300

    fixed error 'SingleSpaceSeparator'

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -23,8 +23,8 @@ public class Ball {
     @Override
     public String toString() {
         return "Ball{"
-                +  "color='" + color + '\''
+                + "color='" + color + '\''
                 + ", number=" + number
-                +  '}';
+                + '}';
     }
 }
