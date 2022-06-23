1f6408e7f37366c7679d4de285152c26d7bc5bd4
Azarov Artur <azarovartyr@gmail.com>
2022-06-23 16:42:04 +0300

    renamed enum Colors to Color

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int randomNumber = new Random().nextInt(Colors.values().length);
-        return Colors.values()[randomNumber].toString();
+        int randomNumber = new Random().nextInt(Color.values().length);
+        return Color.values()[randomNumber].toString();
     }
 }
