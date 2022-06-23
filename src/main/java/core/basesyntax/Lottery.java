e4930682680f33ce8f184a1628e820967f463e8b
Oleksandr <dovy_al@ukr.net>
2022-06-23 21:21:00 +0300

    added MAX RANDOM_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    static final int Range = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(Range));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_RANDOM_NUMBER));
     }
 }
