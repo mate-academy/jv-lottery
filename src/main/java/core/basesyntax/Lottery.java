3c5f1406ce36ebb5951e0868a2434ce50fc4e030
Azarov Artur <azarovartyr@gmail.com>
2022-06-23 16:56:28 +0300

    created constant MAXIMUM_NUMBER in Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int MAXIMUM_NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(MAXIMUM_NUMBER);
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(randomNumber);
