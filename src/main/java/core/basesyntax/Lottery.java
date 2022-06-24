b8acebff1274a7f2c37484e89a1640e7dcee0897
Zhuk Sergei <svzhuk@ukr.net>
2022-06-24 12:33:23 +0300

    changed modifier and variable name int

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int number = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int ballNumber = random.nextInt(number);
+        int ballNumber = random.nextInt(MAX_RANDOM_NUMBER);
         Color ballColor = colorSupplier.getRandomColor();
         return new Ball(ballColor, ballNumber);
     }
