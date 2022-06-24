e7b843634fdf9ca1e83f6acde9169886dfeb7c90
Zhuk Sergei <svzhuk@ukr.net>
2022-06-24 10:19:55 +0300

    remove empty line made global Random and ColorSupplier created a constant from 100 made a global Random

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall() {
-        int ballNumber = new Random().nextInt(100);
-        Color ballColor = new ColorSupplier().getRandomColor();
+    private final int number = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
+    public Ball getRandomBall() {
+        int ballNumber = random.nextInt(number);
+        Color ballColor = colorSupplier.getRandomColor();
         return new Ball(ballColor, ballNumber);
     }
 }
