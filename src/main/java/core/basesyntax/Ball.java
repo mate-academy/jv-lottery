8e01c32767e5564c2091d5111655fc728886cdfe
Voinkov Roman <gredisroman@gmail.com>
2022-06-26 21:52:22 +0200

    changes one

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -6,8 +6,8 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\''
+        return "Ball{"
+                + "color='" + color + '\''
                 + '}';
     }
 
