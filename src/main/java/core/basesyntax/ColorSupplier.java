1db644315c5c90d8f542d71b6a082ed15041c97a
Vasyl Moshun <vasylmoshun@gmail.com>
2022-06-29 15:47:31 +0300

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class ColorSupplier {
-
     public String getRandomColor() {
         int index = (int)(Math.random() * Color.values().length);
         return Color.values()[index].toString();
