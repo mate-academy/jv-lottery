e9f593a4fab290be1689f91c278a466aced02a40
Vasyl Moshun <vasylmoshun@gmail.com>
2022-06-29 14:10:48 +0300

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,10 +5,8 @@ public class Application {
     public static void main(String[] args) {
         Lottery ball1 = new Lottery();
         System.out.println(ball1.getRandomBall());
-
         Lottery ball2 = new Lottery();
         System.out.println(ball2.getRandomBall());
-
         Lottery ball3 = new Lottery();
         System.out.println(ball3.getRandomBall());
     }
