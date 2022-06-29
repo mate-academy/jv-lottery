27cbf54144c175aa80a967e02ca4a231d7dda4a3
Vlad Deneha <vladcst26@gmail.com>
2022-06-29 21:18:07 +0300

    deleted empty lines

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,10 +6,8 @@ public class Lottery {
     public Ball getRandomBall() {
         Random random = new Random();
         int number = random.nextInt(100);
-
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor().toString();
-
         return new Ball(number, color);
     }
 }
