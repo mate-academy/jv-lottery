859421e15568d9d0319ab67341e02fb85f6542d1
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-06-30 17:52:15 +0300

    object created outside the loop

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
+
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Lottery lottery = new Lottery();
             System.out.println(lottery.getRandomBall());
         }
     }
-    
 }
