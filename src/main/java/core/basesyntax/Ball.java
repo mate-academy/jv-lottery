d0bfcd6e53d3de94383308c5135d1dc9081c06ee
Alina Nikiforova <0500302717a@gmail.com>
2022-07-03 12:19:49 +0200

    added Ball class

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,35 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                +
+                "color='"
+                + color
+                + '\''
+                +
+                ", number=" + number
+                +
+                '}';
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
