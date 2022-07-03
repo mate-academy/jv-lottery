518f3fc03335eb8c940c35acd664e935c0e7163c
Alina Nikiforova <0500302717a@gmail.com>
2022-07-03 12:20:50 +0200

    added ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
-public class ColorSupplier {
+import java.util.Random;
+
+public class ColorSupplier extends Ball {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(ColorBall.values().length);
+        return ColorBall.values()[index].toString();
     }
 }
