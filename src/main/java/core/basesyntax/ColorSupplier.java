d91e91265c8bd46e4e683e51d4406bf3d9af7125
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-04 19:20:40 +0300

    fixed style checking errors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
+
 import java.util.Random;
 
 public class ColorSupplier {
 
     public String getRandomColor() {
-        return String.valueOf(Ball.Color.values()[new Random().nextInt(Ball.Color.values().length)]);
+        return String.valueOf(Ball.Color.values()[new Random().
+                nextInt(Ball.Color.values().length)]);
     }
 }
