b27f7aed9e28645753606d1e410217c51d19182b
Dmyto Vushnivskyi <d.vyshnicskyi@gmail.com>
2022-07-06 16:05:10 +0300

    Fixed name of constant and return of method getRandomColor

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int maxRandom = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
     private static final Random random = new Random();
     private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(maxRandom));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_RANDOM_NUMBER));
     }
 }
