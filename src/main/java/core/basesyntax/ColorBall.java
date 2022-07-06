67dc5f2ceff7bd5a32f950f8c055cf5543b636b9
Serhii Khomchenko <khomchenko18@gmail.com>
2022-07-06 14:47:47 +0200

    Created solution with Application, Ball, ClorBall, SolorSupplier and Lottery classes.

---
+++ src/main/java/core/basesyntax/ColorBall.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum ColorBall {
+    RED,
+    WHITE,
+    BLUE,
+    BLACK,
+    YELLOW,
+    GREEN,
+    ORANGE,
+    GREY,
+    VIOLET
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        return ColorBall.values()[random.nextInt(ColorBall.values().length)].name();
     }
 }
