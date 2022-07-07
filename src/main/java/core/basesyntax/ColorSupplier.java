5aeb7e9d95fa7e0d87298ef533ff8db5e3182aae
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-07-07 15:07:48 +0300

    added new Random object

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
+        int index = random.nextInt(Colors.values().length);
         return String.valueOf(Colors.values()[index]);
     }
 }
