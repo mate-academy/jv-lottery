c80c48020c43da6fee31f2dcd351e0cad598983b
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-07-07 15:03:04 +0300

    overrode toString() method

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -22,6 +22,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return getColor() + " ball" + " № " + getNumber();
+        return "Ball { color: " + color + ", number: " + number + " }";
     }
 }
