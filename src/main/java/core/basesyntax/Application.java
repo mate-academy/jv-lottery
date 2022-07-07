bd7d60a77d938ea21e09624bf1a22c11f9c90fb8
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-07-07 14:26:01 +0200

    Changes made according to the conditions of the task #5

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Ball [] balls = new Ball[3];
-        Lottery lottery =new Lottery();
+        Lottery lottery = new Lottery();
 
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        Random random= new Random();
+        Random random = new Random();
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
