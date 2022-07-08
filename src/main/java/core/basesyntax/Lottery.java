95b3b0c1f51750a7d33304768e8bc5fe410821d6
ssplot56 <ssplot56@gmail.com>
2022-07-08 15:51:36 +0300

    smth like that?

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static Random random = new Random();
+    final int ballNumber = 100;
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String color = colorSupplier.getRandomColor();
-        int ballNumber = new Random().nextInt(100);
-        return new Ball(color, ballNumber);
+        return new Ball(ColorSupplier.getRandomColor(), random.nextInt(ballNumber));
     }
 }
