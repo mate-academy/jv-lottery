ae39bcaaef79678798e040e37d2c09bf0f630e0f
Andrii Kovalskyi <kovalskyi.andrii90@gmail.com>
2022-07-08 15:37:28 +0200

    Added fixes after review v1.1

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,13 +4,13 @@ public class Ball {
     private final String color;
     private final int number;
 
-    @Override
-    public String toString() {
-        return "Ball color is " + this.color + ", Ball number is " + this.number;
-    }
-
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball color is " + this.color + ", Ball number is " + this.number;
     }
 }
