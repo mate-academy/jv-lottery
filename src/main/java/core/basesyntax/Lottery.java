99d6b4e279964a4950a4bd197ed0cf67d0ec32b0
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-07-08 09:35:18 +0200

    Changes made according to the conditions of the task #7

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_RANDOM_VALUE =100;
+    private static final int MAX_RANDOM_VALUE = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
