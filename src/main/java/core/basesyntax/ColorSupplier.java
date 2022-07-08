5a00780eaed2d233edae12d89952d922199a41d4
ssplot56 <ssplot56@gmail.com>
2022-07-08 17:46:25 +0300

    Another try

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     private static Random random = new Random();
 
-    public static String getRandomColor() {
+    public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static Random random = new Random();
-    private final int ballNumber = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
 
     public Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(), random.nextInt(ballNumber));
+        ColorSupplier colorSupplier = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_RANDOM_NUMBER));
     }
 }
