8b17029f6bb29b4a5270fdc605171d01b70e66fb
Ivan Litvynets <Litvyvanya@gmail.com>
2022-07-10 20:47:55 +0300

    task solving

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    ORANGE,
+    VIOLET
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall(){
+        Random random = new Random();
+        return new Ball(new ColorSupplier().getRandomColor(), random.nextInt(100));
+    }
+}
