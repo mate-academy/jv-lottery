a5fda6a1a05398b7dc9f962994e6341c90accbf5
Ivan Litvynets <Litvyvanya@gmail.com>
2022-07-12 00:24:44 +0300

    Build & fixed checkstyle error

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,19 +1,25 @@
 package core.basesyntax;
+
 public class Ball {
     private String color;
     private int number;
+
     public Ball() {
 
     }
+
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
+
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball{"
+                + "color='"
+                + color + '\''
+                + ", number="
+                + number
+                + '}';
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         Random random = new Random();
-        int index= random.nextInt(Colors.values().length);
+        int index = random.nextInt(Colors.values().length);
         return Colors.values()[index].name();
     }
 }
