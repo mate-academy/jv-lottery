f455a779da9d6b431130a7169847d9c35152f5a5
Yura Koval <koval.yuriy.v@gmail.com>
2022-07-11 14:10:58 +0300

    Correct checkstyle errors in class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,11 +6,11 @@ public class Lottery {
 
     public Ball getRandomBall() {
       ColorSupplier color = new ColorSupplier();
-      String colorBall = color.getRandomColor();
-      Random random = new Random();
-      int numberBall = random.nextInt(100);
-      Ball ball = new Ball();
-      ball.setBall(colorBall, numberBall);
-      return ball;
+        String colorBall = color.getRandomColor();
+        Random random = new Random();
+        int numberBall = random.nextInt(100);
+        Ball ball = new Ball();
+        ball.setBall(colorBall, numberBall);
+        return ball;
     }
 }
