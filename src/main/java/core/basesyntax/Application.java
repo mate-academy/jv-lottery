f5ff9e34bad9f768873ae37ed87d4244bc3812ba
Yura Koval <koval.yuriy.v@gmail.com>
2022-07-11 13:14:13 +0300

    jv-lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery lottery = new Lottery();
-        System.out.println(lottery.getRandomBall());
+        for (int i = 0; i < 3; i++) {
+            Lottery lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
