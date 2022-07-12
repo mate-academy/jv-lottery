d75d3825dd61cef4db19bc0332929cabfc26388b
Slava Kobzar <slvkobzar@gmail.com>
2022-07-12 17:44:23 +0300

    Finished "Lottery" homework

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Ball {
+    final String color;
+    final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return color + " ball with number " + number;
+    }
+}
