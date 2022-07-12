157060144b1a2cd7fdd15a79bc3d3bbdf32e87f0
Yura Koval <koval.yuriy.v@gmail.com>
2022-07-13 00:53:02 +0300

    I did Random as class level  variable

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
+    private Random index = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        int indLen = index.nextInt(Color.values().length);
+        return Color.values()[indLen].name();
     }
 }
