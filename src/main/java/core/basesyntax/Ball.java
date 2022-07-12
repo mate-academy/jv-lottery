a6fc1b3872d5a643a07fe274c583ac3dbc4d1b92
Slava Kobzar <slvkobzar@gmail.com>
2022-07-12 23:19:43 +0300

    Fixed test errors

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    final String color;
-    final int number;
+    private String color;
+    private int number;
 
     public Ball(String color, int number) {
         this.color = color;
