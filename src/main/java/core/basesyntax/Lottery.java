f8849c14050c92d3d48a72c81af92e844915e02a
Andrii Malenko <andmalenko@gmail.com>
2022-07-12 11:30:37 +0300

    I've changed all requests.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_NUMBER = 100;
+    private Random random = new Random();
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
-        return new Ball(supplier.getRandomColor(), new Random().nextInt(100));
+        return new Ball(supplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
 }
