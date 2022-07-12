cac9d43a7295329ebb47704c98fdf24031ab43b3
Yura Koval <koval.yuriy.v@gmail.com>
2022-07-13 01:06:32 +0300

    I made number 100 as constant field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public class Lottery {
 
+    private static final int mrn = 100;
+
     public Ball getRandomBall() {
         ColorSupplier color = new ColorSupplier();
         String colorBall = color.getRandomColor();
         Random random = new Random();
-        int numberBall = random.nextInt(100);
+        int numberBall = random.nextInt(mrn);
         Ball ball = new Ball();
         ball.setBall(colorBall, numberBall);
         return ball;
