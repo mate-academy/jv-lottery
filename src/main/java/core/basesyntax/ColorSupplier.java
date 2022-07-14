5b1b2e9b437a653fd897d7052c67166a7851b220
golov <holovkodv12@gmail.com>
2022-07-14 16:05:59 +0300

    Created a new class Ball, added two variables plus a constructor of them, overridden the toString() method. Built up an ENUM class, filled it with a list of different colors. Changed the method getRandomColor(). Created a Lottery class and performed a method  getRandomBall() which return a random color and number Implement the method getRandomBall() in the Application class.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int colorBall = new Random().nextInt(Color.values().length);
+        return Color.values()[colorBall].toString();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color = colorSupplier.getRandomColor();
+        Random ran = new Random();
+        int number = ran.nextInt(100);
+        return new Ball(color, number);
+    }
+}
