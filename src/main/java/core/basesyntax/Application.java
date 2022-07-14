a37559d3964585cf8c3d6d8c848a5569bffcf0af
RomanDubovskyi <rdubowskij@gmail.com>
2022-07-14 19:39:36 +0300

    removed unneeded code

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,6 @@ package core.basesyntax;
 public class Application {
 
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
-
         System.out.println("Hello world");
     }
 }
