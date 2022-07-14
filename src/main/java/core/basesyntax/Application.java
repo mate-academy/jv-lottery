44a3e3430090588e295840f603e02d195bd90640
RomanDubovskyi <rdubowskij@gmail.com>
2022-07-14 17:55:45 +0300

    fixed compilation, added empty lines

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+
+
+
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
 
-        System.out.println("Hello world")
+        System.out.println("Hello world");
     }
 }
