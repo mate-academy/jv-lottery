e6925b80bc8a9f7051e6c5d64b64ea1d7ca43b94
Alexander <liero2D@gmail.com>
2022-07-15 15:24:38 +0300

    Created the Ball and Lottery classes. Methods in classes too. Enum. Please check the task. Thanks.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int indexRandomColor = new Random().nextInt(Color.values().length);
+        return Color.values()[indexRandomColor].toString();
     }
 }
