44344f685e355c4ca96a1f44ef3bd4a4f3950875
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-16 21:47:30 +0200

    Lottery2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+
     public static void main(String[] args) {
-    Ball ballfirst = Lottery.getRandomBall();
-    Ball ballsecond = Lottery.getRandomBall();
-    Ball ballthird = Lottery.getRandomBall();
-    System.out.println(ballfirst);
-    System.out.println(ballsecond);
-    System.out.println(ballthird);
+
+    Ball ball1 = Lottery.getRandomBall();
+    Ball ball2 = Lottery.getRandomBall();
+    Ball ball3 = Lottery.getRandomBall();
+
+    System.out.println(ball1);
+    System.out.println(ball2);
+    System.out.println(ball3);
     }
 }
