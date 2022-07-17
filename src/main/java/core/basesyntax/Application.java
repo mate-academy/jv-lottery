fb875c562bf692eb05897da20deb2e5d5d64a68f
Igor Petrichenko <i.petrichenko@gmail.com>
2022-07-17 14:02:54 +0300

    Created Lottery basket with 3 random ball

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
+        var lotteryBasket = new Object[]{ball1, ball2, ball3};
+
+        Arrays.stream(lotteryBasket).forEach(System.out::println);
     }
+
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    BLUE,
+    GREEN,
+    RED,
+    WHITE,
+    YELLOW
+}
