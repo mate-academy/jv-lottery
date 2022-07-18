3950a930e7269528f35e6b5a36b764b0dfc9639d
Maksym <101647779+Maksym-Kulakov@users.noreply.github.com>
2022-07-18 15:00:34 +0200

    Update src/main/java/core/basesyntax/Lottery.java
    Co-authored-by: Mykyta Arkhanhelskyi <nickarchangel97@gmail.com>

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BOUND = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier color = new ColorSupplier();
 
