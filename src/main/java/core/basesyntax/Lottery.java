1f65acc086a5069e5ed01172c0ac947ec5367e95
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 12:37:34 +0200

    random & color objs creating shifted out of meth & const created

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     Random random = new Random();
     ColorSupplier color = new ColorSupplier();
+    private final static int BOUND = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(BOUND));
         ball.setColor(color.getRandomColor());
         return ball;
     }
