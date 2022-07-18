a50c5743cb5b01fd1714c28099f99b455763f2e1
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 11:00:28 +0200

    static meth removed, Random field declared

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].name();
+    private Random random;
+
+    public String getRandomColor() {
+        random = new Random();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
