67f43c3ecb6e72e75e2ad6b843fad1f5233acda6
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-18 12:31:59 +0200

    random obj creating shifted out of meth

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random random;
+    private Random random = new Random();
 
     public String getRandomColor() {
-        random = new Random();
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
