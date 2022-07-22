7751c7e104068beda97f24ee85304aaa8136681f
Liubomyr Motsiak <motsiakliubomyr@gmail.com>
2022-07-22 15:20:18 +0300

    1. Create Ball class with following fields: color, number. 2. Create Enum with colors. 3. Implement method getRandomColor() in class ColorSupplier 4. Create class Lottery with getRandomBall() method. 5. In Application class create three balls and print information about them. 6. fixed error with String getRandomColor().

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -14,9 +14,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball {" +
-                "color = '" + color + '\'' +
-                ", number = " + number +
-                '}';
+        return "Ball color is " + color + "; Ball number is " + number;
     }
 }
