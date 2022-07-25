1ac6141d5c85813faf2e13b858fdffc50f0f2562
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-07-25 15:49:41 +0300

    empty lines were removed, name of enum was changed, new constant in Lottery was added

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum Color {
+    BLACK,
+    WHITE,
+    RED,
+    YELLOW,
+    GREEN
+}
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int RANDOM_FROM = 100;
 
     public Ball getRandomBall() {
-        return new Ball((new Random().nextInt(100) + 1), new ColorSupplier().getRandomColor());
+        return new Ball((new Random().nextInt(RANDOM_FROM) + 1),
+                new ColorSupplier().getRandomColor());
     }
 }
