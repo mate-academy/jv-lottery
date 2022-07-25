0b0a4c1d2e30f37d3dccc0893801e570a2054a7f
Benatti <ssunica0@gmail.com>
2022-07-25 15:01:25 +0300

    fixed common mistakes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public static final int MAX_NUMBER = 100;
+
     public Ball getRandomBall() {
         return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER));
     }
