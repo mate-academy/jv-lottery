ad0d3881f8810732c5abb075b16ce4e54605ee93
Benatti <ssunica0@gmail.com>
2022-07-25 14:52:01 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,16 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int COUNT_BALLS = 3;
+
     public static void main(String[] args) {
-        Ball[] balls = {new Lottery().getRandomBall(),
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall()};
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[COUNT_BALLS];
+        for (Ball ball : balls) {
+            ball = lottery.getRandomBall();
+        }
         for (Ball ball: balls) {
-            System.out.println(ball.toString());
+            System.out.println(ball);
         }
     }
 }
