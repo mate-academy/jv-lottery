0ac1558154c401da3113e5ffe9553a14cf14286e
Vyacheslav Verbanov <v.verbanov@gmail.com>
2022-07-25 16:20:14 +0200

    rename enum Color, make maximum ballnumber as constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(new Random().nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
