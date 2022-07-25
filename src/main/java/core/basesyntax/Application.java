7f0e319db2d41274a8d692d7a1f81c604665e511
Anna Popova <anitapopova1996@gmail.com>
2022-07-25 13:27:34 +0300

    Change Lottery variable array to object Lottery. Changed loop foreach -> loop for.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery[] ball = {new Ball(),new Ball(), new Ball()};
-        for (Lottery balls : ball) {
-            System.out.println(balls.getRandomBall());
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
