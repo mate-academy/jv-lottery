0d1bb1bf1d34fb95b1712313245cdd2db755f6c6
Anna Popova <anitapopova1996@gmail.com>
2022-07-25 12:59:13 +0300

    Removed unnecessary instances and created just one in Application in main().

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery ball1 = new Ball();
-        Lottery ball2 = new Ball();
-        Lottery ball3 = new Ball();
-
-        Lottery[] ball = {ball1, ball2, ball3};
+        Lottery[] ball = {new Ball(),new Ball(), new Ball()};
         for (Lottery balls : ball) {
             System.out.println(balls.getRandomBall());
         }
