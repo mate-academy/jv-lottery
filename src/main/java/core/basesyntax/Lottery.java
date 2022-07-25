49accdab94f019ef0bc0e4b236f21153e6bd8767
Anna Popova <anitapopova1996@gmail.com>
2022-07-25 14:55:13 +0300

    Added constant in Lottery class.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int BOUND = 100;
+    private static final int NUMBER_BOUND = 100;
+
     public Ball getRandomBall() {
         Random rand = new Random();
-        int ballNumber = rand.nextInt(BOUND);
+        int ballNumber = rand.nextInt(NUMBER_BOUND);
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
 
