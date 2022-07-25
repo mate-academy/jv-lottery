46d4028a14da517131abeacd54c56be684b1c6cd
Benatti <ssunica0@gmail.com>
2022-07-25 11:35:49 +0300

    fixed common mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = {Lottery.getRandomBall(), Lottery.getRandomBall(), Lottery.getRandomBall()};
+        Ball[] balls = {new Lottery().getRandomBall(),
+                new Lottery().getRandomBall(),
+                new Lottery().getRandomBall()};
         for (Ball ball: balls) {
             System.out.println(ball.toString());
         }
