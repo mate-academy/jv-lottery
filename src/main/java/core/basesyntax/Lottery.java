f7e8c6e129fc4aa3b4d7743ce5bd11f7dfd85399
Liubomyr Motsiak <motsiakliubomyr@gmail.com>
2022-07-25 21:04:39 +0300

    1. returned the original version 2. deleted array ball

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int number = 100;
     Random random = new Random();
     ColorSupplier colorSupplier = new ColorSupplier();
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(number));
         return ball;
     }
 }
