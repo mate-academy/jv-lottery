4e8ca1d707aee9a03c211cb246ea3f50a120b2cf
Andrii Shevchuk <zovten156@gmail.com>
2022-07-26 12:12:34 +0300

    Made a classes 'Ball' whith 2 fields, 'Lottery' with `getRandomBall()` method, enam 'Colors', and test in a 'Application'.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random rnd = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        int index = rnd.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    Random rnd = new Random();
+    ColorSupplier colorSupplier = new ColorSupplier();
+    int HUNDRED = 100;
+
     public Ball getRandomBall() {
-        String color = new ColorSupplier().getRandomColor().toString();
-        int number = new Random().nextInt(100);
+        String color = colorSupplier.getRandomColor().toString();
+        int number = rnd.nextInt(HUNDRED);
         Ball ball = new Ball(color, number);
         return ball;
     }
