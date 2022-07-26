e885d542ecb40bbdf3b1a42356724569ff14f820
Andriii Shevchuk <zovten156@gmail.com>
2022-07-26 10:09:45 +0300

    Update src/main/java/core/basesyntax/Application.java
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery ball = new Lottery();
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
             System.out.println(ball.getRandomBall());
         }
