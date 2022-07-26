46e57d446179694f524c6c9bd742fdc4f181550e
Yaroslav Vashchenko <yaroslav.vashchenko@gmail.com>
2022-07-26 18:00:23 +0300

    homework lottery after 1st review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        final int MAX_NUMBER = 100;
+        final int max_number = 100;
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
-        int number = random.nextInt(MAX_NUMBER);
+        int number = random.nextInt(max_number);
         return new Ball(colorSupplier.getRandomColor(), number);
     }
 }
