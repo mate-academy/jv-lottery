ab5230ab2df60ae35ce2e13fcf2b34992a29d767
Nazar <litvik.12@gmail.com>
2022-07-26 14:38:14 +0300

    changed method of taking random color from enum

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,13 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private String[] colors = new String[Color.values().length];
 
     public String getRandomColor() {
-        for (int i = 0; i < Color.values().length; i++) {
-            colors[i] = Color.values()[i].toString();
-        }
         Random random = new Random();
-        return colors[random.nextInt(colors.length)];
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
