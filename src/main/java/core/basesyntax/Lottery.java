11bcfadd677c589c5db0e81281c47cc96be97a3a
Mikhail Maiorov <majorov.michail@gmail.com>
2022-07-26 12:51:32 +0300

    -constant value MAX_BALL_VALUE was added -method getRandomBall() was refactored

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_VALUE = 100;
+
     public Ball getRandomBall() {
         ColorSupplier color = new ColorSupplier();
-        return new Ball(new Random().nextInt(99) + 1, color.getRandomColor());
+        return new Ball(new Random().nextInt(MAX_BALL_VALUE), color.getRandomColor());
     }
 }
