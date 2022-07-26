c99fa9f3f12eacdcc77036d02521ae5906160a90
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-26 11:58:54 +0200

    added constant BOUND

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BOUND = 100;
+
     public Ball getRandomBall() {
         Random random = new Random();
-        int value = random.nextInt(100);
+        int value = random.nextInt(BOUND);
         return new Ball(new ColorSupplier().getRandomColor().toString(), value);
     }
 }
