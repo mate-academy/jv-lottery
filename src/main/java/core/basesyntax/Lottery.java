2ce736a3cae1f0afda0e9f710ec4fe6f6c0843b6
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-07-26 12:17:14 +0300

    Maximal number on ball was made as constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
+
     public Ball getRandomBall() {
         Random random = new Random();
         ColorSupplier colorSupplier = new ColorSupplier();
-        int number = random.nextInt(100);
+        int number = random.nextInt(MAX_NUMBER);
         String color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
