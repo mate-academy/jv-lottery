baf323b2f2b668cf3db6acedb65f80d0e794ed3d
Artem Chubai <chubayee@gmail.com>
2022-07-26 14:01:43 +0300

    fixed replace 100 with constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery firstAttempt = new Lottery();
         Lottery secondAttempt = new Lottery();
         Lottery thirdAttempt = new Lottery();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MaxRandomBalls = 100;
+
     public String getRandomBall() {
         Random random = new Random();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(MaxRandomBalls);
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = colorSupplier.getRandomColor();
         Ball randomsBall = new Ball(randomColor, randomNumber);
