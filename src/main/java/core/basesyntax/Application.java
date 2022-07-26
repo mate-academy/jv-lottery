d9b57389db1d7d674e4e713330cf6a8c214b0f08
Artem Chubai <chubayee@gmail.com>
2022-07-26 17:29:16 +0300

    fixed ALL mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery firstAttempt = new Lottery();
-        Lottery secondAttempt = new Lottery();
-        Lottery thirdAttempt = new Lottery();
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            System.out.println(firstAttempt.getRandomBall()
-                    + secondAttempt.getRandomBall()
-                    + thirdAttempt.getRandomBall());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
