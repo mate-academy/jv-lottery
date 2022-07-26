8f94aac053df3bfd685c51d9b59515f1a0453ab1
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-07-26 13:12:58 +0300

    five commit making final variable of lottery class and ColorSupplier

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     static final Random random = new Random();
     static final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int number = random.nextInt(100);
         String color = colorSupplier.getRandomColor(random);
