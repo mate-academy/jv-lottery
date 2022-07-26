f43e7cb938b55b345753e3aebe3584a9d06dffd4
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-26 11:57:16 +0200

    added getter and setter fot class Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -9,8 +9,24 @@ public class Ball {
         this.number = number;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     @Override
     public String toString() {
-        return color + " ---> " + number;
+        return "Color: " + getColor() + ", " + "number: " + getNumber();
     }
 }
