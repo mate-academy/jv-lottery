31328d968c9cea513fa0a86b7b36ac7db84c5836
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-26 22:23:07 +0300

    code style

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,16 @@ public class Ball {
     private String color;
     private int number;
 
-    @Override
-    public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
-    }
-
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                + "color='" + color + '\''
+                + ", number=" + number
+                + '}';
     }
 }
