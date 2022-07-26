94fd4eeff332b4c44060907f6fedeb392b5e0fd2
asha <ashajanebrown@gmail.com>
2022-07-26 23:56:10 +0300

    lottery for three random balls created

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        int numberOfBalls = 3;
+
+        for (int i = 0; i < numberOfBalls; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball.toString());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    BLACK,
+    WHITE,
+    BROWN,
+    GREEN,
+    BLUE,
+    YELLOW,
+    PINK,
+    GREY
+}
