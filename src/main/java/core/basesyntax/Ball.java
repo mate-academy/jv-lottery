ef7bf5f28cec621e2080d8d8221de285ddcf988f
Veers <raadch3011@gmail.com>
2022-07-26 22:26:31 +0300

    created classes: Lottery, ColorSupplier, Colors(enum class) and Ball. Ball have fields: String color and int number. It has a constructor and overriden method to string. Lottery creates a new Ball class with random number and color(colors we take from ColorSupplier which takes from enum class)

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
+    private String color;
+    private int number;
 
-    public  Ball(String color, int number) {
+    public Ball(String color, int number) {
         this.color = color;
+        this.number = number;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void  setNumber(int number) {
         this.number = number;
     }
 
--- src/main/java/core/basesyntax/Colors.java
+++ src/main/java/core/basesyntax/Colors.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-
 public enum Colors {
     RED,
     GREEN,
