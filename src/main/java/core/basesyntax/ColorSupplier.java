a29fca5625c178c6f30f1309bb67e25c42c3a0d3
Nazar <litvik.12@gmail.com>
2022-07-26 14:34:43 +0300

    added classes Ball, Color, Lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private String[] colors = new String[Color.values().length];
+
     public String getRandomColor() {
-        return null;
+        for (int i = 0; i < Color.values().length; i++) {
+            colors[i] = Color.values()[i].toString();
+        }
+        Random random = new Random();
+        return colors[random.nextInt(colors.length)];
     }
 }
