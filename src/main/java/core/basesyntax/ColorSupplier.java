400e73d18e899e9f622a6f8790babd9ff0ef16ec
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-07-26 11:00:52 +0300

    Added solution with changes from Mentor (fixed enum)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int index = new Random().nextInt(ColorsForBalls.values().length);
-        return String.valueOf(ColorsForBalls.values()[index]);
+        int index = new Random().nextInt(BallColor.values().length);
+        return String.valueOf(BallColor.values()[index]);
     }
 }
 
