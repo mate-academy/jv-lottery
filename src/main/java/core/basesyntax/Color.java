f3e6aae335ca26b7c3a7b4e46201c88653c65ce6
Nazar <litvik.12@gmail.com>
2022-07-26 15:54:07 +0300

    deleted unused method, added right constant NUMBER...

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,28 +1,19 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public enum Color {
-
-    RED("Красный"),
-    BLUE("Синий"),
-    GREEN("Зелёный"),
-    GRAY("Серый"),
-    PURPLE("Фиолетовый"),
-    PINK("Розовый"),
-    YELLOW("Жёлтый"),
-    WHITE("Белый"),
-    BLACK("Черный");
+    RED("Red"),
+    BLUE("Blue"),
+    GREEN("Green"),
+    GRAY("Gray"),
+    PURPLE("Purple"),
+    PINK("Pink"),
+    YELLOW("Yellow"),
+    WHITE("White"),
+    BLACK("Black");
 
     private String color;
+
     Color(String color) {
         this.color = color;
-    }
-
-    public String getColor() {
-        Random random = new Random();
-        int numberColor = random.nextInt(8) + 1;
-        Color.valueOf(String.valueOf(numberColor));
-        return "";
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static int count = 99;
+    private static final int NUMBER = 99;
     private Random random = new Random();
 
-    public Object getRandomBall(String color) {
-        int numberOfBall = random.nextInt(count) + 1;
-        Ball ball = new Ball(color,numberOfBall);
-        return ball;
+    public Ball getRandomBall(String color) {
+        int numberOfBall = random.nextInt(NUMBER) + 1;
+        return new Ball(color,numberOfBall);
     }
 }
