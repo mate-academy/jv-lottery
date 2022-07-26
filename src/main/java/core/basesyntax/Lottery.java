0f65523beb7ee303ea9d6549ea485cae8d7da2f8
Artem Chubai <chubayee@gmail.com>
2022-07-26 19:24:37 +0300

    fixed name MaxNumber

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MaxRandomBalls = 100;
+    private static final int MAX_NUMBER = 100;
 
     public String getRandomBall() {
         Random random = new Random();
-        int randomNumber = random.nextInt(MaxRandomBalls);
+        int randomNumber = random.nextInt(MAX_NUMBER);
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = colorSupplier.getRandomColor();
         Ball randomsBall = new Ball(randomColor, randomNumber);
