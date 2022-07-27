724b67bf9c0e98573ff9ddf4ce9a2698abd85c7f
UmyaFamilia <umyafamilya1974@gmail.com>
2022-07-27 23:21:30 +0300

    mate

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery[] lotteries = new Lottery[3];
+        for (int i = 0; i < lotteries.length; i++) {
+            lotteries[i] = new Lottery();
+            System.out.println("color "+lotteries[i].getRandomBall().getColor()+" number "+lotteries[i].getRandomBall().getNumber());
+        }
     }
 }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -7,23 +7,18 @@ public class Ball {
         this.color=color;
         this.number=number;
     }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     public int getNumber() {
         return number;
     }
-
     public void setNumber(int number) {
         this.number = number;
     }
-
     @Override
     public String toString() {
         return "Ball{" +
