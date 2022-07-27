2430878a75e2318f89290473ed81b81046ff59ac
Danylo Frolov <frolovdanilik@gmail.com>
2022-07-27 17:58:42 +0300

    codestyle corrections

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_POSSIBLE_NUM = 100;
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_POSSIBLE_NUM));
+        return new Ball(new ColorSupplier().getRandomColor(),
+                new Random().nextInt(MAX_POSSIBLE_NUM));
     }
 }
