107c621afcff7287c069b56e5f7dc6176abda024
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2022-07-27 14:42:49 +0300

    create array of balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball firstBall = lottery.getRandomBall();
-        Ball secondBall = lottery.getRandomBall();
-        Ball thirdBall = lottery.getRandomBall();
-        System.out.println("first ball -> " + firstBall);
-        System.out.println("second ball -> " + secondBall);
-        System.out.println("third ball -> " + thirdBall);
+        Ball[] balls = new Ball[]{lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
+        for (Ball ball : balls) {
+            System.out.println(ball);
+        }
     }
 }
