f3e1304d36f2f6735b5087974b75d1a7a9671eb4
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-07-27 16:57:30 +0300

    My home work lottery v03

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
     static final int MAX_NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
