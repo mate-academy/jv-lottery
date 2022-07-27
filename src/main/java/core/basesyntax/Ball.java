f7290fa20946824fc78673e735b82e6ccb901687
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-07-27 11:50:50 +0300

    Lottery was developed

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,6 +4,11 @@ public class Ball {
     private String name;
     private String color;
 
+    public Ball(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
     @Override
     public String toString() {
         return "Ball{" +
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+/**
+ * A lottery with multi-colored balls is held.
+ */
+public class Lottery extends ColorSupplier {
+    private static final int MAX_NUMBER = 100;
+
+    /**
+     * Method creates a ball with a random color and a random number
+     * (the maximum possible number is 100).
+     * @return - new Ball
+     */
+    public Ball getRandomBall() {
+        String ballColor = getRandomColor();
+        int ballNamber = new Random(MAX_NUMBER).nextInt() + 1;
+        return new Ball(Integer.toString(ballNamber), ballColor);
+    }
+}
