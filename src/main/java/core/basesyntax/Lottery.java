d420f2db61146dc4ca296f9ca00835e39439a3ff
fliarkovskyi <fliarkovskyi@gmail.com>
2022-07-27 16:19:38 +0300

    fix static field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
 
-    static final int MAX_NUMBER_BALL = 100;
+    private static final int MAX_NUMBER_BALL = 100;
 
     public Ball getRandomBall() {
         int randomNumberBall = new Random().nextInt(MAX_NUMBER_BALL);
