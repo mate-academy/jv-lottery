479f2b70993014eacbe8a5c9c9a76fd96d3a64ce
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-07-27 16:12:25 +0300

    added numbering balls

--- src/main/java/mate/Main.java
+++ src/main/java/mate/Main.java
@@ -3,7 +3,7 @@ package mate;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println((i + 1)+ " " + new Lottery().getRandomBall().toString());
+            System.out.println((i + 1) + " " + new Lottery().getRandomBall().toString());
         }
     }
 }
