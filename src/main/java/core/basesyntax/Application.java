a0d56e69f1c7c8b9bdac76cc8be26d02da53276d
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-07-27 23:19:43 +0300

    was created one object Lottery instead three in loop before; was changed integer literal by constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[3];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int MAX_VALUE = 100;
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(new Random().nextInt(MAX_VALUE));
         return ball;
     }
 }
