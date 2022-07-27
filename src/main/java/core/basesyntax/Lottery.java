8b4f966cae6ebf02fce0558cf049a390b6443b98
Alex <alexandrov.alex.al@gmail.com>
2022-07-27 21:32:48 +0300

    Changed the type of the constant.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int LIMIT = 100;
+
     public Ball getRandomBall() {
-        final int limit = 100;
         Ball ball = new Ball();
         ColorSupplier colorSupplier = new ColorSupplier();
         ball.setColor(colorSupplier.getRandomColor());
         Random random = new Random();
-        ball.setNumber(random.nextInt(limit));
+        ball.setNumber(random.nextInt(LIMIT));
         return ball;
     }
 }
