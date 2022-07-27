03177d859e66fa56330c7e2e82230160461709b2
Vasyl Pankiv <wasilius2@gmail.com>
2022-07-27 14:41:58 +0300

    add Ball class

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Ball {
+    String color;
+    int num;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", num=" + num +
+                '}';
+    }
+}
