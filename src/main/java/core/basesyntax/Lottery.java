1afe72c05422e7baca00a2b8654a8b490d9c4baa
Sofiia <sofiakazzz@gmail.com>
2022-07-27 17:51:16 +0300

    realized task is to return three random balls

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_RandomNumber = 100;
+    private static final int MAX_Number = 100;
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(MAX_RandomNumber));
+        ball.setNumber(new Random().nextInt(MAX_Number));
         return ball;
     }
 }
