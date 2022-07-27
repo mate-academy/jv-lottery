7b7f1ac5b5e79b655bfca0dfc5076b77a8392714
Valeriy M <valeriym250@gmail.com>
2022-07-27 11:37:42 +0300

    Fixed Bugs,
    I can`t give name like "BOUND_INDEX".
    I tried call variable "BOUND_INDEX", but have next  messages
    
    [ERROR] E:\IT\trying_install_IDEA\Projects\Tasks_From_Git\lottery\src\main\java\core\basesyntax\Lottery.java:6:23: Abbreviation in name 'BOUND' must contain no more than '2' consecutive capital letters. [AbbreviationAsWordInName]
    [ERROR] E:\IT\trying_install_IDEA\Projects\Tasks_From_Git\lottery\src\main\java\core\basesyntax\Lottery.java:6:23: Member name 'BOUND' must match pattern '^[a-z][a-z0-9][a-zA-Z0-9]*$'. [MemberName]
    [ERROR] E:\IT\trying_install_IDEA\Projects\Tasks_From_Git\lottery\src\main\java\core\basesyntax\Lottery.java:14:16: Abbreviation in name 'getBOUND' must contain no more than '2' consecutive capital letters. [AbbreviationAsWordInName]

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,15 +1,30 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Lottery {
-    public Ball getRandomBall() {
-        Ball randomBall = new Ball();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        final int bound = 100;
+    private Ball randomBall = new Ball();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final int boundIndex = 100;
 
+    public Ball getRandomBall() {
         randomBall.setColor(colorSupplier.getRandomColor());
-        randomBall.setNumber(new Random().nextInt(bound));
+        randomBall.setNumber(colorSupplier.getRandom().nextInt(boundIndex));
         return randomBall;
     }
+
+    public int getBoundIndex() {
+        return boundIndex;
+    }
+
+    public void setRandomBall(Ball randomBall) {
+        this.randomBall = randomBall;
+    }
+
+    public ColorSupplier getColorSupplier() {
+        return colorSupplier;
+    }
+
+    public void setColorSupplier(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
 }
