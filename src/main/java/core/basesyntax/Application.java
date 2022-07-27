96c1135e4cd4f833250078a0beef173a11157c0f
LitvinOleksandr <sanek.litvin@gmail.com>
2022-07-27 17:25:26 +0300

    Added classic colors Use Encapsulation for name and color. This methods return color and number. Used for (i...) from example file, added "iterations" for easily changed amount times.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        int iterations = 3; //How many times you want to play lottery
+        Ball[] balls = new Ball[iterations];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = Lottery.getRandomBall();
+            System.out.println(balls[i].toString());
+        }
     }
 }
