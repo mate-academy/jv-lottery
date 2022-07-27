41f7aefca9c3ba0e921aa3b342d42d7d3786b482
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-07-27 14:36:03 +0300

    added Lottery and method getRandomBall()

---
+++ src/main/java/mate/Lottery.java
@@ -0,0 +1,15 @@
+package mate;
+
+import java.util.Random;
+
+public class Lottery {
+
+    static final int MAX_NUMBER = 100;
+
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(new Random().nextInt(MAX_NUMBER));
+        return ball;
+    }
+}
