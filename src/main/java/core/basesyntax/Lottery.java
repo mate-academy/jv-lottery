6d96f5405ed8cc60da40755a35ea59364a1ff4dc
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-07-27 10:42:06 +0300

    add constant MAX_VALUE

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,6 @@ public class Lottery {
     private static final int MAX_VALUE = 99;
 
     public Ball getRandomBall() {
-
         ColorSupplier color = new ColorSupplier();
         int number = new Random().nextInt(MAX_VALUE) + 1;
         Ball ball = new Ball(color.getRandomColor(), number);
