db44a7fc10c42f1de456ea3d1d624255ecfeb211
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2022-07-27 17:49:27 +0300

    private constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int MAX_NUMBER = 100;
+    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
