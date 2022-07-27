f0d4bbfc1162cbafc7013b2bec329ef8c7c0ad6c
asha <ashajanebrown@gmail.com>
2022-07-27 14:26:50 +0300

    added max lottery number constant deleted an empty line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,7 +5,6 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         int numberOfBalls = 3;
-
         for (int i = 0; i < numberOfBalls; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball.toString());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
-        int number = new Random().nextInt(101);
+        int number = new Random().nextInt(MAX_NUMBER);
         String color = new ColorSupplier().getRandomColor();
         return new Ball(color, number);
     }
