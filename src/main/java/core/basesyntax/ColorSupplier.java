68814d35010eb271f8bcecb225dbd6950bc105ff
UmyaFamilia <umyafamilya1974@gmail.com>
2022-07-28 16:54:55 +0300

    mate

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,8 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         Random random = new Random();
-        int index = 0;
-        index = random.nextInt(DifferentColors.values().length);
-        return DifferentColors.values()[index].toString();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 import java.util.Random;
-
 public class Lottery {
+    public final static int BORDER=100;
     public Ball getRandomBall() {
         Random random = new Random();
-        return new Ball(new ColorSupplier().getRandomColor(),random.nextInt(100));
+        return new Ball(new ColorSupplier().getRandomColor(),random.nextInt(BORDER));
     }
 }
