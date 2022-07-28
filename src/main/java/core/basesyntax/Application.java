0550f90267216a8d2ce99974d617bbf5f3455dc7
LubusUzh <lubus.uzh@gmail.com>
2022-07-28 12:07:36 +0300

    Fixed sout method in Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < 5; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println(ball.toString());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
