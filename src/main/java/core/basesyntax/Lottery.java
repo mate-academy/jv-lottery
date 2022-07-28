70e4cebf450ccf74e2f29aecb52180e32e740b74
UmyaFamilia <umyafamilya1974@gmail.com>
2022-07-28 17:00:39 +0300

    mate

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 import java.util.Random;
+
 public class Lottery {
-    public final static int BORDER=100;
+    private static final int BORDER = 100;
+
     public Ball getRandomBall() {
         Random random = new Random();
         return new Ball(new ColorSupplier().getRandomColor(),random.nextInt(BORDER));
