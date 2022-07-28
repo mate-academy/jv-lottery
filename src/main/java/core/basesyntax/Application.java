70a2b38349bda432fdffe0e102438b8d51a20674
LitvinOleksandr <sanek.litvin@gmail.com>
2022-07-28 14:44:25 +0300

    called Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        int iterations = 3; //How many times you want to play lottery
+        int iterations = 3;
         Ball[] balls = new Ball[iterations];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = Lottery.getRandomBall();
-            System.out.println(balls[i].toString());
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i]);
         }
     }
 }
