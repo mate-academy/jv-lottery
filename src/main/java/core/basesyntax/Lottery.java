9d29440517703453f97615c5042fd61791ed536d
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2022-07-28 15:09:48 +0300

    Add private to final int MAX_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int MAX_NUMBER = 100;
+    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         String color = new ColorSupplier().getRandomColor();
