22d2cbfcec77dea20e31fc8fba23364b023740cc
Sofiia <sofiakazzz@gmail.com>
2022-07-28 14:40:09 +0300

    private static final int MAX_NUMBER  got name, all letters in  UPPER CASE

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_Number = 100;
+    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(MAX_Number));
+        ball.setNumber(new Random().nextInt(MAX_NUMBER));
         return ball;
     }
 }
