b586a6cc9be6185b857581a02ef6267e7493d263
IraHurko <iragurko19@gmail.com>
2022-07-28 15:27:57 +0300

    fixed a mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery[] balls = new Lottery[] {new Lottery(), new Lottery(), new Lottery()};
-        for ( Lottery ball : balls) {
+        for (Lottery ball : balls) {
             System.out.println(ball.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     static final int bound = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = String.valueOf(colorSupplier.getRandomColor());
