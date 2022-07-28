cbe537d4c1f49993c821653b0adacc21dc5eb3ac
Ilja Nekrituk <Nekrit99@gmail.com>
2022-07-28 16:26:54 +0300

    new lottery outside the loop

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int BOUND = 100;
-    private final int randomNumber = new Random().nextInt(BOUND);
+    private static final int BOUND = 100;
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), randomNumber);
+        ColorSupplier color = new ColorSupplier();
+        int number = new Random().nextInt(BOUND);
+        return new Ball(color.getRandomColor(), number);
     }
 }
