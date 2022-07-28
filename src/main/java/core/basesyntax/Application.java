215279b24a7913822864b758ceaf19132ea2a489
IraHurko <iragurko19@gmail.com>
2022-07-28 12:31:44 +0300

    Deleted commit on a line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery ball1 = new Lottery();
         System.out.println(ball1.getRandomBall());
         Lottery ball2 = new Lottery();
