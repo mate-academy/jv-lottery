e9a6c8566febdb691d26c28766d5ee6c3b3cb772
Misha Brovkin <haasiakira@gmail.com>
2022-07-28 14:32:27 +0300

    Ñfixed constant name and modificator

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,7 +7,7 @@ public class Lottery {
     private final ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int value = new Random().nextInt(maxValue);
+        int value = new Random().nextInt(MAX_VALUE);
         return new Ball(color.getRandomColor(), value);
     }
 }
