aba412947351e63225d1b6aa6ad87becaf7e479b
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-07-28 13:52:56 +0300

    all empty lines are removed, enum name is changed , constant is created

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
-        int number = new Random().nextInt(100);
+        int number = new Random().nextInt(NUMBER);
         return new Ball(colorSupplier.getRandomColor(),number);
-
     }
 }
 
