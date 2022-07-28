1d37f6d68bc20c90d34f5cac6ca25b606759c960
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-07-28 08:30:44 +0300

    was added access modification to constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int MAX_VALUE = 100;
+    private static final int MAX_VALUE = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
