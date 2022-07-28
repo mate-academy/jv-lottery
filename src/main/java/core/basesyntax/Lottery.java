7b964621a8780e22f9cffa6ffff709f9cf083b81
Misha Brovkin <haasiakira@gmail.com>
2022-07-28 12:20:49 +0300

    Ðixed Ñcheckstyle errors

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int MAX_VALUE = 100;
+    private final int maxValue = 100;
     private final ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int value = new Random().nextInt(MAX_VALUE);
+        int value = new Random().nextInt(maxValue);
         return new Ball(color.getRandomColor(), value);
     }
 }
