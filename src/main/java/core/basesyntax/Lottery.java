36b0d8d10f838fd42fbc0c2b64ff83ae8ab1adcd
Thousandfold <you@example.com>
2022-07-28 15:24:07 +0300

    fixe_constant_in_class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int LIMIT = 100;
+
     public Ball getRandomBall() {
         ColorSupplier color = new ColorSupplier();
         Random value = new Random();
-        final int limit = 100;
-        int num = value.nextInt(limit);
+        int num = value.nextInt(LIMIT);
         return new Ball(color.getRandomColor().toString(), num);
     }
 }
