816ff67cdcc6d8009388b5ba8d00692f5e4f4716
Andriy Linkov <linkoff.as@gmail.com>
2022-07-29 09:31:01 +0300

    Created classes: Ball, Lottery; enum: Colors. Implemented method getRandomColor() in class ColorSupplier

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+
+        Ball randomBall1 = lottery.getRandomBall();
+        Ball randomBall2 = lottery.getRandomBall();
+        Ball randomBall3 = lottery.getRandomBall();
+
+        System.out.println(randomBall1);
+        System.out.println(randomBall2);
+        System.out.println(randomBall3);
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    GREEN,
+    BLUE
+}
