c3f76cd52500df50e16172dfb547db0a6be73321
Sergei Matiko <adgodst@gmail.com>
2022-07-30 21:33:43 +0300

    hm-solution

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        int number = new Random().nextInt(101);
+        Ball randomBall = new Ball(new ColorSupplier().getRandomColor(), number);
+        return randomBall;
+    }
+}
