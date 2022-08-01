8091cafb7976c2cf323702d67e5851e22719bbda
Andriy Linkov <linkoff.as@gmail.com>
2022-08-01 10:35:42 +0300

    removed empty line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-
         for (int i = 0; i < 3; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
