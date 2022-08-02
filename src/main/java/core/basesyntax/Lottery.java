f27d8a14d4799192183a49e227cb4aed43a9b836
victoriadobryden <vikidobryden@gmail.com>
2022-08-02 04:11:28 +0300

    Refactored hw lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int BOUND = 100;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Color color = colorSupplier.getRandomColor();
-        final int number = new Random().nextInt(100);
+        final int number = new Random().nextInt(BOUND);
         Ball ball = new Ball(color, number);
         return ball;
     }
