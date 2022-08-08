c6d9bcad17552bcabc4d6f798fb776607800dc7e
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-08 18:37:49 +0200

    Correct mistakes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random index = new Random();
+    private Random index = new Random();
+
     public String getRandomColor() {
         return Colors.values()[index.nextInt(Colors.values().length)].getTitle();
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
     public void getRandomBall() {
         Ball ball = new Ball();
         ball.setNumber(random.nextInt(100));
