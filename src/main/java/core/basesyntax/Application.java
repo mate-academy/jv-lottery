93fd5481caae4f8feb4c25f116d11916ee3ed5d8
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-08 15:46:19 +0200

    Code style has been corrected

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-    Lottery firstBall = new Lottery();
-    Lottery secondBall = new Lottery();
-    Lottery thirdBall = new Lottery();
-    getLotteryBalls(firstBall,secondBall,thirdBall);
+        Lottery firstBall = new Lottery();
+        Lottery secondBall = new Lottery();
+        Lottery thirdBall = new Lottery();
+        getLotteryBalls(firstBall,secondBall,thirdBall);
     }
-    public static void getLotteryBalls(Lottery ... lotteries) {
+
+    public static void getLotteryBalls(Lottery... lotteries) {
         for (Lottery lottery : lotteries) {
             lottery.getRandomBall();
         }
