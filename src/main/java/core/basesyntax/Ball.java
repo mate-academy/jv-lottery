02b89b59cf17231b713e08acc2a372023903c19b
StYugurt <stulkiv@gmail.com>
2022-08-08 22:40:46 +0300

    Lusuj the best

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Ball {
+    private final Color color;
+    private final int number;
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball color is " + getColor() + ", number is " + getNumber();
+    }
+
+}
