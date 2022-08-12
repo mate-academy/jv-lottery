bd159e2e260d82613a32137ef0dc5f20addbaddd
Mykhailo Levko <mykhailo.levko@gmail.com>
2022-08-11 21:10:16 -0400

    jv-lottery

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -22,9 +22,12 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
+        return "Ball{"
+                +
+                "color='" + color + '\''
+                +
+                ", number=" + number
+                +
                 '}';
     }
 }
