893b41ddef38b1d5f2b4b3581aa0bc2d4474a663
petrozagorodnii <patriculusus@gmail.com>
2022-08-13 15:59:41 +0300

    Extracted ColorSupplier and Random as static variables in Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final Random random = new Random();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
     }
 }
