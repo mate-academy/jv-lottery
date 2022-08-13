756f0fbb25d0436097b238c8b1471b746a340cb8
Andrii Petruk <andrzej.petruk@bk.ru>
2022-08-13 10:03:31 +0200

    created a Random field in the class ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+
+    private static final Random random = new Random();
+
+    public static String getRandomColor() {
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].name().toLowerCase();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private ColorSupplier randomColor = new ColorSupplier();
-    private Random random = new Random();
-
     public Ball getRandomBall() {
-        int randomValue = random.nextInt(100);
-        return new Ball(randomColor.getRandomColor(),randomValue);
+        int randomValue = new Random().nextInt(100);
+        return new Ball(ColorSupplier.getRandomColor(),randomValue);
     }
 }
