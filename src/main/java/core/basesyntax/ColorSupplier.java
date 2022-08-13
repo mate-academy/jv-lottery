540b531025cd74ca4206b3fd54e252536ba0d38f
petrozagorodnii <patriculusus@gmail.com>
2022-08-13 15:52:00 +0300

    Fixed 'METHOD_DEF' should be separated from previous statement.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final Random random = new Random();
+
     public String getRandomColor() {
         return BallColors.values()[random.nextInt(BallColors.values().length)].toString();
     }
