855b8b0c36e629efcf606ee2a6c0d24872a2ddbe
Andrii Petruk <andrzej.petruk@bk.ru>
2022-08-13 12:26:49 +0200

    created a Random field in the class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    private static final Random random = new Random();
+
     public Ball getRandomBall() {
-        int randomValue = new Random().nextInt(100);
+        int randomValue = random.nextInt(100);
         return new Ball(ColorSupplier.getRandomColor(),randomValue);
     }
 }
