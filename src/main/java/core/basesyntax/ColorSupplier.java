7c528c5e8afab13064419acbc7eefbc665ba5da8
petrozagorodnii <patriculusus@gmail.com>
2022-08-13 15:49:31 +0300

    Extracted: private static final Random random = new Random();

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
     public String getRandomColor() {
-        return BallColors.values()[new Random().nextInt(BallColors.values().length)].toString();
+        return BallColors.values()[random.nextInt(BallColors.values().length)].toString();
     }
 }
