6bccb18979f7717ce9c9206eca14e8c7cd3de6f4
MAxim Sychinskiy <max.sychinskiy@gmail.com>
2022-08-14 18:13:13 +0300

    Added fixes to class ColorSupplier. Variable randon declared only once.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
+    private Random random = new Random();
+
     public Colors getRandomColor() {
-        Random random = new Random();
         int colorBallIndex = random.nextInt(Colors.values().length);
         return Colors.values()[colorBallIndex];
-
     }
 }
