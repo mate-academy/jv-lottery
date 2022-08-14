ea09bd15b8000d7634fe80e6f98de58a739d6496
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-14 12:49:57 +0300

    added class Random instead of new Random changed variable random to randomNumber

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int random = new Random().nextInt(Colors.values().length);
-        return String.valueOf(Colors.values()[random]);
+        Random random = new Random();
+        int randomNumber = random.nextInt(Colors.values().length);
+        return String.valueOf(Colors.values()[randomNumber]);
     }
 }
