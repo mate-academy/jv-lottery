4a924d475e79f4e1f442c489d54e59e5d2a5249c
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-14 10:45:43 +0300

    changed color, number access to private

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        int number = new Random().nextInt(101);
-        Ball randomBall = new Ball(new ColorSupplier().getRandomColor(), number);
+        int ranodmNumber = new Random().nextInt(101);
+        Ball randomBall = new Ball(new ColorSupplier().getRandomColor(), ranodmNumber);
         return  randomBall;
     }
 }
