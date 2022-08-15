5096b68873c493bfde5e79ea0056cbc3d989f03d
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-16 01:24:15 +0300

    + private static final

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final Random random = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(100));
         return ball;
     }
 }
+
