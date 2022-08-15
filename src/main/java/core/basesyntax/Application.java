044141d38a74d1fae93ad44aafbf62c1b9801292
Leon Shama <leon_88@ukr.net>
2022-08-15 21:53:37 +0300

    Requested changes done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Lottery firstBall = new Ball();
-        Lottery secondBall = new Ball();
-        Lottery thirdBall = new Ball();
-
-        Lottery[] lotteries = new Lottery[]{firstBall, secondBall, thirdBall};
-        for (Lottery lottery : lotteries) {
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
