730e0f048d21e60f4b3ae3ecf82c780891be144b
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-15 14:10:02 +0300

    added loop

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Random random = new Random();
-
     public String getRandomColor() {
+        Random random = new Random();
         int randomNumberForColor = random.nextInt(Colors.values().length);
         return Colors.values()[randomNumberForColor].toString();
     }
