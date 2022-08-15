1679131d842cf8549aafcc3aa410ed4684a06381
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-16 01:24:00 +0300

    + private static final

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
+
     public String getRandomColor() {
-        Random random = new Random();
         int randomNumber = random.nextInt(Colors.values().length);
         return String.valueOf(Colors.values()[randomNumber]);
     }
 }
+
+
