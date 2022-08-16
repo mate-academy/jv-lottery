ef813183804232a0bbb1fc73aaa37861c4e4dee5
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-16 10:06:11 +0300

    + private static final int BALL_NUMBER = 100;

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final Random random = new Random();
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private static final Random RANDOM = new Random();
+    private static final int BALL_NUMBER = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setColor(COLOR_SUPPLIER.getRandomColor());
+        ball.setNumber(RANDOM.nextInt(BALL_NUMBER));
         return ball;
     }
 }
