de255e4939076825db167176574c45418a88256b
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-08-16 10:04:50 +0300

    fixed final

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int numberOfBalls = 3;
-        for (int i = 0; i < numberOfBalls; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall().toString());
         }
     }
