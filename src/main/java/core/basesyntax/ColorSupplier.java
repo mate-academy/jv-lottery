151599df1e581493a8309af5bccd036fff53d2c2
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-16 09:10:44 +0300

    changed all

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-private static final Random random = new Random();
+    private static final Random random = new Random();
 
     public String getRandomColor() {
         int index = random.nextInt(Colors.values().length);
