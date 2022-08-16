d6ce318ca2f70644a8f2c9cbe0cc98fdd066eccd
Gulbanu Bibicheva <bibicheva@gmail.com>
2022-08-16 07:25:57 +0200

    jv-lottery task: deleted/added line, added constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,6 +7,7 @@ public class Lottery {
     private static final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+        int upperLimit = 100;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(upperLimit));
     }
 }
