a1b86e92ace780f7e60139bbec777f0984d539eb
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-08-16 09:09:00 +0300

    changed all

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int maxValue = 100;
+    private static final int MAX_VALUE = 100;
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(
-                new ColorSupplier().getRandomColor(), new Random().nextInt(maxValue));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_VALUE));
     }
 }
