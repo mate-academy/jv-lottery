fadb8c80d893c33999fde4fce958c6db30b2a24f
Mykhailo Levko <mykhailo.levko@gmail.com>
2022-08-16 21:16:04 -0400

    jv-lottery/problem solving

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
     public Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         return Color.values()[index];
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBER = 100;
+
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
         Color randomColor = new ColorSupplier().getRandomColor();
