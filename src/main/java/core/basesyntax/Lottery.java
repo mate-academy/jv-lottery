9abf06d501c78a919401a979f8939618f4367ab5
MAxim Sychinskiy <max.sychinskiy@gmail.com>
2022-08-17 19:49:09 +0300

    Added fixes to class Lottery. ColourSupplier and random are global variables now.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,12 +5,13 @@ import java.util.Random;
 public class Lottery {
     private static final int BALL_NUMBER = 100;
 
+    private Random random = new Random();
+    private ColorSupplier coloursSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball newRandomBall = new Ball();
-        ColorSupplier coloursSupplier = new ColorSupplier();
         Colors randomColor = coloursSupplier.getRandomColor();
         newRandomBall.setColor(randomColor.name());
-        Random random = new Random();
         newRandomBall.setNumber(random.nextInt(BALL_NUMBER));
         return newRandomBall;
     }
