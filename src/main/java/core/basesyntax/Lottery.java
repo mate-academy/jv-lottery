50bcfb1fabc2f7225db8a796caada372b6c94215
Sergei Matiko <adgodst@gmail.com>
2022-08-18 18:45:14 +0300

    Homework with edits made

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BOUND = 100;
-    private Random randomVariables = new Random();
-    private ColorSupplier colorVariables = new ColorSupplier();
+    private Random random = new Random();
+    private ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorVariables.getRandomColor(), randomVariables.nextInt(MAX_BOUND) + 1);
+        return new Ball(color.getRandomColor(), random.nextInt(MAX_BOUND) + 1);
     }
 }
