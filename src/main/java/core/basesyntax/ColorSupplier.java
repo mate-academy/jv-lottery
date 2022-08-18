dc147a8b2a048f3380435e955035957b4be12f6b
MAxim Sychinskiy <max.sychinskiy@gmail.com>
2022-08-18 19:34:15 +0300

    I must pay more attention to comments on Github.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random randomSupplier = new Random();
+    private Random random = new Random();
 
     public Colors getRandomColor() {
-        int colorBallIndex = randomSupplier.nextInt(Colors.values().length);
-        return Colors.values()[colorBallIndex];
+        int colorIndex = random.nextInt(Colors.values().length);
+        return Colors.values()[colorIndex];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BALL_NUMBER = 100;
+    private static final int MAX_BALL_NUMBER = 100;
 
     private Random random = new Random();
     private ColorSupplier coloursSupplier = new ColorSupplier();
@@ -12,7 +12,7 @@ public class Lottery {
         Ball newRandomBall = new Ball();
         Colors randomColor = coloursSupplier.getRandomColor();
         newRandomBall.setColor(randomColor.name());
-        newRandomBall.setNumber(random.nextInt(BALL_NUMBER));
+        newRandomBall.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return newRandomBall;
     }
 }
