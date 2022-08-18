4c5346998b645189d64ed6236a11bd1975afa6bf
AndriiP <andrey.pslk@gmail.com>
2022-08-18 22:57:01 +0300

    ColorSupplier: in getRandom method return enum string value changed from .toString() to .name(). Lottery: constant renamed from BALL_NUMBER_BOUND to MAX_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BALL_NUMBER_BOUND = 100;
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color ballColor = Color.valueOf(colorSupplier.getRandomColor());
-        int ballNumber = random.nextInt(BALL_NUMBER_BOUND);
+        int ballNumber = random.nextInt(MAX_NUMBER);
         return new Ball(ballColor, ballNumber);
     }
 }
