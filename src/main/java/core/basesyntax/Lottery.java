e5b4db5c8264c6a62a3a7a62c90a5e140a1b3fcc
sasha <radovietsoleksandr@gmail.com>
2022-08-19 00:30:25 +0300

    jv-lottery7

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,8 +6,8 @@ public class Lottery {
 
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
-        String as = supplier.getRandomColor();
+        String randomColor = supplier.getRandomColor();
         int index = new Random().nextInt(100);
-        return new Ball(as, index);
+        return new Ball(randomColor, index);
     }
 }
