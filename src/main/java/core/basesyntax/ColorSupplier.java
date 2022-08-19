62794540c22592f00417f69637ffd536f7e41346
mister.Sokor <102083412+GranulaGrace@users.noreply.github.com>
2022-08-19 09:08:34 -0500

    1- created a separate file for enum, 2- in class Application have created just one instance of Lottery and gave other name; 3- used default name() method of enum instead of valueOf() method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
     public static String getRandomColor() {
         int colorIndex = new Random().nextInt(Colors.values().length);
         Colors str = Colors.values()[colorIndex];
