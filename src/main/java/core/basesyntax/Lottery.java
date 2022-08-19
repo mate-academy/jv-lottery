2525c9db6f9ceca2bc1ae3492c09dd541ef483af
Mykhailo Levko <mykhailo.levko@gmail.com>
2022-08-18 20:39:37 -0400

    jv-lottery/problem-solving-2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int NUMBER = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        Color randomColor = new ColorSupplier().getRandomColor();
+        Color randomColor = colorSupplier.getRandomColor();
         randomBall.setColor(randomColor.name());
-        randomBall.setNumber(new Random().nextInt(NUMBER));
+        randomBall.setNumber(random.nextInt(MAX_RANDOM_NUMBER));
         return randomBall;
     }
 }
