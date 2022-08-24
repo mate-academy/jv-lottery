6d11a9008b426d2feb9eb7fa789dcad5c32fc449
Oleksandr Kolisnyk <kolisnykol@gmail.com>
2022-08-25 00:31:05 +0300

    solution after 2nd review
    In classes:
    ColorSupplier.java
    Lottery.java
    - removed static

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    // static is used, because the method getRandomColor() is called without binding
-    // to a specific instance of the class
-    public static Colors getRandomColor() {
-        Random random = new Random();
+    private static final Random random = new Random();
+
+    public Colors getRandomColor() {
         return Colors.values()[random.nextInt(Colors.values().length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int MAX_NUMBER = 100;
+    private static final int MAX_NUMBER = 100;
+    private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Random random = new Random();
-        //  + 1  is used, because nextInt(MAX_NUMBER) return number in range [0, MAX_NUMBER-1],
-        //                                                       but we need [1, MAX_NUMBER]
-        Ball randomBall = new Ball(random.nextInt(MAX_NUMBER) + 1, ColorSupplier.getRandomColor());
-        return randomBall;
+        return new Ball(random.nextInt(MAX_NUMBER) + 1, colorSupplier.getRandomColor());
     }
 }
