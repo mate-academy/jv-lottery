e25db5c35774c9833762504fb262c57816926766
Bohdan Pankiv <pankivbogdan96@gmail.com>
2022-08-25 14:10:16 +0300

    Fixed checkstyle

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -14,6 +14,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return this.color+" , "+this.number;
+        return this.color + " , " + this.number;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
         int number = random.nextInt(100);
