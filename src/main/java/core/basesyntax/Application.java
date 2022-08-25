5f1e0785bb9bd773c9722d303ff00419a9f58c99
Bohdan Pankiv <pankivbogdan96@gmail.com>
2022-08-25 14:05:14 +0300

    Create class Ball and Lottery. In Main print three Balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,15 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery ball1 = new Lottery();
+        Lottery ball2 = new Lottery();
+        Lottery ball3 = new Lottery();
+        Ball randomBall1 = ball1.getRandomBall();
+        Ball randomBall2 = ball2.getRandomBall();
+        Ball randomBall3 = ball3.getRandomBall();
+        System.out.println(randomBall1.toString());
+        System.out.println(randomBall2.toString());
+        System.out.println(randomBall3.toString());
+
     }
 }
---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+/**
+ *
+ */
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return this.color+" , "+this.number;
+    }
+}
