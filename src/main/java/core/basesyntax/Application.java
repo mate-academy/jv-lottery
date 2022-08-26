64781d952adc743468865f3173bdeb4323062ac3
sasha <radovietsoleksandr@gmail.com>
2022-08-27 00:14:09 +0300

    lotto4

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ public class Application {
     public static void main(String[] args) {
 
         Lottery lottery = new Lottery();
-
          System.out.println(lottery.getRandomBall());
          System.out.println(lottery.getRandomBall());
          System.out.println(lottery.getRandomBall());
