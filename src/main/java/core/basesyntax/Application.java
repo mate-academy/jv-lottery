ff82f6350217cd22632ba820649a31d3b28a0be2
sasha <radovietsoleksandr@gmail.com>
2022-08-27 00:18:54 +0300

    commit5

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
 
         Lottery lottery = new Lottery();
+
          System.out.println(lottery.getRandomBall());
          System.out.println(lottery.getRandomBall());
          System.out.println(lottery.getRandomBall());
