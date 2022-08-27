9c16a5ec9f6a557bf4e0bfcce4cba3a019e913e1
Nikita Kolesnikov <kit1datasvit@gmail.com>
2022-08-28 00:52:21 +0300

    complete task

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+public class Ball {
+
+    String color;
+    int number;
+
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Colors {
+
+    GREEN,
+    RED,
+    YELLOW,
+    BLACK,
+    BROWN,
+    GREY,
+    BLUE,
+    PINK,
+    WHITE
+
+}
