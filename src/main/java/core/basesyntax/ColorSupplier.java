3a6e8f5b89921fb91454ca7d14a2c3af1680619d
Vladyslav Boholii <vlad.boholii@gmail.com>
2022-09-06 18:01:53 +0300

    added Ball, Color, Lottery and ColorSupplier logic

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+
+    private static final Random random = new Random();
+
+    public static Color getRandomColor() {
+        int randomIndex = random.nextInt(Color.values().length);
+        return Color.values()[randomIndex];
     }
 }
