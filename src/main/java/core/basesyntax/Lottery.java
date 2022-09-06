3ace4a4325554315241ae9406ab85fe13de9abc8
Olexii Melnik <alexmelnikk@gmail.com>
2022-09-06 11:43:43 +0300

    errors have been fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery  {
-    private  Random random = new Random();
+public class Lottery {
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
