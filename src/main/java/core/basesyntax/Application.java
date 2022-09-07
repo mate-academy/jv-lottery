e19bc19daa9e2a78844095f50530cb683754ce0e
Yuliia Polishchuk <108663552+niagara420@users.noreply.github.com>
2022-09-07 16:55:46 +0300

    Update src/main/java/core/basesyntax/Application.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,14 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-
-        Lottery firstBall = new Lottery();
-        Lottery secondBall = new Lottery();
-        Lottery thirdBall = new Lottery();
-
-        Lottery[] threeBalls = {firstBall, secondBall, thirdBall};
-        for (Lottery eachBall : threeBalls) {
-            System.out.println(eachBall.getRandomBall());
+        Lottery lottery= new Lottery();
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
