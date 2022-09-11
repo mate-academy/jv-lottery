b2459c3d6ef37f532ce8c27044b8604443c92532
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-09-11 23:04:18 +0300

    jv-lottery solution after checkstyle

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,19 +1,38 @@
 package core.basesyntax;
 
 public class Ball {
-    public Colors color;
-    public int number;
+    private Colors color;
+    private int number;
 
     public Ball(Colors color, int number) {
         this.color = color;
         this.number = number;
     }
 
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
+        return "Ball{"
+                +
+                "color='" + color + '\''
+                +
+                ", number=" + number
+                +
                 '}';
     }
 }
