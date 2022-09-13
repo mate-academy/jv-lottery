441877b558fd92e8c1137b1055740a9b99c315d3
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-09-13 12:51:36 +0300

    changes with final variable

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    final int bound = 100;
+    private final int bound = 100;
     private Color color = new ColorSupplier().getRandomColor();
-    private int number = new Random().nextInt(bound);
+    private int number = new Random().nextInt(getBound());
+
+    public int getBound() {
+        return bound;
+    }
 
     public Ball getRandomBall() {
         Ball ball = new Ball(color, number);
