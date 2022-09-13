3948eef875ad617e5139f007cb2923ae99b29a20
Yurii Trokhymenko <uta1965_@ukr.net>
2022-09-13 09:16:11 +0200

    added fixes after review 3 - Lottery (private ColorSupplier)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     static final int maxRandom = 100;
     private Random random = new Random();
+    private ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ColorSupplier supplier = new ColorSupplier();
         Color color = supplier.getRandomColor();
         int number = random.nextInt(maxRandom);
         return new Ball(color.name(),number);
