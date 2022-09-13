ef53da1d8e8d0696b31bec68c4127acec1e5c983
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-09-13 12:41:48 +0300

    some changes added

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int bound = 100;
+    final int bound = 100;
     private Color color = new ColorSupplier().getRandomColor();
     private int number = new Random().nextInt(bound);
-
-    public int getBound() {
-        return bound;
-    }
 
     public Ball getRandomBall() {
         Ball ball = new Ball(color, number);
