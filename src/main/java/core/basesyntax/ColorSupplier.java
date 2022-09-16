70c673faed3fd5efd098d26281bcd760241f3c1d
maksim-untilov <maksimuntilov7@gmail.com>
2022-09-16 18:29:43 +0300

    create Ball class with the following fields: color and number and override toString() method create enum with different colors implement method getRandomColor() in class ColorSupplier. This method should return a random color, based on the enum values (use Random class for this) Then create a Lottery class with getRandomBall() method, which will return the ball. This method should create a ball with a random color and a random number (the maximum possible number should be 100) and return this ball from the method.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public Colors getRandomColor() {
+        Random random = new Random();
+        int colorIndex = random.nextInt(Colors.values().length);
+        return Colors.values()[colorIndex];
     }
 }
