30f3ef09f99343a5b035179752d5a1e98461f4ca
Dmytro <play.d.shaa@gmail.com>
2022-09-17 19:41:37 +0300

    solved a task, but also tabs is red(??)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public colors getRandomColor() {
+        int index = new Random().nextInt(colors.values().length);
+        Random randomColor = new Random();
+        return colors.values()[index];
     }
 }
