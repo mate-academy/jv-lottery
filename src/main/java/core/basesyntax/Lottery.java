1b66cfcf9a79cdad063f9a52b45b3714cb4b646a
Dmytro <play.d.shaa@gmail.com>
2022-09-18 12:59:22 +0300

    fixed maven bugs again

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random number = new Random();
+    private Random number = new Random();
     private ColorSupplier supplier = new ColorSupplier();
 
     public void setSupplier(ColorSupplier supplier) {
