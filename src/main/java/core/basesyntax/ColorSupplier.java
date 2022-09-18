122cfcfba569be199a9ba1329c560f51cc89b55d
Dmytro <play.d.shaa@gmail.com>
2022-09-18 13:47:56 +0300

    agree, 8 line is redundant but don't understand about global random.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     public Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
-        Random randomColor = new Random();
         return Color.values()[index];
     }
 }
