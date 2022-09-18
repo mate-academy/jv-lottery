a7e3c1330c98fe01d83643a46943b50ff166acd6
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-18 18:45:20 +0200

    Second version

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class Ball {
-    int number;
-    String color;
+    private int number;
+    private String color;
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "number=" + number +
-                ", color='" + color + '\'' +
-                '}';
+        return "Ball{"
+                +  "number=" + number +
+                ", color='" + color
+                + '\''  +  '}';
     }
 
 }
