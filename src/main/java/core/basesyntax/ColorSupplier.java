204e0cd738a858c3422b0d62077fe6a5e5540198
Dmytro <play.d.shaa@gmail.com>
2022-09-18 14:09:40 +0300

    global random.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
+
     public Color getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index];
     }
 }
-
