705d77ffaf45c2f2ef0ccd5babc9c12d9f50f71f
Dmytro <play.d.shaa@gmail.com>
2022-09-18 13:26:00 +0300

    fixed maven

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final Random random = new Random();
     private ColorSupplier supplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(supplier.getRandomColor());
