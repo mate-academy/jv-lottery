fc3e35e89bf8db7da6e6b4c5fb21b1ffd7d1f2c6
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-19 10:42:21 +0200

    Fifth version

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,10 +6,10 @@ public class Application {
     public static void main(String[] args) {
         ColorSupplier supplier = new ColorSupplier();
         for (int i = 0; i < 3; i++) {
-            System.out.println(supplier.getRandomColor());
+            //System.out.println(supplier.getRandomColor());
             Random random = new Random();
             int value = random.nextInt(7);
-            System.out.println(" " + value);
+            System.out.println(supplier.getRandomColor() + " " + value);
         }
     }
 }
