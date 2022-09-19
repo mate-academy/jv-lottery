70e7c4432418d58dbb6d23b4ce11bf364b5c72a6
SomniumDZ <mshveda28@gmail.com>
2022-09-19 16:55:49 +0300

    Fix using toString() to get String representation of enum

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,6 +6,6 @@ public class ColorSupplier {
     public String getRandomColor() {
         Random random = new Random();
         int colorIndex = random.nextInt(Color.values().length);
-        return Color.values()[colorIndex].toString().toLowerCase();
+        return Color.values()[colorIndex].name();
     }
 }
