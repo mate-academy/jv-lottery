8bc67c58c3cfa466d333ba742995fe0b4a7290dd
SomniumDZ <mshveda28@gmail.com>
2022-09-19 16:49:26 +0300

    Add toSting() in Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -24,4 +24,9 @@ public class Ball {
     public void setNumber(int number) {
         this.number = number;
     }
+
+    @Override
+    public String toString() {
+        return "Ball #" + number + "; Color: " + color;
+    }
 }
