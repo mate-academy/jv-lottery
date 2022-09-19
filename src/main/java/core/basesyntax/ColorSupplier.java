78d294a459000a81f19fa0e1fc97205c7b777725
SomniumDZ <mshveda28@gmail.com>
2022-09-19 16:45:11 +0300

    Implement getRandomColor() method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        int colorIndex = random.nextInt(Color.values().length);
+        return Color.values()[colorIndex].toString().toLowerCase();
     }
 }
