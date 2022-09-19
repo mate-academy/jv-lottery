7cb86c59af182582b566968af3ebd61f95794ebf
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-19 14:14:14 +0200

    Seventh version

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final Random random = new Random();
+
     public Colors getRandomColor() {
         int index = random.nextInt(Colors.values().length);
         return Colors.values()[index];
