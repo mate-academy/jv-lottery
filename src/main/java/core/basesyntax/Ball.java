73ee8eceaed3a5970f6bffe34fd9981b7d92be00
Roman Mariushkin <ramzzzesik99@gmail.com>
2022-09-20 16:27:02 +0300

    creat lottery with random ball.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,13 +4,13 @@ public class Ball {
     private Color color;
     private int number;
 
-    @Override
-    public String toString() {
-        return "color = " + color + ", number = " + number;
-    }
-
     public Ball(Color color, int number) {
         this.color = color;
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "color = " + color + ", number = " + number;
     }
 }
