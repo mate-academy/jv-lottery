7b1fe5d204e9f0269f9e604b8c9dd348ec059198
Mykhailo Dolia <mndolia@gmail.com>
2022-09-20 08:28:45 +0100

    Edited: CheckStyle in class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-   Lottery getRandomBall() {
-      return new Ball(new ColorSupplier().getRandomColor().name(), new Random().nextInt(100));
-   }
+    Lottery getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor().name(), new Random().nextInt(100));
+    }
 }
