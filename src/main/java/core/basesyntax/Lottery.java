1749ad9e89116412ebda41c8662c35be9b7e92fb
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-20 17:33:54 +0200

    Added constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final Random random = new Random();
     private static final ColorSupplier colorSupplier = new ColorSupplier();
+    public static final int RANDOM_CONSTANT = 100;
 
     public Ball getRandomBall() {
-        final int value = random.nextInt(100);
+        int value = random.nextInt(RANDOM_CONSTANT);
         return new Ball(colorSupplier.getRandomColor(), value);
     }
 }
