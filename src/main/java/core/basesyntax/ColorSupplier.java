a6596c8500f52e008e3928e5437e9652c0fc6ff3
Oleksandr Klymenko <klim777angel@gmail.com>
2022-09-21 21:48:38 +0300

    Deleted few empty lines.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-
     public String getRandomColor() {
         int index = random.nextInt(Colors.values().length);
         return Colors.values()[index].name();
