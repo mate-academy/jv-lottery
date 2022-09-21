12f890644a406824f23356477f3218ea0335f10c
Oleksandr Klymenko <klim777angel@gmail.com>
2022-09-21 20:49:20 +0300

    Added private to Ball and ColorSupplier and added empty line to main.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Ball {
-    String color;
-    int number;
+    private String color;
+    private int number;
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
+
     @Override
     public String toString() {
         return "Ball number " + number + " and color is " + color;
