8dceed93276eef0b4c86a86b73d060c46b98d7bc
o-shyshkan <o.shyshkan@gmail.com>
2022-09-21 23:30:36 +0300

    Fixed constructor as the first method

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,14 +4,14 @@ public class Ball {
     private SetOfColors color;
     private int number;
 
-    @Override
-    public String toString() {
-        return "Ball " + this.color+ " number " + this.number;
-    }
-
-    Ball(SetOfColors color, int number){
+    Ball(SetOfColors color, int number) {
         this.color = color;
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball " + this.color +  " number " + this.number;
     }
 
 }
