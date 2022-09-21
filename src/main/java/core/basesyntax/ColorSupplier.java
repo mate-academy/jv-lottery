8db4ee8515888b0f97c0a0b2a43a6593f5fdbb00
Oleksandr Klymenko <klim777angel@gmail.com>
2022-09-21 21:07:15 +0300

    Added few new empty lines.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
 
     private final Random random = new Random();
+    
     public String getRandomColor() {
         int index = random.nextInt(Colors.values().length);
         return Colors.values()[index].name();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    
+
     private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier supplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(supplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
