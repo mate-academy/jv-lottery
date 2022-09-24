51c9e73f2839e410f8d6b92aee4b4bfccac87d74
Viktoriia Maherko <viktoriiamaherko@gmail.com>
2022-09-24 14:59:10 +0300

    Fixed some problems

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Lottery lottery = new Lottery();
             System.out.println(lottery.getRandomBall());
         }
     }
