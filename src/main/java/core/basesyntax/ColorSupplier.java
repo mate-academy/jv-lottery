a5e61755f03fde445f47c738ba54553a6c62ddcf
GrishaSeniv <seniv.grisha@gmail.com>
2022-09-26 20:50:43 +0300

    added test in main method, and added ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        int randomColorNumber = random.nextInt(ColorsEnum.values().length);
+        return ColorsEnum.values()[randomColorNumber].toString();
+    }
+
+    public Random getRandom() {
+        return random;
     }
 }
