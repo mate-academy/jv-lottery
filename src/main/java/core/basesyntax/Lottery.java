4a8d75f0a3310e717299ab7aec10c3ccc315fa38
Mykhailo Dolia <mndolia@gmail.com>
2022-09-26 13:32:33 +0100

    add fixes after review. add static variable & checkstyle constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-    private static final int MAX_NUMBER = 100;
+    private final static int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
