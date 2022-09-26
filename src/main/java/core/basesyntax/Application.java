75ee9867762632601c80e247f3264e9dac625957
Bohdan Vyshnevskyy <yksvenhsiv@gmail.com>
2022-09-26 11:59:12 +0300

    added magic numbers and improved static methods

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int MAGIC_NUMBER = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < MAGIC_NUMBER; i++) {
             System.out.println(Lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
+import static core.basesyntax.ColorSupplier.getRandomColor;
+
 import java.util.Random;
 
 public class Lottery {
     public static Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(ColorSupplier.getRandomColor());
+        ball.setColor(getRandomColor());
         ball.setNumber(new Random().nextInt(100));
         return ball;
     }
