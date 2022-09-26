d69d0f194801507458d79ba2b408df20f8139e06
galenkoigor <galenkoigor@gmail.com>
2022-09-26 13:03:13 +0200

    refactored code with mentor recommendation

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,9 @@ public class Application {
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
         for (int i = 0; i < BALLS_NUMBER; i++) {
-            System.out.println(new Lottery().getRandomBall());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER + 1));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER + 1));
     }
 }
