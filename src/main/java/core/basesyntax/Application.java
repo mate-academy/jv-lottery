37b229a5efd8c561900b0fa568a58063981574cb
shmax <mshveda28@gmail.com>
2022-09-26 12:45:34 +0300

    Add constant BALL_AMOUNT; replace magic number

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int BALL_AMOUNT = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < 3; i++) {
+        Ball[] balls = new Ball[BALL_AMOUNT];
+        for (int i = 0; i < BALL_AMOUNT; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
