cce16d21eab7b5c9ead866b10d1712331ec442a6
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-26 10:00:09 +0300

    Lottery v3 common mistakes and constants remarks fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int BALLS_COUNT = 3;
+
     public static void main(String[] args) {
         for (int i = 0; i < BALLS_COUNT; i++) {
             System.out.println(Lottery.getRandomBall());
