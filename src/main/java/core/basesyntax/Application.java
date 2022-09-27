bb435afb9006af37ad74eb010a1a5e3fb5033b8f
Pavlo Gook <pavlogook@gmail.com>
2022-09-27 19:23:04 +0300

    Added Ball, Color and Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+
+        for (Ball ball : balls) {
+            ball = lottery.getRandomBall();
+            System.out.println(ball.toString());
+        }
     }
 }
