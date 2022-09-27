3fffc47e7a996c7bfb6881de015173baa5420444
Luka Lozovyk <lukalozovyk@gmail.com>
2022-09-27 23:58:19 +0300

    Classes Ball, Lottery and enum Colors created. Methods getRandomColor() and getRandomBall() implemented.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(Colors.values().length);
+        return Colors.values()[index].toString().toLowerCase(Locale.ROOT);
     }
 }
