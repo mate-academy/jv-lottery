fcf30dc828705515f0a98bb5a85e4bb321d0c624
Roman Novosad <roman.novosad@yahoo.com>
2022-09-27 16:07:01 +0200

    Variable random changed to global with access modifier private

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].name();
+        return Colors.values()[random.nextInt(Colors.values().length)].name();
     }
 }
