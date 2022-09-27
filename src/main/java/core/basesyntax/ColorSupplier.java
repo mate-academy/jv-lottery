790bc47e6b1ae2ae304895efe9c25072ab1aee90
Bodddd <skrypnikn@gmail.com>
2022-09-27 13:49:31 +0300

    create a simple lottery that will return balls

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
+    public String getRandomColor() {
         return Colors.values()[new Random().nextInt(Colors.values().length)].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,6 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(),new Random().nextInt(100));
+        return new Ball(new ColorSupplier().getRandomColor(),new Random().nextInt(100));
     }
 }
