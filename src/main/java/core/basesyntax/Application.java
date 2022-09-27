38387fdb1a49cd0e425a9eed55f9be0d99966c36
drKacher <kacherkpi@gmail.com>
2022-09-27 22:24:37 +0200

    test and  work property :) v2 after code check

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
+import static java.lang.System.out;
+
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
             Ball randomBall = lottery.getRandomBall();
-            System.out.println(randomBall.toString());
+            out.println(randomBall.toString());
         }
     }
 }
