5a9f5930c668e6fe407b4c760a9dd1709e6f9a79
Bohdan Pankiv <pankivbogdan96@gmail.com>
2022-09-27 14:47:22 +0300

    all magic number in my code is contain

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,15 +1,16 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUM = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball [] randomBall = {new Lottery().getRandomBall(),
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall()};
+        Lottery lottery = new Lottery();
+        Ball[] randomBall = new Ball[NUM];
 
         for (Ball ball : randomBall) {
+            ball = lottery.getRandomBall();
             System.out.println(ball.toString());
         }
-
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int COUNT_NUMBER = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
-        int number = random.nextInt(100);
+        int number = random.nextInt(COUNT_NUMBER + 1);
         return new Ball(colorSupplier.getRandomColor(),number);
     }
 }
