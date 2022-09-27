8131f925b513efb664c742797b9461a627f2f499
NSay0 <krvipa@gmail.com>
2022-09-27 17:39:59 +0300

    Added classes: Ball, Color, Lottery. Implemented methods: getRandomColor() in ColorSupplier, getRandomBall() in Lottery. Added testing loops in psvm

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALL_AMOUNT = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[BALL_AMOUNT];
+        for (int ball = 0; ball < BALL_AMOUNT; ball++) {
+            balls[ball] = lottery.getRandomBall();
+        }
+        for (Ball ball : balls) {
+            System.out.println(ball.toString());
+        }
     }
 }
