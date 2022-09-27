516cda30964769c1b28f0cc83f604114d22797d9
Roman Novosad <roman.novosad@yahoo.com>
2022-09-27 15:21:09 +0200

    after mvn check amended class Ball namely set access modifier private for variables number and color and added getters and setters

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,10 +6,11 @@ public class Lottery {
     private static final int BOUND = 100;
     private Random randomNumber = new Random();
     private ColorSupplier randomColor = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        randomBall.number = randomNumber.nextInt(BOUND);
-        randomBall.color = randomColor.getRandomColor();
+        randomBall.setNumber(randomNumber.nextInt(BOUND));
+        randomBall.setColor(randomColor.getRandomColor());
         return randomBall;
     }
 }
