0a63dbc5e3025b7cd64ca6cfa71ba8668da71e8c
Bodddd <skrypnikn@gmail.com>
2022-09-27 13:37:37 +0300

    create a simple lottery that will return balls

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball {color = " + color + ", number = " + number + '}';
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLUE,
+    GREEN,
+    YELLOW,
+    ORANGE,
+    RED,
+    WHITE,
+    VIOLET,
+    BROWN,
+    AQUA,
+    BLACK,
+    CYAN,
+    PURPLE
+}
