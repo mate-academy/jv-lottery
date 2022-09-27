af9ee760567ee98e7da84b9492c5fd4d47984e9f
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-09-27 21:08:16 +0300

    Lottery service is ready

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        int index = random.nextInt(Color.values().length);
+        Color color = Color.values()[index];
+        return color.toString();
     }
 }
