4d209afb367ba9bf75ffe255eda87f24a9652e3d
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-09-28 13:37:05 +0300

    Implemented method getRandomBall v2.0

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int FIGURE_COUNT = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random randomNumber = new Random();
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor().toString(),
-                new Random().nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), randomNumber.nextInt(FIGURE_COUNT));
     }
 }
