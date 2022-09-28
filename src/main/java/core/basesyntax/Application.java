288b705688d2f29610c0dcd3d3b1353c4216c4ff
apc10bbe <poka4ailo.oleg@gmail.com>
2022-09-28 19:15:58 +0300

    added magic number as constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int COUNT = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < COUNT; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball.toString());
         }
