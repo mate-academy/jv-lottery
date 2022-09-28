c581539a9360e4b27d0236ecea999d9cd966a39a
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-09-28 15:38:22 +0300

    Implemented method getRandomBall v3.0

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final int index = new Random().nextInt(Colors.values().length);
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        return Colors.values()[index].name();
+        return Colors.values()[random.nextInt(Colors.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int FIGURE_COUNT = 100;
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Random randomNumber = new Random();
+    private static final int BALL_NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random randomNumber = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), randomNumber.nextInt(FIGURE_COUNT));
+        return new Ball(colorSupplier.getRandomColor(), randomNumber.nextInt(BALL_NUMBER));
     }
 }
