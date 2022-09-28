b8c93ca7f293df7f9a056b010458bdfc19b4756d
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-09-28 12:38:22 +0300

    added fixes after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,18 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-        Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-        System.out.println(ball1.toString());
-        System.out.println(ball2.toString());
-        System.out.println(ball3.toString());
+        Random random = new Random();
+        ColorSupplier supplier = new ColorSupplier();
+        Lottery lottery = new Lottery(random, supplier);
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+        for (int i = 0; i < balls.length; i++) {
+            System.out.println(balls[i].toString());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+    private final Random random;
+    private final ColorSupplier supplier;
+
+    public Lottery(Random random, ColorSupplier supplier) {
+        this.random = random;
+        this.supplier = supplier;
+    }
     public Ball getRandomBall() {
-        ColorSupplier supplier = new ColorSupplier();
-        Random random = new Random();
-        Ball ball = new Ball(supplier.getRandomColor(), random.nextInt(100));
-        return ball;
+        return new Ball(supplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
     }
 }
