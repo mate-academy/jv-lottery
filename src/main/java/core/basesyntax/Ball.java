c54e89d942d0866b29884e5f1fb51979c504697c
AlinaOryshak <alinaoryshak@gmail.com>
2022-09-28 20:52:41 +0300

    creating 3 balls and printing info about them

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,35 @@
+package core.basesyntax;
+
+public class Ball {
+    private Color color;
+    private int number;
+
+    public Ball() {
+    }
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball number " + this.getNumber() + " has " + this.getColor() + " color.";
+    }
+}
---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum Color {
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    rose,
+    black,
+    white
+}
