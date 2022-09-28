9705e5aecf4f6a72b7ea4863faa526e55202e29e
Matolch <ukrrviakiev@gmail.com>
2022-09-28 15:58:37 +0300

    Add new clases and metod to the program

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static Random random;
+
+    public ColorSupplier(Random random) {
+        ColorSupplier.random = random;
+    }
+
     public String getRandomColor() {
-        return null;
+        int indexEnum = random.nextInt(Colors.values().length);
+        return (Colors.values()[indexEnum]).name();
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    WHITE,
+    GREEN,
+    RED,
+    BLUE,
+    YELLOW
+}
