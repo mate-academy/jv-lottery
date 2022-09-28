650337a532e24b9568eccbac458aff76eaf15161
Tymur Prokhorov <tymurprokhorov@gmail.com>
2022-09-28 22:17:15 +0300

    Enum and classes created, methods created, Random practice complete. Task complete.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final int COLOR_COUNT = 6;
+    private Random random = new Random();
+
+    public ColorSupplier(Random random) {
+        this.random = random;
+    }
+
     public String getRandomColor() {
-        return null;
+        int colorNumber = new Random().nextInt(DifferentColors.values().length);
+        return (DifferentColors.values()[colorNumber]).name();
     }
 }
---
+++ src/main/java/core/basesyntax/DifferentColors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum DifferentColors {
+    RED,
+    GREEN,
+    BLUE,
+    BLACK,
+    BROWN,
+    WHITE
+}
