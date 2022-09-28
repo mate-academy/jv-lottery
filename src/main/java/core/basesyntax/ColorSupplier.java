76978ae12262d94e37ce0efdc2878201b2e02ac4
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-09-28 18:07:41 +0300

    Fixed random inside ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random random = new Random();
+
     public ColorsForBall getRandomColor() {
-        int colorForBallRandom = new Random().nextInt(ColorsForBall.values().length);
+        int colorForBallRandom = random.nextInt(ColorsForBall.values().length);
         return ColorsForBall.values()[colorForBallRandom];
     }
 }
