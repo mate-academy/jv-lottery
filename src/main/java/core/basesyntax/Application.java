1dc3bb0dd429bde04db71203b870fd1369e548bc
Bohdan Senyk <senyk2001@gmail.com>
2022-09-28 11:56:11 +0300

    1. changed all magic numbers to constants 2.implemented String  representation of enum constants with name() method 3.implemented a constructor for a Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 3;
+
     public static void main(String[] args) {
-        Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Random random = new Random();
+        Lottery lottery = new Lottery(random,colorSupplier);
+        for (int i = MIN_RANGE; i < MAX_RANGE; i++) {
             System.out.println(lottery.getRandomBall().toString());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,19 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_NUMBER_MAX = 100;
+    private final Random random;
+    private final ColorSupplier colorSupplier;
+
+    public Lottery(Random random, ColorSupplier colorSupplier) {
+        this.random = random;
+        this.colorSupplier = colorSupplier;
+    }
+
     public Ball getRandomBall() {
-        int index = new Random().nextInt(Color.values().length);
         Ball ball = new Ball();
-        ball.setColor(String.valueOf(Color.values()[index]));
-        ball.setNumber(new Random().nextInt(100));
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(BALL_NUMBER_MAX));
         return ball;
     }
 }
