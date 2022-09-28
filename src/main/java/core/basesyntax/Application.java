bd0733abd9415819a571cc22c0a5d730ac88bd2b
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-09-28 15:33:29 +0300

    added fixes after 2nd review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,14 +4,13 @@ import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
+        final int SIZE = 3;
         Random random = new Random();
         ColorSupplier supplier = new ColorSupplier();
         Lottery lottery = new Lottery(random, supplier);
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < balls.length; i++) {
+        Ball[] balls = new Ball[SIZE];
+        for (int i = 0; i < SIZE; i++) {
             balls[i] = lottery.getRandomBall();
-        }
-        for (int i = 0; i < balls.length; i++) {
             System.out.println(balls[i].toString());
         }
     }
