eed5b14252b8f07f70754f09309dbe603021e6d5
Andrii Matveev <matveev.tesla@gmail.com>
2022-09-28 18:35:17 +0300

    lottery has been done

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall(){
+        ColorSupplier supplier = new ColorSupplier();
+        int index = new Random().nextInt(100);
+        Ball ball = new Ball();
+        ball.setNumber(index);
+        ball.setColor(supplier.getRandomColor());
+        return ball;
+    }
+}
