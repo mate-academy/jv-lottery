1e057bd42c174c2b8385f959e71c549fd22e3d47
Bohdan Pankiv <pankivbogdan96@gmail.com>
2022-09-28 11:39:15 +0300

    add field NUMBER_OF_BALL

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int NUM = 3;
+    private static final int NUMBER_OF_BALL = 3;
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] randomBall = new Ball[NUM];
+        Ball[] randomBall = new Ball[NUMBER_OF_BALL];
 
         for (Ball ball : randomBall) {
             ball = lottery.getRandomBall();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int COUNT_NUMBER = 100;
+    private static final int MAX_POSITIVE_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int number = random.nextInt(COUNT_NUMBER + 1);
+        int number = random.nextInt(MAX_POSITIVE_NUMBER);
         return new Ball(colorSupplier.getRandomColor(),number);
     }
 }
