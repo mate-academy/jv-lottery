093b147a8a60f380f933ff34d3b57d4c9fd019b7
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-09-28 15:29:34 +0300

    redundant empty lines, objects creation and local variables issues are fixed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        Random random = new Random();
-        int index = random.nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color.name();
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
