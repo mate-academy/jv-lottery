c3ce123564be69ad0f073b378ad338a236a8822a
Anton Pushkar <112934645+anton-pushkar@users.noreply.github.com>
2022-09-28 16:24:53 +0300

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random random = new Random();
-    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int FIGURE_COUNT = 100;
+    private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int randomColor = random.nextInt(100);
+        int randomColor = random.nextInt(FIGURE_COUNT);
         return new Ball(color, randomColor);
     }
 }
