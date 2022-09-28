ccba1ce26af4ddc4053ee618eb81996e4a732e88
Pavlo.Polovyi <pasha32662@gmail.com>
2022-09-28 11:09:36 +0300

    Fixed everything according to remarks. Also, fixed problems with JavaCI checks

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int numberOfObjectsToCreate = 3;
+
     public static void main(String[] args) {
-        int numberOfObjectsToCreate = 3;
         Lottery lottery = new Lottery();
         for (int i = 0; i < numberOfObjectsToCreate; i++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private final ColorSupplier supplier = new ColorSupplier();
     private final Random randomizer = new Random();
-    public static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
-        return new Ball(randomizer.nextInt(MAX_NUMBER + 1), supplier.getRandomColor(randomizer));
+        int randNum = randomizer.nextInt(MAX_NUMBER + 1);
+        Color color = supplier.getRandomColor(randomizer);
+        return new Ball(randNum, color);
     }
 }
