18ec1d6ea34b1dbdf3569463e52f787258af1940
Artur Markoboh <arthur4655@gmail.com>
2022-09-28 11:17:04 +0300

    Added constants NUMBERS_LIMIT and COLORS_COUNT

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final int COLORS_COUNT = ColorList.values().length;
+
     public String getRandomColor() {
-        int index = new Random().nextInt(ColorList.values().length);
+        int index = new Random().nextInt(COLORS_COUNT);
         return ColorList.values()[index].name().toLowerCase();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBERS_LIMIT = 100;
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(NUMBERS_LIMIT));
     }
 }
