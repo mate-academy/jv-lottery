c63bbd04adae9a7c8a2ef4e4e8e220275b5c98fe
Serhii Konst <serhiikonstantynov.sk@gmail.com>
2022-09-29 00:08:28 +0300

    Bibiz9na's-solution-to-jv-lottery

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,31 @@
+package core.basesyntax;
+
+public class Ball {
+    private Colors color;
+    private int number;
+
+    public Ball(Colors color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String toString() {
+        return "The ball number is " + getNumber() + " and color is " + getColor();
+    }
+}
