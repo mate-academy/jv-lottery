9a6e63d12cda32249a0568389b7f9147192e56ea
Oleksandr Drobot <drob4@ukr.net>
2022-09-28 23:33:09 +0300

    made class variable "index" in class ColorSupplier and "supplier" in class Lottery, added constant MAX_NUMBER, in class ColorSupplier toString()-> name()

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int MaxNumber = 100;
+    private final int Max_Number = 100;
     private final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(supplier.getRandomColor(), new Random().nextInt(MaxNumber));
+        return new Ball(supplier.getRandomColor(), new Random().nextInt(Max_Number));
     }
 }
