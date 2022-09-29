8d5c3b1edcc297dd5ac8bf2244655245e3749b41
MuzykaVP <muzykavp.work@gmail.com>
2022-09-29 13:30:22 +0300

    Enum name changed to singular

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-    private final Colors[] colors = Colors.values();
+    private final Color[] colors = Color.values();
 
     public String getRandomColor() {
         return colors[random.nextInt(colors.length)].name();
