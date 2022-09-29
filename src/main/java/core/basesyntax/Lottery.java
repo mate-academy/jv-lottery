35ebcbfef3648046c263c3255e425c3b5a1d728f
Oleksii Bilko <lucheus@gmail.com>
2022-09-29 12:50:34 +0300

    fixed common mistakes and style

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_COUNT = 100;
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        randomBall.setNumber(new Random().nextInt(100));
+        randomBall.setNumber(random.nextInt(BALL_COUNT));
         randomBall.setColor(new ColorSupplier().getRandomColor());
         return randomBall;
     }
