1b8e9e8f301c22ae67e87ebc2b9a4ab1e0fc3560
Bohdan Pankiv <pankivbogdan96@gmail.com>
2022-09-29 12:04:49 +0300

    deleted array in main class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,11 +6,8 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] randomBall = new Ball[NUMBER_OF_BALL];
-
-        for (Ball ball : randomBall) {
-            ball = lottery.getRandomBall();
-            System.out.println(ball);
+        for (int i = 0; i < NUMBER_OF_BALL; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
