fff3de1d2ca833cc09568f8cbcea95a37438a4eb
Oleksandr Drobot <drob4@ukr.net>
2022-09-29 19:16:37 +0300

    made class variable "random" in classes ColorSupplier and Lottery, added constant MAX_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int Max_Number = 100;
+    private static final int MAX_NUMBER = 100;
     private final ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(supplier.getRandomColor(), new Random().nextInt(Max_Number));
+        return new Ball(supplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
 }
