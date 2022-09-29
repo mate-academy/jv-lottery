27f8df4d42233328075570fb82597fe6cd321fce
Oleksii Bilko <lucheus@gmail.com>
2022-09-29 15:13:53 +0300

    fixed magic numbers

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static final int BALL_COUNT = 100;
     private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
         randomBall.setNumber(random.nextInt(BALL_COUNT));
-        randomBall.setColor(new ColorSupplier().getRandomColor());
+        randomBall.setColor(colorSupplier.getRandomColor());
         return randomBall;
     }
 }
