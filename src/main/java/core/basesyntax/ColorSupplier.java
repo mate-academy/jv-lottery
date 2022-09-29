ac0cde86e1e0b161cd50a01a413f23d3c30d999a
apc10bbe <poka4ailo.oleg@gmail.com>
2022-09-29 12:47:11 +0300

    initialize dependencies in fields and checked mvn clean package

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public String getRandomColor() {
         int randomColor = random.nextInt(Colors.values().length);
         return Colors.values()[randomColor].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Lottery extends ColorSupplier {
     //private final Random random = new Random();
     private final Ball ball = new Ball();
+
     public Ball getRandomBall() {
         int value = getRandom().nextInt(100);
+
         ball.setNumber(value);
         ball.setColor(getRandomColor());
         return ball;
