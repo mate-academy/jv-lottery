dd78a7cb626edbf300aa272d100f13d6a48d4015
B1b1z9na <114086390+B1b1z9na@users.noreply.github.com>
2022-09-29 15:52:46 +0300

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+private static final int FIGURE_COUNT = 3;
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < FIGURE_COUNT; i++) {
             Ball ball = new Lottery().getRandomBall();
             System.out.println(ball.toString());
         }
