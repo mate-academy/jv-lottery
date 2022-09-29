91df7a599b1055b3ab62796fd3d70e84829a67f9
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-09-29 17:38:40 +0300

    fix common mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
-        Lottery firstBall = new Lottery();
-        Lottery secondBall = new Lottery();
-        Lottery thirdBall = new Lottery();
-        System.out.println(firstBall.getRandomBall());
-        System.out.println(secondBall.getRandomBall());
-        System.out.println(thirdBall.getRandomBall());
+    public void main(String[] args) {
+        Lottery[] someBalls = new Lottery[3];
+        for (int i = 0; i < someBalls.length; i++) {
+            someBalls[i] = new Lottery();
+            System.out.println(someBalls[i].getRandomBall());
+        }
         // create three balls using class Lottery and print information about them in console
     }
 }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,15 +3,12 @@ package core.basesyntax;
 public class Ball {
     private String color;
     private int number;
-
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
-
     public Ball() {
     }
-
     public enum color {
         BLACK,
         BLUE,
@@ -19,7 +16,6 @@ public class Ball {
         WHITE,
         RED
     }
-
     @Override
     public String toString() {
         return "Ball"
