3ae5a7cc02131fcca2826dcca6f86bc7b75740c8
Andrew Miroshnikov <giggle2594@gmail.com>
2022-09-29 15:44:27 +0300

    Solve mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,8 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[NUMBER_OF_BALLS];
-        for (int i = 0; i < balls.length; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
