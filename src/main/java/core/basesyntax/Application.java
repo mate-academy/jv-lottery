d850d5b774d263d871da5d2e3b15bc638e6885b5
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-09-29 17:50:54 +0300

    add constant (numbers of balls)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
     public void main(String[] args) {
-        Lottery[] someBalls = new Lottery[3];
+        Lottery[] someBalls = new Lottery[NUMBER_OF_BALLS];
         for (int i = 0; i < someBalls.length; i++) {
             someBalls[i] = new Lottery();
             System.out.println(someBalls[i].getRandomBall());
