6fd93bd63dce921e1338c840514e5ac7da912535
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-09-29 17:17:29 +0300

    Initialized balls massive in the Loop, initialized Random and ColoSupplier object at the constructor, made variable for magic number, removed redudant line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-        Lottery lotter = new Lottery();
-        Ball[] balls = {lotter.getRandomBall(),lotter.getRandomBall(),lotter.getRandomBall()};
+        Lottery lottery = new Lottery(new Random(), new ColorSupplier());
+        Ball[] balls = new Ball[4];
+        for (int i = 0; i < 4; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
         for (Ball ball: balls) {
             System.out.println(ball);
         }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,19 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final Random random;
+    private final ColorSupplier supplier;
+
+    public Lottery(Random random, ColorSupplier supplier) {
+        this.random = random;
+        this.supplier = supplier;
+    }
+
     public Ball getRandomBall() {
+        int figureNumber = random.nextInt(101);
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
-        Ball ball = new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
+        Ball ball = new Ball(colorSupplier.getRandomColor(), figureNumber);
         return ball;
     }
 }
