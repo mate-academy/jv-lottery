edbb3a39318226b1afbc509ff5515b104d5932b2
Andrii Matveev <matveev.tesla@gmail.com>
2022-09-29 13:42:32 +0300

    made magic numbers constants

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_COUNT = 100;
     private ColorSupplier supplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
-        int index = random.nextInt(100);
+        int index = random.nextInt(BALL_COUNT);
         Ball ball = new Ball(supplier.getRandomColor(), index);
         return ball;
     }
