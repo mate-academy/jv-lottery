6330d6e14d5874ae79313b0cba170a58523e7605
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-09-29 17:48:19 +0300

    new mvn some edits after fix common mistaskes

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,19 +3,23 @@ package core.basesyntax;
 public class Ball {
     private String color;
     private int number;
+
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
+
     public Ball() {
     }
-    public enum color {
+
+    public enum Color {
         BLACK,
         BLUE,
         YELLOW,
         WHITE,
         RED
     }
+
     @Override
     public String toString() {
         return "Ball"
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
+
 import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int index = new Random().nextInt(Ball.color.values().length);
-        return String.valueOf(Ball.color.values()[index]);
+        int index = new Random().nextInt(Ball.Color.values().length);
+        return String.valueOf(Ball.Color.values()[index]);
     }
 }
