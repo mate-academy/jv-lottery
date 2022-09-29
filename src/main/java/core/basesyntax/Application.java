691eb974a997dbc1d802bec78be986b51af6c080
Serhii Konst <serhiikonstantynov.sk@gmail.com>
2022-09-29 21:20:42 +0300

    jv-oop-mistakes-fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 public class Application {
     private static final int FIGURE_COUNT = 3;
+    private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
         for (int i = 0; i < FIGURE_COUNT; i++) {
-            Ball ball = new Lottery().getRandomBall();
-            System.out.println(ball.toString());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
