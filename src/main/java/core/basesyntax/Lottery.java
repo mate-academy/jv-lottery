a00866feb536158c803acc2a303540cf2fee1e7f
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-09-29 13:03:43 +0300

    Fix maskakes 3

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall() {
-        Ball ball = new Ball();
+
+    private Ball ball;
+
+    public Lottery() {
+        ball = new Ball();
         ColorSupplier colorSupplier = new ColorSupplier();
 
         String randomColor = colorSupplier.getRandomColor();
@@ -12,6 +15,9 @@ public class Lottery {
 
         Random random = new Random();
         ball.setNumber(random.nextInt(100));
+    }
+
+    public Ball getRandomBall() {
         return ball;
     }
 }
