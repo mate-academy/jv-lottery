54d4995784ddfe8989bb4ae4024858d843253cd5
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-09-29 11:38:30 +0300

    Corrected generating random numbers in Lottery class. Removed toString() in getRandomBall().

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final int range = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int random = new Random().nextInt(100);
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random);
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(range));
     }
 }
