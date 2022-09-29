fdcebac63f3c11c5e8a52e9193761b8eb77b180c
yehbreus <yeh.breus@gmail.com>
2022-09-29 09:35:04 +0300

    fix problem when we create new instance of Random class for every getRandomBall method run

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_RANDOM_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), new Random()
+        return new Ball(colorSupplier.getRandomColor(), random
                 .nextInt(MAX_RANDOM_NUMBER + 1));
     }
 }
