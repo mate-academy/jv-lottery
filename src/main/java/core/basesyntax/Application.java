1d52c741c1476b4d4496603a16a678868bc04259
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-09-30 14:06:01 +0300

    Replaced magic number in loop and made constant LENGTH;

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Application {
+    private static final int LENGTH = 4;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery(new Random(), new ColorSupplier());
-        Ball[] balls = new Ball[4];
+        Ball[] balls = new Ball[LENGTH];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
