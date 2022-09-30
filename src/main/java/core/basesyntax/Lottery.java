1fc9e58aca0b2dafc3d3956ebf59bd4ad7419f5a
Roman Usyk <roman.usik.93@ukr.net>
2022-09-30 17:20:54 +0300

    variables numberOfBalls in the class Lottery  and numberOfGames in the class Application  were redefined as constants

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_OF_BALLS = 100;
     private final Random random;
     private final ColorSupplier supplier;
-    private static final int NUMBER_OF_BALLS = 100;
 
     public Lottery(Random random, ColorSupplier supplier) {
         this.random = random;
