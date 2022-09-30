8f4f27a3e9bceb13ef45b376d4e21d29096eb110
Roman Usyk <roman.usik.93@ukr.net>
2022-09-30 15:38:52 +0300

    variables numberOfBalls in the class Lottery  and numberOfGames in the class Application  were redefined as constants

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
-        int numberOfGames = 3;
+        final int numberOfGames = 3;
         Lottery lottery = new Lottery(new Random(), new ColorSupplier());
         for (int i = 0; i < numberOfGames; i++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private final Random random;
     private final ColorSupplier supplier;
+    private static final int numberOfBalls = 100;
 
     public Lottery(Random random, ColorSupplier supplier) {
         this.random = random;
@@ -13,7 +14,6 @@ public class Lottery {
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        int numberOfBalls = 100;
         int number = this.random.nextInt(numberOfBalls);
         ball.setNumber(number);
         ball.setColor(supplier.getRandomColor());
