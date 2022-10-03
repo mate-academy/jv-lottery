315b3ec9bdd40344edb2c156fa8959f1a8dac6a1
Ilya Samilyak <samilyakilya@gmail.com>
2022-10-04 00:37:51 +0300

    Fixed all after review2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-//        ColorSupplier supplier = new ColorSupplier();
+        ColorSupplier supplier = new ColorSupplier();
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random random = new Random();
     private static final int MAX_NUMBER = 100;
+    private final Random random = new Random();
     private final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
