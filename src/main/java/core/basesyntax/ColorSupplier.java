967788e4da2549cb2e927e4e8d1f01bd601a89af
Nazar Hnatyuk <stim1.one@gmail.com>
2022-10-05 15:27:20 +0300

    1. Went through the checklist under the task and correct all the points. 2. Did all the numbers constant.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    static final int COLOR_NUMBER = 3;
+    private final Random random = new Random();
+
     public Color getRandomColor() {
-        Random random = new Random();
-        int colorNumber = random.nextInt((2) + 1);
+        int colorNumber = random.nextInt(COLOR_NUMBER);
         if (colorNumber == 1) {
             return Color.BLACK;
         } else if (colorNumber == 2) {
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int COLOR_SUPPLIER = 100;
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
+
         Ball ball = new Ball();
         ColorSupplier cs = new ColorSupplier();
-        Random random = new Random();
-
-        ball.setBall(random.nextInt(99) + 1);
+        ball.setBall(random.nextInt(COLOR_SUPPLIER));
         ball.setColor(cs.getRandomColor());
         return ball;
     }
