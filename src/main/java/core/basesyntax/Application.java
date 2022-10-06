cf0c8c4bfa0f253c2ada672604e4f5dac38dec02
Katya Humeniuk <kapelka.boo@gmail.com>
2022-10-06 22:30:55 +0300

    Fixed constant names.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int lotteryCount = 3;
+    private static final int LOTTERY_COUNT = 3;
 
     public static void main(String[] args) {
-        Lottery[] lottery = new Lottery [lotteryCount];
+        Lottery[] lottery = new Lottery [LOTTERY_COUNT];
         for (int i = 0; i < lottery.length; i++) {
             lottery[i] = new Lottery();
             System.out.println(lottery[i].getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int magicInt = 100;
+    private static final int MAGIC_INT = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(magicInt));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAGIC_INT));
     }
 }
