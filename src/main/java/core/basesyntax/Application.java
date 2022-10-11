edf28c425998b5fa4d36b4db18c7eeab68204650
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-11 23:59:46 +0200

    refactored

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,8 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        lottery.getRandomBall();
-        lottery.getRandomBall();
-        lottery.getRandomBall();
+        System.out.println(lottery.getRandomBall());
+        System.out.println(lottery.getRandomBall());
+        System.out.println(lottery.getRandomBall());
     }
 }
