03d7c9053afa02b966b66e6e5d7cf9b38d6f4d2a
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-10-11 17:18:35 +0300

    fixed name of variable ColorSupplier, deleted empty string.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int BALL_NUMBER = 100;
-    private final ColorSupplier randomColor = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         int randomNumber = new Random().nextInt(BALL_NUMBER);
-        return new Ball(randomColor.getRandomColor(), randomNumber);
+        return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
 }
