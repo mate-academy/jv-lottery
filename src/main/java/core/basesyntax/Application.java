6081beff419d3e208f3c14eabc742288e1344447
Ivan Drebot <drebotsvan9@gmail.com>
2022-10-16 14:54:15 +0300

    added toString method for print info

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
-        final int Lottery_Balls = 3;
-        Lottery lottery = new Lottery();
+    private static final int LOTTERY_BALLS = 3;
 
-        for (int i = 0; i < Lottery_Balls; i++) {
-            System.out.println(lottery.getRandomBall());
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        Ball ball = lottery.getRandomBall();
+
+        for (int i = 0; i < LOTTERY_BALLS; i++) {
+            ball.toString();
         }
     }
 }
