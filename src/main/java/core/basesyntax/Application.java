7dce48c324166b27dec6102d7a5a6d1e1f354847
Nazar Hnatyuk <stim1.one@gmail.com>
2022-10-17 18:18:43 +0300

    Lottery homework fix 4)
    Changed BALL to BALL_NUMBER
    Removed empty line in Lottery method

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-
-    public static final int BALL = 3;
+    public static final int BALL_NUMBER = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < BALL; i++) {
+        for (int i = 0; i < BALL_NUMBER; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
