43d0f3bcfc16aedf8e328894df0f1d03b303bc7a
Andriy <526473+gb96@users.noreply.github.com>
2022-10-17 21:35:25 +0300

    7st commit

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random;
     public ColorSupplier() {
-        random =new Random();
+        random = new Random();
     }
+
     public String getRandomColor() {
         int i = random.nextInt(Color.values().length);
         return Color.values()[i].name();
