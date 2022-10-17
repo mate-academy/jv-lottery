503513e605daf0651e5dbc293e406c3b0c518b8e
Andriy <Andriy@email.com>
2022-10-17 19:00:46 +0300

    4rd Fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private Random rand;
-
+    private ColorSupplier colorSupplier;
     public Ball getRandomBall() {
-        return new Ball(rand.nextInt(100), new ColorSupplier().getRandomColor());
+        return new Ball(rand.nextInt(100), colorSupplier.getRandomColor());
     }
 }
