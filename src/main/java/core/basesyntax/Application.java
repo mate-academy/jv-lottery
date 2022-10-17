b142a971a3f46b65f142a8f0cec46d52d2218fdf
Andriy <89684976+SichniyAndriy@users.noreply.github.com>
2022-10-17 20:58:59 +0300

    Update src/main/java/core/basesyntax/Application.java
    Co-authored-by: Zakhar Chorny <90692308+zachar-chorny@users.noreply.github.com>

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Lottery lot = new Lottery();
+        Lottery lottery = new Lottery();
         Ball[] balls = new Ball[3];
         for (int i = 0; i < balls.length; ++i) {
             balls[i] = lot.getRandomBall();
