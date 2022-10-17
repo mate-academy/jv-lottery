c6c3dfabcdfaf3f7af006676fb0c0d698510a18a
Andriy <526473+gb96@users.noreply.github.com>
2022-10-17 22:14:07 +0300

    12th fix - Application class adding line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int AMOUNT = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
 
