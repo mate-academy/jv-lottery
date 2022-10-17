f185a2f5ce34b8393bc1e9e96598a31d59774c30
Nazar Hnatyuk <stim1.one@gmail.com>
2022-10-17 13:58:03 +0300

    Lottery homework fix 3.
    Fixed mistakes in GetRandomColor().

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,11 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-    private final Color[] color = Color.values();
 
     public Color getRandomColor() {
-
         int colorNumber = random.nextInt(Color.values().length);
-        return color[colorNumber];
+        return Color.valueOf(Color.values()[colorNumber].name());
     }
 }
