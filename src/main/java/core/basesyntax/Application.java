da6a7f8439a34953c5332f8a0d0136e9530304a8
Petro Korets <petrkorets@gmail.com>
2022-10-19 04:48:07 +0300

    Corrected whitespaces

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,7 +5,7 @@ public class Application {
         final int initialIterrator = 0;
         final int maxQuantityOfLotteries = 3;
         Lottery lottery = new Lottery();
-        for (int i = initialIterrator; i < maxQuantityOfLotteries ; i++) {
+        for (int i = initialIterrator; i < maxQuantityOfLotteries; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
