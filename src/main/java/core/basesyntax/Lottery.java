8bb21c98d92960e0605c6b9b449f2cf037d3bcfa
Petro Korets <petrkorets@gmail.com>
2022-10-20 01:24:50 +0300

    Changed static variable definition order in Lottery class.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_RANDOM_VALUE = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private static final int MAX_RANDOM_VALUE = 100;
 
     protected Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_RANDOM_VALUE));
