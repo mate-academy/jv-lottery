90db297087b42a730fd1a96f07cd5fdd1a350612
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-21 13:36:32 +0200

    refactored

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ public class Application {
     private static final int BALL_NUMBER = 3;
 
     public static void main(String[] args) {
-        
         Lottery lottery = new Lottery();
 
         for (int i = 0; i < BALL_NUMBER; i++) {
