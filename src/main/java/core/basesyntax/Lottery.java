4794aa4a4ab8568082e8a34367e7cd77760891d4
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-21 13:38:09 +0200

    refactored

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier supplier = new ColorSupplier();
     private final Random random = new Random();
-    private final int BOUND_NUMBER = 100;
 
     public Ball getRandomBall() {
+        int BOUND_NUMBER = 100;
         return new Ball(supplier.getRandomColor(), random.nextInt(BOUND_NUMBER));
     }
 }
