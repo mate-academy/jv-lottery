bc4443d1ec8b4c5d7cd0244f97863790eeec0a17
Dmytro Koval <dimonkoval@gmail.com>
2022-10-22 19:02:07 +0300

    Dmytro Koval about Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
+            Ball randomBall = lottery.getRandomBall();
+            System.out.println(randomBall.getColour() + " " + randomBall.getNumber());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Ball {
+    private String colour;
+    private int number;
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
