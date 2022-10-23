a5daedbe9c9bde05da0fd60ece80219207fa13cd
Dmytro Koval <dimonkoval@gmail.com>
2022-10-23 15:32:14 +0300

    correction after checklist

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        Random random = new Random();
+        int index = random.nextInt(Colors.values().length);
+        return Colors.values()[index].name();
     }
 }
