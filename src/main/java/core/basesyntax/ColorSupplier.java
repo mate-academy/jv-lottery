777f9a8a195269af8723cfdfa528ccbedb5d1ded
Alona Vyrodova <alenavirodova@gmail.com>
2022-10-24 20:53:21 +0300

    ENUM changes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,6 @@ package core.basesyntax;
 public class ColorSupplier {
     public static String getRandomColor() {
         int index = Lottery.RANDOM.nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        return Colors.values()[index].name();
     }
 }
