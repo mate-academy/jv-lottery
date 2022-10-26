0d595448e4d4cab056120097c712f0bf98f32b64
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-10-26 22:45:32 +0300

    Created three classes and one enum.

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        System.out.println(colorSupplier.getRandomColor());
+    }
+}
