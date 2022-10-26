d08804720d94622d9e4e2cae6ee0b6c970ca6f66
Yurii Storozhuk <yurii.storozhuk@gmail.com>
2022-10-26 09:37:26 +0300

    fix(checklist): avoid .toString() with .name() with enum

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Lottery {
     public Ball getRandomBall() {
-        String color = new ColorSupplier().getRandomColor().toString();
+        String color = new ColorSupplier().getRandomColor().name();
         int number = new NumberSupplier().getRandomNumber();
         return new Ball(color, number);
     }
