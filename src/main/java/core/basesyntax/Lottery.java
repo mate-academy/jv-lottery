e9df1773dcaffb37799ebb302dc1cfa89d95980b
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-10-27 16:41:15 +0300

    Made random class-level variable, added loop to create several objects in Application class, changed toString() method to name() method in ColorSupplier class.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BOUND = 100;
+    private Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(random.nextInt(BOUND));
         return ball;
     }
 }
