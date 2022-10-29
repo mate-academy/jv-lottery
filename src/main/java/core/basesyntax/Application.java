cb43d16ec2b7efc938edb36c31de17252b9a48da
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-29 13:26:40 +0200

    method separated

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     private static final int NUMBER_OF_BALLS = 3;
     private static Lottery lottery = new Lottery();
+
     public static void main(String[] args) {
         Ball[] balls = new Ball [NUMBER_OF_BALLS];
         for (int i = 0; i < balls.length; i++) {
