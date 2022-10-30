86b7ba2a2787efc6830bd027a0d93f8db98fd93c
Myhailo Mykhasov <mikhailmikhasev@gmail.com>
2022-10-31 00:03:17 +0200

    Removed empty lines. Made Random and ColorSupplier variables in Lottery class.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-
     public static void main(String[] args) {
         Ball ball = new Ball();
         for (int i = 0; i < 3; i++) {
             ball = new Lottery().getRandomBall();
             System.out.println(ball);
         }
-
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Ball getRandomBall() {
+    private Random random = new Random();
+    private ColorSupplier colSup = new ColorSupplier();
+
+    public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColour(new ColorSupplier().getRandomColor().toString());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setColour(colSup.getRandomColor().toString());
+        ball.setNumber(random.nextInt(100));
         return ball;
     }
-
 }
