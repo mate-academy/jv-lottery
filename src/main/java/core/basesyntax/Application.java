418d1d28744df80f172f80dc2de1e7b5468c1518
Iskamele <iskamele@gmail.com>
2022-10-30 15:03:58 +0200

    Added a constant with the number of balls. Added a loop that displays the result of the getRandomBall method to the console.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int THE_NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < THE_NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
