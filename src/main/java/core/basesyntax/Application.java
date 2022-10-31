4b0377a2c320784fbe3f3a14268e1f26c8ce539a
ArtemMakovskyy <photomak79@gmail.com>
2022-10-31 22:41:53 +0200

    Fixed mistakes. 1) In Applcation.main take out object 'Lottery' from loop. 2) In Lottery and ColorSupplier created variables out of method. 3) Created constant 'limit' in Lottery.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         int ballsQuantity = 3;
+        Lottery lottery = new Lottery();
         for (int i = 0; i < ballsQuantity; i++) {
-            System.out.println(new Lottery().getRandomBall());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final int limit = 100;
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(new Random().nextInt(100));
-        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(random.nextInt(limit));
+        ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
 }
