3e051bbe48f8947f5c18b41ab6fbd06bf70ed3a7
ArtemMakovskyy <photomak79@gmail.com>
2022-10-31 14:01:53 +0200

    Create Ball, ColorSupplier, Lottery classes and enum Colors. In main create output three random balls.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        int ballsQuantity = 3;
+        for (int i = 0; i < ballsQuantity; i++) {
+            System.out.println(new Lottery().getRandomBall());
+        }
     }
 }
