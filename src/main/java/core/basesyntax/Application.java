1297511d68918abd705423a15e0dd530ae6e889b
Sander <alex.kapusniak@gmail.com>
2022-11-01 15:25:39 +0200

    change creating instance in main

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 public class Application {
     private static final int MAX_VALUE = 3;
     private static Ball[] balls = new Ball[MAX_VALUE];
+    private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
         }
     }
 }
