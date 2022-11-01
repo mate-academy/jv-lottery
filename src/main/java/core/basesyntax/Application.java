f4f8e9ba99551a3beaef190873e93bfccfc933a6
Artem Otsabera <eskvair86@gmail.com>
2022-11-01 15:41:06 +0200

    changed Application, ColorSupplier and Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery firstBall = new Lottery();
-        Lottery secondBall = new Lottery();
-        Lottery thirdBall = new Lottery();
-
-        System.out.println(firstBall.getRandomBall());
-        System.out.println(secondBall.getRandomBall());
-        System.out.println(thirdBall.getRandomBall());
+        Lottery ball = new Lottery();
+        for (int i = 0; i < 3; i++) {
+            System.out.println(ball.getRandomBall());
+        }
     }
 }
-
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BallLimit = 100;
+    private static final int BALL_LIMIT = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball anyBall = new Ball();
         anyBall.setColor(colorSupplier.getRandomColor().name());
-        anyBall.setNumber(random.nextInt(BallLimit));
+        anyBall.setNumber(random.nextInt(BALL_LIMIT));
         return anyBall;
     }
 }
-
