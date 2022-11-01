b3356ff8f3e530ed4c535906a118673ef7ca72fc
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-01 19:10:57 +0200

    Classes Lottery,Ball,Application,Colors,ColorSupplier were created

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(Colors.values().length);
+        return Colors.values()[index].name();
     }
 }
+
