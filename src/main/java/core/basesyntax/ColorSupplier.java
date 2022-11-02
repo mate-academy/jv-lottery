78e5278856f0e9975531a10d40a85eb4fa20f2d5
Andrei_Bondarenko <bondorol007@gmail.com>
2022-11-02 13:25:33 +0200

    This is bug fix last version.But I don`t understand why we create "Ball" class. What does it do?

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -12,6 +12,7 @@ public class ColorSupplier {
     public String getRandomColor() {
         Color[] colors = Color.values();
         Random random = new Random();
+
         return randomColor = colors[random.nextInt(colors.length)].toString();
     }
 }
