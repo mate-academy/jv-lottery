2f41a6c1d3fb6ab3d39e43117f43ff256f79cf7f
Iskamele <iskamele@gmail.com>
2022-11-03 21:48:43 +0200

    Made Random class-level variable. Changed the getRandomBall() method so that it uses the received random values and creates an object using the constructor from the Ball class.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BALL_NUMBER = 100;
+    private static final int NUMBER_ON_BALL = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Ball ball = new Ball();
-        ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(BALL_NUMBER));
-        return ball;
+        int number = random.nextInt(NUMBER_ON_BALL);
+        String color = colorSupplier.getRandomColor();
+        return new Ball(color, number);
     }
 }
