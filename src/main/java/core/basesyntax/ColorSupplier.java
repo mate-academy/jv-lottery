3009cbf9dcdbe6ed38bb51d9fc27791a43e8d5e4
Andrei_Bondarenko <bondorol007@gmail.com>
2022-11-03 17:12:19 +0200

    jv-lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Color[] colors = Color.values();
+        Random random = new Random();
+        return colors[random.nextInt(colors.length)].toString();
     }
 }
