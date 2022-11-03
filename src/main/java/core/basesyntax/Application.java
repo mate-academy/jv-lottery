2eb37f73ecb8911fcf1a5eb841c7e42269833cd5
Igor Puhach <igorpuhach21@gmail.com>
2022-11-03 13:41:04 +0200

    fixed Application classes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for ( int i = 0;i < 3; i++){
+        for( int i = 0;i < 3; i++) {
             System.out.println(lottery.getrandomball());
         }
     }
