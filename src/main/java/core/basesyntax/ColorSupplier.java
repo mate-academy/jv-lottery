cd7d6118254cf5a744049e98037d8da8954028e7
Igor Puhach <igorpuhach21@gmail.com>
2022-11-03 21:13:28 +0200

    fixed bugs after review #2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random randomcolor = new Random();
+
     public String getRandomColor() {
         int index = randomcolor.nextInt(Colors.values().length);
         return String.valueOf(Colors.values()[index]);
