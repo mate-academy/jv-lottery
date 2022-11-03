a113d43e90d31ae58f5baa3c0db897510d420b81
Anton Kurylo <antonkurilo9@gmail.com>
2022-11-04 00:05:19 +0200

    added three ball

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Ball[] balls = new Ball[3];
+
+        for (int i = 0; i < 3; i++) {
+            balls[i] = Lottery.getRandomBall();
+        }
+
+        System.out.println(Arrays.toString(balls));
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static Color getRandomColor() {
+        Random random = new Random();
+        int index = random.nextInt(Color.values().length);
+
+        return Color.values()[index];
     }
 }
