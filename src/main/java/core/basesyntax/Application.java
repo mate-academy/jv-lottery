42e32fde1645e7736f9b8c08a8941632d7b53a31
Myhailo Mykhasov <mikhailmikhasev@gmail.com>
2022-11-03 21:20:16 +0200

    fixed creating redundant objects.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Ball ball = new Ball();
+        Lottery lot = new Lottery();
         for (int i = 0; i < 3; i++) {
-            ball = new Lottery().getRandomBall();
+            ball = lot.getRandomBall();
             System.out.println(ball);
         }
     }
