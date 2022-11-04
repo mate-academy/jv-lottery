706768d6148d64fa917752e001fa15469007622a
Anton Kurylo <antonkurilo9@gmail.com>
2022-11-04 20:52:34 +0200

    remove comments

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Ball[] balls = new Ball[3];
 
         for (int i = 0; i < 3; i++) {
