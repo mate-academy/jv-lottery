f71d8e4d06077e08fa621acadc6e2ca45a3f5709
Olga Kolosova <olgakolosova0214@gmail.com>
2022-11-04 23:28:21 +0200

    Fixes: Override to.String in class Ball. Make Random,ColorSupplier class fields in class ColorSupplier. Create 3 balls in class Application(with random number and color)

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,16 @@ public class Ball {
     private final int number;
     private final Color color;
 
-    public Ball(Color color, int number){
+    public Ball(Color color, int number) {
         this.number = number;
         this.color = color;
     }
 
     @Override
     public String toString() {
-        return "Ball " +
-                "number: " + number +
-                ", color: " + color;
+        return "Ball "
+                + "number: " + number
+                + ", color: " + color;
     }
 }
 
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 import java.util.Random;
 
-    public class ColorSupplier {
-        private final Random random = new Random();
+public class ColorSupplier {
+    private final Random random = new Random();
+
     public Color getRandomColor() {
         int value = random.nextInt(Color.values().length);
         return Color.values()[value];
