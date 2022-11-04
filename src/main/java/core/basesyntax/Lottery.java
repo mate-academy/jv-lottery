61341c16205738f80f61c996a818476c559007df
Dima Byrka <fobossen@gmail.com>
2022-11-04 21:35:45 +0200

    Fixed METHOD_DEF error

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int RANDOM_COUNT = 100;
+
     public Ball getRandomBall() {
         return new Ball(new Random().nextInt(RANDOM_COUNT),new ColorSupplier().getRandomColor());
     }
