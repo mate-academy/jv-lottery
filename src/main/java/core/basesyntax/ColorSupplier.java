aac5bb375ac99da16aa2d22cc7f9c99168972e70
Andrei_Bondarenko <bondorol007@gmail.com>
2022-11-04 10:22:49 +0200

    jv-lottery  fix constructor`s bug

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,21 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    String colorRandomBall;
+    private String colorRandomBall;
+
+    public String getColorRandomBall() {
+        return colorRandomBall;
+    }
+
+    public void setColorRandomBall(String colorRandomBall) {
+        this.colorRandomBall = colorRandomBall;
+    }
+
     public String getRandomColor() {
+
         Color[] colors = Color.values();
         Random random = new Random();
-        colorRandomBall  = colors[random.nextInt(colors.length)].name();
+        colorRandomBall = colors[random.nextInt(colors.length)].name();
         return colorRandomBall;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    final int numberBall = 100;
+    private int numberBall = 100;
+
+    public int getNumberBall() {
+        return numberBall;
+    }
+    public void setNumberBall(int numberBall){
+        this.numberBall = numberBall;
+    }
+
     public Ball getRandomBall() {
+
         ColorSupplier colorSupplier = new ColorSupplier();
         Random random = new Random();
         Ball ball = new Ball(random.nextInt(numberBall), colorSupplier.getRandomColor());
