421e4a872c6e5f3d7df627ac04cbd032be8d1a0a
Denys Yastrebov <programmer_p@outlook.com>
2022-11-05 11:58:53 +0200

    fixed private variabels

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
 
-    Random number = new Random();
-    ColorSupplier colour = new ColorSupplier();
+    private Random number = new Random();
+    private ColorSupplier colour = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(number.nextInt(MAX_NUMBER), colour.getRandomColor());
