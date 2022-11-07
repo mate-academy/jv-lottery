6037d8b9275a72fb51d5b46a5fcc5f43c638dd01
Myhailo Mykhasov <mikhailmikhasev@gmail.com>
2022-11-07 19:58:47 +0200

    Created public static final int MAX_BALL_NUMBER = 100

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_BALL_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colSup = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColour(colSup.getRandomColor().toString());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
