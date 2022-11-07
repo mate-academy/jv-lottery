f5b48c660594909317ee865da6367913d7edf270
Olga Kolosova <olgakolosova0214@gmail.com>
2022-11-07 20:51:30 +0200

    Fixes: Create new Lottery() in class Application.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,15 +1,11 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball [] balls = new Ball[3];
+        Lottery ball = new Lottery();
         for (int i = 0; i < 3; i++) {
-            balls[i] = new Lottery().getRandomBall();
-            System.out.println(Arrays.toString(balls));
+            System.out.println(ball.getRandomBall());
         }
-
     }
 }
