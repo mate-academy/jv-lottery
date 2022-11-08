15ff83e121273a1841bba671b9bd78eecfe837c2
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-11-08 17:42:49 +0200

    Class Ball created Color and number fields are created.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,4 +1,22 @@
 package core.basesyntax;
 
 public class Ball {
+    private String color;
+    private int number;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
