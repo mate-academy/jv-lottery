f6b7d2768c7195ec5a3170f540db4af7696c9aa6
Olga Kolosova <olgakolosova0214@gmail.com>
2022-11-08 19:01:54 +0200

    Fixes: Create new Lottery() in class Application.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int VALUE = 100;
+    private static final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color color = supplier.getRandomColor();
-        int number = random.nextInt(VALUE);
+        int number = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(color, number);
     }
 }
