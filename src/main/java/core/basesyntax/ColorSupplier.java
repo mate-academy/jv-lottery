452835f9d45db8e7b9030229a362613b8619a079
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-11-10 16:56:28 +0200

    Class Ball created Color and number fields are created. Override toString() created class Colors ENUM was created with 4 different colors Method that gives random colors was created in ColorSupplier Class Lottery was created Method getRandomBall was executed In Main called random ball 3 times using loop. checkstyle mistakes were fixed.
    Mistakes after review comments were fixed. Thank you for your guidance :)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,16 +1,10 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class ColorSupplier extends Lottery {
     public String getRandomColor() {
 
-        int randomIndex = new Random().nextInt(Colors.values().length);
+        int randomIndex = random.nextInt(Color.values().length);
 
-        Ball ball = new Ball();
-
-        ball.setColor(String.valueOf(Colors.values()[randomIndex]));
-
-        return ball.getColor();
+        return String.valueOf(Color.values()[randomIndex]);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,22 +2,25 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends Application {
+public class Lottery {
+    Random random = new Random();
     public String getRandomBall() {
 
-        Random random = new Random();
+        int maxPossibleNumber = 100;
 
-        int randomValue = random.nextInt(100);
+        int randomValue = random.nextInt(maxPossibleNumber);
 
         Ball ball = new Ball();
 
         ball.setNumber(randomValue);
 
-        int randomBallNumber = ball.getNumber();
+        ColorSupplier randomColor = new ColorSupplier();
 
-        String randomBallColor = ball.getRandomColor();
+        String rndClr = randomColor.getRandomColor();
 
-        return "The ball with " + randomBallColor + " color and " + randomBallNumber + " number";
+        ball.setColor(rndClr);
+
+        return ball.toString();
     }
 
 }
