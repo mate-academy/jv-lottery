2546b70a27268887d2e26d690df606dfb0923cf8
Andrei_Bondarenko <bondorol007@gmail.com>
2022-11-11 21:52:13 +0200

    created constant in Lottery class and removed empty line

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Color [] COLORS = Color.values();
+    private final Color [] colors = Color.values();
     private Random random;
 
     public String getRandomColor() {
         random = new Random();
-        return COLORS[random.nextInt(COLORS.length)].name();
+        return colors[random.nextInt(colors.length)].name();
     }
 }
