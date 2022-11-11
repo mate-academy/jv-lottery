22f3cbcf77c9232084cadfae07368fe4bd957ceb
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2022-11-11 21:49:24 +0200

    Made Random in ColorSupplier a class-level variables Method in ColorSupplier class was changed from toString() to name()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
 
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        return Color.values()[index].toString();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
