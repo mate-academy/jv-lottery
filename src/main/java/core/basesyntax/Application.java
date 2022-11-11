c64c41944b2838232c9832bb4e0947f1fb70fecc
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2022-11-12 00:25:18 +0200

    added lottery solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery ball = new Ball();
+        System.out.println(ball.getRandomBall().toString());
+        System.out.println(ball.getRandomBall().toString());
+        System.out.println(ball.getRandomBall().toString());
     }
 }
