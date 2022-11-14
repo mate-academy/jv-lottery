b1b85af08a4abac27ba00ca34702c48d63aca808
Andrii Volos <andrea.home88@gmail.com>
2022-11-14 12:55:32 +0200

    added loop for main class and use name method for enum and mvn clean...

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
-        String color = Colors.values()[index].toString();
+        String color = Colors.values()[index].name();
+
         return color;
     }
 }
