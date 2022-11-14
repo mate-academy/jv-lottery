06e411ad231a3a3feacb2fa5abc5404ead53ad4e
Andrii Volos <andrea.home88@gmail.com>
2022-11-14 12:20:47 +0200

    added loop for main class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery ball1 = new Lottery();
-        Lottery ball2 = new Lottery();
-        Lottery ball3 = new Lottery();
-        System.out.println(ball1.getRandomBall());
-        System.out.println(ball2.getRandomBall());
-        System.out.println(ball3.getRandomBall());
+        Lottery[] ball = new Lottery[3];
+        for (int i = 0; i < ball.length; i++) {
+            ball[i] = new Lottery();
+            System.out.println(ball[i].getRandomBall());
+        }
     }
 }
