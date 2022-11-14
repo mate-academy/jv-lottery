e2d6784b8bd6e16baa9c53528fd55e11dc1b35ea
Melnychuk <melnychukmyhailo@gmail.com>
2022-11-14 09:23:48 +0200

    white space deleted

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public  Ball getRandomBall() {
+    public Ball getRandomBall() {
         int ballNumber = new Random().nextInt(101);
         ColorSupplier colorSupplier = new ColorSupplier();
         String colorOfBall = colorSupplier.getRandomColor();
