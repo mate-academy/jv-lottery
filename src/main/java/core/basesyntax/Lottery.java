8c290ec9f3ddc8ab891fcd57835178fe73ba75dc
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2022-11-16 17:50:44 +0200

    fixed getRandomBall in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBER_OF_VALUES = 100;
-    private final Ball newBall = new Ball();
     private final Random random = new Random();
     private final ColorSupplier randomColor = new ColorSupplier();
 
-    public String getRandomBall() {
+    public Ball getRandomBall() {
+        Ball newBall = new Ball();
         newBall.setColor(randomColor.getRandomColor());
         newBall.setNumber(random.nextInt(NUMBER_OF_VALUES));
-        return newBall.toString();
+        return newBall;
     }
 }
