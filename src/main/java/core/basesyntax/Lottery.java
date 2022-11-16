9882cdbb494ffada56350747ad9e46270393c65c
user <graban2253@gmail.com>
2022-11-16 18:23:14 +0200

    Corrected mistakes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     public String getRandomBall() {
         int index = new Random().nextInt(100);
         return new Ball(new ColorSupplier().getRandomColor(), index).toString();
