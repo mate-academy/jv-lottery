0f41e4da6d5d3c242154d8924201fe0ecc391b25
Tymofii Skrypko <skrypko270901@gmail.com>
2022-11-17 17:40:02 +0100

    all required changes are applied

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
