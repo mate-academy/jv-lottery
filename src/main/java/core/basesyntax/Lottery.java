b35207057671309e132dbfc9528ff94aec0a9a81
Myhailo <melnychukmyhailo@gmail.com>
2022-11-18 22:08:31 +0200

    - create BALL_COUNT value; - move Random and ColorSupplier classes implementation from getRandomBall() method;

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_COUNT = 101;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
     public Ball getRandomBall() {
-        int ballNumber = new Random().nextInt(101);
-        ColorSupplier colorSupplier = new ColorSupplier();
+        int ballNumber = random.nextInt(BALL_COUNT);
         String colorOfBall = colorSupplier.getRandomColor();
         return new Ball(colorOfBall, ballNumber);
     }
