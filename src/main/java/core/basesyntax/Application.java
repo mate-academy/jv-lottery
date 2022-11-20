951dd49dc731beb55e2d76861a054053e17c053a
Myhailo <melnychukmyhailo@gmail.com>
2022-11-20 21:22:26 +0200

    NUMBERS_OF_BALLS added; loop added;

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery randomBall = new Lottery();
-        Ball firstBall = randomBall.getRandomBall();
-        Ball secondBall = randomBall.getRandomBall();
-        Ball thirdBall = randomBall.getRandomBall();
-        System.out.println(firstBall.toString());
-        System.out.println(secondBall.toString());
-        System.out.println(thirdBall.toString());
+        Ball [] balls = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            balls[i] = randomBall.getRandomBall();
+            System.out.println(balls[i].toString());
+        }
     }
 }
