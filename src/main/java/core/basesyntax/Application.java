a0df1fb27de23ea8ff434cd2eb7312762d326c3c
Dmytro M <dmytro.malinnikov@gmail.com>
2022-11-21 20:52:37 +0200

    implement random ball generation logic

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+        System.out.println(Arrays.toString(balls));
     }
 }
