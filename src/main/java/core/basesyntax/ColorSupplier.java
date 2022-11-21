b9bea9a7a1ebf32bf7564d6bda827717d8328425
Марк Міллер <youtte1g@gmail.com>
2022-11-21 10:19:19 +0200

    some variables was renamed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
 
-    public Color getRandomIndex() {
+    public Color getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index];
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,12 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int RANGE = 100;
     private final ColorSupplier supplier = new ColorSupplier();
-
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        Color randomIndex = supplier.getRandomIndex();
-        Ball ball = new Ball(randomIndex.name(), random.nextInt(RANGE));
+        Color randomColor = supplier.getRandomColor();
+        Ball ball = new Ball(randomColor.name(), random.nextInt(RANGE));
         return ball;
     }
 }
