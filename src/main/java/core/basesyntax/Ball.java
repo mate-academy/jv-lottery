a1d5b8d09000a63980dd1d9cf5f7d3a450c0a1a4
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2022-11-21 20:13:55 +0200

    Colors and Ball classes added

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Ball {
+    int number;
+    String color;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String colour) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum Colors {
+    blue,
+    green,
+    orange,
+    red,
+    yellow,
+}
