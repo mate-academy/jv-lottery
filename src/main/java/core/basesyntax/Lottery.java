a1d166c7b74a129ae1186cf9a6f9b3d612c068c1
Ivan Vovnyanko <moonkit.dior@gmail.com>
2022-11-22 22:31:16 +0300

    solution

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int RANDOM_NUMBER_COUNT = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
