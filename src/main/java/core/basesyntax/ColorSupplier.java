455bed52e2f2057af800f544988a3b6450f09b40
Ivan Vovnyanko <118556810+ivan-vovnyanko@users.noreply.github.com>
2022-11-22 23:23:55 +0300

    Update src/main/java/core/basesyntax/ColorSupplier.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+    
     public String getRandomColor() {
         Random random = new Random();
         int value = random.nextInt(Color.values().length);
