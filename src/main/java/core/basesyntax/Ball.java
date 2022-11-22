c6acb59f403847b28ea64beffa2ed56cca1ebc33
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2022-11-22 21:00:21 +0200

    Checkstyle was fixed

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,19 +1,20 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Ball {
-    int number;
-    String color;
+    private int number;
+    private String color;
+
     public Ball(int number, String color) {
         this.number = number;
+
         this.color = color;
     }
+
     @Override
     public String toString() {
-        return "Ball {" +
-                "number=" + number +
-                ", color='" + color + '\'' +
-                '}';
+        return "Ball {"
+                + "number=" + number
+                + ", color='" + color + '\''
+                + '}';
     }
 }
