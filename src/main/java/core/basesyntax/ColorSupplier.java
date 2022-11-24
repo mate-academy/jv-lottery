b2c7662f86cc6ec54d6cbaa4414978d047031142
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-11-24 13:33:35 +0200

    ColorSupplier class implemented

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final Random RANDOM = new Random();
+    private final int SIZE = Colors.values().length;
     public String getRandomColor() {
-        return null;
+
+        int position = RANDOM.nextInt(SIZE);
+        return Colors.values()[position].name();
     }
 }
