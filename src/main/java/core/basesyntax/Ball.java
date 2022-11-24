070c57bbf4bfc245ebb5908461c7cccc4caba66d
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2022-11-24 13:21:45 +0200

    Ball class created, toString() method overrided

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    @Override
+    public String toString() {
+        return this.getColor() + " ball, number - " + this.getNumber();
+    }
+}
+
