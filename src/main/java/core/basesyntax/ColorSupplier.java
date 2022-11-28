b5d7aa93421251b426030923f426ceca55a86fb8
salesforcetrainees2 <loranazarenko26@gmail.com>
2022-11-28 23:14:43 +0200

    Hot fix name

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int COLOR_COUNT = 4;
-    private final Random rand = new Random();
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        int colorIdx = rand.nextInt(COLOR_COUNT);
+        int colorIdx = random.nextInt(Color.values().length);
         return Color.values()[colorIdx].name();
     }
 }
