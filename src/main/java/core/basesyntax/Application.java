7ed3c2b24f58f92cc981b9d628b8c7fd87c38f46
Alina Smirnova <0970352020a@gmail.com>
2022-11-29 23:18:09 +0300

    fixed after comments and cheked style

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
