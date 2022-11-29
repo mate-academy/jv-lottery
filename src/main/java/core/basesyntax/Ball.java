9157942015440ce10f32f4df25be6b6b90a90abc
Alina Smirnova <0970352020a@gmail.com>
2022-11-29 23:15:22 +0300

    fixed after comments and cheked style

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -6,15 +6,19 @@ public class Ball {
     public String getColor() {
         return color;
     }
+
     public void setColor(String color) {
         this.color = color;
     }
+
     public int getNumber() {
         return number;
     }
+
     public void setNumber(int number) {
         this.number = number;
     }
+
     @Override
     public String toString() {
         return "Ball: " + "color - " + color + "\nnumber - " + number;
