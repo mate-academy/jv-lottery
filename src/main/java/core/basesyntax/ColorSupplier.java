b8217eadf07adf14a969c81153cb532126ff8f05
Serhii Kopervas <sergkopervas@gmail.com>
2022-12-01 22:19:37 +0100

    Initial commit

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random rnd = new Random();
+        Colors[] colors = Colors.values();
+        return (colors[rnd.nextInt(Colors.values().length)].toString());
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    WHITE,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    BROWN,
+    ORANGE,
+    PINK,
+    PURPLE,
+    GREY
+}
