eb87ea68e0b1ce714f46f3f26f0c65c199e4f329
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2022-12-04 17:09:45 +0200

    made constant BALL_NUMBER_LIMIT

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_NUMBER_LIMIT = 100;
     private final Random random = new Random();
     private final ColorSupplier supplier = new ColorSupplier();
-    private static final int BALL_NUMBER_LIMIT = 100;
+
     Ball getRandomBall() {
         Ball ball = new Ball(random.nextInt(BALL_NUMBER_LIMIT), supplier.getRandomColor());
         return ball;
