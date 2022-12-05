65814bdb5006605c86519943c0ee862d93dbd715
Maksym Tymoshchenko <timoschenkomaks123@gmail.com>
2022-12-05 12:27:09 +0200

    edited some common mistakes

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,14 @@ public class Ball {
     private String color;
     private int number;
 
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
     @Override
     public String toString() {
         return "Ball color: " + color + ", ball number: " + number;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        Ball ball = new Ball();
         ColorSupplier colorSupplier = new ColorSupplier();
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(100));
     }
 }
