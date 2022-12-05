302f0ffa37b264248dbb7e982631780e3025c321
Ihor Vynogradskyi <avassorv@gmail.com>
2022-12-05 18:36:21 +0200

    my new correct lottery

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setColor(String.valueOf(colorSupplier.getRandomColor()));
+        ball.setNumber(random.nextInt(100));
+        return ball;
+    }
+}
