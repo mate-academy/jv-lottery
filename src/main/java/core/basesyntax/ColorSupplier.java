a2762172dd03f9704306b7029dcaf40f61175319
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-12-05 17:05:30 +0200

    Unnecessary methods removed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,12 +5,8 @@ import java.util.Random;
 public class ColorSupplier {
     private Random random = new Random();
 
-    public Random getRandom() {
-        return random;
-    }
-
     public String getRandomColor() {
-        int index = getRandom().nextInt(Color.values().length);
-        return (Color.values()[index]).name();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,21 +5,13 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_MAX_VALUE = 100;
     private Random random = new Random();
-    private ColorSupplier randomColor = new ColorSupplier();
-
-    public Random getRandom() {
-        return random;
-    }
-
-    public ColorSupplier getRandomColor() {
-        return randomColor;
-    }
-
-    public int getRandomNumber() {
-        return random.nextInt(NUMBER_MAX_VALUE);
-    }
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(randomColor.getRandomColor(), getRandomNumber());
+        return new Ball(colorSupplier.getRandomColor(), getRandomNumber());
+    }
+
+    private int getRandomNumber() {
+        return random.nextInt(NUMBER_MAX_VALUE);
     }
 }
