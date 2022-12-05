c3fd725bdc602739110e7a9d0fdbc1a93bda9390
Serhii Kopervas <sergkopervas@gmail.com>
2022-12-05 19:41:27 +0100

    Made Random a class-level variable. Made random a private and finalized variable.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random randomColor = new Random();
+    private final Random random = new Random();
+
     public String getRandomColor() {
         Color[] colors = Color.values();
-        return colors[randomColor.nextInt(Color.values().length)].name();
+        return colors[random.nextInt(Color.values().length)].name();
     }
 }
