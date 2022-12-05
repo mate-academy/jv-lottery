8fcff30f37909856e8834f2956e976c2c0dc6e98
Ihor Vynogradskyi <np.logist@ukr.net>
2022-12-05 13:53:49 +0200

    add classes again

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String toString(Ball ball) {
+        return "Color: " + ball.color + ". Number: " + ball.number + ".";
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE,
+    WHITE,
+    BLACK
+}
