149661f78e17b87e0cae5b4daa4cf095c89871a0
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-12-05 15:48:19 +0200

    Commits merged

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,9 +1,25 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
+    private static final int NUMBER_MAX_VALUE = 100;
+    private Random random = new Random();
+    private ColorSupplier randomColor = new ColorSupplier();
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public ColorSupplier getRandomColor() {
+        return randomColor;
+    }
+
+    public int getRandomNumber() {
+        return random.nextInt(NUMBER_MAX_VALUE);
+    }
+
     public Ball getRandomBall() {
-        ColorSupplier randomColor = new ColorSupplier();
-        BallNumberSupplier randomBallNumber = new BallNumberSupplier();
-        return new Ball(randomColor.getRandomColor(), randomBallNumber.getRandomNumber());
+        return new Ball(randomColor.getRandomColor(), getRandomNumber());
     }
 }
