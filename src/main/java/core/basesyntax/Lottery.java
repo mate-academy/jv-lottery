ff1d14014cda1287e02281562b9d90015bfbffc9
Inna Diachkina <innadyachkina@gmail.com>
2022-12-06 09:20:02 +0200

    change modiffier

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BALL_NUMBER = 100;
+    private static final int MAX_BALL_RANDOM_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(BALL_NUMBER));
+        ball.setNumber(random.nextInt(MAX_BALL_RANDOM_NUMBER));
         return ball;
     }
 }
