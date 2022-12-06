3134ac93ecdf2a67e681ed0e1e01fc3a8918c58a
Inna Diachkina <innadyachkina@gmail.com>
2022-12-06 07:13:08 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int BALL_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(random.nextInt(BALL_NUMBER));
         return ball;
     }
 }
