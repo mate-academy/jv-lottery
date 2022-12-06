15779c8924e34e739e548e4df97e853e332fc0db
Oleksandr Karpiuk <karpo99old@gmail.com>
2022-12-06 14:06:10 +0200

    work solution finish

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,11 +3,17 @@ package core.basesyntax;
 public class Ball {
     private Color color;
     private int number;
+
     public Ball(Color color, int number) {
         this.color = color;
         this.number = number;
     }
-   public String toString() {
-        return color+ " " +number;
-   }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color=" + color +
+                ", number=" + number +
+                '}';
+    }
 }
