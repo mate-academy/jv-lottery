7b5e0840ebc4b011f20d06dc27e1c4039191c8eb
Your NameMykola <mykola.osolinskyi@gmail.com>
2022-12-07 20:07:14 +0100

    lottery second commit

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int BALLS_QUANTITY = 3;
+    private static final int MAX_BALLS_QUANTITY = 3;
 
     public static void main(String[] args) {
         Lottery ball1 = new Lottery();
-        for (int i = 0; i < BALLS_QUANTITY; i++) {
+        for (int i = 0; i < MAX_BALLS_QUANTITY; i++) {
             System.out.println(ball1.getRandomBall());
         }
     }
