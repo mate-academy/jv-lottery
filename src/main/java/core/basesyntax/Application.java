a50e125d86a6e1ca8199431a26bd91fab5474804
Your NameMykola <mykola.osolinskyi@gmail.com>
2022-12-07 19:54:31 +0100

    lottery first commit

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_QUANTITY = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery ball1 = new Lottery();
+        for (int i = 0; i < BALLS_QUANTITY; i++) {
+            System.out.println(ball1.getRandomBall());
+        }
     }
 }
