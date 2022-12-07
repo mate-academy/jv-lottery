6293b2acba0692b582b03a4208e5f003a1e73bee
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-07 21:47:02 +0200

    add whitespace

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final int numRandCol = Color.values().length;
+
     public String getRandomColor() {
         int index = new Random().nextInt(numRandCol);
         return String.valueOf(Color.values()[index]);
