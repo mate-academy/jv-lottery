c265adc8f9f32594d6e8b6420c9d5292d32b7a62
Solomiia Voloshyn <solomiiavoloshyn@gmail.com>
2022-12-07 23:46:29 +0200

    lottery hw done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int MAX_RANDOM_NUMBER_BALL = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 1; i <= MAX_RANDOM_NUMBER_BALL; i++) {
