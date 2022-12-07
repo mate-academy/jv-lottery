889f48b0cf89ae5a122af9afe8f7c8ef680f92e8
Дмитро <dima.smyk99.19@gmail.com>
2022-12-07 18:45:25 +0200

    add for loop and refactor name

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery firstBall = new Lottery();
-        Lottery secondBall = new Lottery();
-        Lottery thirdBall = new Lottery();
-        System.out.println(firstBall.getRandomBall());
-        System.out.println(secondBall.getRandomBall());
-        System.out.println(thirdBall.getRandomBall());
+        for (int i = 0; i < 3; i++) {
+            Lottery uniqueBallForLottery = new Lottery();
+            System.out.println(uniqueBallForLottery.getRandomBall());
+        }
     }
 }
