6b97cf508ecd7ea8b6801dc6ba14de9124940bb4
Yurii Dikalo <dikalotel@gmail.com>
2022-12-07 19:56:06 +0200

    removed redundant empty line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_RANDOM_NUMBER = 100;
     private final Random random = new Random();
-
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
