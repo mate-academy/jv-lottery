024c76ce8960304db5cd958fa9306d69b487d36c
Yurii Dikalo <dikalotel@gmail.com>
2022-12-07 18:06:51 +0200

    removed redundant empty lines, renamed constant MAX

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX = 100;
-
+    private static final int MAX_BALL_RANDOM_NUMBER = 100;
     private final Random random = new Random();
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(random.nextInt(MAX) + 1, colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(MAX_BALL_RANDOM_NUMBER) + 1, colorSupplier.getRandomColor());
     }
 }
