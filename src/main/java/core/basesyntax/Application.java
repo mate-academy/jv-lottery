de111bbb3183e383b0b3c0907a044c6f00bf4bb3
carbo <carbon7557@gmail.com>
2022-12-07 17:50:48 +0000

    Task is completed.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-    Lottery lottery = new Lottery();
-       Ball ball = lottery.getRandomBall();
-       Ball ballOne = lottery.getRandomBall();
-       Ball ballTwo = lottery.getRandomBall();
+        Lottery lottery = new Lottery();
+        Ball ball = lottery.getRandomBall();
+        Ball ballOne = lottery.getRandomBall();
+        Ball ballTwo = lottery.getRandomBall();
 
-       System.out.println(ball);
-       System.out.println(ballOne);
-       System.out.println(ballTwo);
+        System.out.println(ball);
+        System.out.println(ballOne);
+        System.out.println(ballTwo);
     }
 }
