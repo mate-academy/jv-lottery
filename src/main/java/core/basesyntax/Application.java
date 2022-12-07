c9f46d8e3ba67eee3b2352c46ccdc4c2a6e0a997
Дмитро <dima.smyk99.19@gmail.com>
2022-12-07 19:14:48 +0200

    create object out of loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery uniqueBallForLottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Lottery uniqueBallForLottery = new Lottery();
             System.out.println(uniqueBallForLottery.getRandomBall());
         }
     }
