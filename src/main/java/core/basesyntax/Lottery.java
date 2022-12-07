15e5cf88db512c38db9fa2f99b97b910a3a1b905
Yurii Dikalo <dikalotel@gmail.com>
2022-12-07 16:46:19 +0200

    made constant MAX static and fixed its definition order

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random random = new Random();
+    private static final int MAX = 100;
 
-    private final int MAX = 100;
+    private final Random random = new Random();
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
