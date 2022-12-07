76d09f5d666d40cde2638ac0ef12131b2119822c
carbo <carbon7557@gmail.com>
2022-12-07 17:42:46 +0000

    Task is completed.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery {
-    public Ball getRandomBall() {
-        Ball ball = new Ball();
+       public class Lottery {
+     public Ball getRandomBall() {
+         Ball ball = new Ball();
 
-        ColorSupplier colorSupplier = new ColorSupplier();
-        ball.setColor(colorSupplier.getRandomColor().name());
+         ColorSupplier colorSupplier = new ColorSupplier();
+         ball.setColor(colorSupplier.getRandomColor().name());
 
-        Random random = new Random();
+         Random random = new Random();
         final int maximum_number_limit = 100;
         ball.setNumber(random.nextInt(maximum_number_limit));
         return ball;
