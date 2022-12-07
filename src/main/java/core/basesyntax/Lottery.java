469206b7ee98b44633ee6df79617cb04f91609cd
Yurii Dikalo <dikalotel@gmail.com>
2022-12-07 16:25:38 +0200

    added constant MAX to Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     private final Random random = new Random();
+
+    private final int MAX = 100;
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(random.nextInt(100) + 1, colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(MAX) + 1, colorSupplier.getRandomColor());
     }
 }
