88d5ad782b0143cb677f3a88225b831873128604
Butovych <dreamextreme29@gmail.com>
2022-12-08 21:50:42 +0200

    Wrong code for fixing mistakes

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static int getRandomBall() {
+        Random random = new Random();
+        int value = random.nextInt(100);
+        return value;
+    }
+
+}
