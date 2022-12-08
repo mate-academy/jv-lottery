94924da7d2903153a1d6a680453df3133cccab78
Polina Lysytska <nikolavna100500@gmail.com>
2022-12-08 08:24:32 +0200

    requested changes were implemented

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int NUMBER_COUNT = 101;
+    private static final int MAX_BALL_RANDOM_NUMBER = 101;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_COUNT));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_RANDOM_NUMBER));
     }
 }
