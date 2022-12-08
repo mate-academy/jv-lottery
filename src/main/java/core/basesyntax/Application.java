3424f783c814ecbb6d592c43538f7052cab76008
Leonid <leonvil093@gmail.com>
2022-12-08 15:31:50 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Ball ball = new Lottery().getRandomBall();
-            System.out.println(ball.toString());
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private int ballNumber = new Random().nextInt(100);
+    private String ballColor = new ColorSupplier().getRandomColor();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(new Random().nextInt(100));
-        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(ballNumber);
+        ball.setColor(ballColor);
         return ball;
     }
 }
