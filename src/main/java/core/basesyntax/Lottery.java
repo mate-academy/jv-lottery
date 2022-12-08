54f4cc4709de7d4fa5fb49732806c6928880f773
Leonid <leonvil093@gmail.com>
2022-12-08 13:31:07 +0200

    lottery was modified

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,7 @@ import java.util.Random;
 public class Lottery {
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        Random random = new Random();
-        ball.number = random.nextInt(100);
+        ball.number = new Random().nextInt(100);
         ball.color = new ColorSupplier().getRandomColor();
         return ball;
     }
