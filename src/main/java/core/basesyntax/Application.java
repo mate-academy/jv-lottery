1dfd3c95ff8c741cb444ae877a703698cca4ae98
Pandoryms <andrei.hlebov21@gmail.com>
2022-12-08 17:14:22 +0200

    Task was complited

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,11 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery first = new Lottery();
+        Lottery second = new Lottery();
+        Lottery third = new Lottery();
+        System.out.println(first.getRandomBall());
+        System.out.println(second.getRandomBall());
+        System.out.println(third.getRandomBall());
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLUE,
+    RED,
+    GREEN,
+    WHITE,
+    BLACK
+}
