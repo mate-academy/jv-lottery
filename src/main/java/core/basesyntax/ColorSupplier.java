27cf6b988f4090ca19a7efbbcb871421103d90e2
Leonid Topal <118820792+leonid-topal@users.noreply.github.com>
2022-12-08 17:01:40 +0200

    Update src/main/java/core/basesyntax/ColorSupplier.java
    Co-authored-by: Mykyta Arkhanhelskyi <nickarchangel97@gmail.com>

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,6 +6,6 @@ public class ColorSupplier {
     private Random random = new Random();
 
     public String getRandomColor() {
-        return Color.values()[random.nextInt(7)].toString();
+        return Color.values()[random.nextInt(Color.values().length)].toString();
     }
 }
