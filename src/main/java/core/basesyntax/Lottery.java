e9f3aa0c85315de2d7be137ec3ba7693288c23a1
Dastya <daniillipenec@gmail.com>
2022-12-08 16:42:15 +0200

    renamed MAX_BALL_RANDOM_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int maxBallRandomNumber = 100;
+    private static final int MAX_BALL_RANDOM_NUMBER = 100;
     private ColorSupplier supplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(maxBallRandomNumber));
+        ball.setNumber(random.nextInt(MAX_BALL_RANDOM_NUMBER));
         ball.setColor(supplier.getRandomColor().name());
         return ball;
     }
