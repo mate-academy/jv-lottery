df97a893a8448cef31fe4baf9aba781d8f463929
Butovych <dreamextreme29@gmail.com>
2022-12-09 08:56:27 +0200

    Added Getters and Setters

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
 public class Ball {
-    public String color;
-    public int number;
+    private String color;
+    private int number;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     @Override
     public String toString() {
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,8 +6,8 @@ public class Lottery {
     public static Object getRandomBall() {
         Random random = new Random();
         Ball ball = new Ball();
-        ball.color = String.valueOf(ColorSupplier.getRandomColor());
-        ball.number = random.nextInt(100);
+        ball.setColor(String.valueOf(ColorSupplier.getRandomColor()));
+        ball.setNumber(random.nextInt(100));
         return ball;
     }
 }
