f09ff04750994b3094f603b7ed6c4aff0f4f7a6f
Oleksandr Karpiuk <karpo99old@gmail.com>
2022-12-12 17:38:14 +0200

    redy for check style solution

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        Random random = new Random();
+        int index = random.nextInt(100);
+        Ball ball = new Ball(colorSupplier.getRandomColor(), index);
+        return ball;
+    }
+}
