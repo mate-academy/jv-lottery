0e17a10b42002f4f5a9630878c9e847216e231f6
Denis Pugovkin <den.pugovkin@gmail.com>
2022-12-15 20:49:26 +0200

    1. Replaced constant before method. 2. Added name() in toString for more informative.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier supplier = new ColorSupplier();
     private final Random random = new Random();
+    final int maxValue = 100;
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        final int maxValue = 100;
         randomBall.setColor(supplier.getRandomColor());
         randomBall.setNumber(random.nextInt(maxValue));
         return randomBall;
