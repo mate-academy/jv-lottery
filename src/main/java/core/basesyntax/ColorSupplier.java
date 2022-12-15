20f8e6edfc91b809ce63927f0e7ed590fb35ed33
Kostia Demchuk <kostiademchuk23@gmail.com>
2022-12-15 15:43:32 +0200

    Fixed after review.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final int NUMBER_OF_COLOR = Color.values().length;
+    private Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].name();
+        return Color.values()[random.nextInt(NUMBER_OF_COLOR)].name();
     }
 }
--- src/main/java/core/basesyntax/Colors.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public enum Colors {
-    BLACK,
-    WHITE,
-    RED,
-    YELLOW,
-    BLUE
-}
