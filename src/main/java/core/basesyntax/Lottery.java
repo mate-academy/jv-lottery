a35769064aad890ad3729bf893910ddc2e1ccdc0
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2022-12-16 21:43:19 +0200

    fixed style problems again

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private final int NUMBER_LIMIT = 100;
+    private static final int NUMBER_LIMIT = 100;
 
     public Ball getRandomBall() {
 
