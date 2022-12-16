07b5f2a5fb2b2b7e2e36cc33c6f4a10bf1df4889
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2022-12-16 21:40:12 +0200

    fixed style problems again

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,14 +5,14 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private final int LIMIT = 100;
+    private final int NUMBER_LIMIT = 100;
 
     public Ball getRandomBall() {
 
         Ball ball = new Ball();
 
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(LIMIT));
+        ball.setNumber(random.nextInt(NUMBER_LIMIT));
 
         return ball;
     }
