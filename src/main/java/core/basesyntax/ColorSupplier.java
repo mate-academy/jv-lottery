b20a7aebfe5d0b2a0641ccc9d928a2ada24751ea
Kovaev <eugenkoval1010@gmail.com>
2022-12-17 19:17:33 +0200

    implement method getRandomColor

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(Color.values().length);
+        String color = String.valueOf(Color.values()[index]);
+        return color;
     }
 }
