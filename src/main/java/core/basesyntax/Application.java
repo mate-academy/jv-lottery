3dcf31c53a144e2e600a6931d2e7a87f91f44b97
Sergii Gainovskyi <112598705+JohnGrey17@users.noreply.github.com>
2022-12-19 02:58:51 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        Lottery lottery2 = new Lottery();
+        Lottery lottery3 = new Lottery();
 
+        System.out.println(lottery.getRandomBall());
+        System.out.println(lottery2.getRandomBall());
+        System.out.println(lottery3.getRandomBall());
     }
 }
