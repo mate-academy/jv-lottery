9988e7ac2e3c6bb6c0154c2ef63c056f1fb35bc2
Kovaev <eugenkoval1010@gmail.com>
2022-12-20 14:36:28 +0200

    added changes in ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,12 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
-    private int index;
     private Random random = new Random();
 
     public String getRandomColor() {
-        index = random.nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         Color colorRandom = Color.values()[index];
         return colorRandom.name();
     }
