3cb47bf53e4638001bae97fce7526c60712d2092
Vadym Khodorovskyi <vadimhodor@gmail.com>
2022-12-21 18:32:19 +0200

    moved initialization Random and ColorSupplier classes to class-level

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        int randomNumber = random.nextInt(100);
+        return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
 }
