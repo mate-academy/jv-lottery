ce20e82d76ac0a50ced89b310f91a5df5235ff4c
Anatolii Nazarov <anatoly_n@ukr.net>
2022-12-22 18:25:22 +0200

    Created three balls and received numbers and colors for their. Added whitespace and corrected indentation levels

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Main {
     private static final int NUMBER_OF_BALLS = 3;
-    public static void main (String[] args) {
 
+    public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
         Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
