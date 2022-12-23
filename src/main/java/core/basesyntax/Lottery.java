f2d36a35bd393541322a838dcaecb7475715fa94
Anatolii Nazarov <anatoly_n@ukr.net>
2022-12-23 15:26:39 +0200

    Edited according with your remarks. Changed name class Colors to Color, moved random to the class field, changed method main and etc.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         String colorRandom = colorSupplier.getRandomColor();
