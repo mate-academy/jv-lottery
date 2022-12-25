ebab1d030bcb258cbaa2612e14774a22d1574ee8
Artem Ahapov <artemagapov777@gmail.com>
2022-12-25 19:59:04 +0200

    Прибрав наслідування з класа Lottery. Створив об'єкт colorSupplier для доступу до getRandomColor.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,13 +2,14 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
     private static final int MAXIMUM_NUMBER = 100;
+    private static ColorSupplier colorSupplier;
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(getRandomColor());
+        ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(MAXIMUM_NUMBER));
         return ball;
     }
