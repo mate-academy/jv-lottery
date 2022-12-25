5d2106dea9d5e4ef8535765d528837a471ac2255
Artur Muliukov <arthur.muliukov@gmail.com>
2022-12-25 14:01:38 +0500

    1.1 Solved

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int random1 = random.nextInt(100);
         String color = colorSupplier.getRandomColor();
