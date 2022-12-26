5861221f33d98cb5c4b42a2ef8d9c3393562068c
Anna Melnychuk <annamelnichuk2512@gmail.com>
2022-12-26 19:26:36 +0200

    HW-lottery-solution fix9

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int max_number = 100;
+    private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int number = random.nextInt(max_number);
+        int number = random.nextInt(MAX_NUMBER);
         String color = colorSupplier.getRandomColor().name();
         return new Ball(number, color);
     }
