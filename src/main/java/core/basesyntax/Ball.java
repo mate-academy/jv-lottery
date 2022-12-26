ff1fe9aeb0da96578d4c1190e2f8f7bb846e9bd2
Mykola Bobyliak <bobimyko@gmail.com>
2022-12-26 18:07:12 +0200

    lottery done

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
-    Ball (String color, int number){
+    private final String color;
+    private final int number;
+
+    Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
+
     @Override
-    public String toString (){
+    public String toString() {
         return "Color = " + this.color + ";\n Number = " + this.number + ";";
     }
 }
