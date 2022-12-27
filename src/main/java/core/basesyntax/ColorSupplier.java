c3b76a053c62fb1e8d668ca6f1af359e436ff149
gordgelin <gordgelin@s69a.su>
2022-12-27 04:28:57 +0200

    Minor changes due to codestyle checks

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final Random random = new Random();
+
     public String getRandomColor() {
 
         return BallColor.values()[random.nextInt(BallColor.values().length)].name();
