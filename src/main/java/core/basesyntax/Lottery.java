c6df1a6390b345e6ded9914672dbacbb40e29ffd
gordgelin <gordgelin@users.noreply.github.com>
2022-12-27 19:54:43 +0200

    fixed constant naming
    Co-authored-by: Aleksandr Bevza <117547096+aleksandr-bevza@users.noreply.github.com>

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final ColorSupplier colorSupplier = new ColorSupplier();
     private static final Random random = new Random();
-    private static final int ballCount = 100;
+    private static final int BALL_COUNT = 100;
     
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(ballCount) + 1);
