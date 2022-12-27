36945421a625c60a35825bbaa7ee0338ac54c2f0
Kateryna <katerynachi5@gmail.com>
2022-12-27 12:38:01 +0100

    Fix enum name, change toString() to name(), create class constant and variables

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_LOTTERY_NUM = 100;
     private Random random = new Random();
+
     public Ball getRandomBall() {
         int randomNumber = random.nextInt(MAX_LOTTERY_NUM);
         ColorSupplier randomColor = new ColorSupplier();
