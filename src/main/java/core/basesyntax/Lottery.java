5d629fc6fc2943fdf78437fc8e944a421784363b
gordgelin <gordgelin@users.noreply.github.com>
2022-12-27 17:59:41 +0200

    Empty lines removed. Balls count declared as a constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final ColorSupplier colorSupplier = new ColorSupplier();
     private static final Random random = new Random();
-
+    private static final int ballCount = 100;
+    
     public Ball getRandomBall() {
-
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100) + 1);
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(ballCount) + 1);
     }
 }
