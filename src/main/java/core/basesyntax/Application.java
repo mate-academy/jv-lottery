5d2036ecc228710114ec58182e52de0f87ae2978
gordgelin <gordgelin@users.noreply.github.com>
2022-12-27 17:44:50 +0200

    Removed empty lines and Thread.sleep calls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,17 +1,13 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         final int lotteryTakes = 3;
-
         Lottery lottery = new Lottery();
 
         for (int i = 0; i < lotteryTakes; i++) {
             System.out.println(lottery.getRandomBall());
-
-            Thread.sleep(1000);
         }
-
     }
 }
