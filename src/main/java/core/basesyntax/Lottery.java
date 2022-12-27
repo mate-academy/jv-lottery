68643fb00f8fd6c46c039642fc30a8a567d24f0f
Kateryna <katerynachi5@gmail.com>
2022-12-27 18:44:33 +0100

    Move randomColor from method to class fields

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_LOTTERY_NUM = 100;
     private Random random = new Random();
+    private ColorSupplier randomColor = new ColorSupplier();
 
     public Ball getRandomBall() {
         int randomNumber = random.nextInt(MAX_LOTTERY_NUM);
-        ColorSupplier randomColor = new ColorSupplier();
         return new Ball(randomColor.getRandomColor(), randomNumber);
     }
 }
