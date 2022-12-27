2e05d633aeaacbc60ae46569fd7ca11b49ce0968
Anatolii Nazarov <anatoly_n@ukr.net>
2022-12-27 11:42:49 +0200

    Moved 100 to constant. Changed value for return according recommendation

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int range = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-
-    private static final int range = 100;
 
     public int getRange() {
         return range;
