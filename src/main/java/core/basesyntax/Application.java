3562fffc3d1b50150b0da55a8af00d49fc6b6d3e
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2022-12-29 15:08:18 +0200

    create class ball with related methods

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-                Lottery lottery = new Lottery();
-                Ball ball1 = lottery.getRandomBall();
-                Ball ball2 = lottery.getRandomBall();
-                Ball ball3 = lottery.getRandomBall();
-                System.out.println(
-                        "ball1: " + ball1 + "\n" +
-                        "ball2: " + ball2 + "\n"
-                                + "ball3: " + ball3
-                );
+        Lottery lottery = new Lottery();
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
+        System.out.println(
+                "ball1: " + ball1 + "\n" +
+                "ball2: " + ball2 + "\n"
+                        + "ball3: " + ball3
+        );
     }
 }
