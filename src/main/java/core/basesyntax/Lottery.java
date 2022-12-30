dd0e078aae4335009e1160f868f58bfe50a9f4c3
Yuriy Kononchuk <y_kononchuk@ukr.net>
2022-12-30 15:39:19 +0200

    Added changes in Lottery-class after 2nd review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private final Random newNumber = new Random();
+    private final ColorSupplier newColor = new ColorSupplier();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+
+        return new Ball(newColor.getRandomColor(), newNumber.nextInt(MAX_NUMBER));
     }
 }
