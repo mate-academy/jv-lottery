7f81f2e3079015519ab188b19a631f3fe842dd3b
Denys Dominskyi <dominskyj@gmail.com>
2022-12-31 22:16:07 +0200

    Solution1

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.color = new ColorSupplier().getRandomColor();
+        ball.number = new Random().nextInt(100);
+        return ball;
+    }
+}
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
+    }
+}
