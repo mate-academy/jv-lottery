76b6788f86c3995bce48b0a01c0d4be44844d9fe
Yaroslav Ometiukh <yaroslav.ometiukh@outlook.com>
2023-01-01 15:38:23 +0200

    created constant for ball number bound in Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_NUMBER_BOUND = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BALL_NUMBER_BOUND));
     }
 }
