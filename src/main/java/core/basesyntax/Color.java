bb8cf725b6201371307c14c4a9d721e90ad76c28
yermilov.m <yermilov.m@hotmail.com>
2023-01-03 00:24:21 +0100

    edited for successful maven build

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,18 +1,22 @@
 package core.basesyntax;
 
 public enum Color {
-    BLACK("\033[1;90m"), // BLACK
-    RED("\033[1;91m"),   // RED
-    GREEN("\033[1;92m"), // GREEN
+    BLACK("\033[1;90m"),
+    RED("\033[1;91m"),
+    GREEN("\033[1;92m"),
     YELLOW("\033[1;93m"),// YELLOW
-    BLUE("\033[1;94m"),  // BLUE
+    BLUE("\033[1;94m"),
     PURPLE("\033[1;95m"),// PURPLE
-    CYAN("\033[1;96m"), // CYAN
-    WHITE("\033[1;97m"); // WHITE
+    CYAN("\033[1;96m"),
+    WHITE("\033[1;97m");
 
-    public final String value;
+    private final String value;
 
     private Color(String color) {
         this.value = color;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
