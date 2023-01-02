93246f8e174852386759f78ceceead485c239cc6
yermilov.m <yermilov.m@hotmail.com>
2023-01-02 23:57:13 +0100

    made getRandomColor() static

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor() {
+    public static String getRandomColor() {
         return Color.values()[new Random().nextInt(Color.values().length)].color;
     }
 }
