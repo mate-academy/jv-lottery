3c9d30d93c54c2572deedfb0285b3c7a20d2af4f
Vladyslav-Voronkov <vladyslavvoronkov@gmail.com>
2023-01-02 19:39:01 +0100

    Fixed checkstyle errors

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,6 +7,7 @@ public class Lottery {
     private ColorSupplier supplier = new ColorSupplier();
     private Random random = new Random();
     private int maxRand = 100;
+
     public Ball getRandomBall() {
         return new Ball(supplier.getRandomColor(),random.nextInt(maxRand));
     }
