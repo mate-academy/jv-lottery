61765302cfa33a982809fe624eb23be7f6d1b38a
yermilov.m <yermilov.m@hotmail.com>
2023-01-02 23:55:43 +0100

    implemented method getRandomColor() in class ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        return Color.values()[new Random().nextInt(Color.values().length)].color;
     }
 }
