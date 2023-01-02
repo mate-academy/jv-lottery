d0ba49363ac59ff2071f6131c273698016aeba20
yermilov.m <yermilov.m@hotmail.com>
2023-01-02 23:46:02 +0100

    edited enum for better experience

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,31 +1,18 @@
 package core.basesyntax;
 
 public enum Color {
-    WHITE(255, 255, 255),
-    VERY_LIGHT_RED(255, 102, 102),
-    LIGHT_RED(255, 51, 51),
-    RED(255, 0, 0),
-    DARK_RED(204, 0, 0),
-    VERY_DARK_RED(153, 0, 0),
-    VERY_LIGHT_BLUE(51, 204, 255),
-    LIGHT_BLUE(51, 153, 255),
-    BLUE(0, 0, 255),
-    DARK_BLUE(0, 0, 204),
-    VERY_DARK_BLUE(0, 0, 153),
-    VERY_LIGHT_GREEN(102, 255, 102),
-    LIGHT_GREEN(0, 255, 51),
-    GREEN(0, 204, 0),
-    DARK_GREEN(0, 153, 0),
-    VERY_DARK_GREEN(0, 102, 0),
-    BLACK(0, 0, 0);
+    BLACK("\033[1;90m"), // BLACK
+    RED("\033[1;91m"),   // RED
+    GREEN("\033[1;92m"), // GREEN
+    YELLOW("\033[1;93m"),// YELLOW
+    BLUE("\033[1;94m"),  // BLUE
+    PURPLE("\033[1;95m"),// PURPLE
+    CYAN("\033[1;96m"),  // CYAN
+    WHITE("\033[1;97m"); // WHITE
 
-    public final int red;
-    public final int green;
-    public final int blue;
+    public final String color;
 
-    private Color(int r, int g, int b) {
-        this.red = r;
-        this.green = g;
-        this.blue = b;
+    private Color(String color) {
+        this.color = color;
     }
 }
