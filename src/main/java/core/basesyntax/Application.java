481a9dbac8e98794791b65f38494dc2e00a9abe1
Alexander Karpenko <karalexander@gmail.com>
2023-01-03 18:52:08 +0200

    added comments

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,25 +1,30 @@
 package core.basesyntax;
 
 public class Application {
+    /**
+     * Commented block represents task solution with the help of arrays
+     * The second block is solution without arrays
+     */
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Lottery firstBall = new Lottery();
-        Lottery secondBall = new Lottery();
-        Lottery thirdBall = new Lottery();
 
-        System.out.print("First ball color: ");
-        System.out.println(firstBall.getRandomBall().color);
-        System.out.print("First ball number: ");
-        System.out.println(firstBall.getRandomBall().number);
-        System.out.println();
-        System.out.print("Second ball color: ");
-        System.out.println(secondBall.getRandomBall().color);
-        System.out.print("Second ball number: ");
-        System.out.println(firstBall.getRandomBall().number);
-        System.out.println();
-        System.out.print("Third ball color: ");
-        System.out.println(thirdBall.getRandomBall().color);
-        System.out.print("Third ball number: ");
-        System.out.println(thirdBall.getRandomBall().number);
+        /*
+        * String[] balls = {
+        *         "First ball", "Second ball", "Third ball"
+        * };
+        * Lottery lot = new Lottery();
+        * for (String ball : balls) {
+        *     System.out.println(ball + " color: " + lot.getRandomBall().color);
+        *     System.out.println(ball + " number: " + lot.getRandomBall().number);
+        *     System.out.println();
+        * }
+        */
+
+        Lottery lot = new Lottery();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i + ". color: " + lot.getRandomBall().color);
+            System.out.println(i + ". number: " + lot.getRandomBall().number);
+            System.out.println();
+        }
     }
 }
