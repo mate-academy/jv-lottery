b0897ccb65cbd29974134e40e20ac1c52f3e8c63
yermilov.m <yermilov.m@hotmail.com>
2023-01-03 22:36:15 +0100

    code improved for mentor approval

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,12 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        System.out.println(Lottery.getRandomBall());
-        System.out.println(Lottery.getRandomBall());
-        System.out.println(Lottery.getRandomBall());
+        Ball ball1 = Lottery.getRandomBall();
+        Ball ball2 = Lottery.getRandomBall();
+        Ball ball3 = Lottery.getRandomBall();
+        Ball[] balls = {ball1, ball2, ball3};
+        for (Ball ball : balls) {
+            System.out.println(ball);
+        }
     }
 }
