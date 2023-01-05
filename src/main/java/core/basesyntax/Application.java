bf9abe790c63b8aa025701fd8f08cac0b742fc4d
Aleksandr Zahorodnii <git.hub.zagar@gmail.com>
2023-01-05 15:23:35 +0100

    fixed after review remade сдÑclass Aplication Ball ColorSuoplier and Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBERS_OF_BALL = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBERS_OF_BALL; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,16 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int BOUND_NUMBER = 100;
+    private static final int BOUND_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
-    public Random getRandom() {
-        return random;
-    }
-
     public Ball getRandomBall() {
-        int number = getRandom().nextInt(BOUND_NUMBER);
+        int number = random.nextInt(BOUND_NUMBER);
         String color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
