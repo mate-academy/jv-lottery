ed3fe58666a0a69f215ce689a50d049054893348
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-05 16:07:30 +0200

    fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,10 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private ColorSupplier colorSupplier;
-    private final int maxNumber = 100;
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int MAX_NUMBER = 100;
+        final int maxNumber = 100;
         int numberRandom = random.nextInt(maxNumber);
         return new Ball(color, numberRandom);
     }
