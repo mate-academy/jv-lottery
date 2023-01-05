b2289d255b5d7d455ea62b0808034342d8b747e4
yermilov.m <yermilov.m@hotmail.com>
2023-01-06 00:34:34 +0100

    modified array creation logic

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,10 +4,12 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery funLottery = new Lottery();
-        Ball ball1 = funLottery.getRandomBall();
-        Ball ball2 = funLottery.getRandomBall();
-        Ball ball3 = funLottery.getRandomBall();
-        Ball[] balls = {ball1, ball2, ball3};
+        Ball[] balls = new Ball[3];
+
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = funLottery.getRandomBall();
+        }
+
         for (Ball ball : balls) {
             System.out.println(ball);
         }
