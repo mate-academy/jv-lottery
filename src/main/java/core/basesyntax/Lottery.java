104f788588c6dc51ff034da26e6a0e1925009788
Horielikov Maksym <hmv.fermats@gmail.com>
2023-01-06 15:47:08 +0200

    fixed error after first commit

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    private final int MIN = 1;
-    private final int MAX = 100;
-
     public Ball getRandomBall() {
-        return new Ball (new ColorSupplier().getRandomColor(), new Random().nextInt(MAX - MIN + 1) + MIN);
+        return new Ball (new ColorSupplier().getRandomColor(), new Random().nextInt(101));
     }
 }
