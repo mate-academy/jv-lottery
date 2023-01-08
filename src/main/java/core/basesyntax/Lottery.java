715a44f4b74b96100327722f88117fdd9a9e58b1
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-01-08 11:06:47 +0200

    refactoring

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Lottery {
-    final int maxSize = 100;
+    final int MAX_SIZE = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(maxSize);
+        ball.setNumber(MAX_SIZE);
         ball.setColor();
         return ball;
     }
