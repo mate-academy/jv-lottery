06046bd9e9363d9cf86dc8d379aa2a88288ba6cd
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-01-09 10:47:16 +0200

    refactoring

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 public class Application {
 
     private static final int NUMBERS_OF_BALL = 3;
+
     public static void main(String[] args) {
         Ball [] balls = new Ball[NUMBERS_OF_BALL];
         Lottery lottery = new Lottery();
 
-        for( int i = 0; i < balls.length; i++) {
+        for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
     public String getRandomColor() {
-        //Random randomColor = new Random();
         int index = random.nextInt(Color.values().length);
         Color[] values = Color.values();
         return values[index].toString();
