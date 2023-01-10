cca8e0af879aa08c8af9bac3c2dddc955f10cd15
Ihor <ihorpetrytskyi333@gmail.com>
2023-01-09 20:52:40 -0800

    lottery solytion

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+Lottery lottery = new Lottery();
+        for ( int i = 0 ; i < 3 ; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
