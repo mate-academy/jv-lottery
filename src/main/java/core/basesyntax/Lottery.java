821bd6e9029025409c9b4ca4e84e37fcc10af663
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-01-10 21:01:12 +0200

    added changes: extra line, naming enum, added constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private Random randomNumber = new Random();
-    private int value = 100;
+    private static final int value = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Ball ball = new Ball(colorSupplier.getRandomColor(),randomNumber.nextInt(value));
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(),randomNumber.nextInt(value));
     }
 }
