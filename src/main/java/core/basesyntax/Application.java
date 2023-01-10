1ebe03b9922de4ccc03000ab84c9611b278922c4
kromch <94765048+kromch@users.noreply.github.com>
2023-01-11 01:37:03 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
-import java.util.Random;
+
 import core.colors.ColorSupplier;
+import java.util.Random;
     
 public class Application {
 
@@ -13,29 +14,35 @@ public class Application {
             this.number = bound >= 1 && bound <= 100 ? random.nextInt(bound) : random.nextInt(100);
             this.namecolor = namecolor;
         }
+
         @Override
         public String toString() {
             Integer num = this.number;
             return "Ball with number " + num.toString() + " witn " + this.namecolor + " color";
         }
+        
     }
 
     class Lottery {
         private ColorSupplier cs;
+ 
         public Lottery() {
             cs = new ColorSupplier();
         }
+        
         public Ball getRandomBall() {
             return new Ball(100, cs.getRandomColor());
         }
+        
     }
 
     public static void main(String args[]) {
 
         Application ap = new Application();
         Application.Lottery lot = ap.new Lottery();
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)  {
             System.out.println("Got " + lot.getRandomBall().toString());
+            }
     }
 
 }
