994f48c47031313e5fbf080a78e48faf533a89de
kromch <94765048+kromch@users.noreply.github.com>
2023-01-11 01:14:24 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
-
+import core.colors.ColorSupplier;
+    
 public class Application {
-    public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+
+    class Ball {
+        private String namecolor;
+        private int number;
+
+        public Ball(int bound, String namecolor) {
+            Random random = new Random();
+            this.number = bound >= 1 && bound <= 100 ? random.nextInt(bound) : random.nextInt(100);
+            this.namecolor = namecolor;
+        }
+        @Override
+        public String toString() {
+            Integer num = this.number;
+            return "Ball with number " + num.toString() + " witn " + this.namecolor + " color";
+        }
     }
+
+    class Lottery {
+        private ColorSupplier cs;
+        public Lottery() {
+            cs = new ColorSupplier();
+        }
+        public Ball getRandomBall() {
+            return new Ball(100, cs.getRandomColor());
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Application ap = new Application();
+        Application.Lottery lot = ap.new Lottery();
+        for (int i = 0; i < 3; i++)
+            System.out.println("Got " + lot.getRandomBall().toString());
+    }
+
 }
