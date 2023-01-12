8105991bde673804708c719bfd75b2a4ecae36a8
Maxim Skory <devisrush@gmail.com>
2023-01-12 22:45:27 +0200

    Constants for magic numbers were added, loop for the balls creation was added

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        System.out.println(lottery.getRandomBall().toString());
-        System.out.println(lottery.getRandomBall().toString());
-        System.out.println(lottery.getRandomBall().toString());
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall().toString());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int BALL_MAX_NUMBER = 100;
+
     public Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(), new Random().nextInt(100));
+        return new Ball(ColorSupplier.getRandomColor(), new Random().nextInt(BALL_MAX_NUMBER));
     }
 }
