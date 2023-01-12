640406c82e8d9e5498fed3559860a640ebe77b4b
Maxim Skory <devisrush@gmail.com>
2023-01-12 22:03:32 +0200

    Checkstyle error was fixed

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -11,9 +11,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color=" + color +
-                ", number=" + number +
-                '}';
+        return "Ball{" + "color=" + color + ", number=" + number + '}';
     }
 }
