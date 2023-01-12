81a320d68c636bce2ecce53907eca01bacbc65a3
Maxim Skory <devisrush@gmail.com>
2023-01-12 21:22:55 +0200

    Ball class was updated with overrode toString() method

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -8,4 +8,12 @@ public class Ball {
         this.color = color;
         this.number = number;
     }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color=" + color +
+                ", number=" + number +
+                '}';
+    }
 }
