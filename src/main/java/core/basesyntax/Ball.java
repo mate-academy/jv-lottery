076c30004d0cf618b498123b24463d5d8a463b18
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-22 13:05:38 +0200

    Added constructor in class Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,13 +1,18 @@
 package core.basesyntax;
 
 public class Ball {
-    public String Color;
+    public String color;
     public int number;
+
+    public Ball (String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
 
     @Override
     public String toString() {
         return "Ball{" +
-                "Color='" + Color + '\'' +
+                "color='" + color + '\'' +
                 ", number=" + number +
                 '}';
     }
