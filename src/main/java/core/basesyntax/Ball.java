67800985760e9be6767dfc6485dfc143b629dd77
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-22 12:33:45 +0200

    Overrided in class Ball method toString()

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,4 +3,12 @@ package core.basesyntax;
 public class Ball {
     public String Color;
     public int number;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "Color='" + Color + '\'' +
+                ", number=" + number +
+                '}';
+    }
 }
