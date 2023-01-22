906315c85b3cd27d9367a708cfe7503ab9f3f3b3
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-22 20:03:41 +0200

    Fixed method getRandomColor

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor() {
-        Random random = new Random();
-        int index = random.nextInt(ColorsOfBall.values().length);
-        return ColorsOfBall.values()[index].name();
+    public Colors getRandomColor() {
+        return Colors.values()[new Random().nextInt(Colors.values().length)];
     }
 }
