38cfd80452a68178820307997528c3c0b91f7d4b
Volodymyr Sakalosh <sva2005@ukr.net>
2023-01-23 22:35:06 +0200

    Fixed constants in class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int COUNT_NUMBER = 101;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(101));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(COUNT_NUMBER));
     }
 }
