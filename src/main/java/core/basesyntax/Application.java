06b0710a7dfcd1691c778b8a42e86c92af9cef2f
Dmytro Voronin <d.voronin.owen@gmail.com>
2023-01-24 20:48:37 +0200

    To create several random balls one Lottery object should be enough

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,16 +3,11 @@ package core.basesyntax;
 public class Application {
 
     public static void main(String[] args) {
-        int numberOfLottery = 3;//number of 3 lottery
-        Lottery[] lottery = new Lottery[numberOfLottery];
-        // array of new lottery
-        for (int i = 0; i < lottery.length; i++) {
-            lottery[i] = new Lottery();
-        }
-        //sout result 3 ball
-        for (int i = 0; i < lottery.length; i++) {
-            System.out.println(lottery[i].getRandomBall().getColor() + " "
-                    + lottery[i].getRandomBall().getNumber());
+        int numberOfLottery = 3;
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < numberOfLottery; i++) {
+            System.out.println(lottery.getRandomBall().getColor() + " "
+                    + lottery.getRandomBall().getNumber());
         }
     }
 
