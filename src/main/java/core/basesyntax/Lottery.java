ed5339d3ce1191b4ff6b861ee96184080d8e6f08
Dmytro Voronin <d.voronin.owen@gmail.com>
2023-01-24 20:50:32 +0200

    maxBound = 100

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    Random random = new Random();//new class instance random
+    ColorSupplier colorSupplier = new ColorSupplier();//new class instance colorSupplier
+    private static final int maxBound = 100;//range random 0...100
     public Ball getRandomBall() {
-        Random randomNumber = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        int randomNumberBall = randomNumber.nextInt(100);//range 0...100
+        int randomNumberBall = random.nextInt(maxBound);
         Ball ball = new Ball();
-        ball.setNumber(String.valueOf(randomNumberBall));
+        ball.setNumber(randomNumberBall);
         ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
