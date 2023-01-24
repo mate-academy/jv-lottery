e289c712f55d0ea759602dbd980a7b4bdf720bab
Kostyantyn Savchenko <kostyantyn.savchenko@gmail.com>
2023-01-24 21:03:07 +0200

    realized random program of creation Ball

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int UPPER_BOUND = 100;
 
     public Ball getRandomBall() {
-        return new Ball(new Random().nextInt(UPPER_BOUND), new ColorSupplier().getRandomColor().name());
+        return new Ball(new Random().nextInt(UPPER_BOUND),
+                new ColorSupplier().getRandomColor().name());
     }
 }
