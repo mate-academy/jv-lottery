b25b35437228d3c81e2615022d8199f5004ef1c6
Dmytro Voronin <d.voronin.owen@gmail.com>
2023-01-24 20:49:59 +0200

    new object in class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private String randomColor;
+    Random random = new Random();
 
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);// random index for color array
-        return Colors.values()[index].toString(); //int to String
-
+        int index = random.nextInt(Colors.values().length);// random index for color array
+        return randomColor = Colors.values()[index].toString(); //int to String
     }
 
 }
