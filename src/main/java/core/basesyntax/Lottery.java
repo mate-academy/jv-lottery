2cdd2e700ee1a027a5c1474c9b985934c37a69a3
Dmytro Voronin <d.voronin.owen@gmail.com>
2023-01-25 16:53:13 +0200

    formatting code

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int MAX_BOUND = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setNumber(random.nextInt(MAX_BOUND));
