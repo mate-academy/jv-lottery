d79099fc15b9a6c1c1297e1262b5f5993dd466b9
Roman Morozov <morozov.ctk@gmail.com>
2023-01-26 19:56:19 +0200

    Class Ball

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Ball {
+    String color;
+    int number;
+
+    @Override
+    public String toString(String color, int number) {
+        return color + number;
+    }
+}
+}
+
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,6 +1,15 @@
 package core.basesyntax;
 
 public class ColorSupplier {
+    public enum Colors {
+        GREEN,
+        RED,
+        WHITE,
+        BLACK,
+        YELLOW,
+        BLUE,
+        PINK
+    }
     public String getRandomColor() {
         return null;
     }
