7191e589fba23dc4b5bdd34c7d836c4feab9c92e
Nataliia <ladna1984@gmail.com>
2023-01-30 22:22:32 +0200

    Changed the line "static final int ITERATIONS = 3;" to "private static final int ITERATIONS = 3;".

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-    static final int NUMBER = 3;
+    private static final int ITERATIONS = 3;
     private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
-        for (int i = 0; i < NUMBER; i++) {
+        for (int i = 0; i < ITERATIONS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
