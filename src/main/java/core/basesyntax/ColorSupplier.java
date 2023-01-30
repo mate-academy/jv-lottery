f09de68349ad3422287afdd0b4085ca49d926ba7
Nataliia <ladna1984@gmail.com>
2023-01-30 22:22:32 +0200

    In Application.java I changed "int NUMBER" to "int ITERATIONS". I also changed a name of the class Color.java, deleted line in ColorSupplier.java. I don't know how to return  new Ball().

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     private Random generateRandomColor = new Random();
 
     public String getRandomColor() {
-        int randIndex = generateRandomColor.nextInt(Colors.values().length);
-        return Colors.values()[randIndex].name();
+        int randIndex = generateRandomColor.nextInt(Color.values().length);
+        return Color.values()[randIndex].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int HUNDRED = 100;
+    private static final int UPPER_BOUND = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(HUNDRED));
+        ball.setNumber(random.nextInt(UPPER_BOUND));
         ball.setColor(colorSupplier.getRandomColor());
+
         return ball;
     }
 }
