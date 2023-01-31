be32f607c2550a64a92ed9eab21f6194cf372615
Сергей <serhii.serdiuk@gmail.com>
2023-01-31 17:23:50 +0200

    Separate method from previous statement

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int BOUND = 100;
+
     public Ball getRandomBall() {
         Color color = new ColorSupplier().getRandomColor();
         int number = new Random().nextInt(BOUND);
