5adbe200cdf09ebed91d54ad58ad3f5aca53f6bf
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2023-02-04 14:28:58 +0200

    fix line separator

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public String getRandomColor() {
         int randomIndex = random.nextInt(Color.values().length);
         return Color.values()[randomIndex].toString();
