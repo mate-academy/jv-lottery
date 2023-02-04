d98dc177cebc95624aa17a46a6a0d0de6534b6c9
Anna Shokun <anne.shokun@gmail.com>
2023-02-04 19:09:09 +0200

    fixed checkstyle mistake

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public String getRandomColor() {
         int index = random.getRandomColorIndex();
         return Color.values()[index].name();
