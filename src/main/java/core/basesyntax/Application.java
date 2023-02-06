6c347acee25c28693d21a717f6fd9038c0362a53
Ilko Good <ilko.engineering@gmail.com>
2023-02-06 18:01:23 +0200

    toString override implemented, balls are now generated and stored in array

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        int numberOfBalls = 3;
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-        System.out.println(ball1.getColor() + " " + ball1.getNumber());
-        System.out.println(ball2.getColor() + " " + ball2.getNumber());
-        System.out.println(ball3.getColor() + " " + ball3.getNumber());
+        Ball [] balls = new Ball[numberOfBalls];
+        for (int i = 0; i < numberOfBalls; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i].toString());
+        }
     }
 }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,19 +1,16 @@
 package core.basesyntax;
 
 public class Ball {
-    private Colors color;
-    private int number;
+    private final Colors color;
+    private final int number;
 
     public Ball(Colors color, int number) {
         this.color = color;
         this.number = number;
     }
 
-    public Colors getColor() {
-        return color;
-    }
-
-    public int getNumber() {
-        return number;
+    @Override
+    public String toString() {
+        return "Ball " + color + " " + number;
     }
 }
