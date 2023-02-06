fcfcc00331f5908321681007bab0cb28050e8cb4
MaksymHorielikov <hmv.fermats@gmail.com>
2023-02-06 15:18:09 +0200

    add const MAX_NUMBER, COUNT_BALLS

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final  int COUNT_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < COUNT_BALLS; i++) {
             System.out.println(lottery.getRandomBall().toString());
         }
     }
