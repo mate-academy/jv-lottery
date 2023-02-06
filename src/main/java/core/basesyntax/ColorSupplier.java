87bdc7e1ef48b6ab41fefc47dbcd89a0bb6486dc
Anna Shokun <anne.shokun@gmail.com>
2023-02-06 18:56:53 +0200

    deleted personal class Random; edited classes Lottery, ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = random.getRandomColorIndex();
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
 }
