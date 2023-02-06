8816ea9fc85f34a4159981a2d09261ac05742fcb
Skrudj <ssinilov@gmail.com>
2023-02-06 19:06:22 +0200

    removed odd empty lines

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     public Colors getRandomColor() {
         Random random = new Random();
         return Colors.values()[random.nextInt(Colors.values().length)];
