41994a5fd305e4aea5fd87bc8540eb17cdc2f678
kromch <94765048+kromch@users.noreply.github.com>
2023-02-07 16:48:36 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 import core.balls.Ball;
 import core.colors.ColorSupplier;
-
+import core.lottery.Lottery;
     
 public class Application {
     public static void main(String[] args) {
