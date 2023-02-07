8f8eac96aa8c3f7a63f1acb5f86360a5bad2c098
Nazar Zvarych <zvarych45@gmail.com>
2023-02-07 23:47:53 +0200

    I created class Ball, Lottery and Colors, modified class ColorSupplier and Application

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final String [] colors = new String[]{"red","green","blue","yellow","pink","black",
-            "white","brown","gray","orange"};
+    private final Colors [] colors = Colors.values();
 
     public String getRandomColor() {
         Random random = new Random();
-        return colors[random.nextInt(colors.length - 1)];
+        return String.valueOf(colors[random.nextInt(colors.length - 1)]);
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    PINK,
+    BLACK,
+    WHITE,
+    BROWN,
+    GRAY,
+    ORANGE
+}
