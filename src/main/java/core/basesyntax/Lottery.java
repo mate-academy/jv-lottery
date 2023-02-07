ba1e60b6c8b3332f07189fda229c138ee973d0aa
Skrudj <ssinilov@gmail.com>
2023-02-07 22:32:36 +0200

    rename constant in lottery class and lottery variable in main

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.security.SecureRandom;
 
 public class Lottery {
-    private static final int RANDOMIZERBOUND = 100;
+    private static final int RANDOMIZER_BOUND = 100;
     private final SecureRandom randomizer = new SecureRandom();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), randomizer.nextInt(RANDOMIZERBOUND));
+        return new Ball(colorSupplier.getRandomColor(), randomizer.nextInt(RANDOMIZER_BOUND));
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Lottery lot = new Lottery();
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 7; i++) {
-            System.out.println(lot.getRandomBall());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
