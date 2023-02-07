4c9669518c042865e943e75bd5b9cb9451e12df1
ivan <kiohi2000@gmail.com>
2023-02-07 16:40:17 +0200

    added class Ball and created random choose color and number

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,23 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
+
+        List<Ball> balls = new ArrayList<>();
+        balls.add(ball1);
+        balls.add(ball2);
+        balls.add(ball3);
+
+        for (Ball ball : balls) {
+            System.out.println(ball.toString());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    BlACK,
+    GREEN,
+    YELLOW,
+    PINK
+}
