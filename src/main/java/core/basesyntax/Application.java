bda57f203cf5605eb02732165694ed3c77631ff8
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-07 11:53:10 +0200

    mentor check loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball = lottery.getRandomBall();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        System.out.println(ball);
-        System.out.println(ball1);
-        System.out.println(ball2);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
