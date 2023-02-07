e580bb1fd1e4bde8393ffc5fccb711f6a7fcfe0c
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-02-07 19:01:48 +0200

    make changes about private final in classes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        Random random = new Random();
         return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
     }
 }
