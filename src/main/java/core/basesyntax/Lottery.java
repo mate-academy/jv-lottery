054a75b9e8776a59aab3cf38c64342bea53a6f8c
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-02-07 18:55:57 +0200

    create class and enum realize methods

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall(){
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
+    }
+}
