c986658fbccc9968edb99753de74d7c87c7cfaf4
Daunito <gatne2004@gmail.com>
2023-02-07 15:33:12 +0100

    added solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,9 +1,8 @@
 package core.basesyntax;
 
-import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
-        return Colors.values()[new Random().nextInt(Colors.values().length)].toString();
+    public String getRandomColor() {
+        return Colors.values()[Lottery.random.nextInt(Colors.values().length)].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static Random random = new Random();
+
     public Ball getRandomBall() {
-        String color = ColorSupplier.getRandomColor();
-        int number = new Random().nextInt(100);
+        ColorSupplier randomColor = new ColorSupplier();
+        String color = randomColor.getRandomColor();
+        int number = random.nextInt(100);
         return new Ball(color, number);
     }
 }
