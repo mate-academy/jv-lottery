3a8279ed5a9e8f8e05b3b787b309d87377064773
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-02-08 15:10:13 +0200

    Created enum Color,classes Ball and Lottery , implemented methods in ColorSupplier and Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_QUANTITY = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        for (int i = 1; i <= BALLS_QUANTITY; i++) {
+            System.out.println(lottery.getRandomBall().toString());
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUM = 100;
+    private final ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
+
+    public Ball getRandomBall() {
+        return new Ball(supplier.getRandomColor(), random.nextInt(MAX_NUM));
+    }
+}
