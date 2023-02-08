c2605ec3308387df0b6b8bac1ab9c75e1fdd2f74
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-08 18:01:08 +0200

    create class Ball with @override toString

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Ball {
+    public String color;
+    public int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
