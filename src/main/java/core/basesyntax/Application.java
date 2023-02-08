4cd217c811046acb0b216001c691109c9360284b
kromch <94765048+kromch@users.noreply.github.com>
2023-02-08 11:35:33 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            System.out.println("Got " + lottery.getRandomBall().toString());
+            System.out.println("Got " + lottery.getRandomBall());
         }
     }
 }
