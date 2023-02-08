55e083af68cd610c15fc7cd1dac36c3e700ff8ad
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-08 15:17:25 +0200

    Change final variable name

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private final int indexRandom = random.nextInt(100);
+    private final int INDEX_RANDOM = random.nextInt(100);
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), indexRandom);
+        return new Ball(colorSupplier.getRandomColor(), INDEX_RANDOM);
     }
 }
