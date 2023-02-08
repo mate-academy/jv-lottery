682a3d5dd4d65fda2ccd62cae82d2ff75efb9a17
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-02-08 20:37:39 +0200

    removed final modificator from Ball fields and added variable numberOfClor

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+    private int numberOfColor = random.nextInt(Color.values().length);
 
     public Color getRandomColor() {
-        return Color.values()[random.nextInt(Color.values().length)];
+        return Color.values()[numberOfColor];
     }
 }
