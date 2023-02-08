b695f51f191874a7fac073a4a670b457cdf97983
Maksym <maxim.yashyn@gmail.com>
2023-02-08 20:15:13 +0200

    created getters and setters of fields color and number, implement random to this fields

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Ball {
-    private String color;
-    private int number;
+    private String color = new ColorSupplier().getRandomColor();
+    private int number = new Random().nextInt(100);
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     @Override
     public String toString() {
