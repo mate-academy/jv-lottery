a181cd9a8521cc58d94e8bac2f79aa67128e1844
Mykyta <nikita.nikonets@gmail.com>
2023-02-08 14:11:33 +0200

    small style fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_LIMIT = 100;
     private Random random = new Random();
     private ColorSupplier supplier = new ColorSupplier();
-    private static final int NUMBER_LIMIT = 100;
 
     public Ball getRandomBall() {
         return new Ball(supplier.getRandomColor(), random.nextInt(NUMBER_LIMIT));
