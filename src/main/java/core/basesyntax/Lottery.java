1211c20aacb2f894c95d5bea67fdc635ff5737cf
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-08 12:31:38 +0200

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
-
 import java.util.Random;
 
 public class Lottery {
-
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
