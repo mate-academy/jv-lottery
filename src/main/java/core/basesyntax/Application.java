69ab911b040eec448cf3400e89b899373a8c4905
Oleksandr Koval <0993079960a@gmail.com>
2023-02-08 15:45:23 +0200

    fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         System.out.println(new Lottery().getRandomBall().toString());
+        System.out.println(new Lottery().getRandomBall().toString());
+        System.out.println(new Lottery().getRandomBall().toString());
         // create three balls using class Lottery and print information about them in console
     }
 }
