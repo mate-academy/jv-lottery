cd54170854b3d17d9d03ef3df44b685be3e98d25
Oleksandr Koval <0993079960a@gmail.com>
2023-02-08 15:55:02 +0200

    final_fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    static final int BALLS_AMOUNT = 3;
+
     public static void main(String[] args) {
-        System.out.println(new Lottery().getRandomBall().toString());
-        System.out.println(new Lottery().getRandomBall().toString());
-        System.out.println(new Lottery().getRandomBall().toString());
+        Lottery lottery = new Lottery();
+        for (int i = 1; i <= BALLS_AMOUNT; i++) {
+            System.out.println(lottery.getRandomBall().toString());
+        }
         // create three balls using class Lottery and print information about them in console
     }
 }
