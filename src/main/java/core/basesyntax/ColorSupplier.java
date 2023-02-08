b46aa3a9dd56798216021053f2f5cc1fb0fd76f8
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-08 12:31:00 +0200

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,6 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
     public String getRandomColor() {
-        return Colors.values()[random.nextInt(Colors.values().length)].name();
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
