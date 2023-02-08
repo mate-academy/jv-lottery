ccda2ab89afe52f023742c11f14954591ee65c00
antonsablev <live.incntr@gmail.com>
2023-02-08 13:46:48 +0100

    Fixes after review Change constant names

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Random RANDOM_COLOR = new Random();
+    private static final Random RANDOM = new Random();
 
     public Color getRandomColor() {
-        int index = RANDOM_COLOR.nextInt(Color.values().length);
+        int index = RANDOM.nextInt(Color.values().length);
         return Color.values()[index];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBER_OF_BALLS = 100;
-    private final Random number = new Random();
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), number.nextInt(NUMBER_OF_BALLS));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_OF_BALLS));
     }
 }
