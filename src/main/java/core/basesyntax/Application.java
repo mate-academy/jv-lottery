be93e8a7d0be405eb2bab5cc736319a55424342d
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-02-08 20:51:06 +0200

    Implemented Lottery class and helper classes.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
+    public static final int BALLS_COUNT = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[BALLS_COUNT];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+        System.out.println(Arrays.toString(balls));
     }
 }
