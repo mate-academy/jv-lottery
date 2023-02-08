88ecaf5baedc244cbfe93bf1873804a6a05f2396
Nadiia Huryk <nadia11huryk@gmail.com>
2023-02-08 17:53:31 +0200

    Revert changes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,11 +1,7 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class ColorSupplier {
-    public Color getRandomColor() {
-        Random random = new Random();
-        int index = random.nextInt(Color.values().length);
-        return Color.values()[index];
+    public String getRandomColor() {
+        return null;
     }
 }
