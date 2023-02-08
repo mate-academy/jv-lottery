c9e5ab8f1258bc17b57f31a828a27c766569ac58
Maksym <maxim.yashyn@gmail.com>
2023-02-08 20:33:02 +0200

    implement main method and created three balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery ball = new Lottery();
+       for (int i = 0; i < 3; i++) {
+           System.out.println(ball.getRandomBall());
+       }
     }
 }
