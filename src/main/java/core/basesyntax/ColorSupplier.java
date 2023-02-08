fa35d9a022aa20d78c87450ce5810cecd2530e23
Denys Dominskyi <dominskyj@gmail.com>
2023-02-08 12:27:01 +0200

    1. Removed empty line 2. Made getRandomNumber() private

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,13 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
-    public int getRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(Color.values().length);
+    public String getRandomColor() {
+        return Color.values()[getRandomNumber()].name();
     }
 
-    public String getRandomColor() {
-        return Color.values()[this.getRandomNumber()].name();
+    private int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(Color.values().length);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_RANDOM_NUMBER = 100;
 
-    public int getRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(MAX_RANDOM_NUMBER);
+    public Ball getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor(), getRandomNumber());
     }
 
-    public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), this.getRandomNumber());
+    private int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(MAX_RANDOM_NUMBER);
     }
 }
