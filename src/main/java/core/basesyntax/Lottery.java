71efc1fdee1c818bec4e8980b0ad3d8300baab48
Artem Grunin <artem.grunin.2006@gmail.com>
2023-02-08 16:10:23 +0200

    added magic number in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER_OF_BALLS = 100;
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(new ColorSupplier().getRandomColor(),
+                new Random().nextInt(MAX_NUMBER_OF_BALLS));
     }
 }
