e00a0f5ed63b7e4f907870d5dba3e7098988252c
Nikita <nsalogub96@gmail.com>
2023-02-08 21:30:30 +0200

    Implementation of lottery task(try3)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int limit = 100;
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int LIMIT = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int randomNumber = random.nextInt(limit);
+        int randomNumber = random.nextInt(LIMIT);
         Color randomColor = colorSupplier.getRandomColor();
         return new Ball(randomColor, randomNumber);
     }
