670bac4741f8281947246ea4dd0e0c6688d95e3b
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-08 18:13:16 +0200

    create Lottery class with getRandomBall() method and test program in main

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUMBER_VALUE = 100;
+    public Ball getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER_VALUE));
+    }
+}
