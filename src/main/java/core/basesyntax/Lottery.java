050cef1725090a49026cabbc7889801e576faa6c
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-08 12:52:45 +0200

    Make indexRandom final, change getRandomBall method.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,9 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
+    private final int indexRandom = random.nextInt(100);
 
     public Ball getRandomBall() {
-        int index = random.nextInt(100);
-        Ball ball = new Ball(colorSupplier.getRandomColor(), index);
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), indexRandom);
     }
 }
