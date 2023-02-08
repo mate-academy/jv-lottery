be0f518951263b511a55323ac3496caa770a5d3e
ivan <kiohi2000@gmail.com>
2023-02-08 15:48:01 +0200

    code reduction

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,23 +1,11 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-
-        List<Ball> balls = new ArrayList<>();
-        balls.add(ball1);
-        balls.add(ball2);
-        balls.add(ball3);
-
-        for (Ball ball : balls) {
-            System.out.println(ball);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
