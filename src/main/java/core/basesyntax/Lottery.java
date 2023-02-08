7e0541b5fbbdfc74ddca8d18c5de84f18d61b8c6
Maksym <maxim.yashyn@gmail.com>
2023-02-08 20:38:33 +0200

    refactor getRandomBall method in Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,8 @@ import java.util.Random;
 public class Lottery {
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier randomColor = new ColorSupplier();
-
+        ball.setColor(new ColorSupplier().getRandomColor());
         ball.setNumber(new Random().nextInt(100));
-        ball.setColor(randomColor.getRandomColor());
-
         return ball;
     }
 }
