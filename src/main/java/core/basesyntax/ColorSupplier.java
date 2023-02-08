c7e2bb9187423fac69658d132afcd91f4667328d
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-08 15:12:02 +0200

    Final was added to some variables

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random random = new Random();
+    private final Random random = new Random();
 
     public String getRandomColor() {
         return Color.values()[random.nextInt(Color.values().length)].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(Color.valueOf(colorSupplier.getRandomColor()), random.nextInt(MAX_NUMBER));
