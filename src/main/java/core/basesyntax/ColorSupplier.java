4bd8dabdc8fd23b121917c16e075b26327379cde
“Ymishok321 <kkoiku3@gmail.com>
2023-02-08 14:19:33 +0200

    Renamed Colors.java to Color.java & moved the dependencies to the constructor

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random;
+
+    public ColorSupplier() {
+        this.random = new Random();
+    }
+
     public String getRandomColor() {
-        int randomIndex = new Random().nextInt(Colors.values().length);
-        return Colors.values()[randomIndex].name();
+        int randomIndex = random.nextInt(Color.values().length);
+        return Color.values()[randomIndex].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,15 @@ import java.util.Random;
 
 public class Lottery {
     private static final int RANDOM_LIMIT = 100;
+    private Random random;
+    private ColorSupplier colorSupplier;
+
+    public Lottery() {
+        this.random = new Random();
+        this.colorSupplier = new ColorSupplier();
+    }
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(RANDOM_LIMIT));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(RANDOM_LIMIT));
     }
 }
