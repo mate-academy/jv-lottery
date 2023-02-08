eb30e20cd5214ab7837434147529829dfdeb16ea
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-02-08 21:47:13 +0200

    Implemented solution to homework

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    YELLOW,
+    RED,
+    BLUE,
+    WHITE,
+    GREEN
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int NUMBER_BORDER = 100;
+    private static final Random RANDOM = new Random();
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        return new Ball(COLOR_SUPPLIER.getRandomColor(), RANDOM.nextInt(NUMBER_BORDER));
+    }
+}
