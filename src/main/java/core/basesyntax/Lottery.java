d39ec1505ff04ba8dd79803fbb284dbeafe779f5
yuriy49 <yuriy121991@gmail.com>
2023-02-09 22:01:38 +0200

    add constant Redundant extra variable

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private final Random random = new Random();
-    private final int bound = 100;
+    private static final int BOUND = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int randomBallNumber = random.nextInt(bound);
-        return new Ball(colorSupplier.getRandomColor(),randomBallNumber);
+        return new Ball(colorSupplier.getRandomColor(),random.nextInt(BOUND));
     }
 }
