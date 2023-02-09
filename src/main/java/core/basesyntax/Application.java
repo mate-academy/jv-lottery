50e0b46f4d88f9e1ce827f2737f0eb348fcaf78b
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-09 11:52:52 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class Application {
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
