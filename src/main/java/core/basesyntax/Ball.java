cfc39581aed9f0795416905cec00be6cd0fbd1a5
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-02-09 19:14:36 +0200

    fixed output in method getRandomColor and enum Colors renamed like singular

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 public class Ball {
-    private Colors color;
+    private String  color;
     private int number;
 
-    public Ball(Colors color, int number) {
+    public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int COLOR_COUNT = Colors.values().length;
+    private static final int COLOR_COUNT = Color.values().length;
 
     private final Random random = new Random();
 
-    public Colors getRandomColor() {
+    public String getRandomColor() {
         int index = random.nextInt(COLOR_COUNT);
-        return Colors.values()[index];
+        return Color.values()[index].toString();
     }
 }
