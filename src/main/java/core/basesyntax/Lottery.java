2a63991e89adddaa12595988f5e4075e59853f49
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-02-09 13:38:55 +0200

    Deleted empty lines, added correct access modifiers, initialize Random outside getRandomColor String, changed enum name to be singular.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int BORDER_NUMBER = 100;
-    Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(BORDER_NUMBER));
