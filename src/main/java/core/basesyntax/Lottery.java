255379ac1e782b406cb4e88de643c8629af730ae
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-09 12:17:27 +0200

    create a lottery object before the loop and add modifier

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER_VALUE = 100;
-    Random random = new Random();
+    private final Random random = new Random();
     ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER_VALUE));
     }
