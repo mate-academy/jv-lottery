70860375895a8bdff772202531797e13e32f7848
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-09 20:03:17 +0200

    I hope it's final

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int BALL_QUANTITY = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < BALL_QUANTITY; i++) {
