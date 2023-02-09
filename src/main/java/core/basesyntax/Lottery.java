f84377cfccfcef4bed3d383058a74a815a2857ad
Oleksandr Koval <0993079960a@gmail.com>
2023-02-09 11:39:26 +0200

    fixed_mistakes_2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int MAX_BALLS = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
+
     public Ball getRandomBall() {
 
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALLS));
