f1e45276273e0daa323e948beeb89206ca499457
Trach Vika <vikaa@Oksanas-MacBook-Pro.local>
2023-02-09 13:03:07 +0200

    rename constant fields

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     public String getRandomColor() {
         return Color.values()[new Random().nextInt(Color.values().length)].toString();
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int maxNumber = 100;
+    private final static int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(maxNumber));
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER));
     }
 }
