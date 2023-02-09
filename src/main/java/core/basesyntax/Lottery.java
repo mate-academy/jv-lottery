b226a19ea42ddd8d71f77bad26ae8c6ef32e6c15
Maksym <maxim.yashyn@gmail.com>
2023-02-09 14:11:55 +0200

    renamed constant number in getRandomBall

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        final int MAX_RANDOM_NUMBER = 100;
-        Ball ball = new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_RANDOM_NUMBER));
+        final int number = 100;
+        Ball ball = new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(number));
         return ball;
     }
 }
