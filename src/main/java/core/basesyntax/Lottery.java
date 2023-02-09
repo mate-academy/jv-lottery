6f8f13c243d351ca6bc0158abb7908cbdc997874
Maksym <maxim.yashyn@gmail.com>
2023-02-09 15:02:01 +0200

    redeclaration of constant random number for a ball object

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    final static private int RANDOM_NUMBER = 100;
+
     public Ball getRandomBall() {
-        final int number = 100;
-        Ball ball = new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(number));
+        Ball ball = new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(RANDOM_NUMBER));
         return ball;
     }
 }
