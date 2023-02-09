4c30e07b0bf6e46ee4c33059e15c68dd209f0876
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-02-09 11:31:27 +0200

    Changed naming

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int THREE = 3;
-    private static final int ZERO = 0;
+    private static final int MAX_BALLS = 3;
+    private static final int START_INDEX  = 0;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = ZERO; i < THREE; i++) {
+        for (int i = START_INDEX; i < MAX_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
