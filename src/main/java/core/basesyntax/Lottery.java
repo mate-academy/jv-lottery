0fc3b8995647e91eba9f54e9a790a3643cd0a7e8
Nikolaichuk Artem <tompsoq@gmail.com>
2023-02-09 15:55:19 +0200

    removed empty lines

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     private static final int MAX_RANDOM_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
