ed8b0e0f14cb497863f656581028f70a33942d4f
Oleksandr Lutsenko <sashagrek03@gmail.com>
2023-02-09 11:49:39 +0200

    change the name numberOfColor to random in ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Random getRandomColor = new Random();
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        return Colors.values()[getRandomColor.nextInt(Colors.values().length)].name();
+        return Colors.values()[random.nextInt(Colors.values().length)].name();
     }
 }
