797faaa67b7c844db71197cfc61125d963c33518
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-02-09 17:31:52 +0200

    new requred changes have made

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,10 +4,9 @@ public class Application {
     private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
-        Ball[] balls = new Ball[NUMBER_OF_BALLS];
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
-            System.out.println(balls[i].toString());
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall().toString());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int BOUND = 100;
     private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball newBall = new Ball();
-        newBall.setColor(new ColorSupplier().getRandomColor());
+        newBall.setColor(colorSupplier.getRandomColor());
         newBall.setNumber(random.nextInt(BOUND));
         return newBall;
     }
