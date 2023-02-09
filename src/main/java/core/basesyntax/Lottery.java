2607b6477575ae05a43ed86c7b92ab99c51ddc93
copypasterr <114933128+copypasterr@users.noreply.github.com>
2023-02-09 12:59:49 +0200

    some fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int RANDOM_RANGE_LIMIT = 100;
-    private final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(RANDOM_RANGE_LIMIT));
