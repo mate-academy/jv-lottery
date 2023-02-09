01920819c56f887ae72a664b38c2f2d55d9619d9
Nikita <nsalogub96@gmail.com>
2023-02-09 12:01:40 +0200

    Implementation of lottery task(try5)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        final int BallLimit = 3;
+        final static int BALL_LIMIT = 3;
 
         Lottery lottery = new Lottery();
-        for (int i = 0; i < BallLimit; i++) {
+        for (int i = 0; i < BALL_LIMIT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
