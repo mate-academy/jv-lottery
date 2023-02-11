57bdc4f55b08fafecfc248a88fc9aed3209df168
Yaroslav Kopiyka <19yar96.creedgmail.com>
2023-02-11 14:04:44 +0200

    random as a field

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,12 +1,8 @@
 package core.basesyntax;
-
 import java.util.Random;
-
 public class ColorSupplier {
     private int index;
-
     private final Random random = new Random();
-
     public String getRandomColor() {
         index = random.nextInt(Color.values().length);
         return Color.values()[index].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(),
-                    new Random().nextInt(MAX));
+                    random.nextInt(MAX));
     }
 }
