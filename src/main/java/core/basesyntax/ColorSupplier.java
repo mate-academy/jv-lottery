a9779e0b1a8771c34283dafa8ee8b649b2ccb9cc
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-02-13 19:26:41 +0200

    hw-2-lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
-        return  Color.values()[index].toString();
+        return Color.values()[index].toString();
     }
 }
