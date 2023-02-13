17c060c25ea51e3609d20d5b94059237b8d8ed58
Vitalii Fedychkanych <fraugerschoh@gmail.com>
2023-02-13 16:35:20 +0200

    fixed problems with static

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,37 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Ball {
+    private Color color;
+    private int number;
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String toString() {
+        return "Ball: "
+                + "color - " + getColor() + " |||"
+                + " number - " + getNumber();
+    }
+}
+
+
