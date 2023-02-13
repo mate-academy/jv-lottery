01ff3bfb5c0a5fb3f925234fd81048752e27291c
Serhii Nosulia <sergeynosylya@gmail.com>
2023-02-13 20:59:43 +0200

    added suggested changes

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -14,6 +14,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return color + " " + number;
+        return "this ball is " + color + " and its number is " + number;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,13 +6,13 @@ public class Lottery {
     public static final int LIMIT = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Ball ball = new Ball();
 
-    public String getRandomBall() {
-        int value = random.nextInt(LIMIT);
+    public Ball getRandomBall() {
+        int ballNumber = random.nextInt(LIMIT);
         String color = colorSupplier.getRandomColor();
+        Ball ball = new Ball();
         ball.setColor(color);
-        ball.setNumber(value);
-        return ball.toString();
+        ball.setNumber(ballNumber);
+        return ball;
     }
 }
