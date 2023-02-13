e78343b598b55f55672e7a0027f2e59d69820928
Artem <artempharm83@gmail.com>
2023-02-13 14:24:48 +0200

    add constant for 100(maxValues)  Corrected in class (ColorSupplire)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int numberOfBalls = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < numberOfBalls; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
+    private final int maxValues = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(maxValues));
         return ball;
     }
 }
