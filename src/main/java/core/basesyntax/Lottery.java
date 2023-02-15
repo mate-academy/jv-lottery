ab81ed539ed2bddaba234ed0d03adf060b31fd2c
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-02-15 19:29:37 +0200

    fix name variable, move Random method

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
 
-    private static final int NUMBER_LIMIT  = 100;
+    private static final int NUMBER_LIMIT = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_LIMIT ));
