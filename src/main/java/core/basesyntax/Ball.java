8b4b1fd11cd06c89733f65a1b2761c7cd6632cd6
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-02-15 21:05:27 +0200

    jv-lottery

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+
+public class Ball {
+    private final String color;
+
+    private final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball №" + number + " and color - " + color + ";";
+    }
+}
+
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    private final Random random = new Random();
+
+    private static final int NUMBER_LIMIT = 100;
+
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_LIMIT));
+    }
+}
+
