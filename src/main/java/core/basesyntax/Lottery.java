6027ea6cbf4a5e79884bd53b9348adbfb5fcb8e7
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-02-15 21:15:36 +0200

    fix static variable

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_LIMIT = 100;
 
     private final Random random = new Random();
-
-    private static final int NUMBERLIMIT = 100;
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBERLIMIT));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_LIMIT));
     }
 }
 
