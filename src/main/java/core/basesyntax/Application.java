165db00548d8017be9677e4e19bbd2f81d630e0d
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-02-17 20:32:32 +0100

    first empty line removed, created constant for magic number.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    public static final short MAX_NUMBER_BALLS = 100;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            System.out.println(lottery.getRandomBall(100));
+            System.out.println(lottery.getRandomBall(MAX_NUMBER_BALLS));
         }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     private final Random random = new Random();
 
     public Color getRandomColor() {
