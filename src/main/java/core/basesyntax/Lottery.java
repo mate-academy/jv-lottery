02123f8260ba6223444d51c49815d8acccfb5d09
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2023-02-17 23:58:24 -0500

    create class Lottery

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Ball randomBall = new Ball();
+        randomBall.setColor(new ColorSupplier().getRandomColor());
+        randomBall.setNumber(new Random().nextInt(101));
+
+        return randomBall;
+    }
+}
