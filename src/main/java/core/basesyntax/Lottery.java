2686185a7dba63215f2f7f409d31e083eb17550f
dreo <mishkin878@gmail.com>
2023-02-20 17:15:34 +0200

    finished lesson with mentor's recommendations+kozhukhovsky+BUILD SUCCESS 2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,8 +6,10 @@ public class Lottery {
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ColorSupplier colorSupplier = new ColorSupplier();
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        String randomColor = colorSupplier.getRandomColor();
+        int randomNumber = new Random().nextInt(100);
+        ball.setColor(randomColor);
+        ball.setNumber(randomNumber);
         return ball;
     }
 }
