a371942152df96668f5c268b5ee6c3ba72345523
dreo <mishkin878@gmail.com>
2023-02-20 16:54:33 +0200

    finished lesson with mentor's recommendations+kozhukhovsky

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball first = new Lottery().getRandomBall();
-        Ball second = new Lottery().getRandomBall(); //it is possible to do the same through a cycle
-        Ball third = new Lottery().getRandomBall();
 
-        System.out.println(first.toString());
-        System.out.println(second.toString());
-        System.out.println(third.toString());
+        for (int i = 0; i < 3;i++){
+        System.out.println(new Lottery().getRandomBall());
+        }
     }
 }
