d017291310fb6986e51b479b3df8f0156e9d63de
dreo <mishkin878@gmail.com>
2023-02-20 20:45:00 +0200

    with mentor's recommendations in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private String randomColor = colorSupplier.getRandomColor();
+    private static final int MAX_POSSIBLE_NUMBER = 100;
+    Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        int randomNumber = new Random().nextInt(100);
-        ball.setColor(randomColor);
-        ball.setNumber(randomNumber);
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(MAX_POSSIBLE_NUMBER));
         return ball;
     }
 }
