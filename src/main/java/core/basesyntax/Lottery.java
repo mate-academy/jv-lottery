79ac83a34f9c01e42960e61e7c75e8175db7049b
Maxym Meleshchenko <maximkame00@gmail.com>
2023-02-21 20:20:20 +0200

    Fixed test failure

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_COUNT = 100;
     private final Random random = new Random();
-    private final  ColorSupplier colorSupplier = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
