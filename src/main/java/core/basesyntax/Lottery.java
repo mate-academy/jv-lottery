103da00a9a4be2f9867cd1138100d5658bdd9144
Your Name <a.o.vozniuk@gmail.com>
2023-02-22 15:33:50 +0200

    update after review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         return new Ball(new Random().nextInt(101), ColorSupplier.getRandomColor());
     }
 }
