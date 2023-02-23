4597a80fcf39074f6c28556a3ea011c6678a9660
Alex-Solod <solodyhin_2@icloud.com>
2023-02-23 02:30:31 +0200

    1) created Ball class with the following fields: color and number and override toString() method. 2) created enum with different colors 3) implemented method getRandomColor() in class ColorSupplier that return a random color, based on the enum values by using Random class. 4) created a Lottery class with getRandomBall() method which return the ball with a random color  and a random number (the maximum number: 100) 5) to test the program, in main() method, I created three balls using getRandomBall() method from Lottery class and print information about them in the console.

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,35 @@
+package core.basesyntax;
+
+public class Ball {
+    private Colors color;
+    private int number;
+
+    public Ball(Colors color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = Colors.valueOf(color);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static Colors getRandomColor()  {
+
+        Random random = new Random();
+        int randomColor = random.nextInt(Colors.values().length);
+        return Colors.values()[randomColor];
     }
 }
