8909dbc82426b136530f95ce91c8c0f1fddd25c1
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-02-23 09:58:04 +0200

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
+
 public class Application {
     public static void main(String[] args) {
-        Lottery ball1 = new Lottery();
-        System.out.println(ball1.getRandomBall().toString());
-        Lottery ball2 = new Lottery();
-        System.out.println(ball2.getRandomBall().toString());
-        Lottery ball3 = new Lottery();
-        System.out.println(ball3.getRandomBall().toString());
+        Lottery ball = new Lottery();
+        for(int i = 0; i < 3; i++){
+            System.out.println(ball.getRandomBall().toString());
+        }
     }
 }
