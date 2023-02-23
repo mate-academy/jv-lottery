d273003eb607c11104e87234ce9aa79c3e0c3104
Alex-Solod <solodyhin_2@icloud.com>
2023-02-23 02:43:24 +0200

    Fix: ColorSupplier.java:6:44: Use a single space to separate non-whitespace characters; Ball.java:30:24: '+' should be on a new line; Ball.java:31:42: '+' should be on a new line; Ball.java:32:38: '+' should be on a new line;

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static Colors getRandomColor()  {
+    public static Colors getRandomColor() {
 
         Random random = new Random();
         int randomColor = random.nextInt(Colors.values().length);
