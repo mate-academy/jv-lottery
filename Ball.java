c49866989689153b25fd1b71fb0d95154d68083b
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-02-23 09:58:49 +0200

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,10 +1,17 @@
 package core.basesyntax;
+
 public class Ball {
-    Colors color;
-    int number;
+    private final Color color;
+    private final int number;
+
+    Ball (Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
     @Override
     public String toString() {
         return "Ball " + color + " " + number;
-
     }
 }
+
