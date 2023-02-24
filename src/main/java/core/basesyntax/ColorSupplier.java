beeeef19021ffe2af36285532e4bc4073bb90784
Victor Rudkovsky <vpcomtek@gmail.com>
2023-02-24 17:04:17 +0200

    -- lottery fixed v1.2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private final Random random = new Random();
+
     public String getRandomColor() {
         int i = random.nextInt(Color.values().length);
         return String.valueOf(Color.values()[i]);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int TOP_BOUND_NUMBER = 100;
-    Random random = new Random();
+    private final int TopBoundNumber = 100;
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(TOP_BOUND_NUMBER));
+        ball.setNumber(random.nextInt(TopBoundNumber));
         return ball;
     }
 }
