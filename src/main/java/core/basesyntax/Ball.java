40eb3987c918d95e9a4b37a95c75f38f9f566006
Alex-Solod <solodyhin_2@icloud.com>
2023-02-24 16:52:05 +0200

    Delete empty space in class Ball Rename countNumber to COUNT_NUMBER

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -8,23 +8,6 @@ public class Ball {
         this.color = color;
         this.number = number;
     }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
     @Override
     public String toString() {
         return "Ball {"
