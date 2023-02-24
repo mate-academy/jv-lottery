2c23afb5a14f4cab6be855969993ae460d396294
Your Name <a.o.vozniuk@gmail.com>
2023-02-24 16:14:40 +0200

    update after review

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
+    private Random random = new Random();
+
     public Color getRandomColor() {
         Color[] colorList = Color.values();
-        Random random = new Random();
         return colorList[random.nextInt(colorList.length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
     private final int randomInputTopBound = 101;
+    private Random random = new Random();
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
         return new Ball(random.nextInt(randomInputTopBound), colorSupplier.getRandomColor());
     }
 }
