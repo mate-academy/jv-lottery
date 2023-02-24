b1733ae59b8a9fc1890e4c76eff23d06d93804fd
Alex-Solod <solodyhin_2@icloud.com>
2023-02-24 12:23:08 +0200

    Delete empty space in class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Lottery {
     private static final int  countNumber = 100;
     private static final Random random = new Random();
-
     public static Ball getRandomBall() {
         Color color = ColorSupplier.getRandomColor();
         int number = random.nextInt(countNumber) + 1;
