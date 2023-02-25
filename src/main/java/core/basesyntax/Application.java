ca919e5f77998d96b7d1b544527e574a50c0c74e
Victor Rudkovsky <vpcomtek@gmail.com>
2023-02-25 13:04:53 +0200

    -- lottery fixed v1.5

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
-        final int NUMBER_OF_BALLS = 3;
         Lottery lottery = new Lottery();
         for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
