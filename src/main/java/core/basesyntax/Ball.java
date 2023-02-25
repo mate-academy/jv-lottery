c5bd37575c26ab56255dfa621731de22c27c8116
Your Name <a.o.vozniuk@gmail.com>
2023-02-25 14:28:26 +0200

    updates after review

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -13,11 +13,6 @@ public class Ball {
         return color;
     }
 
-    @Override
-    public String toString() {
-        return "Ball with color " + color + " and number " + number;
-    }
-
     public void setColor(Color color) {
         this.color = color;
     }
@@ -28,5 +23,10 @@ public class Ball {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball with color " + color + " and number " + number;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    private final int randomInputTopBound = 101;
+    private static final int RANDOM_TOP_BOUND = 101;
     private Random random = new Random();
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
-        return new Ball(random.nextInt(randomInputTopBound), colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(RANDOM_TOP_BOUND), colorSupplier.getRandomColor());
     }
 }
