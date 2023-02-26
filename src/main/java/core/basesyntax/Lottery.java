ba83503e4d9d9cd21c7a892b0bd59597f2b02207
Your Name <a.o.vozniuk@gmail.com>
2023-02-26 09:19:20 +0200

    updates after review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final int RANDOM_TOP_BOUND = 101;
     private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(random.nextInt(RANDOM_TOP_BOUND), colorSupplier.getRandomColor());
     }
 }
