98c07aa2ac5ab4d3ba404eb3c1e4c1760734d123
Alex-Solod <solodyhin_2@icloud.com>
2023-02-27 22:52:31 +0200

    add constant - COUNT_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int CountNumber = 100;
+    private static final int COUNT_NUMBER = 100;
     private static final Random random = new Random();
 
     public static Ball getRandomBall() {
         Color color = ColorSupplier.getRandomColor();
-        int number = random.nextInt(CountNumber) + 1;
+        int number = random.nextInt(COUNT_NUMBER) + 1;
         return new Ball(color, number);
     }
 }
