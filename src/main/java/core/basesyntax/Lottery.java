ac21240c2b035f51a07ff57c1c1cb632f73dd7bc
Alex-Solod <solodyhin_2@icloud.com>
2023-02-28 18:00:00 +0200

    add final

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int COUNT_NUMBER = 100;
-    private final Random random = new Random();
-    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int COUNT_NUMBER = 100;
+    private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
