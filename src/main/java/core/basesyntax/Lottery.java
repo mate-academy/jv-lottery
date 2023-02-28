bbb094c3765dadeba272a142c18b7939f7e07f0b
Alex-Solod <solodyhin_2@icloud.com>
2023-02-28 17:42:24 +0200

    add static

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public final int COUNT_NUMBER = 100;
+    private final int COUNT_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
