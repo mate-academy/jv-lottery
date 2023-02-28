b54f01c5f0b4ba56ccce0b13506da71a2440f7d6
Alex-Solod <solodyhin_2@icloud.com>
2023-02-28 15:59:24 +0200

    non static methods

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int COUNT_NUMBER = 100;
-    private static final Random random = new Random();
+    private final int COUNT_NUMBER = 100;
+    private Random random = new Random();
 
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         Color color = ColorSupplier.getRandomColor();
         int number = random.nextInt(COUNT_NUMBER) + 1;
         return new Ball(color, number);
