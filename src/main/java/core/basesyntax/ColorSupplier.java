e7e85422a5061b5364bd74b33e91a8e1c1fcc294
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-02-28 20:05:20 +0200

    added empty line separator between fields and methods and added white space around in method getRandomBall

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
     private final Color[] colors = Color.values();
+
     public String getRandomColor() {
         int i = random.nextInt(colors.length);
         return colors[i].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class Lottery {
     private static final int LOTTERY_MAX_COUNT = 100;
     private final Random random = new Random();
-    public Ball getRandomBall(){
+
+    public Ball getRandomBall() {
         int index = random.nextInt(LOTTERY_MAX_COUNT);
         return new Ball(new ColorSupplier().getRandomColor(), index);
     }
