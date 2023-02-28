72d48b14b5f968f5c07c303c02a3e80b38221782
Alex-Solod <solodyhin_2@icloud.com>
2023-02-28 18:34:50 +0200

    test

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ public class Application {
     public static final Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
-
         for (int i = 1; i <= 3; i++) {
             System.out.println("Ball " + i + ": " + lottery.getRandomBall());
         }
