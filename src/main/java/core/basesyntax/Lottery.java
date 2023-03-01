3d418dda66adfb9cc5320caa6c91fc4b290f8a89
Tania Chayka <opop2125@gmail.com>
2023-03-01 21:12:27 +0200

    added a variable maxNumber

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
+    private int maxNumber = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        int number = random.nextInt(100);
+        int number = random.nextInt(maxNumber);
         ball.setNumber(number);
         Color color = colorSupplier.getRandomColor();
         ball.setColor(color.name());
