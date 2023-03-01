0ab4ad01c679b4ef139bee89bda76b6ae6975982
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-03-01 07:25:20 +0200

    HomeWork Mutaborrr Lottery Refactoring 6
    HomeWork Mutaborrr Lottery Refactoring 6

--- Lottery.java
+++ Lottery.java
@@ -5,13 +5,9 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private final int count = 100;
-
-    public int getRandomNumber() {
-        return random.nextInt(count);
-    }
+    private static final int count = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), getRandomNumber());
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(count));
     }
 }
