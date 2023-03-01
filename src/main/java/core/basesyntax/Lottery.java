ec7c5667f635fa3d285a4b10d2e07aaceef101dc
testSouth <southparaplan@gmail.com>
2023-03-01 20:06:51 +0200

    change random and ColorSupplier as a class field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier=new ColorSupplier();
+    Random random =new Random();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = new Random().nextInt(100);
+        int number = random.nextInt(100);
         Ball ball = new Ball(color, number);
         return ball;
     }
