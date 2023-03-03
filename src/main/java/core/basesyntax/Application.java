2eb426a870c90dc909603a3bff108d116c39ea36
Oles <a.vykhovanok@gmail.com>
2023-03-03 23:37:31 +0200

    fix loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,8 @@ public class Application {
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
         for (int i = 0; i < NUM_OF_GENERATED_BALLS; i++) {
-            Lottery lottery = new Lottery();
             System.out.println(lottery.getRandomBall());
         }
     }
