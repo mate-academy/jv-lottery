a23564cc143555a6c1bca022886db044c8938ad4
Oleks Korsak <korsak.ua@icloud.com>
2023-03-03 10:55:50 +0200

    add maven clear fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
     
     public Ball getRundomBall() {
         Ball ball = new Ball(colorSupplier.getRandomColor().toString(), random.nextInt(100));
