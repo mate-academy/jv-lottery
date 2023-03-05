0ae9a50db83dc24e59abdce55851baef2f453f7a
Volodymyr Petsukh <v.i.petsukh@gmail.com>
2023-03-05 18:11:12 +0200

    solution task lottery edit style

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-    Ball ball1 = Lottery.getRandomBall();
-    Ball ball2 = Lottery.getRandomBall();
-    Ball ball3 = Lottery.getRandomBall();
+        Ball ball1 = Lottery.getRandomBall();
+        Ball ball2 = Lottery.getRandomBall();
+        Ball ball3 = Lottery.getRandomBall();
 
         System.out.println(ball1);
         System.out.println(ball2);
