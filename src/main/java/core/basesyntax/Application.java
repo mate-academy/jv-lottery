97e98f01582bc614ef07d2c4f5f2e0871388cbcf
Volodymyr Petsukh <v.i.petsukh@gmail.com>
2023-03-05 18:05:45 +0200

    solution task lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Application {
     public static void main(String[] args) {
     Ball ball1 = Lottery.getRandomBall();
     Ball ball2 = Lottery.getRandomBall();
     Ball ball3 = Lottery.getRandomBall();
+
         System.out.println(ball1);
         System.out.println(ball2);
         System.out.println(ball3);
