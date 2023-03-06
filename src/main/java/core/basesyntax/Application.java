7b980f56d3b39158bf41707ce1bba9454f84de99
Oles <a.vykhovanok@gmail.com>
2023-03-06 13:28:51 +0200

    fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ public class Application {
     private static final int NUM_OF_GENERATED_BALLS = 3;
 
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
         for (int i = 0; i < NUM_OF_GENERATED_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
