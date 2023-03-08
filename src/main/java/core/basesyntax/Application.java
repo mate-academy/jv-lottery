f17fc4280e668dce0ab1f377628d9fa8645d9077
Nikita Sachik <nikitasachik@gmail.com>
2023-03-08 09:48:58 +0200

    Check debuger

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,16 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        String s = "Try";
+
+        System.out.println(s);
+        s = "Not";
+        System.out.println(s);
+        int x = 5;
+        x++;
+        x++;
+        x++;
+
+
     }
 }
