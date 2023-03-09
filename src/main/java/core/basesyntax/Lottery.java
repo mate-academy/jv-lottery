c71ffd9627786541805c50dd6df3b07769008442
Oleks Korsak <125895224+okorsak@users.noreply.github.com>
2023-03-09 13:48:47 +0200

    Update src/main/java/core/basesyntax/Lottery.java
    Co-authored-by: Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private final int bound = 100;
+    private static final int MAX_NUMBER = 100;
     
     public Ball getRundomBall() {
         return new Ball(colorSupplier.getRandomColor().toString(),
