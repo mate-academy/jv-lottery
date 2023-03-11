f804f3f2b8180b1dbb456b25ab392f2ebaf6ccf8
Olga M <axiomkaonline@gmail.com>
2023-03-11 19:39:26 +0200

    added enum Color. added class Ball. added class Lottery. created method getRandomBall. created method getRandomColor. added class ColorSuplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
+
 }
+
