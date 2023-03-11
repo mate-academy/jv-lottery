3c5edddf074005dcf9dc6824dc4dcfa3e39fc0ff
Ihor Tkachuk <iono19031@gmail.com>
2023-03-11 16:41:07 +0200

    implemented main() method and added a constant NUMBER_OF_BALLS

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        // Creates three balls using class Lottery and print information about them in console
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            Lottery lotteryBall = new Lottery();
+            System.out.println(lotteryBall.getRandomBall());
+        }
     }
 }
