d972a45c38782d7761a8f3de91570eb0011adf7c
Ihor Tkachuk <iono19031@gmail.com>
2023-03-12 14:57:01 +0200

    created Lottery instance outside loop

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_OF_BOUND = 101;
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(101));
+        ball.setNumber(new Random().nextInt(MAX_OF_BOUND));
         return ball;
     }
 }
