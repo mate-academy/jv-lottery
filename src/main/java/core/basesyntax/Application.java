d9df45a5638286e3e664f3988f48e856a3ea5c23
Ihor Tkachuk <iono19031@gmail.com>
2023-03-12 14:58:31 +0200

    added a field MAX_OF_BOUND

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,9 +4,9 @@ public class Application {
     private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
+        Lottery lotteryBall = new Lottery();
         // Creates three balls using class Lottery and print information about them in console
         for (int i = 0; i < NUMBER_OF_BALLS; i++) {
-            Lottery lotteryBall = new Lottery();
             System.out.println(lotteryBall.getRandomBall());
         }
     }
