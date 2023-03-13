d459382713c1531efd85be7e1d3e90c002c7bbd4
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-13 15:58:37 +0300

    BUILD SUCCESS

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    private String color;
-    private int number;
+    final private String color;
+    final private int number;
 
     public Ball(String color, int number) {
         this.color = color;
