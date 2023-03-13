cfa76506824d115e29c8a2e2e5d2280d9fbf1a93
Ihor Tkachuk <iono19031@gmail.com>
2023-03-13 16:55:11 +0200

    added a field random

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
     public Color getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index];
     }
 }
