4df65ac273baa2819e1c2333da89041797499b50
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-14 06:20:39 +0300

    Changed magic number. Made Random class-level fields.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+    private static Random random = new Random();
+    private static ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
     }
 }
