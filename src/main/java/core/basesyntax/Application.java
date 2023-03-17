fc7b548c72bf18e7725848a6995c12aedba5c35c
Nataliia Valiiaeva <nataliyavalyaeva1@gmail.com>
2023-03-17 13:13:01 +0300

    Added Lottery, Colors, Ball classes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Ball ball1 = new Lottery().getRandomBall();
+        Ball ball2 = new Lottery().getRandomBall();
+        Ball ball3 = new Lottery().getRandomBall();
+        System.out.println(ball3);
+        System.out.println(ball2);
+        System.out.println(ball1);
         // create three balls using class Lottery and print information about them in console
     }
 }
---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class Ball {
+    private Colors color;
+    private int number;
+    public Ball (Colors color, int number){
+        this.color = color;
+        this.number = number;
+    }
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color=" + color +
+                ", number=" + number +
+                '}';
+    }
+}
