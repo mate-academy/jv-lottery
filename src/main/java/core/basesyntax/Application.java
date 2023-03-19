d49642eb5d30c35d53ee4fdc897353a6612156bc
Taras Machyta <taras.machyta.sa@gmail.com>
2023-03-19 15:27:34 +0200

    Update failures.
    Create new enum singular.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-
-        System.out.println("Ball 1: " + ball1);
-        System.out.println("Ball 2: " + ball2);
-        System.out.println("Ball 3: " + ball3);
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < 3; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println("Ball " + (i + 1) + ": " + balls[i]);
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends ColorSupplier {
+    public static final int MY_CONSTANT = 100;
+    private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+
     public static Ball getRandomBall() {
-        Random random = new Random();
-        int number = random.nextInt(100);
-        String color = String.valueOf(ColorSupplier.getRandomColor());
+        int number = random.nextInt(MY_CONSTANT);
+        String color = String.valueOf(colorSupplier.getRandomColor());
         return new Ball(color, number);
     }
 }
