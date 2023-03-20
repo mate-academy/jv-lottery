e21ef093c1b639d5cb4cf94605b14ec2fb864fd7
rus1980707 <rus1980707@gmail.com>
2023-03-20 14:39:06 +0200

    HW-2 Lottery all changes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         final Ball[] balls = new Ball[3];
-        Lottery lottery = new Lottery();
+        final Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int lOT = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    static final int lOT = 100;
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(lOT));
     }
