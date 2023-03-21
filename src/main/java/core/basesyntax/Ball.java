549ed62409aec04c9836e4b0b1c1e39b0c3b7f8c
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-03-21 15:02:37 +0200

    Add print information about ball (color number) in the console

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,4 +1,27 @@
 package core.basesyntax;
 
 public class Ball {
+    private String color;
+    private int number;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static String getRandomBall(){
+    public static String getRandomBall() {
         Ball ball = new Ball();
         ColorSupplier color = new ColorSupplier();
-        ball.color = color.getRandomColor();
+        ball.setColor(color.getRandomColor());
         Random random = new Random();
-        ball.number = random.nextInt(100);
-        return "Number Ball: " + ball.number + " Color Ball: " + ball.color;
+        ball.setNumber(random.nextInt(100));
+        return "Number Ball: " + ball.getNumber() + " Color Ball: " + ball.getColor();
     }
 }
