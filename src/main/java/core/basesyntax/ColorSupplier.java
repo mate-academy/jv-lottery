2971db8793860bf8cce735b4df8583f177563e4f
maksigors <maksigors@gmail.com>
2023-03-21 15:28:34 +0200

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class ColorSupplier {
     public Color getRandomColor() {
         Random rand = new Random();
-        int indexColor = rand.nextInt(Color.values().length
+        int indexColor = rand.nextInt(Color.values().length);
         return Color.values()[indexColor];
     }
 }
