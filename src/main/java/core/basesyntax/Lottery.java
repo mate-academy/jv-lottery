ee2617520dfeedc9fa6e369e8120ea38928f170f
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-03-21 14:51:12 +0200

    Add metod getRandomBall

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,4 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
+    public static String getRandomBall(){
+        Ball ball = new Ball();
+        ColorSupplier color = new ColorSupplier();
+        ball.color = color.getRandomColor();
+        Random random = new Random();
+        ball.number = random.nextInt(100);
+        return "Number Ball: " + ball.number + " Color Ball: " + ball.color;
+    }
 }
