04846ff61dd512cd38257793c96b1a1712e0129c
Oleksandr Kobelchuk <Kobasanyok@icloud.com>
2023-03-24 21:54:03 +0200

    fixed 2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    int index = new Random().nextInt(Color.values().length);
+    private int index = new Random().nextInt(Color.values().length);
+
     public String getRandomColor() {
         return Color.values()[index].name();
     }
