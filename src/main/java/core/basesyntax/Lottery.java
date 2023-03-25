2640910683d7c7be303a2d49c81257589878c834
z1row <pmeysarosh2004@gmail.com>
2023-03-25 22:26:20 +0100

    make variables private

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int number = new Random().nextInt(100);
         return new Ball(colorSupplier.getRandomColor(), number);
