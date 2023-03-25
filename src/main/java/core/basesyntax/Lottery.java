a5a5a8e6c84242a4745b44ddfcd5f82de0022438
z1row <pmeysarosh2004@gmail.com>
2023-03-25 22:23:42 +0100

    make Random class variable

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    ColorSupplier colorSupplier = new ColorSupplier();
     public Ball getRandomBall() {
         int number = new Random().nextInt(100);
-        return new Ball(new ColorSupplier().getRandomColor(), number);
+        return new Ball(colorSupplier.getRandomColor(), number);
     }
 }
