70fc196f1cbd14d485b8f51332727077ffb8232f
z1row <pmeysarosh2004@gmail.com>
2023-03-26 19:03:39 +0200

    made loop for balls[]

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = {lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
         for (Ball ball : balls) {
             System.out.println(ball);
         }
