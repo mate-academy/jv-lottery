a5c480bb5ba8883c87ad25641486c9a216f2dd38
Artem Kostenko <a_kostenco@yahoo.com>
2023-03-26 22:05:17 +0300

    Completed homework "Lottery", ready to review

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(new Random().nextInt(MAX_NUMBER));
+        return ball;
+    }
+}
