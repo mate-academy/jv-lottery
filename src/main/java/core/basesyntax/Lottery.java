24c8beca8a6a4d7b7c897732e4f8e5a332efa77b
Artem <artemenko.as@gmail.com>
2023-03-30 22:56:30 +0300

    Added: private fields ColorSupplier, Random. And ballMaxNum now is a constant value. Added Lottery constructor. getRandomBallNumber transferred from Ball Class.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,13 +1,23 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
+    private final ColorSupplier colorSupplier;
+    private final Random random;
+    private final int ballMaxNum = 100;
+
+    public Lottery() {
+        this.colorSupplier = new ColorSupplier();
+        this.random = new Random();
+    }
+
+    private int getRandomBallNumber() {
+        return random.nextInt(ballMaxNum);
+    }
+
     public String getRandomBall() {
-        Ball ball = new Ball();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String randomColor = colorSupplier.getRandomColor().toString();
-        int randomNum = ball.getRandomNumber();
-        ball.setColor(randomColor);
-        ball.setNumber(randomNum);
-        return ball.getColor() + " " + ball.getNumber();
+        Ball ball = new Ball(colorSupplier.getRandomColor(random), getRandomBallNumber());
+        return ball.toString();
     }
 }
