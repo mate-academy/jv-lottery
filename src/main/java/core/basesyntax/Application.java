887df4350c3e9515f6a3e3c1bce40a54bd27ba0a
Artem <artemenko.as@gmail.com>
2023-03-31 16:50:38 +0300

    renamed final int ballCount to static final int BALL_COUNT as it should be for constant fields. And move this field before psvm.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    static final int BALL_COUNT = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int BALL_COUNT = 3;
         for (int i = 0; i < BALL_COUNT; i++) {
             System.out.println(lottery.getRandomBall());
         }
