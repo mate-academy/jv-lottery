19ac159817f4146ffb88f39a7d9c0fb65ba9191a
Artem <artemenko.as@gmail.com>
2023-03-31 16:46:52 +0300

    renamed final int ballCount to final int BALL_COUNT as it should be for constant fields.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int ballCount = 3;
-        for (int i = 0; i < ballCount; i++) {
+        final int BALL_COUNT = 3;
+        for (int i = 0; i < BALL_COUNT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
