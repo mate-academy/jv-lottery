53bba77794099787117747a503311866b478917a
Artem <artemenko.as@gmail.com>
2023-03-31 22:42:33 +0300

    private final Random random = new Random(); is now belong to ColorSupplier class;

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public Color getRandomColor(Random random) {
+    private final Random random = new Random();
+
+    public Color getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index];
     }
