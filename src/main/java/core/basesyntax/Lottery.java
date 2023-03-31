c5d28017e4515ffc5ed31e7534e3af0270d9bb72
Vladyslav <vladyslavsolianyk@gmail.com>
2023-03-31 16:08:17 +0300

    created two classes and one enam and also implemented the necessary methods in these classes

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAXIMUM_POSSIBLE_NUMBER = 100;
+
+    public Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Random random = new Random();
+
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAXIMUM_POSSIBLE_NUMBER));
+    }
+}
