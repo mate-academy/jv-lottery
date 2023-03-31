ede257057eb463cacb1e0218df555e303d63ed4f
Artem <artemenko.as@gmail.com>
2023-03-31 22:41:00 +0300

    Changed BALL_COUNT: was -> static final int BALL_COUNT = 3; now -> private static final int BALL_COUNT = 3;

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Application {
-    static final int BALL_COUNT = 3;
+    private static final int BALL_COUNT = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
