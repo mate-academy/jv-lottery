290cbffbf508543067ff4cd7dcb8da348bc877eb
Artem <artemenko.as@gmail.com>
2023-03-31 22:46:35 +0300

    private final int ballMaxNum is now -> private static final int BALL_MAX_NUM = 100; public method getRandomBall is now placed before private method getRandomBallNumber.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,20 +3,20 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_MAX_NUM = 100;
     private final ColorSupplier colorSupplier;
     private final Random random;
-    private final int ballMaxNum = 100;
 
     public Lottery() {
         this.colorSupplier = new ColorSupplier();
         this.random = new Random();
     }
 
-    private int getRandomBallNumber() {
-        return random.nextInt(ballMaxNum);
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), getRandomBallNumber());
     }
 
-    public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(random), getRandomBallNumber());
+    private int getRandomBallNumber() {
+        return random.nextInt(BALL_MAX_NUM);
     }
 }
