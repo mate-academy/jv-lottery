9d6ab1feb4e9f29c12dc163b17c904afda64bdb2
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-04-02 22:36:13 +0300

    added fixed mvn clean

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return String.valueOf(Colors.values()[index]);
+        int index = new Random().nextInt(Color.values().length);
+        return String.valueOf(Color.values()[random.nextInt(Color.values().length)]);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static Random random = new Random();
+
     public static String getRandomBall() {
         Ball ball = new Ball();
         ColorSupplier color = new ColorSupplier();
         ball.setColor(color.getRandomColor());
-        Random random = new Random();
         ball.setNumber(random.nextInt(100));
-        return "Number Ball: " + ball.getNumber() + " Color Ball: " + ball.getColor();
+        return ball.toString();
     }
 }
