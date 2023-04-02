f0cb66e3ef3f7b218158d4a32f6d72bdb9bbe255
avereshchynska <aleks.vereshchynska@gmail.com>
2023-04-02 19:32:49 +0200

    jv-lottery solution

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    WHITE,
+    GRAY,
+    BLACK
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery extends Ball{
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setNumber(random.nextInt(100));
+        ball.setColor(colorSupplier.getRandomColor());
+        return ball;
+    }
+}
