b6a0fb0e86615784ec8e927ac4d9a39c66a73590
Sieryi D <hunterst@ukr.net>
2023-04-04 01:35:47 +0300

    added-write-constant-toUPPERCASE

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private final int maxNumber = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(maxNumber));
+        ball.setNumber(random.nextInt(MAX_NUMBER));
         ball.getNumber();
         ball.setColor(colorSupplier.getRandomColor());
         return ball;
