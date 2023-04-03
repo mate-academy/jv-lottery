5ab4d4aa2a213e628c65ba0f130137e4362af1d9
Stipaxa <petrivs572@gmail.com>
2023-04-03 22:09:34 +0300

    Changed ColorSupplier class. Replaced method for getting String representation of enum constants. Extract number magic balls as constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        final int COUNT_BALLS = 3;
         Lottery lottery = new Lottery();
-        final Ball[] magicBalls = new Ball[3];
+        final Ball[] magicBalls = new Ball[COUNT_BALLS];
         for (int i = 0; i < magicBalls.length; i++) {
             magicBalls[i] = lottery.getRandomBall();
             System.out.println(magicBalls[i]);
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,6 +7,6 @@ public class ColorSupplier {
 
     public String getRandomColor() {
         Color[] colors = Color.values();
-        return colors[random.nextInt(colors.length)].toString();
+        return colors[random.nextInt(colors.length)].name();
     }
 }
