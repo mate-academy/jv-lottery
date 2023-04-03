8740ae59ef2cac263922911070b06f69914add5d
Stipaxa <petrivs572@gmail.com>
2023-04-03 19:01:50 +0300

    Fix array with magicBalls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,11 +4,10 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[500];
-
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = lottery.getRandomBall();
-            System.out.println(balls[i]);
+        final Ball[] magicBalls = new Ball[3];
+        for (int i = 0; i < magicBalls.length; i++) {
+            magicBalls[i] = lottery.getRandomBall();
+            System.out.println(magicBalls[i]);
         }
     }
 }
