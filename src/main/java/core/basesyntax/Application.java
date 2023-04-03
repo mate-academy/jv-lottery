4f908d5cd857f2cecaaf7e46ab713e086046add8
Stipaxa <petrivs572@gmail.com>
2023-04-03 22:19:19 +0300

    Rename const

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    static final int COUNT_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        final int COUNT_BALLS = 3;
         Lottery lottery = new Lottery();
         final Ball[] magicBalls = new Ball[COUNT_BALLS];
         for (int i = 0; i < magicBalls.length; i++) {
