3b67992b3db638534f01fe9683c7a2bfedbedf84
inbox-reva <inbox.reva@gmail.com>
2023-04-05 20:38:34 +0300

    make Random class field adn fix Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random rundom = new Random();
 
     public Ball getRandomBall() {
-        Color color = new ColorSupplier().getRandomColor();
-        int number = new Random().nextInt(100);
+        Color color = colorSupplier.getRandomColor();
+        int number = rundom.nextInt(100);
         return new Ball(color, number);
     }
 }
