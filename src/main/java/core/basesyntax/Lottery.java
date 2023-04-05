48b57f0912a8d38d24826138695134937778b63e
inbox-reva <inbox.reva@gmail.com>
2023-04-05 20:46:05 +0300

    avoid magic numberss

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int COUNT = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random rundom = new Random();
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int number = rundom.nextInt(100);
+        int number = rundom.nextInt(COUNT);
         return new Ball(color, number);
     }
 }
