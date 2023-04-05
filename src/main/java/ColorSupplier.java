a6fdb1b7830d74306ae92c716271c8a55d39fdde
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-05 20:42:33 +0200

    RANDOM changed to random

--- src/main/java/ColorSupplier.java
+++ src/main/java/ColorSupplier.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 public class ColorSupplier {
-    private static final Random RANDOM = new Random();
+    private static final Random random = new Random();
 
     public static Color getRandomColor(){
         Color[] someColor = Color.values();
-        int index =  RANDOM.nextInt(Color.values().length);
+        int index =  random.nextInt(Color.values().length);
         return Color.values()[index];
     }
 }
--- src/main/java/Lottery.java
+++ src/main/java/Lottery.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 public class Lottery {
-    private final Random RANDOM = new Random();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Color color = ColorSupplier.getRandomColor();
-        int number = RANDOM.nextInt(100) + 1;
+        int number = random.nextInt(100) + 1;
         return new Ball(color.name(), number);
     }
 }
