a5c919615dee52a036b5a945f838c3a05a9f3b30
Vladyslav Dovhopolyi <dovhopolii@gmail.com>
2023-04-05 19:59:05 +0200

    3 changes on constant number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 
 public class Main {
+    private static final int NUM_BALLS = 3;
+
     public static void main (String[] args){
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUM_BALLS; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
