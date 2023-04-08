293c6846b8927008b6cb2e8c153c358e811b1c9e
Taras Zakala <zakalataras4@gmail.com>
2023-04-08 12:57:50 +0300

    use for loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,11 +4,9 @@ import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
-        Lottery lottery = new Lottery(colorSupplier, random);
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
+        Lottery lottery = new Lottery(new ColorSupplier(), new Random());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
