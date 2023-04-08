5f7a50b38b292cc17801649b9cd4f16adeb38ce7
Vasya <xaski514@gmail.com>
2023-04-08 14:43:49 +0300

    jv-implement lottery magic number fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int maxCount = 3;
+
     public static void main(String[] args) {
+
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < maxCount; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println("Ball " + i + " : " + ball);
         }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final int maxValue = 100;
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int value = random.nextInt(100);
+        int value = random.nextInt(maxValue);
         return new Ball(randomColor, value);
     }
 }
