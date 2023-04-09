62b4e80d85d1f1e566deb1f51bf82f5607ab5eaa
Ros <rospsix@gmail.com>
2023-04-09 20:31:56 +0100

    Color Supplier class which get values from ENUM class and give random value

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(DifferentColor.values().length);
+        return DifferentColor.values()[index].toString();
     }
 }
