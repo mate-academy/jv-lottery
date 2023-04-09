74d82fb174862d4f2d11e90ace6025b7dd3d2b27
Ros <rospsix@gmail.com>
2023-04-09 20:51:22 +0100

    in this class, I created three new balls to test the functionality

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball ball1 = Lottery.getRandomBall();
+        Ball ball2 = Lottery.getRandomBall();
+        Ball ball3 = Lottery.getRandomBall();
+
+        System.out.println("Number : " + ball1.getNumbers() + ", " + "Color : " + ball1.getColor());
+        System.out.println("Number : " + ball2.getNumbers() + ", " + "Color : " + ball2.getColor());
+        System.out.println("Number : " + ball3.getNumbers() + ", " + "Color : " + ball3.getColor());
     }
 }
