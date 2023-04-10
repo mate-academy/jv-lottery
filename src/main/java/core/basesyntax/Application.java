d5b6105366298261c0c9fe5f0a94866357412e51
Taras Zakala <zakalataras4@gmail.com>
2023-04-10 11:32:15 +0300

    made a constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Application {
+    private static final int NUMBERS_OF_BALL = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery(new ColorSupplier(), new Random());
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBERS_OF_BALL; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
