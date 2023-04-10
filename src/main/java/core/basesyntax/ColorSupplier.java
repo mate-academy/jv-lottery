89716f172b181319c0a5746007107edf39939a04
Ros <rospsix@gmail.com>
2023-04-10 20:26:17 +0100

    fixed checklist(I hope :)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Random random = new Random();
-    private static final Color[] colors = Color.values();
+    private final Random random = new Random();
+    private final Color[] colors = Color.values();
 
     public String getRandomColor() {
         int index = random.nextInt(colors.length);
-        return colors[index].toString();
+        return colors[index].name();
     }
 }
