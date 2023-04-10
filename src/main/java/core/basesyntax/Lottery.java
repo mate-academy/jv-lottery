f8ef480f8f59d263a36b3ea3e107e78d24b33d2b
avereshchynska <aleks.vereshchynska@gmail.com>
2023-04-10 16:43:52 +0200

    jv-lottery solution

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery extends Ball {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private int CONSTANT_CASE = 100;
+    private int MAX_POSSIBLE = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(CONSTANT_CASE));
+        ball.setNumber(random.nextInt(MAX_POSSIBLE));
         ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
