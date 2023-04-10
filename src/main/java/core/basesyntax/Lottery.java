32123823d11781596d37f8b1aa5ef5fdace75918
Ros <rospsix@gmail.com>
2023-04-10 20:23:52 +0100

    delete static.After had an error "Abbreviation in name 'MAX_NUMBER' must contain no more than '2' consecutive capital letters. [AbbreviationAsWordInName]",and rename this variable to maxNumber.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_NUMBER = 100;
-    private static final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final Random random = new Random();
 
-    public static Ball getRandomBall() {
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
+    public Ball getRandomBall() {
+        final int maxNumber = 100;
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(MAX_NUMBER) + 1;
+        int number = random.nextInt(maxNumber) + 1;
         return new Ball(color, number);
     }
 }
