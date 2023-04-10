986cf16aea20b3f97814f9d7e83fedcb34b5e815
Ros <rospsix@gmail.com>
2023-04-10 20:26:45 +0100

    delete array.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
-
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = Lottery.getRandomBall();
-            System.out.println(balls[i].toString());
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
         }
     }
 }
