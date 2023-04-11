0e8f632f56bb643e59bfe534ec1fcfaee100e469
Ros <rospsix@gmail.com>
2023-04-11 12:27:56 +0100

    make MAX_NUMBER static final,and use CONSTANT_CASE

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
+    static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        final int maxNumber = 100;
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(maxNumber) + 1;
+        int number = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(color, number);
     }
 }
