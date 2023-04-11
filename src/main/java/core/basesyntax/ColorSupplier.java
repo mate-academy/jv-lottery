2882dea05e6a08da9e538b70932b1041bad73593
Ros <rospsix@gmail.com>
2023-04-11 15:15:47 +0100

    removed redundant colors.And use method Color.values(); in getRandomColor();

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-    private final Color[] colors = Color.values();
 
     public String getRandomColor() {
+        Color [] colors = Color.values();
         int index = random.nextInt(colors.length);
         return colors[index].name();
     }
