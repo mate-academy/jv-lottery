22aac264a610b79a6bd382bdccf6801e7b36e5c7
Andriy Karpa <McMerfee@i.ua>
2023-04-12 21:05:50 +0300

    edited the comment

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,7 @@ public class Application {
         Ball[] ball = new Ball[3];
         Lottery lottery = new Lottery();
         for (int i = 0; i < ball.length; i++) {
-            ball[i] = lottery.getRandomBall();
-            System.out.println(ball[i]);
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
