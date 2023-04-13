a6014b782d94cfc8f9855aae74c63a3ec35a2ccc
Andrii Khomenko <andpey.homenko@gmail.com>
2023-04-13 21:59:52 +0300

    bug fix according to comment

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    static final int THREE_BALL = 3;
+
     public static void main(String[] args) {
         Lottery lucky = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < THREE_BALL; i++) {
             System.out.println("Ball " + (i + 1) + ": " + lucky.getRandomBall());
         }
-
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
     private ColorSupplier color = new ColorSupplier();
     private Random random = new Random();
-    private int maxVariable = 100;
 
     public Ball getRandomBall() {
-
         Ball ball = new Ball();
         ball.setColor(color.getRandomColor().toString());
-        ball.setNumber(random.nextInt(maxVariable));
+        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
