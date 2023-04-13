2bd79ab7443744ae34e4b6f63eb69668f708bdbc
Andriy Karpa <McMerfee@i.ua>
2023-04-13 20:34:15 +0300

    edit loop FOR

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] ball = new Ball[3];
         Lottery lottery = new Lottery();
-        for (int i = 0; i < ball.length; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
