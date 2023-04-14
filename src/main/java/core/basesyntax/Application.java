ac0b74662655988ecacb03ccb979877023f72a07
Ihor <igor.doggy.2010@icloud.com>
2023-04-14 21:56:22 +0300

    Made Lottery, ColorSupplier and Random class fields. Made number 100 as a constant. Method getRandomColor isnt static. Enum Colors now called "Color".

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,12 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_BALLS = 3;
+    private static final Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
         }
         for (Ball ball : balls) {
             System.out.println(ball.toString());
