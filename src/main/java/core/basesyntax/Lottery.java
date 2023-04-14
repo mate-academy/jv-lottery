8320965cd520864f9ae61637c64080ec560b5b21
Ihor <igor.doggy.2010@icloud.com>
2023-04-14 21:43:53 +0300

    Made ColorSupplier and Random class fields. Made number 100 as a constant. Method getRandomColor isnt static.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
-        String color = ColorSupplier.getRandomColor().name().toLowerCase();
-        int number = new Random().nextInt(100);
+        String color = colorSupplier.getRandomColor().name().toLowerCase();
+        int number = random.nextInt(NUMBER);
         return new Ball(color, number);
     }
 }
