8469c7b5adca51c8b838f756289cbcc251a84480
Nikita <kaptetos@gmail.com>
2023-04-17 20:01:29 +0300

    create Ball class with the following fields: color and number and override toString() method

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return color + " ball, number " + number;
+    }
+}
+
+
+
+
+
+
+
+
+
