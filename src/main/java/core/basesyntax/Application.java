6c1d16265a452a6c442853c726e344cf79dc6b28
MykytaKat <129322235+MykytaKat@users.noreply.github.com>
2023-04-18 18:18:21 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        int numberOfBalls = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < numberOfBalls; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
