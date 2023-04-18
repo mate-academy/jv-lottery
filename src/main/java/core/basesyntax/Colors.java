ba3b225bfbc677a9dea03296e221208e38516f43
MykytaKat <129322235+MykytaKat@users.noreply.github.com>
2023-04-18 18:21:04 +0200

    fixed Colors

--- src/main/java/core/basesyntax/Colors.java
+++ src/main/java/core/basesyntax/Colors.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
 
 public enum Colors {
-    Red,
-    Green,
-    Yellow,
-    Blue,
-    Black,
-    White
-
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    BLACK,
+    WHITE
 }
