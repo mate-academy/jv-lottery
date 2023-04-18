29fb7b8cc86078593ff3bdf557b4836ef6584b7f
Oleh Pryshchepa <poleja122@gmail.com>
2023-04-18 16:05:36 +0300

    processed all comments

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         Ball[] balls = new Ball[]{
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall()
+                lottery.getRandomBall(),
+                lottery.getRandomBall(),
+                lottery.getRandomBall()
         };
         for (Ball ball : balls) {
             System.out.println(ball);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private Ball ball = new Ball(new Random().nextInt(100), new ColorSupplier().getRandomColor());
+
     public Ball getRandomBall() {
-        return new Ball(new Random().nextInt(100), new ColorSupplier().getRandomColor());
+        return ball;
     }
 }
