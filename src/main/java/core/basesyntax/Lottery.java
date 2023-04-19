bdf83b08d3258cc2ad0162c003b5da2b7802c170
VladSchweps <vladschweps1@gmail.com>
2023-04-19 12:25:01 +0300

    Added task solution

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        String color = ColorSupplier.getRandomColor();
+        int number = new Random().nextInt(100) + 1;
+        return new Ball(color, number);
+    }
+
+}
