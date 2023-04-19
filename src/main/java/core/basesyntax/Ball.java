b6afaa4014b7700a5c812a76365da0615a1f0b4a
Chris <tynkastya@gmail.com>
2023-04-19 16:16:51 +0300

    implemented task

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return ("Color: " + getColor() + ", Number: " + getNumber());
+    }
+}
