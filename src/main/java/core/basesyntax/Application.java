cabd01f39a1612b31599e4d1902e3b955efe37a2
VladSchweps <vladschweps1@gmail.com>
2023-04-19 17:57:17 +0300

    Added changes after review and after use maven

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,11 @@ package core.basesyntax;
 
 public class Application {
     private static final int MAX_NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < MAX_NUMBER_OF_BALLS; i++) {
-            System.out.println(lottery.getRandomBall().toString());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_BOUND = 100;
     private final Random random = new Random();
-    private final ColorSupplier ColorSupplier = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(), random.nextInt(NUMBER_BOUND));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_BOUND));
     }
 }
