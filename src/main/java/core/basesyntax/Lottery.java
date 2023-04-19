2282d25f6ade68a4f97062dcf0a99edf50d73f02
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-04-19 16:50:22 +0300

    bug fixed v0.2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
-    ColorSupplier colorSupplier = new ColorSupplier();
-    Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor().toString());
