e48bb1e0713fbbf4e168b9230df080786a36c4a1
MykytaKat <129322235+MykytaKat@users.noreply.github.com>
2023-04-19 14:42:20 +0200

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,11 +2,18 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
+    private final ColorSupplier color;
+    private final Random random;
+    private static final int BOUND = 100;
+
+    public Lottery() {
+        this.color = new ColorSupplier();
+        this.random = new Random();
+    }
+
     public Ball getRandomBall() {
-        Random random = new Random();
-        int value = random.nextInt(100);
-        Ball ball = new Ball(getRandomColor(), value);
-        return ball;
+        int value = random.nextInt(BOUND);
+        return new Ball(color.getRandomColor(), value);
     }
 }
