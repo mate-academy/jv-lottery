2f181cb80a3716a84956220a78192876c9b839b7
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-04-20 22:09:08 +0300

    fix checkstyle mistakes in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final Random random = new Random();
+
     public Ball getRandomBall() {
         ColorSupplier supplier = new ColorSupplier();
         int value = random.nextInt(100);
