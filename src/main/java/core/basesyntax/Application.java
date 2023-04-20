57e190545c7a7ea830b1b422b66ad7fd4327b3d8
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-04-20 16:20:43 +0300

    Implements lottery task

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        System.out.println("First " + new Lottery().getRandomBall().toString());
+        System.out.println("Second " + new Lottery().getRandomBall().toString());
+        System.out.println("Third " + new Lottery().getRandomBall().toString());
     }
 }
