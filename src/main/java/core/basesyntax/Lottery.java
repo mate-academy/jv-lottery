4d0b3e7918eae9a61fb0494f4c3839dbd5807ecd
Petro Kobevko <kobevkopi@gmail.com>
2023-04-20 16:57:18 +0200

    Fixed code according to the mentor's comments

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static ColorSupplier colorSupplier = new ColorSupplier();
     private static Random random = new Random();
-    private static int maximumPossibleNumber = 100;
+    private static final int MAXIMUM_POSSIBLE_NUMBER = 100;
 
     public static Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(maximumPossibleNumber));
+        ball.setNumber(random.nextInt(MAXIMUM_POSSIBLE_NUMBER));
         return ball;
     }
 }
