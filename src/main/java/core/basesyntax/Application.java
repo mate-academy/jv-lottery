4d682e13cbecf29e3d62bb2052cc04277ef007fa
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-04-20 16:30:04 +0300

    improved main() method

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("First " + new Lottery().getRandomBall().toString());
-        System.out.println("Second " + new Lottery().getRandomBall().toString());
-        System.out.println("Third " + new Lottery().getRandomBall().toString());
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i + " " + new Lottery().getRandomBall().toString());
+        }
     }
 }
