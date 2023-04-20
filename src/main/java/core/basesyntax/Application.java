f179b70c9d8cf3f02bae64b3cd99fd796ec08c75
Nikita <kaptetos@gmail.com>
2023-04-20 16:03:56 +0300

    changed as recommended

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery(new ColorSupplier());
         for (int i = 0; i < 3; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println(ball);
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
