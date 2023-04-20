3f50e6715fc1c3f407d624dc4f93df45fa228074
Petro Kobevko <kobevkopi@gmail.com>
2023-04-20 15:43:34 +0200

    Added task realization

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Color[] colors = Color.values();
+        int index = new Random().nextInt(colors.length);
+
+        return colors[index].toString();
     }
 }
