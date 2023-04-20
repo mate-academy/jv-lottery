4dc4a0ff19fa2438f34029c2f79cd6df568f6ffc
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-04-20 17:04:06 +0300

    added static final variable to store the maximum possible value of a number on a ball

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(random), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(random), random.nextInt(MAX_NUMBER));
     }
 }
