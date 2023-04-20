34b063e5b01c9822e145898cb27a7cc2e22abe09
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-04-20 16:32:56 +0300

    improved main() method   - attempt two

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 1; i <= 3; i++) {
-            System.out.println(i + " " + new Lottery().getRandomBall().toString());
+            System.out.println(i + " " + lottery.getRandomBall().toString());
         }
     }
 }
