4c986ab7f64aa168f83b9ed1f45cf48dc33bb079
Vitalii <razzhivinner@gmail.com>
2023-04-20 18:07:33 +0300

    Create three classes and implemented required methods.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery randomBall = new Lottery();
+        System.out.println(randomBall.getRandomBall());
+        System.out.println(randomBall.getRandomBall());
+        System.out.println(randomBall.getRandomBall());
         // create three balls using class Lottery and print information about them in console
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    ORANGE,
+    YELLOW,
+    WHITE,
+    BLUE,
+    GREEN,
+    BLACK,
+    PURPLE
+}
