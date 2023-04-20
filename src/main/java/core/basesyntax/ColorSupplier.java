bc7430b611d9152ad9c8fd7e2c626997e47d7243
Chris <tynkastya@gmail.com>
2023-04-20 14:37:42 +0300

    implemented task 3

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int randomNumberColor = new Random().nextInt(Color.values().length);
+    private final Random random = new Random();
+    private int randomNumberColor = random.nextInt(Color.values().length);
 
     public String getRandomColor() {
         return Color.values()[randomNumberColor].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private Random random = new Random();
+    private static final int MAX_RANGE = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(100));
-        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(random.nextInt(MAX_RANGE));
+        ball.setColor((colorSupplier.getRandomColor()));
         return ball;
     }
 }
