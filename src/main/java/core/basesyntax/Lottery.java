5b31bf0b3d04dacc19d358e04ed4159621ecb74a
Vitalii <razzhivinner@gmail.com>
2023-04-21 20:27:57 +0300

    Create three classes and implemented required methods. Make change and fixed bugs. Fix constant and remove comment

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int restriction = 100;
+    private static final int RESTRICTION = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(restriction));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(RESTRICTION));
     }
 }
