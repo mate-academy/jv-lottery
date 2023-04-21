8f7fec9c0d3a5f4b6f6b498ae0dae3b8f695230c
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-04-21 13:26:16 +0300

    created class Ball, Lottery, enum with Colors implemented their methods Tested program in class Application by creating three random balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,16 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery firstBall = new Lottery();
+        System.out.println("firstBall "
+                + firstBall.getRandomBall());
+
+        Lottery secondBall = new Lottery();
+        System.out.println("secondBall "
+                + secondBall.getRandomBall());
+
+        Lottery thirdBall = new Lottery();
+        System.out.println("thirdBall "
+                + thirdBall.getRandomBall());
     }
 }
