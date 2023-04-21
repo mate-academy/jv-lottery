462e9048611548d5a9be4d70219f4377f5277ad9
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-04-21 10:01:58 +0300

    add Lottery solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         Ball[] balls = new Ball[] {lottery.getRandomBall(), lottery.getRandomBall(),
                 lottery.getRandomBall()};
-        for (int i = 0; i < balls.length; i++) {
-            System.out.println(balls[i]);
-        }
+        Arrays.stream(balls).forEach(System.out::println);
     }
 }
