68b5ce1979d9c4787b03033c8b7305897fd6bf3f
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-04-22 00:37:12 +0300

    Done task jv lottery

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(),random.nextInt(100));
+    }
+}
+
