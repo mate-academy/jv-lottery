26cc08b8c370b98a61f0fd1bf85be5f8e9d24ad1
Viktor Bykh <bykhviktor21@gmail.com>
2023-04-21 19:58:37 +0300

    added solution hw jv-lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int result = new Random().nextInt(DifferentColors.values().length);
+        return String.valueOf(DifferentColors.values()[result]);
     }
 }
---
+++ src/main/java/core/basesyntax/DifferentColors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum DifferentColors {
+    RED,
+    PURPlE,
+    GREEN,
+    YELLOW,
+    BLUE,
+    CYAN,
+    ORANGE,
+    PINK,
+    GRAY,
+    BLACK
+}
