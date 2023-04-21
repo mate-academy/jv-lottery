be38fe07f73be2c1ce4929d1615ed17a28e427d8
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-04-21 19:37:06 +0300

    improve solution jv-lottery 3

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int numberogBound = 100;
+    private final int numberofBound = 100;
     private final Random numberRandom = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor().name(), numberRandom.nextInt(numberogBound));
+        return new Ball(colorSupplier.getRandomColor().name(), numberRandom.nextInt(numberofBound));
     }
 }
