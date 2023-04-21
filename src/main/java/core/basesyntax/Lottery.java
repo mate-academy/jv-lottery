9a3f3d4011ec831ed1f5d8cca68e57c74d371b10
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-04-21 14:59:12 +0300

    Revert "fixed all problems"
    This reverts commit 02ec4fd942a236eca158d32075d4e38786ed8f2f.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final static int MAX_VALUE = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_VALUE));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
     }
 }
