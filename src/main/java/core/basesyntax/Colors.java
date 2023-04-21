0034e06581394dfa6cf9515df142216c61b3398e
Serhii Dudko <nechupara@gmail.com>
2023-04-21 16:15:13 +0300

    implemented lottery feature with generating rando Balls

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+enum Colors {
+    red,
+    blue,
+    white,
+    gray,
+    green,
+    yellow,
+    pink,
+    orange,
+    brown,
+    purple,
+    gold,
+    teal,
+    violet,
+    magenta,
+    cyan,
+    crimson
+}
