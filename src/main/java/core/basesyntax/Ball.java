9817b0746c14db695103d547c94bf44878142ed8
Igor Maksimov <maksigors@gmail.com>
2023-04-22 18:44:54 +0300

    fixed all mistakes

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,18 +1,27 @@
+package core.basesyntax;
+
 public class Ball {
-    Color color;
-    int number;
+    private Color color;
+    private int number;
 
     Ball(Color color, int number) {
         this.color = color;
         this.number = number;
     }
 
+    public Color getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball{"
+                + "color='" + color + '\''
+                + ", number=" + number
+                + '}';
     }
 }
--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,3 +1,5 @@
+package core.basesyntax;
+
 public enum Color {
    RED, GREEN, BLUE, BLACK, WHITE, YELLOW;
 }
