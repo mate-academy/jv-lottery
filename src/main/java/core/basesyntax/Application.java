9643f74758712aee94edf7694ded1069e37b36ce
Mykyta Naumov <nneed4use@gmail.com>
2023-04-24 00:38:45 +0300

    Random balls balling good to me, checkstyle complete

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            Ball randBall = Lottery.getRandomBall();
+            System.out.println(randBall);
+        }
         // create three balls using class Lottery and print information about them in console
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor(),
+                new Random().nextInt(100));
+    }
+}
