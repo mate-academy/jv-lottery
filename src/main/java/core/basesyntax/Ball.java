c1c44542cfdeddbfcbc9ea335422a5b84b0c2bb3
Artem Fomenko <u2glbro@gmail.com>
2023-04-24 18:57:13 +0300

    Made color and number private.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Ball {
-    String color = ColorSupplier.getRandomColor().toString();
-    Random numberRandom = new Random();
-    int number = numberRandom.nextInt(100);
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
 
     @Override
     public String toString() {
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
     public static Ball getRandomBall() {
-        Ball ball = new Ball();
+        Ball ball = new Ball(ColorSupplier.getRandomColor().toString(), new Random().nextInt(100));
         return ball;
     }
 }
