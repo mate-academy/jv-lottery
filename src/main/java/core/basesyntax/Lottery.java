5def9fa92046b61ef5fc7b534c871faafab0c9ff
Igor Maksimov <maksigors@gmail.com>
2023-04-24 16:57:49 +0300

    added fields from method in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int BALL_MAX_NUMBER = 100;
+    private Random rand = new Random();
+    private ColorSupplier randColor = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ColorSupplier randColor = new ColorSupplier();
-        Random rand = new Random();
         int randNumber = (rand.nextInt(BALL_MAX_NUMBER) + 1);
         return new Ball(randColor.getRandomColor(), randNumber);
     }
