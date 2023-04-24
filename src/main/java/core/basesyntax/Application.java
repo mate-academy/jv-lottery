e7291d5db7925aec7bbfdfe460ca30ee266c68d9
Володимир <vova.malyarchuk2@gmail.com>
2023-04-24 11:56:56 +0300

    implemented task

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int Balls_Number = 3;
+
     public static void main(String[] args) {
-        final int Balls_Number = 3;
+
         Lottery lottery = new Lottery();
         for (int i = 0; i < Balls_Number; i++) {
             System.out.println(lottery.getRandomBall());
