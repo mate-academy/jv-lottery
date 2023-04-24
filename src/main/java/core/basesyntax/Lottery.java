52ed9cd7170d5d9c8ac015ed8f5eb99b1475ebe6
Mykyta Naumov <nneed4use@gmail.com>
2023-04-24 15:48:53 +0300

    Random instance declaration has been removed from method to class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static ColorSupplier colorSupplier = new ColorSupplier();
+
+    private static Random random = new Random();
     private static final byte MAX_POSSIBLE_BALL_NUMBER = 100;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(),
-                new Random().nextInt(MAX_POSSIBLE_BALL_NUMBER));
+                random.nextInt(MAX_POSSIBLE_BALL_NUMBER));
     }
 }
