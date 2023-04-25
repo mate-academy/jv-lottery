78c6ee04958daffc8379c828dd7d2f4bb91ce0cc
Artem Fomenko <u2glbro@gmail.com>
2023-04-25 21:18:57 +0300

    hot fix.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random random = new Random();
+    private final Random random = new Random();
 
-    public static Ball getRandomBall() {
-        int number = random.nextInt(100);
-        String color = ColorSupplier.getRandomColor().toString();
+    public Ball getRandomBall() {
+        int maxNumber = 100;
+        int number = random.nextInt(maxNumber);
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color = colorSupplier.getRandomColor().toString();
         return new Ball(color, number);
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+
         for (int i = 0; i < 3; i++) {
-            System.out.println(Lottery.getRandomBall());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
