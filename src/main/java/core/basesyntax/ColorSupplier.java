4e2e5fbb33a0df23644e0f5c30983a6a155ef93c
Olha <olga.olegovna2802@online.ua>
2023-04-26 21:08:37 +0100

    Create a simple lottery that will return balls with a random color and random number

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    private Random random = new Random();
+
+    public Colors getRandomColor() {
+        int index = new Random().nextInt(Colors.values().length);
+        return Colors.values()[index];
+    }
+
+    public int getRandomNumber() {
+        return random.nextInt(100);
     }
 }
