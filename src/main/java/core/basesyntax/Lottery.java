c49ebdc0f5869652e7a86d530f11e9cd3e02752b
OLYA <Ole4kastar79@gmail.com>
2023-05-01 00:17:27 +0300

    fixed EmptyLineSeparator error

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private Random random = new Random();
+
     public Lottery() {
     }
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ColorSupplier colorSuplier = new ColorSupplier();
