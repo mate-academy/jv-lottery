0899a69c5e5a1eb86f3a4205be2f1e0ef4b34ea4
Olha <olga.olegovna2802@online.ua>
2023-04-30 19:40:35 +0100

    Fix the class ColorSupplier Change the class Lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,15 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int NUMBER_OF_BALL = 100;
-    private Random random = new Random();
-
     public Color getRandomColor() {
-        int index = random.nextInt(Color.values().length);
+        int index = new Random().nextInt(Color.values().length);
         return Color.values()[index];
-    }
-
-    public int getRandomNumber() {
-        return random.nextInt(NUMBER_OF_BALL);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,10 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
+    private static final int NUMBER_OF_BALL = 100;
+    private Random random = new Random();
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(ball.getRandomColor());
-        ball.setNumber(ball.getRandomNumber());
+        ball.setNumber(random.nextInt(NUMBER_OF_BALL));
         return ball;
     }
 }
