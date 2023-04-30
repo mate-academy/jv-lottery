e5e8626963b54a9ddbe7a0f0c3d18ec368926730
OLYA <Ole4kastar79@gmail.com>
2023-05-01 00:03:30 +0300

    create class Lottery with method getRandomBall

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private Random random = new Random();
+    public Lottery() {
+    }
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ColorSupplier colorSuplier = new ColorSupplier();
+        ball.color = colorSuplier.getRandomColor();
+        ball.number = random.nextInt(MAX_NUMBER);
+        return ball;
+    }
+}
