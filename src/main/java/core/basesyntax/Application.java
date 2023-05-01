de16f784631376893122358c26ccf3a21cae8aac
OLYA <Ole4kastar79@gmail.com>
2023-05-01 22:50:31 +0300

    fixes in Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,16 +3,10 @@ package core.basesyntax;
 public class Application {
     private static final int BALLS_COUNT = 3;
 
-    public Application() {
-    }
-
     public static void main(String[] args) {
-        Ball[] balls = new Ball[BALLS_COUNT];
         Lottery lottery = new Lottery();
-        for (int i = 0; i < balls.length; ++i) {
-            balls[i] = lottery.getRandomBall();
-            System.out.println(balls[i].toString());
+        for (int i = 0; i <BALLS_COUNT; ++i) {
+            System.out.println(lottery.getRandomBall());
         }
-
     }
 }
