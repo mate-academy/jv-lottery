becec83c42a3a103b1a260574401ff0696237ce9
OLYA <Ole4kastar79@gmail.com>
2023-05-01 23:07:09 +0300

    fixes , make new Random() class field

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,20 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public ColorSupplier() {
-    }
+
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = (new Random()).nextInt(ColorSupplier.Colors.values().length);
-        return ColorSupplier.Colors.values()[index].name();
-    }
-
-    public enum Colors {
-        BLUE,
-        RED,
-        GREEN,
-        PINK,
-        YELLOW
+        int index = random.nextInt(ColorsEnum.Colors.values().length);
+        return  ColorsEnum.Colors.values()[index].name();
     }
 }
 
