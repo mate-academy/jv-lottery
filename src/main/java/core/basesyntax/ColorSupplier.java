0f60637efff20db2c8324ee6778d736eb7f742d5
OLYA <Ole4kastar79@gmail.com>
2023-05-02 21:07:59 +0300

    fixes 2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,14 +1,9 @@
 package core.basesyntax;
-
 import java.util.Random;
-
 public class ColorSupplier {
-
     private final Random random = new Random();
-
     public String getRandomColor() {
-        int index = random.nextInt(ColorsEnum.Colors.values().length);
-        return ColorsEnum.Colors.values()[index].name();
+        int index = random.nextInt(Colors.values().length);
+        return Colors.values()[index].name();
     }
 }
-
