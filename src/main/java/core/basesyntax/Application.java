68cf16a80bd3cf8d70f56be2217a6a28e6772d65
OLYA <Ole4kastar79@gmail.com>
2023-05-02 21:07:42 +0300

    fixes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < BALLS_COUNT; ++i) {
-            System.out.println(lottery.getRandomBall());
+            System.out.println(lottery.getRandomBall().toString());
         }
     }
 }
