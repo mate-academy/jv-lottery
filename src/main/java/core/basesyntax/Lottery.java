d17b333105eba519fd15443dcc18a220b077058f
Artem Fomenko <u2glbro@gmail.com>
2023-05-04 18:48:55 +0300

    Make maxNumber class-level.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private final Random random = new Random();
+    private final int maxNumber = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int maxNumber = 100;
         int number = random.nextInt(maxNumber);
-        ColorSupplier colorSupplier = new ColorSupplier();
+        //ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor().toString();
         return new Ball(color, number);
     }
