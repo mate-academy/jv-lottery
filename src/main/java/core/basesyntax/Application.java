3c42812452884e917356e0ed7783e5676abef5c2
Yura Uniyat <yurauniyat123@gmail.com>
2023-05-04 15:47:10 +0300

    Homework Java/Core/OOP/Lottery. Added changes after new review.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        int numberOfBalls = 3;
-        Lottery[] balls = new Lottery[numberOfBalls];
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery();
-            System.out.println(balls[i].getRandomBall());
-        }
+        Lottery lottery = new Lottery();
+        System.out.println(lottery.getRandomBall());
+        System.out.println(lottery.getRandomBall());
+        System.out.println(lottery.getRandomBall());
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int maxNumber = 100;
+    private static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random randomNumbers = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), randomNumbers.nextInt(maxNumber));
+        return new Ball(colorSupplier.getRandomColor(), randomNumbers.nextInt(MAX_NUMBER));
     }
 }
