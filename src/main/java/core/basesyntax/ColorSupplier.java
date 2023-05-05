21055c17e9f54bfee25290524643f80a8b43aaee
marinakusniruk <Kushnirukmv@gmail.com>
2023-05-05 09:39:48 +0200

    Hometask jv-lottery correct3

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,15 +4,6 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-    private String color;
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    private static final int BAUNDARY_VALUE = 100;
     private final Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(BAUNDARY_VALUE));
         return ball;
     }
 }
