caa67ee0a530c2d911f0ead283a0b358d9224dce
Artem Fomenko <u2glbro@gmail.com>
2023-05-05 18:34:45 +0300

    Make MAX_NUMBER static.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
-    private final int maxNumber = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int number = random.nextInt(maxNumber);
-        //ColorSupplier colorSupplier = new ColorSupplier();
-        String color = colorSupplier.getRandomColor().toString();
+        int number = random.nextInt(MAX_NUMBER);
+        String color = colorSupplier.getRandomColor().name();
         return new Ball(color, number);
     }
 }
