cd45f56ac97ac38f9a4e1ef47d2b1af7d0b9b5f0
Viktor Budivskyi <meyson443@gmail.com>
2023-05-11 18:35:05 +0300

    when committing, issues a warning that the objects color and number must be final

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    private Colors color;
-    private int number;
+    private final Colors color;
+    private final int number;
 
     public Ball(Colors color, int number) {
         this.color = color;
