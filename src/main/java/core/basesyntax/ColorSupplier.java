3fc274493dd3e121e0e2cf5a4cb1b8bf44f08e8b
pochtalon <pochtalon1010@gmail.com>
2023-05-11 07:09:54 +0300

    Added separate line

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
+
     public String getRandomColor() {
         int bound = Colour.values().length;
         int index = random.nextInt(bound);
