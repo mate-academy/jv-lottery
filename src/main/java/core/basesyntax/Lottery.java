87d697cbb9eb729c70a69ab345fca0351e30809c
Viktor Budivskyi <meyson443@gmail.com>
2023-05-12 14:19:23 +0300

    fixed bugs in the code according to the review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier supplier = new ColorSupplier();
-        int number = new Random().nextInt(101);
+        int number = random.nextInt(101);
         return new Ball(supplier.getRandomColor(), number);
     }
 }
