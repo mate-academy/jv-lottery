fd4bc989d602471b9448d5d6f72e18371875da99
pochtalon <pochtalon1010@gmail.com>
2023-05-13 00:25:35 +0300

    Added private access to Random

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int LOTTERY_BOUND = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
-    Random random = new Random();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
