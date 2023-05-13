ceb31470815b5e140a06e457d561fa82795a635e
pochtalon <pochtalon1010@gmail.com>
2023-05-13 23:14:15 +0300

    Added private access to Random

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        int ballCount = 3;
+        final int ballCount = 3;
         for (int i = 0; i < ballCount; i++) {
             System.out.println(lottery.getRandomBall());
         }
