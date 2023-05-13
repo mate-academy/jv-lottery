ea05539a520600baad35d5d5dc02acd176ed5167
Kristina Shpilchyna <kriss.mva@gmail.com>
2023-05-13 21:16:08 +0200

    updated toString with + from new line

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -14,9 +14,9 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
+        return "Ball {"
+                + "color = '" + color + '\''
+                + ", number = " + number +
                 '}';
     }
 }
