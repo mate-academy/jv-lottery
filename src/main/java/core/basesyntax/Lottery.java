b21d13ef6ebf76feabecd1dd528e2e0e24470665
Roman Zatorskyy <rzbehappy@gmail.com>
2023-05-15 14:49:38 +0200

    new solution with fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         Random RANDOM = new Random();
         Color color = ColorSupplier.getRandomColor();
         int number = RANDOM.nextInt(100);
