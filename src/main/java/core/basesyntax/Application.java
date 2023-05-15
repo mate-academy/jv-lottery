7ce3c10a990b51d6b869397cbc6d9df7729733d2
Mykhailo <mykhailokuryk16@gmail.com>
2023-05-15 13:15:31 +0300

    fixed solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBERS_OF_LOTTERY_BALLS = 3;
+
     public static void main(String[] args) {
 
         System.out.println("Play a lottery!");
         System.out.println("You get this balls:");
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println(ball);
+        for (int i = 0; i < NUMBERS_OF_LOTTERY_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
