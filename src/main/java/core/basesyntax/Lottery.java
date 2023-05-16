323cf313656cfad1834d67a602d760a791566a22
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-05-16 14:54:10 +0300

    renamed constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random RANDOM_NUMBER = new Random();
-    private  final ColorSupplier COLOR_OF_BALL = new ColorSupplier();
+    private final Random BALL_RANDOM_NUMBER = new Random();
+    private  final ColorSupplier BALL_RANDOM_COLOR = new ColorSupplier();
 
         public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(RANDOM_NUMBER.nextInt(100));
-        ball.setColor(COLOR_OF_BALL.getRandomColor());
+        ball.setNumber(BALL_RANDOM_NUMBER.nextInt(100));
+        ball.setColor(BALL_RANDOM_COLOR.getRandomColor());
         return ball;
     }
 
