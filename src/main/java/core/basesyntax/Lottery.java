ef1ac5c33c557a5b144d7bfff684542c3698e3b6
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-05-16 14:25:29 +0300

    created  instance ball inside the method, renamed constant of class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random randomNumber = new Random();
+    private final Random RANDOM_NUMBER = new Random();
+    private  final ColorSupplier COLOR_OF_BALL = new ColorSupplier();
 
-    private final ColorSupplier color = new ColorSupplier();
-
-    private final Ball ball = new Ball();
-
-    public Ball getRandomBall() {
-        ball.setNumber(randomNumber.nextInt(100));
-        ball.setColor(color.getRandomColor());
+        public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setNumber(RANDOM_NUMBER.nextInt(100));
+        ball.setColor(COLOR_OF_BALL.getRandomColor());
         return ball;
     }
 
