61924fa33202b0835bbb2d62ea9696a626e0a9b2
Roman Zatorskyy <rzbehappy@gmail.com>
2023-05-16 13:56:48 +0200

    lottery solution

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Ball {
+    private final Color color;
+    private final int number;
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String toString() {
+        return "Ball {" + "color = " + color + ", number = " + number + '}';
+    }
+}
