c519d088fdbd238eb59d20428e3f8b117dcaac70
Kseniia Loginova <ksenixen@gmail.com>
2023-05-17 21:04:50 +0300

    Fixed requested changes after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,15 +1,10 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
-        Ball[] balls = new Ball[3];
         Lottery lottery = new Lottery();
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = lottery.getRandomBall();
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
         }
-        System.out.println(Arrays.toString(balls));
     }
 }
