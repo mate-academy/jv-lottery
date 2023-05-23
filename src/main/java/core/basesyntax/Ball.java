c8c3fb3096b127dba3d43fd5b8aeec37d7534e6c
Vlad Zinevych <vladisznv@gmail.com>
2023-05-23 18:01:40 +0300

    added private status for "color" and "number"

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
+    private final String color;
+    private final int number;
 
     public Ball(String color, int number) {
         this.color = color;
