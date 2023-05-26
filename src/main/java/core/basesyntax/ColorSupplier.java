8b744e9628db80cb8cc3d3a2d66f677dcf9c0e4e
Sashchenko Nikita <saschenko7@gmail.com>
2023-05-26 16:35:50 +0300

    hw done

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
-public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+import java.util.Random;
+
+public class ColorSupplier  {
+    public static Ball.Color getRandomColor() {
+        Ball.Color[] colors = Ball.Color.values();
+        Random random = new Random();
+        int index = random.nextInt(colors.length);
+        return colors[index];
     }
 }
