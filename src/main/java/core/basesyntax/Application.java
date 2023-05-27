7be74e53bfa9d217e495fc0f5aee0ca187742713
happytroya <happytroya2001@gmail.com>
2023-05-27 21:53:23 +0300

    refactored code

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,7 @@ public class Application {
         Lottery lottery = new Lottery();
 
         for (int i = 0; i < 3; i++) {
-            Ball ball  = lottery.getRandomBall();
+            Ball ball = lottery.getRandomBall();
             System.out.println(ball.toString());
         }
     }
--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,13 +1,11 @@
 package core.basesyntax;
 
-
 import java.util.Random;
 
 public class Ball extends ColorSupplier {
     private String color = getRandomColor();
     private int number = new Random().nextInt(100);
-
-
+    
     @Override
     public String toString() {
         return "color: " + this.color + " number: " + this.number;
