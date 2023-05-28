5057e0fdbe8fa654a4f50638862f74bdf3cbbd5b
happytroya <happytroya2001@gmail.com>
2023-05-28 18:40:06 +0300

    codestyle fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
-        Ball newBall = new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
-        return newBall;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
     }
 }
