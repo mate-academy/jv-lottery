59b9cb4b18e3e74b6c04f765b18703d947ebadb1
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-01 20:45:25 +0300

    Replace repeating sout actions with a loop in Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,13 +3,12 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
+        Ball[] ball = new Ball[3];
 
-        System.out.println(ball1.toString());
-        System.out.println(ball2.toString());
-        System.out.println(ball3.toString());
+        for (int i = 0; i < ball.length; i++) {
+            ball[i] = lottery.getRandomBall();
+            System.out.println(ball[i].toString());
+        }
     }
 }
 
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public Colors getRandomColor() {
-        Random random = new Random();
         int randomColorNumber = random.nextInt(Colors.values().length);
         Colors[] colorsList = Colors.values();
 
