4f99934d37a5c4ff50abcee4f25fbeafe2a726cc
LilyH <lilygrebenik@gmail.com>
2023-06-02 21:25:31 +0300

    Static modifiers were changed.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,18 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
-        Ball firstBall = Lottery.getRandomBall();
-        Ball secondBall = Lottery.getRandomBall();
-        Ball thirdBall = Lottery.getRandomBall();
+        Lottery lottery = new Lottery();
 
-        System.out.println(firstBall);
-        System.out.println(secondBall);
-        System.out.println(thirdBall);
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+
+        for(int i = 0; i < balls.length; i++) {
+            balls[i] = new Ball();
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i]);
+        }
+
     }
+
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(ColorSupplier.getRandomColor());
+        ColorSupplier colSup = new ColorSupplier();
+        ball.setColor(colSup.getRandomColor());
         Random rand = new Random();
         ball.setNumber(rand.nextInt(100));
         return ball;
