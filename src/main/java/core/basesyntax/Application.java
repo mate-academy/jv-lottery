363febe00f82a91a397639a4effede700abdc82d
Chorniypistolet <volkivskiy95@gmail.com>
2023-06-04 19:27:57 +0300

    Used a loop in Application class, corected hw

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ballFirst = lottery.getRandomBall();
-        Ball ballSecond = lottery.getRandomBall();
-        Ball ballThird = lottery.getRandomBall();
-        System.out.println("Ball 1 = " + ballFirst);
-        System.out.println("Ball 2 = " + ballSecond);
-        System.out.println("Ball 3 = " + ballThird);
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < 3; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println("Ball: " + balls[i]);
+        }
     }
 }
