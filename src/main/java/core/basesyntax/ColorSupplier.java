154910adacced538c876d08b5d12ba93baa7a6c0
Sashchenko Nikita <saschenko7@gmail.com>
2023-06-05 14:44:24 +0300

    Fix mvn

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     private static final Color[] COLORS = Color.values();
     private static final Random random = new Random();
+
     public static Color getRandomColor() {
         return COLORS[random.nextInt(COLORS.length)];
     }
