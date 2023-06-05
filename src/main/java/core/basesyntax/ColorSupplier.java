714d14ade9045374f4ca913edd7f99fa19d5de7b
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-05 20:54:24 +0300

    create method getRandomColor()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Colors[] colors = Colors.values();
+        int randomNumber = new Random().nextInt(colors.length);
+        return colors[randomNumber].toString();
     }
 }
