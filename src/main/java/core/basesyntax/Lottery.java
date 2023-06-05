c28d4889e11ee728a91d7d250a66ea1a067d0a16
Sergei Kudelin <chertterner686@gmail.com>
2023-06-05 21:52:09 +0300

    second time fixed after mentor check

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_VALUE = 100;
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ColorSupplier colorSupplier = new ColorSupplier();
