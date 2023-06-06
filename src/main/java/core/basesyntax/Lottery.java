fc77e5fbc25729f1d5fc479e5c0986496346ba95
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-06 16:26:02 +0300

    remove static context

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAXIMUM_NUMBER = 100;
-    private static Random random = new Random();
-    private static ColorSupplier colorSupplier = new ColorSupplier();
-    public static Ball getRandomBall() {
+    private final int MAXIMUM_NUMBER = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    public Ball getRandomBall() {
         int randomNumber = random.nextInt(MAXIMUM_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
