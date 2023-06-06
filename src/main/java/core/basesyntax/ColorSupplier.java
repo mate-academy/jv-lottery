f2b10b11e3be092c804627170f4890b002604eb4
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-06 16:27:01 +0300

    move variable from method to class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
     public String getRandomColor() {
         Color[] colors = Color.values();
-        int randomNumber = new Random().nextInt(colors.length);
+        int randomNumber = random.nextInt(colors.length);
         return colors[randomNumber].toString();
     }
 }
