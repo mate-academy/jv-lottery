8c773a4112156afa3700343a960a9eb05d227e58
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-06 22:05:22 +0300

    move variable from method to class and make it static

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+    public static Color[] colors = Color.values();
 
     public String getRandomColor() {
-        Color[] colors = Color.values();
         int randomNumber = random.nextInt(colors.length);
         return colors[randomNumber].toString();
     }
