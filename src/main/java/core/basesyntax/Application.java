3102f98c8e8b97b8829151663320d4dbf4304659
LilyH <lilygrebenik@gmail.com>
2023-06-06 22:42:59 +0300

    Static methods were changed to objects. For loop was used for creating several objects

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,9 +4,7 @@ public class Application {
     private static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
-
         for(int i = 0; i < balls.length; i++) {
             balls[i] = new Ball();
             balls[i] = lottery.getRandomBall();
