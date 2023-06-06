c72116b5e040104d0e81d515ee6797e1f7987acb
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-06 16:23:45 +0300

    moved variables from method to class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAXIMUM_NUMBER = 100;
+    private static Random random = new Random();
+    private static ColorSupplier colorSupplier = new ColorSupplier();
     public static Ball getRandomBall() {
-        int randomNumber = new Random().nextInt(100);
-        return new Ball(new ColorSupplier().getRandomColor(), randomNumber);
+        int randomNumber = random.nextInt(MAXIMUM_NUMBER);
+        return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
 }
