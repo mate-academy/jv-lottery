8b8d664acc03b33c472e9e4fdc006e52f7dfd26c
Chorniypistolet <volkivskiy95@gmail.com>
2023-06-06 21:30:32 +0300

    1) I didn't use static metods 2) I tried to fix random (if i understood your massage correct) 3) I used a loop in Application class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static Color getRandomColor() {
+
+    public Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         return Color.values()[index];
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        Random random = new Random();
         int randomNumber = random.nextInt(100);
-        String randomColor = String.valueOf(ColorSupplier.getRandomColor());
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String randomColor = String.valueOf(colorSupplier.getRandomColor());
         return new Ball(randomColor, randomNumber);
     }
 }
