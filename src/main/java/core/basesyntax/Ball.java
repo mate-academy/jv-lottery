48017ce38714c309ad564f381e6367724ebc4a26
Vitalii <stelmaschyk.vg@gmail.com>
2023-06-09 13:16:42 +0300

    added fixes about checkstyle, and correct main class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Ball extends ColorSupplier {
     private String color;
     private int number;
-    
+
     @Override
     public String toString() {
         return "Ball has color " + getRandomColor() + " and number " + getRandomNumber();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class Lottery extends Ball {
-
     public static Ball getRandomBall() {
         Ball ball = new Ball();
         ball.getRandomNumber();
