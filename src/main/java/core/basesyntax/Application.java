9f46466b4cc61c0616eece5b0776ed20912b29a1
ANDREY <andrey.kiyas@gmail.com>
2023-06-09 16:11:26 +0300

    correction of comments by Leonid Sivko on PR

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Lottery lottery1 = new Lottery();
-        Lottery lottery2 = new Lottery();
-        Lottery lottery3 = new Lottery();
+        Lottery lottery = new Lottery();
 
-        System.out.println(lottery1.getRandomBall());
-        System.out.println(lottery2.getRandomBall());
-        System.out.println(lottery3.getRandomBall());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
