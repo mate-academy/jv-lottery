d37417c804dbdf6044edfc352c32e3a401946839
Sergii Dancha <sergdan666@gmail.com>
2023-06-10 13:18:04 +0300

    added fixes after review Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 import java.util.Random;
 
+
 public class Lottery {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier;
     private final Random random = new Random();
 
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 100;
 
     public Lottery() {
         this.colorSupplier = new ColorSupplier();
