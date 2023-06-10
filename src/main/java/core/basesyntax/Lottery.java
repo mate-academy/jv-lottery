5123e1e570cbcf5751cc54e8781375dfc27e4a7c
Efim Turkovskiy <turkovskiy.efim@outlook.com>
2023-06-10 10:12:05 +0300

    fixedTask

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random random1 = new Random();
+    private static final int NUMBER = 100;
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall(Ball ball) {
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random1.nextInt(100));
+        ball.setNumber(random.nextInt(NUMBER));
         return ball;
     }
 }
