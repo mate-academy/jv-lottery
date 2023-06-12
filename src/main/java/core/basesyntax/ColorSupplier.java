fee1db7d8e841ccce9c3b1014e28611dd0a245f8
Kostiantyn Kononenko <ext-kostiantyn.kononenko@here.com>
2023-06-12 18:51:24 +0300

    add solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final Random random;
+
+    public ColorSupplier() {
+        random = new Random();
+    }
+
     public String getRandomColor() {
-        return null;
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
