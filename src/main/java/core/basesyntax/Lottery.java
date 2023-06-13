6a2e38e6d2304eb35494f44fd642efe00aa6a2c8
Anton Uzhva <anton.itec91@gmail.com>
2023-06-13 13:43:51 +0300

    Fix some code style issues

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
+
 import java.util.Random;
 
 public class Lottery {
-    private Random random = new Random();
     private static final int MAX_NUMBER_FOR_RANDOM_RANGE = 100;
+    private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
