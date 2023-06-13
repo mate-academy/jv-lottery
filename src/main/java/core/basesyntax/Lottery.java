e58de13fbc1a0dde8bc8e9230a0c8fd0941c3f9a
Vitalii <stelmaschyk.vg@gmail.com>
2023-06-13 19:50:03 +0300

    Fixed added notes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,14 +4,12 @@ import java.util.Random;
 
 public class Lottery extends ColorSupplier {
     private static int MAX_NUMBER = 100;
-    private String ballColor;
-    private int numberOfBall;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
-    Ball getRandomBall() {
-        ballColor = colorSupplier.getRandomColor();
-        numberOfBall = random.nextInt(MAX_NUMBER);
+    public Ball getRandomBall() {
+        String ballColor = colorSupplier.getRandomColor();
+        int numberOfBall = random.nextInt(MAX_NUMBER);
         return new Ball(ballColor, numberOfBall);
     }
 }
