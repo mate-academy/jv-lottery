3c777cb09b9884aa178a4ebfd4859fb059e6bccc
I <dovgopolkir@gmail.com>
2023-06-13 22:11:33 +0300

    added ball as example

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball ball = new Ball();
+
+        System.out.println(ball);// create three balls using class Lottery and print information about them in console
     }
 }
