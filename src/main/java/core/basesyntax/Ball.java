c5c27e2acf3f9a0d83e621d47412916f9ddbca49
I <dovgopolkir@gmail.com>
2023-06-13 20:42:20 +0300

    added String color and int number in class Ball; added setters and getters for color and number; added override toString()

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,4 +1,29 @@
 package core.basesyntax;
 
 public class Ball {
+    private String color;
+    private int number;
+    public Ball (String color, int number){
+        this.color = color;
+        this.number = number;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+    public String setColor(String color){
+        this.color = color;
+        return color;
+    }
+    public int setNumber(){
+        this.number = number;
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return this.number + " " + this.color;
+    }
 }
