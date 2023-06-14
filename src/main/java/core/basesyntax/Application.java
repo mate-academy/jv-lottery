eb0884e685b03a0d179b469646d259f34222c1bb
szykoc <szymon.kociuba@gmail.com>
2023-06-14 16:23:30 +0200

    Fixes after CI build

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+
+
+
+
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
 
-        System.out.println("Hello World")
+        System.out.println("Hello World");
     }
 }
