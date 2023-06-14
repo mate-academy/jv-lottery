87c4c6efeeb5718911851698f42e7ca5312b2ce7
szykoc <szymon.kociuba@gmail.com>
2023-06-14 16:28:11 +0200

    Fixes in code style

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,6 @@ package core.basesyntax;
 
 public class Application {
 
-
-
-
-
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
 
