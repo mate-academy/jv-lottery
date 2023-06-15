63654b4b60f0eba0a5c53c31ca781899f2f765d9
I <dovgopolkir@gmail.com>
2023-06-15 23:31:46 +0300

    added changes in the class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,15 +4,10 @@ import java.util.Random;
 
 public class Lottery extends Ball {
     private static final int FIGURE_COUNT = 100;
-    private Random random = new Random();
-   // public Lottery(String color, int number) {
-    //    super(color, number);
-    //}
 
+    private Random random = new Random();
 
     public Ball getRandomBall(String color, int number) {
-
-        //int figureNumber = random.nextInt(Colors.values().length);
         number = random.nextInt(FIGURE_COUNT);
         color = String.valueOf(Colors.values()[number]);
         return new Ball();
