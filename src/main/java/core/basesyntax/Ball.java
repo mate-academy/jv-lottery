040885e64dc10c122172f538db13156f0c143d80
I <dovgopolkir@gmail.com>
2023-06-15 23:03:43 +0300

    operator wrap "+" was replaced in the class Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -23,9 +23,9 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball{"
+                + "color='" + color + '\''
+                + ", number=" + number
+                + '}';
     }
 }
