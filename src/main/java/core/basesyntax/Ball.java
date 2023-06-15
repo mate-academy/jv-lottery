ab1aa66d0b05518152a57f58d816fb279cef7f3c
I <dovgopolkir@gmail.com>
2023-06-15 22:57:22 +0300

    added changes to the class Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,12 +3,6 @@ package core.basesyntax;
 public class Ball {
     private String color;
     private int number;
-
-    public Ball(String color, int number) {
-        this.color = color;
-        this.number = number;
-    }
-
     public String getColor() {
         return color;
     }
