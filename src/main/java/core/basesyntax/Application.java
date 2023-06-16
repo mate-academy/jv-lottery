8cf43f61ce0f1eccf0d59a7a94ac454024d88439
mIst <bogdankharchenko77@gmail.com>
2023-06-16 13:06:06 +0300

    added fixes after checkstyle review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,16 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
-        Ball firstBall = Lottery.getRandomBall();
-        Ball secondBall = Lottery.getRandomBall();
-        Ball thirdBall = Lottery.getRandomBall();
+    private static final int NUMBER_OF_BALLS = 3;
 
-        System.out.println(firstBall);
-        System.out.println(secondBall);
-        System.out.println(thirdBall);
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+        for (Ball ball : balls) {
+            System.out.println(ball);
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Lottery {
 
-    public static Ball getRandomBall() {
-        String color = ColorSupplier.getRandomColor().toString();
+    public Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color = colorSupplier.getRandomColor().toString();
         Random random = new Random();
         int number = random.nextInt(100);
         return new Ball(color, number);
