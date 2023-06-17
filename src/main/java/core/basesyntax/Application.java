4a67b7245836d3a164be3b281324758a1f0d2457
Andrii Hryniuk <grinuk0706@gmail.com>
2023-06-17 23:06:11 +0300

    error correction

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,17 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery ballOne = new Ball();
+        Lottery balTwo = new Ball();
+        Lottery balThree = new Ball();
+        print(ballOne.getRandomBall());
+        print(balTwo.getRandomBall());
+        print(balThree.getRandomBall());
+    }
+
+    public static void print(Ball ball) {
+        System.out.println("Your ball have "
+                + ball.getColor() + " color and number "
+                + ball.getNumber());
     }
 }
---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Ball extends Lottery {
+    private String color;
+    private int number;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
