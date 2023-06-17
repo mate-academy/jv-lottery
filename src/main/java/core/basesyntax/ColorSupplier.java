8e9f49788b11b0f0ee5654fe5301e9f44d1eefcb
Mykhayl <mate.academy.student.2023@gmail.com>
2023-06-17 17:33:00 +0300

    Changes based on reviewer feedback: * extra comments removed * inline variables moved to class attributes * MAX_NUMBER added

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
+
     public Color getRandomColor() {
-        int rnd = new Random().nextInt(Color.values().length);
-        return Color.values()[rnd];
+        int randomColor = random.nextInt(Color.values().length);
+        return Color.values()[randomColor];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    ColorSupplier colorSupplier = new ColorSupplier();
+
+    Random random = new Random();
+
+    private static int MAX_NUMBER = 100;
+
     public Ball getRandomBall() {
-        Color color = new ColorSupplier().getRandomColor();
-        int number = new Random().nextInt(100);
+        Color color = colorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_NUMBER);
         return new Ball(color, number);
     }
 }
