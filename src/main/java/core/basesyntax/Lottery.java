476be763ae845681909d5217ed632733eb8e5ca3
Mykhayl <mate.academy.student.2023@gmail.com>
2023-06-17 17:37:58 +0300

    corrected attribute visibility

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Lottery {
 
-    ColorSupplier colorSupplier = new ColorSupplier();
-
-    Random random = new Random();
-
     private static int MAX_NUMBER = 100;
+
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
