d88f0b0ccf456ac1fd385f32e59f2bf02bad035d
I <dovgopolkir@gmail.com>
2023-06-18 10:44:48 +0300

    added changes to the class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,14 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends Ball {
-    private static final int FIGURE_COUNT = 100;
-
-    private Random random = new Random();
-
-    public Ball getRandomBall(String color, int number) {
-        number = random.nextInt(FIGURE_COUNT);
-        color = String.valueOf(Color.values()[number]);
-        return new Ball();
+class Lottery {
+    public static Ball getRandomBall() {
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        String color = ColorSupplier.getRandomColor();
+        return new Ball(color, number);
     }
 }
