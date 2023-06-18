55e5561f75dc817c8293282c65959a3872ea1acd
Wasja Zejkan <vasazejkan5@gmail.com>
2023-06-18 21:02:05 +0300

    Add working solution: created classes: Lottery, Ball, enum Colors, implemented method getRandomColor() and created 3 random balls

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Ball {
+    private Colors color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = Colors.valueOf(color);
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" + "color=" + color + ", number=" + number + '}';
+    }
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    RED,
+    YELLOW,
+    BLUE,
+    GREEN,
+    WHITE,
+    PINK,
+    GREY,
+    ORANGE
+}
