d92f41219c51554d89dcc7c9bb9607f0322b2c53
Jean-Mark Kimbembe <marselium@gmail.com>
2023-06-18 11:12:14 +0300

    fixed Application class, initialization of Ball array

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Ball [] balls = new Ball[3];
         Lottery lottery = new Lottery();
-        Ball [] balls = {lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
-        for (Ball ball : balls) {
-            System.out.println(ball.toString());
+        for (int i = 0; i < balls.length; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
