906c9f1fc9da2d983740f7af68efa73c4b6e0511
I <dovgopolkir@gmail.com>
2023-06-18 10:45:16 +0300

    added changes to the class Ball

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random random = new Random();
-
-    public String getRandomColor() {
-        int index = random.nextInt(Color.values().length);
-        return String.valueOf(Color.values()[index]);
+    public static String getRandomColor() {
+        Random random = new Random();
+        Color[] colors = Color.values();
+        int index = random.nextInt(colors.length);
+        return colors[index].toString();
     }
 }
