a09d662dae4ddb69b27ddba73690d16bc01bcb00
I <dovgopolkir@gmail.com>
2023-06-18 22:56:09 +0300

    added changes into the class ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
-        Random random = new Random();
-        Color[] colors = Color.values();
-        int index = random.nextInt(colors.length);
-        return colors[index].toString();
+    //private static final int FIGURE_COUNT = 11;
+    private static final int COLOR_COUNT = 11;
+    private Random random = new Random();
+
+    public Color getRandomColor() {
+        int colorIndex = random.nextInt(COLOR_COUNT);
+        return Color.values()[colorIndex];
     }
 }
