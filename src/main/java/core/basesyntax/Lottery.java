f1762079c62b132e61caeef6ef1957993044298f
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-19 23:58:32 +0300

    made class variable and added constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final static int MAX_NUMBER = 100;
+    private  ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
     public Ball getRandomBall() {
-        int randomNumber = new Random().nextInt(100);
-        String randomColor = new ColorSupplier().getRandomColor();
+        int randomNumber = random.nextInt(MAX_NUMBER);
+        String randomColor = colorSupplier.getRandomColor();
         return new Ball(randomColor,randomNumber);
     }
 }
