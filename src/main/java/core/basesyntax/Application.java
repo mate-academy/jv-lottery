b732fbffc8512affb03a8ac7722420a114d7b680
Jean-Mark Kimbembe <marselium@gmail.com>
2023-06-19 18:39:58 +0300

    Fixed Lottery, ColorSupplier classes. Adding balls to the array with lottery.getRandomBall

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,8 @@ public class Application {
         Ball [] balls = new Ball[3];
         Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            System.out.println(lottery.getRandomBall());
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i].toString());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     private static final int MAX_VALUE = 100;
-
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(this.colorSupplier.getRandomColor(), this.random.nextInt(MAX_VALUE));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_VALUE));
     }
 }
