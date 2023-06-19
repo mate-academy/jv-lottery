b395f446bf383ea12a370b46e532c73f5171e592
I <dovgopolkir@gmail.com>
2023-06-19 17:47:05 +0300

    fixed mistakes in the class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 class Lottery {
+    private Random random = new Random();
 
-    public static Ball getRandomBall() {
-        Random random = new Random();
+    public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Color color = colorSupplier.getRandomColor();
         int number = random.nextInt(100) + 1;
-        return new Ball(color.name(), number);
+        return new Ball(color, number);
     }
 }
