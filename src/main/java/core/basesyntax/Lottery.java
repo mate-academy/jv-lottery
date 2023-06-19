640e9522a94d2e5c070243b99438d17d45d0f2c6
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-20 00:28:39 +0300

    fixed bug with static in lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final static int MAX_NUMBER = 100;
-    private  ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int MAX_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
