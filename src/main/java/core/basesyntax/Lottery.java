575c0f90d4dc2de7a9c491b2bce6aebb3045cee0
I <dovgopolkir@gmail.com>
2023-06-19 21:33:15 +0300

    added constant to avoid magic numbers; relocated colorSupplier

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 class Lottery {
+    private static final int FIGURE_COUNT = 100;
     private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         Color color = colorSupplier.getRandomColor();
-        int number = random.nextInt(100) + 1;
+        int number = random.nextInt(FIGURE_COUNT);
         return new Ball(color, number);
     }
 }
