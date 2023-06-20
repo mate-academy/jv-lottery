c03e0759398853432e590f269639327e8d8bd14b
Maksym Musiienko <maxymvm@gmail.com>
2023-06-20 22:45:12 +0300

    did requested changes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random random = new Random();
     private static int BOUND = Color.values().length;
+    private Random random = new Random();
+
     public Color getRandomColor() {
         return Color.values()[random.nextInt(BOUND)];
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     public static final int BOUND = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(BOUND));
     }
