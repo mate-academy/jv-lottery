2224603aa3dca7596b531fb0d7be912cdfa2c4b6
Roman Bondarenko <71646951+itzRoma@users.noreply.github.com>
2023-06-20 13:17:15 +0300

    getRandomColor method improved
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -11,12 +11,6 @@ public class ColorSupplier {
 
     public String getRandomColor() {
         int randomOrdinal = random.nextInt(Color.values().length);
-        for (Color color : Color.values()) {
-            if (color.ordinal() == randomOrdinal) {
-                return color.name();
-            }
-        }
-        // unreachable, randomOrdinal will always be in range from 0 to (Color.values().length - 1)
-        return "COLORLESS";
+        return Color.values()[randomOrdinal].name();
     }
 }
