82d6377937cd96286b8bc93741595a77dc60242a
anastezia12 <for.game.anaria@gmail.com>
2023-06-23 01:25:34 +0200

    1

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    Random rand = new Random();
+
+    public Colors getRandomColor() {
+        Colors[] colors = Colors.values();
+        return colors[rand.nextInt(colors.length)];
     }
 }
