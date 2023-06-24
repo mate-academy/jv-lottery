fe9e56f90400aa315d861d0dd8cf081385f615af
heilxwotan1 <noobdima228@gmail.com>
2023-06-24 12:57:58 +0300

    1. made ColorSupplier and Random class level fiels 2. removed magic number, declared const MAX_NUM

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_NUM = 100;
+    private final ColorSupplier randomColor = new ColorSupplier();
+    private final Random randomInt = new Random();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(randomColor.getRandomColor(), randomInt.nextInt(MAX_NUM));
     }
 }
