91d53aac9827a4504433955433449891bea57936
oKukHtina <olakukhtina95@gmail.com>
2023-06-24 15:57:59 +0300

    change code after review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     private int getRandomNumber() {
-        return new Random().nextInt(100);
+        return random.nextInt(100);
     }
 
     public Ball getRandomBall() {
-        ColorSupplier color = new ColorSupplier();
         Ball ball = new Ball();
-        Colors randomColor = color.getRandomColor();
+        Colors randomColor = colorSupplier.getRandomColor();
         ball.setColor(randomColor);
         int randomNumber = getRandomNumber();
         ball.setNumber(randomNumber);
