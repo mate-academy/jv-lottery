184f62f0a657144ead5d373f1ee6cacb63e2070c
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-06-26 17:41:13 +0300

    refactored Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
+    private static final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier supplier = new ColorSupplier();
         ball.setColor(supplier.getRandomColor());
         ball.setNumber(new Random().nextInt(MAX_NUMBER));
         return ball;
