6ca7c43de211552b9bb40a01c78fc53068096be9
Zblashchuk <Zblashchuk@gmail.com>
2023-06-27 23:44:50 +0300

    jv-lottery

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Ball {
+    private final String color;
+    private final int number;
+
+    public Ball(String color, int number) {
+      this.color = color;
+      this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball [color=" + color + ", number=" + number + "]";
+    }
+}
