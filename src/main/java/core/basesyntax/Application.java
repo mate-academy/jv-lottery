d17121f343cc0e39055e9c5a3d0c5f052b50254a
Volodymyr Shashkov <finansist33333324@gmail.com>
2023-06-27 14:18:46 +0300

    Revert "hm-2-after-review"
    This reverts commit 8a2252f53894cdba7d3c6c3562ce5918d98080b1.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        int n = 100;
         Lottery ball1 = new Lottery();
-        for (int i = 0; i < n; i++) {
-            System.out.println(ball1.getRandomBall());
-        }
+        Lottery ball2 = new Lottery();
+        Lottery ball3 = new Lottery();
+        System.out.println(ball1.getRandomBall());
+        System.out.println(ball2.getRandomBall());
+        System.out.println(ball3.getRandomBall());
     }
 }
