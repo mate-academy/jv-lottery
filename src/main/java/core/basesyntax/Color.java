4f08de6438850d48aa309c0e84490adb2c8ce8ff
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-06-27 20:32:31 +0300

    create class Ball and enum Color, implemented getRandomColor() method in class ColorSupplier

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum Color {
+    BLACK,
+    GRAY,
+    WHITE,
+    YELLOW,
+    ORANGE,
+    RED,
+    PINK,
+    PURPLE,
+    BLUE
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final Random randomizer = new Random();
     public String getRandomColor() {
-        return null;
+        int randomNumber = randomizer.nextInt(Color.values().length);
+        return Color.values()[randomNumber].toString();
     }
 }
