a7c90684d34dcd5e9c346595d5fea1404780518a
moiseyevak <136981478+moiseyevak@users.noreply.github.com>
2023-06-27 18:19:43 +0300

    if i do method no static he doesn`t work. i don`t know how to do it.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
     public static String getRandomColor() {
         Random random = new Random();
-        int index = new Random().nextInt(Colors.values().length);
-        Colors randomColor = Colors.values()[index];
+        int index = random.nextInt(Color.values().length);
+        Color randomColor = Color.values()[index];
         return String.valueOf(randomColor);
     }
 
