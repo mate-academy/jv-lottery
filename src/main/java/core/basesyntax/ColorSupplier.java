6483fb10ea4bbd27a8d4d20243b6693a0d5f6d3b
Visqe <Veetee@tutanota.com>
2023-06-27 17:28:19 +0300

    improoved random generation of color and number

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
 
     public Color getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int numberOfColors = Color.values().length;
+        int index = random.nextInt(numberOfColors);
         return Color.values()[index];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    Random random = new Random();
+    ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        int randomNumber = new Random().nextInt(100);
-        Color randomColor = new ColorSupplier().getRandomColor();
+        Color randomColor = colorSupplier.getRandomColor();
+        int randomNumber = random.nextInt(100);
         return new Ball(randomColor, randomNumber);
     }
 }
