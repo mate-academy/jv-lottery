0d58629b848d2caaf74698aacd1c480961243d87
Visqe <Veetee@tutanota.com>
2023-06-27 13:52:41 +0300

    created Ball.class

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Ball {
+    private final String color;
+    private final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String toString() {
+        return "color is: " + color + "; number is: " + number + ";";
+    }
+}
