ca5d2aed03da01f597f2608800511037a2e6c15a
Olex <sanekparkhomovsky@gmail.com>
2023-06-27 20:04:36 +0300

    added final number of balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    static final int BALL_NUMBER = 3;
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < BALL_NUMBER; i++) {
             System.out.println(Lottery.getRandomBall());
         }
     }
