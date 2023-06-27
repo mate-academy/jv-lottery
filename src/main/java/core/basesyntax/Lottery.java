272daf9cf9a2e26faf34e0d14a2c37d30e0f47f5
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-06-27 21:13:44 +0300

    added some implementation to solve the simple lottery task and to test it

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final Random randomizer = new Random();
     private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int RANGE_OF_RANDOMIZER = 100;
 
     public Ball getRandomBall() {
-        int randomNumberOfBall = randomizer.nextInt(100);
+        int randomNumberOfBall = randomizer.nextInt(RANGE_OF_RANDOMIZER);
         Color randomColorOfBall = colorSupplier.getRandomColor();
         return new Ball(randomColorOfBall, randomNumberOfBall);
     }
