1e5cfe292a528571a027abcd73c6f6f118f8d069
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-06-28 23:45:59 +0300

    fixed style errors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final int COLORS_COUNT = Colors.values().length;
-    Random random = new Random();
+    private final Random random = new Random();
+
     public Colors getRandomColor() {
         return Colors.values()[random.nextInt(COLORS_COUNT)];
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Lottery {
     private static final int RANDOM_MAX = 100;
-    Random random = new Random();
-    ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier supplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(supplier.getRandomColor(), random.nextInt(RANDOM_MAX));
     }
