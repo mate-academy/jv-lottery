410c6dc38df72f71c8fee21cc306e0068328a467
Vladyslav Keda <dakadofordisluv@gmail.com>
2023-06-28 21:58:36 +0300

    Fixed mistakes: - removed methods which is not used - changed variable 'color', from String to Color - changed 'toString' to default realization

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,32 +1,19 @@
 package core.basesyntax;
 
 public class Ball {
-    private String color;
-    private int number;
+    private final Color color;
+    private final int number;
 
-    public Ball(String color, int number) {
+    public Ball(Color color, int number) {
         this.color = color;
         this.number = number;
     }
 
     @Override
     public String toString() {
-        return color + " " + number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
+        return "Ball{"
+                + "color=" + color
+                + ", number=" + number
+                + '}';
     }
 }
