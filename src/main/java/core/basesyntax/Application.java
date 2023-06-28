8c30a3e2c538e450344902b0ef0846f55e2f3d00
Vladyslav Keda <dakadofordisluv@gmail.com>
2023-06-28 21:53:06 +0300

    added 'createAndPrintRandomBalls'

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        createAndPrintRandomBalls(3);
+    }
+
+    private static void createAndPrintRandomBalls(int value) {
         Lottery lottery = new Lottery();
-        Ball first = lottery.getRandomBall();
-        Ball second = lottery.getRandomBall();
-        Ball third = lottery.getRandomBall();
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+        for (int i = 0; i < value; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
