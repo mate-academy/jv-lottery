cee508e3760b3a72bdbe7597f9eb6c961139eb61
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-06-29 09:45:03 +0300

    was chanded toString() to name() in return statement

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,6 +7,6 @@ public class ColorSupplier {
     public String getRandomColor() {
         int randomColorIndex = new Random().nextInt(Color.values().length);
         Color color = Color.values()[randomColorIndex];
-        return color.toString();
+        return color.name();
     }
 }
