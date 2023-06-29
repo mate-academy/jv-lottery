f0e199b9d7fc14a23a453bbb53996c1a2db32776
Oleh <popovich158@gmail.com>
2023-06-29 23:47:13 +0300

    Fixed mistakes in class Lottery. Moved creation of object ball from class to method.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAXIMUM_NUMBER = 100;
-    private Ball ball = new Ball();
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
+        Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(MAXIMUM_NUMBER));
         return ball;
