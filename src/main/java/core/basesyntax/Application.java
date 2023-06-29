3cbfa33036ba2629047f699194f1705d95a282fb
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-06-29 22:36:06 +0300

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball1 = Lottery.getRandomBall();
-        Ball ball2 = Lottery.getRandomBall();
-        Ball ball3 = Lottery.getRandomBall();
-
-        System.out.println("Ball 1: " + ball1);
-        System.out.println("Ball 2: " + ball2);
-        System.out.println("Ball 3: " + ball3);
+        Ball[] randomBalls = new Ball[3];
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
+            randomBalls[i] = lottery.getRandomBall();
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println(randomBalls[i]);
+        }
     }
 }
