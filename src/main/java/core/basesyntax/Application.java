fd443441c4eedc4ce497696b642bb61bc48e4c60
Ihor <kalaurdelta96@gmail.com>
2023-06-29 14:13:23 +0300

    Created method which create random balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball ball1 = new Lottery().getRandomBall();
+        Ball ball2 = new Lottery().getRandomBall();
+        Ball ball3 = new Lottery().getRandomBall();
+
+        System.out.println(ball1.toString());
+        System.out.println(ball2.toString());
+        System.out.println(ball3.toString());
+
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum Colors {
+    WHITE,
+    RED,
+    YELLOW,
+    BLUE,
+    BLACK
+}
