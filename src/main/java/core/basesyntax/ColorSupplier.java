0439b440afc1c6c088c620d8debb40bb3e9bf1e5
Arsenii Mehdalskyi <arsen2506@knu.ua>
2023-06-29 14:26:22 +0300

    Done

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Random random = new Random();
+        Colors[] colors = Colors.values();
+        return colors[random.nextInt(10)].name();
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Colors {
+    YELLOW,
+    BLUE,
+    GREEN,
+    PURPLE,
+    WHITE,
+    RED,
+    PINK,
+    BROWN,
+    BLACK,
+    ORANGE
+}
