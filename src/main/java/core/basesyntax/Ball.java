8ca0095bac3b3cc418dc078e34f007a78b9e3e61
DaniilNepelev <kakashilvng@gmail.com>
2023-06-29 14:16:32 +0300

    fixed all bugs and hear your comments about my last try

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,11 +1,8 @@
 package core.basesyntax;
 
-import java.text.MessageFormat;
-
 public class Ball {
     private Enum color;
     private int number;
-
 
     Ball(Enum color, int number) {
         this.color = color;
@@ -14,9 +11,9 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color=" + color +
-                ", number=" + number +
-                '}';
+        return "Ball{"
+                + "color=" + color
+                + ", number=" + number
+                + '}';
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     public String getRandomBall() {
         Random random = new Random();
-        ColorSupplier color= new ColorSupplier();
+        ColorSupplier color = new ColorSupplier();
         int randomNumber = random.nextInt(100);
         Enum randomColor = color.getRandomColor();
         Ball ball = new Ball(randomColor, randomNumber);
