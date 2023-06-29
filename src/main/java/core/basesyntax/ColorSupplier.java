4dda2b9072b5f0d1b38259ce2961066078422529
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-06-29 17:08:23 +0300

    Create Ball, Lottery classes Create Color enum Implement getRandomBall() method in Lottery class, getRandomColor() in ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
