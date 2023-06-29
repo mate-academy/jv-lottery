3f1f6ce341fc64e618f576baf1f2f0c833fe68b3
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-06-29 12:46:18 +0300

    solution implemented

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,12 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        int numberOfBalls = 3;
+        Ball[] balls = new Ball[numberOfBalls];
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < numberOfBalls; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i]);
+        }
     }
 }
