2c33321b510d1d835648abce1398806e2f8bce02
Oleh <popovich158@gmail.com>
2023-06-29 13:27:40 +0300

    Fixed mistakes in Application's class.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int NUMBER_OF_BALL = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-        System.out.println(ball1);
-        System.out.println(ball2);
-        System.out.println(ball3);
+        Ball[] balls = new Ball[NUMBER_OF_BALL];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+
     }
 }
