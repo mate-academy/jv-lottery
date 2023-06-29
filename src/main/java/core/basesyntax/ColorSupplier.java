d3b331b5620f348158263e19fde335156cde4dc5
Ihor Vasylenko <vasylenkoihor23@gmail.com>
2023-06-30 00:45:13 +0300

    implemented task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Colors [] colors = Colors.values();
+        Random random = new Random();
+        int number = random.nextInt(colors.length);
+        return String.valueOf(colors[number]);
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        String color = ColorSupplier.getRandomColor();
+        Random random = new Random();
+        int number = random.nextInt(100);
+        return new Ball(color, number);
+    }
+}
