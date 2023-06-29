5958e6948c09aadacb46877207ec81dca73461f9
holsoni <soniakostashchuk@gmail,com>
2023-06-29 09:59:39 +0300

    implemented classes Ball, Lottery and ENUM ColorSupplier; 3 ball objects created and printed in console

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUMBER = 100;
+
+    public Ball getRandomBall() {
+        String color = new ColorSupplier().getRandomColor();
+        int number = new Random().nextInt(MAX_NUMBER);
+        return new Ball(color, number);
+    }
+}
