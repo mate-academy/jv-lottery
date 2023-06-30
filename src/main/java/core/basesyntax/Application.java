0e3d0a5f39000245bffe59dcb70ab5cc89e1b0ee
Mykyta Zuber <nikitazuber@gmail.com>
2023-06-30 16:05:07 +0300

    Fixed ballListsCreation

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,10 +6,10 @@ import lottery.Lottery;
 public class Application {
     public static void main(String[] args) {
         Lottery newLottery = new Lottery();
-        Ball[] ballsList = {
-               newLottery.getRandomBall(),
-               newLottery.getRandomBall(),
-               newLottery.getRandomBall() };
+        Ball[] ballsList = new Ball[3];
+        for (int i = 0 ; i < 3; ++i) {
+            ballsList[i] = newLottery.getRandomBall();
+        }
         for (Ball ball : ballsList) {
             System.out.println(ball.toString());
         }
