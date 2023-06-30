a7599779b18d9aa5d914d6198040884c7583718c
Artem Reizer <artem1707200290@gmail.com>
2023-06-30 13:52:31 +0300

    added required changes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,8 @@ public class Application {
     private static final int MAX_INDEX = 3;
 
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < MAX_INDEX; i++) {
-            Lottery lottery = new Lottery();
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends Ball {
-    private static final int BALL_NUM = 100;
-    private final int value = new Random().nextInt(BALL_NUM);
+    private static final int MAX_BALL_NUMBER = 100;
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        randomBall.setNumber(value);
+        randomBall.setNumber(random.nextInt(MAX_BALL_NUMBER));
         randomBall.setColor(colorSupplier.getRandomColor());
         return randomBall;
     }
