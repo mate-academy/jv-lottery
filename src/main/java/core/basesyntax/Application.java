194f1ed887fda86baec43a1428b4f177e9983d34
holsoni <soniakostashchuk@gmail,com>
2023-06-30 14:06:30 +0300

    improved codestyle and fixed main method logic

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUM_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = lottery.getRandomBall();
-        }
-        for (Ball ball : balls) {
-            System.out.println(ball);
+        for (int i = 0; i < NUM_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Color color = new ColorSupplier().getRandomColor();
-        int ballNumber = new Random().nextInt(MAX_BALL_NUMBER);
+        Color color = colorSupplier.getRandomColor();
+        int ballNumber = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(color, ballNumber);
     }
 }
