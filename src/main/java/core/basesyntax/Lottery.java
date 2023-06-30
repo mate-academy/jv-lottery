12a8e02a736f63459f5b40c58dd52635ff9521d2
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-06-30 15:09:54 +0300

    declared separate variables for max ball number, new ColorSupplier() and new Random()

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+    private String randomColor = new ColorSupplier().getRandomColor();
+    private int randomNumber = new Random().nextInt(MAX_BALL_NUMBER);
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setColor(randomColor);
+        ball.setNumber(randomNumber);
         return ball;
     }
 }
