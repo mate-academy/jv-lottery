91415c8f437f612d7e113e47891a9d9d5b407598
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-06-30 21:51:33 +0300

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] randomBalls = new Ball[3];
         Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            randomBalls[i] = lottery.getRandomBall();
-        }
-        for (int i = 0; i < 3; i++) {
-            System.out.println(randomBalls[i]);
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
