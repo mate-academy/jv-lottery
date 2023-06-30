50bc50c809e7be764d9822d10c1505dfe85bf461
Denis Moroz <moroz06denis@gmail.com>
2023-06-30 13:37:36 +0200

    update level random

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,11 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final int MAX_NUMBER_OF_COLORS = Colors.values().length;
+    private final Random random = new Random();
+
 
     public Colors getRandomColor() {
-        int index = new Random().nextInt(MAX_NUMBER_OF_COLORS);
+        int index = random.nextInt(MAX_NUMBER_OF_COLORS);
         return Colors.values()[index];
     }
 }
