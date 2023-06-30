ed5cd1dfd820fc7c891537041e6f76da49bc114b
Oleksandr Savenets <savenvoin@gmail.com>
2023-06-30 18:30:21 +0300

    moved Rendom the class-level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
