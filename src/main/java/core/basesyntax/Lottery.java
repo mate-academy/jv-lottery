7e0f980adcc63bb924393b254b7b609e8c19ce4a
Oleksandr Savenets <savenvoin@gmail.com>
2023-06-30 14:48:16 +0300

    made fields final

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_NUMBER = 100;
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Random random = new Random();
+    private static final int MAX_NUMBER = 101;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
