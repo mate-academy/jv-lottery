af0d306e459dcd1c71865d26cacaa7681cbf05fd
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-06-30 14:06:25 +0300

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Random RANDOMIZER = new Random();
+    private final Random randomizer = new Random();
 
     public Color getRandomColor() {
-        int randomNumber = RANDOMIZER.nextInt(Color.values().length);
+        int randomNumber = randomizer.nextInt(Color.values().length);
         return Color.values()[randomNumber];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random RANDOMIZER = new Random();
-    private final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
     private static final int RANGE_OF_RANDOMIZER = 100;
+    private final Random randomizer = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int randomNumberOfBall = RANDOMIZER.nextInt(RANGE_OF_RANDOMIZER);
-        Color randomColorOfBall = COLOR_SUPPLIER.getRandomColor();
+        int randomNumberOfBall = randomizer.nextInt(RANGE_OF_RANDOMIZER);
+        Color randomColorOfBall = colorSupplier.getRandomColor();
         return new Ball(randomColorOfBall, randomNumberOfBall);
     }
 }
