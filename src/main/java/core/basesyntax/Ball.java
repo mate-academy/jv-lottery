054e3c7e37fc029e85bec19c20a44e69a163a6e6
DaniilNepelev <kakashilvng@gmail.com>
2023-06-30 14:21:50 +0300

    fixed errors with maven

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    public final Colors color;
-    public final int number;
+    private final Colors color;
+    private final int number;
 
     Ball(Colors color, int number) {
         this.color = color;
