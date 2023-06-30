83c0b2fa4a70bf9f45019da922d7d58d6b6850f4
Vitalii <mkukoba11@ukr.net>
2023-06-30 14:00:38 +0300

    made 100 constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
+    private final int roofNumber = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(roofNumber));
     }
 }
