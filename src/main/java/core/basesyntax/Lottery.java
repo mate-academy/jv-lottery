fa7791c23fd8f1f96f794664e8376a288ac2a163
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-06-30 11:51:08 +0300

    added loop, added constant fied, changed objects declaration to a class level

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_INTEGER = 101;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(101));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_INTEGER));
     }
 }
