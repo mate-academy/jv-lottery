45bb8cf41a18aaeeb40c145b2575c85d1309b4fe
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-06-30 13:53:15 +0300

    Correction of all comments. Rename Colors to Color. Add loops.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final int bound = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        int number = new Random().nextInt(100);
+        int number = new Random().nextInt(bound);
         return new Ball(colorSupplier.getRandomColor(), number);
     }
 }
