e1dd9a24ce1391ad77762a83391225f230f567a6
Vitalii <mkukoba11@ukr.net>
2023-06-30 14:43:21 +0300

    imlemented fixes with names and constants

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-    private final int roofNumber = 100;
+    private static final int ROOF_NUMBER = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(roofNumber));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(ROOF_NUMBER));
     }
 }
