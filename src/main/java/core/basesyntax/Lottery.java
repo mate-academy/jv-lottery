ca1b32d8126ae44bd1b3a3960eca7608cef9659b
Ihor <kalaurdelta96@gmail.com>
2023-06-30 16:36:57 +0300

    Changed algorithm of class "Lottery".

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
-    private final int numberOfBall = new Random().nextInt(MAX_NUMBER);
-    private final String colorOfBall = new ColorSupplier().getRandomColor();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(numberOfBall);
-        ball.setColor(colorOfBall);
+        ball.setNumber(random.nextInt(MAX_NUMBER));
+        ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
 }
