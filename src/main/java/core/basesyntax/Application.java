ed70279795096f2871d5d652f72c317c71fe462b
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2023-06-30 18:34:31 +0300

    fixed creation several instances of Lottery class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Ball[] balls = new Ball[3];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
         }
     }
