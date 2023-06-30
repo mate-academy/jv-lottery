dd4c609734566cf11907d0df3485a377a7e837c2
Mykhailo Vorobets <misha.shark6969@gmail.com>
2023-06-30 11:04:00 +0300

    correcting mistakes on the checklist

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_USERS = 3;
+
     public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_USERS];
         for (int i = 0; i < balls.length; i++) {
