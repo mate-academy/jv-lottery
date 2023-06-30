aec28d20a0ed9ded74865af007f580298735f792
Visqe <Veetee@tutanota.com>
2023-06-30 14:25:15 +0300

    style improovements

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -11,4 +11,3 @@ public class ColorSupplier {
         return Color.values()[index];
     }
 }
-
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final static int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(randomColor, randomNumber);
     }
 }
