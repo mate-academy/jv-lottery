65cdd07ade00593833ac8bb9ff586511b4c0d350
Oleksandr Savenets <savenvoin@gmail.com>
2023-06-30 14:06:40 +0300

    changed declaration ColorSupplier, Lottery and Random objects

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -7,9 +7,10 @@ public class Application {
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
         }
         System.out.println(Arrays.toString(balls));
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier colorSupplier = new ColorSupplier();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(MAX_NUMBER));
+        ball.setNumber(random.nextInt(MAX_NUMBER));
         return ball;
     }
 }
