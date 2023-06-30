9d733d1402d0329ec08317fed04c9beb1428d35f
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-06-30 12:06:28 +0300

    added formating to final sout

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] winners = new Ball[3];
+        Ball[] winners = new Ball[100];
 
         for (int i = 0; i < winners.length; i++) {
             Ball winner = Lottery.getRandomBall();
             winners[i] = winner;
         }
-        System.out.println("Lucky balls today is: \n" + Arrays.toString(winners));
+        System.out.println("\n Lucky balls today is: \n" + Arrays.toString(winners));
     }
 }
