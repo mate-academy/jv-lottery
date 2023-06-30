cb73ae00b3fe81784be72ddaea045669a98ec283
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-06-30 12:03:05 +0300

    added values to match number of results with `winners` array length

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Ball[] winners = new Ball[3];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < winners.length; i++) {
             Ball winner = Lottery.getRandomBall();
             winners[i] = winner;
         }
