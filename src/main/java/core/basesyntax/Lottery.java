b3e781a7588e9b4aec97f776a43f23ebfdfbc040
satori1111 <romakuchmii@gmail.com>
2023-06-30 20:33:38 +0300

    fixed getRandomBall and added spaces

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BALL_NUMBER_MAX_VALUE = 100;
+    private static final int BALL_NUMBER_MAX_VALUE = 101;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
