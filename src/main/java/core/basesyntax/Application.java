f546dd043362a0fe23dc7895f5a263330bc3a236
Ihor <kalaurdelta96@gmail.com>
2023-06-30 14:35:49 +0300

    Changed method of creating and outputting balls to the console. Renamed ENAM. Changed algorithm of class "Lottery".

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball1 = new Lottery().getRandomBall();
-        Ball ball2 = new Lottery().getRandomBall();
-        Ball ball3 = new Lottery().getRandomBall();
-
-        System.out.println(ball1.toString());
-        System.out.println(ball2.toString());
-        System.out.println(ball3.toString());
-
+        for (int i = 0; i < 3; i++) {
+            Ball ball = new Lottery().getRandomBall();
+            System.out.println(ball.toString());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private final int numberOfBall = new Random().nextInt(MAX_NUMBER);
+    private final String colorOfBall = new ColorSupplier().getRandomColor();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(new Random().nextInt(100));
-        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(numberOfBall);
+        ball.setColor(colorOfBall);
         return ball;
     }
 }
