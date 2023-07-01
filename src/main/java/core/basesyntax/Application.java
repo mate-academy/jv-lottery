c2638a90edfdf037a399d6807ed02680b39c428a
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-01 11:54:53 +0300

    colorsuplier, lottery and application classes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        int numberOfBalls = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < numberOfBalls; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int ballNumberBound = 100;
+    private static final int BALL_NUMBER_BOUND = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(ballNumberBound));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BALL_NUMBER_BOUND));
     }
 }
