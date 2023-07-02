d36aa2f1119fff891f2bf4629bcfa32c4f003f39
Ihor <kalaurdelta96@gmail.com>
2023-07-02 20:55:16 +0300

    Changed algorithm  for outputting information about balls to the console. Changed 'toString' method on '.name' method for ENUMs in "ColorSupplier" class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Ball ball;
         for (int i = 0; i < 3; i++) {
-            Ball ball = new Lottery().getRandomBall();
+            ball = new Lottery().getRandomBall();
             System.out.println(ball.toString());
         }
     }
