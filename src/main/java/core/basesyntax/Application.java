4099707ed4ab1683d73acb8bb23130a90b1c7ce6
Ihor <kalaurdelta96@gmail.com>
2023-07-03 10:31:56 +0300

    created Lottery object outside of the loop

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball;
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            ball = new Lottery().getRandomBall();
-            System.out.println(ball.toString());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
