83874ca9f29edc238cb5be9f3b0b0779b0f1a0fb
yermilov.m <yermilov.m@hotmail.com>
2023-07-03 02:47:37 +0200

    Color supplier class modified

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
+import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Color[] colors = Color.values();
+        int index = new Random().nextInt(colors.length);
+        return colors[index].toString();
     }
 }
