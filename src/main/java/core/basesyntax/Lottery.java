41596315f44e918822fc1cda03b8cce87715583a
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-03 19:47:49 +0300

    deleted reducant files from index

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private final Random randomNumberGenerator = new Random();
-    private final ColorSupplier colorSupply = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         int randomNumber = randomNumberGenerator.nextInt(MAX_BALL_NUMBER);
-        Color color = colorSupply.getRandomColor();
+        Color color = colorSupplier.getRandomColor();
         return new Ball(color, randomNumber);
     }
 }
