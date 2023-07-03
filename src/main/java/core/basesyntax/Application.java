9dbd711d866df473fe403c9c8396bea6a2f1a440
Mykyta Zuber <nikitazuber@gmail.com>
2023-07-03 12:43:27 +0300

    Refactoring code

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -7,11 +7,10 @@ public class Application {
     public static void main(String[] args) {
         Lottery newLottery = new Lottery();
         Ball[] ballsList = new Ball[3];
-        for (int i = 0; i < 3; ++i) {
-            ballsList[i] = newLottery.getRandomBall();
-        }
+
         for (Ball ball : ballsList) {
-            System.out.println(ball.toString());
+            ball = newLottery.getRandomBall();
+            System.out.println(ball);
         }
     }
 }
--- src/main/java/lottery/Lottery.java
+++ src/main/java/lottery/Lottery.java
@@ -4,7 +4,9 @@ import core.basesyntax.ColorSupplier;
 import java.util.Random;
 
 public class Lottery {
+
+    public static int MAX_NUMBER_BALL = 100;
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER_BALL));
     }
 }
