8c2fd9d580171fb5dd2f27f467976f9d5113162d
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-07-03 15:48:35 +0200

    HM - 2 , Done V1.2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBER = 100;
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall(){
         Ball ball = new Ball();
