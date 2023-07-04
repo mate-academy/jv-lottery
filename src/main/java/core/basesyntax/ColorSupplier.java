d2b1d7a003743c9a8491af8262ec0af2795e96df
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-07-04 13:00:25 +0200

    HM - 2 , Done V1.42 enum names should be in singular form

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,8 +6,8 @@ public class ColorSupplier {
     private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = random.nextInt(Colors.values().length);
-        Colors colors = Colors.values()[index];
+        int index = random.nextInt(Color.values().length);
+        Color colors = Color.values()[index];
         return colors.name();
 
     }
