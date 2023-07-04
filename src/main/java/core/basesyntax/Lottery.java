0223c75398fb6183b9f07bf204322b266cc4c811
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-07-04 16:51:42 +0300

    implemented simple lottery with balls

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        int number = random.nextInt(MAX_BALL_NUMBER) + 1;
+        return new Ball(colorSupplier.getRandomColor(), number);
+    }
+}
