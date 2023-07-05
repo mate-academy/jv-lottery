5ddda528e4af22142cd231863c8a7838e33f3631
Taras Nedaskivskyi <nadashkivskyitv@gmail.com>
2023-07-05 21:08:11 +0300

    solution of the task - errors in classes ColorSupplier and Lottery have been fixed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        Random random = new Random();
         int colorsCount = Color.values().length;
         return Color.values()[random.nextInt(colorsCount)].name();
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
         Ball randomBall = new Ball();
         randomBall.setColor(colorSupplier.getRandomColor());
-        randomBall.setNumber(random.nextInt(MAX_BALL_NUMBER));
+        randomBall.setNumber(random.nextInt(MAX_BALL_NUMBER + 1));
         return randomBall;
     }
 }
