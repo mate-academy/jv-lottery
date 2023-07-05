bb6dabbbd700055174b899a48a3620f6bd6a0260
Andrii Riaboshapka <postagama@gmail.com>
2023-07-05 22:19:32 +0300

    implement method getRandomColor()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    private static final String[] COLORS = { "RED", "BLUE", "BLACK", "YELLOW", "PINK", "GREEN"};
+    private static final Random random = new Random();
+
+    public static String getRandomColor() {
+        int index = random.nextInt(COLORS.length);
+        return COLORS[index];
     }
 }
