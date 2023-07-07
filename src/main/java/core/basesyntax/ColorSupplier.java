a2bee17ca4776e9520562b40c0778ad61f553e92
Andrii Riaboshapka <postagama@gmail.com>
2023-07-07 21:38:58 +0300

    Changed ColorSupplier class, getRandomColor() return type, type colors, dell COLORS array

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,12 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class ColorSupplier {
-    private static final String[] COLORS = { "RED", "BLUE", "BLACK", "YELLOW", "PINK", "GREEN"};
-    private static final Random random = new Random();
-
-    public static String getRandomColor() {
-        int index = random.nextInt(COLORS.length);
-        return COLORS[index];
+class ColorSupplier {
+    public Color getRandomColor() {
+        Color[] colors = Color.values();
+        Random random = new Random();
+        int index = random.nextInt(colors.length);
+        return colors[index];
     }
 }
