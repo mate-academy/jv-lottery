a4fecb33a2b235c710dd0d4d9fe7c06a5da34056
breakingtumble <yaroslavglavatsky@gmail.com>
2023-07-10 00:58:48 +0300

    made a new constant BALLS_COUNT

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int BALLS_COUNT = 5;
+
     public static void main(String[] args) {
-        final int BALLS_COUNT = 3;
         Ball[] balls = new Ball[BALLS_COUNT];
         Lottery lottery = new Lottery();
         for (int i = 0; i < balls.length; i++) {
