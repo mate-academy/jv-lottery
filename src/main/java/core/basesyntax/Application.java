765ffe1ee15d007de287c182bbd2de8d00ff5a28
Danial <danialbest55@gmail.com>
2023-07-09 19:41:59 +0300

    try1

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,8 +6,8 @@ public class Application {
     public static void main(String[] args) {
         ColorSupplier colorSupplier = new ColorSupplier();
         Lottery lottery = new Lottery(colorSupplier);
-        final int NUMBER_OF_BALLS = 3;
-        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        final int NUMBER_OFBALLS = 3;
+        Ball[] balls = new Ball[NUMBER_OFBALLS];
 
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
