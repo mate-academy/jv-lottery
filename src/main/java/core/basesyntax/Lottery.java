13f61202ce12295a5f47ccf3d8c4b73fb2954756
Vitalii Seleshchuk <seleschuk@gmail.com>
2023-07-09 12:09:50 +0300

    hm-2-solution (after mentor's remark) 1. Removed unnecessary comments 2. Changed the colorSupplier variable from method to class level

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int randomNumber = new Random().nextInt(100) + 1;
         return new Ball(colorSupplier.getRandomColor(), randomNumber);
