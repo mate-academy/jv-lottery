48bb34f3ea4b74310844a09c848e32ee1e68c499
Danial <danialbest55@gmail.com>
2023-07-09 19:39:33 +0300

    change

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
-    static private final int FIGURE_COUNT = 8;
+    private final int FIGURE_COUNT = 8;
 
     public Color getRandomColor() {
         Color[] colors = Color.values();
