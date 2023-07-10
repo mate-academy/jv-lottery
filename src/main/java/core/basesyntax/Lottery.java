7fa02d3465b137b9d00221794f719e96ec7fcfab
Vitalii Seleshchuk <seleschuk@gmail.com>
2023-07-10 22:55:55 +0300

    hm-2-solution (after mentor's remark) 1. Corrected variable name from "BAAL" to "BALL" in Lottery.java 2. Removed commented imports in Application.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BAAL_COUNT = 100;
+    private static final int BALL_COUNT = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int randomNumber = random.nextInt(BAAL_COUNT);
+        int randomNumber = random.nextInt(BALL_COUNT);
         return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
 }
