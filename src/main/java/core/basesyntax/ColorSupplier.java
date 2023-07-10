eda70fb5734ef96506ce9cba7f292cd1faadaaf7
Vitalii Seleshchuk <seleschuk@gmail.com>
2023-07-10 13:42:26 +0300

    hm-2-solution (after mentor's remark) changes after mentor's comments (Attempt 5) 1.Redundant comment from Lottery Class was deleted 2.ColorSupplier Class was corrected due to mentor’s comment

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        Color color = Color.values()[new Random().nextInt(Color.values().length)];
-        return color.toString();
+        Color color = Color.values()[random.nextInt(Color.values().length)];
+        return color.name();
     }
 }
