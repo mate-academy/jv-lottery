9d306ce978f136b771bbc5ad6c61220752fc737c
Danial <danialbest55@gmail.com>
2023-07-10 18:43:59 +0300

    I try the best4

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,17 +4,16 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier;
-    private Random magicNumbers ;
+    private Random magicNumbers;
 
     public Lottery(ColorSupplier colorSupplier) {
         this.colorSupplier = colorSupplier;
         this.magicNumbers = new Random();
     }
 
-
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor().name();
-        int number = magicNumbers.nextInt(100)+1;
+        int number = magicNumbers.nextInt(100) + 1;
         return new Ball(color, number);
     }
 }
