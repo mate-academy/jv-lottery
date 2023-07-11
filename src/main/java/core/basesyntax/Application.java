4e5d81465e0b0e00f99a2f328311eea9a72c7aa7
LTX <48835997+Lin-Rin@users.noreply.github.com>
2023-07-11 20:25:03 +0300

    changed array creation in main, refactored naming.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,12 @@ package core.basesyntax;
 public class Application {
 
     public static void main(String[] args) {
-        Lottery lot = new Lottery();
-        Ball[] balls = new Ball[]{lot.getRandomBall(), lot.getRandomBall(), lot.getRandomBall()};
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[3];
+
+        for (int i = 0; i < 3; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
 
         for (Ball ball : balls) {
             System.out.println(ball);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 public class Lottery {
 
+    private static final int LIMIT_FOR_RANDOM_NUMBERS = 100;
+
     private final Random rand = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = rand.nextInt(100);
+        int number = rand.nextInt(LIMIT_FOR_RANDOM_NUMBERS);
 
         return new Ball(number, color);
     }
