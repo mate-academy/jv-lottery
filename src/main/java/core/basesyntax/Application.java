9040c0485859531b94ddd99af5751ef14fc582c7
evgenkolner <evgen.kolner@gmail.com>
2023-07-13 16:30:02 +0300

    completed hw2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball ballFirst = new Lottery().getRandomBall();
+        Ball ballSecond = new Lottery().getRandomBall();
+        Ball ballThird = new Lottery().getRandomBall();
+        System.out.println(ballFirst);
+        System.out.println(ballSecond);
+        System.out.println(ballThird);
     }
 }
