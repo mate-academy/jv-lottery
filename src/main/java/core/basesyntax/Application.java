be64bc32d53174bb96638c7ab06741e69e0b4a83
Artem Hlukhoman <giantking1231@gmail.com>
2023-07-14 16:02:47 +0300

    created cycle in Application, made static method nonStatic, for enum used name()

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-
-        System.out.println(ball1.toString());
-        System.out.println(ball2.toString());
-        System.out.println(ball3.toString());
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i].toString());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class Lottery {
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(ColorSupplier.getRandomColor());
+        ColorSupplier colorSupplier = new ColorSupplier();
+        ball.setColor(colorSupplier.getRandomColor());
         Random random = new Random();
         ball.setNumber(random.nextInt(100));
         return ball;
