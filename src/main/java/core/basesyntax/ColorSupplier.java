cfda68d0bf039817249c012262cd6c3933baee8d
Parashchuk <Andry.parashchuk@gmail.com>
2023-07-15 13:09:39 +0200

    Implement class Ball and classes to get random balls

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    enum Color {
+        YELLOW,
+        GREEN,
+        BLACK,
+        BLUE,
+        PINK,
+        PURPLE,
+        RED,
+        WHITE,
+        ORANGE
+    }
+
+    public static String getRandomColor() {
+        Random random = new Random();
+        return String.valueOf(Color.values()[random.nextInt(9)]);
     }
 }
