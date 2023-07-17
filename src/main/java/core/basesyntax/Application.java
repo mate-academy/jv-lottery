e0d8371dcfc416bde3ad337461b1936b676bf03f
Yana Bilyk <bilyk.yn@gmail.com>
2023-07-17 17:06:24 -0400

    Created classes and methods, did clean package

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,9 +4,10 @@ public class Application {
     private static final int MAX_BALL_AMMOUNT = 3;
 
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         Ball[] balls = new Ball[MAX_BALL_AMMOUNT];
         for (int i = 0;i < MAX_BALL_AMMOUNT; i++) {
-            balls[i] = Lottery.getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
 
-    public static Ball getRandomBall() {
-        String color = ColorSupplier.getRandomColor().name();
+    public Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color = colorSupplier.getRandomColor().name();
         int number = new Random().nextInt(MAX_BALL_NUMBER);
         return new Ball(color, number);
     }
