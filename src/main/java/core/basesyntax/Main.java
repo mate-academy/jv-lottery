8b6ef92f5a6e78e3cea929712f61243aeea25a7e
Solomia Bobyak <mia.bobyak@gmail.com>
2023-07-18 23:53:05 +0300

    create three balls and print information

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        Ball firstBall = lottery.getRandomBall();
+        Ball secondBall = lottery.getRandomBall();
+        Ball thirdBall = lottery.getRandomBall();
+        System.out.println(firstBall);
+        System.out.println(secondBall);
+        System.out.println(thirdBall);
+    }
+}
