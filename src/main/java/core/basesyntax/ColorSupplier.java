6985ad18b30659b1cc1335502dd1c10fdfcd0825
Solomia Bobyak <mia.bobyak@gmail.com>
2023-07-18 23:50:40 +0300

    add method in class ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     public Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color;
+        return Color.values()[index];
     }
 }
