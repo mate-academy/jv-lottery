ca215bf5483fea8b81d4afcb5d71fadd8a9afe03
Vlad Rublenko <vladik.rar@ukr.net>
2023-07-24 16:50:16 +0300

    deleted some white spaces and separated import from prev' statement

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
-public class Application extends Lottery{
+public class Application extends Lottery {
     public static void main(String[] args) {
-        for(int i = 1; i <= 3; i++){
+        for (int i = 1; i <= 3; i++) {
             System.out.println("Ball №" + i + ":" + "\n" + getRandomBall());
         }
     }
