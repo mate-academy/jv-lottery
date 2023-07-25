4f2a4011ff3d11e0baba5a31340c883232643b8e
Solomia Bobyak <mia.bobyak@gmail.com>
2023-07-25 17:53:49 +0300

    I rewrote the variables that should be at the class level. Used the standard method of enum name()

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private Ball ball = new Ball();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        Ball ball = new Ball();
-        ball.setColor(String.valueOf(colorSupplier.getRandomColor()));
-        int index = new Random().nextInt(NUMBER);
+        ball.setColor(colorSupplier.getRandomColor().name());
+        int index = random.nextInt(NUMBER);
         ball.setNumber(index);
         return ball;
     }
