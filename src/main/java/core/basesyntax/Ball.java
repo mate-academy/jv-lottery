cb6661efc35771aff961f02c92107db17b8f437e
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-07-25 11:07:22 +0300

    fix error Variable 'color'  and 'number' must be private and have accessor methods

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,22 @@
 package core.basesyntax;
 
 public class Ball {
-    String color;
-    int number;
+
+    private String color;
+    private int number;
 
     Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
     @Override
     public String toString() {
         return color + "ball with a number" + number;
