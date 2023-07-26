4b374ac22159cffad8a09d3310b1517cfbf718ad
Bohdan Maksymenko <bogdan.maksimenko.2002@gmail.com>
2023-07-27 00:45:56 +0300

    delete instance of colorSuplier class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public static Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         int randomNum = new Random().nextInt(100);
-        return new Ball(colorSupplier.getRandomColor(),randomNum);
+        return new Ball(ColorSupplier.getRandomColor(),randomNum);
     }
 }
