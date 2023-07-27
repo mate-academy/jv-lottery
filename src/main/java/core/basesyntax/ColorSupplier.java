31b0762f95866ffae0f75da4ddfd02113f9e4bc2
Lev Shtana <120460187+CapybaraFrog@users.noreply.github.com>
2023-07-27 16:26:41 +0300

    I moved the randomizers, also moved the ColorSupplier, and finally got rid of the static methods

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
 
-    public static String getRandomColor() {
-        Random random = new Random();
-        return Color.values()[random.nextInt(Color.values().length)].toString();
+    public String getRandomColor() {
+        return Color.values()[random.nextInt(Color.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_VALUE_OF_THE_BALL = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
-    public static Ball getRandomBall() {
-        Random random = new Random();
-        return new Ball(new ColorSupplier().getRandomColor(),random.nextInt(MAX_VALUE_OF_THE_BALL));
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(),random.nextInt(MAX_VALUE_OF_THE_BALL));
     }
 }
