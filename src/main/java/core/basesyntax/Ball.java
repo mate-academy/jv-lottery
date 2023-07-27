377b1d1611854affd9a74842a0c85f8a691ca12d
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-07-27 11:18:36 +0300

    created loterry app

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return color + " ball with number: " + number;
+    }
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private enum Color {
+        Red,
+        Blue,
+        Green,
+        White,
+        Black,
+        Orange,
+        Purple
+    }
+
     public String getRandomColor() {
-        return null;
+        return Color.values()[new Random().nextInt(Color.values().length)].name();
     }
 }
