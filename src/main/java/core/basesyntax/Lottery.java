2b0ced66ed3a888fb5d6dfc37cb5c91800f99890
Peresada Kostya <peresadakostya81@gmail.com>
2023-07-29 21:30:09 +0300

    tried to assign correct type in public Ball method, extract new Random() as class variable in ColorSupplier class, rename enum in singular form, extract ColorSupplier and Random to class variables similar to comment in ColorSupplier.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public final Random random = new Random();
-    public final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
     }
