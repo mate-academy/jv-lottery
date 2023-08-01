fa06929d6f13bf509b62a1cc54e336d459e6340d
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-01 17:58:19 +0300

    Change variable order.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_NUMBER_RANDOM = 100;
     private final Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int BALL_NUMBER_RANDOM = 100;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(BALL_NUMBER_RANDOM));
