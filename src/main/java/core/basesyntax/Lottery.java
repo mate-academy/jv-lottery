ea607ea55ae225be24db9d0a1b593d315ee636e3
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-01 17:56:12 +0300

    Removed magic number, rename enum class.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int BALL_NUMBER_RANDOM = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BALL_NUMBER_RANDOM));
     }
 }
