ee9b5b6752bfd1aaa67f4682b201bc037d7d4de0
Yana Bilyk <bilyk.yn@gmail.com>
2023-08-03 19:00:30 -0400

    avoid static methods

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,9 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
-
+    ColorSupplier colorSupplier = new ColorSupplier();
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor().name();
         int number = new Random().nextInt(MAX_BALL_NUMBER);
         return new Ball(color, number);
