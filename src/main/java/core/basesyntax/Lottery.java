11ad2b8e1ff69324c67417a7519015e2c7111194
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-08-03 16:54:57 +0300

    fix:  ColorSupplier 100 should be class constant;make random class field, not local variable;remove this line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_RANDOM_NUMBER = 100;
+    static final int MAX_RANDOM_NUMBER = 100;
     ColorSupplier colorSupplier = new ColorSupplier();
     String color = colorSupplier.getRandomColor();
     int number = new Random().nextInt(MAX_RANDOM_NUMBER) + 1;
