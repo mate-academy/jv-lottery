c32156e8ba05222b79c5eab25722d3a1c01d572a
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-08-03 17:04:55 +0300

    fix: VisibilityModifier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,22 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public Color[] getColors() {
-        return colors;
-    }
-
-    public void setColors(Color[] colors) {
-        this.colors = colors;
-    }
-
-    public int getRandomIndex() {
-        return randomIndex;
-    }
-
-    public void setRandomIndex(int randomIndex) {
-        this.randomIndex = randomIndex;
-    }
-
     private Color[] colors = Color.values();
     private int randomIndex = new Random().nextInt(colors.length);
 
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,41 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int MAX_RANDOM_NUMBER = 100;
+    private static final int MAX_RANDOM_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private String color = colorSupplier.getRandomColor();
-
-    public int getMAX_RANDOM_NUMBER() {
-        return MAX_RANDOM_NUMBER;
-    }
-
-    public ColorSupplier getColorSupplier() {
-        return colorSupplier;
-    }
-
-    public void setColorSupplier(ColorSupplier colorSupplier) {
-        this.colorSupplier = colorSupplier;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    private int number = new Random().nextInt(MAX_RANDOM_NUMBER) + 1;
 
     public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(MAX_RANDOM_NUMBER) + 1;
+
         return new Ball(color, number);
     }
 }
