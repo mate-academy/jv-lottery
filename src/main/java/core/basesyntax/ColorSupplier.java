febbec70c1c6be7a110056ccb9c35d36ebdc6292
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-08-03 16:57:50 +0300

    fix: VisibilityModifier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Color[] colors = Color.values();
-    int randomIndex = new Random().nextInt(colors.length);
+    public Color[] getColors() {
+        return colors;
+    }
+
+    public int getRandomIndex() {
+        return randomIndex;
+    }
+
+    private Color[] colors = Color.values();
+    private int randomIndex = new Random().nextInt(colors.length);
 
     public String getRandomColor() {
         return colors[randomIndex].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,27 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static final int MAX_RANDOM_NUMBER = 100;
-    ColorSupplier colorSupplier = new ColorSupplier();
-    String color = colorSupplier.getRandomColor();
-    int number = new Random().nextInt(MAX_RANDOM_NUMBER) + 1;
+    private final int MAX_RANDOM_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private String color = colorSupplier.getRandomColor();
+
+    public int getMAX_RANDOM_NUMBER() {
+        return MAX_RANDOM_NUMBER;
+    }
+
+    public ColorSupplier getColorSupplier() {
+        return colorSupplier;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    private int number = new Random().nextInt(MAX_RANDOM_NUMBER) + 1;
 
     public Ball getRandomBall() {
         return new Ball(color, number);
