ef4591feca3491faaabfe70b4bf20e9bce9ce440
Olena Stankevych <yelenastankevych@gmail.com>
2023-08-06 22:05:55 +0300

    Refactored ColorSupplier and Lottery classes (added static initialization)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static Random random;
+
+    static {
+        random = new Random();
+    }
+
     public String getRandomColor() {
-        int colorIndex = new Random().nextInt(Colors.values().length);
+        int colorIndex = random.nextInt(Colors.values().length);
         return Colors.values()[colorIndex].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,16 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    private static final ColorSupplier colorSupplier;
+    private static final ColorSupplier COLOR_SUPPLIER;
+    private static final Random RANDOM;
 
     static {
-        colorSupplier = new ColorSupplier();
+        COLOR_SUPPLIER = new ColorSupplier();
+        RANDOM = new Random();
     }
 
     public Ball getRandomBall() {
-        int number = new Random().nextInt(100);
-        String color = colorSupplier.getRandomColor();
+        int number = RANDOM.nextInt(100);
+        String color = COLOR_SUPPLIER.getRandomColor();
         return new Ball(color, number);
     }
 }
