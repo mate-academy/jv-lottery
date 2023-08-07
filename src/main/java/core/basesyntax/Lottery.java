390bb5a515dee7f534f0446ffdc1c5611b8a13fd
evgenkolner <evgen.kolner@gmail.com>
2023-08-07 19:31:12 +0300

    added fixes #3 hw2.2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,6 @@ public class Lottery {
     private static final int QUANTITY = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private Ball ball = new Ball(random.nextInt(), colorSupplier.toString());
 
     public Ball getRandomBall() {
         return new Ball(random.nextInt(QUANTITY), colorSupplier.getRandomColor().name());
