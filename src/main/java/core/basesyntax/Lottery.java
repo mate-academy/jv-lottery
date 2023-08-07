0fbe8b4c75ccda919759903f5f147c6e98ea47e9
Stanislav Viner <stanislav.viner@gmail.com>
2023-08-07 16:20:18 +0300

    Added requested changes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,23 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final int maxNumber = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    public ColorSupplier getColorSupplier() {
+        return colorSupplier;
+    }
+
+    public void setColorSupplier(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
     public Ball getRandomBall() {
-        String str = new ColorSupplier().getRandomColor();
-        int index = new Random().nextInt(100) + 1;
-        return new Ball(str, index);
+        int number = new Random().nextInt(maxNumber) + 1;
+        return new Ball(colorSupplier.getRandomColor(), number);
     }
 }
