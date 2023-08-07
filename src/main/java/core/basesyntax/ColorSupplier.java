6977c72dd5c0a8e7e9604d8a33c2481194ce142e
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-08-07 12:49:10 +0300

    The static variable is created. The inner variable is created.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final int COLOR_QUANTITY = Color.values().length;
+
     public String getRandomColor() {
-        return String.valueOf(Color.values()[new Random().nextInt(Color.values().length)]);
+        int randomIndex = new Random().nextInt(COLOR_QUANTITY);
+        return String.valueOf(Color.values()[randomIndex]);
     }
 }
