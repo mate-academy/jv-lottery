fd85f2d797cbdbb1f62968ab2a55acaada8609d9
Teract10s <trs.leonov@gmail.com>
2023-08-08 20:48:13 +0300

    implements all tasks

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,23 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Ball[] balls = generateBalls(3);
+        printBallsInfo(balls);
         // create three balls using class Lottery and print information about them in console
+    }
+
+    private static Ball[] generateBalls(int size) {
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[size];
+        for (int i = 0; i < size; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+        return balls;
+    }
+
+    private static void printBallsInfo(Ball[] balls) {
+        for (Ball ball : balls) {
+            System.out.println(ball);
+        }
     }
 }
