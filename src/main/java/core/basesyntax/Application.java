3981cfbe09b3e9f13d064465f18f215651adc084
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-08-08 14:51:00 +0300

    fixes after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int MAX_COUNT_OF_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Ball[] balls = new Ball[MAX_COUNT_OF_BALLS];
