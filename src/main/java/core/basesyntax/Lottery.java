87824242d346eb835e6816a11c46dd9c7f89ff83
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-08-08 09:52:33 +0300

    improved code quality

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int number = new Random().nextInt(100);
         String color = colorSupplier.getRandomColor().name();
