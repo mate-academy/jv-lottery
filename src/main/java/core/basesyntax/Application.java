2a3298450d5533b672af014c28afbc88b3a8ceec
Yana Bilyk <bilyk.yn@gmail.com>
2023-08-07 21:15:54 -0400

    checked common mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int MAX_BALL_AMMOUNT = 3;
+    private static final int MAX_BALL_AMOUNT = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[MAX_BALL_AMMOUNT];
-        for (int i = 0;i < MAX_BALL_AMMOUNT; i++) {
+        Ball[] balls = new Ball[MAX_BALL_AMOUNT];
+        for (int i = 0; i < MAX_BALL_AMOUNT; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
