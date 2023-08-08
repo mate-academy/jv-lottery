898c7cf0b38b1f805559fc09b59eb3eae97aa971
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-08 20:08:18 +0300

    formatting enum Color

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public enum Color {
-    BLACK("Black"), BLUE("Blue"), GREEN("Green"), ORANGE("Orange"), PINK("Pink"), RED("Red"), WHITE("White"), YELLOW("Yellow");
+    BLACK("Black"),
+    BLUE("Blue"),
+    GREEN("Green"),
+    ORANGE("Orange"),
+    PINK("Pink"),
+    RED("Red"),
+    WHITE("White"),
+    YELLOW("Yellow");
 
     private final String color;
 
