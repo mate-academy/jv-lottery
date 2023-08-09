9cf0bb60ddc074127c96cb4fedd322fec4453d24
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-08-09 09:40:31 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,22 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Ball {
-    private static final int FIGURE_COUNT = 100;
-    private ColorSupplier colorSupplier = new ColorSupplier();
     private int number;
     private String color;
 
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
-    }
-
-    public void setColor(ColorSupplier color) {
-        this.color = colorSupplier.getRandomColor();
-    }
-
-    public void setNumber() {
-        this.number = new Random().nextInt(FIGURE_COUNT);
     }
 
     @Override
