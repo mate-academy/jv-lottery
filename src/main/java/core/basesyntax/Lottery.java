460c94fdabfa4b4ed23697f74744ff1f7f5cefa7
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-08-10 00:52:48 +0300

    tested in Application and fixed errors

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int BOUND_OF_NUMBER = 100;
-    Random random = new Random();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
