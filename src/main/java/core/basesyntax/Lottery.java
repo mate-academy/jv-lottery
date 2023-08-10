418840d73314f6929506983ec67311ebe406fda7
Vyacheslav <torbin.official@gmail.com>
2023-08-10 11:43:18 +0300

    Fixed syntax errors

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public final int MAX_NUMBER = 100;
+    private int maxNumber = 100;
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER));
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(maxNumber));
     }
 }
