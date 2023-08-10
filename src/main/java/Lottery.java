108e452119025794afe9456ddf159d438f60b02f
Eugene Bormotov <bormotovjenya2510@gmail.com>
2023-08-10 18:36:29 +0200

    Created a Ball class with the following fields: color and number and replaced the toString() method, also created an enum with different colors.   Implemented the getRandomColor() method in the ColorSupplier class, this method returns a random color based on enum values.   Created a Lottery class with a getRandomBall() method that returns a ball, this method creates a ball with a random color and a random number, the maximum number being 100.  Tested the program in the main() method.

--- src/main/java/Lottery.java
+++ src/main/java/Lottery.java
@@ -1,13 +1,12 @@
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int maxNumber = 100;
 
     public String getRandomBall() {
-
-        int randomNumber = random.nextInt(maxNumber);
+        int randomNumber = random.nextInt(MAX_NUMBER);
         return "We have such a ball: " + colorSupplier.getRandomColor() + " " + randomNumber;
     }
 }
