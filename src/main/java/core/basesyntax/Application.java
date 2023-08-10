4593289f21630a362457d6ea5a116495c1f3ad14
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-08-10 10:52:50 +0300

    Actually fixed code style errors now

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,11 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Application {
     private static final int BALL_COUNT = 3;
     private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
-        for(int i = 0; i < BALL_COUNT; i++) {
+        for (int i = 0; i < BALL_COUNT; i++) {
             System.out.println(lottery.getRandomBall().toString());
         }
     }
