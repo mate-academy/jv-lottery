3a95baebe6623238a2f33cc6728f028c70bc317c
Alex Rosenkreuz <141297005+AlexRosenkreuz@users.noreply.github.com>
2023-08-10 11:08:44 +0300

    Update src/main/java/core/basesyntax/Application.java
    Co-authored-by: Liliia Ponomarenko <96740815+liliia-ponomarenko@users.noreply.github.com>

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         for (int i = 0; i < BALL_COUNT; i++) {
-            System.out.println(lottery.getRandomBall().toString());
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
