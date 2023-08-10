88c75c4f3f0b9fb968f5cfc12a5aa78c10c31bd1
Yura Novak <yuranovakr@gmail.com>
2023-08-10 15:28:46 +0300

    lottery solution task.04

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final ColorSupplier supplierOfColors = new ColorSupplier();
+    private static final int NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(supplierOfColors.getRandomColor(), new Random().nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(NUMBER));
     }
 }
