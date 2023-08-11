05ab712753bd130cdbbdd793d0525118a257e770
Pavlo Stefiniv <pastcwdt@gmail.com>
2023-08-11 09:49:49 +0300

    Fixing constant, toString for name() and Random method-level to class level.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        int numberOfBallsToPrint = 3;
+        final int numberOfBallsToPrint = 3;
         Lottery lottery = new Lottery();
         for (int i = 0; i < numberOfBallsToPrint; i++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,8 @@ public class Lottery {
     private static final int NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
+    private Random random = new Random();
+
     public Ball getRandomBall() {
         return new Ball(new Random().nextInt(NUMBER), colorSupplier.getRandomColor());
     }
