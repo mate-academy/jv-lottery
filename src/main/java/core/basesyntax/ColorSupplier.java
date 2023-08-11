787b622f618d705ea8b2168bc9cd170b9c8976f3
Yevhen Kukharchuk <jancker19@ukr.net>
2023-08-11 10:15:09 +0300

    fix instances - deleted statis instances

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Random randomIndex = new Random();
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = randomIndex.nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBERS_BOUND = 100;
-    private static final Random random = new Random();
-    private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
