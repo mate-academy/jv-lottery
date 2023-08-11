954b0e2e4013cbb86c47c4c9c3f82df44d78e45a
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-08-11 16:47:17 +0300

    fixed magic number max_ball_number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
+    private static final int MAX_BALL_NUMBER = 100;
     private final Random randomNumber = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
 
-        Ball randomBall = new Ball(colorSupplier.getRandomColor(), randomNumber.nextInt(100));
-        return randomBall;
+        return new Ball(colorSupplier.getRandomColor(), randomNumber.nextInt(MAX_BALL_NUMBER));
     }
 }
