b5bd23055364ca272b2a2729986c64ed80384362
Nazar <nazarmos13@gmail.com>
2023-08-11 16:58:45 +0300

    fixed mistakes in the several classes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,8 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private final ColorSupplier color = new ColorSupplier();
-
+    private final int numberOfNumbers = 100;
     private final Ball ball = new Ball(color.getRandomColor(), random.nextInt(numberOfNumbers));
-    private static final int numberOfNumbers = 100;
 
     public Ball getRandomBall() {
         return ball;
