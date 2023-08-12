9d502bc564e0c1e76c6f1cbb0d30147529c84fc7
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-12 20:42:26 +0300

    Fix common problems. Replace static method with non-static. Replace redundant temporary variable.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
+    public String getRandomColor() {
         Random random = new Random();
         Color[] colors = Color.values();
-        Color randomColor = colors[random.nextInt(colors.length)];
-        return randomColor.getColor();
+        return colors[random.nextInt(colors.length)].name();
     }
 }
