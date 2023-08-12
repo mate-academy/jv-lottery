a9621cc9b9c82fb9b6b7e754a4e81c2932b3e91b
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-12 20:19:31 +0300

    Create Lottery class with the BALL_NUMBER_RANDOM_BOUNDARY constant. Implement method getRandomNumber. Implement method getRandomBall.

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    private static final int BALL_NUMBER_RANDOM_BOUNDARY = 101;
+    public Ball getRandomBall() {
+        String randomColor = ColorSupplier.getRandomColor();
+        int randomNumber = getRandomNumber();
+        return new Ball(randomColor, randomNumber);
+    }
+
+    private int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(BALL_NUMBER_RANDOM_BOUNDARY);
+    }
+}
