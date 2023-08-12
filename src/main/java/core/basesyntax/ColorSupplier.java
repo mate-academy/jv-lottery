c9579a153ed8205d8553bb22b2dbe65f9cd32836
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-12 20:15:18 +0300

    Implement method getRandomColor for ColorSupplier class.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Random random = new Random();
+        Color[] colors = Color.values();
+        Color randomColor = colors[random.nextInt(colors.length)];
+        return randomColor.getColor();
     }
 }
