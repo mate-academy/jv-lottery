96596ad552b06d80c6b835dcde72c1ebbe54d6c8
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-12 20:41:21 +0300

    Fix common problems. For now create new object with for loop.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball firstRandomBall = lottery.getRandomBall();
-        Ball secondRandomBall = lottery.getRandomBall();
-        Ball thirdRandomBall = lottery.getRandomBall();
-        System.out.println(firstRandomBall + "\n" + secondRandomBall + "\n" + thirdRandomBall);
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        for(int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i].toString());
+        }
     }
 }
