3b76f993dd948203ab16cc07ba98fb28f7b50c56
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-12 20:23:30 +0300

    Create three balls objects using getRandomBall method of Lottery class. Print info about balls to the console.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,6 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        System.out.println(lottery.getRandomBall().toString());
+        Ball firstRandomBall = lottery.getRandomBall();
+        Ball secondRandomBall = lottery.getRandomBall();
+        Ball thirdRandomBall = lottery.getRandomBall();
+        System.out.println(firstRandomBall + "\n" + secondRandomBall + "\n" + thirdRandomBall);
     }
 }
