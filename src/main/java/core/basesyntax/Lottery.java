b98244724827989f062b85317b8f71337d4d0f4c
Erik <erik.lodkorikh@gmail.com>
2023-08-13 23:54:11 +0200

    Rename method and variable

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,18 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int FIGURE_COUNT = 100;
+    private static final int MAX_FIGURE_RANGE = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
-    public int getRmInt() {
-        return random.nextInt(FIGURE_COUNT) + 1;
+    private int getRandomBallNamber() {
+        return random.nextInt(MAX_FIGURE_RANGE) + 1;
     }
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(getRmInt());
+        ball.setNumber(getRandomBallNamber());
         return ball;
     }
 }
