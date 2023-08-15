ca1b12bae671e5a778a446207b63bceefae1699f
Anastasiia <anastasiiafi7@gmail.com>
2023-08-15 09:44:48 +0300

    color/ colour changed

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public enum Colour {
+public enum Color {
     BLUE,
     PINK,
     GREY,
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,7 +6,7 @@ public class ColorSupplier {
     private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = new Random().nextInt(Colour.values().length);
-        return Colour.values()[index].name().toLowerCase();
+        int index = new Random().nextInt(Color.values().length);
+        return Color.values()[index].name().toLowerCase();
     }
 }
