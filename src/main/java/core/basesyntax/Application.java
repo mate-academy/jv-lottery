bbc0ac239610031edc28d2a06913755c3f51971f
Andrii Mikhaylovskiy <Andrew.chase999@gmail.com>
2023-08-15 23:37:21 +0300

    added fixes after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,14 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        int numberOfBalls = 3; // Change this to the desired number of balls
-        Ball[] balls = new Ball[numberOfBalls];
-
-        for (int i = 0; i < numberOfBalls; i++) {
+        int numbersOfBalls = 3; //для обрання кількості куль (за умовою повинно бути 3)
+        Ball[] balls = new Ball[numbersOfBalls];
+        for (int i = 0; i < numbersOfBalls; i++) {
             balls[i] = Lottery.getRandomBall();
         }
 
-        for (int i = 0; i < numberOfBalls; i++) {
+        for (int i = 0; i < numbersOfBalls; i++) {
             System.out.println("Ball " + (i + 1) + ": " + balls[i]);
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     public static Ball getRandomBall() {
         Color randomColor = ColorSupplier.getRandomColor();
-        int randomNumber = new Random().nextInt(100) + 1; // Generate number between 1 and 100
+        int randomNumber = new Random().nextInt(100) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
