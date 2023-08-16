bd24c9cb3ca4610f9cc128b87e459942f33bd879
katym <katy.mozharovska@gmail.com>
2023-08-16 18:21:24 +0300

    required changes added

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
+    private Random random = new Random();
 
-        return Colors.values()[index].toString();
+    public String getRandomColor() {
+        int index = random.nextInt(Color.values().length);
+
+        return Color.values()[index].name();
     }
 }
