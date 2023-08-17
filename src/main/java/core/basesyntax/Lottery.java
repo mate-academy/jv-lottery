5120997bb4a859391f6cf4f821fe734b2a495b1f
LeonIllya <dd0733567@gmail.com>
2023-08-17 10:33:26 +0300

    first commit

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public final int bound = 100;
+
+    public String getRandomBall() {
+        Random random = new Random();
+        ColorSupplier supplier = new ColorSupplier();
+        return "color: " + supplier.getRandomColor() + " number: " + random.nextInt(bound);
+    }
+}
