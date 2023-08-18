cfb7a0dcfbe41568a7b0f618e268d561c09f537d
Andrii Mikhaylovskiy <Andrew.chase999@gmail.com>
2023-08-18 19:06:23 +0300

    -extract Random to a class field

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         Color color = Color.values()[index];
