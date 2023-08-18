c46a513b14ab36b3954cf4e3f5bab7e9de2757e0
LeonIllya <dd0733567@gmail.com>
2023-08-18 14:44:20 +0300

    fixed remarks

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Lottery {
     private static final int BOUND = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
