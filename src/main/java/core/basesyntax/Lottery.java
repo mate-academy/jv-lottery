c9361f404f8e91b0c44ca928ae30ddfc6ae24f1f
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2023-08-19 21:27:51 +0300

    took style-problems from class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_OF_BOUND = 100;
     private Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(random.nextInt(NUMBER_OF_BOUND), colorSupplier.getRandomColor());
