b30059b0eb0bd0094bae8009b232e85b7c1371d6
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2023-08-19 21:16:03 +0300

    resolution hw-2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int NUMBER_BOUND = 100;
+    private static final int NUMBER_OF_BOUND = 100;
     private Random random = new Random();
     ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(random.nextInt(NUMBER_BOUND), colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(NUMBER_OF_BOUND), colorSupplier.getRandomColor());
     }
 }
