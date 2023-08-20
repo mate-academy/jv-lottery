fbbc3939d28364a4f00b0995f15ee1d5da220bf1
Oleksii Shpak <willwildred@gmail.com>
2023-08-20 17:43:34 +0200

    Lottery now creating new random ball with color and number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,6 +1,16 @@
 package core.basesyntax;
+
+import java.util.Random;
+
 public class Lottery {
+    private static final int MAXIMUM_NUMBER = 100;
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Ball ball = new Ball();
+
     public Ball getRandomBall() {
-        return new Ball();
+        ball.color = colorSupplier.getRandomColor();
+        ball.number = random.nextInt(MAXIMUM_NUMBER);
+        return ball;
     }
 }
