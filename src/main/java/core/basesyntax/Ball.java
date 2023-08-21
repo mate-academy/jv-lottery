cfe0165ce2d8262ef1e65f2eb57bc20ee3dd8f6a
Oleksii Shpak <willwildred@gmail.com>
2023-08-21 12:04:20 +0200

    changed public fields to private added constructor to class added .name method of ENUM inside toString-override

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,13 +1,18 @@
 package core.basesyntax;
 
 public class Ball{
-    public Color color;
-    public int number;
+    private Color color;
+    private int number;
+
+    public Ball (Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
 
     @Override
     public String toString() {
         return "Ball{" +
-                "color=" + color +
+                "color=" + color.name() +
                 ", number=" + number +
                 '}';
     }
