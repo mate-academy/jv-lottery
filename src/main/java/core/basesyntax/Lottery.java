c0759efcf15cd357b18b691a5f6b9fb4cf54abd8
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 12:35:32 +0300

    fixed checkstyle issue

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery extends ColorSupplier {
     private static final Random random = new Random();
+
     public Ball getRandomBall() {
         String randomColor = getRandomColor();
         int randomNumber = random.nextInt(100);
