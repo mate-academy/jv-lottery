425debf27b4affd98d9a51705c7ac9d925914414
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 12:16:55 +0300

    added for loop in Application for object creation

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-        System.out.println(ball1 + "\n" + ball2 + "\n" + ball3);
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
+        }
     }
 }
