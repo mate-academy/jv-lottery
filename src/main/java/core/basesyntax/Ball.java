1f76fc0d863efcbac2afd8056d83c48247312ec3
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 10:43:25 +0300

    added overridden toString() method

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,4 +3,12 @@ package core.basesyntax;
 public class Ball {
     private String color;
     private int number;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
 }
