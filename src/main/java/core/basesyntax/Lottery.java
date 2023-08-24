7a8bde47a2809b3cb5710f12ddc86b74aa0ecbc8
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 14:31:25 +0300

    renamed Colors and created constant for magic num

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends ColorSupplier {
+    private static final int BALL_NUM_COUNT = 100;
     private static final Random random = new Random();
 
     public Ball getRandomBall() {
         String randomColor = getRandomColor();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(BALL_NUM_COUNT);
         return new Ball(randomColor, randomNumber);
     }
 }
