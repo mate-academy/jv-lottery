01c6c2d2d76920e393ae17d0633571d148acaf40
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 12:54:19 +0300

    added getters and setters

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -12,6 +12,22 @@ public class Ball {
     public Ball() {
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     @Override
     public String toString() {
         return "Ball{"
