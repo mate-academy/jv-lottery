a3da56aeb743f58c0a4abcdefb233b0763a7daae
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 14:21:48 +0300

    Random moved to the class level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier extends Ball {
+    private static final Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
+        int index = random.nextInt(Colors.values().length);
         Colors color = Colors.values()[index];
         return color.name();
     }
