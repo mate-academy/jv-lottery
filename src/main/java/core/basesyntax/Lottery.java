29ce7a5d41cbf5a5c8a5f950ba2eb2b64b9e6aca
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-24 12:28:40 +0300

    edited random as a single instance

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends ColorSupplier {
+    private static final Random random = new Random();
     public Ball getRandomBall() {
         String randomColor = getRandomColor();
-        Random random = new Random();
         int randomNumber = random.nextInt(100);
         return new Ball(randomColor, randomNumber);
     }
