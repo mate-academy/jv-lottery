b4ba7e40e0eac3446ab3a7516191a1d49c69bfe4
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-08-25 16:34:27 +0300

    redesigned Lottery.java logic (added composition instead of inheritance)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,12 +2,21 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
     private static final int BALL_NUM_COUNT = 100;
     private static final Random random = new Random();
 
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Lottery(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
+    public Lottery() {
+    }
+
     public Ball getRandomBall() {
-        String randomColor = getRandomColor();
+        String randomColor = colorSupplier.getRandomColor();
         int randomNumber = random.nextInt(BALL_NUM_COUNT);
         return new Ball(randomColor, randomNumber);
     }
