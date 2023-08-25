77e0227afd463405b23988dc0dbdbcbd2738e3d8
Ivan Kusurhashev <ivankusur@gmail.com>
2023-08-25 17:09:56 +0300

    task2Lottery-OOP, fixed all changes made suggested Mentors,  (Maven test pass)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,10 +4,10 @@ public class Application {
     private static final int NUMBER_OF_BALL = 3;
 
     public static void main(String[] args) {
-        Lottery lotteryObject = new Lottery();
+        Lottery lottery = new Lottery();
         for (int i = 0; i < NUMBER_OF_BALL; i++) {
-            Ball b = lotteryObject.getRandomBall();
-            System.out.println(b);
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,10 +1,19 @@
 package core.basesyntax;
 
-public class Lottery extends ColorSupplier {
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        String color = getRandomColor();
+        String color = colorSupplier.getRandomColor();
         int number = getRandomNumber();
         return new Ball(color, number);
+    }
+
+    public int getRandomNumber() {
+        return random.nextInt(MAX_NUMBER);
     }
 }
