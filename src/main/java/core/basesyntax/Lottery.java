36598175191a4f32908f8ebbc112eeeb1c43726f
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-08-26 22:35:01 +0200

    Created Ball class that contains information about Ball and overrided method toString(). Created enum Color that contains colors for balls and CollorSupplier class that return a random color. Created Lottery class that use ColorSupplier to generate random color for ball and also generates a random number for ball. In Application class inizializated main method that create and print information about 3 unique balls.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random randomNumber = new Random();
-    ColorSupplier randomColor = new ColorSupplier();
+    private Random randomNumber = new Random();
+    private ColorSupplier randomColor = new ColorSupplier();
 
     public Ball getRandomBall() {
         int ballNumber = randomNumber.nextInt(100);
