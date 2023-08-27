c61a752fb51861cd9e82882e849cf8c6e2c24603
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-08-27 12:10:50 +0200

    Combined filling balls array and printing info from that array in one cycle. Extracted random as class variable in ColorSupplier class. In Lottery class created constant value that contains max ball number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int MAX_BALL_NUMBER = 100;
     private Random randomNumber = new Random();
     private ColorSupplier randomColor = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int ballNumber = randomNumber.nextInt(100);
+        int ballNumber = randomNumber.nextInt(MAX_BALL_NUMBER);
         Color ballColor = randomColor.getRandomColor();
         return new Ball(ballColor.toString(), ballNumber);
     }
