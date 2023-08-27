95d6643d1bf253a5dcad0d0d9520c19c3c03acbc
Egor Reshetnik <mwm3335@gmail.com>
2023-08-27 22:32:14 +0300

    created and return ball

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Ball ball = new Ball(); // color and number are created in the default constructor
+        return ball;
+    }
+}
