d2816588c072dd472782e38159fe8b52e638572a
Egor Reshetnik <mwm3335@gmail.com>
2023-08-27 22:33:57 +0300

    created 3 balls and return information about them

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Application {
     public static void main(String[] args) {
-        for (int i = 0; i < 3 ; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(new Lottery().getRandomBall().toString());
         }
     }
