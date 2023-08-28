74ddcdcfe8890b50b90bfd144dc239091d907a59
Egor Reshetnik <mwm3335@gmail.com>
2023-08-28 13:31:30 +0300

    add random field and add 2 set methods

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private Random random = new Random();
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(random.nextInt(100));
         ball.setColor(new ColorSupplier().getRandomColor());
         return ball;
     }
