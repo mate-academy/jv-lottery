0ce7d1580995439908baeb581dca10c100c095e7
Petro Smoliar <smp22pl@gmail.com>
2023-08-30 14:49:48 +0200

    changed names

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBERS_LIMIT = 100;
-    private final ColorSupplier colorBall = new ColorSupplier();
+    private final ColorSupplier color = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorBall.getRandomColor(), random.nextInt(NUMBERS_LIMIT));
+        return new Ball(color.getRandomColor(), random.nextInt(NUMBERS_LIMIT));
     }
 }
