9541ec76f90911981002bb5519f8e442aeb34171
Petro Smoliar <smp22pl@gmail.com>
2023-08-30 14:30:07 +0200

    removed empty lines

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBERS_LIMIT = 100;
     private final ColorSupplier colorBall = new ColorSupplier();
-    private final Random numberBall = new Random();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorBall.getRandomColor(), numberBall.nextInt(NUMBERS_LIMIT));
+        return new Ball(colorBall.getRandomColor(), random.nextInt(NUMBERS_LIMIT));
     }
 }
