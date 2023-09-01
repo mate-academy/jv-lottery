f557b2fac2e6a8a917bf4dfc202cf5e25455157b
Yurii <wervmer>
2023-09-01 15:59:18 +0300

    implemented getRandomColour method, changet method type: String -> Color enum

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import core.basesyntax.models.Color;
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    private static final int COLOR_COUNT = 8;
+
+    public Color getRandomColor() {
+        return Color.values()[new Random().nextInt(COLOR_COUNT)];
     }
 }
