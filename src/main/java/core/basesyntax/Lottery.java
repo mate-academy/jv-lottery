896c1c62c5b6eb5d9d38ff957b3807a0f4689aa6
Ruslan Pankiv <haspyd@.com>
2023-09-02 08:21:37 +0300

    Installed maven, executed mvn clean package, resolved errors

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    static Random randomNumber = new Random();
+    private static Random randomNumber = new Random();
+
+    public static Random getRandomNumber() {
+        return randomNumber;
+    }
+
+    public static void setRandomNumber(Random randomNumber) {
+        Lottery.randomNumber = randomNumber;
+    }
+
     public static Ball getRandomBall() {
         Ball rundomBall;
         rundomBall = new Ball(ColorSupplier.getRandomColor(), randomNumber.nextInt(100));
