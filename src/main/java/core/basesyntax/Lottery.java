787a17d56a8ffa570025588953d9250fb5e8fe07
Evgen <wizardw093@gmail.com>
2023-09-04 15:56:24 +0300

    fixed checkstyle errors_2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public ColorSupplier getColorSupplier() {
+        return colorSupplier;
+    }
+
+    public void setColorSupplier(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
