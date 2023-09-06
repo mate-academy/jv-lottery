aa8539abac100cb578aac36840a6ef5a6cad8cbb
Vadym Stanishevskyi <mainvadym@gmail.com>
2023-09-07 01:41:56 +0200

    changed return data type from Color to String in getRandomColor() method, created 3 random balls in array using class Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int NUNBER_OF_BALLS = 3;
+    private static final int NUMBER_OF_BALLS = 3;
     private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
-        for (int i = 0; i < NUNBER_OF_BALLS; i++) {
-            Ball ball = lottery.getRandomball();
-            System.out.println(ball);
+        Ball[] ball = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            ball[i] = lottery.getRandomball();
+            System.out.println(ball[i]);
         }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,7 +6,7 @@ public class ColorSupplier {
     private static final int COLORS_COUNT = Color.values().length;
     private final Random random = new Random();
 
-    public Color getRandomColor() {
-        return Color.values()[random.nextInt(COLORS_COUNT)];
+    public String getRandomColor() {
+        return Color.values()[random.nextInt(COLORS_COUNT)].name();
     }
 }
