63c118fc355fd2e012639386cc26ea73783415c2
Marta Zatserkovna <mzatserkovna10@gmail.com>
2023-09-06 17:57:27 +0300

    created new classes, methods, random and enum

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    BLUE,
+    PINK,
+    WHITE,
+    YELLOW,
+    PURPLE,
+    GREEN,
+    BLACK
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        Color randomColor = Color.valueOf(new ColorSupplier().getRandomColor());
+        int randomNumber = new Random().nextInt(100) + 1;
+        return new Ball(randomColor, randomNumber);
+    }
+}
