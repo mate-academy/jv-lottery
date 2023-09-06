2a7297ca5776fbb8f7b38dac1768fe42875e70d2
Vadym Stanishevskyi <mainvadym@gmail.com>
2023-09-06 02:17:59 +0200

    rewrote methot getRandomColor and moved random to the class field

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,10 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class ColorSupplier extends Ball {
-
-    public static Colors getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index];
+public class ColorSupplier {
+    private final int colorsCount = Color.values().length;
+    private final Random random = new Random();
+    public Color getRandomColor() {
+        int colorIndex = random.nextInt(colorsCount);
+        return Color.values()[colorIndex];
     }
 }
