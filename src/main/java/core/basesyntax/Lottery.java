b50c0aa8f0125c1f92a82ed1d7bf647075ffe0dd
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-09-08 16:01:52 +0300

    extract classes as class variables

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_LIMIT = 100;
+
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int number = new Random().nextInt(99) + 1;
-        return new Ball(new ColorSupplier().getRandomColor(), number);
+        int number = random.nextInt(NUMBER_LIMIT);
+        return new Ball(colorSupplier.getRandomColor(), number);
     }
-
 }
