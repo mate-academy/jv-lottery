2f7686f59ffd3ca11e6d994c42f7c68f07a4302c
Vladyslav Druhov <space.drugov@gmail.com>
2023-09-09 12:29:02 +0300

    Create simple lottery solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    private static final Color[] ALL_COLORS = Color.values();
+    private static final Random random = new Random();
+
+    public static Color getRandomColor(){
+        int index = random.nextInt(ALL_COLORS.length);
+        return ALL_COLORS[index];
     }
 }
