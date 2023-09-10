f1ca8b898a800b26f5b3203799106466cbd2ce3c
Marta Zatserkovna <mzatserkovna10@gmail.com>
2023-09-10 10:13:01 +0300

    added fixes after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,7 @@ public class Application {
         Lottery lottery = new Lottery();
         // create three balls using class Lottery and print information about them in console
         for (int i = 0; i < 3; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println("Ball " + (i + 1) + ": " + ball);
+            System.out.println("Ball " + (i + 1) + ": " + lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random random = new Random();
+    private static final Random random = new Random();
 
     public static Ball getRandomBall() {
         final Color randomColor = Color.valueOf(new ColorSupplier().getRandomColor());
-        final int randomNumber = new Random().nextInt(100) + 1;
+        final int randomNumber = random.nextInt(100);
         return new Ball(randomColor, randomNumber);
     }
 }
