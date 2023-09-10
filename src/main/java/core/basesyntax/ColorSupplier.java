922377d53a5fe47a848896ab9c35d0dd865e836f
Vladyslav Druhov <space.drugov@gmail.com>
2023-09-10 17:07:33 +0300

    Added all the changes to the checklist.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final Color[] ALL_COLORS = Color.values();
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
-    public static Color getRandomColor(){
-        int index = random.nextInt(ALL_COLORS.length);
+    public static Color getRandomColor() {
+        int index = RANDOM.nextInt(ALL_COLORS.length);
         return ALL_COLORS[index];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    static Ball getRandomBall() {
         Color randomColor = ColorSupplier.getRandomColor();
         int randomNumber = new Random().nextInt(100) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
+
