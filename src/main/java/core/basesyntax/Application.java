28d0d988139c8ba948864f1c04c8ed6ef64d5627
Tetiana Koval <tetyanak0102@gmail.com>
2023-09-12 12:25:15 +0300

    implemented Lottery and Ball classes.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        System.out.println(Lottery.getRandomBall());
+        System.out.println(Lottery.getRandomBall());
+        System.out.println(Lottery.getRandomBall());
     }
 }
---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    BLUE,
+    BLACK,
+    YELLOW,
+    WHITE,
+    ORANGE,
+    PINK,
+    PURPLE,
+    GREEN
+}
