09c95bb13645bfce1a7c726eafe7b50a22b03e71
Roman Kuzyk <kuzya228337@mail.com>
2023-09-13 21:26:40 +0300

    added fixes after review again

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -13,7 +13,7 @@ public class Lottery {
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = new Random().nextInt(100) + 1;
+        int randomNumber = random.nextInt(100) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
