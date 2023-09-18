9c4e3d6eb8c9fe1b2a920bf5a7de1f49e9e44fcc
Jakub Sekowski <sekowskijakub@gmail.com>
2023-09-18 20:42:54 +0200

    Fixes after code review: - removed Ball Array (now only one ball is created) - added parameterless constructor in Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,6 +4,9 @@ public class Ball {
     private String color;
     private int number;
 
+    public Ball() {
+    }
+
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
