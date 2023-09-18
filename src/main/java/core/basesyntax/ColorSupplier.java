205d26f2c65db06ddfceb7ca2e670c056d93948a
Banderan <bartoszwojcik2004@gmail.com>
2023-09-18 15:28:44 +0200

    adding default modifier & separate method for maven in pullrequest

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-     private int index = new Random().nextInt(Colors.values().length);
+    int index = new Random().nextInt(Colors.values().length);
+
     public String getRandomColor() {
         Colors colors = Colors.values()[index];
         return colors.toString();
