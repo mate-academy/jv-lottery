105192a93274bc7e014a305d62003e2f94861522
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-09-19 20:04:28 +0300

    Added new classes and methods.

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_NUMBER = 100;
+
+    public Ball getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(MAX_NUMBER));
+    }
+}
