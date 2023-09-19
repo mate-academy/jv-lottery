5d526f670dd3d6a84b609889067d9788629d0948
almanac1914 <144943174+almanac1914@users.noreply.github.com>
2023-09-19 10:35:38 -0600

    Errors corrected

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+    
     public static void main(String[] args) {
         ColorSupplier colorSupplier = new ColorSupplier();
         Lottery lottery = new Lottery(colorSupplier);
         
-        Ball randomBall1 = lottery.getRandomBall();
-        Ball randomBall2 = lottery.getRandomBall();
-        Ball randomBall3 = lottery.getRandomBall();
-
-        System.out.println("Random Ball 1: " + randomBall1);
-        System.out.println("Random Ball 2: " + randomBall2);
-        System.out.println("Random Ball 3: " + randomBall3);
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            Ball randomBall = lottery.getRandomBall();
+            System.out.println("Random Ball: " + randomBall);
+        }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,7 +6,7 @@ public class ColorSupplier {
     private Random random = new Random();
     
     public String getRandomColor() {
-        ColorEnum[] colors = ColorEnum.values();
-        return colors[random.nextInt(colors.length)].toString();
+        Color[] colors = Color.values();
+        return colors[random.nextInt(colors.length)].name();
     }
 }
