82feee1fb6466d6ad6334998e38b21a99f7a5034
unknown <liubchenkalexander@gmail.com>
2023-09-19 23:34:08 +0300

    fixed mvn problems

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[3];
-        for(int i = 0; i < balls.length; i++) {
+        for (int i = 0; i < balls.length; i++) {
             balls[i] = new Ball();
         }
         System.out.println(Arrays.toString(balls));
