1f5bd69b213cfcc399df61ffede8425070f5d230
Jakub Sekowski <sekowskijakub@gmail.com>
2023-09-20 11:31:48 +0200

    Fixes after code review: - fixed use of constant in Lottery class and constant's name for better understanding of the code

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int LIMIT_OF_NUMBERS = 101;
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(LIMIT_OF_NUMBERS));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER + 1));
     }
 }
