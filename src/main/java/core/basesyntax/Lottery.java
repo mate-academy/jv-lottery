d5ea0b9dab0e5ef92fda8426041cf72f95607d6e
Jakub Sekowski <sekowskijakub@gmail.com>
2023-09-20 08:34:48 +0200

    Fixes after code review: - fixed Main() - fixed Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final int LIMIT_OF_NUMBERS = 101;
-    private Random randomValuesSupplier = new Random();
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int randomInt = randomValuesSupplier.nextInt(LIMIT_OF_NUMBERS);
-        ColorSupplier colorSupplier = new ColorSupplier();
-        return new Ball(colorSupplier.getRandomColor(), randomInt);
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(LIMIT_OF_NUMBERS));
     }
 }
