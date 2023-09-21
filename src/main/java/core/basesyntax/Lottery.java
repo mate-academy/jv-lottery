01d6674e5de9bd645df6aa3f901d1fe9acaed230
Nataliia <gzodchie@gmail.com>
2023-09-21 13:18:44 +0300

    added lottery task solving

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        ColorSupplier color = new ColorSupplier();
+        int number = new Random().nextInt(100);
+
+        return new Ball(color.getRandomColor(), number);
+    }
+}
