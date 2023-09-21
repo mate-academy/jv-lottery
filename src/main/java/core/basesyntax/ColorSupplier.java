3ef8aa558f7861bd46872f2360c03ae80640c4a1
AdamMudrak <mudrets2000@gmail.com>
2023-09-22 01:51:29 +0300

    Delete not needed TODO

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    //TODO probably we need to implement toString() from Ball.java somehow...
     public String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         Color color = Color.values()[index];
