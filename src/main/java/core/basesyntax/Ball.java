57d854a82d264daa46e9ece774408950fd6d6fa3
AdamMudrak <mudrets2000@gmail.com>
2023-09-22 01:46:27 +0300

    Add ball

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+//I didn't get how and why I should override toString() and why here
+public class Ball {
+    private String color;
+    private int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
