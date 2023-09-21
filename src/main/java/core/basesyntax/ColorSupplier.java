620713bd803d64192dcca15654a0f752794d6c44
AdamMudrak <mudrets2000@gmail.com>
2023-09-22 01:47:11 +0300

    Implement method getRandomColor()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    //TODO probably we need to implement toString() from Ball.java somehow...
     public String getRandomColor() {
-        return null;
+        int index = new Random().nextInt(Color.values().length);
+        Color color = Color.values()[index];
+        return color.name();
     }
 }
