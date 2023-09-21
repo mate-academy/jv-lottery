f6fbe3abe9bc6ae785b99590cb62497c9a9cad07
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-09-21 19:16:14 +0300

    created Ball class; overrode method .toString; created enum Color with colors; implemented method getRandomColor; implemented method getRandomBall;

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery newLottery = new Lottery();
+        Ball firstBall = newLottery.getRandomBall();
+        Ball secondBall = newLottery.getRandomBall();
+        Ball thirdBall = newLottery.getRandomBall();
+
+        System.out.println(firstBall);
+        System.out.println(secondBall);
+        System.out.println(thirdBall);
     }
 }
