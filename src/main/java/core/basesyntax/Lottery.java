7fa0dd7b934be871386b0176e3cd2b5e26d0e783
Vadim Kadasiev <144882268+VadimKadasiev@users.noreply.github.com>
2023-09-22 22:20:01 +0300

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,10 +1,7 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Lottery {
-    public String getRandomBall(){
-        int number = new Random().nextInt(100);
-        return new ColorSupplier().getRandomColor() + number;
+    public String getRandomBall() {
+        return new Ball().toString();
     }
 }
