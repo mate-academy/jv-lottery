5580403b8adda4020f38cb2b9deb5d85be991d10
Bohdan Bilko <rapsbodik@gmail.com>
2023-09-22 18:09:00 +0300

    added solution created Ball class, Colors enum, implement getRandomColor() in ColorSupplier class

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLACK,
+    WHITE,
+    RED,
+    GREEN,
+    BLUE,
+    PINK,
+    PURPLE,
+    YELLOW,
+    GREY,
+    ORANGE
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public String getRandomBall() {
+        Ball ball = new Ball();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Random random = new Random();
+
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(100));
+
+        return ball.toString();
+    }
+}
