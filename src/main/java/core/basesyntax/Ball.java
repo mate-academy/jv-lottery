90c1660b7e7638c62c824259508cc9c6aebf1859
Vadim Kadasiev <144882268+VadimKadasiev@users.noreply.github.com>
2023-09-23 13:30:07 +0300

    Update Ball 23/09.java

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,10 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Ball {
-    private final String COLOR = new ColorSupplier().getRandomColor();
-    private final int NUMBER = new  Random().nextInt(100);
+    private String color;
+    private int number;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     public String toString() {
-        return COLOR + " ball by number " + NUMBER;
+        return color + " ball by number " + number;
     }
 }
