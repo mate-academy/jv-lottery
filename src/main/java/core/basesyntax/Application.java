71d6e2f45d3c98dc278a711939d80bd6a4b2df94
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-09-23 18:02:44 +0300

    added a random number and random color if you click the button

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,18 +3,3 @@ package core.basesyntax;
 enum Color {
     RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE
 }
-class Ball {
-    private final Color color;
-    private final int number;
-
-    public Ball(Color color, int number) {
-        this.color = color;
-        this.number = number;
-    }
-
-    @Override
-    public String toString() {
-        return "Ball [color=" + color + ", number=" + number + "]";
-    }
-
-}
---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+class Ball {
+    private final Color color;
+    private final int number;
+
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+    @Override
+    public String toString() {
+        return "Ball [color=" + color + ", number=" + number + "]";
+    }
+
+}
