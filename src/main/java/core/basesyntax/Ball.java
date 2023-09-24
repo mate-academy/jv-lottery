caaf69a872086540a71cba4a3207675a5b80442a
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-09-24 14:47:01 +0300

    added a random number and random color if you click the button

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -8,6 +8,7 @@ class Ball {
         this.color = color;
         this.number = number;
     }
+
     @Override
     public String toString() {
         return "Ball [color=" + color + ", number=" + number + "]";
