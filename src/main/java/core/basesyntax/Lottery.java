635068c3e6c8271ec7bf25100a617a8c325ee7eb
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-09-25 18:31:42 +0300

    added a random number and random color if you click the button

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
+
 import java.util.Random;
+
 class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = new Random().nextInt(100)+ 1;
+        int randomNumber = new Random().nextInt(100) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
