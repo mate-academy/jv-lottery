f32388d795feac805357aae3aace39fbbd19997b
Oleh Shvets <olehpost69@gmail.com>
2023-09-26 21:15:55 +0300

    lottery_my_solution_changed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,23 +4,11 @@ import java.util.Random;
 
 public class ColorSupplier {
 
-    enum Color {
-    WHITE,
-    BlACK,
-        RED,
-        GREEN,
-        BLUE,
-        YELLOW,
-        ORANGE,
-        PURPLE;
-    }
-
     private static Random random = new Random();
 
     public static String getRandomColor() {
-
         Color[] color = Color.values();
         int randomIndex = random.nextInt(color.length);
-        return color[randomIndex].toString();
+        return color[randomIndex].name();
     }
 }
