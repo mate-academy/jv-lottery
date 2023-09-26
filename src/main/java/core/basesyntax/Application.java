603b61aa273f3d708df5e32ea1201c202eb6f062
Hlib Kulikov <forzen123212@proton.me>
2023-09-26 18:09:56 +0300

    Fixed checkstyle

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    static final int COUNT_OF_BALLS = 3;
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        int countOfBalls = 3;
 
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < countOfBalls; i++) {
+        Ball[] balls = new Ball[COUNT_OF_BALLS];
+        for (int i = 0; i < COUNT_OF_BALLS; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int maxNumber = 100;
+    private static final int MAX_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(),
-                random.nextInt(maxNumber) + 1);
+                random.nextInt(MAX_NUMBER) + 1);
     }
 }
