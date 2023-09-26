19773c0518f0d6980488fa6f3ecc33f9070ba27c
Hlib Kulikov <forzen123212@proton.me>
2023-09-26 18:17:17 +0300

    Fixed checkstyle

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     static final int COUNT_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
