dda1731699ede079d23227e0cbae221d84a46288
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-09-26 17:28:52 +0300

    For now Color colors is class field, not local variable.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Color[] colors = Color.values();
+
     public String getRandomColor() {
         Random random = new Random();
-        Color[] colors = Color.values();
         return colors[random.nextInt(colors.length)].name();
     }
 }
