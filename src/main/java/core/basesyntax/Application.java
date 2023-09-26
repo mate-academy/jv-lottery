e751cb8c896acdcdac53f9ddfd918cc254c23397
Igor Korolevich <igorkorolj@gmail.com>
2023-09-26 18:57:53 +0300

    created 3 lottery balls and their output

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball firstLottery = Lottery.getRandomBall();
+        Ball secLottery = Lottery.getRandomBall();
+        Ball thrtLottery = Lottery.getRandomBall();
+        System.out.println(firstLottery);
+        System.out.println(secLottery);
+        System.out.println(thrtLottery);
     }
 }
