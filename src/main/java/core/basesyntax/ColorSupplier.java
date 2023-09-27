43f18e511ab87b7cdeb42014efc7b689119e116e
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-09-28 02:42:52 +0300

    Created all the needed classes. Realised their functionality.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Color[] colors = Color.values();
+        Random random = new Random();
+        int randomIndex = random.nextInt(colors.length);
+        Color randColor = colors[randomIndex];
+        return randColor.toString();
     }
 }
