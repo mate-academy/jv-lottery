ca95ca67ab72c2766f7bab9146847cbc35e76942
Danil Ponomarenko <akame.dante13@gmail.com>
2023-09-27 13:09:26 +0300

    man clean package: Separated methods with new line.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_BOUND = 101;
     private final ColorSupplier supplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         Random random = new Random();
         return new Ball(supplier.getRandomColor(), random.nextInt(NUMBER_BOUND));
