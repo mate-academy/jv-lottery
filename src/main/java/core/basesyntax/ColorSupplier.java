55a74f812f7453e7e7665818b424a4a5fab8d237
Nataliia Shtykh <forstudying93@gmail.com>
2023-09-27 18:52:46 +0300

    fixed common mistakes (jv-lottery)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
+    public String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         Color color = Color.values()[index];
         return color.name();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        Random random = new Random();
-        Ball ball = new Ball(ColorSupplier.getRandomColor(), random.nextInt(100));
-        return ball;
+    Random random = new Random();
+    public Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
     }
 }
