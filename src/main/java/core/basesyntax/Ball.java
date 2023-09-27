7b14d0b8a33c0fa2f37e5a3ebdb4ac1db1679893
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-27 15:59:07 +0300

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,11 +4,9 @@ public class Ball {
     private int number;
     private String color;
 
-
     public void setNumber(int number) {
         this.number = number;
     }
-
 
     public void setColor(String color) {
         this.color = color;
