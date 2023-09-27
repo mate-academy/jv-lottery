c585125c41cc2d83efd1ce65f4210d7930e41725
Myself <wetball74@gmail.com>
2023-09-27 19:59:10 +0300

    mvn clear

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-   public  String color;
-   public  int number;
+    private String color;
+    private int number;
 
     public Ball(Colors color, int number) {
         this.color = String.valueOf(color);
@@ -11,6 +11,6 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "number - " + number +  "  " + "color - " + color;
+        return "number - " + number + "  " + "color - " + color;
     }
 }
