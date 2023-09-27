83cd8f497225a39a81ae19942f8bc5d0be3e6da0
Igor Korolevich <igorkorolj@gmail.com>
2023-09-27 23:00:26 +0300

    added an array of colors to the class level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Colors[] allColors;
+    public ColorSupplier() {
+        allColors = Colors.values();
+    }
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].getColor();
+        int index = new Random().nextInt(allColors.length);
+        return allColors[index].getColor();
     }
 }
