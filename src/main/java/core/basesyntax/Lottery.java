3aa3114ed05747e517c13b4169466a1bd7863f74
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-27 15:54:26 +0300

    created the required classes, implemented the methods

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Random random = new Random();
+        Ball ball = new Ball();
+        ColorSupplier color = new ColorSupplier();
+        ball.setNumber(random.nextInt(100));
+        ball.setColor(color.getRandomColor());
+        return ball;
+    }
+}
