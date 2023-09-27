5f47982c0a254eda2cf25e9aa5a137b915fbf9c2
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-27 15:57:00 +0300

    removed getters for number & color

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,17 +4,11 @@ public class Ball {
     private int number;
     private String color;
 
-    public int getNumber() {
-        return number;
-    }
 
     public void setNumber(int number) {
         this.number = number;
     }
 
-    public String getColor() {
-        return color;
-    }
 
     public void setColor(String color) {
         this.color = color;
