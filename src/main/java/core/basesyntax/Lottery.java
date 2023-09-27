a34dd541c30e5c5cf402dfa894cf296186b8acec
TkachukTara <zaz42988@gmail.com>
2023-09-27 14:02:40 +0300

    100 is a magic number. Add it as constant field MAX_BALL_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int MAX_BALL_NUMBER = 100;
 
     public Ball getRandomBall() {
-        int ballNumber = random.nextInt(100); // Generate Random Number for ball
+        int ballNumber = random.nextInt(MAX_BALL_NUMBER); // Generate Random Number for ball
         Ball ball = new Ball(ballNumber, colorSupplier.getRandomColor());
         return ball;
     }
