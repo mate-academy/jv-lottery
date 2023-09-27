52a5687de721789d7c3de413c75c7f59fcc96a5a
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-09-27 14:37:42 +0300

    fixed codestyle

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_USERS = 3;
+
     public static void main(String[] args) {
         Lottery newLottery = new Lottery();
 
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
     private Random random = new Random();
     private Color[] colors = Color.values();
+
     public Color getRandomColor() {
         return colors[random.nextInt(colors.length)];
     }
