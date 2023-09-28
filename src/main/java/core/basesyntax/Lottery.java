982bb4816a7d7a384592a64d1163c7526e5084ee
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-09-28 15:27:35 +0300

    Remade int NUMBER_OF_BALLS as a constant Fixed value constant MAXIMUM_POSSIBLE_BALL_NUMBER Removed toString() method from sout in loop for in Application

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAXIMUM_POSSIBLE_BALL_NUMBER = 100;
+    private static final int MAXIMUM_POSSIBLE_BALL_NUMBER = 101;
     private ColorSupplier ballColor = new ColorSupplier();
     private Random random = new Random();
 
