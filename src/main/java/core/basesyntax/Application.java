b0aa416e7a024dff9fb40ecd75f0511aa6f0456b
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-09-28 12:29:20 +0300

    checkstyle fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Ball[] lotteryBalls = new Ball[NUMBER_OF_BALLS];
