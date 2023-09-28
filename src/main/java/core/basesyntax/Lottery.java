80ee11f302c5f9d432f406960b336c8b57a1fc81
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-28 12:14:45 +0300

    fixed checkstyle mistakes after the previous commit

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random random = new Random();
-    Ball ball = new Ball();
-    ColorSupplier color = new ColorSupplier();
+    private Random random = new Random();
+    private Ball ball = new Ball();
+    private ColorSupplier color = new ColorSupplier();
+
     public Ball getRandomBall() {
         ball.setNumber(random.nextInt(101));
         ball.setColor(color.getRandomColor());
