25bbac76ad2c868dc2073d21a194f47bc77837f8
Vadym Hurnik <vad.com.z@gmail.com>
2023-09-28 11:43:37 +0300

    fixed numerous objects of Random class creating, changed constant value, reworked lottery creation

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_COUNT = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
+        for (int i = 0; i < BALLS_COUNT; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Lottery {
-    private static final int MAX_BALL_NUMBER = 100;
+    private static final int MAX_BALL_NUMBER = 101;
     private ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(supplier.getRandomColor(), new Random().nextInt(MAX_BALL_NUMBER));
+        return new Ball(supplier.getRandomColor(), supplier.getRandom().nextInt(MAX_BALL_NUMBER));
     }
 }
