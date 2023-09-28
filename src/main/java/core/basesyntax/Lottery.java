372051f0f0845d49db51c7998f35d33daffbb429
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-09-28 23:08:13 +0300

    created classes and implemented methods described in README.md; created 3 balls and printed information about them

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(101));
+    }
+}
