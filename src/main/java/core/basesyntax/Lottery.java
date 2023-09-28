8dc0f9a454528949a3f80d5b5911aa634a8ac1a9
Igor Korolevich <igorkorolj@gmail.com>
2023-09-28 11:22:40 +0300

    added colorSupplier, Random to class-level.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 101;
-    private Ball lotteryBall;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        lotteryBall = new Ball();
-        lotteryBall.setColor(new ColorSupplier().getRandomColor());
-        lotteryBall.setNumber(new Random().nextInt(MAX_BALL_NUMBER));
+        Ball lotteryBall = new Ball();
+        lotteryBall.setColor(colorSupplier.getRandomColor());
+        lotteryBall.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return lotteryBall;
     }
 }
