8eb430b590efe10cca3c147cec948fd9e4656f24
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-09-28 12:47:02 +0300

    optimized Lottery class and constant value changed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_BALL_NUMBER = 100;
+    private static final int MAX_BALL_NUMBER = 101;
+    private Random random = new Random();
+    private ColorSupplier randomColor = new ColorSupplier();
 
     public Ball getRandomBall() {
-        Random random = new Random();
         Ball ball = new Ball();
-        ColorSupplier randomColor = new ColorSupplier();
         ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         ball.setColor(randomColor.getRandomColor());
         return ball;
