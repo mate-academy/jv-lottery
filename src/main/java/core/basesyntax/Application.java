4acb7a72a59a4da46294bb0848dd1d054f0428a0
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-28 14:49:06 +0300

    added constant, removed balls array, added lottery instance instead

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery[] balls = new Lottery[3];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            balls[i] = new Lottery();
-            System.out.println(balls[i].getRandomBall());
+            lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_COUNT = 101;
     private Random random = new Random();
     private Ball ball = new Ball();
     private ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ball.setNumber(random.nextInt(101));
+        ball.setNumber(random.nextInt(NUMBER_COUNT));
         ball.setColor(color.getRandomColor());
         return ball;
     }
