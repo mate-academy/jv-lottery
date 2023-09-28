f701a5e0bfc9d4084f3c5249e7b5f5a53a6a1ba9
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-28 18:05:53 +0300

    added ballNum constant and Ball constructor

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
 public class Ball {
+    private static final int BALL_NUMBER = 3;
     private int number;
     private String color;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public static int getBallNumber() {
+        return BALL_NUMBER;
+    }
 
     public void setNumber(int number) {
         this.number = number;
@@ -10,6 +20,10 @@ public class Ball {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     @Override
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,12 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_COUNT = 101;
     private Random random = new Random();
-    private Ball ball = new Ball();
+
     private ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ball.setNumber(random.nextInt(NUMBER_COUNT));
-        ball.setColor(color.getRandomColor());
-        return ball;
+        return new Ball(color.getRandomColor(), random.nextInt(NUMBER_COUNT));
     }
 }
