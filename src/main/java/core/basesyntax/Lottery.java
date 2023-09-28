bac28adb297249e90543be05e320682a686ebe49
Svyryd Valentyn <lutumsved@gmail.com>
2023-09-28 10:46:56 +0300

    other grammar fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int number = new Random().nextInt(MAX_BALL_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), number);
