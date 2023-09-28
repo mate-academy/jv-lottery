860e9cd49404928a4d5483137d369e21e47fb856
Igor Korolevich <igorkorolj@gmail.com>
2023-09-28 11:21:13 +0300

    added lottery to main method

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Application {
     private static final int LOTTERY_START_NUMBER = 3;
-    private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < LOTTERY_START_NUMBER; i++) {
             System.out.println(lottery.getRandomBall());
         }
