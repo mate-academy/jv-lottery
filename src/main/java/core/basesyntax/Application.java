ff9a591c7ac104136d19b8b2cc95e8e615716d74
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-09-28 12:22:57 +0300

    added NUMBER_OF_BALLS constant field

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball[] lotteryBalls = new Ball[3];
+        Ball[] lotteryBalls = new Ball[NUMBER_OF_BALLS];
         Lottery lottery = new Lottery();
         for (int i = 0; i < lotteryBalls.length; i++) {
             lotteryBalls[i] = lottery.getRandomBall();
