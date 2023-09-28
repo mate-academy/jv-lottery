415848195ceff7400bdb9d98152c04f35934ac73
Nataliia Shtykh <forstudying93@gmail.com>
2023-09-28 17:05:18 +0300

    fixed all mistakes after the review

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,7 +7,6 @@ public class ColorSupplier {
 
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color.name();
+        return Color.values()[index].name();
     }
 }
