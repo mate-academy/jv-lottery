8594ed33ef64b6dcdcdd1e1bc26dfd8aee429747
Влад <vladlesn86@gmail.com>
2023-09-28 14:36:11 +0300

    add Lotterey class with random method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,18 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        Colors color = Colors.values()[index];
-        return color.name();
+        Lotterey lotterey = new Lotterey();
+        return lotterey.getColor();
+    }
+
+    class Lotterey {
+        private Random random = new Random();
+
+        private int index = random.nextInt(Colors.values().length);
+        private Colors color = Colors.values()[index];
+
+        public String getColor() {
+            return color.name();
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -17,4 +17,3 @@ public class Lottery {
         return new Ball(color, index);
     }
 }
-
