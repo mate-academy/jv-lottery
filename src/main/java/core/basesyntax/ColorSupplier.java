b9dd632a1b17f4a97e71d7e85b6e434c34b8932e
Svyryd Valentyn <lutumsved@gmail.com>
2023-09-28 12:39:25 +0300

    other grammar fix 2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
     }
 }
