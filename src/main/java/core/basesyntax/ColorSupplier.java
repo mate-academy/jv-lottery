9f9f5f910161135cf82247b362c36e7b7493311b
Anton Dudar <dudar.anton4uk@gmail.com>
2023-09-28 12:11:23 +0300

    changed local vars to class-level and updated random num limit

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
     public String getRandomColor() {
-        Random random = new Random();
         int index = random.nextInt(Color.values().length);
         Color color = Color.values()[index];
         return color.name();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    Random random = new Random();
+    Ball ball = new Ball();
+    ColorSupplier color = new ColorSupplier();
     public Ball getRandomBall() {
-        Random random = new Random();
-        Ball ball = new Ball();
-        ColorSupplier color = new ColorSupplier();
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(101));
         ball.setColor(color.getRandomColor());
         return ball;
     }
