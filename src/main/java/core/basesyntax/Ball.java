e9c900b3b9d366a3153a52056a98c51250658b3a
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-09-28 19:41:44 +0200

    hw-jv-lottely

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -12,8 +12,9 @@ public class Ball {
     @Override
     public String toString() {
         return "Ball{" +
-                "number=" + number +
-                ", color='" + color + '\'' +
-                '}';
+                "number=" + number
+                + ", color='" + color
+                + '\''
+                + '}';
     }
 }
