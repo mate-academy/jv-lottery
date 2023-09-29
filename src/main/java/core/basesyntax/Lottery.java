7887ea56a80d4787c284d34c50a1e475fce16e65
Oleh Shvets <olehpost69@gmail.com>
2023-09-29 18:04:41 +0300

    lottery_changed1

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    private static final Random random = new Random();
     private static final int MAX_NUMBER = 100;
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,17 +1,14 @@
 package core.basesyntax;
 
 public class Main {
-
     private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
 
-        Lottery[] lotteries = new Lottery[NUMBER_OF_BALLS];
-
-        for (int i = 0; i < lotteries.length; i++) {
-            lotteries[i] = new Lottery();
-            Ball balls = lotteries[i].getRandomBall();
-            System.out.println(balls);
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
         }
     }
 }
