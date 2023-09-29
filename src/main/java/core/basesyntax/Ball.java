bb2e8dbcc9861f7d952dde52a6e3c90ff01b65d5
AgroFix <colalexm@gmail.com>
2023-09-29 15:29:51 +0300

    Fixed some minor points which needed to be resolved

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,12 +3,8 @@ package core.basesyntax;
 public class Ball {
     private String color;
     private int number;
-
-    public void setColor(String color) {
+    public Ball(String color, int number) {
         this.color = color;
-    }
-
-    public void setNumber(int number) {
         this.number = number;
     }
 
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_VALUE = 102;
+    private static final int MAX_VALUE = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private Ball ball = new Ball();
+
 
     public Ball getRandomBall() {
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(MAX_VALUE));
-        return ball;
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_VALUE);
+        return new Ball(color, number);
     }
 }
