e60736d7d06d3b02ac857fb3e136ab4f55dcece3
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-10-01 18:56:37 +0300

    removed redundant array and comment, improved ball log to console

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,12 +4,9 @@ public class Application {
     static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
-        Ball[] lotteryBalls = new Ball[NUMBER_OF_BALLS];
         Lottery lottery = new Lottery();
-        for (int i = 0; i < lotteryBalls.length; i++) {
-            lotteryBalls[i] = lottery.getRandomBall();
-            System.out.println(lotteryBalls[i]);
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
