f11cab48b006be8e885156bf9a6bdd184269fb53
Bohdan Bilko <rapsbodik@gmail.com>
2023-10-02 11:24:45 +0300

    fixed all mistakes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_VALUE = 100;
+    private static final int MAX_VALUE = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
