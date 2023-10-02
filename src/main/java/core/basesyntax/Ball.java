54e46d8edcd4ca4da61c122c2dee7711add739cb
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-02 17:24:04 +0300

    add Lottery

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,15 +3,17 @@ package core.basesyntax;
 public class Ball {
     private Color color;
     private int number;
+
     public void setColor(Color newColor) {
         this.color = newColor;
     }
+
     public void setNumber(int newNumber) {
         this.number = newNumber;
     }
+
     @Override
     public String toString() {
         return Integer.toString(this.number) + " " + this.color;
-
     }
 }
