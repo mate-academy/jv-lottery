368dc2e38175f04c0ea7049cb2e9eec22e31edb7
Nataliia <gzodchie@gmail.com>
2023-10-02 11:10:40 +0300

    fixed ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,8 +7,6 @@ public class ColorSupplier {
 
     public Color getRandomColor() {
         int index = random.nextInt(Color.values().length);
-        Color color = Color.values()[index];
-
-        return color;
+        return Color.values()[index];
     }
 }
