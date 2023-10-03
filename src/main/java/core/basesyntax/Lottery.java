65b4e6b0360a2a2bfbdfab4e2936a6f5b7e94162
Sasha <sahayatskiv@gmail.com>
2023-10-03 21:04:00 +0300

    add constant variables to Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
 
     private Random random = new Random();
 
     public Ball getRandomBall() {
-        int randomNumber = random.nextInt(100);
-        String rundomColor = colorSupplier.getRandomColor();
-        Ball randomBall = new Ball(rundomColor, randomNumber);
+        // Abbreviation in name 'RANDOM_NUMBER'
+        // must contain no more than '2' consecutive capital letters.
+        final int Random_Number = random.nextInt(100);
+        // Abbreviation in name 'RANDOM_COLOR'
+        // must contain no more than '2' consecutive capital letters.
+        final String Random_Color = COLOR_SUPPLIER.getRandomColor();
+        Ball randomBall = new Ball(Random_Color, Random_Number);
         return randomBall;
     }
 }
