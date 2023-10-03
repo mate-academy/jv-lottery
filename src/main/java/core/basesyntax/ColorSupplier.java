7d71e50bd92d7d5d32468f5a284b0714e4a9f13d
TymurPanchenk0 <tymur.panchenko@gmail.com>
2023-10-03 15:09:10 +0300

    Lottery changes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
-    Color[] colors = Color.values();
+    private Random random = new Random();
+    private Color[] colors = Color.values();
+
     public Color getRandomColor() {
         return colors[random.nextInt(colors.length)];
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,8 @@ import java.util.Random;
 class Lottery {
     private static final int NUMBER_COUNT = 100;
 
-    ColorSupplier colorSupplier = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
