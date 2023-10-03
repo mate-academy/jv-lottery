ceba544aaf5538ed381831d845087b8da4e6be9b
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-03 18:46:03 +0300

    Made changes with Ball init

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class Ball {
     private Color color;
     private int number;
+
     Ball(Color color, int number) {
         this.color = color;
         this.number = number;
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAXCount = 100;
     private Random random = new Random();
     private ColorSupplier color = new ColorSupplier();
-    private static final int MAXCount = 100;
+
     public Ball getRandomBall() {
         return new Ball(color.getRandomColor(),random.nextInt(MAXCount));
     }
