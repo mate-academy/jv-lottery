eb650114c2d5cb6de21504785d702e088b3f810c
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-10-03 15:13:42 +0300

    Changes from static

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery= new Lottery();
         for (int i = 0; i < 6; i++) {
-            Ball ball1 = Lottery.getRandomBall();
-            System.out.println(ball1.toString());
+            Ball ball1 = lottery.getRandomBall();
+            System.out.println(ball1);
         }
         // create three balls using class Lottery and print information about them in console
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    private Random random =new Random();
+    public Ball getRandomBall() {
         Ball newOne = new Ball();
-        newOne.setNumber(new Random().nextInt(100));
-        newOne.setColor(ColorSupplier.getRandomColor());
+        newOne.setNumber(random.nextInt(100));
+        ColorSupplier color = new ColorSupplier();
+        newOne.setColor(color.getRandomColor());
         return newOne;
     }
 }
