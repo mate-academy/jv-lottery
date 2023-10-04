d3460573b004aaec9722bd9ba330fa6b8c497052
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-04 17:55:12 +0300

    correcting grammatical errors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final String[] colors = new String[]{"red","blue","green","black","white"};
+
     public String getRandomColor() {
         Random rand = new Random();
         return colors[rand.nextInt(colors.length)];
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         Random rand = new Random();
         ColorSupplier colorSupplier = new ColorSupplier();
         return new Ball(colorSupplier.getRandomColor(),rand.nextInt(100));
