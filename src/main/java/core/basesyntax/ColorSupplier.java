8a9d7b2d71f1d06a718399dff727ca17c01d9f30
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-04 18:15:03 +0300

    correcting grammatical errors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final String[] colors = new String[]{"red","blue","green","black","white"};
-
     public String getRandomColor() {
         Random rand = new Random();
-        return colors[rand.nextInt(colors.length)];
+        return Colors.values()[rand.nextInt(Colors.values().length)].toString();
     }
 }
