94df7a1a4f0e6f51c23b3dcf679b84470535027d
Влад <vladlesn86@gmail.com>
2023-10-05 12:35:24 +0300

    did some codestyle fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random random = new Random();
+    private Random random = new Random();
+    private final int bound = 100;
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(bound));
     }
 }
