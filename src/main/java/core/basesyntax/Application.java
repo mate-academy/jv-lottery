5e7329b855084616bcfe251ab5cb2fd86e863831
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-10-06 12:41:09 +0200

    updated BOUND to 101. changed the Ball class and the Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    private static int count = 3;
+    private static final int COUNT = 3;
 
     public static void main(String[] args) {
-        while (count > 0) {
-            Lottery winCar = new Lottery();
-            System.out.println(winCar.getRandomBall());
-            count--;
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < COUNT; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Colors[] colors = Colors.values();
     private final Random random = new Random();
 
-    public String getRandomColor() {
-        Colors randColor = colors[random.nextInt(colors.length)];
-        return colors[random.nextInt(colors.length)].name();
+    public Color getRandomColor() {
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index];
     }
 }
