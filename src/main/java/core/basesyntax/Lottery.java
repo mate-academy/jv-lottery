4d8938a9872097127795bd1655e14b4790e364bd
danX <danildryzov@gmail.com>
2023-10-06 20:22:40 +0200

    used constant name, made objects belong to class instance, extract random to a class field ColorSupplier, same as in lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int oneHundred = 100;
+    private static final int QUANTITY_LIMIT = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Ball ball = new Ball();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Ball ball = new Ball();
-
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(oneHundred));
+        ball.setNumber(random.nextInt(QUANTITY_LIMIT));
 
         return ball;
     }
