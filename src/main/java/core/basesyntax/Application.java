0d92faea2ca1271dcce5d790dfc1394b0a82425a
petro feniak <petro.fenyak@gmail.com>
2023-10-08 20:42:25 +0300

    created three balls using class Lottery and print information about them in console

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,14 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Ball firstBall = new Lottery().getRandomBall();
+        Ball secondBall = new Lottery().getRandomBall();
+        Ball thirdBall = new Lottery().getRandomBall();
+
+        System.out.println(firstBall.toString()
+                + "\n"
+                + secondBall.toString()
+                + "\n"
+                + thirdBall.toString());
     }
 }
