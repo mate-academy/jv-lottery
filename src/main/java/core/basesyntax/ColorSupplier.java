abf5e4cb285c1d5c87c4bb837e733d7ed6e0eb1a
Tony Hut <tonnyhut@outlook.com>
2023-10-08 11:31:51 +0200

    Ðimplement method ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.time.DayOfWeek;
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+
+    public Colors getRandomColor() {
+        int index = new Random().nextInt(DayOfWeek.values().length);
+        return Colors.values()[index];
     }
 }
