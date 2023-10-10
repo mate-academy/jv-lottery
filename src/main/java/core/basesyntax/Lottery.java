3ab4c2b833e377df63d28a201144fe77f17ba1e2
petro feniak <petro.fenyak@gmail.com>
2023-10-10 22:21:15 +0300

    correction of methods according recommendations

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,31 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int range = 100;
-    private Random number = new Random();
-    private ColorSupplier color = new ColorSupplier();
-
-    public int getRange() {
-        return range;
-    }
-
-    public Random getNumber() {
-        return number;
-    }
-
-    public void setNumber(Random number) {
-        this.number = number;
-    }
-
-    public ColorSupplier getColor() {
-        return color;
-    }
-
-    public void setColor(ColorSupplier color) {
-        this.color = color;
-    }
+    private final Random number = new Random();
+    private final ColorSupplier color = new ColorSupplier();
 
     public Ball getRandomBall() {
+        int range = 100;
         return new Ball(color.getRandomColor(), number.nextInt(range));
     }
 }
