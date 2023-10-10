cfce69b7df373e9beeef910beb9acbeea0ba81fb
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-10-10 13:27:40 +0300

    rename enum to Color

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Colors[] allColors = Colors.values();
+    private final Color[] allColors = Color.values();
     private final Random random = new Random();
 
-    public Colors getRandomColor() {
+    public Color getRandomColor() {
         int index = random.nextInt(allColors.length);
         return allColors[index];
     }
