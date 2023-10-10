78a898ef0d48552f7e3ce7aff372ad96f920cfcb
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-10-10 13:17:31 +0300

    Made the 'Random' object 'final' in 'ColorSupplier'

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier;
-    private final Random random;
+    private final Random random = new Random();
 
     public Lottery() {
         this.colorSupplier = new ColorSupplier();
-        this.random = new Random();
     }
 
     public Ball getRandomBall() {
