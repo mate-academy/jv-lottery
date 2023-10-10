88cc4f98bd590e879085d5e5a0d8b7b5775a95d5
khorkovenko <khorkovenko>
2023-10-10 17:33:39 +0300

    test commit

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         final int arraySize = 3;
+
         Ball[] balls = new Ball[arraySize];
+
         Lottery lottery = new Lottery();
 
         for (int i = 0; i < arraySize; i++) {
