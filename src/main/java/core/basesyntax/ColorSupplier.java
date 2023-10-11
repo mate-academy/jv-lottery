85a00faf1b41987235ceae23fbf8ba75657bd984
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-11 16:17:24 +0300

    create class Ball, ColorSupplier, Lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final String[] colors = new String[]{"white", "black", "red", "blue"};
+    private final Random rand = new Random();
+
     public String getRandomColor() {
-        return null;
+        return colors[rand.nextInt(colors.length)];
     }
 }
