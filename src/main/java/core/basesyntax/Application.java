5b2ba2e23e0a797a1741e7d7d33f1598eabc5a72
Taras Kushnir <taras.kushnir.tk@gmail.com>
2023-10-11 22:46:46 +0200

    did clean and package

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball randomBall = lottery.getRandomBall();
-        System.out.println("Random Ball: " + randomBall);
+        Ball randomBall1 = lottery.getRandomBall();
+        Ball randomBall2 = lottery.getRandomBall();
+        Ball randomBall3 = lottery.getRandomBall();
+        System.out.println("Random Ball: " + randomBall1);
+        System.out.println("Random Ball: " + randomBall2);
+        System.out.println("Random Ball: " + randomBall3);
     }
 }
