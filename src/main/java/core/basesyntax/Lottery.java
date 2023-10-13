87803db2bb8f2ae9f1ec1cba04eb4a2c552eaa15
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-10-13 19:14:43 +0300

    Lottery_home_task_try_#2_comments_have_been_eliminated

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final int maxBalls = 100;
+    private final Random random = new Random();
+    private final ColorSupplier randomColor = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(new Random().nextInt(100), new ColorSupplier().getRandomColor().toString());
+        return new Ball(random.nextInt(maxBalls), randomColor.getRandomColor());
     }
-
 }
-
