a4e3699cbc3fca01615ed334c149c77e69bf8593
Snizhana Sosiedova <snejik1111@gmail.com>
2023-10-13 23:30:58 +0300

    Fixes: created constants for Random and Bound

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BOUND = 101;
+    private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BOUND));
     }
 }
