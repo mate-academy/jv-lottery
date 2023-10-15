68eae12af734a1548af25d2f16ea7242d3c1a8b7
Dominik Gągało <cliffcamer@gmail.com>
2023-10-15 13:18:19 +0200

    HW Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        ColorSupplier.Lottery lottery = new ColorSupplier.Lottery();
+
+        ColorSupplier.Ball ball1 = lottery.getRandomBall();
+        ColorSupplier.Ball ball2 = lottery.getRandomBall();
+        ColorSupplier.Ball ball3 = lottery.getRandomBall();
+
+        System.out.println(ball1);
+        System.out.println(ball2);
+        System.out.println(ball3);
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    enum Colors {
+        BLUE, YELLOW, GREEN, BLACK, PURPLE;
+
+        public static Colors getRandomColor() {
+            Colors[] colors = Colors.values();
+            Random random = new Random();
+            return colors[random.nextInt(colors.length)];
+        }
+    }
+
+    static class Ball {
+        private final Colors color;
+
+        private final int number;
+
+        public Ball(Colors color, int number) {
+        this.color = color;
+        this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            return "A " +  color + " Ball, with a number=" + number;
+        }
+    }
+
+    static class Lottery {
+        private static final int maxNumber = 100;
+
+        private final Random random;
+
+        public Lottery(){
+            this.random = new Random();
+        }
+
+        public Ball getRandomBall() {
+            Colors color = Colors.getRandomColor();
+            int number = random.nextInt(maxNumber) + 1;
+            return new Ball(color, number);
+        }
     }
 }
