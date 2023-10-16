0db43fd566f11cb1dce4e279bc00e9147e961c93
Andriy Starzhevskiy <fkordprivate@gmail.com>
2023-10-16 15:38:45 +0300

    revert erroneous commits
    This reverts commit def745ce689c8c907f36683a3f505561a76a95c8.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     public Colors getRandomColor() {
-        return Colors.values()[new Random().nextInt(Colors.values().length)];
+        int index = new Random().nextInt(Colors.values().length);
+        return Colors.values()[index];
     }
 }
