16a0983985fe5ca6a7f4d7fd0eadcbc92cb96ef7
Andriy Starzhevskiy <fkordprivate@gmail.com>
2023-10-16 13:18:33 +0300

    created the Ball class created enum Colors created the Lottery class and implemented the getRandomBall method

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Ball {
+    static String color;
+    static int number;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
