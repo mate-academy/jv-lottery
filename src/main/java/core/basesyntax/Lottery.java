c2d9eff59a7b90d8d6f3256c8c6d88666e5286cb
Andriy Starzhevskiy <fkordprivate@gmail.com>
2023-10-17 13:54:46 +0300

    fixed an error in int 'MAX_BALL_NUMBER'

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int number = 3;
+    private static final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(number));
+        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
