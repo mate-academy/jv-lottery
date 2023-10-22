2bef763f38ede992c1731ff67b25363376667a9a
Bohdan Kost <kostbogdan@icloud.com>
2023-10-22 21:13:51 +0300

    constants created, random variables corrected

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_NUMBER = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
+        Ball[] balls = new Ball[BALLS_NUMBER];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int RANDOM_LIMIT = 100;
     private ColorSupplier randomColor = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(randomColor.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(random.nextInt(RANDOM_LIMIT));
         return ball;
     }
 }
