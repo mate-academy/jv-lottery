3031d21d020b0ee0cd0bbf130bd190b790dea7e8
bohdan2704 <148899164+bohdan2704@users.noreply.github.com>
2023-10-27 14:58:27 +0300

    Update src/main/java/core/basesyntax/Application.java
    Co-authored-by: Kirill Koval <73041091+kerrrusha@users.noreply.github.com>

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,6 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         final int numberOfBalls = 3;
-        final int maxNumberOnTheBall = 100;
         Ball[] balls = new Ball[numberOfBalls];
         Lottery lottery = new Lottery();
 
