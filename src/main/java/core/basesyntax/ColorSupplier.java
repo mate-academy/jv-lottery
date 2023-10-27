162e21bbcc1d7a2e0488d5bcdcea75bc4462b945
Sasha <sasha.panchyshun@gmail.com>
2023-10-27 17:20:13 +0300

    extract random to the private final field

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = getRandom().nextInt(Color.values().length);
         return String.valueOf(Color.values()[index]);
+    }
+
+    public Random getRandom() {
+        return random;
     }
 }
