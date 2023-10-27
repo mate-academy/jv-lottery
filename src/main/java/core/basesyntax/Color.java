ed7601af9e9409246eb0ed70d8bf8efeb5d36720
Sasha <sasha.panchyshun@gmail.com>
2023-10-27 14:47:33 +0300

    Create enum Color and classes Ball and Lottery, implemented methods getRandomColor and getRandomBall. Tested the program by printing results in console

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public enum Color {
+    BLUE,
+    YELLOW,
+    GREEN,
+    WHITE,
+    BLACK,
+    RED,
+    GREY,
+    PURPLE
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        int index = new Random().nextInt(Color.values().length);
+        return String.valueOf(Color.values()[index]);
     }
 }
