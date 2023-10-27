5e75fa37fefbe3b0d540cff151040e23ede58be6
bohdan2704 <148899164+bohdan2704@users.noreply.github.com>
2023-10-27 14:58:18 +0300

    Update src/main/java/core/basesyntax/ColorSupplier.java
    Co-authored-by: Kirill Koval <73041091+kerrrusha@users.noreply.github.com>

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,13 +4,7 @@ import core.basesyntax.objects.Color;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Random random;
-
-    public ColorSupplier() {
-        // SHOULD I CREATE RANDOM OBJECT AS FIELD FOR ALL CLASS,
-        // OR IT IS BETTER TO LEAVE IT AS VARIABLE IN METHOD getRandomColor ?
-        this.random = new Random();
-    }
+    private final Random random = new Random();
 
     public String getRandomColor() {
         int randomColorCode = random.nextInt(Color.values().length);
