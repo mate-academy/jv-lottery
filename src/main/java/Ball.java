14742e939f97d1531aa6c0b5bb5548503a72d7c2
bobik <oliferchukrivne@i.ua>
2023-10-27 11:48:49 +0300

    jv-lottery checkstyle fixed

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,10 +1,13 @@
 import core.basesyntax.Color;
+
 public class Ball {
     private int number;
     private Color color;
+
     public Ball(int number, Color color) {
         this.color = color;
         this.number = number;
+
     }
     @Override
     public String toString() {
