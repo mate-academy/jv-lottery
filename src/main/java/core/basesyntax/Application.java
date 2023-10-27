7cf04fb35fdf8a1906e197e3879ffeb1f2853a28
Bohdan <bohdan.g.54@gmail.com>
2023-10-27 21:01:42 +0300

    Reformatting

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,8 +6,8 @@ public class Application {
     private static final int numberOfBalls = 3;
 
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery(100);
+        // create three balls using class Lottery and print information about them in console
 //        Ball ball1 = lottery.getRandomBall();
 //        Ball ball2 = lottery.getRandomBall();
 //        Ball ball3 = lottery.getRandomBall();
@@ -15,7 +15,6 @@ public class Application {
 //        System.out.println("1. " + ball1);
 //        System.out.println("1. " + ball2);
 //        System.out.println("1. " + ball3);
-
         Ball[] balls = new Ball[numberOfBalls];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
