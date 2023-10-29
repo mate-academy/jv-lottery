67b15ec50553c21ea8e869727f43e5c324a4936a
Bohdan <bohdan.g.54@gmail.com>
2023-10-29 23:40:45 +0200

    Refractor

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,8 +6,8 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery(100);
 
-        final int NUMBER_OF_BALLS = 3;
-        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        final int numberOfBalls = 3;
+        Ball[] balls = new Ball[numberOfBalls];
         // create three balls using class Lottery and print information about them in console
 
         for (int i = 0; i < balls.length; i++) {
