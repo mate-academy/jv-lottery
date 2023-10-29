e5e3cb28a5aa1a01bd066a681171e0834960ea37
Bohdan <bohdan.g.54@gmail.com>
2023-10-29 23:00:03 +0200

    Refractor

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
 import core.basesyntax.objects.Ball;
-
 import java.util.Random;
 
 public class Lottery {
     private final Random random;
-    private final int MAX_NUMBER_ON_BALL;
+    private final int maxNumberOnTheBall;
     private final ColorSupplier colorSupplier;
 
     public Lottery(int MAX_NUMBER_ON_BALL) {
-        this.MAX_NUMBER_ON_BALL = MAX_NUMBER_ON_BALL;
+        this.maxNumberOnTheBall = MAX_NUMBER_ON_BALL;
         this.random = new Random();
         colorSupplier = new ColorSupplier(random);
     }
@@ -18,7 +17,7 @@ public class Lottery {
     public Ball getRandomBall() {
         String ballColor = colorSupplier.getRandomColor();
         // +1 to include the last number
-        int ballRandomNumber = random.nextInt(MAX_NUMBER_ON_BALL) + 1;
+        int ballRandomNumber = random.nextInt(maxNumberOnTheBall) + 1;
         return new Ball(ballColor, ballRandomNumber);
     }
 }
