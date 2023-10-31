3545e9b76529eff73ec6089c2fb750d1eaebf399
Serhii Makarchuk <sergiymakarchuk1507@gmail.com>
2023-10-31 21:05:52 +0200

    maybe corrected

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,6 +4,11 @@ public class Ball {
     private Color color;
     private int number;
 
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
     public Color getColor() {
         return color;
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int FIGURE_COUNT = 101;
+    private static final int BALL_NUMBER_LIMIT = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int randomInt = random.nextInt(FIGURE_COUNT);
-        Ball ball = new Ball();
-        ball.setNumber(randomInt);
-        ball.setColor(colorSupplier.getRandomColor());
-        return ball;
+        int randomInt = random.nextInt(BALL_NUMBER_LIMIT);
+        return new Ball(colorSupplier.getRandomColor(), randomInt);
+
     }
 }
