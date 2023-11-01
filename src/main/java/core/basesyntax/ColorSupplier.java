43749e953a86f38daf29dc69f25df3aaf6b53eea
Obusker <mr.kudryachenko@gmail.com>
2023-11-01 18:00:55 +0200

    Changes was commited in order with comments, however, not fully understanded.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ColorSupplier {
 
     public String getRandomColor() {
-        final int INDEX = new Random().nextInt(Color.values().length);
-        return Color.values()[INDEX].name();
+        final int Index = new Random().nextInt(Color.values().length);
+        return Color.values()[Index].name();
     }
 }
