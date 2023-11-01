395f2580f0f38fbd64d75bf62c767b8036eb00e7
Obusker <146196819+Obusker@users.noreply.github.com>
2023-11-01 21:06:57 +0200

    Update Constant.java

--- src/main/java/core/basesyntax/Constant.java
+++ src/main/java/core/basesyntax/Constant.java
@@ -1,12 +1,2 @@
-package core.basesyntax;
-
-import java.util.Random;
-
-public class Constant {
-    private final int ballNumber = new Random().nextInt(100);
-
-    public int getBallNumber() {
-        return ballNumber;
-    }
 
 }
