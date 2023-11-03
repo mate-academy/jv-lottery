7e9f2bd0c9c4a008be4fa080c204e608f49b88d5
Misha <mishadyakivnich@gmail.com>
2023-11-03 18:31:53 +0200

    push from idei

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ public class Application {
     private static final int NUMBER_OF_REPEAT = 3;
 
     public static void main(String[] args) {
-
         Lottery lottery = new Lottery();
         for (int i = 0; i < NUMBER_OF_REPEAT; i++) {
             System.out.println(lottery.getRandomBall());
