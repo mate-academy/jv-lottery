4362b578345d8bf0facffda702b0cd78084f1120
Obusker <mr.kudryachenko@gmail.com>
2023-11-03 14:44:33 +0200

    another commit according to comments.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class Application {
-
-    private static final Lottery LOTTERY = new Lottery();
+    private static final int NUMBER_OF_USERS = 3;
+    private static final Ball[] BALLS = new Ball[NUMBER_OF_USERS];
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        for (int i = 0; i < 3; i++) {
-            System.out.println(LOTTERY.getRandomBall());
+        for (int i = 0; i < BALLS.length; i++) {
+            BALLS[i] = new Lottery().getRandomBall();
+            System.out.println(BALLS[i]);
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     private static final int BALL_NUMBER_LIMIT = 100;
-
     private final Ball randomBall = new Ball();
     private final ColorSupplier color = new ColorSupplier();
     private final Random number = new Random();
 
     public Ball getRandomBall() {
-
         randomBall.setColor(color.getRandomColor());
         randomBall.setNumber(number.nextInt(BALL_NUMBER_LIMIT));
         return randomBall;
