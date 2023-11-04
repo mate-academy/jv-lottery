67b049ae903b991799f43b87a2b8ca466c2bd1b1
Tetiana Khrestina <khrestinatatiana@gmail.com>
2023-11-04 19:57:23 +0200

    1.Create a Ball class with the following fields: color and number and override the toString() method 2.Create an enum with different colors 3.Implement method getRandomColor() in class ColorSupplier. 4.Ccreate a Lottery class with the getRandomBall() method, which will return the ball. 5.In the main() method, create three balls using the getRandomBall() method from the Lottery class. And print information about them in the console.

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    YELLOW,
+    BLUE,
+    GREEN,
+    ORANGE,
+    PURPLE,
+    BLACK,
+    WHITE,
+    GRAY,
+    BROWN
+
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        String randomColor = ColorSupplier.getRandomColor();
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1;
+        return new Ball(randomColor,randomNumber);
+    }
+}
