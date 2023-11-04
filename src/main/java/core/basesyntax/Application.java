5317bf995f88b1a6840f6095e607050b77ff2c48
MilianCode <milian.code@gmail.com>
2023-11-05 00:27:14 +0100

    Fixed jv-lottery-solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
         Ball[] loterryBalls = new Ball[3];
         for (int i = 0; i < 3; i++) {
-            loterryBalls[i] = new Lottery().returnRandomBall();
+            loterryBalls[i] = lottery.returnRandomBall();
             System.out.println(loterryBalls[i].toString());
         }
     }
