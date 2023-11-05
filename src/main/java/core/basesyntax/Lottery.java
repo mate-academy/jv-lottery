cd0d24190585696b55b745cfc0a4388493d38b4b
Tetiana Khrestina <khrestinatatiana@gmail.com>
2023-11-05 15:26:27 +0200

    1.Used loop condition in Application.java. 2.Deleted static method in ColorSupplier.java and in Lottery.java. 3.Made 100 as constants with self-descriptive name in Lottery.java.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int maxNumber = 100;
+    private static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String randomColor = String.valueOf(colorSupplier.getRandomColor());
-        int randomNumber = new Random().nextInt(maxNumber) + 1;
+        int randomNumber = new Random().nextInt(MAX_NUMBER) + 1;
         return new Ball(randomColor,randomNumber);
     }
 }
