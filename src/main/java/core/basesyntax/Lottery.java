161897ed1077cf89e3b06ed749d65e286232eda3
Andrii <allias5000@gmail.com>
2023-11-06 21:54:10 +0200

    ColorSupplier - random is now class level variable. Lottery - ColorSupplier & Random are now class-level variable. Magic number fixed.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier colorSupplier = new ColorSupplier();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(random.nextInt(MAX_NUMBER));
         return ball;
     }
 }
