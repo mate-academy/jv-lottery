cdeb818ff5d99061d33059da0e7a33b17ed5a480
Andrii <allias5000@gmail.com>
2023-11-06 17:39:35 +0200

    Added class ball with random color and number generator (CollorSupplier, Lottery)

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public class Ball {
+    private Color color;
+    private int number;
+
+    @Override
+    public String toString() {
+        return("Ball number = " + color + ". " + "Ball color = " + number);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
