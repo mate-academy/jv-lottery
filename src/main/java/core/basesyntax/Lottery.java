575c28a8dae8656ec2a587005e819e3f2b3c0eb0
Ivan <lavrinenkoivan51@gmail.com>
2023-11-07 18:13:33 +0100

    fixed magic number problem and extra toString() method

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int BALLS_QUANTITY = 101;
+    private static final int MAX_BALL_NUMBER = 101;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int number = random.nextInt(BALLS_QUANTITY);
+        int number = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), number);
     }
 }
