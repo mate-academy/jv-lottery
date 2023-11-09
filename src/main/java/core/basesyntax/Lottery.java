31821e347e28b2d45e05d2a02f44b7976d045be0
Tetiana Khrestina <khrestinatatiana@gmail.com>
2023-11-09 18:03:09 +0200

    1. In src/main/java/core/basesyntax/Lottery.java: changed String.valueOf() to name(). 2. In src/main/java/core/basesyntax/Lottery.java: made randome variable of class-level.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        String randomColor = String.valueOf(colorSupplier.getRandomColor());
-        int randomNumber = new Random().nextInt(MAX_NUMBER) + 1;
+        String randomColor = colorSupplier.getRandomColor().name();
+        int randomNumber = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
