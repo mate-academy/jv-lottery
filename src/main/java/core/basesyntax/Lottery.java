6b70d9f9215256cfa7585619286b3e32dc085d2a
Bohdan <kbs20043131@gmail.com>
2023-11-09 21:02:04 +0200

    checklist add3

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int MAX_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
+
     public Ball getRandomBall() {
         int randomint = random.nextInt(MAX_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), randomint);
