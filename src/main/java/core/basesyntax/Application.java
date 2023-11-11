97c2e8140f0176bd00c20dd67cc070ed5d8b81d1
Maksym Mazlov <mazlovmaxim@gmail.com>
2023-11-11 16:22:59 +0200

    created lottery that returns random balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int COUNT_BALL = 3;
+    public static final int MAX_POSSIBLE_NUMBER_FOR_BALL = 100;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball [] balls = new Ball[COUNT_BALL];
+        for (Ball resultBall: balls) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
