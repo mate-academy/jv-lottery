b08199a45594e691fd6064b7d2ad9c855618d101
artem <artemsa223@gmail.com>
2023-11-12 14:40:51 +0000

    added loop to Application class, Random and colorSupplier changed to  class level field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int HUNDRED =100 ;
-    Random random = new Random();
-    ColorSupplier supplier = new ColorSupplier();
+    private static final int HUNDRED = 100;
+    private final Random random = new Random();
+    private final ColorSupplier supplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
