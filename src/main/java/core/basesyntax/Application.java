d53f44f142e18b441ddf6f36359c851c47a54618
artem <artemsa223@gmail.com>
2023-11-12 13:44:34 +0000

    added Ball,Colors,Lottery classes and realized method getRandomBall()

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,16 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery bal = new Lottery();
+
+        Ball ball1 = bal.getRandomBall();
+        Ball ball2 = bal.getRandomBall();
+        Ball ball3 = bal.getRandomBall();
+
+        System.out.println(ball1);
+        System.out.println(ball2);
+        System.out.println(ball3);
+
         // create three balls using class Lottery and print information about them in console
     }
 }
