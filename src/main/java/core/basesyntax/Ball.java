c1c58e5a5aa8aaa3f2ec9b579802caf548fbbb2b
Nonzeq <bgdn.kobylchak@gmail.com>
2023-11-13 16:23:19 +0200

    modify Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Ball {
     private final Color color;
     private final int number;
-    private static final Random random = new Random();
-    private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final Random RANDOM = new Random();
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
 
     public Ball() {
-        this.color = colorSupplier.getRandomColor();
-        this.number = random.nextInt(100);
+        this.color = COLOR_SUPPLIER.getRandomColor();
+        this.number = RANDOM.nextInt(100);
     }
 
     @Override
