d945f04ceed065cc7edc674b91768d62ad00f5bb
Nonzeq <bgdn.kobylchak@gmail.com>
2023-11-13 17:47:04 +0200

    move random and supplier from Ball to Lottery

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,14 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Ball {
-    private static final Random RANDOM = new Random();
-    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
     private final Color color;
     private final int number;
 
-    public Ball() {
-        this.color = COLOR_SUPPLIER.getRandomColor();
-        this.number = RANDOM.nextInt(100);
+    public Ball(Color color, int number) {
+        this.color = color;
+        this.number = number;
     }
 
     @Override
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Lottery {
+    private static final Random RANDOM = new Random();
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private static final int MAGIC_NUMBER = 100;
+
     public Ball getRandomBall() {
-        return new Ball();
+        return new Ball(COLOR_SUPPLIER.getRandomColor(),RANDOM.nextInt(MAGIC_NUMBER));
     }
 }
