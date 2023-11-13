7be939596d17000e65b3205b4b3872ec4f69ef7e
Nikita <nik.pogrebnoy@gmail.com>
2023-11-13 21:04:17 +0200

    added constant fixed: use .name() not .toString(), its mentioned in checklist

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_RANDOM_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random randomNumber = new Random();
-    private static final int MAX_RANDOM_NUMBER = 100;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(),randomNumber.nextInt(MAX_RANDOM_NUMBER));
