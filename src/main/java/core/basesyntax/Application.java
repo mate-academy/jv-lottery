a47548bc21a8314843f71bb9bfea93818f30a174
Nikita <nik.pogrebnoy@gmail.com>
2023-11-13 15:12:59 +0200

    renamed constant added constructor initialised Ball by constructor

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,7 +5,6 @@ public class Application {
     private static final Lottery lottery = new Lottery();
     private static final Ball[] balls = new Ball[AMOUNT_OF_BALLS];
 
-
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         for (int i = 0; i < balls.length; i++) {
