c1410686266a6855ad97016d7c9a61f32ad53cc9
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:03:10 +0200

    added solution to jv-lottery task with all required methods and classes implemented

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        final int ballCount = 3;
+        String[] lotteryResult = new String[ballCount];
+        for (int i = 0; i < ballCount; i++) {
+            lotteryResult[i] = new Lottery().getRandomBall();
+            System.out.println(lotteryResult[i]);
+        }
     }
 }
