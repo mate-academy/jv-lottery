3ec309d7fa485f598b88ce47fc09b2676912cce6
Oleh <mihailyakyb45@gmail.com>
2023-11-16 18:27:37 +0200

    doneHomework3

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     public String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
         return Colors.values()[index].toString();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     public Ball getRandomBall() {
         int randomNumber = new Random().nextInt(100);
         return new Ball(new ColorSupplier().getRandomColor(), randomNumber);
