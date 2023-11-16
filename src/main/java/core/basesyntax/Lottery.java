56e3ded0d606ae16a5578f8950b4400e71114e84
Jakub <jszymanski97@gmail.com>
2023-11-16 17:34:14 +0000

    Color Supplier extension removed, Random field created, Max random number assigned to a static field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,12 +2,16 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
+public class Lottery {
+
+    private static final int MAX_BALL_NUMBER = 101;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(getRandomColor());
-        Random random = new Random();
-        ball.setNumber(random.nextInt(101));
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
