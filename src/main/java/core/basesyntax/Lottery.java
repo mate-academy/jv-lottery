42acf42fd8e9aa08876035f5216533a83cb7c509
Oleh <mihailyakyb45@gmail.com>
2023-11-16 22:05:30 +0200

    fixed hometask2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         int maxNumber = Ball.getMaxNumber();
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
         int randomNumber = random.nextInt(maxNumber);
         return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
