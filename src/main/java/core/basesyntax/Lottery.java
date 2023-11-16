5e85019c79a1ca8d5ae3b666b41faecc48e00777
Kinereiko <kinereiko@gmail.com>
2023-11-16 22:25:34 +0200

    Add constant for magic number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int maxNumber = 100;
     private final ColorSupplier supplier = new ColorSupplier();
     private final Random randNum = new Random(100);
-    private static final int maxNumber = 100;
 
     public Ball getRandomBall() {
         Ball randBall = new Ball();
