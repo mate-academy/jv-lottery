e7476dfae5c73ecc9c43d600545aaf6a8cbf8f25
Kinereiko <kinereiko@gmail.com>
2023-11-17 15:26:31 +0200

    Renamed constants

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private final ColorSupplier supplier = new ColorSupplier();
-    private final Random random = new Random(100);
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball randBall = new Ball();
