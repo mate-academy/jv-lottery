56f85c76b52b6b4ee034490c085cabdffd706535
Oleh <mihailyakyb45@gmail.com>
2023-11-17 13:21:20 +0200

    added constant to Lottery

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,17 +1,12 @@
 package core.basesyntax;
 
 public class Ball {
-    private static final int MAX_NUMBER = 100;
     private String color;
     private int number;
 
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
-    }
-
-    public static int getMaxNumber() {
-        return MAX_NUMBER;
     }
 
     @Override
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int maxNumber = Ball.getMaxNumber();
-        int randomNumber = random.nextInt(maxNumber);
+        int randomNumber = random.nextInt(Lottery.MAX_NUMBER);
         return new Ball(colorSupplier.getRandomColor(), randomNumber);
     }
 }
