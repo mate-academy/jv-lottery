64540f9a22005a17a5fbbef7310e70a43c5e77a3
Igor Hunder <gunderigorv@gmail.com>
2023-11-23 16:48:26 +0200

    I created a lottery code

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    public enum Color {
+        RED,
+        BLUE,
+        GREEN,
+        ORANGE;
+    }
+
     public String getRandomColor() {
-        return null;
+        Color[] colors = Color.values();
+        Random random = new Random();
+        int index = random.nextInt(colors.length);
+        return colors[index].toString();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier;
+
+    public Lottery() {
+        colorSupplier = new ColorSupplier();
+    }
+
+    public Ball getRandomBall() {
+        String randomColor = colorSupplier.getRandomColor();
+        Random random = new Random();
+        int randomNumber = random.nextInt(101);
+        return new Ball(randomColor, randomNumber);
+
+    }
+}
