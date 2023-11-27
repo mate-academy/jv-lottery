904bbbc70efad9b42304e8c2155656c5972aefe8
Vitalii Teben <VitalikTeben@gmail.com>
2023-11-27 16:29:32 -0500

    provide solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public Color getRandomColor() {
+        Random random = new Random();
+        Color[] colors = Color.values();
+        int colorIndex = random.nextInt(0, colors.length);
+        return colors[colorIndex];
     }
 }
