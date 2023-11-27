543eae19f41b62b73aa6aa8dc5c4b5875e43ed83
Yarolsav <kiritok712@gmail.com>
2023-11-27 23:40:00 +0100

    FixHMjv-lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private static final Random RANDOM = new Random();
+    private static final int MAX_RANDOM_NUMBER = 100;
+
     public static Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String randomColor = colorSupplier.getRandomColor();
-        int randomNumber = new Random().nextInt(100) + 1; // 1 to 100
+        String randomColor = COLOR_SUPPLIER.getRandomColor();
+        int randomNumber = RANDOM.nextInt(MAX_RANDOM_NUMBER) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
