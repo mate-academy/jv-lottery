026fd969f334c7c73ac33e1e4d4f5a413ba41431
yaroslav olieniev <olieniev.od.ua@gmail.com>
2023-12-01 20:39:44 +0300

    removed static keyword where needed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int randomMax = 100;
-    private static ColorSupplier colorSupplier = new ColorSupplier();
-    private static Random random = new Random();
+    private static final int RANDOM_MAX = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public String getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(randomMax));
+        ball.setNumber(random.nextInt(RANDOM_MAX));
         return ball.toString();
     }
 }
