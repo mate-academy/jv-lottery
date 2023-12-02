f5dfd6abe14ac2b67a81c39100da99dc73596fdc
Orest 40th <student2528@bmet.org.ua>
2023-12-02 19:37:45 +0200

    removed unnecessary class instances corrected static methods

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Ball[] balls = new Ball[3];
+        Lottery lotteryTry = new Lottery();
         for (Ball ball: balls) {
-            ball = Lottery.getRandomBall();
+            ball = lotteryTry.getRandomBall();
             System.out.println(ball.toString() + "\n" + ball.getNumber());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
+    private ColorSupplier supplier = new ColorSupplier();
+    private Random random = new Random();
+    private static final int MAGIC_NUM = 100;
+
+    public Ball getRandomBall() {
+        return new Ball(supplier.getRandomColor(), random.nextInt(MAGIC_NUM));
     }
 }
