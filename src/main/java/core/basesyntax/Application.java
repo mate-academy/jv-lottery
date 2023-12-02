533fee95825b2be4e589c56cd0d3772228e3a74b
Oleksandr Kalenyk <carbonmail88@gmail.com>
2023-12-02 17:00:13 +0200

    Removed redunant methods, changed toString override and loop in Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,13 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
         Lottery lottery = new Lottery();
 
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = lottery.getRandomBall();
-            System.out.println(balls[i].getColor() + " ball with number "
-                    + balls[i].getNumber());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int limit = 100;
     private Random random = new Random();
     private ColorSupplier supplier = new ColorSupplier();
 
     public final Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(supplier.getRandomColor().name());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(limit));
         return ball;
     }
 }
