4a4abbaa58febc668f9b0ba50e301b2b574f0119
Orest 40th <student2528@bmet.org.ua>
2023-12-02 22:01:58 +0200

    maven correction

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAGIC_NUM = 100;
     private ColorSupplier supplier = new ColorSupplier();
     private Random random = new Random();
-    private static final int MAGIC_NUM = 100;
 
     public Ball getRandomBall() {
         return new Ball(supplier.getRandomColor(), random.nextInt(MAGIC_NUM));
