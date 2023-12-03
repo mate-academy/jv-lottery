e3dc637b7fb961366a46fe22fd48e03838a68f93
rk0v <rz20@ukr.net>
2023-12-03 18:20:53 +0200

    moved ColorSupplier in to class field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public final class Lottery {
     private static final int MAXIMUM_POSSIBLE_NUMBER = 100;
     private final Random random = new Random();
+    private final String randomColor = new ColorSupplier().getRandomColor();
 
     public Ball getRandomBall() {
         final Ball randomBall = new Ball();
-        final String randomColor = new ColorSupplier().getRandomColor();
         final int randomNumber = random.nextInt(MAXIMUM_POSSIBLE_NUMBER);
         randomBall.setColor(randomColor);
         randomBall.setNumber(randomNumber);
