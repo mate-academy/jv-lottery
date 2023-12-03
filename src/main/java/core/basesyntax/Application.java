d164f336c37ad331b089f9ff9f4fd560a1cbc0a3
skillfuldev <chernonog.evgeniy@gmail.com>
2023-12-03 10:09:03 +0200

    added_hw-jv-lottery-solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball[] ballsArray = {new Lottery().getRandomBall(),
+                new Lottery().getRandomBall(),
+                new Lottery().getRandomBall()};
+
+        for (Ball ball : ballsArray) {
+            System.out.println(ball);
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        ColorSupplier colorSupplier = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(), randomNumber);
+    }
+}
