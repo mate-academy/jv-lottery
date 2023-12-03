5a24f4200192a49ba911ab59bb0c6b164b97ed69
Vlad Bondarev <bondarev.21@icloud.com>
2023-12-03 21:59:06 +0200

    changed solution after rewiev

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -8,6 +8,6 @@ public class ColorSupplier {
 
     public String getRandomColor() {
         int index = random.nextInt(enumLength);
-        return String.valueOf(Color.values()[index]);
+        return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private int maxValue = 100;
+    private static final int MAX_VALUE = 100;
     // if I assing it as MAX_VALUE then there is a mistake when calling mvn clean package
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
-        int number = random.nextInt(maxValue);
+        int number = random.nextInt(MAX_VALUE);
         String color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
