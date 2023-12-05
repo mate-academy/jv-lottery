f389dd1d3b8df8a3d4dfcfcafecf066efd7e46b3
Yaroslav Berezhniak <yaroslavix100@gmail.com>
2023-12-05 17:48:42 +0200

    task completed with error correction

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final Colors[] COLORS = Colors.values();
+    private static final Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        Colors randomColor = COLORS[random.nextInt(COLORS.length)];
+        return randomColor.name();
     }
 }
