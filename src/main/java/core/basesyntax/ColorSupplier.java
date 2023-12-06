3efc7c7066eb84e5300a2afce33b4235baa9ef68
Yaroslav Berezhniak <yaroslavix100@gmail.com>
2023-12-06 10:18:55 +0200

    make constructor in ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,11 @@ import java.util.Random;
 public class ColorSupplier {
     private static final Colors[] COLORS = Colors.values();
     private static final Random random = new Random();
+    private final String randomColor;
+
+    public ColorSupplier() {
+        this.randomColor = getRandomColor();
+    }
 
     public String getRandomColor() {
         Colors randomColor = COLORS[random.nextInt(COLORS.length)];
