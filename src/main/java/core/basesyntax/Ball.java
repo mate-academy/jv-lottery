9688355347d00a4558f4436483fa923acdb13d0e
Yevhen <shymeiko.zenya@gmail.com>
2023-12-10 20:08:16 +0200

    fixedSpaces2

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -8,12 +8,15 @@ public class Ball {
         this.color = color;
         this.number = number;
     }
+
     public void setNumber(int number) {
         this.number = number;
     }
+
     public void setColor(String color) {
         this.color = color;
     }
+
     @Override
     public String toString() {
         return "Ball number is : " + number + "\nBall color is : " + color;
