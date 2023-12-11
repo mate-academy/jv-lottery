4b929d7318a908dc97c9e0c12a49f974b6a43423
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-11 17:20:51 +0200

    the task is done or not

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     public static final int MAX_BOUND = 101;
-    public  static  final  Random random = new Random();
-    public  static  final Ball ball = new Ball();
-    public  static  final ColorSupplier colorSupplier = new ColorSupplier();
+    public static final Random random = new Random();
+    public static final Ball ball = new Ball();
+    public static final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(MAX_BOUND));
