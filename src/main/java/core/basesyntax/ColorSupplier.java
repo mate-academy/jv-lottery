db93c1b590ccd9b917463bc467170492a0f477ef
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-11 17:13:01 +0200

    the task is done or not

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    public  static  final  Random random = new Random();
     public String getRandomColor() {
-        Random random = new Random();
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].toString();
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,18 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_BOUND = 101;
+    public  static  final  Random random = new Random();
+    public  static  final Ball ball = new Ball();
+    public  static  final ColorSupplier colorSupplier = new ColorSupplier();
     public Ball getRandomBall() {
-        Ball ball = new Ball();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String color = colorSupplier.getRandomColor();
-        ball.setColor(color);
-        Random random = new Random();
-        /*const MAX_BOUND = 101;
+        ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(MAX_BOUND));
-
-        Cannot resolve symbol 'constant'
-        */
-        ball.setNumber(random.nextInt(100));
         return ball;
     }
 }
