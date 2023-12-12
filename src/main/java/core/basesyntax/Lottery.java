46f3809e02f1be4ad8d3be2a059a8450fc40af3d
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2023-12-12 15:02:13 +0200

    replaced static

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     public static final int MAX_BOUND = 101;
-    public final Random random = new Random();
-    public final ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(),random.nextInt(MAX_BOUND));
