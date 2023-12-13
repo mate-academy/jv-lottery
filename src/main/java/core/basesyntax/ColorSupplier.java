c661d7ad6cee7fba288909d774c6711ba7e842cc
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-13 15:39:23 +0100

    ColorSupplier getRandomColor Implementation

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        Colors[] values = Colors.values();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex].toString();
     }
 }
