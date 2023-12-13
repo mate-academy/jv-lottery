bb15e5dd2b74a3047e7e5568b5e00c189c8145d7
AlexL <alexlada86@gmail.com>
2023-12-13 22:14:11 +0100

    solved task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        return Color.values()[random.nextInt(Color.values().length)].toString();
     }
 }
