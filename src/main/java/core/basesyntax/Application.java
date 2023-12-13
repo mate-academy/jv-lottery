567f88db2a128ec7d029f5c282fdff8a4adbc987
Lolita <ilolita796@gmail.com>
2023-12-13 13:52:30 +0200

    Update project

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        System.out.println("First ball: " + lottery.getRandomBall());
+        System.out.println("Second ball: " + lottery.getRandomBall());
+        System.out.println("Third ball: " + lottery.getRandomBall());
     }
 }
