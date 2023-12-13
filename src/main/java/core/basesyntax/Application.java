3f4e6aa8b6465f6ad6a79b78800087520f21855c
Lolita <ilolita796@gmail.com>
2023-12-13 15:12:35 +0200

    fixed issues

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        System.out.println("First ball: " + lottery.getRandomBall());
-        System.out.println("Second ball: " + lottery.getRandomBall());
-        System.out.println("Third ball: " + lottery.getRandomBall());
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAXIMUM_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random randomNumber = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
-        return new Ball(colorSupplier.getRandomColor().toString(), random.nextInt(100));
+        String color = colorSupplier.getRandomColor().toString();
+        int number = randomNumber.nextInt(MAXIMUM_NUMBER);
+        return new Ball(color, number);
     }
 }
