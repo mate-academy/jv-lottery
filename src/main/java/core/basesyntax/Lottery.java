d87e8bfc3cc0e44f4151de5e65fdf2f5987f1d32
Marek Koscielewski <pwmarek96@gmail.com>
2023-12-14 12:54:11 +0100

    Changed Class Lottery method to static , added constant RANGE , deleted Lottery object from main

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,12 +2,14 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier {
-    public Ball getRandomBall() {
+public class Lottery {
+    private static final int RANGE = 100;
+
+    public static Ball getRandomBall() {
         Ball ball = new Ball();
         Random random = new Random();
-        ball.setColor(getRandomColor());
-        ball.setNumber(random.nextInt(101));
+        ball.setColor(ColorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(RANGE + 1));
         return ball;
     }
 }
