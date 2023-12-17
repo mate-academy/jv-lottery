1e8fee1b4dba798ae4076d91f16be86afbf0b692
Artem <artem.kozakevych@gmail.com>
2023-12-17 22:01:55 +0200

    fix class field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,17 +3,15 @@ package core.basesyntax;
 import core.basesyntax.ball.Ball;
 import core.basesyntax.ball.Color;
 import core.basesyntax.ball.ColorSupplier;
-
 import java.util.Random;
 
 public class Lottery {
 
     private static final int BOUND = 100;
-    ColorSupplier colorSupplier = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public String getRandomBall() {
-        final int indexColor = new Random().nextInt(Color.values().length);
         int number = random.nextInt(BOUND);
         return new Ball(colorSupplier.getRandomColor(), number).toString();
     }
--- src/main/java/core/basesyntax/ball/ColorSupplier.java
+++ src/main/java/core/basesyntax/ball/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private int indexColor;
-    Random random = new Random();
+    private Random random = new Random();
 
     public Color getRandomColor() {
         indexColor = random.nextInt(Color.values().length);
