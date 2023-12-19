1bbf6be4a4adb3dc1b596ce847de31383ad7d7a9
Maryna Bodina <bananafox2002@gmail.com>
2023-12-19 20:40:54 +0200

    done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
+import static core.basesyntax.Lottery.getRandomBall;
+
 public class Application {
     public static void main(String... args) {
         for (int i = 0; i < 3; i++) {
-            Ball ball = Lottery.getRandomBall();
+            Ball ball = getRandomBall();
             System.out.println(ball);
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final Random random = new Random();
     private static final int MAX_RANDOM_NUMBER = 100;
 
     public static Ball getRandomBall() {
         Color randomColor = ColorSupplier.getRandomColor();
-        Random random = new Random();
         int randomNumber = random.nextInt(MAX_RANDOM_NUMBER) + 1;
         return new Ball(randomColor, randomNumber);
     }
 }
+
