3e97f29b30382ca3779e72ee379972d6e8f629bb
Viktor Oblapenko <viktor.oblapenko@gmail.com>
2023-12-23 15:47:25 +0200

    created classes and implemented methods according to the assignment

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball firstBall = lottery.getRandomBall();
+        Ball secondBall = lottery.getRandomBall();
+        Ball thirdBall = lottery.getRandomBall();
+
+        System.out.println(firstBall.toString());
+        System.out.println(secondBall.toString());
+        System.out.println(thirdBall.toString());
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private Random random;
+
+    ColorSupplier(Random random) {
+        this.random = random;
+    }
+
     public String getRandomColor() {
-        return null;
+        Colors[] values = Colors.values();
+        int randomIndex = random.nextInt(values.length);
+        Colors randomColor = values[randomIndex];
+        return randomColor.toString();
     }
 }
