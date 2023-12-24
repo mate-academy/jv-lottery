30a08b35ed738535144cdcc7a8f5e0dae52d3205
NekitCool1 <gnnikita97@gmail.com>
2023-12-24 18:22:08 +0200

    Created Ball class, enum colors and implemented getRandomColor in class ColorSupplier, and created Lottery class with the getRandomBall()

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         ColorSupplier c = new ColorSupplier();
         return new Ball(c.getRandomColor(), new Random().nextInt(100));
     }
