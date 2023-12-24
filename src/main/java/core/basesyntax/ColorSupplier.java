b0dc3019bc6d2abfae15ff067c8266b915c24802
voody <tarasyashchuk089@gmail.com>
2023-12-24 13:57:46 +0200

    added variables to class-level

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor() {
-        Color color = Color.values()[new Random().nextInt(Color.values().length)];
+    private Random random = new Random();
+    private Color color;
 
+    public String getRandomColor() {
+        color = Color.values()[random.nextInt(Color.values().length)];
         return color.name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_VALUE = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-
-        ColorSupplier colorSupplier = new ColorSupplier();
-
         String color = colorSupplier.getRandomColor();
-
-        return new Ball(color, new Random().nextInt(MAX_VALUE));
+        return new Ball(color, random.nextInt(MAX_VALUE));
     }
 }
