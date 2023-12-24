4b5839df71a39a62d0a0125f23babd740d9bdd56
Viktor Oblapenko <viktor.oblapenko@gmail.com>
2023-12-24 18:19:54 +0200

    added a constructor to the Ball class and change the naming style of the constant value

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int NUMBER_LIMIT = 101;
+    /*the constant name is made in camel case because the checkstyle plugin gives an error on
+    the naming with two capital letter in the row */
+    private final int numberLimit = 101;
 
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier(random);
 
     public Ball getRandomBall() {
-        Ball ball = new Ball();
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(NUMBER_LIMIT));
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(numberLimit));
     }
 }
