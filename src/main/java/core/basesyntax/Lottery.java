deda5a455c770c654ad74052c7ff829e0ecb29c8
Viktor Oblapenko <viktor.oblapenko@gmail.com>
2023-12-25 22:08:34 +0200

    made the constant value static

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    /*the constant name is made in camel case because the checkstyle plugin gives an error on
-    the naming with two capital letter in the row */
-    private final int numberLimit = 101;
-
+    static final int NUMBER_LIMIT = 101;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier(random);
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(numberLimit));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(NUMBER_LIMIT));
     }
 }
