e42b17b5675377f8992c96a3b7a2df3677bc7036
Yuliia <innishastudy@gmail.com>
2023-12-26 21:26:08 +0200

    Created a simple lottery that will return balls with a random color and random number

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Ball {
     protected String color;
     protected int number;
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     public String getRandomBall() {
-       Random random = new Random();
-       ColorSupplier colorSupplier = new ColorSupplier();
-       return new Ball(colorSupplier.getRandomColor(), random.nextInt(100)).toString();
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100)).toString();
     }
 }
