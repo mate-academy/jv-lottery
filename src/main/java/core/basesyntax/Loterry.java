ce6ff81dceef017435024f36dd13483436a5b186
Havrylenko Volodymyr <gvs88986@gmail.com>
2023-12-29 07:24:43 +0200

    corrected code-style 29.12.23

--- src/main/java/core/basesyntax/Loterry.java
+++ src/main/java/core/basesyntax/Loterry.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Loterry {
+    private static final int ORIGIN = 1;
+    private static final int BOUND = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(),random.nextInt(1,100));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(ORIGIN, BOUND));
     }
 }
