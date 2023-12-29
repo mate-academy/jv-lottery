50e3215ad0a2e9dd3f9c068681808467f9fb3786
Olexander <rybkolhoz@gmail.com>
2023-12-29 10:35:49 +0200

    ball-constructor-declaration-fixed

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return this.color + " " + this.number;
+    }
+}
