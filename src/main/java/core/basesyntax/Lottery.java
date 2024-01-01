3e32932521714cd29104345f64219f8c0c74e791
Vitaliy <vitaliyufc@gmail.com>
2024-01-01 14:03:25 +0300

    Fixed OOPBasic Java Core

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Random;
+
+public class Lottery {
+
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    private final Random random = new Random();
+
+    public Ball getRandomBall() {
+
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(100));
+    }
+}
