8fbe4d15c9cd39de443e17b6891958bc7ba011a0
Marharyta Salii <margarita12122015@gmail.com>
2024-01-02 16:57:29 +0200

    jv-lottery-added-changes#2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,8 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         for (int i = 0; i < 3; i++) {
-            Ball lotteryBall = Lottery.getRandomBall();
+            Lottery random = new Lottery();
+            Ball lotteryBall = random.getRandomBall();
             System.out.println(lotteryBall);
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static Random random = new Random();
-    private static Color color;
+    private static ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int MAX_NUMBER = 100;
 
     public static Ball getRandomBall() {
-        int number = random.nextInt(100);
-        color = ColorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_NUMBER);
+        Color color = colorSupplier.getRandomColor();
         return new Ball(color, number);
     }
 }
