9f0d1ec36ffa30b3021c8f1eb0e54b4023060983
Ruslan Panasiuk <ruslanpanasuik@gmail.com>
2024-01-04 14:39:19 +0200

    Comit 3. Fixed the errors

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
     private static final int MAX_BOUND = 101;
 
     public Ball getRandomBall() {
-        String color = String.valueOf(new ColorSupplier().getRandomColor());
+        Color color = new ColorSupplier().getRandomColor();
         int randomNumber = new Random().nextInt(MAX_BOUND);
         return new Ball(color, randomNumber);
     }
