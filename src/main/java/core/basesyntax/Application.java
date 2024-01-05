79d1b8ca60a8a41b937aef13c8568c28d6a67479
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-05 15:37:03 +0200

    added a loop by creating balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,7 +6,10 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = {lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
         System.out.println(Arrays.toString(balls));
     }
 }
