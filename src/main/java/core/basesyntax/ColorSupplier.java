c451675e72bf13d7e100c8d01f204f73f380355c
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-01-05 16:31:16 +0200

    renamed enum, fixed use toString, make constant

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,11 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
-    private String color;
 
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
-        color = Color.values()[index].name();
-        return color;
+        return Color.values()[index].name();
     }
 }
