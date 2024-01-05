a7c56d0d171aa98d7e0836499b07f9d1ca3251f0
Vlad Shenderchuk <shenderchuk.vlad@gmail.com>
2024-01-05 13:12:12 +0200

    put the constant in the correct order

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER_BALL = 101;
     private ColorSupplier color = new ColorSupplier();
     private Random random = new Random();
-    private static final int MAX_NUMBER_BALL = 101;
 
     public String getRandomBall() {
         int num = random.nextInt(MAX_NUMBER_BALL);
