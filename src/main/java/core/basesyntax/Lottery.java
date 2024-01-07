07eb596886833c8deb3476db2ac460747c9ccdde
Kate Drevilo <rabmargo@gmail.com>
2024-01-07 09:22:07 +0200

    New homework. fixed errors.

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Random random = new Random();
+        Color[] colors = Color.values();
+        String randomColor = colors[random.nextInt(colors.length)].name();
+        int randomNumber = random.nextInt(100) + 1;
+        return new Ball(randomColor, randomNumber);
+    }
+}
