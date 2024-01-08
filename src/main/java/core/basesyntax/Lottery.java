a6530d5ecf12c7e729b15d6459d1f1178a70b54d
NekitCool1 <gnnikita97@gmail.com>
2024-01-08 18:38:11 +0200

    Constant field was renamed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int constant = 1000;
+    private static final int CONSTANT = 1000;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(constant));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(CONSTANT));
     }
 }
