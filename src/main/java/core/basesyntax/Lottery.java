f1d924e4ab75f1975a36909e42376c6f403589c7
Ruslan Panasiuk <ruslanpanasuik@gmail.com>
2024-01-08 15:26:41 +0200

    Comit 4. Fixed bugs with private fields.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BOUND = 101;
 
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        Color color = new ColorSupplier().getRandomColor();
-        int randomNumber = new Random().nextInt(MAX_BOUND);
+        Color color = colorSupplier.getRandomColor();
+        int randomNumber = random.nextInt(MAX_BOUND);
         return new Ball(color, randomNumber);
     }
 }
