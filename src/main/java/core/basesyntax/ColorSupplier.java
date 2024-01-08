2e1e2d3f6a5b706ca13b164cb4432e8d3db54a7b
Kate Drevilo <rabmargo@gmail.com>
2024-01-08 22:44:22 +0200

    "fixed methods getRandomBall and getRandomColor"

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        String color = Color.values()[index].name();
-        return color;
+        Random random = new Random();
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final ColorSupplier colorSupplier;
+
+    public Lottery() {
+        this.colorSupplier = new ColorSupplier();
+    }
+
     public Ball getRandomBall() {
+        String randomColor = colorSupplier.getRandomColor();
         Random random = new Random();
-        Color[] colors = Color.values();
-        String randomColor = colors[random.nextInt(colors.length)].name();
         int randomNumber = random.nextInt(100) + 1;
         return new Ball(randomColor, randomNumber);
-
     }
 }
