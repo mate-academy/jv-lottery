5eb7c7b25a4c1cfc3a0cb48941e7b980cb2d4ab5
Milosz <szwajkowski.milosz@gmail.com>
2024-01-11 10:01:30 +0100

    Fixed line of code exceeding 100 chars

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     private static final int LIMITING_NUMBER = 101;
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(LIMITING_NUMBER));
+        return new Ball(new ColorSupplier().getRandomColor(),
+                new Random().nextInt(LIMITING_NUMBER));
     }
 }
