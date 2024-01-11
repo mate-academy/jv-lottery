d230ce3bbd49bad4914a463143062fa714cf7862
Milosz <szwajkowski.milosz@gmail.com>
2024-01-11 09:56:50 +0100

    Changed style of constant int variable from "limitingNumber" to "LIMITING_NUMBER"

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int limitingNumber = 101;
+    private static final int LIMITING_NUMBER = 101;
 
     public Ball getRandomBall() {
-        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(limitingNumber));
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(LIMITING_NUMBER));
     }
 }
