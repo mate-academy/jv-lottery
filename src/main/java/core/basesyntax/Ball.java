7b23b154323b7135804ee34ad648d114dd9a2b89
OSKORBA <elskorba@gmail.com>
2024-01-17 00:06:54 +0200

    solution for task Lottery

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,2 +1,19 @@
-package core.basesyntax;public class Ball {
+package core.basesyntax;
+
+public class Ball extends ColorSupplier {
+    private final Colors color;
+    private final int number;
+
+    public Ball(int number) {
+        this.color = getRandomColor();
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{ "
+                + "color= " + color
+                + ", number= " + number
+                + " }";
+    }
 }
--- src/main/java/core/basesyntax/Colors.java
+++ src/main/java/core/basesyntax/Colors.java
@@ -1,2 +1,10 @@
-package core.basesyntax;public enum Colors {
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    GREY,
+    PINK
 }
