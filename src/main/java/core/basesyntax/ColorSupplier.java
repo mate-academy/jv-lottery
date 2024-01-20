7e4c52bf1db4c0c8fe539ad84deb4f79b0093f2e
unknown <janiszewski.maciej1@gmail.com>
2024-01-20 13:36:58 +0100

    First Solution of Lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public Color getRandomColor() {
+
+        Random random = new Random();
+        int index = new Random().nextInt(Color.values().length);
+        Color color = Color.values()[index];;
+        return Color.valueOf(color.toString());
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        Color color = colorSupplier.getRandomColor();
+        int number = random.nextInt(100);
+        return new Ball(color, number);
+    }
+}
+
