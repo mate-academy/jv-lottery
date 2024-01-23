f7c436b1bf1d78cddeab341b206d296e32f3fb1e
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-01-23 13:40:45 +0100

    Forth solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -6,10 +6,10 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        String[] lotteries = new String[3];
-        for (int i = 0; i < lotteries.length; i++) {
-            lotteries[i] = String.valueOf(lottery.getRandomBall());
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
         }
-        System.out.println(Arrays.toString(lotteries));
+        System.out.println(Arrays.toString(balls));
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private int bound = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(bound));
+        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
