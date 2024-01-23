7aab2e19878d9aabe45ff37b7f1787b695d58bf1
Sergey Sukhachov <westard@proton.me>
2024-01-24 01:40:40 +0200

    final fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -8,8 +8,8 @@ public class Lottery {
     private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-    int number = random.nextInt(MAX_BALLS) + 1;
-    String color = colorSupplier.getRandomColor();
-    return new Ball(color, number);
+        int number = random.nextInt(MAX_BALLS) + 1;
+        String color = colorSupplier.getRandomColor();
+        return new Ball(color, number);
     }
 }
