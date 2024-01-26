7edeed3d9a921bd136406018dff1257d1567a509
Mykola <mykolamalovanchuk@gmail.com>
2024-01-27 01:13:52 +0200

    create Ball, and enum Colors

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class Ball {
+    String collor;
+
+    int number;
+
+}
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    PINK,
+    GREEN,
+    YELLOW,
+    ORANGE,
+    BLUE,
+    VIOLET,
+    WHITE,
+    GRAY,
+    BROWN,
+    DARK
+}
