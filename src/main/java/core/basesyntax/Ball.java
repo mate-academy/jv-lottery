69226cfadf4ec110e5f6fe1b8b965923dc73a8bd
Владислав Чабак <vlad.chabak@gmail.com>
2024-01-26 13:35:48 +0000

    fixed5

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,6 +4,11 @@ public class Ball {
     private String color;
     private int number;
 
+    public Ball(String randomColor, int number) {
+        this.color = randomColor;
+        this.number = number;
+    }
+
     @Override
     public String toString() {
         return "Ball{"
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,13 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private Random random = new Random();
-    private Ball ball = new Ball();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
 
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
     }
 }
