863613b623b2c2fda0c2d16f46aefd722902604c
Mykola <mykolamalovanchuk@gmail.com>
2024-01-27 01:40:18 +0200

    add Lottery class, getRandomColor method, getRandomBall method

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
 public class Ball {
-    String collor;
+    private String collor;
+    private int number;
 
-    int number;
+    Ball(String collor, int number) {
+        this.collor = collor;
+        this.number = number;
+    }
 
+    public String getCollor() {
+        return collor;
+    }
+
+    public void setCollor(String collor) {
+        this.collor = collor;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                + "collor='" + collor + '\''
+                + ", number=" + number
+                + '}';
+    }
 }
