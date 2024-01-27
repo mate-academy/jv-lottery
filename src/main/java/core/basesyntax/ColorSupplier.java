3f1d4d3686ce9978119cff1a869f0e3b2e045dd5
Kacper <kacperkukula97@gmail.com>
2024-01-28 00:49:19 +0100

    First Solution, added 3 new classes and modified 2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final int COLORS_COUNT = Colors.values().length;
+    private Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        int colorIndex = random.nextInt(COLORS_COUNT);
+        Colors color = Colors.values()[colorIndex];
+        return color.name();
     }
 }
