47cf93c97bf967a945208f53af5189dafe8d66e4
Kacper <kacperkukula97@gmail.com>
2024-01-28 14:03:20 +0100

    Second solution. Renamed 'Colors' enum to 'Color', extracted random number to a constant class field.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int COLORS_COUNT = Colors.values().length;
+    private static final int COLORS_COUNT = Color.values().length;
     private Random random = new Random();
 
     public String getRandomColor() {
         int colorIndex = random.nextInt(COLORS_COUNT);
-        Colors color = Colors.values()[colorIndex];
+        Color color = Color.values()[colorIndex];
         return color.name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-
+    private static final int MAX_RANDOM_NUMBER = 101;
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_RANDOM_NUMBER));
     }
 }
