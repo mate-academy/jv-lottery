61d28b7b3a68b96811ad8323f034b115195780b2
Oleksandr Kasian <kasiashka@gmail.com>
2024-01-28 13:42:26 +0200

    removed Random to class create new constant for magic numbers

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+    private final int maxBallNumber = 100;
 
     public Ball getRandomBall() {
-        Random random = new Random();
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(maxBallNumber));
         return ball;
     }
 }
