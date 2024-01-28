83bf435660ce9fcbf859cfba7c2256f4b9e07c1d
Kacper <kacperkukula97@gmail.com>
2024-01-28 14:05:33 +0100

    Third Solution. Fixed whitespaces and variable definition order.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_RANDOM_NUMBER = 101;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private static final int MAX_RANDOM_NUMBER = 101;
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_RANDOM_NUMBER));
     }
