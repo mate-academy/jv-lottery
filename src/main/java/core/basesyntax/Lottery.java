78cc62e42a428ac5aeaa82a527cecf821dd88d5e
makaroshyna <katya23225@gmail.com>
2024-02-01 00:25:13 +0200

    Change MAX_BALL_NUMBER to include 100.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_BALL_NUMBER = 100;
+    private static final int MAX_BALL_NUMBER = 101;
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random number = new Random();
