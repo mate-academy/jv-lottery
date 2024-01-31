f9f4461f213f5f1c60d7cd9ea3a16047efebe5c8
makaroshyna <katya23225@gmail.com>
2024-02-01 00:25:54 +0200

    Rename class field random.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,9 +6,9 @@ public class Lottery {
     private static final int MAX_BALL_NUMBER = 101;
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random number = new Random();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), number.nextInt(MAX_BALL_NUMBER));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
     }
 }
