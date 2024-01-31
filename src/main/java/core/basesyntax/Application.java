0c9378b32888c2e934d521070bc2346cf5814079
Myroslav Stefanchuk <stefanchykmyroslav@gmail.com>
2024-01-31 15:55:06 +0100

    added all tasks

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball ball1 = Lottery.getRandomBall();
+        System.out.println("Ball 1: " + ball1);
+        Ball ball2 = Lottery.getRandomBall();
+        System.out.println("Ball 2: " + ball2);
+        Ball ball3 = Lottery.getRandomBall();
+        System.out.println("Ball 3: " + ball3);
     }
 }
