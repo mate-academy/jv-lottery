d25c09f758e7e2cdacef46b8b95c70e8af113651
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-01-31 21:03:17 +0200

    update version

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Color[] COLORS = Color.values();
+    private static final Random RANDOM = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index];
+        int index = RANDOM.nextInt(COLORS.length);
+        Color color = COLORS[index];
         return color.toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private ColorSupplier colorSupplier;
+    private Random random;
+
+    public Lottery() {
+        this.colorSupplier = new ColorSupplier();
+        this.random = new Random();
+    }
+
     public Ball getRandomBall() {
-        ColorSupplier randomColor = new ColorSupplier();
-        int randomNumber = new Random().nextInt(100) + 1;
-        Ball ball = new Ball(randomColor.getRandomColor(), randomNumber);
+        final int randomNumber = random.nextInt(101);
+        Ball ball = new Ball(colorSupplier.getRandomColor(), randomNumber);
 
         return ball;
     }
