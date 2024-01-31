d524a1a8afdcf3866cd104b8678f8f479b881c32
makaroshyna <katya23225@gmail.com>
2024-02-01 00:56:50 +0200

    Exclude 0 from ball number generation.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_BALL_NUMBER = 101;
+    private static final int MAX_BALL_NUMBER = 100;
 
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALL_NUMBER) + 1);
     }
 }
