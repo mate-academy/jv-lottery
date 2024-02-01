e08e30ac377344c3ce0672220b7953cfdc79c1e4
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-02-01 16:27:38 +0200

    1.Introduced class-level constant for the number of balls to generate 2.Avoided "hardcoding" in Application class. 3.replaced "random" value to the class level.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball = lottery.getRandomBall();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
 
-        System.out.println(ball);
-        System.out.println(ball1);
-        System.out.println(ball2);
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+            System.out.println(balls[i]);
+        }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public Colors getRandomColor() {
-        Random random = new Random();
+    private final Random random = new Random();
 
+    public Colors getRandomColor() {
         int index = random.nextInt(Colors.values().length);
         return Colors.values()[index];
     }
