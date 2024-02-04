5bf893830e7b57110092cb92cd701e42631ca47e
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-02-04 16:44:01 +0200

    fix mistakes after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Lottery lottery = new Lottery();
-            Ball ball = lottery.getRandomBall();
-            System.out.println(ball);
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
