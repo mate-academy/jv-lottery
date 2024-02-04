2450c6625004d9afe43f9f137a8afde9a58fd6d4
HAIOVIY <gaevoywork@gmail.com>
2024-02-04 19:37:38 +0200

    changed variables accessors

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    public String color;
-    public int number;
+    private String color;
+    private int number;
 
     public Ball(String color, int number) {
         this.color = color;
