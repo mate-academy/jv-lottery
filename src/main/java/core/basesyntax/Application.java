2b8c7c44a4ade666aac27450ba8b4edb6038d780
Taras Fedak <tarasfedak@me.com>
2024-02-04 23:40:31 +0200

    Initial commit

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        System.out.println("First " + lottery.getRandomBall());
+        System.out.println("Second " + lottery.getRandomBall());
+        System.out.println("Third " + lottery.getRandomBall());
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        Color someColor = Color.values()[random.nextInt(Color.values().length)];
+        return someColor.toString();
     }
 }
