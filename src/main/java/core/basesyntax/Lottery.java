8bf9119536c80056168b5dea9e593f6bb1de805f
Bohdan Buhrak <bohdan0410@gmail.com>
2024-02-04 20:01:35 +0200

    The task has been resolved after mentor's review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
+    private static final int MAX_SIZE = 101;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int MAX_SIZE = 101;
 
     public Ball getRandomBall() {
 
