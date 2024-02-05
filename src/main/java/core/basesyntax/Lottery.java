1919daa24c769e57355a76bc288d338f88d63123
Myroslav Stefanchuk <stefanchykmyroslav@gmail.com>
2024-02-05 20:08:28 +0100

    added fixes for class Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final Random random = new Random();
+    private final int maxRandomNumber = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = new Random().nextInt(100);
+        int randomNumber = random.nextInt(maxRandomNumber);
         return new Ball(randomColor, randomNumber);
     }
 }
