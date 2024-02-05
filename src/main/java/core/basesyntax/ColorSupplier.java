6f527cc39efff8e54f620b0e553d0276089c07de
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-02-05 19:44:17 +0200

    Extracted Random objects to constants

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Random RANDOM_NUMBER = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        return Color.values()[index].toString();
+        int index = RANDOM_NUMBER.nextInt(Color.values().length);
+        return Color.values()[index].name();
     }
 }
