370394b57ec102734432f1e12c0f024058f5132a
Iryna Stanislavchuk <iryna.bondar@ukt.net>
2024-02-06 05:06:15 +0200

    Added classes Ball, Lottery and enum. Edited classes ColorSupplier and Application.

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int COLOR_COUNT = 101;
+    private ColorSupplier colorSupplier;
+
+    public Lottery(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(COLOR_COUNT);
+        return new Ball(color, number);
+    }
+}
