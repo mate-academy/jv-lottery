59cfb5073610ed36f51a2328deb3b8928bd58e91
Patryk Tyl <tyl.pat@wp.pl>
2024-02-06 14:28:44 +0100

    Mentor fixes have been made

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random;
+
     public ColorSupplier() {
         this.random = new Random();
     }
+
     public Color getRandomColor() {
-            Color[] colors = Color.values();
-            return colors[random.nextInt(colors.length)];
+        Color[] colors = Color.values();
+        return colors[random.nextInt(colors.length)];
     }
 }
