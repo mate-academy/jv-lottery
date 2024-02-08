00971cd1bb25aeea605951a1f8bb887c0c2f0351
Patryk Tyl <tyl.pat@wp.pl>
2024-02-08 14:34:15 +0100

    final version of lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -8,7 +8,6 @@ public class Lottery {
         Color randomColor = colorSupplier.getRandomColor();
         int randomNumber = new Random().nextInt(101);
         return new Ball(randomColor,randomNumber);
-        // create three balls using class Lottery and print information about them in console
     }
 
 }
