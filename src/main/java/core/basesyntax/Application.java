d7d0a45f74c55b445f439b3b6594a932f9d88955
Nazar Klimovych <rksvjdbx@gmail.com>
2024-02-09 00:09:07 +0200

    Added implementation of task jv-lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+
+        int numberOfBallsToGenerate = 3;
+
+        for (int i = 0; i < numberOfBallsToGenerate; i++) {
+            System.out.println(lottery.getRandomBall().toString());
+        }
     }
 }
