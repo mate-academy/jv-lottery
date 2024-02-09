efffe23ff780a3f4c10de5cc6c94e3ea93e33f88
Ivan Prystaia <greqit@gmail.com>
2024-02-09 16:22:45 +0200

    gix bugs

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,16 +5,16 @@ public class Application {
         Lottery lottery = new Lottery();
         ColorSupplier colorSupplier = new ColorSupplier();
         Ball ball = new Ball();
-        ball.color = colorSupplier.getRandomColor();
-        ball.number = lottery.getRandomBall();
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(lottery.getRandomBall());
         Ball ball1 = new Ball();
-        ball1.color = colorSupplier.getRandomColor();
-        ball1.number = lottery.getRandomBall();
+        ball1.setColor(colorSupplier.getRandomColor());
+        ball1.setNumber(lottery.getRandomBall());
         Ball ball2 = new Ball();
-        ball2.color = colorSupplier.getRandomColor();
-        ball2.number = lottery.getRandomBall();
-        System.out.println("Your ball is " + ball.color + " " + ball.number);
-        System.out.println("Your ball is " + ball1.color + " " + ball.number);
-        System.out.println("Your ball is " + ball2.color + " " + ball.number);
+        ball2.setColor(colorSupplier.getRandomColor());
+        ball2.setNumber(lottery.getRandomBall());
+        System.out.println("Your ball is " + ball.getColor() + " " + ball.getNumber());
+        System.out.println("Your ball is " + ball1.getColor() + " " + ball1.getNumber());
+        System.out.println("Your ball is " + ball2.getColor() + " " + ball2.getNumber());
     }
 }
