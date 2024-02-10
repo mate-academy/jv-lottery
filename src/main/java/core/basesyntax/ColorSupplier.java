602a34384d5ffdc0a5b9e743eaed47fe188de564
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-10 21:26:42 +0200

    added modifiers in the ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
-    Color[] colors = Color.values();
-    int maxIndex = colors.length;;
+    private final Color[] colors = Color.values();
+    private final int maxIndex = colors.length;
+
     public Color getRandomColor() {
         int index = random.nextInt(maxIndex);
         return colors[index];
