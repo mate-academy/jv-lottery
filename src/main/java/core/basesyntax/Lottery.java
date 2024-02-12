db050f7af2c703db3c01a725227ebed4bd776d18
TonyH277 <robhobs50@gmail.com>
2024-02-12 14:08:38 +0200

    Change constant name in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int RANDOM = 101;
+    public static final int MAX_BALL_NUMBER = 101;
 
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(random.nextInt(RANDOM), colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(MAX_BALL_NUMBER), colorSupplier.getRandomColor());
     }
 }
+
