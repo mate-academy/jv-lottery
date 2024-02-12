31c68dbbd69c69cb8529e8df7852515a840a6bcc
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-12 22:37:11 +0200

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     public static final int AMOUNT_OF_BALL = 99;
 
+    ColorSupplier colorSuplier = new ColorSupplier();
+    Random random = new Random();
+    
     public Ball getRandomBall() {
-        return new Ball(
-                new ColorSupplier().getRandomColor(),
-                new Random().nextInt(AMOUNT_OF_BALL));
+        return new Ball(colorSuplier.getRandomColor(), random.nextInt(AMOUNT_OF_BALL));
     }
 }
