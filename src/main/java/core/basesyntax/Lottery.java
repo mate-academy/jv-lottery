c71694304ed0034bbc28b1c66966b0dd18f4362c
Valentyn Sharshon <woosay333@gmail.com>
2024-02-12 13:52:07 +0200

    separate constants and other fields with an empty line

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBER_BOUND = 101;
+
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
