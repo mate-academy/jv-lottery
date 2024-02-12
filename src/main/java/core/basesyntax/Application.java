01dff505406569d685e8da7ab2f8090c9b9cb85f
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-12 22:35:08 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,10 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
+
+        for (int i = 0; i < 2; i ++) {
         System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
+        }
+
     }
 }
