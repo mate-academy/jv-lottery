c7059ddb4a7d09b2a78edb599f1ca873e3722a2e
TonyH277 <robhobs50@gmail.com>
2024-02-12 13:02:17 +0200

    Update code style according to comments & common mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    public static final int SUPPLY = 3;
+    public static final int BALL_COUNT = 3;
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        for (int i = 0; i < SUPPLY; i++) {
+        for (int i = 0; i < BALL_COUNT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int RANDOM_BOUND = 101;
+    public static final int RANDOM = 101;
 
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(random.nextInt(RANDOM_BOUND), colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(RANDOM), colorSupplier.getRandomColor());
     }
 }
