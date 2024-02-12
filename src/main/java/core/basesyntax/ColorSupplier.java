0f4d398248a38d32b32b7d7622a4ccf39331d168
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-12 12:42:26 +0200

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class ColorSupplier {
 
     public static final int AMOUNT_OF_COLORS = 11;
-    Random random = new Random();
+    
+    private Random random = new Random();
     
     public String getRandomColor() {
         return Color.values()[random.nextInt(AMOUNT_OF_COLORS)].name();
