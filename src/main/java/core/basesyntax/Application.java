b9c51e2d2478ac01cded3e1e34e0ef9c533ce800
Andriy Gromysh <andriygromysh@ukr.net>
2024-02-13 20:55:40 +0200

    Remarks corrected

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Lottery lottery = new Lottery(colorSupplier);
         for (int i = 0; i < 3; i++) {
-            Ball ball = Lottery.getRandomBall();
+            Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,16 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        Color randomColor = ColorSupplier.getRandomColor();
-        return new Ball(randomColor.toString(), randomNumber);
+    private final Random random = new Random();
+    private ColorSupplier colorSupplier;
+
+    public Lottery(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
     }
 
+    public Ball getRandomBall() {
+        int randomNumber = random.nextInt(100);
+        Color randomColor = colorSupplier.getRandomColor();
+        return new Ball(randomColor.toString(), randomNumber);
+    }
 }
