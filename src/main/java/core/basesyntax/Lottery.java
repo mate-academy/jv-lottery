e6400e012ebc23dd5e7089f0f9eac705700ec7d7
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-13 13:30:50 +0200

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int MAX_AMOUNT_OF_BALLS = 99;
+    public static final int MAX_BALL_NUMBER = 101;
 
     private ColorSupplier colorSuplier = new ColorSupplier();
     private Random random = new Random();
