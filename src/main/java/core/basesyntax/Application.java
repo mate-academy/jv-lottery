a6af0371a027c18fe4a450e0467f3f8e52076e91
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-13 13:03:26 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Application {
+
+    public static final int AMOUNT_OF_LOTTERY_GAMES = 3;
+    
     public static void main(String[] args) {
         
-        public static final int AMOUNT_OF_LOTTERY_GAMES = 3;
         Lottery lottery = new Lottery();
 
         for (int i = 0; i < AMOUNT_OF_LOTTERY_GAMES; i++) {
