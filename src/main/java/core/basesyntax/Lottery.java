e433489bbc80987bf775d81e3eb40a545a89a211
Patryk Tyl <tyl.pat@wp.pl>
2024-02-13 19:17:53 +0100

    Changed '101' for a private static final constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_RANDOM_NUMBER = 101;
     private ColorSupplier colorSupplier;
     private Random random;
 
@@ -13,7 +14,7 @@ public class Lottery {
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(101);
+        int randomNumber = random.nextInt(MAX_RANDOM_NUMBER);
         return new Ball(randomColor, randomNumber);
     }
 }
