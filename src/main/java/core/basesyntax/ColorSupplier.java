d3a5bf44465600f58e6fa22b846f568c945704db
Nazar Klimovych <rksvjdbx@gmail.com>
2024-02-13 17:17:49 +0200

    Made ball max random number a constant, also in ColorSupplier, in method getRandomColor changed toString() to name() method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,6 +7,6 @@ public class ColorSupplier {
 
     public String getRandomColor() {
         int randomIndex = random.nextInt(Colors.values().length);
-        return Colors.values()[randomIndex].toString();
+        return Colors.values()[randomIndex].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_MAX_RANDOM_NUMBER = 100;
+
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        int maxNumber = 100;
-        int number = random.nextInt(maxNumber + 1);
+        int number = random.nextInt(BALL_MAX_RANDOM_NUMBER + 1);
         String color = colorSupplier.getRandomColor();
 
         return new Ball(color, number);
