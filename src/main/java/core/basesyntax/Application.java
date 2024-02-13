817b6269352a4d9b4b0242b9e5ab4074f99cac19
Oleksandr Farion <sashafarion@gmail.com>
2024-02-13 12:53:20 +0200

    renamed max ball number const and added const for number of iterations.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Application {
+    public static final int NUMBER_OF_ITERATIONS = 3;
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
             System.out.println(lottery.getRandomBall().toString());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,11 @@ import core.basesyntax.model.Ball;
 import java.util.Random;
 
 public class Lottery {
-    private static final int NUMBER = 101;
+    private static final int MAX_BALL_NUMBER = 101;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(random.nextInt(NUMBER), colorSupplier.getRandomColor());
+        return new Ball(random.nextInt(MAX_BALL_NUMBER), colorSupplier.getRandomColor());
     }
 }
