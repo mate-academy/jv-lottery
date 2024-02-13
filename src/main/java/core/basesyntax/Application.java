07467458593efa958468fd3f582fa6a85150b169
Oleksandr Farion <sashafarion@gmail.com>
2024-02-13 12:55:15 +0200

    added separating line

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static final int NUMBER_OF_ITERATIONS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
