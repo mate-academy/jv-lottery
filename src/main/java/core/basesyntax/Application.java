d5f62c8ecb7f3378b09ece9bc7fd660c20c4f1eb
MolchanovAlexander <rybkolhoz@gmail.com>
2024-02-14 15:02:39 +0200

    your constant field renamed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int countOfIterations = 3;
+    private static final int COUNT_OF_ITERATIONS = 3;
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        for (int i = 0; i < countOfIterations; i++) {
+        for (int i = 0; i < COUNT_OF_ITERATIONS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
