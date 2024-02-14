4387d6616f666d634a55962e911e2cd019ef5e86
Chopyk Mykola <record7654@gmail.com>
2024-02-14 11:43:53 +0200

    Refactored

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,13 @@ import java.util.Random;
 
 public class Lottery {
 
+    private final int maxBallValue; // i`m not sure, it`s would be better to make this a constant?
+
+    public Lottery() {
+        maxBallValue = 101;
+    }
+
     public Ball getRandomBall() {
-        int number = new Random().nextInt(101);
-        return new Ball(number, new ColorSupplier().getRandomColor());
+        return new Ball(new Random().nextInt(maxBallValue), new ColorSupplier().getRandomColor());
     }
 }
