ea76f0db9fa7574bd254f0dce88139b2210961ff
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-14 15:30:55 +0200

    Java Lottery - style issue

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,6 +1,23 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Ball {
     String color;
     int number;
+    public int getRandomNumber() {
+    Random value = new Random();
+    number = value.nextInt(101);
+    return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "number=" + number +
+                '}';
+    }
 }
+
+
+
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,13 +1,10 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Lottery {
-    Random number = new Random();
-    int value = number.nextInt(101);
     ColorSupplier color = new ColorSupplier();
+    Ball ball = new Ball();
 
     String getRandomBall() {
-        return color.getRandomColor() + " " + value;
+        return color.getRandomColor() + " " + ball.getRandomNumber();
     }
 }
