5290a582d3fd567e390a33e43b458dd1152041fd
VolandevlodD <vladradko2000@gmail.com>
2024-02-14 22:41:59 +0200

    Implement simple lottery with random color balls
    Added classes to implement a basic lottery with balls of random color and number. Each ball has a random color and number. Implemented the Ball class with color and number fields, an enum Color with different colors, a ColorSupplier class with a getRandomColor() method, and a Lottery class with a getRandomBall() method. Added testing in the Application class to demonstrate the functionality.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        List<Ball> ballList = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            ballList.add(lottery.getRandomBall());
+        }
+        System.out.println(ballList);
     }
 }
