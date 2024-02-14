4941914e2e28223f1b47c0ecae3ba65b26848f4f
Andriy Gromysh <andriygromysh@ukr.net>
2024-02-14 19:18:48 +0200

    Corrected

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Lottery lottery = new Lottery(colorSupplier);
+        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
             System.out.println(lottery.getRandomBall());
         }
