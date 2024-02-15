7671cf10f8f8bd10b75821c16cf481f48fdfccfb
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-15 18:38:13 +0200

    Java Lottery - fixed Ball and new Random

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class ColorSupplier {
 
-
     public Color getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         return Color.values()[index];
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int MAX_NUMBER = 101;
+    private static final int MAX_NUMBER = 101;
     private Random value = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     private int getRandomNumber() {
         return value.nextInt(MAX_NUMBER);
