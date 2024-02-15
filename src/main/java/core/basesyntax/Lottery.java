1d52e3803611b111d395e392dd714a1883f2e8ff
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-02-15 13:53:35 +0200

    fixed - Random in both classes - thx for help Is the reason of doing this - (Random initiated in class not in method ) is optiomization? But we can have other methods in this class which may not use random at all but still we will call for Random
    MVP - fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random random = new Random();
+    private Random random = new Random();
     private ColorSupplier color = new ColorSupplier();
+
     public Ball getRandomBall() {
         int number = random.nextInt(100);
         return new Ball(color.getRandomColor(),number);
