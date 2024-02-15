cf09b5b71ba3632f5cd8d0c5b03fe05ee03ad67a
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-15 13:31:01 +0200

    Java Lottery - fixed print, getRandomNumber, magic number, many objects

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,15 +4,6 @@ import java.util.Random;
 
 public class Ball {
     private Color color;
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
     private int number;
 
     @Override
