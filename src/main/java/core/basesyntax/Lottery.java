bc3566f1529e54fffb53985039a6613a5797c01e
Mykola Skrypalov <skripalov1@gmail.com>
2024-02-15 16:31:57 +0200

    fixed variables name and implementation

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_VALUE = 101;
     private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int ballNumber = random.nextInt(MAX_VALUE);
-        String color = new ColorSupplier().getRandomColor();
-        return new Ball(color, ballNumber);
+        int randomNumber = random.nextInt(MAX_VALUE);
+        String randomColor = colorSupplier.getRandomColor();
+        return new Ball(randomColor, randomNumber);
     }
 }
