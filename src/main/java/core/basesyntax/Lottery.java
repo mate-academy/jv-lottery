c1faccc3526884cba9550702ed6ae6ceddadb425
rel1c-hub <pavloo.serikov@gmail.com>
2024-02-15 17:38:35 +0200

    first commit

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        Color randomColor = ColorSupplier.getRandomColor();
+        int randomNumber = new Random().nextInt(101);
+        return new Ball(randomColor, randomNumber);
+    }
+}
