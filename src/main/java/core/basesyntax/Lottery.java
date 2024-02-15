09d6c0af1b5aa41f3bf66a89f2d88e03a259b3d6
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-15 13:45:36 +0200

    Java Lottery - fixed print, getRandomNumber, magic number, many objects

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public final int MAX_NUMBER = 101;
+    public static final int MAX_NUMBER = 101;
+
     public int getRandomNumber() {
         Random value = new Random();
         return value.nextInt(MAX_NUMBER);
