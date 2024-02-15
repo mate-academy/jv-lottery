be52b4a36f8c8318582f520945a1168f74cdc00d
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-15 17:06:27 +0200

    Java Lottery - fixed bad names for variables

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_NUMBER = 101;
+    public static final int MAX_NUMBER = 101;
     private Random value = new Random();
 
     private int getRandomNumber() {
