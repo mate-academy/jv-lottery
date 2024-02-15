eccb17ac3ff60ed4b43211ca5ce620db2fa1d665
VolandevlodD <vladradko2000@gmail.com>
2024-02-15 16:55:24 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_QUANTITY = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
-        System.out.println(lottery.getRandomBall());
+        for (int i = 0; i < BALLS_QUANTITY; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
     }
 }
