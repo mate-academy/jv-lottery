79f4921e2a9e4854162adc1f16fb6787e608427f
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-15 13:42:32 +0200

    Java Lottery - fixed print, getRandomNumber, magic number, many objects

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static final int COUNT_LOTTERYS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery ball = new Lottery();
