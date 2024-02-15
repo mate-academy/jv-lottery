10a49065f9425c986eb5b02a9a46a9fcb18e2da2
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-02-15 14:34:15 +0200

    resolved comments jv-lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,10 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        final int NUMBER_OF_BALLS = 3;
+        int NumberOfBalls = 3;
 
         Lottery lottery = new Lottery();
-        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+        for (int i = 0; i < NumberOfBalls; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
