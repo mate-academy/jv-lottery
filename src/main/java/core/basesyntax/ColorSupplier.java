161df6063ef68cc8b2110e52ef0e32831667b3e9
Tatiana <ttvv1801@gmail.com>
2024-02-16 19:30:04 +0100

    move main to a separate file Main.java or something,make color supplier a class level variable too, both shouldn't be static

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,15 +2,10 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public enum ColorSupplier {
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW,
-    ORANGE;
+public class ColorSupplier {
+    private final Random random = new Random();
 
-    private static final Random RANDOM = new Random();
-    public static ColorSupplier getRandomColor() {
-        return values()[RANDOM.nextInt(values().length)];
+    public Color getRandomColor() {
+        return Color.values()[random.nextInt(Color.values().length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,18 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ColorSupplier color = ColorSupplier.getRandomColor();
+        Color color = colorSupplier.getRandomColor();
         int number = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(color.toString(), number);
-    }
-
-    public static void main(String[] args) {
-        Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println("Ball " + (i + 1) + ": " + ball);
-        }
     }
 }
