d5184561cea3196abebcbee65f35c52435f74e23
Bohdan Buhrak <bohdan0410@gmail.com>
2024-02-17 14:45:46 +0200

    The task has been resolved after mentor's review attempt 2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,11 +4,10 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         final Lottery lottery = new Lottery();
-        Ball[] balls = {lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
+        Ball[] balls = {lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall(), lottery.getRandomBall()};
 
         for (Ball ball : balls) {
             System.out.println(ball);
         }
     }
 }
-
