7d38c5742e12e72ce04e0a10aac4c36c188d2003
Alona Martynova <alona.a.martynova@gmail.com>
2024-02-20 21:06:42 +0200

    created a lottery that will return balls with a random color and random number (classes: Ball,Lottery, several methods) and tested the program in the main() method.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball [] setOfBalls = new Ball [3];
+        Ball[] setOfBalls = new Ball[3];
+        Lottery lottery = new Lottery();
         for (int i = 0; i < setOfBalls.length; i++) {
-            setOfBalls[i] = new Lottery().getRandomBall();
+            setOfBalls[i] = lottery.getRandomBall();
+            System.out.println(setOfBalls[i].toString());
         }
-        System.out.println(Arrays.toString(setOfBalls));
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends ColorSupplier {
-    private final int maxValue = 100;
-    private Random ballNumber = new Random();
-    private ColorSupplier ballColor = new ColorSupplier();
+    private static final int MAX_VALUE = 100;
+    private Random getBallNumber = new Random();
+    private ColorSupplier getBallColor = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(ballColor.getRandomColor(),ballNumber.nextInt(maxValue));
+        return new Ball(getBallColor.getRandomColor(), getBallNumber.nextInt(MAX_VALUE));
     }
 }
