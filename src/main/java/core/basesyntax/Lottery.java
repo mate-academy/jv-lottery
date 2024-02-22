6c5399b4004ce8336df8acd2bd335448604b04f9
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-02-22 23:29:49 +0200

    Update Lottery.java
    Use private final class-level variable instead of a local ones,
    added constant like a field (I decided to add it here and not in the class Ball)
    Кeduced the number of temporary variables in getRandomBall() method

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        Ball ball = new Ball();
-        Random random = new Random();
-        ColorSupplier colorSupplier = new ColorSupplier();
-        colorSupplier.getRandomColor();
-        ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
 }
