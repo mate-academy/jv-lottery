204a1c7b94f30fcef38c12b328ea56da8f400dcb
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-02-22 23:23:59 +0200

    Update Colors.java
    Use different enum code style and override toString methods for values

--- src/main/java/core/basesyntax/Colors.java
+++ src/main/java/core/basesyntax/Colors.java
@@ -1,12 +1,52 @@
 package core.basesyntax;
 
 public enum Colors {
-    White,
-    Black,
-    Red,
-    Yellow,
-    Orange,
-    Green,
-    Blue,
-    Purple
+    WHITE {
+        @Override
+        public String toString() {
+            return "White";
+        }
+    },
+    BLACK {
+        @Override
+        public String toString() {
+            return "Black";
+        }
+    },
+    RED {
+        @Override
+        public String toString() {
+            return "Red";
+        }
+    },
+    YELLOW {
+        @Override
+        public String toString() {
+            return "Yellow";
+        }
+    },
+    ORANGE {
+        @Override
+        public String toString() {
+            return "Orange";
+        }
+    },
+    GREEN {
+        @Override
+        public String toString() {
+            return "Green";
+        }
+    },
+    BLUE {
+        @Override
+        public String toString() {
+            return "Blue";
+        }
+    },
+    PURPLE {
+        @Override
+        public String toString() {
+            return "Purple";
+        }
+    }
 }
