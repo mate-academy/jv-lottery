0b21bd421bd604d35e138d73888bd8c150f5549e
Oleh <vojtihivskijo@gmail.com>
2024-02-23 22:51:33 +0200

    fixed style issues

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Random;
-
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier colorSupplier = new ColorSupplier();
     private static final int MAX_NUMBER = 101;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(MAX_NUMBER));
     }
