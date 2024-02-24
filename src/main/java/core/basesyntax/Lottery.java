f2e14c108075d0f61cf40205900d358b4de241b9
Oleh Vodolazov <oleh.vodolazov@gmail.com>
2024-02-24 18:57:39 +0200

    fix mvn clean package error

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final ColorSupplier color = new ColorSupplier();
     private static final int MAX_NUMBER = 100;
+    private final ColorSupplier color = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
