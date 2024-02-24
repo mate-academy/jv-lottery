085e2c6a37ea0198338de0d51f9a51ca39318954
Oleh <vojtihivskijo@gmail.com>
2024-02-24 20:08:58 +0200

    Ball doesn't extends Lottery, toString isn't overrided in Color

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 101;
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), new Random().nextInt(MAX_NUMBER));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
 
 }
