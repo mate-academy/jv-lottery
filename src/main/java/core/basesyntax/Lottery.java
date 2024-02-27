315dd5d29dd8e700dc66de4659fab9c50ea409d6
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-27 13:40:18 +0200

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,1 +1,8 @@
+public class Lottery {
+    public static  Ball getRandomBall() {
+        Color randomColor = ColorSupplier.getRandomColor();
+        int randomNumber = new Random().nextInt(100) + 1;
+        return new Ball(randomColor, randomNumber);
+    }
+}
 
