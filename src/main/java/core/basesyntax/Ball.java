7e2f87101e49c379e9ac6c060d28b88272f332eb
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-27 13:40:57 +0200

    Update Ball.java

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,1 +1,16 @@
+enum Color {
+    RED, BLUE, YELLOW, GREEN
+}
 
+ public class Ball {
+    private Color color;
+    private int number;
+    public Ball(Color color, int number){
+        this.color = color;
+        this.number = number;
+    }
+}
+@Override
+public  String  toString() {
+    return "Шар: цвет - " + color + ", номер - " + number;
+}
