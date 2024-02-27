10be0072db5838fa69c5a433fae4c1debb61cbf6
T0sh1s <144794622+T0sh1s@users.noreply.github.com>
2024-02-27 13:42:42 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball ball1 = Lottery.getRandomBall(ball1);
+        Ball ball2 = Lottery.getRandomBall(ball2);
+        Ball ball3 = Lottery.getRandomBall(ball3);
+
+        System.out.println(ball1);
+        System.out.println(ball1);
+        System.out.println(ball1);// create three balls using class Lottery and print information about them in console
     }
 }
