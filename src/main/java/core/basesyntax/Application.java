fa297ac97e8bff43ff2648d17296c319a39d9fa7
global source <danmaksimuk@gmail.com>
2024-02-28 13:27:35 +0100

    fixed all the bugs in the code.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,12 +4,12 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
+        Ball firstBall = lottery.getRandomBall();
+        Ball secondBall = lottery.getRandomBall();
+        Ball thirdBall = lottery.getRandomBall();
 
-        System.out.println("Ball 1: " + ball1);
-        System.out.println("Ball 2: " + ball2);
-        System.out.println("Ball 3: " + ball3);
+        System.out.println("Ball 1: " + firstBall);
+        System.out.println("Ball 2: " + secondBall);
+        System.out.println("Ball 3: " + thirdBall);
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
-        Colors randomColor = ColorSupplier.getRandomColor();
+        Color randomColor = ColorSupplier.getRandomColor();
         int randomNumber = new Random().nextInt(100) + 1;
         return new Ball(randomColor, randomNumber);
     }
