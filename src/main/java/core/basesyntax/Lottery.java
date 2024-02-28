82a017df8c2af687df00e0cb8edd025f9c4e563e
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-02-28 14:50:04 +0200

    Corrected all comments

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier newcolor = new ColorSupplier();
     private final Random random = new Random();
-    private final int Max_Value = 100;
+    private final int MaxValue = 100;
 
     public Ball getRandomBall() {
         String color = newcolor.getRandomColor();
-        int number = random.nextInt(Max_Value) + 1;
+        int number = random.nextInt(MaxValue) + 1;
         return new Ball(color, number);
     }
 }
