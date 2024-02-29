7b8b3976796e9b9874342df17f0fef1c051ffce3
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-02-29 15:15:41 +0200

    changed coments

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Random RANDOMNUM = new Random();
+    private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = RANDOMNUM.nextInt(Colors.values().length);
+        int index = random.nextInt(Colors.values().length);
         return Colors.values()[index].name();
     }
 }
