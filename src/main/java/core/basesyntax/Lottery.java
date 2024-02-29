11c50149234a5ac34375e91468981506c67e97df
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-02-29 12:49:35 +0200

    Fixed constant names

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random RANDOM = new Random();
+    private static final Random random = new Random();
     private static final int MAX_VALUE = 100;
-    private static final ColorSupplier NEWCOLOR = new ColorSupplier();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        String color = NEWCOLOR.getRandomColor();
-        int number = RANDOM.nextInt(MAX_VALUE) + 1;
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_VALUE) + 1;
         return new Ball(color, number);
     }
 }
