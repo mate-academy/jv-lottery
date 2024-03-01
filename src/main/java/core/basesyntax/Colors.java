064818ad0e3ab00a0b6c33d56c45a55abeb9d4ed
Wersebu <cz.damax@gmail.com>
2024-03-01 16:17:01 +0100

    Creating one enum and write code for 4 class. Program lottery complete

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    GREEN,
+    BLACK,
+    WHITE,
+    YELLOW,
+    PINK,
+    ORANGE,
+    LIME,
+    CYAN,
+    PURPLE,
+
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier;
+
+    public Lottery(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
+    public Ball getRandomBall() {
+        String randomColor = colorSupplier.getRandomColor();
+        int randomNumber = new Random().nextInt(100) + 1;
+        return new Ball(randomColor, randomNumber);
+    }
+
+}
