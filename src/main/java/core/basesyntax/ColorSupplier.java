538d52fa040dab32f8dd5806153a35e34c0ee38d
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-03-01 15:55:23 +0200

    1)Created Ball, Lottery classes 2)Added body to Ball class (getter, setter, + enum color) 3)Implemented STATIC methods in a Lottery and ColorSupplier classes. 4)Created 3 random balls by static getRandomBall method and ran main method.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static Color getRandomColor() {
+        Color[] colors = Color.values();
+        int index = new Random().nextInt(Color.values().length);
+        return colors[index];
     }
 }
