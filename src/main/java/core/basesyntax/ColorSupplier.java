1a7e544eea8a0c57d510dfa3decd26670e46ad9b
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-02 16:26:15 +0200

    ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class ColorSupplier {
     private static final Random random = new Random();
 
-    public String getRandomColor() {
+    public Color getRandomColor() {
         Color[] colors = Color.values();
-        return colors[random.nextInt(colors.length)].name();
+        return colors[random.nextInt(colors.length)];
     }
 }
