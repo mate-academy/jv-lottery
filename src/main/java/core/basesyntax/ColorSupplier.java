6abf61e90ef42658556dd03fd60b6640cf78250a
Wersebu <cz.damax@gmail.com>
2024-03-04 19:24:29 +0100

    Fixed no used random initialized problem

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -10,7 +10,7 @@ public class ColorSupplier {
     }
 
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
+        int index = random.nextInt(Color.values().length);
         Color color = Color.values()[index];
         return color.name();
     }
