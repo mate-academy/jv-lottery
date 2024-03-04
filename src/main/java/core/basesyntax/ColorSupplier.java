7ff4af9bb00818a90469fc38c360f6874fdba812
Wersebu <cz.damax@gmail.com>
2024-03-04 10:48:31 +0100

    Class name changed/Switched implementation of random

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private final Random random;
+
+    public ColorSupplier() {
+        this.random = new Random();
+    }
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
-        Colors color = Colors.values()[index];
+        int index = new Random().nextInt(Color.values().length);
+        Color color = Color.values()[index];
         return color.name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private ColorSupplier colorSupplier;
+    private static final int MAX_RANDOM_NUMBER = 100;
+    private final ColorSupplier colorSupplier;
+    private final Random random;
 
     public Lottery(ColorSupplier colorSupplier) {
         this.colorSupplier = colorSupplier;
+        this.random = new Random();
     }
 
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
-        int randomNumber = new Random().nextInt(100) + 1;
+        int randomNumber = random.nextInt(MAX_RANDOM_NUMBER) + 1;
         return new Ball(randomColor, randomNumber);
     }
 
