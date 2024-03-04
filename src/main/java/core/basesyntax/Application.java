b07bfef8451cfeab24f9cb64fc05b28c6938f49f
Julianevinska <Julianevinska@gmail.com>
2024-03-04 22:13:38 +0100

    fixed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Lottery ballOne = new Lottery();
-        Lottery ballTwo = new Lottery();
-        Lottery ballThree = new Lottery();
+        Lottery lottery = new Lottery();
 
-        System.out.println(ballOne.getRandomBall().toString());
-        System.out.println(ballTwo.getRandomBall().toString());
-        System.out.println(ballThree.getRandomBall().toString());
+        System.out.println(lottery.getRandomBall().toString());
+        System.out.println(lottery.getRandomBall().toString());
+        System.out.println(lottery.getRandomBall().toString());
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        Random random = new Random();
-        Colors[] colors = Colors.values();
+        Color[] colors = Color.values();
         int indexOfRandom = random.nextInt(colors.length);
         return colors[indexOfRandom].toString();
     }
