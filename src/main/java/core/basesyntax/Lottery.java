709722b5452959ce4327c34c4291d4d321c22e1e
Banderan <bartoszwojcik2004@gmail.com>
2024-03-05 11:07:04 +0100

    Program before tests

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,2 +1,16 @@
-package core.basesyntax;public class Lottery {
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    public Ball getRandomball(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(101);
+
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String randomColor = colorSupplier.getRandomColor();
+
+        return new Ball(randomColor, randomNumber);
+    }
 }
