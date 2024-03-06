ffdb03a7d44eba07faab36c153b44c1429713814
Banderan <bartoszwojcik2004@gmail.com>
2024-03-06 15:33:40 +0100

    Fixing code after review with checking mvn

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
-    Colors[] colors = Colors.values();
+    private Random random = new Random();
+    private Colors[] colors = Colors.values();
+
     public String getRandomColor() {
         return colors[random.nextInt(colors.length)].toString();
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAXIMUM_POSSIBLE_NUMBER = 101;
-    Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomball() {
         int randomNumber = random.nextInt(MAXIMUM_POSSIBLE_NUMBER);
