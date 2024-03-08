08a23a114219df2d2cc5e594dcab973072e4240b
Oleksandr Fetisenko <alexandr.fetisenko@gmail.com>
2024-03-08 22:41:19 +0200

    - Fixed magic number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_COUNT = 101;
     private Random random;
     private ColorSupplier supplier;
 
@@ -17,7 +18,7 @@ public class Lottery {
     }
 
     Ball getRandomBall() {
-        int randomNumber = random.nextInt(101);
+        int randomNumber = random.nextInt(NUMBER_COUNT);
         return new Ball(supplier.getRandomColor(), randomNumber);
     }
 }
