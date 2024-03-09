cb52441c359d313f5eedeb5d07f7c61b75ce1aa2
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-09 18:54:57 +0200

    Instances moved as final fields of class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
     public Ball getRandomBall() {
-        Colors randomColor = new ColorSupplier().getRandomColor();
-        int randomNumber = new Random().nextInt(100);
+        String randomColor = colorSupplier.getRandomColor();
+        int randomNumber = random.nextInt(100);
         return new Ball(randomColor, randomNumber);
     }
 }
