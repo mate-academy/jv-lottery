c68f68c7ce2a8c42853efdf975920a1dd45be210
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-09 18:59:04 +0200

    Instances moved as final fields, returning color as String instead of enum value

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public Colors getRandomColor() {
-        Colors[] colors = Colors.values();
-        int index = new Random().nextInt(colors.length);
-        return colors[index];
+    private final Colors[] colors = Colors.values();
+    private final Random random = new Random();
+
+    public String getRandomColor() {
+        int index = this.random.nextInt(this.colors.length);
+        return this.colors[index].toString();
     }
 }
