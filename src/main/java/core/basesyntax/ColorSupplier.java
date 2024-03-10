489b9d0a6d1d79e2f901803097a9b4c959b6e697
Oleksandr Fetisenko <alexandr.fetisenko@gmail.com>
2024-03-10 17:00:14 +0200

    Deleted static from getRandomColor method

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
+    public String getRandomColor() {
         Color[] colors = Color.values();
         Random random = new Random();
         int index = random.nextInt(colors.length);
