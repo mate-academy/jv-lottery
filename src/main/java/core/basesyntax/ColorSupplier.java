b2c7a07777d1244217b1019ac6ae0927d50c1780
Ruslan Shastkiv <shastkiv.ruslan.dev@gmail.com>
2024-03-10 20:10:01 +0200

    [Java Core/OOP/Lottery]solution v1

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final Random random = new Random();
+    private final Colors[] colors = Colors.values();
+
     public String getRandomColor() {
-        return null;
+        int randTemp = random.nextInt(colors.length);
+        return colors[randTemp].toString();
     }
 }
