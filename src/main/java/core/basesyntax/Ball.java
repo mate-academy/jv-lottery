c2286ace060f64aa9893e1c81f62cf55f5a4d385
Tymofii Snopov <snop.0904@gmail.com>
2024-03-11 17:49:37 +0100

    this task is done

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class Ball {
-    String colour;
-    int number;
+    private String colour;
+    private int number;
 
     public Ball(Color randomColor, int randomNumber) {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return colour + number;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-        public static Ball getRandomBall() {
-            Color randomColor = ColorSupplier.getRandomColor();
-            Random random = new Random();
-            int randomNumber = random.nextInt(100) + 1; // Random number between 1 and 100
-            return new Ball(randomColor, randomNumber);
-        }
+    public static Ball getRandomBall() {
+        Color randomColor = ColorSupplier.getRandomColor();
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1; // Random number between 1 and 100
+        return new Ball(randomColor, randomNumber);
+    }
 }
 
