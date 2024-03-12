2d201e2fef6e8b3c96034b6d49a983b281caddcb
Alisa Hoys <hoys@protonmail.com>
2024-03-12 17:49:53 +0200

    Corrected style

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random random;
     private static final int MAX_RANDOM_NUMBER = 100;
-
+    private final Random random;
+    
     public Lottery() {
         this.random = new Random();
     }
+
     public Ball getRandomBall() {
         int randomNumber = random.nextInt(MAX_RANDOM_NUMBER);
         ColorSupplier colorSupplier = new ColorSupplier();
