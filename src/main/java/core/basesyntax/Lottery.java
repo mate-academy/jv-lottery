cfda411ba801b8dc2ae85072eae9a1d19d2fa5dc
Alisa Hoys <hoys@protonmail.com>
2024-03-12 17:46:10 +0200

    Added constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private final Random random;
+    private static final int MAX_RANDOM_NUMBER = 100;
 
     public Lottery() {
         this.random = new Random();
     }
     public Ball getRandomBall() {
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(MAX_RANDOM_NUMBER);
         ColorSupplier colorSupplier = new ColorSupplier();
         Colors color = colorSupplier.getRandomColor();
         return new Ball(color, randomNumber);
