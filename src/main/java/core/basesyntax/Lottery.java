3ee1316592659414548544f4d25bd5b1ba18e341
Alisa Hoys <hoys@protonmail.com>
2024-03-12 17:44:30 +0200

    Made "random" a class level field

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final Random random;
+
+    public Lottery() {
+        this.random = new Random();
+    }
     public Ball getRandomBall() {
-        Random random = new Random();
         int randomNumber = random.nextInt(100);
         ColorSupplier colorSupplier = new ColorSupplier();
         Colors color = colorSupplier.getRandomColor();
