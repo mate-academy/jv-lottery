91bc95cbf4c85e9879f33949d739c21e0d2c6ec5
Anastasiia Nikita <anastasiya.folk@gmail.com>
2024-03-13 11:45:45 +0200

    Fix after review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_BALLS = 3;
+    private static final Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
         Ball[] balls = new Ball[NUMBER_OF_BALLS];
 
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Lottery().getRandomBall();
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i].toString());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         int randomInt = random.nextInt(MAX_NUMBER);
-        String randomColor = new ColorSupplier().getRandomColor();
+        String randomColor = colorSupplier.getRandomColor();
         return new Ball(randomColor, randomInt);
     }
 }
