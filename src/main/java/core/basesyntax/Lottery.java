3622603beb7904d4b696198a0d608012e5c1c103
illia riaboi <riaboy94@gmail.com>
2024-03-13 23:31:12 +0200

    Solution

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private static final int MAX_BALLS_NUMBER = 100;
+    private final static int MAX_BALLS_NUMBER = 100;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_BALLS_NUMBER));
