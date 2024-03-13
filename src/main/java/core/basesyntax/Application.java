76a10f84e2658889075849df8b345c3abb1c41c6
illia riaboi <riaboy94@gmail.com>
2024-03-13 23:23:12 +0200

    Solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball[] balls = {
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall(),
-                new Lottery().getRandomBall()};
-        for (Ball ball: balls) {
-            System.out.println(ball);
+        Ball[] balls = new Ball[3];
+        for (int i = 0; i < 3; i++) {
+            balls[i] = new Lottery().getRandomBall();
+            System.out.println(balls[i]);
         }
     }
 }
