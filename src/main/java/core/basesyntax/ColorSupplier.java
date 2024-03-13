d5bee26a0c75cc1ecab66ce06d25a4d719e14dfe
illia riaboi <riaboy94@gmail.com>
2024-03-13 14:36:05 +0200

    Solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class  ColorSupplier {
+public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
         return Colors.values()[index].toString();
