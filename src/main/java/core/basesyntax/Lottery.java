1fecf8fa120922efc9dcdc49b2bbe148e6a76bcb
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-14 13:02:57 +0200

    RandomMaxValue like a static constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int randomMaxValue = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
-        int randomMaxValue = 100;
         int randomNumber = random.nextInt(randomMaxValue);
         return new Ball(randomColor, randomNumber);
     }
