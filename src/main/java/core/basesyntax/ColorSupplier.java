211820c7627d0bd288a5bf41a8f3be219e4af35d
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-14 13:01:48 +0200

    Singular enum Color

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Colors[] colors = Colors.values();
+    private final Color[] colors = Color.values();
     private final Random random = new Random();
 
     public String getRandomColor() {
