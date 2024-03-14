957002c282928505d39cf2d8dea162d6d53c9964
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-14 17:05:29 +0200

    Random max value in uppercase

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int randomMaxValue = 100;
+    private static final int RANDOM_MAX_VALUE = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(randomMaxValue);
+        int randomNumber = random.nextInt(RANDOM_MAX_VALUE);
         return new Ball(randomColor, randomNumber);
     }
 }
