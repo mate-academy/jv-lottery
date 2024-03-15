a0a8679b35f78e57c1139430db9055ccc5be80ff
Vadik Rakitin <vadym.rakitin@gmail.com>
2024-03-15 21:37:14 +0200

    fixed magic number and used name() for return.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_RANDOM_BALL_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(MAX_RANDOM_BALL_NUMBER);
         return new Ball(randomNumber, randomColor);
     }
 }
