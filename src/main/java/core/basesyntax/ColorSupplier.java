d17d2a25514dc38eab1c8d44a5cb70bbd95f25ef
Vadik Rakitin <vadym.rakitin@gmail.com>
2024-03-15 20:43:40 +0200

    fixed solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
+
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
         Color color = Color.values()[index];
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
         int randomNumber = random.nextInt(100);
