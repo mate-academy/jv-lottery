bf7a402c0ecb00621d94b52bec2047dec569f814
Eugene <eugene.taranenko.it@gmail.com>
2024-03-19 16:52:00 +0200

    all objects are created

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.Random;
+public class Ball {
+    private final String color;
+    private final int number;
+
+    public Ball() {
+        this.color = ColorSupplier.getRandomColor();
+        this.number = new Random().nextInt(101);
+    }
+
+    @Override
+    public String toString() {
+        return "color: " + this.color + " | " + "number: " + this.number;
+    }
+}
