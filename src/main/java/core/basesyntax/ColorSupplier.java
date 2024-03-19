124e479a344ceb383713a914fb6d7acadaa2d2c4
Tomasz Baran <MarcusOfficial@tuta.io>
2024-03-19 16:50:34 +0100

    made colors field static in ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final Color[] colors = Color.values();
+
     public static Color getRandomColor() {
         Random random = new Random();
-        Color[] colors = Color.values();
         int randomIndex = random.nextInt(colors.length);
 
         return colors[randomIndex];
