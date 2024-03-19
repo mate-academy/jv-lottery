20b5ebd9a7d6c49086ac5f62fed2a2f0941fe37f
Alisa Hoys <hoys@protonmail.com>
2024-03-19 16:55:11 +0200

    Created ColorSupplier instance on class level of the Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_RANDOM_NUMBER = 100;
+    private final ColorSupplier supplier = new ColorSupplier();
+
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         int randomNumber = random.nextInt(MAX_RANDOM_NUMBER);
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String color = colorSupplier.getRandomColor();
+        String color = supplier.getRandomColor();
         return new Ball(color, randomNumber);
     }
 }
