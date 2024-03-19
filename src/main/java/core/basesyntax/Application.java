a83e1835943ef0560288958e2f41d67e8ef1f014
Valeria <valeriaua97@gmail.com>
2024-03-19 15:01:23 +0100

    fixed test failure

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-
         for (int i = 0; i < 3; i++) {
-             Lottery lottery = new Lottery();
-
-             System.out.println(lottery.getRandomBall());
+            Lottery lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
