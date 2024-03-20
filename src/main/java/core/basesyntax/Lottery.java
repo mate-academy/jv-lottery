4118b2b62b34bde2b004a05920606a4a9c9d2efc
Eugene <eugene.taranenko.it@gmail.com>
2024-03-20 21:54:50 +0200

    made limitNumbers as psf class-level

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
+    private static final int limitNumbers = 101;
 
     public Ball getRandomBall() {
-        final int limitNumbers = 101;
         String color = colorSupplier.getRandomColor();
         int number = random.nextInt(limitNumbers);
         return new Ball(color, number);
