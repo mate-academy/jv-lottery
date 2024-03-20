125ee6fb74d3c69ab9fb5dd136a4f46f08d431a8
citruscaviar <citruscaviar96@gmail.com>
2024-03-20 21:48:11 +0300

    magic number constant fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 101;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int MAX_NUMBER = 101;
 
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
