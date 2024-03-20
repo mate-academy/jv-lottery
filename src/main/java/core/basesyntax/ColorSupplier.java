08bf16403a59c39e6ea87941e5b71ddea6797afa
Bohdan Buhrak <bohdan0410@gmail.com>
2024-03-20 22:19:08 +0200

    deleted redundant methods

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Color[] colors = Color.values();
     private static final Random random = new Random();
 
     public Color getRandomColor() {
+        Color[] colors = Color.values();
         return colors[random.nextInt(colors.length)];
     }
 }
