f37daa65cb380af3bddc1b942c67ab909276c432
Eugene <eugene.taranenko.it@gmail.com>
2024-03-20 18:27:26 +0200

    changed local variables to class-level

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         final int limitNumbers = 101;
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
         String color = colorSupplier.getRandomColor();
         int number = random.nextInt(limitNumbers);
         return new Ball(color, number);
