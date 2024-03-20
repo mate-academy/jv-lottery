52628cd85e61a9becb9707eda4c57210f5241c60
Letu <lean7ua@gmail.com>
2024-03-20 20:31:12 +0200

    removed unused methods in Ball class and redundant blank line in Lottery class, also set constant to uppercase

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    private static final int maxNumber = 100;
+    private static final int MAX_NUMBER = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(maxNumber));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(MAX_NUMBER));
     }
 }
