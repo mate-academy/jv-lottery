3517c65239c4d813a1579ec842cb9b03a918219d
Eugene <eugene.taranenko.it@gmail.com>
2024-03-20 17:34:40 +0200

    removed comments, corrected variables,removed magic num, created instances of ColorSuplier & Random classes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int numbersOfColors = Color.values().length;
+    private final int numbersOfColors = Color.values().length;
     private Random random = new Random();
 
     public String getRandomColor() {
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+
     public Ball getRandomBall() {
-        String color = new ColorSupplier().getRandomColor();
-        int number = new Random().nextInt(101);
+        final int limitNumbers = 101;
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Random random = new Random();
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(limitNumbers);
         return new Ball(color, number);
     }
 }
