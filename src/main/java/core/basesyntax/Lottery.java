f1c2ddaaef3b327c8f0b70832ffcdc31ab58b027
Eugene <eugene.taranenko.it@gmail.com>
2024-03-22 01:07:19 +0200

    changed constant name to uppercase

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int limitNumbers = 101;
+    private static final int LIMIT_NUMBERS = 101;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(limitNumbers);
+        int number = random.nextInt(LIMIT_NUMBERS);
         return new Ball(color, number);
     }
 }
