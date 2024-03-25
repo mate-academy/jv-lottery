3fa570fde74ba6a73207009de55ce59cf8f7299b
Max Krup <tochnonemaks@gmail.com>
2024-03-25 12:47:09 +0200

    add mvn requirements

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    ColorSupplier color = new ColorSupplier();
-    Random random = new Random();
+    private ColorSupplier color = new ColorSupplier();
+    private Random random = new Random();
+
     public Ball getRandomBall() {
         return new Ball(color.getRandomColor(),random.nextInt(1, 101));
     }
