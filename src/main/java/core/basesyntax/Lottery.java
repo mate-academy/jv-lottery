0d2e3637f3aedbd7c3ddf706fcbc13d6eb424d08
Tomasz Baran <MarcusOfficial@tuta.io>
2024-03-25 13:06:18 +0100

    fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private static final Random random = new Random();
+    private static final int upperBound = 101;
 
     public Ball getRandomBall() {
         Color color = new ColorSupplier().getRandomColor();
-        int randomNumber = random.nextInt(101);
+        int randomNumber = random.nextInt(upperBound);
 
         return new Ball(color, randomNumber);
     }
