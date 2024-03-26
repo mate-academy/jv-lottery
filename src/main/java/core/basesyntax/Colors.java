203ce3bfc01093f4dfde83930d5b8a347a962555
AnnaAAA3432 <144123194+AnnaAAA3432@users.noreply.github.com>
2024-03-26 16:47:53 +0000

    request review

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED, BLUE, GREEN
+
+}
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,18 +1,26 @@
 package core.basesyntax;
 
-public class Lottery {
-    private int number;
-    private String color;
+import java.util.Random;
 
-    public Lottery(int number, String color) {
-        this.number = number;
-        this.color = color;
+public class Lottery {
+    private static final int BALLS_COUNT = 10;
+    private static final int MAX_BALL_NUMBER = 100;
+
+    public static void main(String[] args) {
+      for (int i = 0; i < BALLS_COUNT; i++) {
+          Ball ball = getRandomBall();
+          System.out.println(ball);
+          System.out.println();
+      }
     }
 
-    public void displayInfo() {
-        System.out.println("Lottery Ball:");
-        System.out.println("Number: " + number);
-        System.out.println("Color: " + color);
-        System.out.println();
+    private static Ball getRandomBall() {
+        ColorSupplier colorSupplier = new ColorSupplier();
+
+        Random random = new Random();
+        int number = random.nextInt(MAX_BALL_NUMBER - 1) + 1;
+
+
+        return new Ball(number, colorSupplier.getRandomColor());
     }
 }
