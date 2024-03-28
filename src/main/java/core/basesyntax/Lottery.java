b813c6f5594a20f1efa1bf57403fa035cd79a79a
Anna-Sofia Halas <annasofiia.halas@gmail.com>
2024-03-28 12:37:21 +0200

    Use constants for magic numbers

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     protected ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
+    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         ColorSupplier.Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(100) + 1;
+        int randomNumber = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(randomColor.toString(), randomNumber);
     }
 }
