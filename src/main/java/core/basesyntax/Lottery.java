4c16565e2047547d41b4bd2f26f48107174d88f7
Anna-Sofia Halas <annasofiia.halas@gmail.com>
2024-03-28 13:07:38 +0200

    Use name() for enum representation

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     protected ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         ColorSupplier.Color randomColor = colorSupplier.getRandomColor();
         int randomNumber = random.nextInt(MAX_NUMBER) + 1;
-        return new Ball(randomColor.toString(), randomNumber);
+        return new Ball(randomColor.name(), randomNumber);
     }
 }
