dad8b21388a508c9ab249578cc148a1657722460
Yurii Soliar <us1585707@gmail.com>
2024-03-30 12:07:41 +0200

    resolve lottery task

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.entity.Ball;
+
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball firstBall = Lottery.getRandomBall();
+        Ball secondBall = Lottery.getRandomBall();
+        Ball thirdBall = Lottery.getRandomBall();
+
+        System.out.println(firstBall.toString());
+        System.out.println(secondBall.toString());
+        System.out.println(thirdBall.toString());
     }
 }
