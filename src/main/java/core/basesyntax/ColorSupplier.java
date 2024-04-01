25b01b1aacc134c60cd6cc23102f7f5294c1f31f
Illia Petrov <illiapetrov10@gmail.com>
2024-04-01 18:00:46 +0200

    Fixed task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -9,7 +9,6 @@ public class ColorSupplier {
         random = new Random();
     }
 
-
     public Color getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index];
