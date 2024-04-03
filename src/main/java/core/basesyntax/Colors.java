34a15e2488c12dc7c91b83ae66c891154a440ce8
Kateryna <keitkat98@gmail.com>
2024-04-03 12:21:49 +0300

    added lottery and random

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public enum Colors {
+    BLUE,
+    GREEN,
+    PINK,
+    PURPLE,
+    YELLOW,
+    ORANGE,
+    TURQUOISE,
+    MINT
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    public Ball getRandomBall() {
+        Colors color = ColorSupplier.getRandomColor();
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        return new Ball(color, number);
+    }
+}
