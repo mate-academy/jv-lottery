9a4e51eeab45dc52e0bc71255697f86bed4e8fce
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-04-04 20:24:42 +0200

    added classes for lottery calculations

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        Color[] colors = Color.values();
+        int randomIndex = new Random().nextInt(colors.length);
+        return colors[randomIndex].toString();
     }
 }
