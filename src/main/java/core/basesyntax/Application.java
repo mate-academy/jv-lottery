aba251ef3097b60cf10f6b5564e272d3fa94eeeb
Anastasiia Solenova <anastasiiasolenova@gmail.com>
2024-04-06 22:02:12 +0300

    solution updated

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
 public class Application {
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ballOne = lottery.getRandomBall();
-        Ball ballTwo = lottery.getRandomBall();
-        Ball ballThree = lottery.getRandomBall();
+        int numberOfBalls = 3;
+        Ball ball = null;
+        for (int i = 0; i < numberOfBalls; i++) {
+            ball = lottery.getRandomBall();
+        }
 
-        System.out.println(ballOne);
-        System.out.println(ballTwo);
-        System.out.println(ballThree);
+        System.out.println(ball);
+
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    ColorSupplier colorSupplier = new ColorSupplier();
+    Random random = new Random();
+    private static final int randomNumber = 100;
     public Ball getRandomBall() {
         Ball ball = new Ball();
 
-        ColorSupplier colorSupplier = new ColorSupplier();
-        ball.setColor(Color.valueOf(ColorSupplier.getRandomColor().name()));
+        ball.setColor(Color.valueOf(colorSupplier.getRandomColor().name()));
 
-        Random random = new Random();
-        int num = random.nextInt(100);
+        int num = random.nextInt(randomNumber);
         ball.setNumber(num);
 
         return ball;
