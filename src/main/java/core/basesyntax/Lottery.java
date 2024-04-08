1086d2fe2723654387713db85617da75de63b16f
maste <masterthief816@gmail.com>
2024-04-08 17:08:40 +0300

    completed all tack

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier;
+    public Lottery(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor().toString();
+        int number = new Random().nextInt(100) + 1; // Generate a random number between 1 and 100
+        return new Ball(color, number);
+    }
+}
