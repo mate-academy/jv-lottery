45d5ace45b1337163c1e5f568d6976176fe78fd6
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-04-09 15:26:15 +0200

    please point out the error in the code and what exactly needs to be changed, I don’t know how to pass the test

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
 
-    private final static Random random = new Random();
+    private static final Random random = new Random();
+
     public static Color getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index];
