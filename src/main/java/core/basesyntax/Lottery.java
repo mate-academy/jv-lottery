dad733074ff5631f8519fb500cf04a400ef58879
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-04-09 09:38:37 -0400

    changed variables naming

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int MAX_NUMBER_OF_BALLS = 100;
-    private final ColorSupplier supplier = new ColorSupplier();
+    public static final int MAX_BALL_NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         return new Ball(
-                supplier.getRandomColor(),
-                random.nextInt(MAX_NUMBER_OF_BALLS + 1)
+                colorSupplier.getRandomColor(),
+                random.nextInt(MAX_BALL_NUMBER + 1)
         );
     }
 }
