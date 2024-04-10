80f0a0da7800b6c78b85bfcfd9382169097d41e8
Artur <a_blyskal@wp.pl>
2024-04-10 11:03:13 +0200

    jv-lottery - fixed code

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public Colors getRandomColor() {
+        Random random = new Random();
+        Colors[] colors = Colors.values();
+        int randomIndex = random.nextInt(colors.length);
+
+        return colors[randomIndex];
     }
 }
