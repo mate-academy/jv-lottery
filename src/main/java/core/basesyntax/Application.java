c4c4ce4c4408d090e2c535540d3144a9eaac7d47
Artur <a_blyskal@wp.pl>
2024-04-10 18:33:48 +0200

    jv-lottery - made getRandomBall non-static, created max number, moved printThreeRandomBalls to Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,15 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery.printThreeRandomBalls();
+        printThreeRandomBalls();
+    }
+
+    public static void printThreeRandomBalls() {
+        System.out.println("Three Random Balls:");
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,19 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static final Random random = new Random();
+    private static final int MAX_NUMBER = 100;
 
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Colors randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(100) + 1;
+        int randomNumber = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(randomColor, randomNumber);
-    }
-
-    public static void printThreeRandomBalls() {
-        System.out.println("Three Random Balls:");
-        for (int i = 0; i < 3; i++) {
-            Ball ball = getRandomBall();
-            System.out.println(ball);
-        }
     }
 }
