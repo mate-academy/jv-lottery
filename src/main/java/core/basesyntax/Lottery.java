851985c89bc02c98e84cf3b0fcc9f4866d093d5a
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-04-10 15:33:52 +0200

    Lottery 3rd attempt

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class Lottery {
     public static Ball getRandomBall() {
-        Colors randomColor = ColorSupplier.getRandomColor();
-        Random someRandomBall = new Random();
-        int randomBall = someRandomBall.nextInt(101);
+        final Colors randomColor = ColorSupplier.getRandomColor();
+        final Random randomNumber = new Random();
+        int randomBall = randomNumber.nextInt(101);
         return new Ball(randomColor, randomBall);
     }
 }
