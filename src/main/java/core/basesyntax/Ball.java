e57ac2e885a4a527555a44a92fc576e2d75e4aa7
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-10 20:21:07 +0300

    add class Ball, fields and toString method

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Ball {
+    String color;
+    int number;
+
+    public String toString() {
+        return "Ball Color " + color + " and number " + number;
+    }
+}
