aabe400fdf2f8a3aa788cf4a516b1c2f4e6b0ec8
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-04-10 14:58:16 +0200

    Lottery 1st attempt

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,12 +4,12 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
 
-                Ball ball1 = Lottery.getRandomBall();
-                Ball ball2 = Lottery.getRandomBall();
-                Ball ball3 = Lottery.getRandomBall();
+        Ball ball1 = Lottery.getRandomBall();
+        Ball ball2 = Lottery.getRandomBall();
+        Ball ball3 = Lottery.getRandomBall();
 
-                System.out.println("First ball" + ball1);
-                System.out.println("Second ball" + ball2);
-                System.out.println("Third ball" + ball3);
-            }
-        }
+        System.out.println("First ball" + ball1);
+        System.out.println("Second ball" + ball2);
+        System.out.println("Third ball" + ball3);
+    }
+}
