622280607c618cd5ffc2f5e369a71f1521fca243
Andrii Datko <a.p.datko@gmail.com>
2024-04-10 13:31:04 +0300

    The ball is now generated using the constructor.

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(101);
-        Ball newBall = new Ball();
-        newBall.setColor(color);
-        newBall.setNumber(number);
-        return newBall;
+        int number = random.nextInt(0, MAX_NUMBER + 1);
+        return new Ball(color, number);
     }
 }
