e2f1c54c0f2434f6df0fe989264af678ce4b3a09
Artur <a_blyskal@wp.pl>
2024-04-11 12:56:16 +0200

    jv-lottery - made colorSupplier constant, made lottery final, deleted printThreeRandomBalls() method

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        printThreeRandomBalls();
-    }
-
-    public static void printThreeRandomBalls() {
+        final Lottery lottery = new Lottery();
         System.out.println("Three Random Balls:");
-        Lottery lottery = new Lottery();
         for (int i = 0; i < 3; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Lottery {
     private static final Random random = new Random();
     private static final int MAX_NUMBER = 100;
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         Colors randomColor = colorSupplier.getRandomColor();
         int randomNumber = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(randomColor, randomNumber);
