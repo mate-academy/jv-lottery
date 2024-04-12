1a2a78f2d9dc6cec6e0dd46dece72521e8b1ec6d
flamehost <flamehost@syneforge.com>
2024-04-12 22:06:03 +0300

    fix one space

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        randomBall.setColor( new ColorSupplier().getRandomColor());
+        randomBall.setColor(new ColorSupplier().getRandomColor());
         randomBall.setNumber(new Random().nextInt(100));
         System.out.println(randomBall);
         return randomBall;
