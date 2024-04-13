9dc72a238345ea0769c7ca34982dedee4b379ac9
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-13 20:10:33 +0300

    fix class ColorSupplier,Ball and Lottery.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private final Random random = new Random();
 
     final String getRandomColor() {
         int index = random.nextInt(Color.values().length);
