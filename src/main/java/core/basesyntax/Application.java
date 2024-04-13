bc7cc2a28abeca7dff343dcd7e4c894ced3485ae
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-13 11:32:47 +0300

    add ballsToCreate in Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        int ballsToCreate = 3;
         Lottery lottery = new Lottery();
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= ballsToCreate; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
