52bce2a68aeae0572650c8192db0a2b227c85cee
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-04-13 11:34:44 +0300

    add ballsToCreate in Application class and fixed the mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Application {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         int ballsToCreate = 3;
         Lottery lottery = new Lottery();
         for (int i = 1; i <= ballsToCreate; i++) {
