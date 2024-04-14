347bfe0bfce2c299a6fd5afe33ca2b9cd3f414fa
kozma <kozmakruckov@gmail.com>
2024-04-14 19:12:56 +0300

    fixed Application class and Lottery class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBERS_OF_BALL = 3;
+
     public static void main(String[] args) {
-        Lottery ball1 = new Lottery();
-        Lottery ball2 = new Lottery();
-        Lottery ball3 = new Lottery();
-        System.out.println(ball1.getRandomBall());
-        System.out.println(ball2.getRandomBall());
-        System.out.println(ball3.getRandomBall());
+        Lottery[] balls = new Lottery[NUMBERS_OF_BALL];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = new Lottery();
+            System.out.println(balls[i].getRandomBall());
+        }
+
     }
 }
