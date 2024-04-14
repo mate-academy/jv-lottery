1b4ac42a47da1e1f32441d3d635e97f956455294
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-14 19:36:52 +0300

    make number of balls constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,8 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        int numberOfBalls = 3;
+        for (int i = 0; i < numberOfBalls; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_NUMBER = 101;
     private Random random;
     private ColorSupplier colorSupplier;
 
@@ -12,6 +13,6 @@ public class Lottery {
     }
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(101));
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(this.BALL_NUMBER));
     }
 }
