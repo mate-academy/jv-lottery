559281357d148e8c8a6cff91d48b68733c84b549
Pavlo Mykhailyk <163852106+Daedrus1@users.noreply.github.com>
2024-04-14 14:33:46 +0200

    Update Lottery.java
    Fixed class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Lottery {
     private final Random random = new Random();
-
+    ColorSupplier colorSupplier = new ColorSupplier();
+    
     public Ball getRandomBall() {
         int maxNumber = 100;
         int minNumber = 1;
-        ColorSupplier colorSupplier = new ColorSupplier();
         Color randomColor = colorSupplier.getRandomColor();
         int randomNumber = random.nextInt(maxNumber - minNumber + 1) + minNumber;
         return new Ball(randomColor.name(), randomNumber);
