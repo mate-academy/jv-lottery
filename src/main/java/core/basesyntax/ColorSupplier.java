41aeae0b0797630bc1a3039a302ebcff94680948
kozma <kozmakruckov@gmail.com>
2024-04-15 11:52:12 +0300

    fixed ColorSuplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static Random random = new Random();
+
     public String getRandomColor() {
-        int index = new Random().nextInt(DifferentColors.values().length);
+        int index = random.nextInt(DifferentColors.values().length);
         return DifferentColors.values()[index].name();
     }
 }
