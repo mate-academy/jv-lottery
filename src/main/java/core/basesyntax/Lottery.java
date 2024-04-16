7e3673335954d43d60790756dd8de68c9d8203d6
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-04-16 11:58:36 +0200

    Lottery retry

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,7 @@ public final class Lottery {
     public static Ball getRandomBall() {
         final Colors RandomColor = ColorSupplier.getRandomColor();
         final Random RandomNumber = new Random();
-        final  int RandomBall = RandomNumber.nextInt(101);
+        final int RandomBall = RandomNumber.nextInt(101);
         return new Ball(RandomColor, RandomBall);
     }
 }
