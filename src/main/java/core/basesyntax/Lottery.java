15f0b3ee4af7ea9aaca85c39c4fba8f958e88e1f
Nataliya Plastun <natalya21753@gmail.com>
2024-04-16 20:09:05 +0300

    commit changes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,20 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final Random random = new Random();
+    private static ColorSupplier colorSupplier;
+
+    private ColorSupplier getColorSupplier() {
+        if (colorSupplier == null) {
+            colorSupplier = new ColorSupplier();
+        }
+        return colorSupplier;
+    }
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = String.valueOf(colorSupplier.getRandomColor());
-        int randomNumber = new Random().nextInt(100) + 1;
+        int randomNumber = random.nextInt(100);
         return new Ball(randomColor, randomNumber);
     }
 }
