dc8acd0629f92efbdbfd8a2f7970d9160ad65c94
Nataliya Plastun <natalya21753@gmail.com>
2024-04-16 14:02:03 +0300

    changed method getRandomBall non-static in Lottery class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println(Lottery.getRandomBall());
+            Lottery lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = String.valueOf(colorSupplier.getRandomColor());
         int randomNumber = new Random().nextInt(100) + 1;
