0652f3c9a6c54c61412df2d2f85669249df732f7
Anton <antchaycka@gmail.com>
2024-04-16 19:38:43 +0200

    changed "toString" in "Ball", removed not required parts from "Application" and made output through "toString", made "random" class-level in "ColorSupplier" and "Lottery", made "color" in "Lottery" class-level, added "bound" in "Lottery"

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball;
         for (int i = 0; i < 3; i++) {
-            ball = lottery.getRandomBall();
-            System.out.println(ball.getColor() + " " + ball.getNumber());
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball.toString());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private int bound = 100;
+    private ColorSupplier color = new ColorSupplier();
+    private Random random = new Random();
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier color = new ColorSupplier();
         ball.setColor(color.getRandomColor());
-        ball.setNumber(new Random().nextInt(101));
+        ball.setNumber(random.nextInt(bound + 1));
         return ball;
     }
 }
