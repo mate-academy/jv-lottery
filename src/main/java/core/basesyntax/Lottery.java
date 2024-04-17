e46e17e7e692c30ce952f9c1852903c05bbb4e4f
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-04-17 18:51:47 +0200

    Fixed Constants

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static final int valueNumber = 100;
+    public static final int BALL_MAX_NUMBER = 100;
+
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(valueNumber) + 1;
+        int randomNumber = random.nextInt(BALL_MAX_NUMBER) + 1;
         return new Ball(randomColor.name(), randomNumber);
     }
 }
