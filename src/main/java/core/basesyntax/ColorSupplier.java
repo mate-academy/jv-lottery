117df62f951c7af598a076e262f88e2a83300370
Krzysztof Herdzin <kdherdzin@gmail.com>
2024-04-17 12:45:09 +0200

    Lottery 5th attempt

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class ColorSupplier {
     private static final Random RANDOM = new Random();
     private static final Colors[] COLORS = Colors.values();
-    
+
     public static Colors getRandomColor() {
-        int RandomIndex = RANDOM.nextInt(COLORS.length);
-        return COLORS[RandomIndex];
+        int randomIndex = RANDOM.nextInt(COLORS.length);
+        return COLORS[randomIndex];
     }
 }
