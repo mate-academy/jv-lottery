49c55ca4f5e64b9bc7f0214a071e6ff1569233b9
Sofiia Boiko <boikosofiiawork@gmail.com>
2024-04-17 20:23:49 +0300

    rewrite magic number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(MAX_BALL_NUMBER);
 
         return new Ball(color, randomNumber);
     }
