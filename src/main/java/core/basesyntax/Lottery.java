382d4fc3890abbe5a4a863ada3dfb1a03cffdff1
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-04-17 17:13:25 +0200

    Fixed make it class level field2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int valueNumber = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int maxNumber = 100;
-        int minNumber = 1;
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(maxNumber - minNumber + 1) + minNumber;
+        int randomNumber = random.nextInt(valueNumber) + 1;
         return new Ball(randomColor.name(), randomNumber);
     }
 }
