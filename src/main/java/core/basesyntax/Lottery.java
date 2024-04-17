0b502b46ed13077956ce94901ad8ca69264736e7
Sofiia Boiko <boikosofiiawork@gmail.com>
2024-04-17 20:26:08 +0300

    checkstyle fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final ColorSupplier colorSupplier = new ColorSupplier();
     private static final int MAX_BALL_NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
