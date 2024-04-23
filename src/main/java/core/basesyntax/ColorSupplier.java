cee3bfe79a6ab1c1f63250ba324a19c91d68173c
Nikita Khilobok <nicktich284@gmail.com>
2024-04-23 17:18:10 +0300

    Task completed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static int index;
 
     static String getRandomColor() {
-        index = new Random().nextInt(Colors.values().length);
+        int index = new Random().nextInt(Colors.values().length);
         return Colors.values()[index].name();
     }
 }
