ecae2a84ef778e93d63df50d28cfe4e2eb415661
mgsorigo <marek.gucma@sorigo.pl>
2024-04-23 11:09:16 +0200

    Lottery4

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random RANDOM = new Random();
-    private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
     private static final int MAX_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
-    public Ball getRandomBall(){
-        String color = COLOR_SUPPLIER.getRandomColor();
-        int number = RANDOM.nextInt(MAX_NUMBER) + 1;
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_NUMBER) + 1;
         return new Ball(color, number);
     }
 }
+
+
