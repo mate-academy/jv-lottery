0f161c545c1e46a007b8c8d6d639bdaeea0cb379
Andrii Riezin <andrey.o.rezin@gmail.com>
2024-04-27 19:08:03 +0300

    fixed checkstyle issues

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int MAX_BALL_NUMBER = 100;
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int number = random.nextInt(MAX_BALL_NUMBER);
+        int maxBallNumber = 100;
+        int number = random.nextInt(maxBallNumber);
         return new Ball(color, number);
     }
 }
