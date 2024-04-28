6b860460165a0e316813449c7f7a37befd194bd0
Andrii Riezin <andrey.o.rezin@gmail.com>
2024-04-28 12:46:21 +0300

    added private access modifiers

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Application {
-    static final int LOOP_COUNT = 3;
+    private static final int LOOP_COUNT = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
