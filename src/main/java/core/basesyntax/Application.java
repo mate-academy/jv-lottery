e7172cd469bdca43fedfbf8e711eb99ff34b6d1c
Andrii Riezin <andrey.o.rezin@gmail.com>
2024-04-28 11:53:23 +0300

    names updated according to adjustments 2.0

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+    static final int LOOP_COUNT = 3;
+
     public static void main(String[] args) {
-        final int LOOP_COUNT = 3;
         Lottery lottery = new Lottery();
         for (int i = 0; i < LOOP_COUNT; i++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    static final int MAX_BALL_VALUE = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        final int MAX_BALL_VALUE = 100;
         int number = random.nextInt(MAX_BALL_VALUE);
         return new Ball(color, number);
     }
