0fecb7a6f6dfafa4b5529ce33649ba249727decd
User <masterthief816@gmail.com>
2024-04-28 14:33:20 +0300

    task done

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,10 +1,14 @@
 package core.basesyntax;
+
 import java.util.Random;
+
 public class Lottery {
-    private final  ColorSupplier colorSupplier;
+    private final ColorSupplier colorSupplier;
+
     public Lottery(ColorSupplier colorSupplier) {
         this.colorSupplier = colorSupplier;
     }
+
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor().toString();
         int number = new Random().nextInt(100) + 1; // Generate a random number between 1 and 100
