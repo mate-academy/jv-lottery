d3fc131b56863520a893170f73336cc1b72a0f47
d1kenpiken <skvyra.anton@gmail.com>
2024-04-29 16:24:29 +0200

    created solution

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+        public static Ball getRandomBall() {
+
+                Color randomColor = ColorSupplier.getRandomColor();
+                Random random = new Random();
+                int randomNumber = random.nextInt(100) + 1;
+                return new Ball(randomColor, randomNumber);
+        }
+}
