2328e787d6bb7af94045e56a6e76abb189e7edab
d1kenpiken <skvyra.anton@gmail.com>
2024-04-29 16:38:01 +0200

    created solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Application {
-public static void main(String[] args) {
+        public static void main(String[] args) {
 
         Ball firstBall = Lottery.getRandomBall();
         Ball secondBall = Lottery.getRandomBall();
