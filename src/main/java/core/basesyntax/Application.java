4c99a01dbf0d313682a3552e1717f004978aa219
d1kenpiken <skvyra.anton@gmail.com>
2024-04-29 16:32:07 +0200

    created solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Ball firstBall = Lottery.getRandomBall();
-        Ball secondBall = Lottery.getRandomBall();
-        Ball thirdBall = Lottery.getRandomBall();
+                Ball firstBall = Lottery.getRandomBall();
+                Ball secondBall = Lottery.getRandomBall();
+                Ball thirdBall = Lottery.getRandomBall();
 
-        System.out.println(firstBall);
-        System.out.println(secondBall);
-        System.out.println(thirdBall);
-    }
+                System.out.println(firstBall);
+                System.out.println(secondBall);
+                System.out.println(thirdBall);
+        }
 }
