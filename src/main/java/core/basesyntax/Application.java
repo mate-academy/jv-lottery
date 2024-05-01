458936a26d286960f6e62222177461d84b230584
Karol Gajda <karolgaj97@interia.pl>
2024-05-01 17:34:39 +0200

    Making new Balls in loop at Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball firstBall = Lottery.getRandomBall();
-        Ball secondBall = Lottery.getRandomBall();
-        Ball thirdBall = Lottery.getRandomBall();
-        System.out.println(firstBall);
-        System.out.println(secondBall);
-        System.out.println(thirdBall);
-        // create three balls using class Lottery and print information about them in console
+        Ball[] ball = new Ball[3];
+        for (int i = 0; i < ball.length; i++) {
+            ball[i] = new Ball();
+        }
+            for (Ball balls : ball) {
+                Lottery.getRandomBall(balls);
+                System.out.println(balls);
+            // create three balls using class Lottery and print information about them in console
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        Ball ball = new Ball();
+    public static void getRandomBall(Ball ball){
         ColorSupplier colorSupplier = new ColorSupplier();
         ball.setColor(String.valueOf(colorSupplier.getRandomColor()));
         Random number = new Random();
         ball.setNumber(number.nextInt(101));
-        return ball;
     }
 }
