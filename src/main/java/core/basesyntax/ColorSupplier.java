6fc6e343860cbad32657af4cd3ba90ebcbfec682
Margaryta <splinter4234@gmail.com>
2024-05-02 20:59:42 +0300

    added task solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public static String getRandomColor() {
+        enum Colors {
+            BLUE,
+            WHITE,
+            BLACK,
+            YELLOW,
+            RED
+        }
+
+        int index = new Random().nextInt(Colors.values().length);
+        Colors color = Colors.values()[index];
+        return color.toString();
     }
 }
