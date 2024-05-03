610b5baa5dceb2c7ef3facf9cb1fccd67ee56f0b
dkisly <dkisly081@gmail.com>
2024-05-03 17:36:58 +0300

    hwdone2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 import java.util.Random;
 class ColorSupplier {
     public static String getRandomColor() {
-        Color[] colors = Color.values();
-        Random random = new Random();
-        return colors[random.nextInt(colors.length)].toString();
-    }
+Color[] colors = Color.values();
+Random random = new Random();
+return colors[random.nextInt(colors.length)].toString();
+  }
 }
