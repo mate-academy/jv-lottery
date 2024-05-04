a942d54d74f72adf52049664c76eb1c50b4a118e
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-05-04 10:05:21 +0200

    created and implemented Color enum with ColorSupplier class for lettery ball

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private final Random random;
+
+    public ColorSupplier() {
+        this.random = new Random();
+    }
+
     public String getRandomColor() {
-        return null;
+        Color[] colors = Color.values();
+        int colorIndex = random.nextInt(colors.length);
+
+        return colors[colorIndex].name();
     }
 }
