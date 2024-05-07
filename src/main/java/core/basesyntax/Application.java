d275e603c4cf7c909582571267c8bb7fb05e4d64
Greshch <greshchook@gmail.com>
2024-05-07 11:22:57 +0300

    done-7 fix 3 as a constant

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        final byte BALLS = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < BALLS; i++) {
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
