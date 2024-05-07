f1d427f58697fd13011a0793029297b85337631a
d1kenpiken <skvyra.anton@gmail.com>
2024-05-07 12:48:20 +0200

    fixed problems

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
 
         int numberOfBalls = 3;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < numberOfBalls; i++) {
             Ball ball = Lottery.getRandomBall();
             System.out.println("Ball " + (i + 1) + ": " + ball);
         }
