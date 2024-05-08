50529423183e727ea4a1f55fbd6cfa11b7da64db
jakibczukw <jakibczukvlad@gmail.com>
2024-05-08 21:25:24 +0200

    fixed my code. Moved object creations, getRandomBall() now returns object Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,19 @@ public class Ball {
     private String color;
     private int number;
 
-    @Override
-    public String toString() {
-        return "Color = " + this.color + "; Number = " + this.number;
-    }
-
     public void setColor(String color) {
         this.color = color;
     }
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                + "color='" + color + '\''
+                + ", number=" + number
+                + '}';
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public String getRandomBall() {
-        Ball ball = new Ball();
-        ColorSupplier supplier = new ColorSupplier();
+
+    private Ball ball = new Ball();
+    private ColorSupplier supplier = new ColorSupplier();
+    private Random random = new Random();
+
+    public Ball getRandomBall() {
         ball.setColor(supplier.getRandomColor());
-        Random random = new Random();
         ball.setNumber(random.nextInt(100));
-        return ball.toString();
+        return ball;
     }
 }
