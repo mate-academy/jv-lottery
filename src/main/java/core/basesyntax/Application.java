beb15ee2a2549c65891ac0dc32b39bae8213156a
Karol Gajda <karolgaj97@interia.pl>
2024-05-09 18:17:09 +0200

    corrections after the first solution (cleaning code)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        Ball[] ball = new Ball[1000];
+        Ball[] ball = new Ball[3];
         for (int i = 0; i < ball.length; i++) {
             ball[i] = new Ball();
         }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
+    private final Random random = new Random();
+
     public Color getRandomColor() {
         Color[] color = Color.values();
         int randomIndex = random.nextInt(color.length);
