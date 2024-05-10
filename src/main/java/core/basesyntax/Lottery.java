625376b5558240d64d267884045e20cba0e6a136
jakibczukw <jakibczukvlad@gmail.com>
2024-05-10 09:20:29 +0200

    magic numbers fixed, ball object creation fixed

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-
-    private Ball ball = new Ball();
+    private static final int MAX_COUNT = 100;
     private ColorSupplier supplier = new ColorSupplier();
     private Random random = new Random();
 
     public Ball getRandomBall() {
+        Ball ball = new Ball();
         ball.setColor(supplier.getRandomColor());
-        ball.setNumber(random.nextInt(100));
+        ball.setNumber(random.nextInt(MAX_COUNT));
         return ball;
     }
 }
