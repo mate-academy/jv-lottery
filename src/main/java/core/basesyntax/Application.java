65375ff56b972e07ee651dc08249f4f2b7b89d43
Anton <panyutin15@gmail.com>
2024-05-14 17:35:32 +0300

    added solution of task according to README.md

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Ball firstBall = Lottery.getRandomBall();
+        Ball secondBall = Lottery.getRandomBall();
+        Ball thirdBall = Lottery.getRandomBall();
+
+        System.out.println("First ball: " + firstBall
+                           + "\nSecond ball: " + secondBall
+                           + "\nThird ball: " + thirdBall);
     }
 }
