b29489506d32bd36caef2bdee2b90959f6b898f9
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-05-15 12:41:39 +0200

    remove epmty

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,17 +1,16 @@
 package core.basesyntax;
 
 public class Ball {
-    private String color;
-    private int number;
+    private final Color color;
+    private final int number;
 
-    public Ball(String color, int number) {
+    public Ball(Color color, int number) {
         this.color = color;
         this.number = number;
-
     }
 
     @Override
     public String toString() {
-        return "Ball: color = " + color + ", Number = " + number;
+        return " color = " + color + ", number = " + number;
     }
 }
