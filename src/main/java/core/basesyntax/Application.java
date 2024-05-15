41a305bce8c11b9dfe1e53786c167e4d683de4ef
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-05-15 12:40:50 +0200

    create a new loop for

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,15 +1,12 @@
 package core.basesyntax;
 
-import static core.basesyntax.Lottery.getRandomBall;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball1 = getRandomBall();
-        Ball ball2 = getRandomBall();
-        Ball ball3 = getRandomBall();
+        Lottery lottery = new Lottery();
 
-        System.out.println(ball1);
-        System.out.println(ball2);
-        System.out.println(ball3);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ball " + (i + 1) + ": " + lottery.getRandomBall());
+        }
     }
 }
