920255fee2da5cf32dc3ebc527cad133acf65f61
Nazar <nazarmo13@gmail.com>
2024-05-20 15:12:38 +0300

    fixed the mistakes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final int index = new Random().nextInt(Colors.values().length);
+
     public String getRandomColor() {
         return Colors.values()[index].name();
     }
