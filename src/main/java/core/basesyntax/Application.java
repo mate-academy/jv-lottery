d14c8413d786d960d7067e30625ed9ac488a2377
Oleh <olehliashyk@gmail.com>
2024-05-21 12:20:03 +0300

    jv-lottery-solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,12 +4,10 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-
-        System.out.println(ball1);
-        System.out.println(ball2);
-        System.out.println(ball3);
+        // Створимо 3 об'єкти Ball за допомогою циклу for
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random random = new Random();
+    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final int maximumNumber = 100;
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(101);
+        int number = random.nextInt(maximumNumber + 1);//Включно з верхнею межею
 
         return new Ball(color, number);
     }
