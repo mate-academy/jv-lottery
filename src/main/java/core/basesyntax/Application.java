c60c89aa5cbfd4ec55bb29d28778739298aefa62
Dmytro Duka <dimadukalis@gmail.com>
2024-05-27 18:54:07 +0300

    corrected version

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,8 @@ public class Application {
     private static final int NUM_ITERATION = 3;
 
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         for (int i = 0; i < NUM_ITERATION; i++) {
-            Lottery lottery = new Lottery();
             Ball ball = lottery.getRandobBall();
             System.out.println(ball);
         }
