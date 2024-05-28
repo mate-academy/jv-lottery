eed4428049297440ab450d18d27a2d8c8eb6444f
Anna Stepanenko <spashiba.anna@gmail.com>
2024-05-28 16:58:03 +0300

    Corrected ColorSupplier.java (18th line) and combined 7 and 8 lines in Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,8 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < 100500; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println("Ball " + (i + 1) + ": " + ball);
+            System.out.println("Ball " + (i + 1) + ": " + lottery.getRandomBall());
         }
     }
 }
