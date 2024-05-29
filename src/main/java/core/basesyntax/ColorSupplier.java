b04cb6e2801595348a06db5055221ec5c5f65dea
Anna Stepanenko <spashiba.anna@gmail.com>
2024-05-29 13:00:54 +0300

    Were corrected 7th line in Application. Random number limit was set to 3 (Application, line 6). Was rename line 6 in Lottery and corrected line 8. Enum placed in the end of the outer class (ColorSupplier).

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,16 +5,16 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
 
+    public String getRandomColor() {
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index].name();
+    }
+
     enum Color {
         RED,
         GREEN,
         BLUE,
         YELLOW,
         ORANGE
-    }
-
-    public String getRandomColor() {
-        int index = random.nextInt(Color.values().length);
-        return Color.values()[index].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,17 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAGIC_NUMBER = 100;
+    private static final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
-    private final ColorSupplier colorSupplier;
-
-    public Lottery() {
-        this.colorSupplier = new ColorSupplier();
-    }
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(MAGIC_NUMBER) + 1;
+        int number = random.nextInt(MAX_BALL_NUMBER) + 1;
         return new Ball(color, number);
     }
 }
