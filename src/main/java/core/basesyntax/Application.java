49848088d966ac6b9a7f9597745eb5f05e5973c9
Anton Pekariev <litsss30071989@gmail.com>
2024-05-30 12:53:53 +0300

    JV-LOTTERY task done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Lottery lottery = new Lottery(colorSupplier);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(lottery.getRandomBall());
+        }
         // create three balls using class Lottery and print information about them in console
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier;
+
+    public Lottery(ColorSupplier colorSupplier) {
+        this.colorSupplier = colorSupplier;
+    }
+
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        Random random = new Random();
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(100));
+        return ball;
+    }
+}
