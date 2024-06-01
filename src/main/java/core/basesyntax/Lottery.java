61f607e35ac339ac99c22686ba20514e9a5254e4
Anton Pekariev <litsss30071989@gmail.com>
2024-06-01 18:58:31 +0300

    "ball" instance creation moved to "getRandomBall" method

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Lottery {
     private static final int RANGE_BOUND = 100;
     private ColorSupplier colorSupplier;
-    private Ball ball = new Ball();
     private Random random = new Random();
 
     public Lottery(ColorSupplier colorSupplier) {
@@ -13,6 +12,7 @@ public class Lottery {
     }
 
     public Ball getRandomBall() {
+        Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(random.nextInt(RANGE_BOUND));
         return ball;
