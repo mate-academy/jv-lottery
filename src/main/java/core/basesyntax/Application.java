34ba6c761043f70bdb0ec476ec697f146f22394c
Oleksii <obabych1@stu.vistula.edu.pl>
2024-06-04 19:40:35 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,11 +2,10 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_USERS = 3;
+    private static final Ball[] balls = new Ball[NUMBER_OF_USERS];
+    private static final Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
-        Ball[] balls = new Ball[NUMBER_OF_USERS];
-        Lottery lottery = new Lottery();
-
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
             System.out.println("The ball " + (i + 1) + " " + balls[i]);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random random = new Random();
+    private static final Random random = new Random();
 
     public Ball getRandomBall() {
         int numberRandom = random.nextInt(101);
