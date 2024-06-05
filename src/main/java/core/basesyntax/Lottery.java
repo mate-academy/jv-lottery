19b00834f95ae097ba2a57bcf6b2e5f943bd9a39
Dmytro <topfort17@gmail.com>
2024-06-05 11:23:06 +0200

    make layers5

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static final Random random = new Random();
 
-    public static final Ball BALL1 = new Ball(ColorSupplier.getRandomColors(), random.nextInt(100) + 1);
-    public static final Ball BALL2 = new Ball(ColorSupplier.getRandomColors(), random.nextInt(100) + 1);
-    public static final Ball BALL3 = new Ball(ColorSupplier.getRandomColors(), random.nextInt(100) + 1);
+    public static final Ball BALL1 = new Ball(ColorSupplier.getRandomColors(),
+            +random.nextInt(100) + 1);
+    public static final Ball BALL2 = new Ball(ColorSupplier.getRandomColors(),
+            +random.nextInt(100) + 1);
+    public static final Ball BALL3 = new Ball(ColorSupplier.getRandomColors(),
+            +random.nextInt(100) + 1);
 
     public static Ball getRandomBall() {
         Color.Colors randomColor = ColorSupplier.getRandomColors();
