3d65f12470d52a68d5f6121fc672162f870c52c5
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-06-07 11:27:46 +0300

    have to live longer to learn)

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         for (int i = 0; i < NUMBER_OF_BALLS; i++) {
