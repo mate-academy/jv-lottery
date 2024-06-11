c0377dc5694ae69753f8ee7abcf6552d001325e1
Dmytro <topfort17@gmail.com>
2024-06-11 10:10:19 +0200

    make layers13

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,17 +1,15 @@
 package core.basesyntax;
 
+import java.util.List;
+
 public class Application {
     private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
+        List<Ball> balls = lottery.generateBalls(NUMBER_OF_BALLS);
 
-        System.out.println(lottery.getBall1());
-        System.out.println(lottery.getBall2());
-        System.out.println(lottery.getBall3());
-
-        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
-            Ball ball = lottery.getRandomBall();
+        for (Ball ball : balls) {
             System.out.println(ball);
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,36 +1,25 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
-    private final Ball ball1;
-    private final Ball ball2;
-    private final Ball ball3;
-
-    public Lottery() {
-        this.ball1 = getRandomBall();
-        this.ball2 = getRandomBall();
-        this.ball3 = getRandomBall();
-    }
-
-    public Ball getBall1() {
-        return ball1;
-    }
-
-    public Ball getBall2() {
-        return ball2;
-    }
-
-    public Ball getBall3() {
-        return ball3;
-    }
-
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
-        int randomNumber = random.nextInt(101);
+        int randomNumber = random.nextInt(MAX_NUMBER + 1);
         return new Ball(randomColor, randomNumber);
+    }
+
+    public List<Ball> generateBalls(int numberOfBalls) {
+        List<Ball> balls = new ArrayList<>();
+        for (int i = 0; i < numberOfBalls; i++) {
+            balls.add(getRandomBall());
+        }
+        return balls;
     }
 }
