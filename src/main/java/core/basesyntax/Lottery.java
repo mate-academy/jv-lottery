71ab4473bd43329b3567b2909891ec53b143bb5f
Nikita Boyko <nikitabojko57@gmail.com>
2024-06-11 18:12:25 +0300

    change Apllication.java and Lottery.java files after review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class Lottery {
     private static final int NUMBER = 101;
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
         randomBall.setColor(colorSupplier.getRandomColor());
-        randomBall.setNumber(new Random().nextInt(NUMBER));
+        randomBall.setNumber(random.nextInt(NUMBER));
         return randomBall;
     }
 }
