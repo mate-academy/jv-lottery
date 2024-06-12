a23e6487fa789ac4c97c4760bd97006e6fe2c0ee
d1kenpiken <skvyra.anton@gmail.com>
2024-06-12 19:45:53 +0200

    fixed problems

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -19,9 +19,7 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-            "color='" + color + '\'' +
-            ", number=" + number +
-            '}';
+        return "Ball{" + "color='" + color + '\''
+            + ", number=" + number + '}';
     }
 }
