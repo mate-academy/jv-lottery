2563195e8f860fc883717d1741ea1b89a2eb2ec9
Artem Stadnik <artem.stadnik.contact@gmail.com>
2024-06-15 16:00:32 +0300

    edited2 hw

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static final Random random = new Random();
+    private static final int maxNumber = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(100);
+        int number = random.nextInt(maxNumber);
         return new Ball(color, number);
     }
 }
