474f4d7b5d67419c5794b8cddbbea2fa0e1eefec
Artem Stadnik <artem.stadnik.contact@gmail.com>
2024-06-15 14:51:22 +0300

    edit1 hw

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Color[] colors = Color.values();
-    private static final Random random = new Random();
+    private final Color[] colors = Color.values();
+    private final Random random = new Random();
 
     public String getRandomColor() {
         int index = random.nextInt(colors.length);
