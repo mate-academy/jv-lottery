e1301e608c6adf6cce1b5709818dfa9eb767ce9f
Artem Stadnik <artem.stadnik.contact@gmail.com>
2024-06-15 14:39:17 +0300

    solved hw

---
+++ src/main/java/core/basesyntax/Color.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Color {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    BLACK,
+    WHITE,
+    PURPLE,
+    ORANGE,
+    GRAY,
+    PINK
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final Color[] colors = Color.values();
+    private static final Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        int index = random.nextInt(colors.length);
+        return colors[index].name();
     }
 }
