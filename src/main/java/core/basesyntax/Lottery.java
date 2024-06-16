fd2ec734db9c1a4f2b98289d62a1ccd9ab9a60ad
Rafal <pyszkowski.r@gmail.com>
2024-06-16 19:28:17 +0200

    final

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,6 @@ public class Lottery {
     ColorSupplier ball = new ColorSupplier();
     public String getRandomBall(){
         Random random = new Random();
-
         return (String.valueOf(random.nextInt(101))+ "   " + ball.getRandomColor());
     }
 }
