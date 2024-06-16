8a259015153a12fd2ae50a4d3321ce37227c1c66
aarrtemm <artmkurmanchuk@gmail.com>
2024-06-16 20:31:50 +0300

    Fixed checklist problems

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 import java.util.Random;
 
-
 public class Lottery {
-    private static final int MAX_NUMBER = 100;
     public static final Random RANDOM = new Random();
+    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
