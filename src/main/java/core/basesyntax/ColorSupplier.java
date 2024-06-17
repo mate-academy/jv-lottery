b94317d05f5c6c3c91e712459eef60177489a0b4
Rafal <pyszkowski.r@gmail.com>
2024-06-17 19:32:12 +0200

    last4

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier{
     int index = new Random().nextInt(Colors.values().length);
+
     public String getRandomColor() {
         Colors ballColor = Colors.values()[index];
         String color = ballColor.name();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Lottery {
     ColorSupplier ball = new ColorSupplier();
+
     public String getRandomBall(){
         Random random = new Random();
-        return (String.valueOf(random.nextInt(101))+ "   " + ball.getRandomColor());
+        return (String.valueOf(random.nextInt(101)) + "   " + ball.getRandomColor());
     }
 }
