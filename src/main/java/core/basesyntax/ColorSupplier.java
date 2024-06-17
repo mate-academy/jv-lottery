59e377a41e0a148305319dcb4aa550cd2783327b
Nikita Kravtsov <deez999deez@gmail.com>
2024-06-17 23:10:48 +0300

    Correct getRandomColor method to generate number properly

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int index = new Random().nextInt(Ball.Color.values().length);
 
     public Ball.Color getRandomColor() {
+        int index = new Random().nextInt(Ball.Color.values().length);
         return Ball.Color.values()[index];
     }
 }
