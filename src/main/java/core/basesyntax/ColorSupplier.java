f95749ff8c07a1c36e8cf7d7dfb1cd7c48f4be58
Rafal <pyszkowski.r@gmail.com>
2024-06-17 19:02:05 +0200

    final3

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.util.Random;
+
 public class ColorSupplier{
     int index = new Random().nextInt(Colors.values().length);
     public String getRandomColor() {
