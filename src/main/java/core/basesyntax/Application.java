137313f77fbbcf26052a8540b744224453bba6a7
Rafal <pyszkowski.r@gmail.com>
2024-06-17 19:29:11 +0200

    last3

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,9 @@
 package core.basesyntax;
-
 public class Application {
 
     public static void main(String[] args) {
         Lottery ball = new Lottery();
-        for (int i = 0; i <3; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(ball.getRandomBall());
         }
     }
