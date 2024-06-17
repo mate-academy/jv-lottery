8f059e35018ac7a17712b6395235cffff8ea939d
B4rt1omiej <bartlomiej.karol.gacek@gmail.com>
2024-06-17 09:13:13 +0200

    "First solution of the task"

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,8 @@ public class Ball {
     private String colour;
     private int number;
 
-    public String getColour() {
-        return colour;
-    }
-
     public void setColour(String colour) {
         this.colour = colour;
-    }
-
-    public int getNumber() {
-        return number;
     }
 
     public void setNumber(int number) {
