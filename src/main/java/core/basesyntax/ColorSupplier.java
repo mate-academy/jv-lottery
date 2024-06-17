5c978b044b0030d4cbb4d64c6aad1d6f9cebacb2
Rafal <pyszkowski.r@gmail.com>
2024-06-17 20:12:07 +0200

    veryLast

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,17 +5,17 @@ import java.util.Random;
 public class ColorSupplier {
     private int index = new Random().nextInt(Colors.values().length);
 
+    public String getRandomColor() {
+        Colors ballColor = Colors.values()[index];
+        return ballColor.name();
+
+    }
     public void setIndex(int index) {
         this.index = index;
     }
 
     public int getIndex() {
         return index;
-    }
-
-    public String getRandomColor() {
-        Colors ballColor = Colors.values()[index];
-        return ballColor.name();
     }
 
 }
