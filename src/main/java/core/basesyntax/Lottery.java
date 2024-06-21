3c06fc46057b28a3abc9d1dfc52416f37ec7b390
skrypkasv <skrypkasv@gmail.com>
2024-06-21 09:19:06 +0300

    fix static field declaration order in Lottery

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int NUMBER_BOUNDARY = 101;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-    private static final int NUMBER_BOUNDARY = 101;
 
     public String getRandomBall() {
         Ball ball = new Ball();
