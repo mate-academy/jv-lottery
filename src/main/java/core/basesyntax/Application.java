a52efe77ec5bfac648d3b75c3fb0477724b1419a
skrypkasv <skrypkasv@gmail.com>
2024-06-21 13:01:17 +0300

    fix magic number in Application after mentor's review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        final int BALLS_LIMIT = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < BALLS_LIMIT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
