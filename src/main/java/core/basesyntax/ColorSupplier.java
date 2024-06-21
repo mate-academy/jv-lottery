ed6a3891f3e311c393e1c7007ac9363dd1539d8f
skrypkasv <skrypkasv@gmail.com>
2024-06-21 07:42:01 +0300

    remove temp variable in ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,9 +5,7 @@ import core.basesyntax.enums.Color;
 import java.util.Random;
 
 public class ColorSupplier {
-    int index = new Random().nextInt(Color.values().length);
-
     public String getRandomColor() {
-        return String.valueOf(Color.values()[index]);
+        return String.valueOf(Color.values()[new Random().nextInt(Color.values().length)]);
     }
 }
