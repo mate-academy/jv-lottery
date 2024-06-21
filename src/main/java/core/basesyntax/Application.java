53419e28e20568cded91d850d47416c9c86384a3
skrypkasv <skrypkasv@gmail.com>
2024-06-21 13:04:51 +0300

    fix final var name in Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        final int BALLS_LIMIT = 3;
+        final int ballsLimit = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < BALLS_LIMIT; i++) {
+        for (int i = 0; i < ballsLimit; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
