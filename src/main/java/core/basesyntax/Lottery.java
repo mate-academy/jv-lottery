9351d47ee474f34aaae49262ad53ca9d1088e2ce
skrypkasv <skrypkasv@gmail.com>
2024-06-21 09:05:27 +0300

    fix Lottery implementation

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private static final int NUMBER_BOUNDARY = 101;
+
     public String getRandomBall() {
         Ball ball = new Ball();
-        ball.setColor(new ColorSupplier().getRandomColor());
-        ball.setNumber(new Random().nextInt(101));
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(random.nextInt(NUMBER_BOUNDARY));
         return ball.toString();
     }
 }
