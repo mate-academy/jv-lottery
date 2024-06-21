939f675af5dfc2cdb9663b76a52d24eef0cebb7a
skrypkasv <skrypkasv@gmail.com>
2024-06-21 06:55:40 +0300

    create getRandomColor in ColorSupplier

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import core.basesyntax.enums.Color;
+
+import java.util.Random;
+
 public class ColorSupplier {
+    int index = new Random().nextInt(Color.values().length);
+
     public String getRandomColor() {
-        return null;
+        return String.valueOf(Color.values()[index]);
     }
 }
