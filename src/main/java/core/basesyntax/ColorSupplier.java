8637b3f94f42a292b827f6d99e2be5d607475ca8
skrypkasv <skrypkasv@gmail.com>
2024-06-22 09:30:07 +0300

    fix getRandomColor after review

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,8 @@ import core.basesyntax.enums.Color;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final int index = new Random().nextInt(Color.values().length);
-
     public String getRandomColor() {
+        int index = new Random().nextInt(Color.values().length);
         return Color.values()[index].name();
     }
 }
