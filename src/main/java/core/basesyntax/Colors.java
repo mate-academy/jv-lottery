7dbd4ccd623975f916578a8319c7daa2a20348c2
Sasha Fetisov <sasafetisov03@gmail.com>
2024-06-23 16:15:39 +0300

    fix code

--- src/main/java/core/basesyntax/Colors.java
+++ src/main/java/core/basesyntax/Colors.java
@@ -6,5 +6,7 @@ public enum Colors {
     BLUE,
     YELLOW,
     BLACK,
-    WHITE
+    WHITE;
 }
+
+
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
+    private int maxValue = 100;
 
     public Ball getRandomBall() {
         Colors color = colorSupplier.getRandomColor();
-        int number = new Random().nextInt(100);
+        int number = random.nextInt(maxValue);
 
         return new Ball(color,number);
     }
