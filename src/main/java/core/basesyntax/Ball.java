9dbb0df0b8f12fe3801bbc25fda1d2342d8c678f
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-06-24 20:29:47 +0300

    create an enum with different colors

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,6 +4,17 @@ public class Ball {
     String color;
     int number;
 
+    public enum Color {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        ORANGE,
+        PURPLE,
+        BLACK,
+        WHITE;
+    }
+
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
