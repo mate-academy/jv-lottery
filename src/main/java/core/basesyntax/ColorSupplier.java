205d73be85f89fecc727a9adbb9b33ff7d635165
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-06-24 20:34:53 +0300

    implement method getRandomColor() in class ColorSupplier. This method should return a random color, based on the enum values (use the Random class for this)

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        int index = new Random().nextInt(Ball.Color.values().length);
+        return String.valueOf(Ball.Color.values()[index]);
     }
 }
