6169d359ce20ece7e0e335cbdd35ff8dd2074be5
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-06-25 15:14:09 +0300

    Wrote MAX as a constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final int max = 100;
+    private static final int MAX = 100;
 
     public Ball getRandomBall() {
         ColorSupplier color = new ColorSupplier();
         Random random = new Random();
-        return new Ball(color.getRandomColor(), random.nextInt(max));
+        return new Ball(color.getRandomColor(), random.nextInt(MAX));
     }
 }
