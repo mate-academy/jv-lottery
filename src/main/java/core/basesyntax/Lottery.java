2b157dbc34de32898d15f1a0abbed78154d46888
Sasha Fetisov <sasafetisov03@gmail.com>
2024-06-25 13:20:39 +0300

    removed static modifier and add final to the colorSupplier

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_VALUE = 100;
-    private static final Random random = new Random();
-    private static ColorSupplier colorSupplier = new ColorSupplier();
+    private final int MAX_VALUE = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
