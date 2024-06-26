372e5a13f401d4e9718a5a217bf82408d918161c
OD <sdenisenko055@gmail.com>
2024-06-26 10:55:56 +0300

    Fixed const name

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private final int BORDER = 101;
+    private final int border = 101;
 
     Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(BORDER);
+        int number = random.nextInt(border);
         return new Ball(color, number);
     }
 }
