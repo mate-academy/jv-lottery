055625dd9ab47aa3a70446b5b4bad63c01fe7e03
OD <sdenisenko055@gmail.com>
2024-06-27 18:47:15 +0300

    fixed state

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BORDER = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int BORDER = 101;
 
     Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
