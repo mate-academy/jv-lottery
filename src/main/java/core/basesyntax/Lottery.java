4fbf06c08fd0b6d29908597675bdfe307985f041
OD <sdenisenko055@gmail.com>
2024-06-27 16:29:39 +0300

    Create constant

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private final int border = 101;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int BORDER = 101;
 
     Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(border);
+        int number = random.nextInt(BORDER);
         return new Ball(color, number);
     }
 }
