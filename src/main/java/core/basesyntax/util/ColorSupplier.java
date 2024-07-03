8a077bea743f56e865a33af886ceb1a66a166d42
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-07-03 11:03:32 +0200

    Lottery +improved ColorSupplier method getRandomColor();

--- src/main/java/core/basesyntax/util/ColorSupplier.java
+++ src/main/java/core/basesyntax/util/ColorSupplier.java
@@ -4,11 +4,11 @@ import core.basesyntax.model.Color;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final int COLOR_QUANTITY = Color.values().length;
+    private static final Color[] colors = Color.values();
     private final Random random = new Random();
 
     public String getRandomColor() {
-        int index = random.nextInt(COLOR_QUANTITY);
-        return Color.values()[index].toString();
+        int index = random.nextInt(colors.length);
+        return colors[index].name();
     }
 }
