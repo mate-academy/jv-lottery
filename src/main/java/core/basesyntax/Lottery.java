12bcb0e5c6b2f8bad26bf9b0359e3d760afc5742
Petro Shchur <pepe2.98.12@icloud.com>
2024-07-06 14:31:06 -0400

    Final example2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor().name();
-        int number = random.nextInt(100);
+        int number = random.nextInt(MAX_BALL_NUMBER + 1);
         return new Ball(color, number);
     }
 }
