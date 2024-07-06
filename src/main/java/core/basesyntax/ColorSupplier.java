3a80cc6968f58cf3cdb9f30738494299ff9f8640
homeworkfirst <zookotelif@gmail.com>
2024-07-06 22:04:03 +0300

    Lottery and ColorSupplier changed

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class ColorSupplier {
     private final Random random = new Random();
 
-    public Color getRandomColor() {
-        int index = random.nextInt(Color.values().length);
-        return Color.values()[index];
+    public String getRandomColor() {
+        Color[] colors = Color.values();
+        int index = random.nextInt(colors.length);
+        return colors[index].toString();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Random randomGenerator = new Random();
+    private static final int Max_Ball_Number = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random randomGenerator = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor().toString(),
-                randomGenerator.nextInt(100) + 1);
+        String color = colorSupplier.getRandomColor();
+        int number = randomGenerator.nextInt(Max_Ball_Number) + 1;
+        return new Ball(color, number);
     }
 }
