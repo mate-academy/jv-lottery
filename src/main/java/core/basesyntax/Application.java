6132366d554f6d4569a14e05c2a7c450dc15338c
Stupak Serhii <pop.kom.dabl@gmail.com>
2024-07-07 19:35:47 +0300

    Corrected AI remark

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println(Lottery.getRandomBall());
+            System.out.println(new Lottery().getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        int randomNumber = new Random().nextInt(100);
-        Ball ball = new Ball(ColorSupplier.getRandomColor(), randomNumber);
-        return ball;
+    public Ball getRandomBall() {
+        return new Ball(new ColorSupplier().getRandomColor(), new Random().nextInt(100));
     }
 }
