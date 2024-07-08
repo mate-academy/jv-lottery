97d5a156f4f62350daba03ba3b441c5d682ded93
homeworkfirst <zookotelif@gmail.com>
2024-07-09 00:38:53 +0300

    System.out.println(ball); changed to   System.out.println(lottery.getRandomBall()); added constant LOTTERY_BALL_AMOUNT

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int Max_Ball_Number = 100;
+    private static final int MAX_BALL_NUMBER = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random randomGenerator = new Random();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = randomGenerator.nextInt(Max_Ball_Number) + 1;
+        int number = randomGenerator.nextInt(MAX_BALL_NUMBER) + 1;
         return new Ball(color, number);
     }
 }
