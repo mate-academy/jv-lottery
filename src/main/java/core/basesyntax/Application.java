23e405f71354a6547ec6eca7c7de4630e4784029
homeworkfirst <zookotelif@gmail.com>
2024-07-09 00:50:02 +0300

    System.out.println(ball); changed to   System.out.println(lottery.getRandomBall()); added constant LOTTERY_BALL_AMOUNT

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int LOTTERY_BALL_AMOUNT = 3;
+        final int LOTTERY_BALL_COUNT = 3;
 
-        for (int i = 0; i < LOTTERY_BALL_AMOUNT; i++) {
+        for (int i = 0; i < LOTTERY_BALL_COUNT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
