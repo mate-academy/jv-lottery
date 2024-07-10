68f75b1dd8ae9e7a1922f98db2604148cbfc92d3
homeworkfirst <zookotelif@gmail.com>
2024-07-10 23:35:25 +0300

    added constant NUMBER_OF_BALLS

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,6 @@ public class Application {
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        /* if I make a constant like (final int LOTTERY_BALL_COUNT = 3;)
-        maven does not pass such code because it considers it incorrect */
 
         for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
