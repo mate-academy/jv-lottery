4c83aee0d1b47a05aa6d47c9d2ad17b301f2c89f
homeworkfirst <zookotelif@gmail.com>
2024-07-10 23:34:29 +0300

    final int LOTTERY_BALL_COUNT = 3; changed to final int lotteryBallCount = 3

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        final int lotteryBallCount = 3;
         /* if I make a constant like (final int LOTTERY_BALL_COUNT = 3;)
         maven does not pass such code because it considers it incorrect */
 
-        for (int i = 0; i < lotteryBallCount; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
