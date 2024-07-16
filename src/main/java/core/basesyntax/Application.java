9c1642268db86c98b403445e616f38f17e5947b5
David Vozniak <david.vozniak.ua@gmail.com>
2024-07-16 10:18:44 -0600

    fixed incorrect object usage

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,13 @@
 package core.basesyntax;
 
 public class Application extends Lottery {
+    private static final int NUMBER_OF_BALLS = 3;
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
         // create three balls using class Lottery and print information about them in console
-        for (int i = 0; i < 3; i++) {
-            System.out.println(new Lottery().getRandomBall());
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
+
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 public class Lottery extends ColorSupplier {
     public static final int MAX_BOUND = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
-    public int getRandomNumber() {
-        return new Random().nextInt(MAX_BOUND);
+    private int getRandomNumber() {
+        return random.nextInt(MAX_BOUND);
     }
 
     public Ball getRandomBall() {
-        return new Ball(getRandomColor(), getRandomNumber());
+        return new Ball(colorSupplier.getRandomColor(), getRandomNumber());
     }
 }
