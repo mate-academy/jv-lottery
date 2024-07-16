5c9dedd9a7cf95cc6402e17fa7a5921167fa81f6
David Vozniak <david.vozniak.ua@gmail.com>
2024-07-16 12:39:16 -0600

    added changes requested by mentor

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -11,4 +11,3 @@ public class Application extends Lottery {
         }
     }
 }
-
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-     int randomNumber = new Random().nextInt(Color.values().length);
 
     public String getRandomColor() {
-        return Color.values()[randomNumber].name();
+        return Color.values()[new Random().nextInt(Color.values().length)].name();
     }
 }
