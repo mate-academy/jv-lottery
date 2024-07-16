dd8a54b180f56b480a49a36dd545841ea2688d73
David Vozniak <david.vozniak.ua@gmail.com>
2024-07-16 10:24:28 -0600

    fixed build failure

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application extends Lottery {
     private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         // create three balls using class Lottery and print information about them in console
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static int randomNumber = new Random().nextInt(Color.values().length);
+    private final static int randomNumber = new Random().nextInt(Color.values().length);
+
     public String getRandomColor() {
         return Color.values()[randomNumber].name();
     }
