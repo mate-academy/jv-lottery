d92382a0946da12a3d63f5d29959d5096df1122b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-17 11:20:19 +0300

    add magic number, and create ball using constructor

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BOUND = 100;
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
 
     public Ball getRandomBall() {
-        Ball ball = new Ball();
-        int randomNumber = random.nextInt(100);
-        ball.setNumber(randomNumber);
-        ball.setColor(colorSupplier.getRandomColor());
-        return ball;
+        return new Ball(colorSupplier.getRandomColor(), random.nextInt(BOUND));
     }
 }
