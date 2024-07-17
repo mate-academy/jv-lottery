f507eb9be11d6c9e1f7f04d3125eef17350f195c
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-07-17 10:59:50 +0200

    some new changes made for no reason

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,6 +7,8 @@ public class Lottery {
         ColorSupplier supplier = new ColorSupplier();
         String color = supplier.getRandomColor();
         int num = new Random().nextInt(100);
+        // Ignore the log below (have to commit)
+        System.out.println("Some new changes made");
 
         return new Ball(color, num);
     }
