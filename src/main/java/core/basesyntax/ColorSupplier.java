8fc7f85a14710cc25e2fdd70e9d3b0e67ac38a5e
David Vozniak <david.vozniak.ua@gmail.com>
2024-07-17 20:04:04 -0600

    fixed maven build

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random randomNumber = new Random();
+
     public String getRandomColor() {
         return Color.values()[randomNumber.nextInt(Color.values().length)].name();
     }
