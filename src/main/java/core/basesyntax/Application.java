42eadbb5c5f10e6aabc11e52b58e210300d53c85
Yuliia Kudrynska <ykudrynska@gmail.com>
2024-07-21 22:25:24 +0300

    fixed comment for Application class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,7 @@ public class Application {
 
     public static void main(String[] args) {
         for (int i = 0; i < NUMBER_OF_BALLS; i++) {
-            Ball ball = Lottery.getRandomBall();
-            System.out.println(ball);
+            System.out.println(Lottery.getRandomBall());
         }
     }
 }
