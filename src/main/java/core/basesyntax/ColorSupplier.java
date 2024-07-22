8ce5b41c8efd6468e0bb92659f89135faa0fdbe2
Yuliia Kudrynska <ykudrynska@gmail.com>
2024-07-22 22:37:51 +0300

    fixed comments

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import java.awt.*;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final Random r = new Random();
+    private final Random random = new Random();
 
     public Color getRandomColor() {
-        return Color.values()[r.nextInt(Color.values().length)];
+        return Color.values()[random.nextInt(Color.values().length)];
     }
 }
