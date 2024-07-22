21209244f8372e0f9bb40b17deeae6000ae00b50
Yuliia Kudrynska <ykudrynska@gmail.com>
2024-07-22 22:36:21 +0300

    fixed comments

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
+import java.awt.*;
 import java.util.Random;
 
 public class ColorSupplier {
-    private static final Random r = new Random();
+    private final Random r = new Random();
 
-    public static Color getRandomColor() {
+    public Color getRandomColor() {
         return Color.values()[r.nextInt(Color.values().length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Lottery {
     static final int MAX_NUMBER = 100;
     private static final Random r = new Random();
+    private static final ColorSupplier color = new ColorSupplier();
 
     public static Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(), r.nextInt(MAX_NUMBER));
+        return new Ball(color.getRandomColor(), r.nextInt(MAX_NUMBER));
     }
 }
