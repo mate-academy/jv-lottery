faeb9e3078a2f482567cdabd004568605cc17af5
Yuliia Kudrynska <ykudrynska@gmail.com>
2024-07-23 23:29:45 +0300

    fixed comments

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 public class Application {
-    static final int NUMBER_OF_BALLS = 3;
+    private static final int NUMBER_OF_BALLS = 3;
+    private static Lottery lottery = new Lottery();
 
     public static void main(String[] args) {
-        Lottery lottery = new Lottery();
         for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
