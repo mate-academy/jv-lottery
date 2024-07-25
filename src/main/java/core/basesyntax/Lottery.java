37f7b19f8f8c76600064c67ba8d5c5467a97aaf9
Nikolai <colya.reznick@gmail.com>
2024-07-26 00:18:18 +0300

    Added review fixes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_NUMBER_EXCLUSIVE = 101;
     private static final Random random = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball(ColorSupplier.getRandomColor(),
+        return new Ball(colorSupplier.getRandomColor(),
                 random.nextInt(MAX_BALL_NUMBER_EXCLUSIVE));
     }
 }
