298472b455f577bee28bfedf911d54fdc110d531
d1kenpiken <skvyra.anton@gmail.com>
2024-07-25 20:08:12 +0200

    created solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        int numberOfBalls = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < numberOfBalls; i++) {
-            Ball ball = lottery.getRandomBall();
-            System.out.println("Ball " + (i + 1) + ": " + ball);
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
