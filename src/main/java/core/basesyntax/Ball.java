bc79ca5f1dd7dcd741ecf870af53ff1a739cf133
bilsinas <Nevnerzhitskaiadaryna@gmail.com>
2024-08-01 20:44:12 +0300

    created a method of random color and number generation. For the test, I used the creation of 3 balls

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public class Ball {
+    String color;
+    int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + number;
+    }
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Random random = new Random();
+        ColorSupplier supplier = new ColorSupplier();
+        int number = random.nextInt(100);
+        String color = String.valueOf(supplier.getRandomColor());
+        return new Ball (color, number);
+    }
+}
