648587332c3c6aba31e0f54b2c660977e7c572c0
Dima Davydov <Kilo6559@gmail.com>
2024-08-02 14:27:58 +0300

    Corrected

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,7 +6,7 @@ public class ColorSupplier {
     private static final Random RANDOM = new Random();
 
     public Colors getRandomColor() {
-        int index = new Random().nextInt(Colors.values().length);
+        int index = RANDOM.nextInt(Colors.values().length);
         return Colors.values()[index];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -8,7 +8,7 @@ public class Lottery {
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor().name();
-        int number = new Random().nextInt(101);
+        int number = RANDOM.nextInt(101);
         return new Ball(color, number);
     }
 }
