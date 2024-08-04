c6a01a027896ee290e97504fa82e1186866ed900
Lilia <liliazuravel6@gmail.com>
2024-08-04 18:05:58 +0300

    made 3 a constant number on class level

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int AMOUNT_BALLS = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        int amountBalls = 3;
-        for (int i = 0; i < amountBalls; i++) {
+
+        for (int i = 0; i < AMOUNT_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
