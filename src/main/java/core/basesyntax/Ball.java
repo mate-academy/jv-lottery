c3f58500e3293315ce6b7b3ece9ab2a1d8792523
Dima Davydov <Kilo6559@gmail.com>
2024-08-05 16:47:28 +0300

    Build Success

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,16 +4,19 @@ public class Ball {
     private String color;
     private int number;
 
-    public Ball (String color, int number){
+    public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball {"
+                + "color='"
+                + color
+                + '\''
+                + ", number="
+                + number
+                + '}';
     }
 }
