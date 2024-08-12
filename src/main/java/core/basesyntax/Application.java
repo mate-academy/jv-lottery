93ea502746de6865ec34cc0c2ef89dc8457b3ab2
bilsinas <Nevnerzhitskaiadaryna@gmail.com>
2024-08-12 13:39:46 +0300

    fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int COUNT = 3;
+
     public static void main(String[] args) {
-        final int COUNT = 3;
         Lottery lottery = new Lottery();
         for (int i = 0; i < COUNT; i++) {
             System.out.println(lottery.getRandomBall());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private Random random = new Random();
+
     private ColorSupplier supplier = new ColorSupplier();
     public Ball getRandomBall() {
         int number = random.nextInt(MAX_NUMBER);
