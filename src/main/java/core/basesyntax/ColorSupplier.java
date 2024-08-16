d4c9a8c2b7e234ec59718aa89c9a6e3d8cccba37
karma.misha <karma.misha@gmail.com>
2024-08-16 19:48:47 +0300

    create and implement class Ball and Lottery; create enum Color; implements class ColorSupplier; implements main() in Application class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    private static final int COLOR_COUNT = Color.values().length;
+    private Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        int colorNumber = random.nextInt(COLOR_COUNT);
+
+        return Color.values()[colorNumber].name();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private Random random = new Random();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(100);
+        return new Ball(color, number);
+    }
+}
