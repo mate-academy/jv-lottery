578fa6a1f8d9879af067e0da65435a211f908230
oksana-feshchenko <foksanv@gmail.com>
2024-08-18 13:23:55 +0300

    apply fixes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+
     public String getRandomColor() {
         return Color.values()[new Random().nextInt(0, Color.values().length)].name();
     }
