ac8a09100527ae3754ab74dc89597358a8bb3031
oksana-feshchenko <foksanv@gmail.com>
2024-08-18 13:25:44 +0300

    fix to string

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -12,9 +12,12 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Ball{" +
-                "color=" + color +
-                ", number=" + number +
+        return "Ball{"
+                +
+                "color=" + color
+                +
+                ", number=" + number
+                +
                 '}';
     }
 }
