7089560b5148dba81771bb59ad862b066e3b938a
Alinka Lyn <alinka.lyn@ukr.net>
2024-08-19 18:24:33 +0300

    added changes after review

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_NUMBER =100;
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
