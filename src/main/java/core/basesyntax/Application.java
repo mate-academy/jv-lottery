7031cc460a7b06e4c55e05b8fcf5f942cc1720a5
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-08-20 17:32:05 +0200

    refactoring the Application.java file

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALLS_QUANTITY = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < BALLS_QUANTITY; i++) {
             System.out.println(lottery.getRandomBall());
         }
-
     }
 }
