f748175ec24224fcb0107eaa1c89e0a6c46fa12c
Volodymyr Nyshta <volodymyr.nyshta@gmail.com>
2024-08-21 22:56:04 +0300

    lotteryHW done

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int indexOfColor = new Random().nextInt(Color.values().length);
+        Color color = Color.values()[indexOfColor];
+        return color.toString();
     }
 }
