f4248594fa04f6a5914f98875e22c4301d3e4d7c
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-08-22 18:16:40 +0300

    Fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,7 +5,7 @@ public class Application {
         int ballsCapacity = 3;
 
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
+        Ball[] balls = new Ball[ballsCapacity];
 
         for (int i = 0; i < ballsCapacity; i += 1) {
             balls[i] = lottery.getRandomBall();
