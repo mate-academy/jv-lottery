723f67b67fa412a2e6c46ecadd9e35443b4448f6
StoneBlood <brkalix@gmail.com>
2024-08-22 13:08:43 +0300

    written to the stack

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 class Lottery {
-    private int maxValue = 101;
+    private int MAX_VALUE = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(maxValue);
+        int number = random.nextInt(MAX_VALUE);
         return new Ball(color, number);
     }
 }
