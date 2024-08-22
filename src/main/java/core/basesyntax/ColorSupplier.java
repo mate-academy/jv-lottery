7d746e2fd41be988de11ed47531eb7637dafd1a2
StoneBlood <brkalix@gmail.com>
2024-08-22 13:29:15 +0300

    Created constant MAX_VALUE

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
     private int index = new Random().nextInt(Color.values().length);
 
     public String getRandomColor() {
