668955c7864e5d5a811cdbf496c164e5245f0a1b
Serhiy <sergie3007@gmail.com>
2024-08-23 23:58:37 +0300

    Implement random Ball generation

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,14 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+
+        Ball firstRandomBall = lottery.getRandomBall();
+        Ball secondRandomBall = lottery.getRandomBall();
+        Ball thirdRandomBall = lottery.getRandomBall();
+
+        System.out.println(firstRandomBall);
+        System.out.println(secondRandomBall);
+        System.out.println(thirdRandomBall);
     }
 }
---
+++ src/main/java/core/basesyntax/BallColors.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public enum BallColors {
+    Yellow,
+    Green,
+    Red,
+    Purple,
+    White,
+    Black
+}
