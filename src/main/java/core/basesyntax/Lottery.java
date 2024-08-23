ca1f7359c9979681cdd17371bb044aef371995fb
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2024-08-23 12:58:28 +0300

    added a constant to the Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class Lottery {
     public String getRandomBall() {
         Ball randomBall = new Ball();
+        final int MaxValue = 100;
         ColorSupplier randomColor = new ColorSupplier();
 
-        randomBall.setNumber((int) (Math.random() * 100));
+        randomBall.setNumber((int) (Math.random() * MaxValue));
         randomBall.setColor(randomColor.getRandomColor());
 
         return randomBall.getColor() + " " + randomBall.getNumber();
