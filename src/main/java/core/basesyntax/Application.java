97ac9c29fa22df45f6343fc2e9ef98166a7fc4c4
StoneBlood <brkalix@gmail.com>
2024-08-23 21:50:16 +0300

    great constant SIZE_ARRAY

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Application {
     public static final int SIZE_ARRAY = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
