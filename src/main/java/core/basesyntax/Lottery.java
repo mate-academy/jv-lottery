c0470dc7f6635bc1834041f45d4f8c9592959a23
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2024-08-27 01:58:40 +0200

    lottety-hw-02

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 class Lottery {
+    private int maxValue = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int number = random.nextInt(101);
+        int number = random.nextInt(maxValue);
         String randomColor = String.valueOf(colorSupplier.getRandomColor());
         return new Ball(randomColor, number);
     }
