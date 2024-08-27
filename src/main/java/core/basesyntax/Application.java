0d2c13a5006d95fed12f6aea5ccb036bdeea7a0d
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-08-27 17:06:05 +0200

    I solved all problems

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
-        final int MaxValue = 3;
-        for (int i = 0; i < MaxValue; i++) {
-            Lottery ball = new Lottery();
-            System.out.println(ball.getRandomBall());
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            Lottery lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
         }
 
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_VALUE = 100;
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private Color color;
+    private String color;
 
     private int number = new Random().nextInt(MAX_VALUE);
 
