20596ac6ee9f031829145f085a9eb18e85b7d365
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-08-27 15:56:22 +0200

    I corected constant value

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        final int MaxValue = 3;
-        for (int i = 0; i < MaxValue; i++) {
+        final int MAXVALUE = 3;
+        for (int i = 0; i < MAXVALUE; i++) {
             Lottery ball = new Lottery();
             System.out.println(ball.getRandomBall());
         }
