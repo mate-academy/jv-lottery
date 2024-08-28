bc93c00a738a1cf87937b620b8aa51ccbe42266e
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-08-28 18:14:56 +0200

    Upgraded my code

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int index = new Random().nextInt(Color.values().length);
+    public static final int lengthOfEnum = Color.values().length;
+    private int index = new Random().nextInt(lengthOfEnum);
 
-    public Color getRandomColor() {
-        return Color.values()[index];
+    public String getRandomColor() {
+        return Color.values()[index].toString().toLowerCase(Locale.ROOT);
     }
 }
