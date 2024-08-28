7ac43529bb318adcb36470b149fc7af495d2479a
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-08-28 19:29:27 +0200

    Changed 'for' loop, created more constant values and fixed everything

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_VALUE = 100;
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private String color;
-
-    private int number = new Random().nextInt(MAX_VALUE);
 
     public Ball getRandomBall() {
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+        String color;
+        int number = random.nextInt(MAX_VALUE);
         color = colorSupplier.getRandomColor();
         Ball ball = new Ball(color, number);
         return ball;
