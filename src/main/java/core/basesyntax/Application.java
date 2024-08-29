30a8f79c51661f477965acf369a7d25b5388b7e1
Serhiy <sergie3007@gmail.com>
2024-08-29 16:55:40 +0300

    fix unclear names and make Random regular field

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int INDEX = 3;
+    private static final int ARRAY_SIZE_INDEX = 3;
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[INDEX];
+        Ball[] balls = new Ball[ARRAY_SIZE_INDEX];
 
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random RANDOM = new Random();
-    private static final int INDEX = 100;
+    private static final int MAX_RANDOM_VALUE = 100;
     private static final ColorSupplier COLOR_SUPPLIER = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
-        return new Ball(COLOR_SUPPLIER.getRandomColor(), RANDOM.nextInt(INDEX));
+        return new Ball(COLOR_SUPPLIER.getRandomColor(), random.nextInt(MAX_RANDOM_VALUE));
     }
 }
