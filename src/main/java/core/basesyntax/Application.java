2eb7862970a633447d38ee2e4f0fcd0d60783a0d
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-01 15:03:22 +0300

    done checkstyle

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,7 @@ public class Application extends Lottery {
     public static void main(String[] args) {
         Application app = new Application();
         for (int i = 1; i < 4; i++) {
-            System.out.println ("Ball" + i + ": " + app.getRandomBall());
+            System.out.println("Ball" + i + ": " + app.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class Lottery extends RandoM {
     public String getRandomBall() {
-        return "color of the ball: " + getRandomColor() + ", number of the ball: " + getRandomNumber() + ".";
+        return "color of the ball: " + getRandomColor() + ", number of the ball: "
+                + getRandomNumber() + ".";
     }
 }
