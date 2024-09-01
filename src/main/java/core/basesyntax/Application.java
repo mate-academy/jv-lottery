4a926f6d98f45cd658b6c7103e1063d147ce7b7b
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-01 16:01:33 +0300

    done getRandomBall as static

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
-import static core.basesyntax.Lottery.getRandomBall;
-
 public class Application {
     public static void main(String[] args) {
         int countOfBalls = 4;
         for (int i = 1; i < countOfBalls; i++) {
-            System.out.println("Ball" + i + ": " + getRandomBall());
+            System.out.println("Ball" + i + ": " + Lottery.getRandomBall());
         }
     }
 }
