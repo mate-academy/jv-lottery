b08f8a704e7f9064b554c4e03968476c5ac341ea
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-02 22:52:01 +0300

    done announcement of a new objects (they are private, final and class level field)

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     public static final int BOUND = 100;
+    private final Random random = new Random();
+    private final ColorSupplier firstColor = new ColorSupplier();
 
     public String getRandomBall() {
-        Random random = new Random();
-        ColorSupplier firstColor = new ColorSupplier();
-
         return "color of the ball: " + firstColor.getRandomColor()
                 + ", number of the ball: " + random.nextInt(BOUND) + ".";
     }
