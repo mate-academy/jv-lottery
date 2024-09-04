15f691235d65d3cf3c888a5aa67d7ceae6c95416
Illia Kulyk <kulykillia12@gmail.com>
2024-09-04 14:21:55 +0300

    New version lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     private Random random = new Random();
 
-
     public String getRandomColor() {
         int index = random.nextInt(Color.values().length);
         return Color.values()[index].name();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 101;
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
-    private static final int MAX_NUMBER = 101;
+
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
         int number = random.nextInt(MAX_NUMBER);
