8f50d846fb819939307e7e088bb391c5baddb2e2
Illia Kulyk <kulykillia12@gmail.com>
2024-09-05 15:35:14 +0300

    New lottery without mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,16 +1,14 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
+
         Lottery lottery = new Lottery();
-        int numberOfBalls = 40; //для прикладу тільки
-        if (numberOfBalls > 100 || numberOfBalls <= 0) {
-            System.out.println("Не правильно введено кількість кульок. Введіть число від 1 до 100");
-        } else {
-            for (int i = 0; i < numberOfBalls; i++) {
-                Ball ball = lottery.getRandomBall();
-                System.out.println(ball);
-            }
+
+        for (int i = 0; i < NUMBER_OF_BALLS ; i++ ) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
