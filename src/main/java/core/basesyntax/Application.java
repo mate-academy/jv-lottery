2a64992f3d1101802d2fb8443a807ba3d6c859b9
clo3eX <pansydaisy83@gmail.com>
2024-09-08 17:18:44 +0200

    fixed mistakes in naming and added random instance

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Application {
-    private static final int NumberOfBalls = 3;
+    private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < NumberOfBalls; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
