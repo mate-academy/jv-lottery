6ae13a1f8c065603f3a1986bc4fedd31a762662e
Maks <petliukofficial@gmail.com>
2024-09-08 16:19:39 +0300

    fixed code according to mentor requirements

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
+    private final int maxNumber = 101;
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int number = random.nextInt(101);
+        int number = random.nextInt(maxNumber);
 
         return new Ball(color, number);
     }
