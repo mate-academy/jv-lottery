d1241a33a4c5441210a7f4d14c2221d0d5356476
Oleh Bihun <o.bihun@ukr.net>
2024-09-09 08:23:49 +0300

    added MAX_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private final int MAX_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
-        int number = random.nextInt(100);
+        int number = random.nextInt(MAX_NUMBER);
         return new Ball(color, number);
     }
 }
