2213988be52fea16df71cf402686157a0d63ab80
Sergii Konovalov <sergii2konovalov@gmail.com>
2024-09-14 13:53:23 +0300

    hw done after mvn

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery extends ColorSupplier {
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         int randomNumber = new Random().nextInt(101);
         Ball randomBall = new Ball(getRandomColor(), randomNumber);
         return randomBall;
