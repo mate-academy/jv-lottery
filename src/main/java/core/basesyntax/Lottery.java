c4bd4e54cc5a67a8084fa975ca70c145d366d90f
Sergii Konovalov <sergii2konovalov@gmail.com>
2024-09-14 13:50:56 +0300

    hw done

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery extends ColorSupplier {
+    public Ball getRandomBall(){
+        int randomNumber = new Random().nextInt(101);
+        Ball randomBall = new Ball(getRandomColor(), randomNumber);
+        return randomBall;
+    }
+}
