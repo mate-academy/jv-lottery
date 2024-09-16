6cd9ae88a2974e592874f1f63647ddcbe8f0bb5e
Kateryna Kraska <120599784+katrienkraska@users.noreply.github.com>
2024-09-16 14:27:41 +0300

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,15 @@
-package core.basesyntax;
+import java.util.Random;
 
 public class ColorSupplier {
+    private Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        Color[] colors = Color.values();
+        int index = random.nextInt(colors.length);
+        return colors[index].name();
+    }
+    public enum Color {
+        RED, GREEN, BLUE, YELLOW, BLACK, WHITE
     }
 }
+
