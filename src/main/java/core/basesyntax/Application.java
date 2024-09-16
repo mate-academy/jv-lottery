823b520b72ccb5a3b9daec8c9167b519c604af09
Oleh Ridchenko <olehridchenko@gmail.com>
2024-09-16 23:38:04 +0300

    print info about 3 balls in console

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,5 +3,12 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball ball1 = lottery.getRandomBall();
+        System.out.println(ball1);
+        Ball ball2 = lottery.getRandomBall();
+        System.out.println(ball2);
+        Ball ball3 = lottery.getRandomBall();
+        System.out.println(ball3);
     }
 }
