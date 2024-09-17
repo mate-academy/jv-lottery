550e554e56af22f49e0fe7f2b1df357573d98900
Oleh Ridchenko <olehridchenko@gmail.com>
2024-09-17 21:38:09 +0300

    corrections made + fixed checkstyle problems

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
+
     public String getRandomColor() {
         Color[] colors = Color.values();
         int index = random.nextInt(colors.length);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    public static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
         String randomColor = colorSupplier.getRandomColor();
