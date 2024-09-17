6d5cfa848c689582e4d389560c577e6dd53bea4a
DariaBakal <bakal.dasha01@gmail.com>
2024-09-17 22:45:03 +0300

    implemented getRandomBall() method

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    public static final int MAGIC_NUMBER = 100;
+    private ColorSupplier colorSupplier = new ColorSupplier();
+    private Random random = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        Random random = new Random();
-        return new Ball(colorSupplier.getRandomColor(),random.nextInt(100));
+        return new Ball(colorSupplier.getRandomColor(),random.nextInt(MAGIC_NUMBER));
     }
 }
