3844cb6c4cf2191622e5d1ee77f0e5b034b0b1fa
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2024-09-18 15:59:25 +0300

    Fix4

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,17 +1,25 @@
 package core.basesyntax;
 
 public class Ball {
-    @Override
-    public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
-    }
-
     private String color;
     private int number;
 
+    @Override
+    public String toString() {
+        return "Ball{"
+                +
+                "color='"
+                +
+                color
+                +
+                '\''
+                +
+                ", number="
+                +
+                number
+                +
+                '}';
+    }
 }
 
 
