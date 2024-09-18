ab1eeace8dbc4cc9d63dfccb1fae1bb8305cede7
Alex Smechow <alexsmechow@gmail.com>
2024-09-18 18:35:04 +0300

    fulfilled all the requirements of the task1

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball1 = Lottery.getRandomBall();
-        Ball ball2 = Lottery.getRandomBall();
-        Ball ball3 = Lottery.getRandomBall();
+        Lottery lottery = new Lottery();
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
 
-        System.out.println(ball1);
-        System.out.println(ball2);
-        System.out.println(ball3);
+        System.out.println(ball1.toString());
+        System.out.println(ball2.toString());
+        System.out.println(ball3.toString());
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,17 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
-        Color color = ColorSupplier.getRandomColor();
-        int number = new Random().nextInt(100);
+    private final ColorSupplier colorSupplier;
+    private final Random random;
+
+    public Lottery() {
+        this.colorSupplier = new ColorSupplier();
+        this.random = new Random();
+    }
+
+    public Ball getRandomBall() {
+        Color color = colorSupplier.getRandomColor();
+        int number = random.nextInt(100);
         return new Ball(color, number);
     }
 }
