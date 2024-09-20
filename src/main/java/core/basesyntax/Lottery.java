e05cda68be6d3f21182884e7d2ec7030a7929b82
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-20 22:16:33 +0300

    hot fix 2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,7 +7,6 @@ public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private static final String color = new ColorSupplier().getRandomColor();
 
-
     public Ball getRandomBall() {
         int number = random.nextInt(MAX_BALL_NUMBER);
         return new Ball(color, number);
