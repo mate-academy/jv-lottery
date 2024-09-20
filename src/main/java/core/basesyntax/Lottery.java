bd5cb23be770e1830a05bda3a005180b4401d55c
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2024-09-20 21:12:00 +0300

    Try5
    MVN SAY:
    
    \Lottery.java:8:23: Member name 'Max_Value' must match pattern '^[a-z][a-z0-9][a-zA-Z0-9]*$'. [MemberName]

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final Random RANDOM = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int MAX_VALUE = 100;
+    private final int max = 100;
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int number = RANDOM.nextInt(MAX_VALUE);
+        int number = RANDOM.nextInt(max);
         return new Ball(color, number);
     }
 }
