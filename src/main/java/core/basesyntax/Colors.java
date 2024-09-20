a85adc3fc0bed707d08360acc1551833e6329784
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-20 15:38:07 +0300

    complited the assigment

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    BLUE,
+    YELLOW,
+    GREEN,
+    ORANGE,
+    PURPLE,
+    CYAN,
+    MAGENTA,
+    VIOLET,
+    BLACK,
+    WHITE,
+    BROWN,
+    GRAY,
+    PINK
+}
+
