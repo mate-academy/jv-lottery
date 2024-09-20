7e652c742035a6b51106030e999b9b61cd4cdfbe
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2024-09-20 21:40:15 +0300

    Try6
    MVN SAY:
    
    [ERROR] C:\Users\cha9n\IdeaProjects\jv-lottery2\src\main\java\core\basesyntax\Lottery.java:8:5: Static variable definition in wrong order. [DeclarationOrder]
    [ERROR] C:\Users\cha9n\IdeaProjects\jv-lottery2\src\main\java\core\basesyntax\Lottery.java:8:19: 'static' modifier out of order with the JLS suggestions. [ModifierOrder]
    
    Give me pls example

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final Random RANDOM = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int max = 100;
+    private final int maxvalue = 100;
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int number = RANDOM.nextInt(max);
+        int number = RANDOM.nextInt(maxvalue);
         return new Ball(color, number);
     }
 }
