4817ea777cd86bbb5b5d3d6b2c28d628a44473e8
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-20 21:44:43 +0300

    hot-fix

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,9 +4,9 @@ public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         Ball[] balls = new Ball[] {
-                lottery.getRandomBall(),
-                lottery.getRandomBall(),
-                lottery.getRandomBall()
+            lottery.getRandomBall(),
+            lottery.getRandomBall(),
+            lottery.getRandomBall()
         };
 
         for (Ball ball : balls) {
