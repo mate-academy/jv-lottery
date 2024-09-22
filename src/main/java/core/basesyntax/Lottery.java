d988c4221a221ce973f1fa5fb8a1f6227ba11435
Yevhen Koroliuk <zheckakoroluk@gmail.com>
2024-09-22 15:32:52 +0300

    1111

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Lottery {
-    public Ball getRandomBall() {
+    public String getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = colorSupplier.getRandomColor();
         return randomColor;
