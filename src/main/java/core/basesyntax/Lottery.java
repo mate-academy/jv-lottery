77c83a5bc31c4174d18d9b9c37ac66561bb6264c
Mykhailo Chernykh <270775mik@gmail.com>
2024-09-22 22:21:38 +0200

    made ColorSupplier and Random class-level variable

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALLS_VALUE = 100;
 
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
+
+    private static final Random random = new Random();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-
-        Random random = new Random();
-
         String color = String.valueOf(colorSupplier.getRandomColor().name());
         int number = random.nextInt(MAX_BALLS_VALUE);
         return new Ball(color, number);
