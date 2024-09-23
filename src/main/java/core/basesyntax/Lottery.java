db5ebbaaa9f06aa64827be6242a02046d1ce0a3d
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2024-09-23 13:58:36 +0300

    Try8

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_VALUE = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final int max = 100;
 
     public Ball getRandomBall() {
         Color color = colorSupplier.getRandomColor();
-        int number = random.nextInt(max);
+        int number = random.nextInt(MAX_VALUE);
         return new Ball(color, number);
     }
 }
