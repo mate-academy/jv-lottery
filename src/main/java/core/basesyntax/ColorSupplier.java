b0db2fd65c4e4a1ea869b213b7b6882b97fe8c4f
Mykhailo Chernykh <270775mik@gmail.com>
2024-09-23 12:21:39 +0200

    removed empty line

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final int NUMBER_OF_COLORS = Color.values().length;
-
     private final Random random = new Random();
 
     public String getRandomColor() {
