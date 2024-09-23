b842203a7b267c153397a8b8f3ad287ff8c0925f
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-23 11:21:36 +0300

    final changes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,13 +1,15 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int LIMIT_ARRAY = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[] {
-            lottery.getRandomBall(),
-            lottery.getRandomBall(),
-            lottery.getRandomBall()
-        };
+        Ball[] balls = new Ball[LIMIT_ARRAY];
+
+        for (int i = 0; i < LIMIT_ARRAY; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
 
         for (Ball ball : balls) {
             System.out.println(ball);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final Random random = new Random();
     private static final int MAX_BALL_NUMBER = 100;
-    private static final String color = new ColorSupplier().getRandomColor();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
+        String randomColor = colorSupplier.getRandomColor();
         int number = random.nextInt(MAX_BALL_NUMBER);
-        return new Ball(color, number);
+        return new Ball(randomColor, number);
     }
 }
