577c5cdbba2a4367452f79347acf5a6e1f27871f
Pavlo Sukhanko <swtlouisiana@gmail.com>
2024-09-24 23:16:43 +0100

    fix inheritance

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,15 +2,13 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends Ball {
+public class Lottery  {
 
     public Ball getRandomBall() {
 
         Ball ball = new Ball();
-
         ball.setColor(new ColorSupplier().getRandomColor());
         ball.setNumber(new Random().nextInt(100));
-
         return ball;
     }
 }
