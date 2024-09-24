14a375914e6077f2e50cdcca91b532f8c79aecf7
Kate Kraska <kate2002.kk42@gmail.com>
2024-09-24 12:12:43 +0300

    fix problem

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
 
