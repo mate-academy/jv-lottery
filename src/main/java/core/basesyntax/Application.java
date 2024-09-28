75b84368e03cc360a7b8fccce692099182e7b61b
jekaG <jekagrusha@gmail.com>
2024-09-28 16:05:34 +0300

    create application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery;
+        for (int i = 0; i < 3; i++) {
+            lottery = new Lottery();
+            System.out.println(lottery.getRandomBall());
+        }
         // create three balls using class Lottery and print information about them in console
     }
 }
