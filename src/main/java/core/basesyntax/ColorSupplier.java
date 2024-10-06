9c669ebe8a0c59a2e63afec1a07c3fe083f424e6
Anton <windsweptofficial@gmail.com>
2024-10-06 23:36:32 +0200

    fixed-5

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    private static final int COLOR_VALUES_LENGTH = Color.values().length;
     private final Random random = new Random();
 
     public String getRandomColor() {
-        Color color = Color.values()[random.nextInt(Color.values().length)];
+        Color color = Color.values()[random.nextInt(COLOR_VALUES_LENGTH)];
         return color.name();
     }
 }
