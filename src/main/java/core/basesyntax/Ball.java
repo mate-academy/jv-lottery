2c99e427f9e9830406c406709a39f35b85467aef
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-10-07 12:04:01 +0200

    feat: Implement Ball class

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Ball {
+    private String color;
+    private int number;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
