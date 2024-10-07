0646b5ee615a3c38cac4d12cafe9ae546d228b1d
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-10-07 12:19:29 +0200

    feat: Implement ColorSupplier class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    Random rand = new Random();
+
     public String getRandomColor() {
-        return null;
+
+        return Colors.values()[rand.nextInt(Colors.values().length)].toString();
     }
 }
