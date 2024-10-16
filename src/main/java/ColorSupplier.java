dc5899be615ebf74b9a65f17c4e9ad49926bad0e
Tomasz <kiniro83@gmail.com>
2024-10-16 11:32:17 +0200

    Added required files

---
+++ src/main/java/ColorSupplier.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class ColorSupplier {
+    private static final Random RANDOM = new Random();
+
+    public static Color getRandomColor() {
+        Color[] colors = Color.values();
+        return colors[RANDOM.nextInt(colors.length)];
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
+        }
+    }
+}
