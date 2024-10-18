e7a92b3c4d38e787c6b35d483777620b32f25b67
Sasha Koval <kovalsana40@gmail.com>
2024-10-18 15:11:05 +0300

    try number 1

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    ColorSupplier colorSupplier = new ColorSupplier();
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(100);
+        Ball ball = new Ball();
+        ball.setColor(color);
+        ball.setNumber(number);
+        return ball;
+    }
+}
