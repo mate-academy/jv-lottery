17a1a88e12f321316816361ba533d9b47aaa8cdc
Volikkk <illavolanuk02@gmail.com>
2024-10-22 22:12:14 +0300

    command1.2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-            for (int i = 0; i < 3; i++) {
-                Ball ball = Lottery.getRandomBall();
-                System.out.println(ball);
-            }// create three balls using class Lottery and print information about them in console
+        for (int i = 0; i < 3; i++) {
+            Ball ball = Lottery.getRandomBall();
+            System.out.println(ball);
+        } // create three balls using class Lottery and print information about them in console
     }
 }
