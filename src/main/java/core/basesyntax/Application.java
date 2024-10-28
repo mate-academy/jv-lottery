8e83e92c1347d5d8a15c20dd56fbb168e7f18655
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-10-28 11:27:24 +0100

    add Ball, Color, Lottery classes.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
+        System.out.println(new Lottery().getRandomBall());
         // create three balls using class Lottery and print information about them in console
     }
 }
