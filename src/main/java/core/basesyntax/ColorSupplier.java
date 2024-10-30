95cd1bf42d3a362592e878a99a58fcbc78e6a2b5
michal <mihacik228@gmail.com>
2024-10-30 15:20:31 +0100

    create lottery in java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+enum Color {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    ORANGE;
+}
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    public Color getRandomColor() {
+        int index = new Random().nextInt(Color.values().length);
+        return Color.values()[index];
     }
 }
