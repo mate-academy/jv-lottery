5348c265dea3aefc7327cbaf9c4682b22c69b2ac
michal <mihacik228@gmail.com>
2024-10-31 11:46:15 +0100

    create lottery in java

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -3,13 +3,12 @@ package core.basesyntax;
 public class Ball {
     protected final String color;
     protected final int number;
-
-    public Ball (String color, int number){
+    public Ball (String color, int number) {
         this.color = color;
         this.number = number;
     }
     @Override
     public String toString() {
-        return  "ball color: " + color + ", " +"ball number: " +number;
+        return "ball color: " + color + ", " +"ball number: " + number;
     }
 }
