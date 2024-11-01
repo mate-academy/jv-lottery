26cf03b8e533512fea410eca5f230bfe59d0bb3a
Maksym Meleshchenko <maximkame00@gmail.com>
2024-11-01 13:16:43 +0200

    Fixed structural errors

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
 public class Application {
-    final static int TOTAL_NUMBER_BALLS = 3;
+    public static final int TOTAL_BALLS = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < TOTAL_NUMBER_BALLS; i++) {
+
+        for (int i = 0; i < TOTAL_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    final int MAX_RANDOM_NUMBER = 100;
-    Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
-    Ball ball = new Ball();
+    private static final int MAX_BALL_NUMBER = 100;
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Ball ball = new Ball();
 
     public Ball getRandomBall() {
-        int value = random.nextInt(MAX_RANDOM_NUMBER);
+        int value = random.nextInt(MAX_BALL_NUMBER);
         ball.setColor(colorSupplier.getRandomColor());
         ball.setNumber(value);
         return ball;
