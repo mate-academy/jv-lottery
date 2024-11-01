d3704678fac275a923a53d75bbd3ac4585f69040
Maksym Meleshchenko <maximkame00@gmail.com>
2024-11-01 15:24:18 +0200

    Changed the color.toString() method to the color.name() method in the ColorSuppiler class. In the Application class, the access modifier in the TOTAL_BALLS field has been changed from public to private. Moved creation of the Ball object from the Lottery class level to the getRandomBall() method level

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Application {
-    public static final int TOTAL_BALLS = 3;
+    private static final int TOTAL_BALLS = 3;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,12 +6,11 @@ public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Ball ball = new Ball();
 
     public Ball getRandomBall() {
-        int value = random.nextInt(MAX_BALL_NUMBER);
+        Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(value);
+        ball.setNumber(random.nextInt(MAX_BALL_NUMBER));
         return ball;
     }
 }
