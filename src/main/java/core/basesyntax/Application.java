15db42ba89114714cb2e2ea3404035e5830a8e16
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-02 19:49:07 +0100

    cleanUp code4

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,6 +5,5 @@ public class Application {
         System.out.println(new Lottery().getRandomBall());
         System.out.println(new Lottery().getRandomBall());
         System.out.println(new Lottery().getRandomBall());
-        // create three balls using class Lottery and print information about them in console
     }
 }
