9d4e1258fdd34a179a75bd07feec2a52648f937e
michal <mihacik228@gmail.com>
2024-11-07 21:31:55 +0100

    commits after checks

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,8 +2,6 @@ package core.basesyntax;
 
 import java.util.Random;
 
-import java.util.Random;
-
 public class Lottery {
     private static final int MAX_NUMBER = 100;
     private final Random random = new Random();
