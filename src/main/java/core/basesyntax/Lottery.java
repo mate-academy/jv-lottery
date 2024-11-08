cf068e82a5b3fa36fc37725d077ddd6254264114
Mukuta Pyataev <pataevmikita100@gmail.com>
2024-11-08 17:39:23 +0200

    created lottery

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    
+    private static final Random random = new Random();
+
+    public static Ball getRandomBall() {
+        Color randomColor = ColorSupplier.getRandomColor();
+        int randomNumber = random.nextInt(100) + 1;
+
+        return new Ball(randomNumber,randomColor);
+    }
+}
