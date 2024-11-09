574a0a0507d2c47785c9ef66742e6daadb5b90bd
Teslenko Daniil <burgalhashem@gmail.com>
2024-11-10 01:21:59 +0200

    maked lottery task

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static  int bound = 100;
+    private ColorSupplier colorSupplier;
+
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ColorSupplier colorSupplier = new ColorSupplier();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(new Random().nextInt(bound));
         return ball;
     }
 }
