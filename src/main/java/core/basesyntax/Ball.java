14ab636ba1ae291f974bece8b0298fba7e507d43
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2024-11-12 14:05:22 +0200

    deleted redundant getters and setters for the Ball

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -10,22 +10,6 @@ public class Ball {
         this.number = number;
     }
 
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public Colors getColor() {
-        return color;
-    }
-
-    public void setColor(Colors color) {
-        this.color = color;
-    }
-
     @Override
     public String toString() {
         return "Ball"
