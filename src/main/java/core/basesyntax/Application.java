6598c4924321da48416e3191fe36b6957d763d17
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2024-11-13 01:03:07 +0200

    fixed solution after comments

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         Ball[] balls = new Ball[3];
 
-        for (Ball ball : balls) {
-            System.out.println(Lottery.getRandomBall());
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = Lottery.getRandomBall();
         }
+        System.out.println(Arrays.toString(balls));
     }
 }
