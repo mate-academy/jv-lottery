86a240d1186442864b8de727b8d7e8e104a4fd94
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2024-11-12 12:52:08 +0200

    created a Lottery that produces a randomly colored and numbered ball

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Ball[] balls = new Ball[3];
+
+        for (Ball ball : balls) {
+            System.out.println(Lottery.getRandomBall());
+        }
         // create three balls using class Lottery and print information about them in console
     }
 }
