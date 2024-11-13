6d762733ac735778b9ec97857656bcc76c527e62
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-13 22:03:50 +0000

    5

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
+
 public class Ball {
     private String color;
     private int number;
-    // Конструктор класу Ball
+    
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
+
 import java.util.Random;
+
 public class Lottery {
     private ColorSupplier colorSupplier = new ColorSupplier();
     private Random random = new Random();
