09a6c4c11d33bf0b9c8b6d554646c5ecd8947a5f
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-13 20:37:33 +0000

    o

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,10 +1,13 @@
 package core.basesyntax;
 
 public class ColorSupplier {
+    public String getRandomColor () {
+        return null;
     Random rmd = new Random();
     public Color getRandomColor() {
         int i = rmd.nextInt(Color.values().length);
         String colored = Color.values()[i];
         return colored;
     }
+}
 }
