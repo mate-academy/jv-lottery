3337ecc21286d023b812238c5123a2a79904c383
Manko Khristi <hrystynka2000a1617@gmail.com>
2024-11-14 02:48:00 +0200

    task has been completed

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public class Ball {
+    private int number;
+    private Color color;
+
+    public Ball(int number, Color color) {
+        this.number = number;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{"
+                + "number=" + number
+                + ", color=" + color
+                + '}';
+    }
+}
