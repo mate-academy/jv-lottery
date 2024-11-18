8d132bee65062d513a6697b6d9a89dd5d1d7f808
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2024-11-18 12:00:23 -0500

    3rd

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private static final Random random = new Random();
+
     public static String getRandomColor() {
         Color[] colors = Color.values();
         int randomIndex = random.nextInt(colors.length);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     private static Random random = new Random();
+
     public Ball getRandomBall(){
         String color = ColorSupplier.getRandomColor();
         int number = random.nextInt(100) + 1;
