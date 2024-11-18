ceb4e27997a0d3ad7e74be0ac18c339987088f8d
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2024-11-18 18:20:10 -0500

    fixed some mistakes 4

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -8,7 +8,6 @@ public class Lottery {
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
-
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
         int number = random.nextInt(maxBallCount) + 1;
