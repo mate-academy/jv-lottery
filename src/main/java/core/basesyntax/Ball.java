dfb0f8b69759f16a0a8d8faa01a6eab1a9af4d1e
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2024-11-18 12:10:14 -0500

    reformat

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class Ball {
-    private final Color color ;
+    private final Color color;
     private final int number;
 
     public Ball(String color, int number) {
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Lottery {
     private static Random random = new Random();
 
-    public Ball getRandomBall(){
+    public Ball getRandomBall() {
         String color = ColorSupplier.getRandomColor();
         int number = random.nextInt(100) + 1;
         return new Ball(color, number);
