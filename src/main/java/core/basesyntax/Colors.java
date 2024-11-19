f363071ceeaa6782e59271fc4d50b438e778915b
Bohdan Lysachenko <lysachenko111@ukr.net>
2024-11-19 12:11:52 +0200

    Implement Lottery

---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Colors {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        ORANGE,
+        PURPLE,
+        CYAN,
+        MAGENTA,
+        BLACK,
+        WHITE
+}
