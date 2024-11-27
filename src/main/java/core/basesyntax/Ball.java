5d641366c83cf6fc2750a7c52f03ad5a1ea0c534
natalia burtnyk <nataliaburtnyk@gmail.com>
2024-11-27 11:22:27 -0600

    Created Ball class with fields and methods.

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+public class Ball {
+    public String color;
+    public int number;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + number;
+    }
+}
