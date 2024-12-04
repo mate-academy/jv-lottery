87f7a639cba9c4031a91a0824f45611add0b20ae
Wojtek <wojka66@gmail.com>
2024-12-05 00:28:32 +0100

    Improving the final solution.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-      Ball ballOne = Lottery.getRandomBall();
-      Ball ballTwo = Lottery.getRandomBall();
-      Ball ballThree = Lottery.getRandomBall();
-      System.out.println(ballOne);
-      System.out.println(ballTwo);
-      System.out.println(ballThree);
+      Ball[] ballArray = new Ball[3];
+      for (int i = 0; i < 3; i++) {
+        ballArray[i] = Lottery.getRandomBall();
+        System.out.println(ballArray[i]);
+      }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -2,11 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-public class Lottery extends ColorSupplier{
+public class Lottery {
   public static Ball getRandomBall() {
     Random randomNumber = new Random();
     Ball randomBall = new Ball();
-    randomBall.color = getRandomColor();
+    randomBall.color = ColorSupplier.getRandomColor();
     randomBall.number = randomNumber.nextInt(101);
 
     return randomBall;
