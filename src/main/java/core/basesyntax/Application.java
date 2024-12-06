5898612d87ffa8a23a52a115b9ba227c9019b6e9
DmitryShmerchuk <Dmitryvin1992@gmail.com>
2024-12-06 14:47:18 +0200

    Ready-5

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,15 +1,31 @@
 package core.basesyntax;
+
 import main.java.core.basesyntax.Lottery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
+        int numberOfBalls = 100;
 
-        System.out.println(ball1);
-        System.out.println(ball2);
-        System.out.println(ball3);
+        List<Ball> balls = generateBalls(lottery, numberOfBalls);
+
+        printBalls(balls);
+    }
+
+    private static List<Ball> generateBalls(Lottery lottery, int numberOfBalls) {
+        List<Ball> balls = new ArrayList<>();
+        for (int i = 0; i < numberOfBalls; i++) {
+            balls.add(lottery.getRandomBall());
+        }
+        return balls;
+    }
+
+    private static void printBalls(List<Ball> balls) {
+        for (int i = 0; i < balls.size(); i++) {
+            System.out.println((i + 1) + ". " + balls.get(i));
+        }
     }
 }
