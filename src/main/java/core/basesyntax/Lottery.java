a43090681308cf207017773dbb6fadac5f0aaab4
Acid <deadfoxua1337@gmail.com>
2024-12-14 15:30:21 +0100

    stylefix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-        private static final Random RANDOM = new Random();
-        private static final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final Random RANDOM = new Random();
+    private static final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        return new Ball( colorSupplier.getRandomColor() , RANDOM.nextInt(101) );
+        return new Ball( colorSupplier.getRandomColor() , RANDOM.nextInt(101));
     }
 
 }
