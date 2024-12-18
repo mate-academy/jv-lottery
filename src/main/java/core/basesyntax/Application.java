3d3d3dbf8b1d4366eb8f0a9dd6a225a50ad59bc4
Mich Hmk <gumeniukmich@gmail.com>
2024-12-18 21:46:33 +0200

    Classes created and tested

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        int ballsToCreate = 3;
+        int numberRange = 100;
+        ColorSupplier colorSupplier = new ColorSupplier();
+        Lottery lottery = new Lottery();
+        Ball[] result = new Ball[ballsToCreate];
+
+        for (int i = 0; i < ballsToCreate; i++) {
+            result[i] = lottery.getRandomBall(colorSupplier.getRandomColor(), numberRange);
+        }
+        System.out.println(Arrays.toString(result));
     }
 }
