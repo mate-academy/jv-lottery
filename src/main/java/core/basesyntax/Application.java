1553fc6978a50d7e4cd94c53818ff6ffa78ca407
Mich Hmk <gumeniukmich@gmail.com>
2024-12-18 22:00:24 +0200

    style mistakes

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        final int BALLS_TO_CREATE = 3;
-        final int NUMBER_RANGE = 100;
+        final int Number = 3;
+        final int Range = 100;
         ColorSupplier colorSupplier = new ColorSupplier();
         Lottery lottery = new Lottery();
-        Ball[] result = new Ball[BALLS_TO_CREATE];
+        Ball[] result = new Ball[Number];
 
-        for (int i = 0; i < BALLS_TO_CREATE; i++) {
-            result[i] = lottery.getRandomBall(colorSupplier.getRandomColor(), NUMBER_RANGE);
+        for (int i = 0; i < Number; i++) {
+            result[i] = lottery.getRandomBall(colorSupplier.getRandomColor(), Range);
         }
         System.out.println(Arrays.toString(result));
     }
