569884cf90dd95200fd234baa0bcd0423e2f0d2d
Your_Name Your_Surname <your@email>
2024-12-19 17:20:51 +0200

    added fixes include style

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,14 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball1 = lottery.getRandomBall();
-        Ball ball2 = lottery.getRandomBall();
-        Ball ball3 = lottery.getRandomBall();
-        Ball[] balls = {ball1, ball2, ball3};
-        for (Ball ball: balls) {
-            System.out.println(ball);
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int maxNumber = 100;
-    private static final Random random = new Random();
+    private static final int MAX_NUMBER = 100;
+    private static final Random RANDOM = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(random.nextInt(maxNumber));
+        ball.setNumber(RANDOM.nextInt(MAX_NUMBER));
         return ball;
     }
 }
