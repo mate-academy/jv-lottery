cd2843072b43b8e57878fb48626e50f577d3ab47
Your_Name Your_Surname <your@email>
2024-12-19 20:53:15 +0200

    added fixes and created a constructor for colorSupplier and random fields

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,13 +4,18 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_NUMBER = 100;
-    private static final Random RANDOM = new Random();
-    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random;
+    private final ColorSupplier colorSupplier;
+
+    public Lottery(ColorSupplier colorSupplier, Random random) {
+        this.colorSupplier = colorSupplier;
+        this.random = random;
+    }
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(RANDOM.nextInt(MAX_NUMBER));
+        ball.setNumber(random.nextInt(MAX_NUMBER));
         return ball;
     }
 }
