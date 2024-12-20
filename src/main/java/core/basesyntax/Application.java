13d9e00881ea05fbd1ef9d91623ac3ddffb4cc20
Piotr Hic <piotrhic@gmail.com>
2024-12-20 23:54:34 +0100

    Task is done v2

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+
        Ball ball1 = new Ball();
        Ball ball2 = new Ball();
        Ball ball3 = new Ball();
+
        System.out.println(ball1);
        System.out.println(ball2);
        System.out.println(ball3);
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Lottery {
 
-    ColorSupplier colorSupplier = new ColorSupplier();
-    public Ball getRandomBall(){
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
         Random random = new Random();
