82f3ba43c5e5ccf8afece0178f65e35e8e74709c
Piotr Hic <piotrhic@gmail.com>
2024-12-21 08:07:18 +0100

    Task is done v3

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,16 +1,16 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main ( String[] args ) {
 
-        Lottery lottery = new Lottery();
+        Lottery lottery = new Lottery ( );
 
-       Ball ball1 = lottery.getRandomBall();
-       Ball ball2 = lottery.getRandomBall();
-       Ball ball3 = lottery.getRandomBall();
+        Ball ball1 = lottery.getRandomBall ( );
+        Ball ball2 = lottery.getRandomBall ( );
+        Ball ball3 = lottery.getRandomBall ( );
 
-       System.out.println(ball1);
-       System.out.println(ball2);
-       System.out.println(ball3);
+        System.out.println ( ball1 );
+        System.out.println ( ball2 );
+        System.out.println ( ball3 );
     }
 }
