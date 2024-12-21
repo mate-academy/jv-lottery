cb176ea44bb05d3a230e31d233b1721d93c83168
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2024-12-20 20:12:46 -0400

    Solved lottery task. Fixed some mistakes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,9 +4,8 @@ import java.util.Random;
 
 public class ColorSupplier {
 
-    public static Colors getRandomColor() {
+    public Colors getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
-        Colors[] color = Colors.values();
-        return color[index];
+        return Colors.values()[index];
     }
 }
