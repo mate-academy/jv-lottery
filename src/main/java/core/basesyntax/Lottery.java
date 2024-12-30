8ac2453f90d7e89f04a2a4147cc032ae399d784b
Povch Anastasia <povkhnastiya@gmail.com>
2024-12-30 18:39:10 +0200

    Homework final

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(100) + 1;
+        return new Ball(color, number);
+    }
+}
