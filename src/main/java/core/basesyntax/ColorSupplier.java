4afc9bdc95414303b5b17cd11b0173a58156dc57
Mizuo <mizuo.personal@gmail.com>
2024-12-30 01:17:56 +0100

    maybe fixed x2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final int picker = new Random().nextInt(Color.values().length);
+    private final Random picker = new Random();
 
     public Color getRandomColor() {
-        return Color.values()[picker];
+        return Color.values()[picker.nextInt(Color.values().length)];
     }
 }
