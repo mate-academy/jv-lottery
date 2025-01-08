ad1ae6c2c3d5bb7d4bdf0400d93f89b8d1495bc8
Daniil Sitailo <serzsito2709@gmail.com>
2025-01-08 22:08:13 +0200

    Created a Lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    private static final Color[] COLORS = Color.values();
+    private static final Random RANDOM = new Random();
+
+    public Color getRandomColor() {
+        return COLORS[RANDOM.nextInt(COLORS.length)];
+
     }
+
 }
