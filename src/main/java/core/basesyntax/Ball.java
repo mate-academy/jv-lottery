007329cb0911a321eb6a5b6a1a7e2d7aa1191a12
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-01-10 23:33:44 +0200

    created a lottery class with the getRandomBall method, which returns ball.

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,11 +1,25 @@
 package core.basesyntax;
 
 public class Ball {
-    private String color;
-    private int number;
+    final String color;
+    final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Information about ball:\n color: " + getColor().toLowerCase() + " number: " + getNumber();
     }
+
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public String getRandomColor() {
+    public static String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         Color color = Color.values()[index];
         return color.toString();
