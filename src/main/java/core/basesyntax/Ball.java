7eafe60954eb8b485cfd28a92e66da656c96f88f
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-01-10 23:38:31 +0200

    fixed style

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    final String color;
-    final int number;
+    private String color;
+    private int number;
 
     public Ball(String color, int number) {
         this.color = color;
@@ -19,7 +19,8 @@ public class Ball {
 
     @Override
     public String toString() {
-        return "Information about ball:\n color: " + getColor().toLowerCase() + " number: " + getNumber();
+        return "Information about ball:\n color: " + getColor().toLowerCase()
+                + " number: " + getNumber();
     }
 
 }
