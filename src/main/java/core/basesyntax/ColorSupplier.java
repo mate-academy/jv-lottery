3b45590d20ac2f7471ee116d2f2b7669c6d0dacc
Liliya <klv.liliya.klv@gmail.com>
2025-01-13 16:59:59 +0200

    created main method to test the program

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static String getRandomColor() {
-        return Color.values()[new Random().nextInt(Color.values().length)].toString();
+    public String getRandomColor() {
+        return Color.values()[new Random().nextInt(Color.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int BALL_COUNT = 100;
+
     public Ball getRandomBall() {
-        String color = ColorSupplier.getRandomColor();
-        int number = new Random().nextInt(100);
+        String color = new ColorSupplier().getRandomColor();
+        int number = new Random().nextInt(BALL_COUNT);
         return new Ball(color, number);
     }
 }
