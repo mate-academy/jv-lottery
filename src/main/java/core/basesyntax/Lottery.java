716bd18c58d3b65cd5ac1404ead1109cc0d9bda5
Liliya <klv.liliya.klv@gmail.com>
2025-01-13 16:39:11 +0200

    created class Lottery

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        String color = ColorSupplier.getRandomColor();
+        int number = new Random().nextInt(100);
+        return new Ball(color, number);
+    }
+}
