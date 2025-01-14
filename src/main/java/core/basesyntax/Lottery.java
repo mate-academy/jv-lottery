7ffa64fea48c8343df9bd4f8e96d19bbde1fe2ee
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-01-14 13:41:23 +0200

    edit style 5

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
+
         Random random = new Random();
         String color = new ColorSupplier().getRandomColor();
         int number = random.nextInt(100);
