2c6d569a3240b12aa9fc336230f4ab7f8d0fc94e
Roman Malyarenko <malyarenko.roman@gmail.com>
2025-01-15 21:20:24 +0200

    added a constant and removed unnecessary fields in the Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,13 +5,12 @@ import java.util.Random;
 public class Lottery {
     private static final int NUMBER_OF_BALLS = 100;
     private Random random = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Ball ball = new Ball();
 
     public Ball getRandomBall() {
+        Ball ball = new Ball();
         int randomNumber = random.nextInt(NUMBER_OF_BALLS);
-        String randomColor = colorSupplier.getRandomColor();
         ball.setNumber(randomNumber);
+        String randomColor = new ColorSupplier().getRandomColor();
         ball.setColor(randomColor);
         return ball;
     }
