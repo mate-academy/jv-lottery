e0928f831b554b6b4d45af7c2db79037dcd23796
Kamil Szerszen <szerszenk1997@gmail.com>
2025-01-16 22:26:13 +0100

    the task is ready for checking.

---
+++ src/main/java/core/basesyntax/Lotterry.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lotterry {
+
+    private final Random randomNumber = new Random();
+    private final ColorSupplier colors = new ColorSupplier();
+
+    public void getRandomBall() {
+        Ball ball = new Ball();
+        ball.setColor(colors.getRandomColor());
+        ball.setNumber(randomNumber.nextInt(101));
+        System.out.println(ball);
+
+    }
+}
