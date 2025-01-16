a6b4e1750763dce44085327c77cf1aded0e8dc29
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-01-16 18:39:22 +0200

    initial commit

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,6 +6,7 @@ public class Lottery {
     public Ball getRandomBall() {
 
         Random random = new Random();
+
         String color = new ColorSupplier().getRandomColor();
         int number = random.nextInt(100);
         return new Ball(color, number);
