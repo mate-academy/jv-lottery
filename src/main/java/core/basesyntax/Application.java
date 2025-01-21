c85b27dc6643ceac87ded3f066dd0bf5eb0f77ba
HSlawinski <hubi.slawinski@gmail.com>
2025-01-21 22:59:10 +0100

    Added classes: Lottery Ball Colors and ColorSupplier

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,16 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Colors[] colors = Colors.values();
+        ColorSupplier colorSupplier = new ColorSupplier(colors);
+        Lottery lottery = new Lottery(colorSupplier);
+
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
+
+        System.out.println(ball1);
+        System.out.println(ball2);
+        System.out.println(ball3);
     }
 }
