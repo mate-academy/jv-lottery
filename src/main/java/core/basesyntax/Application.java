8505c119213e560d44806c398df61f27cdfbba25
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2025-01-28 21:47:24 +0200

    solution second try

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,14 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Lottery l1 = new Lottery();
-        System.out.println(l1.getRandomBall());
-
-        Lottery l2 = new Lottery();
-        System.out.println(l2.getRandomBall());
-
-        Lottery l3 = new Lottery();
-        System.out.println(l3.getRandomBall());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(new Lottery().getRandomBall());
+        }
 
     }
 }
