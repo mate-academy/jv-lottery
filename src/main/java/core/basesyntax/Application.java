be974a7e87aebc2a72dd9e987aaa78ed3118c62b
stanislav <stanislavkovalmain@gmail.com>
2025-02-01 13:10:43 +0200

    implemented lottery functionallity

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery(); // Створюємо екземпляр Lottery
+
+        Ball ball1 = lottery.getRandomBall(); // Отримуємо першу кулю
+        Ball ball2 = lottery.getRandomBall(); // Отримуємо другу кулю
+        Ball ball3 = lottery.getRandomBall(); // Отримуємо третю кулю
+
+        System.out.println(ball1); // Виводимо інформацію про кулі
+        System.out.println(ball2);
+        System.out.println(ball3);
     }
 }
