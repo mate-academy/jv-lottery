0e8f190ec311e2bcd265281d59cb7a404f3f08b0
Nazar <nazar0302@gmail.com>
2025-02-03 16:25:20 +0200

    fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,10 +6,7 @@ public class Lottery {
     public String getRandomBall() {
         ColorSupplier color = new ColorSupplier();
         Random number = new Random();
-        StringBuilder ball = new StringBuilder();
-        ball.append(number.nextInt(100))
-                .append(" ")
-                .append(color.getRandomColor().toString());
+        Ball ball = new Ball(color.getRandomColor().toString(), number.nextInt(100));
         return ball.toString();
     }
 }
