8f762a25daec948bc2aee9f96dace5106cc6fabf
MaksymMysiuk <maks140806@gmail.com>
2025-02-04 16:22:10 +0100

    finaly

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final Random RANDOM = new Random();
+    private static final int MAX_NUMBER = 101;
+
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
-        int number = new Random().nextInt(101);
+        int number = RANDOM.nextInt(MAX_NUMBER);
         return new Ball(color, number);
     }
 }
