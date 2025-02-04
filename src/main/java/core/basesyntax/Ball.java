1a5b666fa881284c07acec333f9719eef5085c26
MaksymMysiuk <maks140806@gmail.com>
2025-02-04 15:33:14 +0100

    final

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,19 +1,35 @@
 package core.basesyntax;
 
 public class Ball {
-    public String color;
-    public int number;
+    private String color;
+    private int number;
 
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     @Override
     public String toString() {
-        return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+        return "Ball(" +
+                "color='" + color + "', " +
+                "number=" + number +
+                ")";
     }
 }
