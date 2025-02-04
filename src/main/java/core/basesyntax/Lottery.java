21a05608bd758efd3cef2118ee8b4371963dcbda
MaksymMysiuk <maks140806@gmail.com>
2025-02-04 14:40:33 +0100

    ук

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,7 @@ public class Lottery {
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         String color = colorSupplier.getRandomColor();
-        int number = new Random().nextInt(101); // Число от 0 до 100
+        int number = new Random().nextInt(101);
         return new Ball(color, number);
     }
 }
