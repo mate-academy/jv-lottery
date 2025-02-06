2232daf541707e4ec9020e4b99a1af4be22f0cea
Polina Kirf <polinakirf06@gmail.com>
2025-02-05 16:56:38 -1200

    create a lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int BALL_COUNT = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < BALL_COUNT; i++) {
             Ball ball = Lottery.getRandomBall();
             System.out.println(ball);
         }
-
     }
 }
