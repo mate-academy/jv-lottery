92c839094e75e539c089439cd5a8a6e301f5bf68
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-02-07 01:57:58 +0100

    jv-lottery-solution1

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public static color getRandomColor() {
-        int index = new Random().nextInt(color.values().length);
-        return color.values()[index];
+    public Color getRandomColor() {
+        int index = new Random().nextInt(Color.values().length);
+        return Color.values()[index];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class Lottery {
     private static final Random RANDOM = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        color randomColor = ColorSupplier.getRandomColor();
+        Color randomColor = colorSupplier.getRandomColor();
         int randomNumber = RANDOM.nextInt(100) + 1;
         return new Ball(randomColor.name(), randomNumber);
 
