0b79d1d90a693dc327e6ee6edae38ff9bda00e7a
Vladyslav Bedryk <vladikbedrikl@gmail.com>
2025-02-09 18:22:42 +0100

    fixed creating a ball with a random color and a random number

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,14 +3,11 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        ColorSupplier colorSupplier = new ColorSupplier();
         Lottery lottery = new Lottery();
         Ball[] balls = new Ball[3];
 
         for (int i = 0; i < balls.length; i++) {
-            balls[i] = new Ball();
-            balls[i].setColor(colorSupplier.getRandomColor());
-            lottery.getRandomBall(balls[i]);
+            balls[i] = lottery.getRandomBall();
             System.out.println(balls[i]);
         }
     }
