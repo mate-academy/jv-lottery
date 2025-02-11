6b2fbb1e2dc2d1d169940ddb42f8cf70c77f3782
Mykhail Yehorov <myehan9@gmail.com>
2025-02-11 11:31:09 +0200

    feature

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -10,7 +10,6 @@ public class ColorSupplier {
     public String getRandomColor() {
         int index = new Random().nextInt(Color.values().length);
         Color color = Color.values()[index];
-
         return color.name();
     }
 }
