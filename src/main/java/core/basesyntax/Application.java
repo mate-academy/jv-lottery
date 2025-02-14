38615317112e9fdbe1e397d24a22578124adc02c
Dariusz <dariusz_bereszynski@op.pl>
2025-02-14 19:37:13 +0100

    Implementation of mentor's advices and rearrange codestyle

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery[] lotteries = new Lottery[3];
+
+        for (int i = 0; i < 3; i++) {
+            lotteries[i] = new Lottery();
+            Ball randomBall = lotteries[i].getRandomBall();
+            System.out.println(randomBall);
+        }
     }
 }
+
