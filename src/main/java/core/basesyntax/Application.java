0b8e80d75c156155dbf2232da6cacea2db3e14d8
Roman Zaharii <romazagariy2003@gmail.com>
2025-02-22 11:46:38 +0200

    removed static from getRandomBall() and updated method calls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Ball ball1 = Lottery.getRandomBall();
-        Ball ball2 = Lottery.getRandomBall();
-        Ball ball3 = Lottery.getRandomBall();
+        Lottery lottery = new Lottery();
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
         System.out.println(ball1.toString());
         System.out.println(ball2.toString());
         System.out.println(ball3.toString());
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public static Ball getRandomBall() {
+    public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Color color = Color.valueOf(colorSupplier.getRandomColor());
         int number = new Random().nextInt(101);
