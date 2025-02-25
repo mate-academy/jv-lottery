28968b4c91d2a6c53f8aa5ec941d456ecd9debfb
8ACE8 <konovalovvkoop1357@gmail.com>
2025-02-25 22:28:45 +0300

    Adjust ball number generation range to 1-100

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,7 @@ public class Lottery {
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
         Random rand = new Random();
-        Ball ball = new Ball(rand.nextInt(100), colorSupplier.getRandomColor());
+        Ball ball = new Ball(rand.nextInt(100) + 1, colorSupplier.getRandomColor());
         return ball;
     }
 }
