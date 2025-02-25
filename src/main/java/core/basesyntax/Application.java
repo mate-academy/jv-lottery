1b04b0249e60b688e3211448cd77a2242798f0e1
Denis Basarab <denya2007basarab@gmail.com>
2025-02-25 16:24:32 +0200

    create 2 class Lottery, Ball and enum Colors, also change methods in classes Application and ColorSupplier

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery randomBall = new Lottery();
+        System.out.println(randomBall.getRandomBall());
+        System.out.println(randomBall.getRandomBall());
+        System.out.println(randomBall.getRandomBall());
     }
 }
---
+++ src/main/java/core/basesyntax/Colors.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public enum Colors {
+    RED,
+    BLUE,
+    ORANGE,
+    GREEN,
+    YELLOW,
+    PINK,
+    PURPLE,
+    BROWN,
+    BLACK,
+    WHITE
+}
