dad125eac000a44ef7acc4c3113433924a6b71e4
Denis Basarab <denya2007basarab@gmail.com>
2025-02-25 16:42:57 +0200

    fix method getRandomColor

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -5,6 +5,6 @@ import java.util.Random;
 public class ColorSupplier {
     public String getRandomColor() {
         int randomColorIndex = new Random().nextInt(Colors.values().length);
-        return Colors.values()[randomColorIndex].toString();
+        return Colors.values()[randomColorIndex].name();
     }
 }
