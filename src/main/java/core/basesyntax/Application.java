d461505fdbd49971f973a8032e07697d33572ecd
Yehor Nazarets <nazarets1111@gmail.com>
2025-03-02 23:11:34 +0100

    Created some methods for creating new Ball with random data. UPDATE

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,9 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery l1 = new Lottery();
-        l1.getRundomBall();
-        l1.getRundomBall();
-        l1.getRundomBall();
-
+        for (int i = 0; i < 3; i++) {
+            String str = l1.getRandomBall().toString();
+            System.out.println(str);
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,11 +5,10 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier cs = new ColorSupplier();
 
-    public Ball getRundomBall() {
+    public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(cs.getRandomColor());
         ball.setNumber(new Random().nextInt(100));
-        System.out.println(ball);
         return ball;
     }
 }
