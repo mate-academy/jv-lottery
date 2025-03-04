ca05fd156fbcbf99e5cf19d1e40d5bf3322b59bf
BoichukMaryna <marisha.boychuk97@gmail.com>
2025-03-04 16:33:22 +0000

    Improved code readability, and simplified its maintenance in Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 public class Application {
+    private static final int NUMBER_OF_BALLS = 3;  // Define the constant
+
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {  // Use the constant
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
