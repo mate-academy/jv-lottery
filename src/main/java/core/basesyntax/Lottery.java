518696e54cffa1853ae178cb42338e572ef5f862
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-09 20:55:38 +0200

    removed unnecessary code and corrected mistakes

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int NUMBER_OF_BALLS = 3;
     private static final int NUMBER_LIMIT = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
-
-    public int getNumberOfBalls() {
-        return NUMBER_OF_BALLS;
-    }
 
     public Ball getRandomBall() {
         String color = colorSupplier.getRandomColor();
