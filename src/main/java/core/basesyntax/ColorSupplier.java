e44db45fd18221735e53e73139b221567aae8d44
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-03-12 20:11:33 +0100

    Solved  task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+   private Random random = new Random();
+
     public String getRandomColor() {
-        return null;
+        Colors[] colors = Colors.values();
+        int randomIndex = random.nextInt(colors.length);
+        String randomColor = colors[randomIndex].name();
+        return randomColor;
     }
 }
