fbe80afb4368a462f00ba4f4610d579cb346b216
Oleksii Korovnichenko <korovnichenkoA@gmail.com>
2025-03-18 17:44:11 +0200

    my hm3-solution jv-lottery project version2 after tech check

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,17 +3,18 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private int randomNumber = new Random().nextInt(100);
-    private String randomColor = new ColorSupplier().getRandomColor();
-    private Ball randomBall = new Ball(randomNumber, randomColor);
+    private final int maxBalls = 100;
 
     public Ball getRandomBall() {
-        return this.randomBall;
+        int randomNumber = new Random().nextInt(maxBalls);
+        String randomColor = new ColorSupplier().getRandomColor();
+        Ball randomBall = new Ball(randomNumber, randomColor);
+        return randomBall;
     }
 
     public static void main(String[] args) {
+        Lottery newLot = new Lottery();
         for (int i = 0; i < 3; i++) {
-            Lottery newLot = new Lottery();
             System.out.println(newLot.getRandomBall());
         }
     }
