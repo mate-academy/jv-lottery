8209d229642604f7a0a1ed3968925888f5aa185d
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-03-25 20:31:37 +0100

    made a Lottery

---
+++ src/main/java/org/example/ColorSupplier.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Random;
+
+public class ColorSupplier {
+    private static final Random random = new Random();
+
+    public Color getRandomColor(){
+        Color[] colors = Color.values();
+        int index = random.nextInt(colors.length);
+        return colors[index];
+        }
+    }
+
+
---
+++ src/main/java/org/example/Lottery.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        String randomColor = String.valueOf(colorSupplier.getRandomColor());
+        int randomNumber = random.nextInt(100) + 1; // Random number between 1 and 100
+        return new Ball(randomColor, randomNumber);
+    }
+}
+
