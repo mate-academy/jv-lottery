0a1b8c2eb3d163dff551b4d5f5dbd3a57b329b91
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-03-26 18:43:57 +0100

    fixed a mistake5

--- src/main/java/org/example/Ball.java
+++ src/main/java/org/example/Ball.java
@@ -20,7 +20,7 @@ public class Ball {
     @Override
     public String toString() {
         return "Ball{"
-                + "color='" + color + '\''
+                + "color='" +  color + '\''
                 + ", number=" + number
                 + '}';
     }
