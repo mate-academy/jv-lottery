d57dbf1d739c225ee5a80da55949abbebdbc86e3
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-03-26 18:38:08 +0100

    fixed a mistake4

--- src/main/java/org/example/Ball.java
+++ src/main/java/org/example/Ball.java
@@ -9,6 +9,14 @@ public class Ball {
         this.number = number;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public String toString() {
         return "Ball{"
@@ -17,3 +25,4 @@ public class Ball {
                 + '}';
     }
 }
+
