a2c2042147049867add42239ff79eb38096f21e8
ynik404 <nik.pogrebnoy@gmail.com>
2025-04-05 13:15:39 +0300

    Solved task

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        Random random = new Random();
+        ColorSupplier colorSupplier = new ColorSupplier();
+
+        ball.setNumber(random.nextInt(100));
+        ball.setColor(colorSupplier.getRandomColor());
+        return ball;
+    }
+}
