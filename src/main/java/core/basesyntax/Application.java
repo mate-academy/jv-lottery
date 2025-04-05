97f8057896fb4efcc39ad79f8208f98b7c7efc08
ynik404 <nik.pogrebnoy@gmail.com>
2025-04-05 13:25:39 +0300

    fixed Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -5,8 +5,12 @@ public class Application {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
         Ball[] balls = new Ball[3];
+
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = lottery.getRandomBall();
+        }
+
         for (Ball ball : balls) {
-            ball = lottery.getRandomBall();
             System.out.println(ball.toString());
         }
     }
