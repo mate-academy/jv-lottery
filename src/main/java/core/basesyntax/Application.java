5e0ea1da5c96e60cca61ff3d69fa2a9aaae10c54
Mirrox-cloud <beznosmaksim0102@gmail.com>
2025-04-19 13:38:18 +0300

    solved task

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,16 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();  // Правильне ім'я класу
+
+        // Створюємо 3 випадкових м'ячі
+        Ball ball1 = lottery.getRandomBall();
+        Ball ball2 = lottery.getRandomBall();
+        Ball ball3 = lottery.getRandomBall();
+
+        // Виводимо інформацію про м'ячі
+        System.out.println(ball1);
+        System.out.println(ball2);
+        System.out.println(ball3);
     }
 }
