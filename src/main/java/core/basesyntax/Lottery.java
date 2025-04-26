478f53a648821471cd5eb0882a6393c72ef5b479
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-04-26 18:55:58 +0300

    Made edits to classes "ColorSupplier" and "Lottery"

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final Random random = new Random();
+
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
         ball.setColor(colorSupplier.getRandomColor());
-        ball.setNumber(new Random().nextInt(100));
+        ball.setNumber(random.nextInt(100));
 
         return ball;
     }
