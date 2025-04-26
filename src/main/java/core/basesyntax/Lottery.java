b321acd7b34629080abc11e580dc999e7f737b39
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-04-26 18:34:31 +0300

    Added class "Lottery". Implemented method "getRandomBall"

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setColor(colorSupplier.getRandomColor());
+        ball.setNumber(new Random().nextInt(100));
+
+        return ball;
+    }
+}
