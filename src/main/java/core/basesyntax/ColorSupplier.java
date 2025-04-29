364f88a9081adbb834890232b0efc9db7ae90199
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-29 19:38:38 +0300

    Update ci.yml

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
-public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
-    }
+import java.util.Random;
+
+        public class ColorSupplier {
+            private final Random random = new Random();
+
+            public String getRandomColor() {
+                Color[] colors = Color.values();
+                int index = random.nextInt(colors.length);
+                return colors[index].name();
+     }
 }
