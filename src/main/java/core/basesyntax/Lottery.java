e109f75a1616cc2abe645175379959c21f38e36c
Egor <llikojia162@gmail.com>
2025-05-02 18:33:20 +0200

    - created a `Ball` class with the following fields: `color` and `number` and override the `toString()` method - created an enum with different colors - implemented method `getRandomColor()` in class `ColorSupplier`. Created a `Lottery` class with the `getRandomBall()` method, which will return the ball.

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery extends ColorSupplier {
+    public Ball getRandomBall() {
+        Ball ball = new Ball();
+        ball.setNumber(new Random().nextInt(100));
+        ball.setColor(getRandomColor());
+        return ball;
+    }
+}
