349b541e0e1385e3e88f0b7814c6390bb5ae8f40
Oleksandr <odombrovskyi26@gmail.com>
2025-05-02 20:25:03 +0300

    Java/Java Core/OOP/Lottery

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.model.Ball;
+
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        Ball[] balls = new Ball[]{lottery.getRandomBall(), lottery.getRandomBall(),
+                lottery.getRandomBall()};
+        for (Ball ball : balls) {
+            System.out.println(ball);
+        }
     }
 }
