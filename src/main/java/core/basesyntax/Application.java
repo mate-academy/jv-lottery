330552165c45a5cca976bc15e48255204aa8ad04
Egor <llikojia162@gmail.com>
2025-05-02 18:38:21 +0200

    fixed random ball issue

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
+        Ball ball;
         for (int i = 0; i < 3; i++) {
-            lottery.getRandomBall();
-            System.out.println(lottery.getRandomBall());
+            ball = lottery.getRandomBall();
+            System.out.println(ball);
         }
     }
 }
